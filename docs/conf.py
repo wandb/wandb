5b18ad91b402c5477f7f9a0b69865e0ff3740062
Chris Van Pelt <vanpelt@crowdflower.com>
2017-03-26 14:51:49 -0700

    Testing from my phone while board in Point Reyes

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ import wandb
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napolean']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
