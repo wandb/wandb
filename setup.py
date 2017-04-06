0b03b81edd050b1ff3599aab35ac13a06946312d
Chris Van Pelt <vanpelt@gmail.com>
2017-04-05 22:43:37 -0700

    Bump version: 0.2.2 → 0.3.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='wandb',
-    version='0.2.2',
+    version='0.3.0',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme + '\n\n' + history,
     author="Chris Van Pelt",
--- wandb/__init__.py
+++ wandb/__init__.py
@@ -4,6 +4,6 @@ from .wandb import Api, Error
 
 __author__ = """Chris Van Pelt"""
 __email__ = 'vanpelt@wandb.ai'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 
 __all__ = ["Api", "Error"]
