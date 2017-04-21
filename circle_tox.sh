eab373de3d9b7fb7211b574ab5f4746f8a940fb8
Shawn Lewis <shlewis@gmail.com>
2017-04-21 15:40:17 -0700

    Another attempt at CircleCI parallelization.

---
+++ circle_tox.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+pyenvs="py27 py34 py35 py36"
+
+# reduce to the set we need for this circle node, and interpserse with ","
+pyenvs=$(echo $pyenvs \
+        | tr " " "\n" \
+        | awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}" \
+        | tr "\n" ",")
+
+# trim trailing ","
+TOXENV=${pyenvs%?}
+
+echo "CIRCLE_NODE_INDEX: " $CIRCLE_NODE_INDEX
+echo "CIRCLE_NODE_TOTAL: " $CIRCLE_NODE_TOTAL
+echo "TOXENV: " $TOXENV
+
+export TOXENV
+tox -v --recreate
