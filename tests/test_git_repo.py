75fa73512ac67df9da00b53bde373d1594bf6f56
Chris Van Pelt <vanpelt@gmail.com>
2017-05-23 20:32:25 -0700

    The important parts

---
+++ tests/test_git_repo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_git_repo
+----------------------------------
+
+Tests for the `wandb.GitRepo` module.
+"""
+import pytest
+from wandb import GitRepo
+from .utils import git_repo
+
+def test_last_commit(git_repo):
+    assert len(git_repo.last_commit) == 40
+
+def test_dirty(git_repo):
+    assert not git_repo.dirty
+    open("foo.txt", "wb").close()
+    git_repo.repo.index.add(["foo.txt"])
+    assert git_repo.dirty
+
+def test_remote_url(git_repo):
+    assert git_repo.remote_url is None
+
+def test_create_tag(git_repo):
+    assert git_repo.tag("foo", "My great tag").name == 'wandb/foo'
+
+def test_no_repo():
+    assert not GitRepo(root="/tmp").enabled
+
+def test_no_remote():
+    assert not GitRepo(remote=None).enabled
---
+++ wandb/git_repo.py
@@ -0,0 +1,58 @@
+from git import Repo, exc
+import os
+
+class GitRepo(object):
+    def __init__(self, root=None, remote="origin", lazy=True):
+        self.remote_name = remote
+        self.root = root
+        self._repo = None
+        if not lazy:
+            self.repo
+
+    @property
+    def repo(self):
+        if self._repo is None:
+            if self.remote_name is None:
+                self._repo = False
+            else:
+                try:
+                    self._repo = Repo(self.root or os.getcwd(), search_parent_directories=True)
+                except exc.InvalidGitRepositoryError:
+                    self._repo = False
+        return self._repo
+    
+    @property
+    def enabled(self):
+        return self.repo
+
+    @property
+    def dirty(self):
+        return self.repo.is_dirty()
+
+    @property
+    def last_commit(self):
+        if not self.repo:
+            return None
+        return self.repo.head.commit.hexsha
+
+    @property
+    def remote(self):
+        if not self.repo:
+            return None
+        try:
+            return self.repo.remotes[self.remote_name]
+        except IndexError:
+            return None
+
+    @property
+    def remote_url(self):
+        if not self.remote:
+            return None
+        return self.remote.url
+
+    def tag(self, name, message):
+        return self.repo.create_tag("wandb/"+name, message=message, force=True)
+
+    def push(self, name):
+        if self.remote:
+            return self.remote.push("wandb/"+name, force=True)
