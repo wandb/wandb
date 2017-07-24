a5b0e0849a770f187554af2f9bcdfdf4c664d07f
Chris Van Pelt <vanpelt@gmail.com>
2017-07-23 22:51:51 -0700

    abs path matching...

--- wandb/sync.py
+++ wandb/sync.py
@@ -40,9 +40,9 @@ class Sync(object):
 
     def watch(self, files=[]):
         if len(files) > 0:
-            self._handler._patterns = ["*"+file for file in files]
+            self._handler._patterns = [os.path.abspath(file) for file in files]
         else:
-            self._handler._patterns = ["*.h5", "*.hdf5", "*.json", "*.meta", "*checkpoint*"]
+            self._handler._patterns = [os.path.abspath(file) for file in ["*.h5", "*.hdf5", "*.json", "*.meta", "*checkpoint*"]]
         #TODO: upsert command line
         self._observer.start()
         print("Watching changes for %s/%s" % (self._project, self._bucket))
