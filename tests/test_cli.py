04c3d2041e32741d00c7993bb930d55f3b52fec9
Shawn Lewis <shlewis@gmail.com>
2017-10-09 20:33:08 -0700

    Fix unit tests broken by last change.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,15 +1,25 @@
-import pytest, os, traceback, click
+import pytest
+import os
+import traceback
+import click
 from wandb import __version__
 from wandb import api as wandb_api
 from wandb import cli
 from wandb import git_repo
 from click.testing import CliRunner
 from .api_mocks import *
-import netrc, signal, time
-import six, time, whaaaaat, yaml
+import netrc
+import signal
+import time
+import six
+import time
+import whaaaaat
+import yaml
 import git
 import webbrowser
 import wandb
+
+DUMMY_API_KEY = '1824812581259009ca9981580f8f8a9012409eee'
 
 try:
     # python 3.4+
@@ -20,13 +30,17 @@ except ImportError:
 except ImportError:
     pass
 
+
 @pytest.fixture
 def runner(monkeypatch):
-    monkeypatch.setattr(cli, 'api', wandb_api.Api(default_settings={'project': 'test', 'git_tag': True}, load_settings=False))
+    monkeypatch.setattr(cli, 'api', wandb_api.Api(
+        default_settings={'project': 'test', 'git_tag': True}, load_settings=False))
     monkeypatch.setattr(click, 'launch', lambda x: 1)
-    monkeypatch.setattr(whaaaaat, 'prompt', lambda x: {'project_name': 'test_model', 'files': ['weights.h5']})
+    monkeypatch.setattr(whaaaaat, 'prompt', lambda x: {
+                        'project_name': 'test_model', 'files': ['weights.h5']})
     monkeypatch.setattr(webbrowser, 'open_new_tab', lambda x: True)
     return CliRunner()
+
 
 @pytest.fixture
 def empty_netrc(monkeypatch):
@@ -36,13 +50,16 @@ def empty_netrc(monkeypatch):
             return {'api.wandb.ai': None}
     monkeypatch.setattr(netrc, "netrc", lambda *args: FakeNet())
 
+
 @pytest.fixture
 def local_netrc(monkeypatch):
-    #TODO: this seems overkill...
+    # TODO: this seems overkill...
     origexpand = os.path.expanduser
+
     def expand(path):
         return os.path.realpath("netrc") if "netrc" in path else origexpand(path)
     monkeypatch.setattr(os.path, "expanduser", expand)
+
 
 def git_repo():
     r = git.Repo.init(".")
@@ -50,6 +67,7 @@ def git_repo():
     r.index.add(["README"])
     r.index.commit("Initial commit")
     return git_repo.GitRepo(lazy=False)
+
 
 def test_help(runner):
     result = runner.invoke(cli.cli)
@@ -59,10 +77,12 @@ def test_help(runner):
     assert help_result.exit_code == 0
     assert 'Show this message and exit.' in help_result.output
 
+
 def test_version(runner):
     result = runner.invoke(cli.cli, ["--version"])
     assert result.exit_code == 0
     assert __version__ in result.output
+
 
 @pytest.mark.skip(reason='config reworked, fixes coming...')
 def test_config(runner, monkeypatch):
@@ -74,11 +94,13 @@ def test_config(runner, monkeypatch):
         assert "wandb config set" in result.output
         assert os.path.exists("config-defaults.yaml")
 
+
 @pytest.mark.skip(reason='config reworked, fixes coming...')
 def test_config_show(runner, monkeypatch):
     with runner.isolated_filesystem():
         with open("config-defaults.yaml", "w") as f:
-            f.write(yaml.dump({'val': {'value': 'awesome', 'desc': 'cool'}, 'bad': {'value':'shit'}}))
+            f.write(yaml.dump(
+                {'val': {'value': 'awesome', 'desc': 'cool'}, 'bad': {'value': 'shit'}}))
         result_py = runner.invoke(cli.config, ["show"])
         result_yml = runner.invoke(cli.config, ["show", "--format", "yaml"])
         result_json = runner.invoke(cli.config, ["show", "--format", "json"])
@@ -89,6 +111,7 @@ def test_config_show(runner, monkeypatch):
         assert "awesome" in result_yml.output
         assert "awesome" in result_json.output
 
+
 @pytest.mark.skip(reason='config reworked, fixes coming...')
 def test_config_show_empty(runner, monkeypatch):
     with runner.isolated_filesystem():
@@ -97,6 +120,7 @@ def test_config_show_empty(runner, monkeypatch):
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
         assert "No configuration" in result.output
+
 
 @pytest.mark.skip(reason='config reworked, fixes coming...')
 def test_config_set(runner):
@@ -108,16 +132,19 @@ def test_config_set(runner):
         print(traceback.print_tb(result.exc_info[2]))
         assert "foo='bar'" in result.output
 
+
 @pytest.mark.skip(reason='config reworked, fixes coming...')
 def test_config_del(runner):
     with runner.isolated_filesystem():
         with open("config-defaults.yaml", "w") as f:
-            f.write(yaml.dump({'val': {'value': 'awesome', 'desc': 'cool'}, 'bad': {'value':'shit'}}))
+            f.write(yaml.dump(
+                {'val': {'value': 'awesome', 'desc': 'cool'}, 'bad': {'value': 'shit'}}))
         result = runner.invoke(cli.config, ["del", "bad"])
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
         assert "1 parameters changed" in result.output
+
 
 @pytest.mark.skip(reason='feature disabled')
 def test_push(runner, request_mocker, query_project, upload_url, upsert_run, monkeypatch):
@@ -125,31 +152,36 @@ def test_push(runner, request_mocker, query_project, upload_url, upsert_run, mon
     upload_url(request_mocker)
     update_mock = upsert_run(request_mocker)
     with runner.isolated_filesystem():
-        #So GitRepo is in this cwd
+        # So GitRepo is in this cwd
         os.mkdir('wandb')
         monkeypatch.setattr(cli, 'api', wandb_api.Api({'project': 'test'}))
         with open("wandb/latest.yaml", "w") as f:
-            f.write(yaml.dump({'wandb_version': 1, 'test': {'value': 'success', 'desc': 'My life'}}))
+            f.write(yaml.dump({'wandb_version': 1, 'test': {
+                    'value': 'success', 'desc': 'My life'}}))
         with open('weights.h5', 'wb') as f:
             f.write(os.urandom(5000))
-        result = runner.invoke(cli.push, ['test/default', 'weights.h5', '-m', 'My description'])
+        result = runner.invoke(
+            cli.push, ['test/default', 'weights.h5', '-m', 'My description'])
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
         assert result.exit_code == 0
         assert "Updating run: test/default" in result.output
 
+
 @pytest.mark.skip(reason='feature disabled')
 def test_push_no_run(runner):
     with runner.isolated_filesystem():
         with open('weights.h5', 'wb') as f:
             f.write(os.urandom(5000))
-        result = runner.invoke(cli.push, ['weights.h5', '-p', 'test', '-m', 'Something great'])
+        result = runner.invoke(
+            cli.push, ['weights.h5', '-p', 'test', '-m', 'Something great'])
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
         assert result.exit_code == 2
         assert "Run id is required if files are specified." in result.output
+
 
 @pytest.mark.skip(reason='feature disabled')
 def test_push_dirty_git(runner, monkeypatch):
@@ -164,12 +196,14 @@ def test_push_dirty_git(runner, monkeypatch):
         repo.repo.index.add(["foo.txt"])
         monkeypatch.setattr(cli, 'api', wandb_api.Api({'project': 'test'}))
         cli.api._settings['git_tag'] = True
-        result = runner.invoke(cli.push, ["test", "foo.txt", "-p", "test", "-m", "Dirty"])
+        result = runner.invoke(
+            cli.push, ["test", "foo.txt", "-p", "test", "-m", "Dirty"])
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
         assert result.exit_code == 1
         assert "You have un-committed changes." in result.output
+
 
 @pytest.mark.skip(reason='feature disabled')
 def test_push_dirty_force_git(runner, request_mocker, query_project, upload_url, upsert_run, monkeypatch):
@@ -177,17 +211,19 @@ def test_push_dirty_force_git(runner, request_mocker, query_project, upload_url,
     upload_url(request_mocker)
     update_mock = upsert_run(request_mocker)
     with runner.isolated_filesystem():
-        #So GitRepo is in this cwd
+        # So GitRepo is in this cwd
         monkeypatch.setattr(cli, 'api', wandb_api.Api({'project': 'test'}))
         repo = git_repo()
         with open('weights.h5', 'wb') as f:
             f.write(os.urandom(100))
         repo.repo.index.add(["weights.h5"])
-        result = runner.invoke(cli.push, ["test", "weights.h5", "-f", "-p", "test", "-m", "Dirty"])
+        result = runner.invoke(
+            cli.push, ["test", "weights.h5", "-f", "-p", "test", "-m", "Dirty"])
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
         assert result.exit_code == 0
+
 
 @pytest.mark.skip(reason='feature disabled')
 def test_pull(runner, request_mocker, query_project, download_url):
@@ -205,6 +241,7 @@ def test_pull(runner, request_mocker, query_project, download_url):
         assert "File model.json" in result.output
         assert "File weights.h5" in result.output
 
+
 @pytest.mark.skip(reason='feature disabled')
 def test_pull_custom_run(runner, request_mocker, query_project, download_url):
     query_project(request_mocker)
@@ -218,6 +255,7 @@ def test_pull_custom_run(runner, request_mocker, query_project, download_url):
         assert result.exit_code == 0
         assert "Downloading: test/test" in result.output
 
+
 @pytest.mark.skip(reason='feature disabled')
 def test_pull_empty_run(runner, request_mocker, query_empty_project, download_url):
     query_empty_project(request_mocker)
@@ -229,11 +267,13 @@ def test_pull_empty_run(runner, request_mocker, query_empty_project, download_ur
     assert result.exit_code == 1
     assert "Run has no files" in result.output
 
+
 def test_projects(runner, request_mocker, query_projects):
     query_projects(request_mocker)
     result = runner.invoke(cli.projects)
     assert result.exit_code == 0
     assert "test_2 - Test model" in result.output
+
 
 @pytest.mark.skip(reason='feature disabled')
 def test_status(runner, request_mocker, query_project):
@@ -245,6 +285,7 @@ def test_status(runner, request_mocker, query_project):
         print(traceback.print_tb(result.exc_info[2]))
         assert result.exit_code == 0
         assert "/latest" in result.output
+
 
 @pytest.mark.skip(reason='feature disabled')
 def test_status_project_and_run(runner, request_mocker, query_project):
@@ -285,12 +326,14 @@ def test_restore(runner, request_mocker, query_run, monkeypatch):
         assert "Applied patch" in result.output
         assert "Restored config variables" in result.output
 
+
 def test_projects_error(runner, request_mocker, query_projects):
     query_projects(request_mocker, status_code=400)
     result = runner.invoke(cli.projects)
     assert result.exit_code == 1
     print(result.output)
     assert "Error" in result.output
+
 
 def test_init_new_login(runner, empty_netrc, local_netrc, request_mocker, query_projects, query_viewer):
     query_viewer(request_mocker)
@@ -299,7 +342,7 @@ def test_init_new_login(runner, empty_netrc, local_netrc, request_mocker, query_
         # If the test was run from a directory containing .wandb, then __stage_dir__
         # was '.wandb' when imported by api.py, reload to fix. UGH!
         reload(wandb)
-        result = runner.invoke(cli.init, input="12345\nvanpelt")
+        result = runner.invoke(cli.init, input="%s\nvanpelt" % DUMMY_API_KEY)
         print('Output: ', result.output)
         print('Exception: ', result.exception)
         print('Traceback: ', traceback.print_tb(result.exc_info[2]))
@@ -308,15 +351,17 @@ def test_init_new_login(runner, empty_netrc, local_netrc, request_mocker, query_
             generatedNetrc = f.read()
         with open("wandb/settings", "r") as f:
             generatedWandb = f.read()
-        assert "12345" in generatedNetrc
+        assert DUMMY_API_KEY in generatedNetrc
         assert "test_model" in generatedWandb
+
 
 def test_init_reinit(runner, empty_netrc, local_netrc, request_mocker, query_projects, query_viewer):
     query_viewer(request_mocker)
     query_projects(request_mocker)
     with runner.isolated_filesystem():
         os.mkdir('wandb')
-        result = runner.invoke(cli.init, input="12345\nvanpelt\ny")
+        result = runner.invoke(
+            cli.init, input="%s\nvanpelt\ny" % DUMMY_API_KEY)
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
@@ -325,8 +370,9 @@ def test_init_reinit(runner, empty_netrc, local_netrc, request_mocker, query_pro
             generatedNetrc = f.read()
         with open("wandb/settings", "r") as f:
             generatedWandb = f.read()
-        assert "12345" in generatedNetrc
+        assert DUMMY_API_KEY in generatedNetrc
         assert "test_model" in generatedWandb
+
 
 def test_init_add_login(runner, empty_netrc, local_netrc, request_mocker, query_projects, query_viewer):
     query_viewer(request_mocker)
@@ -334,7 +380,7 @@ def test_init_add_login(runner, empty_netrc, local_netrc, request_mocker, query_
     with runner.isolated_filesystem():
         with open("netrc", "w") as f:
             f.write("previous config")
-        result = runner.invoke(cli.init, input="12345\nvanpelt\n")
+        result = runner.invoke(cli.init, input="%s\nvanpelt\n" % DUMMY_API_KEY)
         print(result.output)
         print(result.exception)
         print(traceback.print_tb(result.exc_info[2]))
@@ -343,8 +389,9 @@ def test_init_add_login(runner, empty_netrc, local_netrc, request_mocker, query_
             generatedNetrc = f.read()
         with open("wandb/settings", "r") as f:
             generatedWandb = f.read()
-        assert "12345" in generatedNetrc
+        assert DUMMY_API_KEY in generatedNetrc
         assert "previous config" in generatedNetrc
+
 
 def test_init_existing_login(runner, local_netrc, request_mocker, query_projects, query_viewer):
     query_viewer(request_mocker)
