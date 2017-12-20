dd0f6f20d5c6df7e64875265ac9b89896e7a2ba1
Shawn Lewis <shlewis@gmail.com>
2017-12-19 16:11:48 -0800

    Bump version: 0.4.36 → 0.4.37

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='wandb',
-    version='0.4.36',
+    version='0.4.37',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     author="Chris Van Pelt",
--- wandb/__init__.py
+++ wandb/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Chris Van Pelt"""
 __email__ = 'vanpelt@wandb.com'
-__version__ = '0.4.36'
+__version__ = '0.4.37'
 
 import click
 import types
