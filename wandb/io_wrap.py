02a65b0bc02f6c6a2680b26014aea72c3a924ee9
Adrian Bauer <agbauer@gmail.com>
2018-01-09 13:46:58 -0800

    Fix optional file methods in FileTee, re-add .isatty() so Keras progress bars use carriage returns instead of newlines (but doesn't help for wandb agent)

--- wandb/io_wrap.py
+++ wandb/io_wrap.py
@@ -68,15 +68,24 @@ class FileTee(object):
         for q in self._queues:
             q.put(message)
 
-    # we implement the optional parts of the file interface in case the user's program uses them
+    # we implement the optional parts of the file interface so these FileTee objects
+    # can be used as in case the user's program uses them
     def fileno(self):
-        return self._orig_stream.fileno()
+        return self._sync_file.fileno()
 
     def flush(self):
-        self._orig_stream.flush()
+        self._sync_file.flush()
 
     def close(self):
-        self._queue.put(None)
+        self._sync_file.close()
+        for q in self._queues:
+            q.put(None)
+
+    def isatty(self):
+        if hasattr(self._sync_file, 'isatty'):
+            return self._sync_file.isatty()
+        else:
+            return False
 
 
 def spawn_reader_writer(get_data_fn, put_data_fn):
