4f34c93ef3b5cc7bf7ffb765ae83fb51eb5dcbf4
Chris Van Pelt <vanpelt@gmail.com>
2018-02-02 19:33:15 -0700

    Initial meta work

---
+++ wandb/meta.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+from wandb import util
+
+METADATA_FNAME = 'wandb-metadata.json'
+
+
+class Meta(object):
+    """Used to store summary metrics during and after a run."""
+
+    def __init__(self, out_dir='.'):
+        self.fname = os.path.join(out_dir, METADATA_FNAME)
+        try:
+            self.data = json.load(open(self.fname))
+        except IOError:
+            self.data = {}
+
+    def write(self):
+        with open(self.fname, 'w') as f:
+            s = util.json_dumps_safer(self.data, indent=4)
+            f.write(s)
+            f.write('\n')
