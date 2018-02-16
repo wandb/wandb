81386161886acd6dce77101d50f3c89fd9cee8ba
Shawn Lewis <shlewis@gmail.com>
2018-02-16 15:09:34 -0800

    Bump version: 0.4.46 → 0.5.0

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import, print_function
 
 __author__ = """Chris Van Pelt"""
 __email__ = 'vanpelt@wandb.com'
-__version__ = '0.4.46'
+__version__ = '0.5.0'
 
 import atexit
 import click
