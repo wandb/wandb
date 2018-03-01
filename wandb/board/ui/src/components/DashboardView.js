77839586af32f7ba619de7fb29ce0d2f4bd11267
Chris Van Pelt <vanpelt@gmail.com>
2018-02-28 17:13:24 -0800

    Basic edit mode in place, improved UI

--- wandb/board/ui/src/components/DashboardView.js
+++ wandb/board/ui/src/components/DashboardView.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactGridLayout, {WidthProvider} from 'react-grid-layout';
 import Panel from './Panel';
-import {Button, Icon} from 'semantic-ui-react';
+import {Button, Icon, Modal} from 'semantic-ui-react';
 import _ from 'underscore';
 import './DashboardView.css';
 
@@ -20,32 +20,77 @@ class DashboardView extends Component {
     );
   };
 
+  renderPanel(panelConfig, i, edit) {
+    return (
+      <Panel
+        editMode={edit}
+        type={panelConfig.viewType}
+        size={panelConfig.size}
+        config={panelConfig}
+        data={this.props.data}
+        updateType={newType =>
+          this.props.updatePanel(i, {
+            ...panelConfig,
+            viewType: newType,
+          })
+        }
+        updateSize={newSize =>
+          this.props.updatePanel(i, {
+            ...panelConfig,
+            size: newSize,
+          })
+        }
+        panelIndex={this.props.id}
+        updateConfig={config =>
+          this.props.updatePanel(i, {
+            ...panelConfig,
+            config: config,
+          })
+        }
+        removePanel={() => this.props.removePanel(i)}
+      />
+    );
+  }
+
   render() {
-    const {data, editMode, width} = this.props;
+    const {editMode, width} = this.props;
     return (
       <div>
         {editMode && (
-          <Button
-            icon
-            size="tiny"
-            style={{position: 'absolute', right: 5, top: 5, zIndex: 102}}
-            onClick={() => {
-              this.props.addPanel({
-                i: this.props.config.length.toString(),
-                x: 0,
-                y: 0,
-                w: 3,
-                h: 2,
-                minW: 3,
-                minH: 1,
-              });
-            }}>
-            <Icon name="plus" />
-          </Button>
+          <span style={{position: 'absolute', right: 5, top: 5, zIndex: 102}}>
+            <Button
+              icon
+              size="tiny"
+              onClick={() => {
+                this.props.addPanel({
+                  i: this.props.config.length.toString(),
+                  x: 0,
+                  y: 0,
+                  w: 3,
+                  h: 2,
+                  minW: 3,
+                  minH: 1,
+                });
+              }}>
+              <Icon name="plus" />
+            </Button>
+            <Button
+              size="tiny"
+              color="green"
+              content="Save"
+              disabled={!this.props.isModified}
+              onClick={() => {
+                this.setState({editMode: false});
+                this.props.updateViews(JSON.stringify(this.props.viewState));
+              }}
+            />
+          </span>
         )}
         <Grid
           className={editMode ? 'editing' : 'display'}
+          draggableCancel=".edit"
           layout={this.props.config || []}
+          verticalCompact={false}
           cols={GRID_WIDTH}
           isDraggable={editMode}
           isResizable={editMode}
@@ -53,33 +98,21 @@ class DashboardView extends Component {
           onLayoutChange={this.onLayoutChange}>
           {this.props.config.map((panelConfig, i) => (
             <div key={i} className="panel">
-              <Panel
-                editMode={this.props.editMode}
-                type={panelConfig.viewType}
-                size={panelConfig.size}
-                config={panelConfig}
-                data={data}
-                updateType={newType =>
-                  this.props.updatePanel(i, {
-                    ...panelConfig,
-                    viewType: newType,
-                  })
+              <Modal
+                dimmer={false}
+                trigger={<Icon link name="edit" />}
+                header="Edit panel"
+                content={
+                  <div style={{padding: 10}}>
+                    {this.renderPanel(panelConfig, i, true)}
+                  </div>
                 }
-                updateSize={newSize =>
-                  this.props.updatePanel(i, {
-                    ...panelConfig,
-                    size: newSize,
-                  })
-                }
-                panelIndex={this.props.id}
-                updateConfig={config =>
-                  this.props.updatePanel(i, {
-                    ...panelConfig,
-                    config: config,
-                  })
-                }
-                removePanel={() => this.props.removePanel(i)}
+                actions={[
+                  'Cancel',
+                  {key: 'save', content: 'Save', positive: true},
+                ]}
               />
+              {this.renderPanel(panelConfig, i)}
             </div>
           ))}
         </Grid>
