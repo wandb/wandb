153145e9e194bb72fb4062fc759a91f152179719
Lukas Biewald <lukeb@cs.stanford.edu>
2018-03-05 20:19:30 -0800

    handle special summary variables

--- wandb/board/ui/src/components/RunViewer.js
+++ wandb/board/ui/src/components/RunViewer.js
@@ -285,19 +285,6 @@ export default class RunViewer extends React.Component {
                       </List.Content>
                     </List.Item>
                   ))}
-                {Object.keys(summaryMetrics)
-                  .filter(key => key.slice(0, 1) === '_')
-                  .sort()
-                  .map((key, i) => (
-                    <List.Item key={'summary' + i}>
-                      <List.Content>
-                        <List.Header style={{color: 'grey'}}>{key}</List.Header>
-                        <List.Description style={{color: 'grey'}}>
-                          {numeral(summaryMetrics[key]).format('0.[000]')}
-                        </List.Description>
-                      </List.Content>
-                    </List.Item>
-                  ))}
               </List>
             )}
             {Object.keys(summaryMetrics).length == 0 && (
