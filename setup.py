d1f11d651f5d89d95c8111b3cd969e1a6a5ca00e
Chris Van Pelt <vanpelt@gmail.com>
2018-03-08 11:30:56 -0800

    Bump version: 0.5.7 → 0.5.8

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ test_requirements = [
 
 setup(
     name='wandb',
-    version='0.5.7',
+    version='0.5.8',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     author="Chris Van Pelt",
