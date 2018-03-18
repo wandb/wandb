625f4e214ec05698c07a25a75b9530513f7fc25a
Shawn Lewis <shlewis@gmail.com>
2018-03-18 12:31:26 -0700

    Ability to remove charts from Runs page entirely.

--- wandb/board/ui/src/containers/Runs.js
+++ wandb/board/ui/src/containers/Runs.js
@@ -37,6 +37,7 @@ import {
   setServerViews,
   setBrowserViews,
   setActiveView,
+  addView,
 } from '../actions/view';
 import update from 'immutability-helper';
 import {BOARD} from '../util/board';
@@ -261,19 +262,21 @@ class Runs extends React.Component {
             </Grid.Column>
           </Grid.Row>
           <Grid.Column width={16}>
-            <ViewModifier
-              viewType="runs"
-              data={this.props.data}
-              pageQuery={this.props.query}
-              updateViews={views =>
-                this.props.updateModel({
-                  entityName: this.props.match.params.entity,
-                  name: this.props.match.params.model,
-                  id: this.props.projectID,
-                  views: views,
-                })
-              }
-            />
+            {this.props.haveViews && (
+              <ViewModifier
+                viewType="runs"
+                data={this.props.data}
+                pageQuery={this.props.query}
+                updateViews={views =>
+                  this.props.updateModel({
+                    entityName: this.props.match.params.entity,
+                    name: this.props.match.params.model,
+                    id: this.props.projectID,
+                    views: views,
+                  })
+                }
+              />
+            )}
           </Grid.Column>
           <Grid.Column width={16} style={{zIndex: 2}}>
             <Popup
@@ -286,6 +289,14 @@ class Runs extends React.Component {
               on="click"
               position="bottom left"
             />
+            {!this.props.haveViews && (
+              <Button
+                floated="right"
+                content="Add Charts"
+                icon="area chart"
+                onClick={() => this.props.addView('runs', 'New View', [])}
+              />
+            )}
             <Button
               floated="right"
               disabled={this.props.data.selectedRuns.length === 0}
@@ -381,6 +392,9 @@ function mapStateToProps(state, ownProps) {
     reduxServerViews: state.views.server,
     reduxBrowserViews: state.views.browser,
     activeView: state.views.other.runs.activeView,
+    haveViews:
+      !_.isEqual(state.views.browser, state.views.server) ||
+      state.views.browser.runs.tabs.length > 0,
   };
 }
 
@@ -397,6 +411,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
       setBrowserViews,
       setActiveView,
       resetViews,
+      addView,
     },
     dispatch,
   );
