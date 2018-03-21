1d800854aeeda414c269e7e8bd808abfbae115ff
vlad <vladimir@gearsaid.com>
2018-03-21 10:54:15 +0100

    Improve test coverage

--- wandb/board/ui/src/setupTests.js
+++ wandb/board/ui/src/setupTests.js
@@ -1,12 +1,24 @@
 import Enzyme, {shallow, render, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {expect} from 'chai';
+import chai, {expect} from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 import configureStore from 'redux-mock-store';
+chai.use(sinonChai);
 
 Enzyme.configure({adapter: new Adapter()});
 
+// TODO: replace it with jest-localstorage-mock
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+global.sinon = sinon;
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 global.expect = expect;
 global.mockStore = configureStore();
+global.localStorage = localStorageMock;
