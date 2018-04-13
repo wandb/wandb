7cc1e525f1422426e1a5eac37d72cff815e7aa76
Lukas Biewald <lukeb@cs.stanford.edu>
2018-04-12 20:18:14 -0700

    Fix up the footer

--- wandb/board/ui/src/components/Footer.js
+++ wandb/board/ui/src/components/Footer.js
@@ -5,17 +5,17 @@ import whitelogo from '../assets/wandb-long-white.svg';
 
 const Footer = () => (
   <Segment className="footer" vertical>
-    <Container textAlign="center">
+    <Container>
       <Grid columns={2} stackable>
-        <Grid.Column textAlign="left" verticalAlign="bottom">
+        <Grid.Column textAlign="left">
           <img
             src={document.body.style.background === '#55565B' ? white : logo}
-            style={{opacity: 0.7, height: '3em'}}
+            style={{opacity: 0.7, height: '3em', textAlign: 'left'}}
             className="logo"
             alt="Weights & Biases"
           />
         </Grid.Column>
-        <Grid.Column textAlign="right" verticalAlign="middle">
+        <Grid.Column textAlign="right" verticalAlign="bottom">
           <List link horizontal>
             <List.Item as="a" target="_blank" href="http://docs.wandb.com">
               Documentation
