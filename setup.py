2c327dcec8c7213c785ef230ed9aefa964f708d9
Chris Van Pelt <vanpelt@gmail.com>
2018-05-15 12:07:36 -0700

    Lower requirments for pypy

--- setup.py
+++ setup.py
@@ -14,13 +14,13 @@ requirements = [
     'six>=1.10.0',
     'psutil>=5.2.2',
     'watchdog>=0.8.3',
-    'GitPython>=2.1.8',
+    'GitPython>=1.0.0',
     'shortuuid>=0.5.0',
     'nvidia-ml-py3>=7.352.0',
     'flask-cors>=3.0.3',
     'flask-graphql>=1.4.0',
     'graphene>=2.0.0',
-    'python-dateutil>=2.7.2'
+    'python-dateutil>=2.6.1'
 ]
 
 test_requirements = [
