3140f320e14d9072671eeabe18748204214dee89
Chris Van Pelt <vanpelt@gmail.com>
2018-05-21 16:47:28 -0700

    Fix replace

--- wandb/jupyter.py
+++ wandb/jupyter.py
@@ -6,7 +6,6 @@ class Run(object):
         self.run = run or wandb.run
 
     def _repr_html_(self):
-        url = self.run.get_url().replace('https://app.wandb.ai',
-                                         'http://app.test') + "?jupyter=true"
+        url = self.run.get_url()+"?jupyter=true"
         return '''<iframe src="%s" style="border:none;width:100%%;height:420px">
         </iframe>''' % url
