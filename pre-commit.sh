3dc8052899e7a86cd45fb0d2c3ba0820478e4b63
Shawn Lewis <shlewis@gmail.com>
2018-05-23 16:42:05 -0700

    Client pre-commit

---
+++ pre-commit.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+#
+# Git pre-commit script that runs our unit tests as needed.
+#
+# To install:
+#   ln -s ../../pre-commit.sh .git/hooks/pre-commit
+
+# Redirect output to stderr.
+exec 1>&2
+
+function log() {
+    echo 'pre-commit:' $@
+}
+
+function dirModified() {
+    # sets is_modified if files in directory $1 have been modified.
+    changed_files="$(git diff --cached --name-only --diff-filter=ACM $1 | wc -l | tr -d '[:space:]')"
+    if [ "$changed_files" != "0" ]; then
+        is_modified=true
+    else
+        is_modified=false
+    fi
+    if [ "$is_modified" = true ]; then
+        log Modifications detected in "$1"
+    fi
+}
+
+# cd to root (may not need this)
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+cd "$ROOT_DIR"
+
+log 'Running pre-commit checks, pass --no-verify to "git commit" to'
+log 'disable.'
+echo
+
+dirModified wandb
+wandb_modified="$is_modified"
+
+if [ "$wandb_modified" = true ]; then
+    log "Running wandb tests."
+    pytest
+    if [ $? -ne 0 ]; then
+        wandb_failed=true
+    fi
+fi
+
+failed=false
+if [ "$wandb_failed" = true ]; then
+    log "wandb tests failed."
+    log "\"cd $ROOT_DIR; pytest\" to run the tests."
+    failed=true
+fi
+
+if [ "$failed" = true ]; then
+    exit 1
+else
+    log "pre-commit check success"
+fi
