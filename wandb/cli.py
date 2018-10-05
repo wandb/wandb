16ea1297c5824a41f1c052cbbe672a21f7a1b29d
Chris Van Pelt <vanpelt@gmail.com>
2018-10-04 23:37:29 -0700

    Fix CI

--- wandb/cli.py
+++ wandb/cli.py
@@ -404,6 +404,8 @@ def pull(project, run, entity):
             click.echo("File %s is up to date" % name)
         else:
             length, response = api.download_file(urls[name]['url'])
+            #TODO: I had to add this because some versions in CI broke click.progressbar
+            sys.stdout.write("File %s\r" % name)
             with click.progressbar(length=length, label='File %s' % name,
                                    fill_char=click.style('&', fg='green')) as bar:
                 with open(name, "wb") as f:
