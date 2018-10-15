cfbf9af57d8e9fca56ea455017ba33efe081eeea
Chris Van Pelt <vanpelt@gmail.com>
2018-10-14 21:16:37 -0700

    Fix CI

--- install-ml.py
+++ install-ml.py
@@ -3,8 +3,6 @@ import sys
 from subprocess import call
 
 version = "".join([str(v) for v in sys.version_info[:2]])
-if version == "27":
-    call(["pip", "install", "http://download.pytorch.org/whl/cpu/torch-0.4.0-cp27-cp27mu-linux_x86_64.whl"])
-else:
-    call(["pip", "install",
-          "http://download.pytorch.org/whl/cpu/torch-0.4.0-cp{version}-cp{version}m-linux_x86_64.whl".format(version=version)])
+call(["pip", "install", "torch_nightly", "-f",
+      "https://download.pytorch.org/whl/nightly/cu90/torch_nightly.html"])
+call(["pip", "install", "torchvision-nightly"])
