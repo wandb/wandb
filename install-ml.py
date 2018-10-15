c1d8cb9c8f3b52f7e44afd05bed8ad1c06de2316
Chris Van Pelt <vanpelt@gmail.com>
2018-10-14 21:40:24 -0700

    Really fix CI

--- install-ml.py
+++ install-ml.py
@@ -4,5 +4,5 @@ from subprocess import call
 
 version = "".join([str(v) for v in sys.version_info[:2]])
 call(["pip", "install", "torch_nightly", "-f",
-      "https://download.pytorch.org/whl/nightly/cu90/torch_nightly.html"])
+      "https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html"])
 call(["pip", "install", "torchvision-nightly"])
