9503522626e9cfe4d1352c0cc937a6716cbf25b2
Chris Van Pelt <vanpelt@gmail.com>
2018-10-17 17:50:08 -0700

    Python 2 test with old torch

--- install-ml.py
+++ install-ml.py
@@ -3,6 +3,10 @@ import sys
 from subprocess import call
 
 version = "".join([str(v) for v in sys.version_info[:2]])
-call(["pip", "install", "torch_nightly", "-f",
-      "https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html"])
-call(["pip", "install", "torchvision-nightly"])
+if version == "27":
+    call(["pip", "install", "http://download.pytorch.org/whl/cpu/torch-0.4.1-cp27-cp27mu-linux_x86_64.whl"])
+    call(["pip", "install", "torchvision"])
+else:
+    call(["pip", "install", "torch_nightly", "-f",
+          "https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html"])
+    call(["pip", "install", "torchvision-nightly"])
