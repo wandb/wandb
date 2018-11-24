7dcf5db3d87c6b7ba0481e1ac2f19641e8c1c9e2
Chris Van Pelt <vanpelt@gmail.com>
2018-11-24 11:29:08 -0800

    Try a sleep...

--- tests/test_headless.py
+++ tests/test_headless.py
@@ -19,6 +19,8 @@ def test_dry_run(runner):
         try:
             res = sh.python("train.py")
             print("RUN", res)
+            # Added this to ensure we wait for file modifications...
+            time.sleep(1)
             run_dir = glob.glob("wandb/dry*")[0]
             meta = json.loads(open(run_dir + "/wandb-metadata.json").read())
             assert meta["state"] == "finished"
