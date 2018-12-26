37698d48bbdccb84b1ea4a26d94adedf19048d0e
Hugo <hugovk@users.noreply.github.com>
2018-12-26 15:49:57 +0200

    Add python_requires to help pip

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
     license="MIT license",
     zip_safe=False,
     keywords='wandb',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
