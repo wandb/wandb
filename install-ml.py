63c96cacb1bfffb8eded09987f81def8fa30594b
Chris Van Pelt <vanpelt@gmail.com>
2019-01-21 13:47:13 -0800

    Switch to main python images on dockerhub

--- install-ml.py
+++ install-ml.py
@@ -4,5 +4,6 @@ from subprocess import call
 
 version = "".join([str(v) for v in sys.version_info[:2]])
 if version == "27":
-    call(["pip", "install", "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.12.0-cp27-none-linux_x86_64.whl"])
+    pass
+    #call(["pip", "install", "https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.12.0-cp27-none-linux_x86_64.whl"])
 call(["pip", "install", "torch", "torchvision", "tensorflow"])
