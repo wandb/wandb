31b4fc0745cf8318ed4dca7c5b63e31e7d72395a
Adrian Bauer <agbauer@gmail.com>
2019-03-18 13:42:51 -0700

    Unfinished nested summary structure that updates the summary on disk every time the user changes something.

--- wandb/summary.py
+++ wandb/summary.py
@@ -23,71 +23,161 @@ h5py = util.get_module("h5py")
 np = util.get_module("numpy")
 
 
-class Summary(object):
+class NestedDict(object):
+    """Nested dict-like object that reports "set" operations to a root object.
+    """
+    def __init__(self, root=None, path=()):
+        if root is None:
+            self._root = self
+        else:
+            self._root = root
+        self._path = path
+        self._dict = {}
+
+    def _mark_dirty(self, path):
+        raise NotImplementedError
+
+    def __getitem__(self, k):
+        return self._dict[k]
+
+    def __contains__(self, k):
+        return k in self._dict
+
+    def __setitem__(self, k, v):
+        path = self._path + (k,)
+
+        if isinstance(v, dict):
+            self._dict[k] = NestedDict(self._root, path)
+            self._dict[k].update(v)
+        else:
+            self._dict[k] = v
+
+        if self._root is not self:
+            self._root._mark_dirty(path)
+
+        return v
+
+    def __delitem__(self, k):
+        del self._dict[k]
+        self._root._mark_dirty(self._path + (k,))
+
+    def update(self, d):
+        for k, v in six.iteritems(d):
+            self[k] = v
+
+
+class Summary(NestedDict):
     """Used to store summary metrics during and after a run."""
 
     def __init__(self, run, summary=None):
+        super(Summary, self).__init__()
         self._run = run
-        self._summary = summary or {}
         self._h5_path = os.path.join(self._run.dir, DEEP_SUMMARY_FNAME)
         # Lazy load the h5 file
         self._h5 = None
         self._locked_keys = set()
 
-        # Mapping of Python `id()`'s' to dicts representing large objects that
-        # were present when we last encoded this summary. We use this to keep 
-        # track of what we need to update the next time we write this summary.
-        # This means that for many types of object we assume that once
-        # they've been set in the summary they don't change.
-        #
-        # TODO(adrian): right now we only use this for DataFrames. Other large
-        # objects (h5 stuff, images) should probably go in here as well.
-        self._encoded_objects = {}
+        # Mirrored version of self._dict that gets written to JSON
+        # kept up to date by self._mark_dirty().
+        self._json_dict = {}
+
+        if summary is not None:
+            self.update(summary)
 
     def _write(self, commit=False):
         raise NotImplementedError
 
-    def __getitem__(self, k):
-        return self._decode(k, self._summary[k])
+    def get(self, k, default=None):
+        raise NotImplementedError  # XXX
+        return self._summary.get(k.strip(), default)
 
     def __setitem__(self, k, v):
-        key = k.strip()
-        self._summary[key] = v
-        self._locked_keys.add(key)
+        k = k.strip()
+        super(Summary, self).__setitem__(k, v)
+        self._locked_keys.add(k)
         self._write()
+        return v
 
     def __setattr__(self, k, v):
         if k.startswith("_"):
             super(Summary, self).__setattr__(k, v)
         else:
-            key = k.strip()
-            self._summary[key] = v
-            self._locked_keys.add(key)
-            self._write()
+            self[k] = v
+            return v
 
     def __getattr__(self, k):
         if k.startswith("_"):
             return super(Summary, self).__getattr__(k)
         else:
-            # TODO(adrian): should probably get rid of the strip()'s here.
-            return self._decode(k.strip(), self._summary[k.strip()])
+            return self[k]
 
     def __delitem__(self, k):
+        """
         h5_key = "summary/" + k.strip()
         if self._h5 and h5_key in self._h5:
             del self._h5[h5_key]
             self._h5.flush()
-        del self._summary[k.strip()]
+        """
+        # XXX remove from self._locked_keys
+        del super(Summary, self)[k.strip()]
         self._write()
 
     def __repr__(self):
-        return str(self._summary)
+        return repr(self._dict)
 
     def keys(self):
-        return self._summary.keys()
+        return self._dict.keys()
 
-    def get(self, k, default=None):
-        return self._summary.get(k.strip(), default)
+    def update(self, key_vals=None, overwrite=True):
+        """Passing overwrite=True locks any keys that are passed in.
+
+        Locked keys can only be overwritten by passing overwrite=True.
+        """
+        if key_vals:
+            write_keys = set(key_vals.keys())
+            if not overwrite:
+                # Overwrite keys that haven't been set even if they're in
+                # locked keys. May not be necessary if we update locked keys
+                # in del.
+                write_keys -= self._locked_keys & set(self._dict.keys())
+            write_key_vals = dict((k, key_vals[k]) for k in write_keys)
+            super(Summary, self).update(write_key_vals)
+            if overwrite:
+                self._locked_keys += write_keys
+
+        """
+            for k, v in six.iteritems(key_vals):
+                key = k.strip()
+                if overwrite or key not in self._summary or key not in self._locked_keys:
+                    summary[key] = v
+                if overwrite:
+                    self._locked_keys.add(key)
+        self._summary.update(summary)
+        """
+        self._write(commit=True)
+
+    def _mark_dirty(self, path):
+        last_dict = None
+        value = self
+        for i, key in enumerate(path):
+            if key in value:
+                last_dict = value
+                print(path, key, value)
+                value = value[key]
+
+        # XXX i and key may not be set
+        if i == len(path):  # set or updated value
+            json_dict = self._json_dict
+            for key in path[:-1]:
+                json_dict = json_dict[key]
+            json_dict[path[-1]] = self._encode(value, path)
+        else:  # deleted the key at `path`
+            assert i == (len(path) - 1), \
+                'Nonexistant path {} marked dirty but only {} exists.'.format(path, path[:i])
+
+
+
+
 
     def write_h5(self, key, val):
         # ensure the file is open
@@ -134,68 +224,43 @@ class Summary(object):
         else:
             return v
 
-    def _encode(self, child=None, path_from_root=[], json_root=None):
+    def _encode(self, value, path_from_root):
         """Normalize, compress, and encode sub-objects for backend storage.
 
-        This is not threadsafe.
-
-        child: Summary `dict` to encode. Defaults to `self._summary` but may
-            be any sub-`dict` instead.
-        path_from_root: `list` of key strings from the top-level summary to the
-            current `child`.
-        json_root: The new root dictionary for JSON encoding.
+        value: Object to encode.
+        path_from_root: `tuple` of key strings from the top-level summary to the
+            current `value`.
 
         Returns:
             A new tree of dict's with large objects replaced with dictionaries
             with "_type" entries that say which type the original data was.
         """
 
-        # Constructs a new `dict` tree in `json_child` that discards and/or
+        # Constructs a new `dict` tree in `json_value` that discards and/or
         # encodes objects that aren't JSON serializable.
 
-        json_child = {}
+        if isinstance(value, dict):
+            json_value = {}
+            for key, value in six.iteritems(value):
+                json_value[key] = self._encode(value, path_from_root + (key,))
+            return json_value
+        else:
+            if util.is_pandas_data_frame(value):
+                return util.encode_data_frame(key, value, self._run)
+            else:
+                path = ".".join(path_from_root)
+                friendly_value, converted = util.json_friendly(data_types.val_to_json(key, value))
+                json_value, compressed = util.maybe_compress_summary(friendly_value, util.get_h5_typename(value))
+                if compressed:
+                    self.write_h5(path, friendly_value)
 
-        if child is None:
-            child = self._summary
-
-        if json_root is None:
-            json_root = json_child
-
-        for key, value in six.iteritems(child):
-            path = ".".join(path_from_root + [key])
+                return json_value
+        """
             if isinstance(value, dict):
                 json_child[key], converted = util.json_friendly(
-                    self._encode(value, path_from_root + [key], json_root))
+                    self._encode(value, path_from_root + [key]))
             else:
-                vid = id(value)
-                if vid not in self._encoded_objects:
-                    if util.is_pandas_data_frame(value):
-                        self._encoded_objects[vid] = util.encode_data_frame(key, value, self._run)
-                    else:
-                        friendly_val, converted = util.json_friendly(
-                            data_types.val_to_json(key, value))
-                        self._encoded_objects[vid], compressed = util.maybe_compress_summary(
-                            friendly_val, util.get_h5_typename(value))
-                        if compressed:
-                            self.write_h5(path, friendly_val)
-
-                json_child[key] = self._encoded_objects[vid]
-
-        return json_child
-
-    def update(self, key_vals=None, overwrite=True):
-        # Passing overwrite=True locks any keys that are passed in
-        # Locked keys can only be overwritten by passing overwrite=True
-        summary = {}
-        if key_vals:
-            for k, v in six.iteritems(key_vals):
-                key = k.strip()
-                if overwrite or key not in self._summary or key not in self._locked_keys:
-                    summary[key] = v
-                if overwrite:
-                    self._locked_keys.add(key)
-        self._summary.update(summary)
-        self._write(commit=True)
+        """
 
 
 def download_h5(run, entity=None, project=None, out_dir=None):
@@ -231,7 +296,7 @@ class FileSummary(Summary):
     def _write(self, commit=False):
         # TODO: we just ignore commit to ensure backward capability
         with open(self._fname, 'w') as f:
-            s = util.json_dumps_safer(self._encode(), indent=4)
+            s = util.json_dumps_safer(self._json_dict, indent=4)
             f.write(s)
             f.write('\n')
         if self._h5:
@@ -259,7 +324,7 @@ class HTTPSummary(Summary):
                 self._h5.close()
                 self._h5 = None
             res = self._client.execute(mutation, variable_values={
-                'id': self._run.storage_id, 'summaryMetrics': util.json_dumps_safer(self._encode())})
+                'id': self._run.storage_id, 'summaryMetrics': util.json_dumps_safer(self._json_dict)})
             assert res['upsertBucket']['bucket']['id']
             entity, project, run = self._run.path
             if os.path.exists(self._h5_path) and os.path.getmtime(self._h5_path) >= self._started:
