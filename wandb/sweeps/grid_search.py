8e34ac3d69252deee397fae33f87cb31b15e3a4e
Jeff Raubitschek <jeff@wandb.com>
2019-04-30 13:44:00 -0700

    Change string formatting for python compat

--- wandb/sweeps/grid_search.py
+++ wandb/sweeps/grid_search.py
@@ -22,7 +22,7 @@ class GridSearch(Search):
         for p in params:
             if p.type != HyperParameter.CATEGORICAL and p.type != HyperParameter.CONSTANT:
                 raise ValueError(
-                    f'Parameter {p.name} is a disallowed type with grid search. Grid search requires all parameters to be categorical or constant')
+                    'Parameter %s is a disallowed type with grid search. Grid search requires all parameters to be categorical or constant' % p.name)
 
         # we can only deal with discrete params in a grid search
         discrete_params = [p for p in params if p.type ==
