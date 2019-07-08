3c1f22eb35a8ee1e51a6fe11d56f8f5ab460e56d
Chris Van Pelt <vanpelt@gmail.com>
2019-07-08 15:52:53 -0700

    Lets just not log wonky histos

--- wandb/tensorboard/__init__.py
+++ wandb/tensorboard/__init__.py
@@ -215,6 +215,7 @@ def tf_summary_to_dict(tf_summary_str_or_pb, namespace=""):
         #    audio = wandb.Audio(six.BytesIO(value.audio.encoded_audio_string),
         #                        sample_rate=value.audio.sample_rate, content_type=value.audio.content_type)
         elif kind == "histo":
+            tag = namespaced_tag(value.tag, namespace)
             if len(value.histo.bucket_limit) >= 3:
                 first = value.histo.bucket_limit[0] + \
                     value.histo.bucket_limit[0] - value.histo.bucket_limit[1]
@@ -222,19 +223,16 @@ def tf_summary_to_dict(tf_summary_str_or_pb, namespace=""):
                     value.histo.bucket_limit[-2] - value.histo.bucket_limit[-3]
                 np_histogram = (list(value.histo.bucket), [
                     first] + value.histo.bucket_limit[:-1] + [last])
+                try:
+                    #TODO: we should just re-bin if there are too many buckets
+                    values[tag] = wandb.Histogram(
+                        np_histogram=np_histogram)
+                except ValueError:
+                    wandb.termwarn("Not logging key \"{}\".  Histograms must have fewer than {} bins".format(
+                        tag, wandb.Histogram.MAX_LENGTH), repeat=False)
             else:
-                first = value.histo.bucket_limit[0]
-                last = value.histo.bucket_limit[-1]
-                np_histogram = (list(value.histo.bucket), [
-                    first] + value.histo.bucket_limit[:-1] + [last])
-            tag = namespaced_tag(value.tag, namespace)
-            try:
-                #TODO: we should just re-bin if there are too many buckets
-                values[tag] = wandb.Histogram(
-                    np_histogram=np_histogram)
-            except ValueError:
-                wandb.termwarn("Not logging key \"{}\".  Histograms must have fewer than {} bins".format(
-                    tag, wandb.Histogram.MAX_LENGTH), repeat=False)
+                #TODO: is there a case where we can render this?
+                wandb.termwarn("Not logging key \"{}\".  Found a histogram with only 2 bins.".format(tag), repeat=False)
         elif value.tag == "_hparams_/session_start_info":
             if wandb.util.get_module("tensorboard.plugins.hparams"):
                 from tensorboard.plugins.hparams import plugin_data_pb2
