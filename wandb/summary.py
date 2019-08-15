f3ab222fb89be685ae338bfe24686fa6d6a74452
Lukas Biewald <lukeb@cs.stanford.edu>
2019-08-14 19:51:52 -0700

    key -> k

--- wandb/summary.py
+++ wandb/summary.py
@@ -111,7 +111,7 @@ class SummarySubDict(object):
         path = self._path
 
         if isinstance(v, dict):
-            self._dict[k] = SummarySubDict(self._root, path + (key,))
+            self._dict[k] = SummarySubDict(self._root, path + (k,))
             self._root._root_set(path, [(k, {})])
             self._dict[k].update(v)
         else:
