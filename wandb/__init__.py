252dbaab5037daabd4383fcf7bc5c93a15942284
Annirudh Prasad <annirudh@wandb.com>
2019-08-30 11:53:48 -0700

    Anonymode V2

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -281,29 +281,25 @@ def _init_headless(run, cloud=True):
 def load_ipython_extension(ipython):
     pass
 
+
 def jupyter_login(force=True):
     """Attempt to login from a jupyter environment
 
     If force=False, we'll only attempt to auto-login, otherwise we'll prompt the user
     """
-    key = None
-    if 'google.colab' in sys.modules:
-        key = jupyter.attempt_colab_login(run.api.app_url)
-        if key:
-            os.environ[env.API_KEY] = key
-            util.write_netrc(run.api.api_url, "user", key)
-    if not key and force:
-        termerror(
-            "Not authenticated. Please copy a key from %s/authorize" %
-            run.api.app_url)
-        key = getpass.getpass("API Key: ").strip()
-        prefix, suffix = key.split('-') if '-' in key else ('', key)
-        if len(suffix) == 40:
-            os.environ[env.API_KEY] = key
-            util.write_netrc(run.api.api_url, "user", key)
-        else:
-            raise ValueError("API Key must be 40 characters long")
-    return key
+    def get_api_key_from_browser():
+        key, anonymous = None, False
+        if 'google.colab' in sys.modules:
+            key = jupyter.attempt_colab_login(run.api.app_url)
+        if not key and os.environ.get(env.ALLOW_ANONYMOUS) == "true":
+            key = run.api.create_anonymous_api_key()
+            anonymous = True
+        if not key and force:
+            termerror("Not authenticated.  Copy a key from https://app.wandb.ai/authorize")
+            key = getpass.getpass("API Key: ").strip()
+        return key, anonymous
+
+    return util.prompt_api_key(run.api, browser_callback=get_api_key_from_browser)
 
 
 def _init_jupyter(run):
@@ -317,12 +313,12 @@ def _init_jupyter(run):
     # I also disabled run logging because we're rairly using it.
     # try_to_set_up_global_logging()
     # run.enable_logging()
+    os.environ[env.JUPYTER] = "true"
 
     if not run.api.api_key:
         jupyter_login()
         # Ensure our api client picks up the new key
         run.api.reauth()
-    os.environ["WANDB_JUPYTER"] = "true"
     run.resume = "allow"
     display(HTML('''
         Notebook configured with <a href="https://wandb.com" target="_blank">W&B</a>. You can <a href="{}" target="_blank">open</a> the run page, or call <code>%%wandb</code>
@@ -501,7 +497,7 @@ def monitor(options={}):
 
     class Monitor():
         def __init__(self, options={}):
-            if os.getenv("WANDB_JUPYTER"):
+            if os.getenv(env.JUPYTER):
                 display(jupyter.Run())
             else:
                 self.rm = False
@@ -693,7 +689,7 @@ def join():
 
 def init(job_type=None, dir=None, config=None, project=None, entity=None, reinit=None, tags=None,
          group=None, allow_val_change=False, resume=False, force=False, tensorboard=False,
-         sync_tensorboard=False, name=None, notes=None, id=None, magic=None):
+         sync_tensorboard=False, name=None, notes=None, id=None, magic=None, allow_anonymous=False):
     """Initialize W&B
 
     If called from within Jupyter, initializes a new run and waits for a call to
@@ -812,6 +808,9 @@ def init(job_type=None, dir=None, config=None, project=None, entity=None, reinit
     if dir:
         os.environ[env.DIR] = dir
         util.mkdir_exists_ok(wandb_dir())
+    if allow_anonymous:
+        os.environ[env.ALLOW_ANONYMOUS] = str(allow_anonymous).lower()
+
     resume_path = os.path.join(wandb_dir(), wandb_run.RESUME_FNAME)
     if resume == True:
         os.environ[env.RESUME] = "auto"
@@ -889,7 +888,7 @@ def init(job_type=None, dir=None, config=None, project=None, entity=None, reinit
                 termerror(
                     "No credentials found.  Run \"wandb login\" or \"wandb off\" to disable wandb")
             else:
-                if run.check_anonymous():
+                if util.prompt_api_key(api):
                     _init_headless(run)
                 else:
                     termlog(
