29e994844c85f5724b62ee8e1ec9e18999ef4f08
Chris Van Pelt <vanpelt@gmail.com>
2019-09-05 13:49:33 -0700

    Quick fix for spell

--- wandb/apis/internal.py
+++ wandb/apis/internal.py
@@ -129,7 +129,7 @@ class Api(object):
         try:
             env = env or os.environ
             wandb.config._set_wandb("spell_url", env.get("SPELL_RUN_URL"))
-            return requests.put(env.get("SPELL_API_URL", "https://api.spell.run/wandb_url"), json={
+            return requests.put(env.get("SPELL_API_URL", "https://api.spell.run") + "/wandb_url", json={
                 "access_token": env.get("WANDB_ACCESS_TOKEN"),
                 "url": url
             }, timeout=2)
