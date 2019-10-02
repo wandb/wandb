a367b1d4bdbb639b4eab862c7ce691eb4a861d26
Adrian Swanberg <aswanberg@wandb.com>
2019-10-02 15:04:11 -0700

    Add check for args which are already quotes

--- wandb/compat/windows.py
+++ wandb/compat/windows.py
@@ -12,6 +12,8 @@ def quote_arg(s):
         return '""'
     if _find_unsafe(s) is None:
         return s
+    if s.startswith('"') and s.endswith('"'):
+        return s
 
     # If we found an unsafe character, escape with double quotes.
     return '"' + s + '"'
