b41e641b24e61793be493e36329afdc8bc996738
Chris Van Pelt <vanpelt@gmail.com>
2019-10-02 16:52:08 -0400

    Fix for tensorflow2 and wandb sync

--- wandb/tensorflow/__init__.py
+++ wandb/tensorflow/__init__.py
@@ -7,26 +7,31 @@ from copy import deepcopy
 from wandb.apis.file_stream import Chunk
 
 
-class WandbHook(tf.train.SessionRunHook):
-    def __init__(self, summary_op=None, steps_per_log=1000, history=None):
-        self._summary_op = summary_op
-        self._steps_per_log = steps_per_log
-        # TODO(adrian): might be better to set this to wandb.run.history here
-        # because that is the de facto default.
-        self._history = history
+if tf.__version__[0] == "1":
+    class WandbHook(tf.train.SessionRunHook):
+        def __init__(self, summary_op=None, steps_per_log=1000, history=None):
+            self._summary_op = summary_op
+            self._steps_per_log = steps_per_log
+            # TODO(adrian): might be better to set this to wandb.run.history here
+            # because that is the de facto default.
+            self._history = history
 
-    def begin(self):
-        if self._summary_op is None:
-            self._summary_op = tf.summary.merge_all()
-        self._step = -1
+        def begin(self):
+            if self._summary_op is None:
+                self._summary_op = tf.summary.merge_all()
+            self._step = -1
 
-    def before_run(self, run_context):
-        self._step += 1
-        return tf.train.SessionRunArgs({"summary": self._summary_op})
+        def before_run(self, run_context):
+            self._step += 1
+            return tf.train.SessionRunArgs({"summary": self._summary_op})
 
-    def after_run(self, run_context, run_values):
-        if self._step % self._steps_per_log == 0:
-            log(run_values.results["summary"], history=self._history)
+        def after_run(self, run_context, run_values):
+            if self._step % self._steps_per_log == 0:
+                log(run_values.results["summary"], history=self._history)
+else:
+    class WandbHook(object):
+        def __init__(self, *args, **kwargs):
+            raise NotImplementedError("TensorFlow 2.0+ does not support SessionRunHooks")
 
 
 def stream_tfevents(path, file_api, run, step=0, namespace=""):
@@ -41,7 +46,7 @@ def stream_tfevents(path, file_api, run, step=0, namespace=""):
             parsed = tf_summary_to_dict(summary, namespace=namespace)
             if last_step != parsed[global_step_key]:
                 last_step = parsed[global_step_key]
-                if len(row) > 3: #Must have more than _timestamp, _step, and global_step
+                if len(row) > 3:  # Must have more than _timestamp, _step, and global_step
                     step += 1
                     row["_step"] = step
                     last_row = history_dict_to_json(run, deepcopy(row))
