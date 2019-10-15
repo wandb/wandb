ef0911c47beebab0db8749d764802057d3480e69
Chris Van Pelt <vanpelt@gmail.com>
2019-10-14 17:25:01 -0700

    Feature/docs (#608)
    * Add a few docs
    
    * Adding docs
    
    * pydocs
    
    * Making wild docs
    
    * Manually added pydocs google conversion
    
    * improved documentation for autogenerated docs
    
    * Docs are kinda working
    
    * more docs
    
    * better debugging tools
    
    * Better...
    
    * Fixed multi line args, link :obj:
    
    * continue to clean up docs
    
    * More terrible hacking
    
    * continue to clean up docs
    
    * continue to clean up docs
    
    * Made keras automatically generate docs
    
    * cleanup docs for pydocmd
    
    * This is getting ridiculous
    
    * Add object link
    
    * cleanup docs
    
    * Fix the names, cool
    
    * fix backticks
    
    * Fix the names, cool
    
    * More polish
    
    * Fix codeblocks
    
    * Added github links to docs
    
    * Make links work
    
    * fix link
    
    * Fix CI?
    
    * documenting changes
    
    * documenting changes
    
    * reordering file so the docs make more sense
    
    * adding more docs
    
    * New doc generation action
    
    * Fix doc workflow
    
    * Try to get the docs
    
    * More workflow fun
    
    * Update workflow
    
    * ???
    
    * WTF
    
    * Oh, literally
    
    * Come on
    
    * Must use single quotes :(
    
    * Update deps
    
    * Add fastai
    
    * Echo the ref
    
    * Update data_types.py
    
    * Update data_types.py
    
    * Update data_types.py
    
    * More workflow fun
    
    * Wild action fun
    
    * Add my wild action
    
    * Fix action location
    
    * Relative path
    
    * Fix that shits
    
    * Turn off generation for now
    
    * Insure repo is set
    
    * Typo
    
    * Bam
    
    * Add env vars
    
    * Few more fixes
    
    * Bam
    
    * Try again
    
    * Go docs go!
    
    * Use correct repo
    
    * Use my personal token
    
    * Fix front matter
    
    * Fix keras callback patches, update github action

---
+++ docs/generate.py
@@ -0,0 +1,172 @@
+try:
+    from pydocmd import document
+    from pydocmd.imp import import_object
+    from pydocmd import loader
+    from pydocmd.__main__ import main
+    from yapf.yapflib.yapf_api import FormatCode
+except ImportError:
+    print("You need to run `pip install pydoc-markdown yapf` to generate docs")
+import inspect
+import sys
+import os
+import yaml
+
+
+def trim(docstring):
+    if not docstring:
+        return ''
+    lines = [x.rstrip() for x in docstring.split('\n')]
+    lines[0] = lines[0].lstrip()
+    debug = False
+    if " DEBUG" in docstring:
+        debug = True
+        print("LINES", lines)
+
+    indent = None
+    for i, line in enumerate(lines):
+        if i == 0 or not line:
+            continue
+        new_line = line.lstrip()
+        delta = len(line) - len(new_line)
+        if debug:
+            print("DELTA", delta)
+        if indent is None:
+            # So gnarly
+            if i > 0 and lines[i-1] != "":
+                indent = int(delta / 2)
+        if indent and delta > indent:
+            new_line = ' ' * (delta - indent) + new_line
+        lines[i] = new_line
+
+    return '\n'.join(lines)
+
+
+loader.trim = trim
+
+
+class PythonLoader(object):
+    """
+    Expects absolute identifiers to import with #import_object_with_scope().
+    """
+
+    def __init__(self, config):
+        self.config = config
+
+    def load_section(self, section):
+        """
+        Loads the contents of a #Section. The `section.identifier` is the name
+        of the object that we need to load.
+
+        # Arguments
+          section (Section): The section to load. Fill the `section.title` and
+            `section.content` values. Optionally, `section.loader_context` can
+            be filled with custom arbitrary data to reference at a later point.
+        """
+
+        assert section.identifier is not None
+        obj, scope = loader.import_object_with_scope(section.identifier)
+
+        #TODO: this is insane
+        prefix = None
+        if '.' in section.identifier:
+            parts = section.identifier.rsplit('.', 2)
+            default_title = ".".join(parts[1:])
+            prefix = parts[1]
+        else:
+            default_title = section.identifier
+
+        name = getattr(obj, '__name__', default_title)
+        if prefix and name[0].islower() and prefix not in name:
+            section.title = ".".join([str(prefix), name])
+        else:
+            section.title = name
+        section.content = trim(loader.get_docstring(obj))
+        section.loader_context = {'obj': obj, 'scope': scope}
+
+        # Add the function signature in a code-block.
+        if callable(obj):
+            sig = loader.get_function_signature(
+                obj, scope if inspect.isclass(scope) else None)
+            sig, _ = FormatCode(sig, style_config='google')
+            section.content = '```python\n{}\n```\n'.format(
+                sig.strip()) + section.content
+
+
+loader.PythonLoader = PythonLoader
+
+
+class Section(object):
+    """
+    This is our monkeypatched version of section to enable links to github.  We could put this in google_parser
+    """
+
+    def __init__(self, doc, identifier=None, title=None, depth=1, content=None, header_type='html'):
+        self.doc = doc
+        self.identifier = identifier
+        self.title = title
+        self.link = None
+        try:
+            value = import_object(identifier)
+            lineno = inspect.getsourcelines(value)[1]
+            if len(self.doc.sections) > 0:
+                value = import_object(self.doc.sections[0].identifier)
+            filename = inspect.getsourcefile(value).split("/client/")[-1]
+            branch = os.popen("git rev-parse --abbrev-ref HEAD").read().strip()
+            self.link = "https://github.com/wandb/client/blob/{}/{}#L{}".format(branch, filename, lineno)
+        except TypeError as e:
+            pass
+        self.depth = depth
+        self.content = content if content is not None else '*Nothing to see here.*'
+        self.header_type = header_type
+
+    def maybe_link(self, title):
+        if self.link:
+            return "{}\n[source]({})".format(self.title, self.link)
+        else:
+            return title
+
+    def render(self, stream):
+        """
+        Render the section into *stream*.
+        """
+
+        if self.header_type == 'html':
+            print('<h{depth} id="{id}">{title}</h{depth}>\n'
+                  .format(depth=self.depth, id=self.identifier, title=self.title),
+                  file=stream)
+        elif self.header_type == 'markdown':
+            print('\n' + ('#' * self.depth),
+                  self.maybe_link(self.title), file=stream)
+        else:
+            raise ValueError('Invalid header type: %s' % self.header_type)
+        print(self.content, file=stream)
+
+    @property
+    def index(self):
+        """
+        Returns the #Index that this section is associated with, accessed via
+        `section.document`.
+        """
+
+        return self.document.index
+
+
+document.Section = Section
+sys.argv = ["generate.py", "generate"]
+if __name__ == '__main__':
+    main()
+    config = yaml.load(open("pydocmd.yml"))
+    modules = [(list(doc)[0], list(doc.values())[0]) for doc in config["generate"]]
+    for link, mods in modules:
+        with open("markdown/"+link, "r") as f:
+            content = f.read()
+        with open("markdown/"+link, "w") as f:
+            f.write("---\ndescription: {}\n---\n".format(mods[0].replace("+", "")))
+            f.write(content)
+    with open("markdown/README.md", "w") as f:
+        f.write(
+            "# W&B Documentation\n\nThe current docs on master are also available on our [documentation site](https://docs.wandb.com)\n\n")
+        for link, mods in modules:
+            for mod in mods:
+                f.write("- [{}]({})\n".format(mod.replace("+", ""), link))
+    print("Generated files in the markdown folder!")
---
+++ docs/markdown/keras.md
@@ -0,0 +1,68 @@
+---
+description: wandb.keras
+---
+
+# wandb.keras
+[source](https://github.com/wandb/client/blob/feature/docs/wandb/keras/__init__.py#L0)
+
+
+## WandbCallback
+[source](https://github.com/wandb/client/blob/feature/docs/wandb/keras/__init__.py#L134)
+```python
+WandbCallback(self,
+              monitor='val_loss',
+              verbose=0,
+              mode='auto',
+              save_weights_only=False,
+              log_weights=False,
+              log_gradients=False,
+              save_model=True,
+              training_data=None,
+              validation_data=None,
+              labels=[],
+              data_type=None,
+              predictions=36,
+              generator=None,
+              input_type=None,
+              output_type=None,
+              log_evaluation=False,
+              validation_steps=None,
+              class_colors=None)
+```
+WandbCallback automatically integrates keras with wandb.
+
+**Examples**:
+
+```python
+model.fit(X_train, y_train,  validation_data=(X_test, y_test),
+callbacks=[WandbCallback()])
+```
+ 
+ WandbCallback will automatically log history data from any metrics collected by keras: loss and anything passed into keras_model.compile()
+ 
+ WandbCallback will set summary metrics for the run associated with the "best" training step, where "best" is defined by the `monitor` and `mode` attribues.  This defaults to the epoch with the minimum val_loss. WandbCallback will by default save the model associated with the best epoch..
+ 
+ WandbCallback can optionally log gradient and parameter histograms.
+ 
+ WandbCallback can optionally save training and validation data for wandb to visualize.
+ 
+
+**Arguments**:
+
+- `monitor` _str_ - name of metric to monitor.  Defaults to val_loss.
+- `mode` _str_ - one of {"auto", "min", "max"}. "min" - save model when monitor is minimized "max" - save model when monitor is maximized "auto" - try to guess when to save the model (default). save_model: True - save a model when monitor beats all previous epochs False - don't save models
+- `save_weights_only` _boolean_ - if True, then only the model's weights will be saved (`model.save_weights(filepath)`), else the full model is saved (`model.save(filepath)`).
+- `log_weights` - (boolean) if True save histograms of the model's layer's weights.
+- `log_gradients` - (boolean) if True log histograms of the training gradients
+- `training_data` - (tuple) Same format (X,y) as passed to model.fit.  This is needed for calculating gradients - this is mandatory if `log_gradients` is `True`.
+- `validate_data` - (tuple) Same format (X,y) as passed to model.fit.  A set of data for wandb to visualize.  If this is set, every epoch, wandb will make a small number of predictions and save the results for later visualization.
+- `generator` _generator_ - a generator that returns validation data for wandb to visualize.  This generator should return tuples (X,y).  Either validate_data or generator should be set for wandb to visualize specific data examples.
+- `validation_steps` _int_ - if `validation_data` is a generator, how many steps to run the generator for the full validation set.
+- `labels` _list_ - If you are visualizing your data with wandb this list of labels will convert numeric output to understandable string if you are building a multiclass classifier.  If you are making a binary classifier you can pass in a list of two labels ["label for false", "label for true"].  If validate_data and generator are both false, this won't do anything.
+- `predictions` _int_ - the number of predictions to make for visualization each epoch, max is 100.
+- `input_type` _string_ - type of the model input to help visualization. can be one of: ("image", "images", "segmentation_mask").
+- `output_type` _string_ - type of the model output to help visualziation. can be one of: ("image", "images", "segmentation_mask").
+- `log_evaluation` _boolean_ - if True save a dataframe containing the full validation results at the end of training.
+- `class_colors` - ([float, float, float]) if the input or output is a segmentation mask, an array containing an rgb tuple (range 0-1) for each class.
+ 
+ 
