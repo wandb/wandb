6af53f7412d7b80473434c3adca6f02c8356c6e2
Chris Van Pelt <vanpelt@gmail.com>
2019-10-31 17:09:15 -0700

    Update tests to run latest tensorflows, fix missing dataset_ops (#625)
    * Update tests to run latest tensorflows, fix missing dataset_ops
    
    * Handle no v2
    
    * Change dep

--- wandb/keras/__init__.py
+++ wandb/keras/__init__.py
@@ -28,8 +28,11 @@ else:
 def is_dataset(data):
     dataset_ops = wandb.util.get_module(
         "tensorflow.python.data.ops.dataset_ops")
-    if dataset_ops:
-        return isinstance(data, (dataset_ops.DatasetV1, dataset_ops.DatasetV2))
+    if dataset_ops and hasattr(dataset_ops, "DatasetV2"):
+        dataset_types = (dataset_ops.DatasetV2,)
+        if hasattr(dataset_ops, "DatasetV1"):
+            dataset_types = dataset_types + (dataset_ops.DatasetV1,)
+        return isinstance(data, dataset_types)
     else:
         return False
 
