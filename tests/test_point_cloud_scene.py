2f912d5a37b78aa97895c2bf4ad5c834eac0c5ab
Nicholas Bardy <nicholasbardy@gmail.com>
2019-12-19 15:31:03 -0800

    Update API to specific lidar data

--- tests/test_point_cloud_scene.py
+++ tests/test_point_cloud_scene.py
@@ -7,23 +7,25 @@ wandb.log(
     {
         "point_scene": wandb.Object3D(
             {
-                "type": "scene/v1",
+                "type": "lidar/beta",
                 "points": np.array([[0.4, 1, 1.3], [1, 1, 1], [1.2, 1, 1.2]]),
                 "boxes": np.array(
                     [
                         {
                             "x": 0,
                             "y": 0,
+                            "z": 0,
                             "height": 3,
                             "width": 2,
-                            "depth": 1,
+                            "length": 1,
                         },
                         {
                             "x": 0.3,
                             "y": 0,
+                            "z": 0,
                             "height": 0.2,
                             "width": 0.2,
-                            "depth": 4,
+                            "length": 4,
                         }
                     ]
                 )
