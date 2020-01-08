7c92bb729cd3c061cef240e029da02b49db17cad
Chris Van Pelt <vanpelt@gmail.com>
2020-01-08 14:24:34 -0800

    Pillow 7 removes PILLOW_VERSION

--- install-ml.py
+++ install-ml.py
@@ -3,8 +3,6 @@
 import sys
 from subprocess import call
 
-# Edgeml errors were happening because a package named PIL was installed...
-call(["venv/bin/pip", "uninstall", "PIL"])
 version = "".join([str(v) for v in sys.version_info[:2]])
 if version == "27":
     pass
