16301f8905eac5bf93bf36153b84565efe202aa2
Lavanya Shukla <lavanya.shukla12@gmail.com>
2020-01-18 00:40:37 +0530

    added support for more curves

--- wandb/sklearn/__init__.py
+++ wandb/sklearn/__init__.py
@@ -67,7 +67,7 @@ def log(*estimators, X=None, y=None, X_test=None, y_test=None, labels=None):
                     round_3(sklearn.metrics.recall_score(y_test, y_pred, average="weighted")),
                     round_3(sklearn.metrics.f1_score(y_test, y_pred, average="weighted"))
                 )
-
+a
             # Feature Importances
             if labels is not None:
                 fig = plt.figure()
@@ -79,8 +79,7 @@ def log(*estimators, X=None, y=None, X_test=None, y_test=None, labels=None):
                 fig = plt.figure()
                 ax = plt.axes()
                 # Learning Curve
-                scikitplot.estimators.plot_learning_curve(estimator, X, y, ax=ax)
-                wandb.log({prefix("learning_curve"): fig}, commit=False)
+                plot_learning_curve(estimator, X, y)
 
             if X_test is not None and y_test is not None:
                 y_pred = estimator.predict(X_test)
@@ -144,20 +143,467 @@ def plot_learning_curve(clf, X, y, title='Learning Curve', cv=None,
 
     # Take in data, return wandb.Table()
     def confusion(train, test, trainsize):
+        data=[]
+        for i in range(len(train):
+            data.append(["train"], train[i], trainsize[i])
+            data.append(["test"], test[i], trainsize[i])
         return wandb.Table(
-            columns=['train', 'test', 'train_size'],
-            data=[
-                [train[i], test[i], trainsize[i]] for i in range(len(train))
-            ]
+            columns=['dataset', 'score', 'train_size'],
+            data=data
         )
     wandb.log({'learning_curve': confusion(train_scores_mean, test_scores_mean, train_sizes)})
-    '''
-    ax.grid()
-    ax.plot(train_sizes, train_scores_mean, 'o-', color="r",
-            label="Training score")
-    ax.plot(train_sizes, test_scores_mean, 'o-', color="g",
-            label="Cross-validation score")
-    ax.tick_params(labelsize=text_fontsize)
-    ax.legend(loc="best", fontsize=text_fontsize)
-    '''
     return
+
+def plot_confusion_matrix(y_true, y_pred, labels=None, true_labels=None,
+                          pred_labels=None, title=None, normalize=False,
+                          hide_zeros=False, hide_counts=False, x_tick_rotation=0, ax=None,
+                          figsize=None, cmap='Blues', title_fontsize="large",
+                          text_fontsize="medium"):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    if labels is None:
+        classes = unique_labels(y_true, y_pred)
+    else:
+        classes = np.asarray(labels)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        cm = np.around(cm, decimals=2)
+        cm[np.isnan(cm)] = 0.0
+
+    if true_labels is None:
+        true_classes = classes
+    else:
+        validate_labels(classes, true_labels, "true_labels")
+
+        true_label_indexes = np.in1d(classes, true_labels)
+
+        true_classes = classes[true_label_indexes]
+        cm = cm[true_label_indexes]
+
+    if pred_labels is None:
+        pred_classes = classes
+    else:
+        validate_labels(classes, pred_labels, "pred_labels")
+
+        pred_label_indexes = np.in1d(classes, pred_labels)
+
+        pred_classes = classes[pred_label_indexes]
+        cm = cm[:, pred_label_indexes]
+
+    if title:
+        ax.set_title(title, fontsize=title_fontsize)
+    elif normalize:
+        ax.set_title('Normalized Confusion Matrix', fontsize=title_fontsize)
+    else:
+        ax.set_title('Confusion Matrix', fontsize=title_fontsize)
+
+    image = ax.imshow(cm, interpolation='nearest', cmap=plt.cm.get_cmap(cmap))
+    plt.colorbar(mappable=image)
+    x_tick_marks = np.arange(len(pred_classes))
+    y_tick_marks = np.arange(len(true_classes))
+    ax.set_xticks(x_tick_marks)
+    ax.set_xticklabels(pred_classes, fontsize=text_fontsize,
+                       rotation=x_tick_rotation)
+    ax.set_yticks(y_tick_marks)
+    ax.set_yticklabels(true_classes, fontsize=text_fontsize)
+
+    thresh = cm.max() / 2.
+
+    if not hide_counts:
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            if not (hide_zeros and cm[i, j] == 0):
+                ax.text(j, i, cm[i, j],
+                        horizontalalignment="center",
+                        verticalalignment="center",
+                        fontsize=text_fontsize,
+                        color="white" if cm[i, j] > thresh else "black")
+
+    ax.set_ylabel('True label', fontsize=text_fontsize)
+    ax.set_xlabel('Predicted label', fontsize=text_fontsize)
+    ax.grid(False)
+
+    return ax
+
+
+def plot_roc(y_true, y_probas, title='ROC Curves',
+                   plot_micro=True, plot_macro=True, classes_to_plot=None,
+                   ax=None, figsize=None, cmap='nipy_spectral',
+                   title_fontsize="large", text_fontsize="medium"):
+    y_true = np.array(y_true)
+    y_probas = np.array(y_probas)
+
+    classes = np.unique(y_true)
+    probas = y_probas
+
+    if classes_to_plot is None:
+        classes_to_plot = classes
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    fpr_dict = dict()
+    tpr_dict = dict()
+
+    indices_to_plot = np.in1d(classes, classes_to_plot)
+    for i, to_plot in enumerate(indices_to_plot):
+        fpr_dict[i], tpr_dict[i], _ = roc_curve(y_true, probas[:, i],
+                                                pos_label=classes[i])
+        if to_plot:
+            roc_auc = auc(fpr_dict[i], tpr_dict[i])
+            color = plt.cm.get_cmap(cmap)(float(i) / len(classes))
+            ax.plot(fpr_dict[i], tpr_dict[i], lw=2, color=color,
+                    label='ROC curve of class {0} (area = {1:0.2f})'
+                          ''.format(classes[i], roc_auc))
+
+    if plot_micro:
+        binarized_y_true = label_binarize(y_true, classes=classes)
+        if len(classes) == 2:
+            binarized_y_true = np.hstack(
+                (1 - binarized_y_true, binarized_y_true))
+        fpr, tpr, _ = roc_curve(binarized_y_true.ravel(), probas.ravel())
+        roc_auc = auc(fpr, tpr)
+        ax.plot(fpr, tpr,
+                label='micro-average ROC curve '
+                      '(area = {0:0.2f})'.format(roc_auc),
+                color='deeppink', linestyle=':', linewidth=4)
+
+    if plot_macro:
+        # Compute macro-average ROC curve and ROC area
+        # First aggregate all false positive rates
+        all_fpr = np.unique(np.concatenate([fpr_dict[x] for x in range(len(classes))]))
+
+        # Then interpolate all ROC curves at this points
+        mean_tpr = np.zeros_like(all_fpr)
+        for i in range(len(classes)):
+            mean_tpr += interp(all_fpr, fpr_dict[i], tpr_dict[i])
+
+        # Finally average it and compute AUC
+        mean_tpr /= len(classes)
+        roc_auc = auc(all_fpr, mean_tpr)
+
+        ax.plot(all_fpr, mean_tpr,
+                label='macro-average ROC curve '
+                      '(area = {0:0.2f})'.format(roc_auc),
+                color='navy', linestyle=':', linewidth=4)
+
+    ax.plot([0, 1], [0, 1], 'k--', lw=2)
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate', fontsize=text_fontsize)
+    ax.set_ylabel('True Positive Rate', fontsize=text_fontsize)
+    ax.tick_params(labelsize=text_fontsize)
+    ax.legend(loc='lower right', fontsize=text_fontsize)
+    return ax
+
+
+def plot_ks_statistic(y_true, y_probas, title='KS Statistic Plot',
+                      ax=None, figsize=None, title_fontsize="large",
+                      text_fontsize="medium"):
+    y_true = np.array(y_true)
+    y_probas = np.array(y_probas)
+
+    classes = np.unique(y_true)
+    if len(classes) != 2:
+        raise ValueError('Cannot calculate KS statistic for data with '
+                         '{} category/ies'.format(len(classes)))
+    probas = y_probas
+
+    # Compute KS Statistic curves
+    thresholds, pct1, pct2, ks_statistic, \
+        max_distance_at, classes = binary_ks_curve(y_true,
+                                                   probas[:, 1].ravel())
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    ax.plot(thresholds, pct1, lw=3, label='Class {}'.format(classes[0]))
+    ax.plot(thresholds, pct2, lw=3, label='Class {}'.format(classes[1]))
+    idx = np.where(thresholds == max_distance_at)[0][0]
+    ax.axvline(max_distance_at, *sorted([pct1[idx], pct2[idx]]),
+               label='KS Statistic: {:.3f} at {:.3f}'.format(ks_statistic,
+                                                             max_distance_at),
+               linestyle=':', lw=3, color='black')
+
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.0])
+
+    ax.set_xlabel('Threshold', fontsize=text_fontsize)
+    ax.set_ylabel('Percentage below threshold', fontsize=text_fontsize)
+    ax.tick_params(labelsize=text_fontsize)
+    ax.legend(loc='lower right', fontsize=text_fontsize)
+
+    return ax
+
+
+def plot_precision_recall(y_true, y_probas,
+                          title='Precision-Recall Curve',
+                          plot_micro=True,
+                          classes_to_plot=None, ax=None,
+                          figsize=None, cmap='nipy_spectral',
+                          title_fontsize="large",
+                          text_fontsize="medium"):
+    y_true = np.array(y_true)
+    y_probas = np.array(y_probas)
+
+    classes = np.unique(y_true)
+    probas = y_probas
+
+    if classes_to_plot is None:
+        classes_to_plot = classes
+
+    binarized_y_true = label_binarize(y_true, classes=classes)
+    if len(classes) == 2:
+        binarized_y_true = np.hstack(
+            (1 - binarized_y_true, binarized_y_true))
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    indices_to_plot = np.in1d(classes, classes_to_plot)
+    for i, to_plot in enumerate(indices_to_plot):
+        if to_plot:
+            average_precision = average_precision_score(
+                binarized_y_true[:, i],
+                probas[:, i])
+            precision, recall, _ = precision_recall_curve(
+                y_true, probas[:, i], pos_label=classes[i])
+            color = plt.cm.get_cmap(cmap)(float(i) / len(classes))
+            ax.plot(recall, precision, lw=2,
+                    label='Precision-recall curve of class {0} '
+                          '(area = {1:0.3f})'.format(classes[i],
+                                                     average_precision),
+                    color=color)
+
+    if plot_micro:
+        precision, recall, _ = precision_recall_curve(
+            binarized_y_true.ravel(), probas.ravel())
+        average_precision = average_precision_score(binarized_y_true,
+                                                    probas,
+                                                    average='micro')
+        ax.plot(recall, precision,
+                label='micro-average Precision-recall curve '
+                      '(area = {0:0.3f})'.format(average_precision),
+                color='navy', linestyle=':', linewidth=4)
+
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('Recall')
+    ax.set_ylabel('Precision')
+    ax.tick_params(labelsize=text_fontsize)
+    ax.legend(loc='best', fontsize=text_fontsize)
+    return ax
+
+
+def plot_silhouette(X, cluster_labels, title='Silhouette Analysis',
+                    metric='euclidean', copy=True, ax=None, figsize=None,
+                    cmap='nipy_spectral', title_fontsize="large",
+                    text_fontsize="medium"):
+    cluster_labels = np.asarray(cluster_labels)
+
+    le = LabelEncoder()
+    cluster_labels_encoded = le.fit_transform(cluster_labels)
+
+    n_clusters = len(np.unique(cluster_labels))
+
+    silhouette_avg = silhouette_score(X, cluster_labels, metric=metric)
+
+    sample_silhouette_values = silhouette_samples(X, cluster_labels,
+                                                  metric=metric)
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+    ax.set_xlim([-0.1, 1])
+
+    ax.set_ylim([0, len(X) + (n_clusters + 1) * 10 + 10])
+
+    ax.set_xlabel('Silhouette coefficient values', fontsize=text_fontsize)
+    ax.set_ylabel('Cluster label', fontsize=text_fontsize)
+
+    y_lower = 10
+
+    for i in range(n_clusters):
+        ith_cluster_silhouette_values = sample_silhouette_values[
+            cluster_labels_encoded == i]
+
+        ith_cluster_silhouette_values.sort()
+
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+
+        color = plt.cm.get_cmap(cmap)(float(i) / n_clusters)
+
+        ax.fill_betweenx(np.arange(y_lower, y_upper),
+                         0, ith_cluster_silhouette_values,
+                         facecolor=color, edgecolor=color, alpha=0.7)
+
+        ax.text(-0.05, y_lower + 0.5 * size_cluster_i, str(le.classes_[i]),
+                fontsize=text_fontsize)
+
+        y_lower = y_upper + 10
+
+    ax.axvline(x=silhouette_avg, color="red", linestyle="--",
+               label='Silhouette score: {0:0.3f}'.format(silhouette_avg))
+
+    ax.set_yticks([])  # Clear the y-axis labels / ticks
+    ax.set_xticks(np.arange(-0.1, 1.0, 0.2))
+
+    ax.tick_params(labelsize=text_fontsize)
+    ax.legend(loc='best', fontsize=text_fontsize)
+
+    return ax
+
+
+def plot_calibration_curve(y_true, probas_list, clf_names=None, n_bins=10,
+                           title='Calibration plots (Reliability Curves)',
+                           ax=None, figsize=None, cmap='nipy_spectral',
+                           title_fontsize="large", text_fontsize="medium"):
+    y_true = np.asarray(y_true)
+    if not isinstance(probas_list, list):
+        raise ValueError('`probas_list` does not contain a list.')
+
+    classes = np.unique(y_true)
+    if len(classes) > 2:
+        raise ValueError('plot_calibration_curve only '
+                         'works for binary classification')
+
+    if clf_names is None:
+        clf_names = ['Classifier {}'.format(x+1)
+                     for x in range(len(probas_list))]
+
+    if len(clf_names) != len(probas_list):
+        raise ValueError('Length {} of `clf_names` does not match length {} of'
+                         ' `probas_list`'.format(len(clf_names),
+                                                 len(probas_list)))
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+
+    for i, probas in enumerate(probas_list):
+        probas = np.asarray(probas)
+        if probas.ndim > 2:
+            raise ValueError('Index {} in probas_list has invalid '
+                             'shape {}'.format(i, probas.shape))
+        if probas.ndim == 2:
+            probas = probas[:, 1]
+
+        if probas.shape != y_true.shape:
+            raise ValueError('Index {} in probas_list has invalid '
+                             'shape {}'.format(i, probas.shape))
+
+        probas = (probas - probas.min()) / (probas.max() - probas.min())
+
+        fraction_of_positives, mean_predicted_value = \
+            calibration_curve(y_true, probas, n_bins=n_bins)
+
+        color = plt.cm.get_cmap(cmap)(float(i) / len(probas_list))
+
+        ax.plot(mean_predicted_value, fraction_of_positives, 's-',
+                label=clf_names[i], color=color)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    ax.set_xlabel('Mean predicted value', fontsize=text_fontsize)
+    ax.set_ylabel('Fraction of positives', fontsize=text_fontsize)
+
+    ax.set_ylim([-0.05, 1.05])
+    ax.legend(loc='lower right')
+
+    return ax
+
+
+def plot_cumulative_gain(y_true, y_probas, title='Cumulative Gains Curve',
+                         ax=None, figsize=None, title_fontsize="large",
+                         text_fontsize="medium"):
+    y_true = np.array(y_true)
+    y_probas = np.array(y_probas)
+
+    classes = np.unique(y_true)
+    if len(classes) != 2:
+        raise ValueError('Cannot calculate Cumulative Gains for data with '
+                         '{} category/ies'.format(len(classes)))
+
+    # Compute Cumulative Gain Curves
+    percentages, gains1 = cumulative_gain_curve(y_true, y_probas[:, 0],
+                                                classes[0])
+    percentages, gains2 = cumulative_gain_curve(y_true, y_probas[:, 1],
+                                                classes[1])
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    ax.plot(percentages, gains1, lw=3, label='Class {}'.format(classes[0]))
+    ax.plot(percentages, gains2, lw=3, label='Class {}'.format(classes[1]))
+
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.0])
+
+    ax.plot([0, 1], [0, 1], 'k--', lw=2, label='Baseline')
+
+    ax.set_xlabel('Percentage of sample', fontsize=text_fontsize)
+    ax.set_ylabel('Gain', fontsize=text_fontsize)
+    ax.tick_params(labelsize=text_fontsize)
+    ax.grid('on')
+    ax.legend(loc='lower right', fontsize=text_fontsize)
+
+    return ax
+
+
+def plot_lift_curve(y_true, y_probas, title='Lift Curve',
+                    ax=None, figsize=None, title_fontsize="large",
+                    text_fontsize="medium"):
+    y_true = np.array(y_true)
+    y_probas = np.array(y_probas)
+
+    classes = np.unique(y_true)
+    if len(classes) != 2:
+        raise ValueError('Cannot calculate Lift Curve for data with '
+                         '{} category/ies'.format(len(classes)))
+
+    # Compute Cumulative Gain Curves
+    percentages, gains1 = cumulative_gain_curve(y_true, y_probas[:, 0],
+                                                classes[0])
+    percentages, gains2 = cumulative_gain_curve(y_true, y_probas[:, 1],
+                                                classes[1])
+
+    percentages = percentages[1:]
+    gains1 = gains1[1:]
+    gains2 = gains2[1:]
+
+    gains1 = gains1 / percentages
+    gains2 = gains2 / percentages
+
+    if ax is None:
+        fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    ax.set_title(title, fontsize=title_fontsize)
+
+    ax.plot(percentages, gains1, lw=3, label='Class {}'.format(classes[0]))
+    ax.plot(percentages, gains2, lw=3, label='Class {}'.format(classes[1]))
+
+    ax.plot([0, 1], [1, 1], 'k--', lw=2, label='Baseline')
+
+    ax.set_xlabel('Percentage of sample', fontsize=text_fontsize)
+    ax.set_ylabel('Lift', fontsize=text_fontsize)
+    ax.tick_params(labelsize=text_fontsize)
+    ax.grid('on')
+    ax.legend(loc='lower right', fontsize=text_fontsize)
+
+    return ax
