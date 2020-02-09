8ae679ef1a8d98f7c0996b1e44e33d7598588c3c
Jeff Raubitschek <jeff@wandb.com>
2020-02-08 22:09:58 -0800

    Add wandb.run.config_static (#840)
    * Add wandb.run.config_static
    
    * add some tests

--- wandb/wandb_config.py
+++ wandb/wandb_config.py
@@ -328,3 +328,23 @@ class Config(object):
             s += b'\n\n' + yaml.dump(as_dict, Dumper=yaml.SafeDumper, default_flow_style=False,
                                      allow_unicode=True, encoding='utf-8')
         return s.decode("utf-8")
+
+
+class ConfigStatic(object):
+    def __init__(self, config):
+        object.__setattr__(self, "__dict__", dict(config))
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Error: wandb.run.config_static is a readonly object")
+
+    def __setitem__(self, key, val):
+        raise AttributeError("Error: wandb.run.config_static is a readonly object")
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __str__(self):
+        return str(self.__dict__)
