bf7cd26b6d8fe80100155805268afd746f61ef2e
Nicholas Bardy <nicholasbardy@gmail.com>
2020-02-26 10:30:31 -0800

    tweak bb test

--- standalone_tests/image_bounding_box.py
+++ standalone_tests/image_bounding_box.py
@@ -11,11 +11,13 @@ image = np.random.randint(255, size=(400, 400, 3))
 
 # 2D Bounding Boxes
 def gen_box_type_2():
+    x = random.randint(20, 100)
+    y = random.randint(20, 100)
     box = {"position": {
-        "minX": 100 + random.randint(-50,0),
-        "maxX": 100 + random.randint(0,50),
-        "minY": 100 + random.randint(-50,0),
-        "maxY": 100 + random.randint(0,50),
+        "minX": x,
+        "maxX": x + random.randint(0,250),
+        "minY": y,
+        "maxY": y + random.randint(0,300),
     },
      "class_label" : "car",
      "box_caption": "car conf 0.7",
@@ -47,7 +49,7 @@ def gen_box_img_type_1():
     return box_img
 
 def gen_box_img_type_2():
-    box_img = wandb.Image(image, boxes=[gen_box_type_2() for i in range(1,100)] )
+    box_img = wandb.Image(image, boxes=[gen_box_type_2() for i in range(1,20)] )
     return box_img
 
 wandb.log({
