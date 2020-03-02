7593afbdce87cf7004de34f8673036d895c9dac7
Jeff Raubitschek <jeff@wandb.com>
2020-03-02 15:57:37 -0800

    gym added output framerate

--- tests/test_gym.py
+++ tests/test_gym.py
@@ -8,6 +8,6 @@ def test_patch(wandb_init_run, git_repo):
     wandb.gym.monitor()
     with open("test.gif", "w") as f:
         f.write("test")
-    ir = ImageEncoder("test.gif", (28, 28, 3), 10)
+    ir = ImageEncoder("test.gif", (28, 28, 3), 10, 10)
     ir.close()
     assert wandb_init_run.summary["videos"]['_type'] == "video-file"
