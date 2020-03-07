7f005ef2d9bc18fb5da70ae5c283f78483b23176
Nicholas Bardy <nicholasbardy@gmail.com>
2020-03-06 16:53:25 -0800

    masks

--- standalone_tests/image_masks.py
+++ standalone_tests/image_masks.py
@@ -8,7 +8,25 @@ import numpy as np
 wandb.init(project="test-image-masks")
 
 image = np.random.randint(255, size=(400, 400, 3))
-mask_data = np.random.randint(10, size=(400, 400))
+mask_list = []
+n = 400
+m = 400
+for i in range(n):
+    inner_list = []
+    for j in range(m):
+        v = 0
+        if i < 200:
+            v = 1
+        if i > 200:
+            v = 2
+        if j < 200:
+            v = v + 3
+        if j > 200:
+            v = v + 6
+        inner_list.append(v)
+    mask_list.append(inner_list)
+
+mask_data = np.array(mask_list)
 
 def gen_mask_img():
     mask_img = wandb.Image(np.array(image), masks={
