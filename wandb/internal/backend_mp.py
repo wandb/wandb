f626fe41c7670753ed500ec2be09da0c530feaa8
Jeff Raubitschek <jeff@wandb.com>
2020-03-10 11:53:15 -0700

    blah

--- wandb/internal/backend_mp.py
+++ wandb/internal/backend_mp.py
@@ -444,8 +444,8 @@ class Backend(object):
         if platform.system() == "Windows":
             # https://bugs.python.org/issue38188
             import msvcrt
-            #stdout_handle = msvcrt.get_osfhandle(stdout_fd)
-            #stderr_handle = msvcrt.get_osfhandle(stderr_fd)
+            stdout_fd = msvcrt.get_osfhandle(stdout_fd)
+            stderr_fd = msvcrt.get_osfhandle(stderr_fd)
             multiprocessing.reduction.send_handle(fd_pipe_parent, stdout_fd,  wandb_process.pid)
             multiprocessing.reduction.send_handle(fd_pipe_parent, stderr_fd,  wandb_process.pid)
 
