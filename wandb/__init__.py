5663888cec145728815129c2d7b6f95b6f26f5ce
Jeff Raubitschek <jeff@wandb.com>
2020-03-13 12:58:37 -0700

    Bump version: 0.0.6 → 0.0.7

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = '0.0.6'
+__version__ = '0.0.7'
 
 from wandb.errors import Error
 
