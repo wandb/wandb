ad01616240cf99ea2ca67aa33582361178b6fd12
Jeff Raubitschek <jeff@wandb.com>
2020-04-03 18:22:06 -0700

    Bump version: 0.0.16 → 0.0.17

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name='wandb-ng',
-    version='0.0.16',
+    version='0.0.17',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     long_description_content_type="text/markdown",
