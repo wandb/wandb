aaf32ef723d53f58ae9a2e578003916e3f86f11c
Jeff Raubitschek <jeff@wandb.com>
2020-04-24 17:02:06 -0700

    Bump version: 0.8.32 → 0.8.33

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -9,7 +9,7 @@ from __future__ import absolute_import, print_function
 
 __author__ = """Chris Van Pelt"""
 __email__ = 'vanpelt@wandb.com'
-__version__ = '0.8.32'
+__version__ = '0.8.33'
 
 import atexit
 import click
