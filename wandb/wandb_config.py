d27d60784f0e9e9679e7fd547db39ac5f7085f3c
Chris Van Pelt <vanpelt@gmail.com>
2020-04-25 18:20:51 -0700

    Dont overwrite wandb config in jupyter

--- wandb/wandb_config.py
+++ wandb/wandb_config.py
@@ -178,7 +178,7 @@ class Config(object):
     def load_json(self, json):
         """Loads existing config from JSON"""
         for key in json:
-            if key == "wandb_version":
+            if key == "wandb_version" or key == "_wandb":
                 continue
             self._items[key] = json[key].get('value')
             self._descriptions[key] = json[key].get('desc')
