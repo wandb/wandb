53c530873b6c77bd643a945afe3b4513e17a6e5f
mani <sadhak001@gmail.com>
2020-05-18 23:57:56 +0100

    LightGBM: Plot Feature importances after model training (similar to sklearn & CatBoost)  (#1037)
    * CatBoost: add a echo so that user can see this appear in the console or notebook and go to the Dashboard
    
    * CatBoost: making a copy of the model and running fit() on it, in quiet mode so the output isn't shown on the console
    
    * LightGBM: Plot Feature importances after model training (similar to sklearn and CatBoost)

--- wandb/lightgbm/__init__.py
+++ wandb/lightgbm/__init__.py
@@ -9,6 +9,11 @@ config.update(dict(param_list))
 lgb = lgb.train(param_list, d_train, callbacks=[wandb_callback()])
 '''
 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+from .utils import test_types
+
 import lightgbm
 import wandb
 
@@ -27,3 +32,64 @@ def wandb_callback():
         # Previous log statements use commit=False. This commits them.
         wandb.log({})
     return callback
+
+
+def plot_feature_importances(model=None, feature_names=None,
+                             title='Feature Importance', max_num_features=50):
+    """
+    Evaluates & plots the importance of each feature for the gbm fitting tasks.
+
+    Should only be called with a fitted model (otherwise an error is thrown).
+    Only works with LightGBM model that have a `feature_importance` attribute.
+
+    Arguments:
+        model: Takes in a fitted gbm i.e. LightGBM
+        feature_names (list): Names for features. Makes plots easier to read by
+                                replacing feature indexes with corresponding names.
+
+    Returns:
+        Nothing. To see plots, go to your W&B run page then expand the 'media' tab
+              under 'auto visualizations'.
+
+    Example:
+        wandb.lightgbm.plot_feature_importances(model, ['width', 'height, 'length'])
+    """
+    attributes_to_check = 'feature_importance'
+
+    if not hasattr(model, attributes_to_check):
+        wandb.termwarn(
+            "%s attribute not in model. Cannot plot feature importances." % attributes_to_check)
+        return
+
+    if (test_types(model=model)):
+        feature_names = np.asarray(feature_names)
+        importances = model.feature_importance()
+
+        indices = np.argsort(importances)[::-1]
+
+        if feature_names is None:
+            feature_names = indices
+        else:
+            feature_names = np.array(feature_names)[indices]
+
+        max_num_features = min(max_num_features, len(importances))
+
+        feature_names = feature_names[:max_num_features]
+        importances = importances[:max_num_features]
+
+        # Draw a stem plot with the influence for each instance
+        # format:
+        # x = feature_names[:max_num_features]
+        # y = importances[indices][:max_num_features]
+        def feature_importances_table(feature_names, importances):
+            return wandb.visualize(
+                'wandb/feature_importances/v1', wandb.Table(
+                    columns=['feature_names', 'importances'],
+                    data=[
+                        [feature_names[i], importances[i]] for i in range(len(feature_names))
+                    ]
+                ))
+
+        wandb.log({'feature_importances': feature_importances_table(feature_names, importances)})
+        print("Go to your dashboard to see the LightGBM's Feature Importances graph plotted.")
+        return feature_importances_table(feature_names, importances)
