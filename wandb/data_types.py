41c99ce3fabd4df089d2693d8c7498252f41e453
Nicholas Bardy <nicholasbardy@gmail.com>
2020-06-24 13:28:26 -0700

    Move numpy declaration

--- wandb/data_types.py
+++ wandb/data_types.py
@@ -1197,9 +1197,9 @@ class ImageMask(Media):
     def __init__(self, val, key, **kwargs):
         super(ImageMask, self).__init__()
 
+        np = util.get_module("numpy", required="Semantic Segmentation mask support requires numpy")
         # Add default class mapping
         if not "class_labels" in val:
-            np = util.get_module("numpy", required="Semantic Segmentation mask support requires numpy")
             classes = np.unique(val["mask_data"]).astype(np.int32).tolist()
             class_labels = dict((c, "class_" + str(c)) for c in classes)
             val["class_labels"] = class_labels
