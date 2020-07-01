546722fbf30aec1a685a2d577a4323591bfb371c
Jeff Raubitschek <jeff@wandb.com>
2020-07-01 12:08:09 -0700

    Bump version: 0.9.1 → 0.9.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ kubeflow_requirements = ['kubernetes', 'minio', 'google-cloud-storage', 'sh']
 
 setup(
     name='wandb',
-    version='0.9.1',
+    version='0.9.2',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     long_description_content_type="text/markdown",
