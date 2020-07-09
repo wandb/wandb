818e18aa576cf33ac2991e02a39c7fb7ab71ce5d
Boris Dayma <boris.dayma@gmail.com>
2020-07-09 02:05:54 -0500

    feat: add support for fastai v1 (#29)
    Author: @borisdayma

---
+++ wandb/fastai/__init__.py
@@ -0,0 +1,10 @@
+"""
+Compatibility fastai module.
+
+In the future use:
+    from wandb.framework.fastai import WandbCallback
+"""
+
+from wandb.framework.fastai import WandbCallback
+
+__all__ = ['WandbCallback']
