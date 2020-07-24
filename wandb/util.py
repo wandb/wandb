4b93e4b54cf8f64cd266c5d4683d4ac9fcabeb10
Chris Van Pelt <vanpelt@gmail.com>
2020-07-24 10:20:10 -0700

    Bring back is_frame_like to spline

--- wandb/util.py
+++ wandb/util.py
@@ -277,6 +277,29 @@ def ensure_matplotlib_figure(obj):
     """
     import matplotlib
     from matplotlib.figure import Figure
+    # plotly and matplotlib broke in recent releases,
+    # this patches matplotlib to add a removed method that plotly assumes exists
+    from matplotlib.spines import Spine
+    def is_frame_like(self):
+        """Return True if directly on axes frame.
+
+        This is useful for determining if a spine is the edge of an
+        old style MPL plot. If so, this function will return True.
+        """
+        position = self._position or ('outward', 0.0)
+        if isinstance(position, str):
+            if position == 'center':
+                position = ('axes', 0.5)
+            elif position == 'zero':
+                position = ('data', 0)
+        if len(position) != 2:
+            raise ValueError("position should be 2-tuple")
+        position_type, amount = position
+        if position_type == 'outward' and amount == 0:
+            return True
+        else:
+            return False
+    Spine.is_frame_like = is_frame_like
     if obj == matplotlib.pyplot:
         obj = obj.gcf()
     elif not isinstance(obj, Figure):
