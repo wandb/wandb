efe3b020f211874b50ac8aab6a0483ef5b4ab182
Jeff Raubitschek <jeff@wandb.com>
2020-09-03 11:35:16 -0700

    Bump version: 0.0.42 → 0.0.43

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ kubeflow_requirements = ['kubernetes', 'minio', 'google-cloud-storage', 'sh']
 
 setup(
     name='wandb-ng',
-    version='0.0.42',
+    version='0.0.43',
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     long_description_content_type="text/markdown",
--- wandb/__init__.py
+++ wandb/__init__.py
@@ -19,7 +19,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = '0.0.42'
+__version__ = '0.0.43'
 
 # Used with pypi checks and other messages related to pip
 _wandb_module = 'wandb-ng'
