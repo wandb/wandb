72a1a0142489420d37b45e63a32ec90fe2ab078b
Jeff Raubitschek <jeff@wandb.com>
2020-09-23 11:57:16 -0700

    Resync from anaconda sweeps (#1260)
    * Resync from anaconda sweeps
    
    * fix pdf
    
    * update
    
    * skip bayes test on mac for now

--- wandb/sweeps/test_bayes_search.py
+++ wandb/sweeps/test_bayes_search.py
@@ -1,13 +1,15 @@
 from wandb.sweeps import bayes_search as bayes
 import numpy as np
+import pytest
+import platform
 
 
 def squiggle(x):
-    return np.exp(-(x - 2) ** 2) + np.exp(-(x - 6) ** 2 / 10) + 1 / (x ** 2 + 1)
+    return np.exp(-(x - 2)**2) + np.exp(-(x - 6)**2 / 10) + 1 / (x**2 + 1)
 
 
 def rosenbrock(x):
-    return np.sum((x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0)
+    return np.sum((x[1:] - x[:-1]**2.0)**2.0 + (1 - x[:-1])**2.0)
 
 
 def test_squiggle():
@@ -16,8 +18,7 @@ def test_squiggle():
     X = np.array([np.random.uniform([0.], [5.]) for x in range(200)])
     Y = np.array([f(x) for x in X]).flatten()
     sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-        X, Y, [[-5., 5.]], improvement=1.0
-    )
+        X, Y, [[-5., 5.]], improvement=1.0)
     assert sample[0] < 0., "Greater than 0 {}".format(sample[0])
     # we sample missing a big chunk between 1 and 3
     X = np.append(
@@ -27,11 +28,9 @@ def test_squiggle():
     )
     Y = np.array([f(x) for x in X]).flatten()
     sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-        X, Y, [[0., 4.]]
-    )
-    assert sample[0] > 1. and sample[0] < 4., "Sample outside of 1-3 range: {}".format(
-        sample[0]
-    )
+        X, Y, [[0., 4.]])
+    assert sample[0] > 1. and sample[
+        0] < 4., "Sample outside of 1-3 range: {}".format(sample[0])
 
 
 def test_nans():
@@ -39,14 +38,12 @@ def test_nans():
     X = np.array([np.random.uniform([0.], [5.]) for x in range(200)])
     Y = np.array([np.nan] * 200)
     sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-        X, Y, [[-10, 10]]
-    )
+        X, Y, [[-10, 10]])
     assert sample[0] < 10.  # trying all NaNs
     X += np.array([np.random.uniform([0.], [5.]) for x in range(200)])
     Y += np.array([np.nan] * 200)
     sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-        X, Y, [[-10, 10]]
-    )
+        X, Y, [[-10, 10]])
     assert sample[0] < 10.
 
 
@@ -55,8 +52,7 @@ def test_squiggle_int():
     X = np.array([np.random.uniform([0.], [5.]) for x in range(200)])
     Y = np.array([f(x) for x in X]).flatten()
     sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-        X, Y, [[-10, 10]]
-    )
+        X, Y, [[-10, 10]])
     assert sample[0] < 0., "Greater than 0 {}".format(sample[0])
 
 
@@ -65,9 +61,9 @@ def run_iterations(f, bounds, num_iterations=20):
     y = np.array([f(x) for x in X]).flatten()
     for jj in range(num_iterations):
         sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-            X, y, bounds, improvement=0.1
-        )
-        print("X: {} prob(I): {} pred: {} value: {}".format(sample, prob, pred, f(sample)))
+            X, y, bounds, improvement=0.1)
+        print("X: {} prob(I): {} pred: {} value: {}".format(
+            sample, prob, pred, f(sample)))
         X = np.append(X, np.array([sample]), axis=0)
         y = np.array([f(x) for x in X]).flatten()
 
@@ -79,8 +75,7 @@ def run_iterations_chunked(f, bounds, num_iterations=3, chunk_size=5):
         sample_X = None
         for cc in range(chunk_size):
             sample, prob, pred, samples, vals, stds, sample_probs, prob_of_fail, pred_runtimes = bayes.next_sample(
-                X, y, bounds, current_X=sample_X, improvement=0.1
-            )
+                X, y, bounds, current_X=sample_X, improvement=0.1)
             if sample_X is None:
                 sample_X = np.array([sample])
             else:
@@ -112,16 +107,42 @@ class Run(object):
         self.history = history
 
     def __repr__(self):
-        return 'Run(%s,%s,%s,%s,%s)' % (self.name, self.state, self.config, self.history, self.summaryMetrics)
+        return 'Run(%s,%s,%s,%s,%s)' % (self.name, self.state, self.config,
+                                        self.history, self.summaryMetrics)
 
 
 sweep_config_2params = {
     'metric': {
         'name': 'loss'
-        },
+    },
     'parameters': {
-        'v1': {'min': 1, 'max': 10},
-        'v2': {'min': 1, 'max': 10}}}
+        'v1': {
+            'min': 1,
+            'max': 10
+        },
+        'v2': {
+            'min': 1,
+            'max': 10
+        }
+    }
+}
+
+sweep_config_2params_acc = {
+    'metric': {
+        'name': 'acc',
+        'goal': 'maximize',
+    },
+    'parameters': {
+        'v1': {
+            'min': 1,
+            'max': 10
+        },
+        'v2': {
+            'min': 1,
+            'max': 10
+        }
+    }
+}
 
 
 # search with 0 runs - hardcoded results
@@ -138,19 +159,26 @@ def test_runs_bayes():
 def test_runs_bayes_runs2():
     np.random.seed(73)
     bs = bayes.BayesianSearch()
-    r1 = Run('b', 'finished',
-             {'v1': {'value': 7},
-              'v2': {'value': 6}},
-             {'zloss': 1.2},
-             [{'loss': 1.2},
-              ]
-             )
-    r2 = Run('b', 'finished',
-             {'v1': {'value': 1},
-              'v2': {'value': 8}},
-             {'loss': 0.4},
-             []
-             )
+    r1 = Run('b', 'finished', {
+        'v1': {
+            'value': 7
+        },
+        'v2': {
+            'value': 6
+        }
+    }, {'zloss': 1.2}, [
+        {
+            'loss': 1.2
+        },
+    ])
+    r2 = Run('b', 'finished', {
+        'v1': {
+            'value': 1
+        },
+        'v2': {
+            'value': 8
+        }
+    }, {'loss': 0.4}, [])
     # need two (non running) runs before we get a new set of parameters
     runs = [r1, r2]
     sweep = {'config': sweep_config_2params, 'runs': runs}
@@ -162,13 +190,84 @@ def test_runs_bayes_runs2():
 def test_runs_bayes_runs2_missingmetric():
     np.random.seed(73)
     bs = bayes.BayesianSearch()
-    r1 = Run('b', 'finished',
-             {'v1': {'value': 7},
-              'v2': {'value': 5}},
-             {'xloss': 0.2},
-             []
-             )
+    r1 = Run('b', 'finished', {
+        'v1': {
+            'value': 7
+        },
+        'v2': {
+            'value': 5
+        }
+    }, {'xloss': 0.2}, [])
     runs = [r1, r1]
     sweep = {'config': sweep_config_2params, 'runs': runs}
     params, info = bs.next_run(sweep)
     assert params['v1']['value'] == 1 and params['v2']['value'] == 1
+
+
+# search with 2 finished runs - hardcoded results - missing metric
+def test_runs_bayes_runs2_missingmetric_acc():
+    np.random.seed(73)
+    bs = bayes.BayesianSearch()
+    r1 = Run('b', 'finished', {
+        'v1': {
+            'value': 7
+        },
+        'v2': {
+            'value': 5
+        }
+    }, {'xloss': 0.2}, [])
+    runs = [r1, r1]
+    sweep = {'config': sweep_config_2params_acc, 'runs': runs}
+    params, info = bs.next_run(sweep)
+    assert params['v1']['value'] == 1 and params['v2']['value'] == 1
+
+
+@pytest.mark.skipif(platform.system() == "Darwin", reason="problem with test on mac, TODO: look into this")
+def test_runs_bayes_nan():
+    np.random.seed(73)
+    bs = bayes.BayesianSearch()
+    r1 = Run('b', 'finished', {
+        'v1': {
+            'value': 7
+        },
+        'v2': {
+            'value': 6
+        }
+    }, {}, [
+        {
+            'loss': float('NaN')
+        },
+    ])
+    r2 = Run('b', 'finished', {
+        'v1': {
+            'value': 1
+        },
+        'v2': {
+            'value': 8
+        }
+    }, {'loss': float('NaN')}, [])
+    r3 = Run('b', 'finished', {
+        'v1': {
+            'value': 2
+        },
+        'v2': {
+            'value': 3
+        }
+    }, {}, [
+        {
+            'loss': 'NaN'
+        },
+    ])
+    r4 = Run('b', 'finished', {
+        'v1': {
+            'value': 4
+        },
+        'v2': {
+            'value': 5
+        }
+    }, {'loss': 'NaN'}, [])
+    # need two (non running) runs before we get a new set of parameters
+    runs = [r1, r2, r3, r4]
+    sweep = {'config': sweep_config_2params, 'runs': runs}
+    params, info = bs.next_run(sweep)
+    assert params['v1']['value'] == 10 and params['v2']['value'] == 2
