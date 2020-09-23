cbcf1d0bfc8ae41906e4e4c983eabcba18c7086d
Kyle Goyette <kdgoyette@gmail.com>
2020-09-23 14:32:27 -0700

    add viz file

--- wandb/viz.py
+++ wandb/viz.py
@@ -10,3 +10,52 @@ def visualize(viz_id, value):
     if not isinstance(value, Table):
         raise Error("visualize value must be Table, not {}".format(type(value).__name__))
     return Visualize(viz_id, value)
+
+class CustomChart:
+    def __init__(self, panel_config):
+        self.panel_config = panel_config
+
+def custom_plot_on_table(vega_spec_name, table_key, config_mapping):
+    userQuery = {
+        "userQuery": {
+            "queryFields": [
+                {
+                    "name": "runSets",
+                    "args": [
+                        {
+                            "name": "runSets",
+                            "value": "${runSets}"
+                        }
+                    ],
+                    "fields": [
+                        {
+                            "name": "name",
+                            "fields": []
+                        },
+                        {
+                            "name": "summaryTable",
+                            "args": [
+                                {
+                                    "name": "tableKey",
+                                    "value": table_key
+                                }
+                            ],
+                            "fields": []
+                        }
+                    ]
+                }
+            ],
+        }
+    }
+
+    panel_config = {}
+    panel_config.update(config_mapping)
+    panel_config.update({'panelDefId': vega_spec_name})
+    panel_config.update(
+        {'transform': {"name": "tableWithLeafColNames"}}
+    )
+    panel_config.update(
+        {"historyFieldSettings": {"key": table_key, "x-axis": "_step"}}
+    )
+    panel_config.update(userQuery)
+    return CustomChart(panel_config)
