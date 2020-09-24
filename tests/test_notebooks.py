3d27c61901acaa5bb81c2b9df10e9844c4c2e5ee
Chris Van Pelt <vanpelt@gmail.com>
2020-09-23 18:30:40 -0700

    Much better display in jupyter / colab (#1264)
    * Much better display in jupyter / colab
    
    * Fix tests
    
    * Fix lint error, try ipywidgets first, better formatting
    
    * Polish
    
    * Fix progress logging
    
    * Close the progress widget when were done
    
    * Make sure the links target blank

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -12,9 +12,9 @@ pytestmark = pytest.mark.skipif(
 def test_one_cell(notebook):
     with notebook("one_cell.ipynb") as nb:
         nb.execute_cell(cell_index=1)
-        text = nb.cell_output_text(1)
-        print(text)
-        assert "lovely-dawn-32" in text
+        output = nb.cell_output(1)
+        print(output)
+        assert "lovely-dawn-32" in output[-1]["data"]["text/html"]
         # assert "Failed to query for notebook name" not in text
 
 
