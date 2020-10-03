803c43288e9f3c7a7c3804abeecc5b36136d209b
Jeff Raubitschek <jeff@wandb.com>
2020-10-03 09:26:39 -0700

    fix unicode for py27

--- tests/test_sparkline.py
+++ tests/test_sparkline.py
@@ -4,24 +4,24 @@ from wandb.lib import sparkline
 
 
 def test_sparkline():
-    assert sparkline.sparkify([1,2,3]) == '▁▅█'
+    assert sparkline.sparkify([1,2,3]) == u'▁▅█'
 
 
 def test_sparkline_nan():
-    assert sparkline.sparkify([float('nan'),2,3]) == ' ▁█'
+    assert sparkline.sparkify([float('nan'),2,3]) == u' ▁█'
 
 
 def test_sparkline_inf():
-    assert sparkline.sparkify([float('inf'),2,3]) == ' ▁█'
+    assert sparkline.sparkify([float('inf'),2,3]) == u' ▁█'
 
 
 def test_sparkline_1finite():
-    assert sparkline.sparkify([float('inf'),2,float('-inf')]) == ' ▁ '
+    assert sparkline.sparkify([float('inf'),2,float('-inf')]) == u' ▁ '
 
 
 def test_sparkline_0finite():
-    assert sparkline.sparkify([float('inf'),float('nan'),float('-inf')]) == ''
+    assert sparkline.sparkify([float('inf'),float('nan'),float('-inf')]) == u''
 
 
 def test_sparkline_empty():
-    assert sparkline.sparkify([]) == ''
+    assert sparkline.sparkify([]) == u''
