31945a382eea86c7fac7594dbc5e3b3024b65345
Shawn Lewis <shlewis@gmail.com>
2020-10-06 20:34:03 -0700

    Clean up old plots and add docstrings

--- wandb/plot/pr_curve.py
+++ wandb/plot/pr_curve.py
@@ -1,11 +1,12 @@
 import wandb
 from wandb import util
-from wandb.plots.utils import test_missing, test_types, encode_labels
-from wandb import plots
+from wandb.plots.utils import test_missing, test_types
+
+
 chart_limit = wandb.Table.MAX_ROWS
 
-def pr_curve(y_true=None, y_probas=None, labels=None,
-             plot_micro=True, classes_to_plot=None):
+
+def pr_curve(y_true=None, y_probas=None, labels=None, classes_to_plot=None):
     """
     Computes the tradeoff between precision and recall for different thresholds.
         A high area under the curve represents both high recall and high precision,
@@ -28,7 +29,7 @@ def pr_curve(y_true=None, y_probas=None, labels=None,
     under 'auto visualizations'.
 
     Example:
-    wandb.log({'pr': wandb.plots.precision_recall(y_true, y_probas, labels)})
+    wandb.log({'pr-curve': wandb.plot.pr_curve(y_true, y_probas, labels)})
     """
     np = util.get_module("numpy", required="roc requires the numpy library, install with `pip install numpy`")
     scikit = util.get_module("sklearn", "roc requires the scikit library, install with `pip install scikit-learn`")
@@ -36,8 +37,8 @@ def pr_curve(y_true=None, y_probas=None, labels=None,
     y_true = np.array(y_true)
     y_probas = np.array(y_probas)
 
-    if (test_missing(y_true=y_true, y_probas=y_probas) and
-        test_types(y_true=y_true, y_probas=y_probas)):
+    if (test_missing(y_true=y_true, y_probas=y_probas)
+            and test_types(y_true=y_true, y_probas=y_probas)):
         classes = np.unique(y_true)
         probas = y_probas
 
@@ -53,9 +54,6 @@ def pr_curve(y_true=None, y_probas=None, labels=None,
         indices_to_plot = np.in1d(classes, classes_to_plot)
         for i, to_plot in enumerate(indices_to_plot):
             if to_plot:
-                average_precision = scikit.metrics.average_precision_score(
-                    binarized_y_true[:, i],
-                    probas[:, i])
                 precision, recall, _ = scikit.metrics.precision_recall_curve(
                     y_true, probas[:, i], pos_label=classes[i])
 
@@ -63,32 +61,33 @@ def pr_curve(y_true=None, y_probas=None, labels=None,
                 sample_precision = []
                 sample_recall = []
                 for k in range(samples):
-                    sample_precision.append(precision[int(len(precision)*k/samples)])
-                    sample_recall.append(recall[int(len(recall)*k/samples)])
-
+                    sample_precision.append(precision[int(len(precision) * k / samples)])
+                    sample_recall.append(recall[int(len(recall) * k / samples)])
 
                 pr_curves[classes[i]] = (sample_precision, sample_recall)
-        def pr_table(pr_curves):
-            data=[]
-            count=0
-            for i, class_name in enumerate(pr_curves.keys()):
-                precision, recall = pr_curves[class_name]
-                for p, r in zip(precision, recall):
-                    # if class_names are ints and labels are set
-                    if labels is not None and (isinstance(class_name, int)
-                                    or isinstance(class_name, np.integer)):
-                        class_name = labels[class_name]
-                    # if class_names are ints and labels are not set
-                    # or, if class_names have something other than ints
-                    # (string, float, date) - user class_names
-                    data.append([class_name, round(p, 3), round(r, 3)])
-                    count+=1
-                    if count >= chart_limit:
-                        wandb.termwarn("wandb uses only the first %d datapoints to create the plots."% wandb.Table.MAX_ROWS)
-                        break
-            return plots.line(
-                wandb.Table(
-                columns=['class', 'precision', 'recall'],
-                data=data
-            ), x='recall', y='precision', stroke='class')
-        return pr_table(pr_curves)
+
+        data = []
+        count = 0
+        for i, class_name in enumerate(pr_curves.keys()):
+            precision, recall = pr_curves[class_name]
+            for p, r in zip(precision, recall):
+                # if class_names are ints and labels are set
+                if labels is not None and (isinstance(class_name, int)
+                                           or isinstance(class_name, np.integer)):
+                    class_name = labels[class_name]
+                # if class_names are ints and labels are not set
+                # or, if class_names have something other than ints
+                # (string, float, date) - user class_names
+                data.append([class_name, round(p, 3), round(r, 3)])
+                count += 1
+                if count >= chart_limit:
+                    wandb.termwarn("wandb uses only the first %d datapoints to create the plots." % wandb.Table.MAX_ROWS)
+                    break
+        table = wandb.Table(
+            columns=['class', 'precision', 'recall'],
+            data=data)
+        return wandb.plot_table(
+            'wandb/area-under-curve/v0',
+            table,
+            {'x': 'recall', 'y': 'precision', 'class': 'class'},
+            {'title': 'Precision v. Recall'})
