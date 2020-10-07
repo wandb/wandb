8a1bd07c6578edf9301b6c3ea5b113035f1401dc
Shawn Lewis <shlewis@gmail.com>
2020-10-06 20:39:27 -0700

    Fix flake

--- wandb/plot/__init__.py
+++ wandb/plot/__init__.py
@@ -2,5 +2,16 @@ from wandb.plot.line import line
 from wandb.plot.histogram import histogram
 from wandb.plot.scatter import scatter
 from wandb.plot.bar import bar
+
 from wandb.plot.roc_curve import roc_curve
 from wandb.plot.pr_curve import pr_curve
+
+
+__all__ = [
+    "line",
+    "histogram",
+    "scatter",
+    "bar",
+    "roc_curve",
+    "pr_curve",
+]
