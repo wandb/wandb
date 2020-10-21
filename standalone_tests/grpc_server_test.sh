bc3bef2a6555063465977ec139da7df87fbabfe0
Jeff Raubitschek <jeff@wandb.com>
2020-10-21 15:21:25 -0700

    test passing env

--- standalone_tests/grpc_server_test.sh
+++ standalone_tests/grpc_server_test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
+echo "Stopping all old grpc-server"
+ps auxw | grep "wandb grpc-server" | grep -v grep | awk '{print $2}' | xargs kill
+echo "Wait for servers to be gone..."
+sleep 1
 echo "Running grpc-server in the background..."
 wandb grpc-server &
+echo "Wait for server to be up..."
+sleep 1
 echo "Starting grpc client..."
 python grpc_client.py
 echo "done."
