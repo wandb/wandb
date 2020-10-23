c51c6474a408cd4ce91987183b07b644155b685a
Annirudh Prasad <annirudh@wandb.com>
2020-10-22 17:00:30 -0700

    Fix comment

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -1744,7 +1744,7 @@ class Run(RunBase):
             title (str): The title of the alert, must be less than 64 characters long
             text (str): The text body of the alert
             level (str or wandb.AlertLevel, optional): The alert level to use, either: "INFO", "WARN", or "ERROR"
-            wait_duration (int or timedelta, optional): The time to wait before sending another alert with this title
+            wait_duration (int, float, or timedelta, optional): The time to wait before sending another alert with this title
         """
         level = level or wandb.AlertLevel.INFO
         if isinstance(level, wandb.AlertLevel):
