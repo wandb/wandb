9eaf5777066e19e676a3185e2f79a9cab51f4bf4
Jeff Raubitschek <jeff@wandb.com>
2020-11-02 15:23:05 -0800

    try without pyenv exec

--- standalone_tests/grpc_server_test.sh
+++ standalone_tests/grpc_server_test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "RAN with: $*"
 mkdir -p wandb
-pyenv version
-pip list
-pyenv exec which wandb
 echo "Stopping all old grpc-server"
 set +e
 (ps auxw | grep "wandb grpc-server" | grep -v grep | awk '{print $2}' | xargs kill) 2>/dev/null
@@ -11,9 +8,9 @@ set -e
 echo "Wait for servers to be gone..."
 sleep 1
 echo "Running grpc-server in the background..."
-pyenv exec wandb grpc-server &
+wandb grpc-server &
 echo "Wait for server to be up..."
 sleep 1
 echo "Starting grpc client..."
-pyenv exec python grpc_client.py
+python grpc_client.py
 echo "done."
