bb6b6f1655b0b7139c7282b385ec998c29ffe792
Carey Phelps <cvphelps@cs.stanford.edu>
2020-11-11 22:37:27 -0800

    syntax

--- wandb/sdk/wandb_watch.py
+++ wandb/sdk/wandb_watch.py
@@ -2,44 +2,46 @@
 """
 watch.
 """
-
+​
 import logging
 import os
-
+​
 import wandb
-
+​
 from .lib.ipython import _get_python_type
-
+​
 logger = logging.getLogger("wandb")
-
-
+​
+​
 _global_watch_idx = 0
-
-
+​
+​
 def watch(models, criterion=None, log="gradients", log_freq=1000, idx=None):
     """
     Hooks into the torch model to collect gradients and the topology.  Should be extended
     to accept arbitrary ML models.
-
-    :param (torch.Module) models: The model to hook, can be a tuple
-    :param (torch.F) criterion: An optional loss value being optimized
-    :param (str) log: One of "gradients", "parameters", "all", or None
-    :param (int) log_freq: log gradients and parameters every N batches
-    :param (int) idx: an index to be used when calling wandb.watch on multiple models
-    :return: (wandb.Graph) The graph object that will populate after the first backward pass
+    Args:
+        models (torch.Module): The model to hook, can be a tuple
+        criterion (torch.F): An optional loss value being optimized
+        log (str): One of "gradients", "parameters", "all", or None
+        log_freq (int): log gradients and parameters every N batches
+        idx (int): an index to be used when calling wandb.watch on multiple models
+    
+    Returns:
+        `wandb.Graph` The graph object that will populate after the first backward pass
     """
     global _global_watch_idx
-
+​
     logger.info("Watching")
     # TODO: temporary override for huggingface remove after: https://github.com/huggingface/transformers/pull/4220
     if os.getenv("WANDB_WATCH") == "false":
         return
-
+​
     if wandb.run is None:
         raise ValueError("You must call `wandb.init` before calling watch")
-
+​
     in_jupyter = _get_python_type() != "python"
-
+​
     log_parameters = False
     log_gradients = True
     if log == "all":
@@ -49,7 +51,7 @@ def watch(models, criterion=None, log="gradients", log_freq=1000, idx=None):
         log_gradients = False
     elif log is None:
         log_gradients = False
-
+​
     if not isinstance(models, (tuple, list)):
         models = (models,)
     graphs = []
@@ -62,7 +64,7 @@ def watch(models, criterion=None, log="gradients", log_freq=1000, idx=None):
         if global_idx > 0:
             # TODO: this makes ugly chart names like gradients/graph_1conv1d.bias
             prefix = "graph_%i" % global_idx
-
+​
         wandb.run.history.torch.add_log_hooks_to_pytorch_module(
             model,
             log_parameters=log_parameters,
@@ -71,19 +73,19 @@ def watch(models, criterion=None, log="gradients", log_freq=1000, idx=None):
             log_freq=log_freq,
             jupyter_run=wandb.run if in_jupyter else None,
         )
-
+​
         graph = wandb.wandb_torch.TorchGraph.hook_torch(
             model, criterion, graph_idx=global_idx
         )
         graphs.append(graph)
         # NOTE: the graph is set in run.summary by hook_torch on the backward pass
     return graphs
-
-
+​
+​
 def unwatch(models=None):
     """Remove pytorch gradient and parameter hooks.
-
-    Arguments:
+​
+    Args:
         models (list): Optional list of pytorch models that have had watch called on them
     """
     if models:
