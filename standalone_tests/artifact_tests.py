a49d112096a51966f988acda72babd7838df8255
Tim Sweeney <tim.s.sweeney@gmail.com>
2020-12-01 11:04:46 -0800

    Addressed comments

--- standalone_tests/artifact_tests.py
+++ standalone_tests/artifact_tests.py
@@ -3,6 +3,9 @@ import numpy as np
 import time
 import shutil
 
+def teardown():
+    shutil.rmtree("wandb")
+    shutil.rmtree("artifacts")
 
 def test_artifact_creation_with_diff_type():
     artifact_name = "a1-{}".format(str(time.time()))
@@ -37,8 +40,9 @@ def test_artifact_creation_with_diff_type():
         image_2 = artifact.get("image_2")
         assert image_2 is None
 
-    shutil.rmtree("wandb")
-    shutil.rmtree("artifacts")
 
 if __name__ == "__main__":
-    test_artifact_creation_with_diff_type()
+    try:
+        test_artifact_creation_with_diff_type()
+    finally:
+        teardown()
