33a9c5ca302f09726f2f985fc243fecf424776ad
Kyle Goyette <kdgoyette@gmail.com>
2020-12-16 10:32:10 -0800

    handle singular array of xs, and several

--- standalone_tests/basic_plots.py
+++ standalone_tests/basic_plots.py
@@ -23,6 +23,8 @@ ys.append([random.random() - math.cos(i / 10) for i in range(50)])
 line_series_plot = wandb.plot.line_series(xs, ys, keys, "Get serial With keys now!", "step")
 line_series_plot_no_title_no_keys = wandb.plot.line_series(xs, ys, xname="step")
 
+line_series_singular_x_array = wandb.plot.line_series([i for i in range(100)], ys,title="Get serial with one x", xname="step")
+
 histogram = wandb.plot.histogram(table, value='height', title='my-histo')
 scatter = wandb.plot.scatter(table, x='step', y='height', title='scatter!')
 
@@ -41,6 +43,7 @@ wandb.log({
     'line1': line_plot,
     'line_series1': line_series_plot,
     'line_series_no_title_no_keys': line_series_plot_no_title_no_keys,
+    'line_series_single_x': line_series_singular_x_array,
     'histogram1': histogram,
     'scatter1': scatter,
     'bar1': bar})
--- wandb/plot/line_series.py
+++ wandb/plot/line_series.py
@@ -1,4 +1,5 @@
 import wandb
+from collections.abc import Sequence
 
 
 def line_series(xs, ys, keys=None, title=None, xname=None):
@@ -17,15 +18,17 @@ def line_series(xs, ys, keys=None, title=None, xname=None):
     Example:
         ```
         x = [i for i in range(10)]
-        ys = {
-            "y1": [i for i in range(10)],
-            "y2": [i**2 for i in range(10)]
-        }
+        ys = [
+            [i for i in range(10)],
+            [i**2 for i in range(10)]
+        ]
         wandb.log({'line-series-plot1': wandb.plot.line_series(x, ys, "title", "step")})
         ```
     """
-    assert len(xs) == len(ys), "Number of x-lines and y-lines must match"
     data = []
+    if not isinstance(xs[0], Sequence):
+        xs = [xs for _ in range(len(ys))]
+    assert len(xs) == len(ys), "Number of x-lines and y-lines must match"
     for i, series in enumerate([list(zip(xs[i], ys[i])) for i in range(len(xs))]):
         for x, y in series:
             if keys is None:
