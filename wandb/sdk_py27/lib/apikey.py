6630148da3c309dd9b7654063876eeeb727bb114
Tim Sweeney <tim.s.sweeney@gmail.com>
2020-12-16 07:20:09 -0800

    Fixed wandb.login to respect host param (#1606)

--- wandb/sdk_py27/lib/apikey.py
+++ wandb/sdk_py27/lib/apikey.py
@@ -63,7 +63,7 @@ def prompt_api_key(  # noqa: C901
     """
     input_callback = input_callback or getpass.getpass
     log_string = term.LOG_STRING
-    api = api or InternalApi()
+    api = api or InternalApi(settings)
     anon_mode = _fixup_anon_mode(settings.anonymous)
     jupyter = settings._jupyter or False
     app_url = api.app_url
