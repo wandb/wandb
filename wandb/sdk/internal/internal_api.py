2630e748bcbb103111bc87cb9394f419f87cea61
Justin Brooks <justin@datatorch.io>
2020-12-17 16:04:24 -0500

    chore: fix liniting

--- wandb/sdk/internal/internal_api.py
+++ wandb/sdk/internal/internal_api.py
@@ -1090,17 +1090,10 @@ class Api(object):
         assert run, "run must be specified"
         entity = entity or self.settings("entity")
         query_result = self.gql(
-            query,
-            variable_values={
-                "name": project,
-                "run": run,
-                "entity": entity,
-            },
+            query, variable_values={"name": project, "run": run, "entity": entity,},
         )
         if query_result["model"] is None:
-            raise CommError(
-                "Run does not exist {}/{}/{}.".format(entity, project, run)
-            )
+            raise CommError("Run does not exist {}/{}/{}.".format(entity, project, run))
         files = self._flatten_edges(query_result["model"]["bucket"]["files"])
         return {file["name"]: file for file in files if file}
 
