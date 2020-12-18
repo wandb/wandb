0aecf6f3915d0601b70dd4f222669c9a2d921206
Tim Sweeney <tim.s.sweeney@gmail.com>
2020-12-18 15:19:11 -0800

    Validated regression tests

--- standalone_tests/artifact_object_reference_test.py
+++ standalone_tests/artifact_object_reference_test.py
@@ -33,7 +33,7 @@ os.environ["WANDB_SILENT"] = WANDB_SILENT
 import wandb
 
 
-columns = ["id", "bool", "int", "float", "Image"]
+columns = ["id", "bool", "int", "float", "Image", "Clouds"]
 
 def _make_wandb_image(suffix=""):
     class_labels = {1: "tree", 2: "car", 3: "road"}
