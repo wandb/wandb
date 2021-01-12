c48435efb80c6c6e7294f0f04be9714a3797d87d
bcampbell <bcampbell@rechargeapps.com>
2021-01-12 11:24:52 -0800

    Fixing linting

--- wandb/integration/xgboost/__init__.py
+++ wandb/integration/xgboost/__init__.py
@@ -12,19 +12,21 @@ bst = xgb.train(param_list, d_train, callbacks=[wandb_callback()])
 import wandb
 import xgboost
 
+
 class WandbCallback(xgboost.callback.TrainingCallback):
     def __init__(self):
         pass
 
     def _get_key(self, data, metric):
-        return f'{data}-{metric}'
+        return f"{data}-{metric}"
 
     def after_iteration(self, model, epoch, evals_log):
         for data, metric in evals_log.items():
             for metric_name, log in metric.items():
-                key = self._get_key(data, metric_name) 
+                key = self._get_key(data, metric_name)
                 wandb.log({key: log[-1]}, commit=False)
         wandb.log({})
+
 
 def wandb_callback():
     return WandbCallback()
