036050547925136f520faaf78354542d555548b1
Fariz Rahman <farizrahman4u@gmail.com>
2021-01-22 15:59:22 +0530

    fix

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -7,7 +7,7 @@ def api(runner):
     return wandb.Api()
 
 
-def test_nested_summary(mock_server):
+def test_nested_summary(api, mock_server):
     run = api.runs("test/test")[0]
     summary_dict = {"a": {"b": {"c": 0.9}}}
     summary = wandb.old.summary.Summary(run, summary_dict)
