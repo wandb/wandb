a2d6c9b81ee56bbbdcd0b0779cea7cbb22e46901
KyleGoyette <kdgoyette@gmail.com>
2021-01-21 21:59:36 -0800

    [WB-4366]: Stop moviepy from outputting when making videos or gifs (#1728)
    * add logger=None
    
    * add extra try
    
    * add test for mp4 and gif
    
    * black formatting
    
    * remove unneeded lines in test
    
    Co-authored-by: Jeff Raubitschek <jeff@wandb.com>

--- tests/test_data_types.py
+++ tests/test_data_types.py
@@ -344,11 +344,19 @@ def test_create_bokeh_plot(mocked_run):
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="No moviepy.editor in py2")
-def test_video_numpy(mocked_run):
+def test_video_numpy_gif(mocked_run):
     video = np.random.randint(255, size=(10, 3, 28, 28))
-    vid = wandb.Video(video)
+    vid = wandb.Video(video, format="gif")
     vid.bind_to_run(mocked_run, "videos", 0)
     assert vid.to_json(mocked_run)["path"].endswith(".gif")
+
+
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="No moviepy.editor in py2")
+def test_video_numpy_mp4(mocked_run):
+    video = np.random.randint(255, size=(10, 3, 28, 28))
+    vid = wandb.Video(video, format="mp4")
+    vid.bind_to_run(mocked_run, "videos", 0)
+    assert vid.to_json(mocked_run)["path"].endswith(".mp4")
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="No moviepy.editor in py2")
