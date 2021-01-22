993924423528c19028dbaa5d4b72804befa7955e
Fariz Rahman <farizrahman4u@gmail.com>
2021-01-22 14:37:23 +0530

    test

---
+++ tests/test_summary.py
@@ -0,0 +1,13 @@
+import wandb
+
+
+@pytest.fixture
+def api(runner):
+    return wandb.Api()
+
+
+def test_nested_summary(mock_server):
+    run = api.runs("test/test")[0]
+    summary_dict = {"a": {"b": {"c": 0.9}}}
+    summary = wandb.old.summary.Summary(run, summary_dict)
+    assert summary["a"]["b"]["c"] == 0.9
