1ffef0eb521e0030656c6739ee679084b071dbb2
Jeff Raubitschek <jeff@wandb.com>
2021-01-26 01:32:54 -0800

    update

--- wandb/sdk_py27/internal/handler.py
+++ wandb/sdk_py27/internal/handler.py
@@ -43,11 +43,11 @@ class HandleManager(object):
     # _consolidated_summary: SummaryDict
     # _sampled_history: Dict[str, sample.UniformSampleAccumulator]
     # _settings: SettingsStatic
-    # _record_q: Queue[Record]
-    # _result_q: Queue[Result]
+    # _record_q: "Queue[Record]"
+    # _result_q: "Queue[Result]"
     # _stopped: Event
-    # _sender_q: Queue[Record]
-    # _writer_q: Queue[Record]
+    # _sender_q: "Queue[Record]"
+    # _writer_q: "Queue[Record]"
     # _interface: BackendSender
     # _system_stats: Optional[stats.SystemStats]
     # _tb_watcher: Optional[tb_watcher.TBWatcher]
