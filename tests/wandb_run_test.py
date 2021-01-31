2963eb395e00ce0ece46f018ebb2dc4ccd548cd5
Jeff Raubitschek <jeff@wandb.com>
2021-01-31 11:24:36 -0800

    add basic run test

---
+++ tests/wandb_run_test.py
@@ -0,0 +1,31 @@
+"""
+config tests.
+"""
+
+import pytest
+import yaml
+import wandb
+from wandb import wandb_sdk
+
+
+def test_run_basic():
+    s = wandb.Settings()
+    c = dict(param1=2, param2=4)
+    run = wandb_sdk.wandb_run.Run(settings=s, config=c)
+    assert dict(run.config) == dict(param1=2, param2=4)
+
+
+def test_run_sweep():
+    s = wandb.Settings()
+    c = dict(param1=2, param2=4)
+    sw = dict(param3=9)
+    run = wandb_sdk.wandb_run.Run(settings=s, config=c, sweep_config=sw)
+    assert dict(run.config) == dict(param1=2, param2=4, param3=9)
+
+
+def test_run_sweep_overlap():
+    s = wandb.Settings()
+    c = dict(param1=2, param2=4)
+    sw = dict(param2=8, param3=9)
+    run = wandb_sdk.wandb_run.Run(settings=s, config=c, sweep_config=sw)
+    assert dict(run.config) == dict(param1=2, param2=8, param3=9)
