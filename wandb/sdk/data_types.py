ab9fb33d3a02a60bafd08f13906ab9994a2d2a60
Jeff Raubitschek <jeff@wandb.com>
2021-02-12 12:01:36 -0800

    [WB-4340] Type interface/interface.py (#1833)

--- wandb/sdk/data_types.py
+++ wandb/sdk/data_types.py
@@ -1963,7 +1963,7 @@ class Plotly(Media):
 
 
 def history_dict_to_json(
-    run: "LocalRun", payload: dict, step: Optional[int] = None
+    run: "Optional[LocalRun]", payload: dict, step: Optional[int] = None
 ) -> dict:
     # Converts a History row dict's elements so they're friendly for JSON serialization.
 
@@ -1984,7 +1984,7 @@ def history_dict_to_json(
 
 # TODO: refine this
 def val_to_json(
-    run: "LocalRun",
+    run: "Optional[LocalRun]",
     key: str,
     val: "ValToJsonType",
     namespace: Optional[Union[str, int]] = None,
@@ -1999,11 +1999,13 @@ def val_to_json(
     typename = util.get_full_typename(val)
 
     if util.is_pandas_data_frame(val):
+        assert run
         assert namespace == "summary", "We don't yet support DataFrames in History."
         return _data_frame_to_json(val, run, key, namespace)
     elif util.is_matplotlib_typename(typename) or util.is_plotly_typename(typename):
         val = Plotly.make_plot_media(val)
     elif isinstance(val, SixSequence) and all(isinstance(v, WBValue) for v in val):
+        assert run
         # This check will break down if Image/Audio/... have child classes.
         if (
             len(val)
@@ -2031,6 +2033,7 @@ def val_to_json(
             return [val_to_json(run, key, v, namespace=namespace) for v in val]
 
     if isinstance(val, WBValue):
+        assert run
         if isinstance(val, Media) and not val.is_bound():
             val.bind_to_run(run, key, namespace)
         return val.to_json(run)
