0ce25eb37d8cd81a55ad0288b0f6acf87c0bcacc
David Jackson <davidjackson@Davids-MacBook-Pro.local>
2021-02-22 15:00:30 -0800

    audio refs test s3 -> https

--- tests/test_data_types.py
+++ tests/test_data_types.py
@@ -280,7 +280,7 @@ def test_audio_to_json(mocked_run):
 
 
 def test_audio_refs():
-    audioObj = wandb.Audio("s3://wandb-artifacts-refs-public-test/StarWars3.wav")
+    audioObj = wandb.Audio("https://wandb-artifacts-refs-public-test.s3-us-west-2.amazonaws.com/StarWars3.wav")
     art = wandb.Artifact("audio_ref_test", "dataset")
     art.add(audioObj, "audio_ref")
 
