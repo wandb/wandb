3c7f6a0dec00339a65692b77a5bae95163957662
Fariz Rahman <farizrahman4u@gmail.com>
2021-02-24 00:06:55 +0530

    refac + test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,17 +1,21 @@
+import itertools
 import random
 import sys
 import os
 import pytest
 import numpy
 import platform
+import random
+import string
 
 if sys.version_info >= (3, 9):
     pytest.importorskip("tensorflow")
 import tensorflow
-import plotly
+
+# import plotly
 import matplotlib.pyplot as plt
 
-from . import utils
+# from . import utils
 from wandb import util
 
 try:
@@ -274,3 +278,64 @@ def test_matplotlib_to_plotly():
     fig = utils.matplotlib_without_image()
     assert type(util.matplotlib_to_plotly(plt)) == plotly.graph_objs._figure.Figure
     plt.close()
+
+
+def test_split_files():
+    def rand_string_list(size):
+        width = max(1, int(size / 10))
+        num_lines = int(size / width)
+        return [
+            "".join(
+                random.choices(
+                    string.ascii_letters
+                    + string.punctuation
+                    + string.digits
+                    + string.whitespace,
+                    k=random.randint(1, width),
+                )
+            )
+            for _ in range(num_lines)
+        ]
+
+    file_size = 1  # MB
+    num_files = 10
+    chunk_size = 0.1  # MB
+    files = {
+        "file_%s.txt"
+        % i: {"content": rand_string_list(int(file_size * 1024 * 1024)), "offset": 0}
+        for i in range(num_files)
+    }
+    chunks = list(util.split_files(files, MAX_MB=chunk_size))
+
+    # re-combine chunks
+    buff = {}
+    for c in chunks:
+        for k, v in c.items():
+            if k in buff:
+                buff[k].append(v)
+            else:
+                buff[k] = [v]
+    files2 = {
+        k: {
+            "content": list(
+                itertools.chain(
+                    *(c["content"] for c in sorted(v, key=lambda c: c["offset"]))
+                )
+            ),
+            "offset": 0,
+        }
+        for k, v in buff.items()
+    }
+    assert files == files2
+
+    # Verify chunk offsets (These can be messed up and above assertion would still pass).
+    for fname in files:
+        offset_size_pairs = [
+            (c[fname]["offset"], len(c[fname]["content"])) for c in chunks if fname in c
+        ]
+        offset_size_pairs.sort(key=lambda p: p[0])
+        assert offset_size_pairs[0][0] == 0
+        offsets = [p[0] for p in offset_size_pairs]
+        for i in range(len(offset_size_pairs) - 1):
+            assert offset_size_pairs[i + 1][0] == sum(offset_size_pairs[i])
+        assert sum(offset_size_pairs[-1]) == len(files[fname]["content"])
--- wandb/sdk/internal/file_stream.py
+++ wandb/sdk/internal/file_stream.py
@@ -267,100 +267,12 @@ class FileStreamApi(object):
             if not files[filename]:
                 del files[filename]
 
-        for fs in self._split(files):
+        for fs in wandb.util.split_files(files, MAX_MB=10):
             self._handle_response(
                 util.request_with_retry(
                     self._client.post, self._endpoint, json={"files": fs}
                 )
             )
-
-    def _split(self, files, MAX_MB=10):
-        current_volume = {}
-        current_size = 0
-        max_size = MAX_MB * 1024 * 1024
-
-        def _str_size(x):
-            return len(x) if isinstance(x, bytes) else len(x.encode("utf-8"))
-
-        def _file_size(file):
-            size = file.get("_size")
-            if size is None:
-                size = sum(map(_str_size, file["content"]))
-                file["_size"] = size
-            return size
-
-        def _split_file(file, num_lines):
-            offset = file["offset"]
-            content = file["content"]
-            name = file["name"]
-            f1 = {"offset": offset, "content": content[:num_lines], "name": name}
-            f2 = {
-                "offset": offset + num_lines,
-                "content": content[num_lines:],
-                "name": name,
-            }
-            return f1, f2
-
-        def _num_lines_from_num_bytes(file, num_bytes):
-            size = 0
-            num_lines = 0
-            content = file["content"]
-            while True:
-                size += _str_size(content[num_lines])
-                if size > num_bytes:
-                    break
-                num_lines += 1
-            return num_lines
-
-        files_stack = [
-            {"name": k, "offset": v["offset"], "content": v["content"]}
-            for k, v in files.items()
-        ]
-
-        while files_stack:
-            f = files_stack.pop()
-            # For each file, we have to do 1 of 4 things:
-            # - Add the file as such to the current volume if possible.
-            # - Split the file and add the first part to the current volume and push the second part back onto the stack.
-            # - If that's not possible, check if current volume is empty:
-            # - If empty, add first line of file to current volume and push rest onto stack (This volume will exceed MAX_MB).
-            # - If not, push file back to stack and yield current volume.
-            fsize = _file_size(f)
-            rem = max_size - current_size
-            if fsize <= rem:
-                current_volume[f["name"]] = {
-                    "offset": f["offset"],
-                    "content": f["content"],
-                }
-                current_size += fsize
-            else:
-                num_lines = _num_lines_from_num_bytes(f, rem)
-                if not num_lines and not current_volume:
-                    num_lines = 1
-                if num_lines:
-                    f1, f2 = _split_file(f, num_lines)
-                    current_volume[f1["name"]] = {
-                        "offset": f1["offset"],
-                        "content": f1["content"],
-                    }
-                    files_stack.append(f2)
-                    yield current_volume
-                    current_size = 0
-                    continue
-                else:
-                    files_stack.append(f)
-                    yield current_volume
-                    current_volume = {}
-                    current_size = 0
-                    continue
-            if current_size >= max_size:
-                yield current_volume
-                current_volume = {}
-                current_size = 0
-                continue
-
-        if current_volume:
-            yield current_volume
 
     def stream_file(self, path):
         name = path.split("/")[-1]
