fb012be5df424379a5e8979ac19c47fcf172263b
Tim Sweeney <tim.s.sweeney@gmail.com>
2021-02-24 13:44:26 -0800

    moving away from strings?

--- tests/wandb_integration_test.py
+++ tests/wandb_integration_test.py
@@ -166,14 +166,14 @@ def test_include_exclude_config_keys(live_mock_server, test_settings):
 
 def test_resume_config_merge(live_mock_server, test_settings):
     live_mock_server.set_ctx({"resume": True})
-    live_mock_server.set_ctx({"run.config": json.dumps({})})
+    live_mock_server.set_ctx({"run.config": {}})
     # Start the run without any config
     run = wandb.init(reinit=True, resume=True, settings=test_settings,)
     assert len(run.config.keys()) == 0
     run.finish()
 
     # resume the run with 2 new keys
-    live_mock_server.set_ctx({"run.config": json.dumps({})})
+    live_mock_server.set_ctx({"run.config": {}})
     run = wandb.init(
         reinit=True, resume=True, settings=test_settings, config={"a": 1, "b": 2},
     )
@@ -182,9 +182,7 @@ def test_resume_config_merge(live_mock_server, test_settings):
     run.finish()
 
     # resume the run with 1 new key and 1 ignored update
-    live_mock_server.set_ctx(
-        {"run.config": json.dumps({"a": {"value": 1}, "b": {"value": 2}})}
-    )
+    live_mock_server.set_ctx({"run.config": {"a": {"value": 1}, "b": {"value": 2}}})
     run = wandb.init(
         reinit=True, resume=True, settings=test_settings, config={"b": 200, "c": 3},
     )
@@ -195,11 +193,7 @@ def test_resume_config_merge(live_mock_server, test_settings):
 
     # resume the run with empty
     live_mock_server.set_ctx(
-        {
-            "run.config": json.dumps(
-                {"a": {"value": 1}, "b": {"value": 2}, "c": {"value": 3}}
-            )
-        }
+        {"run.config": {"a": {"value": 1}, "b": {"value": 2}, "c": {"value": 3}}}
     )
     run = wandb.init(reinit=True, resume=True, settings=test_settings,)
     assert run.config["a"] == 1
