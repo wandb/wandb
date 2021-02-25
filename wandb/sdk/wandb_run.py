fc9563614ff7f8d0132cc1b6b1cc456f42c1d17a
Chris Van Pelt <vanpelt@gmail.com>
2021-02-25 15:42:59 -0800

    Only use code_dir if save_code

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -1510,7 +1510,7 @@ class Run(object):
                     "Run `wandb online` or set WANDB_MODE=online to enable cloud syncing."
                 )
             )
-        if self._settings.code_dir is not None:
+        if self._settings.save_code and self._settings.code_dir is not None:
             self.log_code(self._settings.code_dir)
         if self._run_obj and not self._settings._silent:
             self._display_run()
