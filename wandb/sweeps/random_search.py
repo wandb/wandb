9c6d0838b6f6e3dc33cd925d73e4e45e5a7d9684
Lukas Biewald <lukas@wandb.com>
2021-02-25 20:11:04 -0800

    adds option to do random search without replacement

--- wandb/sweeps/random_search.py
+++ wandb/sweeps/random_search.py
@@ -8,13 +8,51 @@ from wandb.sweeps.params import HyperParameterSet
 
 class RandomSearch(Search):
     def next_run(self, sweep):
-        # print(sweep)
         if 'parameters' not in sweep['config']:
             raise ValueError('Random search requires "parameters" section')
+
+        sample_with_replacement = False
+        if 'options' in sweep['config']:
+            if sweep['config']['options']['sample_with_replacement']:
+                sample_with_replacement = True
+
         config = sweep['config']['parameters']
         params = HyperParameterSet.from_config(config)
 
-        for param in params:
-            param.value = param.sample()
+        if (sample_with_replacement):
+            for param in params:
+                param.value = param.sample()
+        else:
+            # build up a set of all the config values in previous runs
+            # limited to the params used in this sweep
+            param_names = [param.name for param in params]
+            previous_runs = sweep['runs']
+            previous_runs_params_list = [run.config or {}
+                                         for run in previous_runs]
+            previous_runs_sweep_params_list = []
+
+            for previous_run_params in previous_runs_params_list:
+                previous_run_sweep_params = []
+                for param_name in param_names:
+                    previous_run_sweep_params.append(
+                        previous_run_params[param_name]['value'])
+
+                previous_runs_sweep_params_list.append(
+                    str(previous_run_sweep_params))
+
+            previous_runs_sweep_params_set = set(
+                previous_runs_sweep_params_list)
+
+            # look for a set of parameters that hasn't been used in a previous run
+            for i in range(10000):
+                param_value_list = []
+                for param in params:
+                    param.value = param.sample()
+                    param_value_list.append(param.value)
+
+                if (str(param_value_list) in previous_runs_sweep_params_set):
+                    continue
+                else:
+                    break
 
         return (params.to_config(), None)
--- wandb/sweeps/test_random_search.py
+++ wandb/sweeps/test_random_search.py
@@ -1,5 +1,6 @@
 import numpy as np
-from wandb.sweeps import random_search
+# from wandb.sweeps import random_search
+import random_search
 import random
 import pytest
 
@@ -8,6 +9,9 @@ sweep_config_2params = {'parameters': {
     'v1': {'min': 3, 'max': 5},
     'v2': {'min': 5, 'max': 6}}}
 
+sweep_config_3params = {'parameters': {
+    'v1': {'min': 3, 'max': 4},
+    'v2': {'min': 5, 'max': 6}}}
 
 sweep_config_bad = {'parameters': {
     'v1': {'max': 3, 'min': 5},
@@ -25,6 +29,47 @@ def test_rand_single():
     assert params['v1']['value'] == 3 and params['v2']['value'] == 6
 
 
+def test_rand_no_replacement():
+    random.seed(73)
+    np.random.seed(73)
+    r1 = Run('b', 'finished', {
+        'v1': {
+            'value': 3
+        },
+        'v2': {
+            'value': 5
+        }
+    }, {'zloss': 1.2}, [
+        {
+            'loss': 1.2
+        },
+    ])
+    r2 = Run('b', 'finished', {
+        'v1': {
+            'value': 3
+        },
+        'v2': {
+            'value': 6
+        }
+    }, {'loss': 0.4}, [])
+    r3 = Run('b', 'finished', {
+        'v1': {
+             'value': 4
+             },
+        'v2': {
+            'value': 5
+        }
+    }, {'loss': 0.4}, [])
+    runs = [r1, r2, r3]
+    sweep = {'config': sweep_config_3params, 'runs': runs}
+    gs = random_search.RandomSearch()
+
+    params, info = gs.next_run(sweep)
+    print(params)
+    assert info == None
+    assert params['v1']['value'] == 4 and params['v2']['value'] == 6
+
+
 def test_rand_bad():
     random.seed(73)
     np.random.seed(73)
@@ -33,3 +78,19 @@ def test_rand_bad():
     sweep = {'config': sweep_config_bad, 'runs': runs}
     with pytest.raises(ValueError) as excinfo:
         params, info = gs.next_run(sweep)
+
+
+class Run(object):
+    def __init__(self, name, state, config, summary, history):
+        self.name = name
+        self.state = state
+        self.config = config
+        self.summaryMetrics = summary
+        self.history = history
+
+    def __repr__(self):
+        return 'Run(%s,%s,%s,%s,%s)' % (self.name, self.state, self.config,
+                                        self.history, self.summaryMetrics)
+
+
+test_rand_no_replacement()
