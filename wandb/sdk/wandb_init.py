fe8b66b8b57c6495eb9fd7c381690a0fb4013407
ariG23498 <aritra.born2fly@gmail.com>
2021-03-03 11:45:41 +0530

    trainling WS

--- wandb/sdk/wandb_init.py
+++ wandb/sdk/wandb_init.py
@@ -650,7 +650,7 @@ def init(
         sync_tensorboard: (bool, optional) Whether to copy all TensorBoard logs
             to W&B (default: False).
             [Tensorboard](https://docs.wandb.com/integrations/tensorboard)
-        resume: (bool, str, optional) Sets the resuming behavior. 
+        resume: (bool, str, optional) Sets the resuming behavior.
             Options:
             "allow", "must", "never", "auto" or None. Defaults to None.
             Cases:
