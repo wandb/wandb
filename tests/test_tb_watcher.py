99efddaabf735f5db3ad8461667df88df83e5966
Kyle Goyette <kdgoyette@gmail.com>
2021-03-09 09:14:30 -0800

    add import or skip

--- tests/test_tb_watcher.py
+++ tests/test_tb_watcher.py
@@ -108,6 +108,8 @@ def test_tb_watcher_save_row_custom_chart(mocked_run, tbwatcher_util):
 
 def test_tb_watcher_logdir_not_exists(mocked_run, tbwatcher_util, capsys):
     # TODO: check caplog for right error text
+    pytest.importorskip("tensorboard.summary.v1")
+    import tensorboard.summary.v1 as tb_summary
     log_dir = os.path.join(mocked_run.dir, "test_tb_dne_dir")
 
     def write_fun():
