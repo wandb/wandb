03c43a7b80364b46e9c9657cae08216a23c61751
Jeff Raubitschek <jeff@wandb.com>
2021-03-15 11:08:34 -0700

    Mark flaky: (1) test_sync_tensorboard_big - tests.test_cli (#1958)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -861,6 +861,8 @@ def test_sync_tensorboard(runner, live_mock_server):
         assert os.listdir(".") == ["events.out.tfevents.1585769947.cvp"]
 
 
+@pytest.mark.flaky
+@pytest.mark.xfail(sys.version_info[:2] == (3, 6), reason="test seems flaky with py36")
 @pytest.mark.skipif(
     sys.version_info >= (3, 9), reason="Tensorboard not currently built for 3.9"
 )
