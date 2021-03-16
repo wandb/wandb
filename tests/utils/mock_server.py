1ed6383468f89d0a359da0cdb111cfcd56b4b4e6
Chris Van Pelt <vanpelt@gmail.com>
2021-03-16 16:32:59 -0700

    Spell working again

--- tests/utils/mock_server.py
+++ tests/utils/mock_server.py
@@ -43,6 +43,7 @@ def mock_server(mocker):
     mocker.patch("wandb.wandb_sdk.internal.file_stream.requests", mock)
     mocker.patch("wandb.wandb_sdk.internal.internal_api.requests", mock)
     mocker.patch("wandb.wandb_sdk.internal.update.requests", mock)
+    mocker.patch("wandb.wandb_sdk.internal.sender.requests", mock)
     mocker.patch("wandb.apis.internal_runqueue.requests", mock)
     mocker.patch("wandb.apis.public.requests", mock)
     mocker.patch("wandb.util.requests", mock)
@@ -806,6 +807,12 @@ index 30d74d2..9a2c773 100644
                 }
             ]
         )
+
+    @app.route("/wandb_url", methods=["PUT"])
+    def spell_url():
+        ctx = get_ctx()
+        ctx["spell_data"] = request.get_json()
+        return json.dumps({"success": True})
 
     @app.route("/pypi/<library>/json")
     def pypi(library):
