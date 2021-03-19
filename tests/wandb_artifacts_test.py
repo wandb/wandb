ab776ec8f29fa2a09aaa169a179046045c4d965e
Fariz Rahman <farizrahman4u@gmail.com>
2021-03-19 19:33:14 +0530

    checksum update

--- tests/wandb_artifacts_test.py
+++ tests/wandb_artifacts_test.py
@@ -249,7 +249,7 @@ def test_add_reference_local_dir_no_checksum(runner):
         artifact = wandb.Artifact(type="dataset", name="my-arty")
         artifact.add_reference("file://" + os.getcwd(), checksum=False)
 
-        assert artifact.digest == "27832cb94f683de172e7012744dadd06"
+        assert artifact.digest == "bd4bd89903919fb0e2a2adb7edff8712"
         manifest = artifact.manifest.to_manifest_json()
         assert manifest["contents"]["file1.txt"] == {
             "digest": "file://file1.txt",
