afb242d3cd1fa986c6775f09fe67169fe7c4eb1d
Kyle Goyette <kdgoyette@gmail.com>
2021-03-29 21:10:52 -0700

    revert broken test

--- tests/test_sender.py
+++ tests/test_sender.py
@@ -147,8 +147,14 @@ def test_save_live_multi_write(
 
 
 def test_save_live_glob_multi_write(
-    mocked_run, mock_server, internal_sender, start_backend, stop_backend
+    mocked_run, mock_server, internal_sender, start_backend, stop_backend, mocker
 ):
+    def mock_min_size(self, size):
+        return 1
+
+    mocker.patch('wandb.filesync.dir_watcher.PolicyLive.RATE_LIMIT_SECONDS', 1)
+    mocker.patch('wandb.filesync.dir_watcher.PolicyLive.min_wait_for_size', mock_min_size)
+
     start_backend()
     internal_sender.publish_files({"files": [("checkpoints/*", "live")]})
     mkdir_exists_ok(os.path.join(mocked_run.dir, "checkpoints"))
@@ -179,8 +185,7 @@ def test_save_live_glob_multi_write(
     # TODO: Get PolicyLive mock working so this test can be performed properly
     # then set this line back to 3. Right now it only does 2, one on first creation
     # second on finish, because there's a 60 second rate limit
-    # assert len(mock_server.ctx["storage?file=checkpoints/test_1.txt"]) == 3
-    assert len(mock_server.ctx["storage?file=checkpoints/test_1.txt"]) == 2
+    assert len(mock_server.ctx["storage?file=checkpoints/test_1.txt"]) == 3
     assert len(mock_server.ctx["storage?file=checkpoints/test_2.txt"]) == 1
 
 
