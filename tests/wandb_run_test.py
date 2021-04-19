d4e0060d160bff9fdc5919f55a2bc056ea6c73bd
Fariz Rahman <farizrahman4u@gmail.com>
2021-04-19 17:11:08 +0530

    test

--- tests/wandb_run_test.py
+++ tests/wandb_run_test.py
@@ -101,3 +101,30 @@ def test_log_code_custom_root(test_settings):
     run = wandb.init(mode="offline", settings=test_settings)
     art = run.log_code(root="../")
     assert sorted(art.manifest.entries.keys()) == ["custom/test.py", "test.py"]
+
+
+def test_except_hook(test_settings):
+    # Test to make sure we respect excepthooks by 3rd parties like pdb
+    errs = []
+    hook = lambda etype, val, tb: errs.append(str(val))
+    sys.excepthook = hook
+
+    # We cant use raise statement in pytest context
+    raise_ = lambda exc: sys.excepthook(type(exc), exc, None)
+
+    raise_(Exception("Before wandb.init()"))
+
+    run = wandb.init(mode="offline", settings=test_settings)
+
+    old_stderr_write = sys.stderr.write
+    stderr = []
+    sys.stderr.write = stderr.append
+
+    raise_(Exception("After wandb.init()"))
+
+    assert errs == ["Before wandb.init()", "After wandb.init()"]
+
+    # make sure wandb prints the traceback
+    assert stderr == ["Exception: After wandb.init()\n", ""]
+
+    sys.stderr.write = old_stderr_write
