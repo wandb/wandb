7d75aaf5ed488c39a8bef9e71c48511aa6380d97
Fariz Rahman <farizrahman4u@gmail.com>
2021-04-26 13:58:05 +0530

    test

--- tests/test_redir.py
+++ tests/test_redir.py
@@ -252,7 +252,7 @@ def test_run_with_console_redirect2(console_settings, capfd):
 
         run.finish()
 
-@pytest.mark.parametrize("console_settings", console_modes, indirect=True)
+@pytest.mark.parametrize("console_settings", [console_modes[1]], indirect=True)
 def test_offline_compression(console_settings, capfd, runner):
     with capfd.disabled():
         s = wandb.Settings(mode="offline")
