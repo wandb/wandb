8a825e93c993a2d7e2de5fda9b1a55c43213da0e
Fariz Rahman <farizrahman4u@gmail.com>
2021-04-26 14:04:22 +0530

    test

--- tests/test_redir.py
+++ tests/test_redir.py
@@ -241,8 +241,8 @@ def test_run_with_console_redirect(console_settings, capfd):
 def test_offline_compression(console_settings, capfd, runner):
     with capfd.disabled():
         s = wandb.Settings(mode="offline")
-        console_settings._apply_settings(s)
-        run = wandb.init(settings=console_settings)
+        # console_settings._apply_settings(s)
+        # run = wandb.init(settings=console_settings)
         # for i in tqdm.tqdm(range(100), ncols=139):
         #     time.sleep(0.05)
 
