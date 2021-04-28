da6689ab4d56233872f1fc5e388161ab5b3730b6
Danny Goldstein <dgold@berkeley.edu>
2021-04-28 12:39:03 -0700

    add mark_preempting

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -2351,6 +2351,12 @@ class Run(object):
         self.finish(exit_code)
         return exc_type is None
 
+    def mark_preempting(self) -> None:
+        """Mark this run as preempting and tell the internal process
+        to immediately report this to the server."""
+        self.preempted = True
+        self._backend.interface.publish_preempting()
+
 
 # We define this outside of the run context to support restoring before init
 def restore(
