99f9312c3e93f9b86ac74b97f722d756f65faab7
stephchen <stephchen@wandb.com>
2021-05-03 13:17:06 -0700

    sagemaker resume fix

--- wandb/sdk/wandb_settings.py
+++ wandb/sdk/wandb_settings.py
@@ -713,6 +713,13 @@ class Settings(object):
             _logger.info("setting login settings: {}".format(login_settings))
         self._update(login_settings, _source=self.Source.LOGIN)
 
+    def _apply_setup(
+        self, setup_settings: Dict[str, Any], _logger: Optional[_EarlyLogger] = None
+    ) -> None:
+        if _logger:
+            _logger.info("setting setup settings: {}".format(setup_settings))
+        self._update(setup_settings, _source=self.Source.SETUP)
+
     def _path_convert_part(
         self, path_part: str, format_dict: Dict[str, Any]
     ) -> Optional[List[str]]:
