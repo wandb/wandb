6797ae1f7a0d6a2e3c227f9d2470ed5973bb9008
Chris Van Pelt <vanpelt@gmail.com>
2021-05-04 14:42:33 -0700

    Fix test

--- tests/integrations/test_torch.py
+++ tests/integrations/test_torch.py
@@ -183,7 +183,7 @@ def test_all_logging_no_full_hooks(wandb_init_run):
         wandb.log({"a": 2})
         assert len(wandb.run._backend.history[0]) == 20
         print("HIST KEYS", wandb.run._backend.history[0].keys())
-        assert len(wandb.run._backend.history[0]['parameters/graph_1conv1.weight']["bins"]) == 65
+        assert len(wandb.run._backend.history[0]["parameters/fc2.bias"]["bins"]) == 65
         assert len(wandb.run._backend.history[0]["gradients/fc2.bias"]["bins"]) == 65
     assert len(wandb.run._backend.history) == 3
 
