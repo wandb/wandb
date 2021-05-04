af9e9575124d80f62e9544bb91c649df44c07a32
Chris Van Pelt <vanpelt@gmail.com>
2021-05-04 14:11:24 -0700

    Print in test for CI errors that I cant reproduce

--- tests/integrations/test_torch.py
+++ tests/integrations/test_torch.py
@@ -182,6 +182,7 @@ def test_all_logging_no_full_hooks(wandb_init_run):
         output.backward(grads)
         wandb.log({"a": 2})
         assert len(wandb.run._backend.history[0]) == 20
+        print("WTF", wandb.run._backend.history[0])
         assert len(wandb.run._backend.history[0]["parameters/fc2.bias"]["bins"]) == 65
         assert len(wandb.run._backend.history[0]["gradients/fc2.bias"]["bins"]) == 65
     assert len(wandb.run._backend.history) == 3
