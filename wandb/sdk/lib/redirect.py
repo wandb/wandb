de5ea8c57b812f2137d86e155f0b597873e422e6
Fariz Rahman <farizrahman4u@gmail.com>
2021-05-08 00:42:54 +0530

    mac fix

--- wandb/sdk/lib/redirect.py
+++ wandb/sdk/lib/redirect.py
@@ -756,17 +756,18 @@ class Redirect(RedirectBase):
                 brk = False
                 data = os.read(self._pipe_read_fd, 4096)
                 if self._stopped.is_set():
-                    if _LAST_WRITE_TOKEN not in data:
+                    t = _LAST_WRITE_TOKEN[:-1]
+                    if t not in data:
                         wandb.termlog("_LAST_WRITE_TOKEN not found!")
                         wandb.termlog(data.decode())
                         # _LAST_WRITE_TOKEN could have gotten split up at the 4096 border
-                        n = len(_LAST_WRITE_TOKEN)
-                        while n and data[-n:] != _LAST_WRITE_TOKEN[:n]:
+                        n = len(t)
+                        while n and data[-n:] != t[:n]:
                             n -= 1
                         if n:
-                            data += os.read(self._pipe_read_fd, len(_LAST_WRITE_TOKEN) - n)
-                    if _LAST_WRITE_TOKEN in data:
-                        data = data.replace(_LAST_WRITE_TOKEN, b"")
+                            data += os.read(self._pipe_read_fd, len(t) - n)
+                    if t in data:
+                        data = data.replace(t, b"")
                         brk = True
                 i = self._orig_src.write(data)
                 if (
