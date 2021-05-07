1462e5b01550a4ae20f82bc936156fdde366243a
Fariz Rahman <farizrahman4u@gmail.com>
2021-05-08 00:23:44 +0530

    mac fix

--- wandb/sdk_py27/lib/redirect.py
+++ wandb/sdk_py27/lib/redirect.py
@@ -757,14 +757,14 @@ class Redirect(RedirectBase):
                 data = os.read(self._pipe_read_fd, 4096)
                 if self._stopped.is_set():
                     if _LAST_WRITE_TOKEN not in data:
-                        # _LAST_WRITE_TOKEN could have gotten split up up at the 4096 border
+                        # _LAST_WRITE_TOKEN could have gotten split up at the 4096 border
                         n = len(_LAST_WRITE_TOKEN)
                         while n and data[-n:] != _LAST_WRITE_TOKEN[:n]:
                             n -= 1
                         if n:
                             data += os.read(self._pipe_read_fd, len(_LAST_WRITE_TOKEN) - n)
                     if _LAST_WRITE_TOKEN in data:
-                        data = data.replace(_LAST_WRITE_TOKEN, b"", 1)
+                        data = data.replace(_LAST_WRITE_TOKEN, b"")
                         brk = True
                 i = self._orig_src.write(data)
                 if (
