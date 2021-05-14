ed7ba01b7f2bd9ac78301b81747f2b0ac5564bc7
Danny Goldstein <danny@goldstein.cloud>
2021-05-14 11:36:14 -0700

    refactor unenumerated dependency on numpy (#2188)
    * add dependency on numpy
    
    * remove dependency on numpy
    
    * remove import SweepConfig that triggers numpy error and use get_module to dynamically import numpy
    
    * formatting
    
    * implement suggestion from @raubitsj
    
    * refactor dynamic imports
    
    * change message a bit
    
    * remove fstring

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ aws_requirements = ['boto3']
 grpc_requirements = ['grpcio==1.27.2']
 kubeflow_requirements = ['kubernetes', 'minio', 'google-cloud-storage', 'sh']
 media_requirements = ['numpy', 'moviepy', 'pillow', 'bokeh', 'soundfile', 'plotly']
+sweeps_requirements = ['numpy']
 
 setup(
     name='wandb',
@@ -77,7 +78,8 @@ setup(
         'gcp': gcp_requirements,
         'aws': aws_requirements,
         'grpc': grpc_requirements,
-        'media': media_requirements
+        'media': media_requirements,
+        'sweeps': sweeps_requirements
     }
 )
 
