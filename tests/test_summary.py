2e22ca062c2edc56c72945933a08cab2894b74c7
Fariz Rahman <farizrahman4u@gmail.com>
2021-05-23 23:59:44 +0530

    format

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -23,6 +23,7 @@ def test_summary_setitem(api, mock_server):
 
 def test_summary_media_setitem(api, mock_server):
     import numpy as np
+
     run = api.runs("test/test")[0]
     with pytest.raises(Exception) as excinfo:
         run.summary["img"] = wandb.Image(np.random.random((100, 100)))
@@ -34,6 +35,8 @@ def test_summary_media_setitem(api, mock_server):
 def test_summary_media_setitem_current_run(
     api, live_mock_server, test_settings, wandb_init_run
 ):
+    import numpy as np
+
     run = api.runs("test/test")[0]
     run.summary["img"] = wandb.Image(np.random.random((100, 100)))
     run.summary.update()
