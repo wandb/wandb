2b12a58388766a0444ab6d22ac83cfbed70809d1
Chris Van Pelt <vanpelt@gmail.com>
2021-05-27 18:47:56 -0700

    Blow up cache, refactor awful patch method

--- wandb/integration/mlflow/__init__.py
+++ wandb/integration/mlflow/__init__.py
@@ -11,6 +11,96 @@ RUNS = {}
 LOG_FLUSH_MINIMUM = 5
 
 
+def _get_or_start_wandb_run(run, run_id=None, name=None):
+    try:
+        from mlflow.tracking.client import MlflowClient
+        import mlflow
+
+        mlflow_client = MlflowClient()
+        #  os.environ[env.SILENT] = "1"
+        os.environ[env.SYNC_MLFLOW] = os.getenv(env.SYNC_MLFLOW, "all")
+        if run_id:
+            os.environ[env.RESUME] = "allow"  # TODO: must?
+        if run.data.tags.get("mlflow.parentRunId"):
+            parent = RUNS.get(run.data.tags["mlflow.parentRunId"], {"run": None})["run"]
+            if parent and parent.group is None:
+                parent.group = run.data.tags["mlflow.parentRunId"]
+                parent.job_type = "parent"
+                parent.save()
+            # TODO: maybe call save
+            os.environ[env.RUN_GROUP] = run.data.tags["mlflow.parentRunId"]
+            os.environ[env.JOB_TYPE] = "child"
+
+        project = os.getenv(
+            env.PROJECT, mlflow_client.get_experiment(run.info.experiment_id).name
+        )
+        config = run.data.tags
+        config["mlflow.tracking_uri"] = mlflow.get_tracking_uri()
+        config["mlflow.experiment_id"] = run.info.experiment_id
+        wandb_run = RUNS.get(run.info.run_id)
+        if wandb_run is None:
+            wandb_run = wandb.init(
+                id=run.info.run_id,
+                project=project,
+                name=name,
+                config=config,
+                reinit=True,
+            )
+
+        wandb.termlog(
+            "Syncing MLFlow metrics, params, and artifacts to: %s"
+            % wandb_run.get_url().split("/runs/")[0],
+            repeat=False,
+        )
+        # TODO: move to telemetry
+        wandb_run.config["_wandb"]["mlflow_version"] = mlflow.__version__
+        RUNS[wandb_run.id] = {"step": 0, "last_log": time.time(), "run": wandb_run}
+        return wandb_run
+    except Exception:
+        wandb.termerror(
+            "Failed to initialize wandb, disable by setting WANDB_SYNC_MLFLOW=false"
+        )
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+        print("\n".join(lines))
+
+
+def _get_run(run_id, only_run=True):
+    from mlflow.tracking.client import MlflowClient
+
+    run = RUNS.get(run_id, {}).get("run") if only_run else RUNS.get(run_id, {})
+    if not run:
+        wandb.termwarn("No run found for %s - cur: %s" % (run_id, wandb.run))
+        run = MlflowClient().get_run(run_id)
+        return _get_or_start_wandb_run(run)
+    else:
+        return run
+
+
+def _fix_step(run_id, metrics, step, timestamp):
+    """Handle different steps by namespacing a new step counter with the first key
+        if global step decreases.  Also auto increase step if we're it's not increasing
+        every LOG_FLUSH_MINIMUM seconds TODO: make this actually work"""
+    key = list(metrics)[0]
+    run_log = _get_run(run_id, False)
+    if step in (None, 0) and int(time.time() - run_log["last_log"]) > LOG_FLUSH_MINIMUM:
+        wandb.termwarn(
+            "Metric logged without a step, pass a step to log_metric.", repeat=False
+        )
+        step = run_log["step"] + 1
+
+    # Run with multiple steps, keeping a separate step count
+    if step < run_log["step"]:
+        metrics[key + "/step"] = step
+        step = run_log["step"]
+
+    if step != run_log["step"]:
+        run_log["step"] = step
+        run_log["last_log"] = time.time()
+
+    return metrics, step
+
+
 def patch(settings=None, sync="all"):
     mlflow = wandb.util.get_module("mlflow")
     # TODO: really hacky, is happening from internal_cli.py for some reason
@@ -18,6 +108,7 @@ def patch(settings=None, sync="all"):
     #    return False
     if not env.sync_mlflow() or mlflow is None or len(wandb.patched["mlflow"]) > 0:
         return False
+
     from mlflow.tracking.client import MlflowClient
 
     # was seeing infinite recursion in pytest
@@ -25,8 +116,6 @@ def patch(settings=None, sync="all"):
         return False
 
     from mlflow.tracking import fluent
-
-    client = MlflowClient()
 
     orig_start_run = fluent.start_run
     orig_end_run = fluent.end_run
@@ -36,57 +125,6 @@ def patch(settings=None, sync="all"):
     MlflowClient.orig_log_batch = MlflowClient.log_batch
     MlflowClient.orig_log_artifact = MlflowClient.log_artifact
     MlflowClient.orig_log_artifacts = MlflowClient.log_artifacts
-
-    def _get_or_start_wandb_run(run, run_id=None, name=None):
-        try:
-            #  os.environ[env.SILENT] = "1"
-            os.environ[env.SYNC_MLFLOW] = os.getenv(env.SYNC_MLFLOW, "all")
-            if run_id:
-                os.environ[env.RESUME] = "allow"  # TODO: must?
-            if run.data.tags.get("mlflow.parentRunId"):
-                parent = RUNS.get(run.data.tags["mlflow.parentRunId"], {"run": None})[
-                    "run"
-                ]
-                if parent and parent.group is None:
-                    parent.group = run.data.tags["mlflow.parentRunId"]
-                    parent.job_type = "parent"
-                    parent.save()
-                # TODO: maybe call save
-                os.environ[env.RUN_GROUP] = run.data.tags["mlflow.parentRunId"]
-                os.environ[env.JOB_TYPE] = "child"
-
-            project = os.getenv(
-                env.PROJECT, client.get_experiment(run.info.experiment_id).name
-            )
-            config = run.data.tags
-            config["mlflow.tracking_uri"] = mlflow.get_tracking_uri()
-            config["mlflow.experiment_id"] = run.info.experiment_id
-            wandb_run = RUNS.get(run.info.run_id)
-            if wandb_run is None:
-                wandb_run = wandb.init(
-                    id=run.info.run_id,
-                    project=project,
-                    name=name,
-                    config=config,
-                    reinit=True,
-                )
-
-            wandb.termlog(
-                "Syncing MLFlow metrics, params, and artifacts to: %s"
-                % wandb_run.get_url().split("/runs/")[0],
-                repeat=False,
-            )
-            # TODO: move to telemetry
-            wandb_run.config["_wandb"]["mlflow_version"] = mlflow.__version__
-            RUNS[wandb_run.id] = {"step": 0, "last_log": time.time(), "run": wandb_run}
-            return wandb_run
-        except Exception:
-            wandb.termerror(
-                "Failed to initialize wandb, disable by setting WANDB_SYNC_MLFLOW=false"
-            )
-            exc_type, exc_value, exc_traceback = sys.exc_info()
-            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
-            print("\n".join(lines))
 
     def start_run(**kwargs):
         run = orig_start_run(**kwargs)
@@ -106,15 +144,6 @@ def patch(settings=None, sync="all"):
     fluent.end_run = end_run
     mlflow.end_run = end_run
 
-    def _get_run(run_id, only_run=True):
-        run = RUNS.get(run_id, {}).get("run") if only_run else RUNS.get(run_id, {})
-        if not run:
-            wandb.termwarn("No run found for %s - cur: %s" % (run_id, wandb.run))
-            run = MlflowClient().get_run(run_id)
-            return _get_or_start_wandb_run(run)
-        else:
-            return run
-
     def log_param(self, run_id, key, value):
         MlflowClient.orig_log_param(self, run_id, key, value)
         _get_run(run_id).config[key] = value
@@ -126,32 +155,6 @@ def patch(settings=None, sync="all"):
         _get_run(run_id).config[key] = value
 
     MlflowClient.set_tag = set_tag
-
-    def _fix_step(run_id, metrics, step, timestamp):
-        """Handle different steps by namespacing a new step counter with the first key
-            if global step decreases.  Also auto increase step if we're it's not increasing
-            every LOG_FLUSH_MINIMUM seconds TODO: make this actually work"""
-        key = list(metrics)[0]
-        run_log = _get_run(run_id, False)
-        if (
-            step in (None, 0)
-            and int(time.time() - run_log["last_log"]) > LOG_FLUSH_MINIMUM
-        ):
-            wandb.termwarn(
-                "Metric logged without a step, pass a step to log_metric.", repeat=False
-            )
-            step = run_log["step"] + 1
-
-        # Run with multiple steps, keeping a separate step count
-        if step < run_log["step"]:
-            metrics[key + "/step"] = step
-            step = run_log["step"]
-
-        if step != run_log["step"]:
-            run_log["step"] = step
-            run_log["last_log"] = time.time()
-
-        return metrics, step
 
     def log_metric(self, run_id, key, value, timestamp=None, step=None):
         MlflowClient.orig_log_metric(self, run_id, key, value, timestamp, step)
