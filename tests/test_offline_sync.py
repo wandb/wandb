dab17f7374cdad4f3a87610a99921b532695a47b
Chris Van Pelt <vanpelt@gmail.com>
2021-05-29 17:20:23 -0700

    Use test dir, print dir contents

--- tests/test_offline_sync.py
+++ tests/test_offline_sync.py
@@ -5,7 +5,7 @@ import glob
 from .utils import fixture_open
 
 
-def test_sync_in_progress(live_mock_server):
+def test_sync_in_progress(live_mock_server, test_dir):
     with open("train.py", "w") as f:
         f.write(fixture_open("train.py").read())
     env = dict(os.environ)
@@ -19,21 +19,18 @@ def test_sync_in_progress(live_mock_server):
     while not os.path.exists(latest_run) and attempts < 20:
         time.sleep(0.1)
         attempts += 1
+    print("wandb dir contents: ", glob.glob("wandb"))
     sync_file = ".wandb"
     for i in range(3):
         # Generally, the first sync will fail because the .wandb file is empty
-        sync = subprocess.Popen(
-            ["wandb", "sync", latest_run], env=os.environ
-        )
+        sync = subprocess.Popen(["wandb", "sync", latest_run], env=os.environ)
         assert sync.wait() == 0
         matches = glob.glob(os.path.join(latest_run, "*.wandb"))
         if len(matches) > 0:
             sync_file = matches[0]
         assert not os.path.exists(os.path.join(sync_file + ".synced"))
     assert offline_run.wait() == 0
-    sync = subprocess.Popen(
-        ["wandb", "sync", latest_run], env=os.environ
-    )
+    sync = subprocess.Popen(["wandb", "sync", latest_run], env=os.environ)
     assert sync.wait() == 0
     assert os.path.exists(os.path.join(sync_file + ".synced"))
     print("Number of upserts: ", live_mock_server.get_ctx()["upsert_bucket_count"])
