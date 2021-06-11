8640dc317b25c75e32546cd076161c4ab4dc27cb
Kyle Goyette <kdgoyette@gmail.com>
2021-06-11 10:38:41 -0700

    Revert "Revert "[WB-5640] Fix Multiprocessing Spawn PDB Regression  (#2254)""
    This reverts commit b5e8c90dbecfcaa35ac74b1b160cd9dcef3df5ff.

--- wandb/sdk/backend/backend.py
+++ wandb/sdk/backend/backend.py
@@ -107,6 +107,7 @@ class Backend(object):
         # Support running code without a: __name__ == "__main__"
         save_mod_name = None
         save_mod_path = None
+        save_mod_spec = None
         main_module = sys.modules["__main__"]
         main_mod_spec = getattr(main_module, "__spec__", None)
         main_mod_path = getattr(main_module, "__file__", None)
@@ -120,7 +121,9 @@ class Backend(object):
                 else None
             )
             main_module.__spec__ = main_mod_spec
-        main_mod_name = getattr(main_mod_spec, "name", None)
+        else:
+            save_mod_spec = main_mod_spec
+
         if main_mod_name is not None:
             save_mod_name = main_mod_name
             main_module.__spec__.name = "wandb.mpmain"
@@ -142,6 +145,7 @@ class Backend(object):
         )
 
         # Undo temporary changes from: __name__ == "__main__"
+        main_module.__spec__ = save_mod_spec
         if save_mod_name:
             main_module.__spec__.name = save_mod_name
         elif save_mod_path:
