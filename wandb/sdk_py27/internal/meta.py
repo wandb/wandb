adcabfb84cb4f29457f33fa80d78367a6f8f0402
stephchen <stephchen@wandb.com>
2021-06-14 14:09:16 -0700

    lint

--- wandb/sdk_py27/internal/meta.py
+++ wandb/sdk_py27/internal/meta.py
@@ -214,8 +214,6 @@ class Meta(object):
         if not self._settings.disable_code:
             if self._settings.program_relpath is not None:
                 self.data["codePath"] = self._settings.program_relpath
-            else:
-                self.data["program"] = "<python with no main file>"
                 if self._settings._jupyter:
                     if self._settings.notebook_name:
                         self.data["program"] = self._settings.notebook_name
