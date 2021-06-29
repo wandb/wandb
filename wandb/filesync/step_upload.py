6dbb58a4ed819aba169469b86639859e9e433969
Min-Young Wu <mwu368@gmail.com>
2021-06-29 11:39:55 -0700

    defer state transition

--- wandb/filesync/step_upload.py
+++ wandb/filesync/step_upload.py
@@ -15,7 +15,7 @@ RequestUpload = collections.namedtuple(
 RequestCommitArtifact = collections.namedtuple(
     "RequestCommitArtifact", ("artifact_id", "finalize", "before_commit", "on_commit")
 )
-RequestFinish = collections.namedtuple("RequestFinish", ())
+RequestFinish = collections.namedtuple("RequestFinish", ("callback"))
 
 
 class StepUpload(object):
@@ -41,9 +41,11 @@ class StepUpload(object):
     def _thread_body(self):
         # Wait for event in the queue, and process one by one until a
         # finish event is received
+        finish_callback = None
         while True:
             event = self._event_queue.get()
             if isinstance(event, RequestFinish):
+                finish_callback = event.callback
                 break
             self._handle_event(event)
 
@@ -63,6 +65,8 @@ class StepUpload(object):
                 self._handle_event(event)
             elif not self._running_jobs:
                 # Queue was empty and no jobs left.
+                if finish_callback:
+                    finish_callback()
                 break
 
     def _handle_event(self, event):
