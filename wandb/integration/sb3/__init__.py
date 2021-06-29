608f08cace6e9438368595d89a595d8f83d991b7
Costa Huang <costa.huang@outlook.com>
2021-06-29 09:54:01 -0400

    update comments

--- wandb/integration/sb3/__init__.py
+++ wandb/integration/sb3/__init__.py
@@ -24,7 +24,13 @@ config = {
 env = DummyVecEnv([make_env])
 model = PPO(config['policy_type'], env, verbose=1, tensorboard_log=f"runs/ppo")
 model.learn(total_timesteps=config['total_timesteps'],
-    callback=WandbCallback(project="sb3", config=config))
+    callback=WandbCallback(
+        project="sb3",
+        config=config,
+        gradient_save_freq=100,
+        model_save_freq=1000,
+        model_save_path="./models",
+))
 ```
 """
 
@@ -45,8 +51,9 @@ class WandbCallback(BaseCallback):
         config - The dictionary configuration
         name - The name of this experiment
         verbose - The verbosity of sb3 output
-        save_freq - Frequency to save the model
-        save_path - Path to the folder where the model will be saved
+        model_save_freq - Frequency to save the model
+        model_save_path - Path to the folder where the model will be saved
+        gradient_save_freq - Frequency to log gradient. If set 0, then gradients are not logged
     """
 
     def __init__(
@@ -55,17 +62,17 @@ class WandbCallback(BaseCallback):
         config,
         name=None,
         verbose=0,
-        save_freq=1000,
-        save_path="./models",
-        log_gradient_freq=100,
+        model_save_freq=1000,
+        model_save_path=None,
+        gradient_save_freq=0,
     ):
         super(WandbCallback, self).__init__(verbose)
         self.project = project
         self.config = config
         self.name = name
-        self.save_freq = save_freq
-        self.save_path = save_path
-        self.log_gradient_freq = log_gradient_freq
+        self.model_save_freq = model_save_freq
+        self.model_save_path = model_save_path
+        self.gradient_save_freq = gradient_save_freq
         wandb.init(
             project=self.project,
             config=self.config,
@@ -76,9 +83,9 @@ class WandbCallback(BaseCallback):
             allow_val_change=True,
         )
         # Create folder if needed
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
-            self.path = os.path.join(self.save_path, "model.zip")
+        if self.model_save_path is not None:
+            os.makedirs(self.model_save_path, exist_ok=True)
+            self.path = os.path.join(self.model_save_path, "model.zip")
 
     def _init_callback(self) -> None:
         d = {}
@@ -89,13 +96,13 @@ class WandbCallback(BaseCallback):
                 d[key] = self.model.__dict__[key]
             else:
                 d[key] = str(self.model.__dict__[key])
-        if self.log_gradient_freq:
+        if self.gradient_save_freq:
             wandb.watch(self.model.policy, log_freq=100, log="all")
         wandb.config.update(d)
 
     def _on_step(self) -> bool:
-        if self.save_path is not None:
-            if self.n_calls % self.save_freq == 0:
+        if self.model_save_path is not None:
+            if self.n_calls % self.model_save_freq == 0:
                 self.model.save(self.path)
                 wandb.save(self.path)
                 if self.verbose > 1:
