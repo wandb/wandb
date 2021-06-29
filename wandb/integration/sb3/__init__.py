2c47539590c95f17abaffc3604b03c521bd69fad
Costa Huang <costa.huang@outlook.com>
2021-06-29 10:39:56 -0400

    refactor

--- wandb/integration/sb3/__init__.py
+++ wandb/integration/sb3/__init__.py
@@ -9,6 +9,7 @@ import gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 from wandb.integration.sb3 import WandbCallback
+import wandb
 
 def make_env():
     env = gym.make("CartPole-v1")
@@ -21,12 +22,20 @@ config = {
     "total_timesteps": 25000
 }
 
+wandb.init(
+    name="my_ppo_experiment",
+    project="sb3",
+    config=config,
+    allow_val_change=True, # auto-log the model's hyperparams
+    sync_tensorboard=True, # auto-upload metrics
+    monitor_gym=True,  # auto-upload the videos of agents playing the game
+    save_code=True,  
+)
+
 env = DummyVecEnv([make_env])
 model = PPO(config['policy_type'], env, verbose=1, tensorboard_log=f"runs/ppo")
 model.learn(total_timesteps=config['total_timesteps'],
     callback=WandbCallback(
-        project="sb3",
-        config=config,
         gradient_save_freq=100,
         model_save_freq=1000,
         model_save_path="./models",
@@ -47,9 +56,6 @@ class WandbCallback(BaseCallback):
         - Added complete hyperparameters recording
         - Added gradient logging
     Arguments:
-        project - The project name under which this experiment lives
-        config - The dictionary configuration
-        name - The name of this experiment
         verbose - The verbosity of sb3 output
         model_save_freq - Frequency to save the model
         model_save_path - Path to the folder where the model will be saved
@@ -58,30 +64,16 @@ class WandbCallback(BaseCallback):
 
     def __init__(
         self,
-        project,
-        config,
-        name=None,
         verbose=0,
         model_save_freq=1000,
         model_save_path=None,
         gradient_save_freq=0,
     ):
         super(WandbCallback, self).__init__(verbose)
-        self.project = project
-        self.config = config
-        self.name = name
+        assert self._wandb.run is not None, "no wandb run detected; use `wandb.init()` to initialize a run"
         self.model_save_freq = model_save_freq
         self.model_save_path = model_save_path
         self.gradient_save_freq = gradient_save_freq
-        wandb.init(
-            project=self.project,
-            config=self.config,
-            sync_tensorboard=True,
-            name=self.name,
-            monitor_gym=True,
-            save_code=True,
-            allow_val_change=True,
-        )
         # Create folder if needed
         if self.model_save_path is not None:
             os.makedirs(self.model_save_path, exist_ok=True)
@@ -96,8 +88,8 @@ class WandbCallback(BaseCallback):
                 d[key] = self.model.__dict__[key]
             else:
                 d[key] = str(self.model.__dict__[key])
-        if self.gradient_save_freq:
-            wandb.watch(self.model.policy, log_freq=100, log="all")
+        if self.gradient_save_freq > 0:
+            wandb.watch(self.model.policy, log_freq=self.gradient_save_freq, log="all")
         wandb.config.update(d)
 
     def _on_step(self) -> bool:
