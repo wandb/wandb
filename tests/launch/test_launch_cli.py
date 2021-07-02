bd150e69511905fe54f3e5d20d3baf334db2376b
Kyle Goyette <kdgoyette@gmail.com>
2021-07-01 23:53:25 -0700

    update cli tests

--- tests/launch/test_launch_cli.py
+++ tests/launch/test_launch_cli.py
@@ -1,10 +1,12 @@
+import json
+
 import wandb
 from wandb.cli import cli
 from wandb.apis.internal import InternalApi
 import pytest
 from tests import utils
 
-from .test_launch import mocked_fetchable_git_repo
+from .test_launch import mocked_fetchable_git_repo, mock_load_backend
 
 
 def test_launch(runner, test_settings, live_mock_server, mocked_fetchable_git_repo):
@@ -41,11 +43,21 @@ def test_launch_add_config_file(runner, test_settings, live_mock_server):
 def test_launch_agent_base(
     runner, test_settings, live_mock_server, mocked_fetchable_git_repo, monkeypatch
 ):
+    def patched_pop_from_queue(self, queue):
+        try:
+            ups = self._api.pop_from_run_queue(
+                queue, entity=self._entity, project=self._project
+            )
+            if ups is None:
+                raise KeyboardInterrupt
+        except Exception:
+            return None
+        return ups
 
     with runner.isolated_filesystem():
         monkeypatch.setattr(
             "wandb.sdk.launch.agent.LaunchAgent.pop_from_queue",
-            lambda c, queue: try_this_out(c, queue),
+            lambda c, queue: patched_pop_from_queue(c, queue),
         )
         result = runner.invoke(cli.launch_agent, "test_project")
         assert result.exit_code == 0
@@ -55,27 +67,80 @@ def test_launch_agent_base(
         assert "Shutting down, active jobs" in result.output
 
 
+def test_launch_cli_with_config_file_and_params(
+    runner, mocked_fetchable_git_repo, live_mock_server
+):
+    config = {
+        "uri": "https://wandb.ai/mock_server_entity/test_project/runs/1",
+        "project": "test_project",
+        "entity": "mock_server_entity",
+        "resource": "local",
+        "overrides": {"args": ["--epochs", "5"]},
+    }
+    with runner.isolated_filesystem():
+        with open("config.json", "w") as fp:
+            json.dump(
+                config,
+                fp,
+            )
+
+        result = runner.invoke(
+            cli.launch,
+            [
+                "-c",
+                "config.json",
+                "-P",
+                "epochs=1",
+                "https://wandb.ai/mock_server_entity/test_project/runs/1",
+            ],
+        )
+        print(result.output)
+        assert result.exit_code == 0
+        assert (
+            "docker run --rm test_project_1_launch:latest python train.py --epochs 1"
+            in result.output
+        )
+
+
+def test_launch_cli_with_config_and_params(
+    runner, mocked_fetchable_git_repo, live_mock_server
+):
+    config = {
+        "uri": "https://wandb.ai/mock_server_entity/test_project/runs/1",
+        "project": "test_project",
+        "entity": "mock_server_entity",
+        "resource": "local",
+        "overrides": {"args": ["--epochs", "5"]},
+    }
+    with runner.isolated_filesystem():
+        result = runner.invoke(
+            cli.launch,
+            [
+                "-c",
+                json.dumps(config),
+                "-P",
+                "epochs=1",
+                "https://wandb.ai/mock_server_entity/test_project/runs/1",
+            ],
+        )
+        print(result.output)
+        assert result.exit_code == 0
+        assert (
+            "docker run --rm test_project_1_launch:latest python train.py --epochs 1"
+            in result.output
+        )
+
+
 def test_launch_no_docker_exec(
-    runner, monkeypatch, mocked_fetchable_git_repo, test_settings,
+    runner,
+    monkeypatch,
+    mocked_fetchable_git_repo,
+    test_settings,
 ):
     monkeypatch.setattr(wandb.sdk.launch.docker, "find_executable", lambda name: False)
     result = runner.invoke(
-        cli.launch, ["https://wandb.ai/mock_server_entity/test_project/runs/1"],
+        cli.launch,
+        ["https://wandb.ai/mock_server_entity/test_project/runs/1"],
     )
     assert result.exit_code == 1
     assert "Could not find Docker executable" in str(result.exception)
-
-
-def try_this_out(self, queue):
-    try:
-        ups = self._api.pop_from_run_queue(
-            queue, entity=self._entity, project=self._project
-        )
-        wandb.termlog("UPS {}".format(ups))
-        if ups is None:
-            wandb.termlog("UPS IS NONE {}".format(ups))
-            raise KeyboardInterrupt
-    except Exception as e:
-        print("Exception:", e)
-        return None
-    return ups
