bf929c4576d0362c07b8ae7764f9a2996381aecd
Jeff Raubitschek <jeff@wandb.com>
2021-07-03 12:52:42 -0700

    update

--- tools/circleci-tool.py
+++ tools/circleci-tool.py
@@ -4,6 +4,7 @@
 import argparse
 import os
 import subprocess
+import sys
 import time
 
 import requests
@@ -21,6 +22,9 @@ parser.add_argument("--dryrun", action="store_true", help="Dont do anything")
 parser.add_argument("--wait", action="store_true", help="Wait for finish or error")
 parser.add_argument("--loop", type=int, help="Outer loop (implies wait)")
 parser.add_argument("--wait-workflow", help="Wait for workflow")
+parser.add_argument("--trigger", action='store_true', help="Trigger workflow")
+parser.add_argument("--download", action='store_true', help="Download coverage")
+parser.add_argument("--status", action='store_true', help="Look for pipeline")
 args = parser.parse_args()
 
 platforms_dict = dict(linux="test", mac="mac", win="win")
@@ -36,19 +40,22 @@ if not branch:
     assert code == 0, "failed git command"
 
 
-def poll(num):
+def poll(pipeline_id=None, workflow_ids=None):
     print("Waiting for pipeline to complete...")
-    url = "https://circleci.com/api/v2/pipeline/{}/workflow".format(num)
     while True:
-        r = requests.get(url, auth=(api_token, ""))
-        assert r.status_code == 200, "Error making api request: {}".format(r)
-        d = r.json()
+        num = 0
         done = 0
-        num = len(d["items"])
-        for item in d["items"]:
-            work_id = item["id"]
+        if pipeline_id:
+            url = "https://circleci.com/api/v2/pipeline/{}/workflow".format(pipeline_id)
+            r = requests.get(url, auth=(api_token, ""))
+            assert r.status_code == 200, "Error making api request: {}".format(r)
+            d = r.json()
+            workflow_ids = [item["id"] for item in d["items"]]
+        num = len(workflow_ids)
+        for work_id in workflow_ids:
             work_status_url = "https://circleci.com/api/v2/workflow/{}".format(work_id)
             r = requests.get(work_status_url, auth=(api_token, ""))
+            # print("STATUS", work_status_url)
             assert r.status_code == 200, "Error making api work request: {}".format(r)
             w = r.json()
             status = w["status"]
@@ -106,15 +113,100 @@ def req():
         poll(uuid)
 
 
+def get_ci_builds(bname="coverage/store-artifactsw", completed=True):
+    # TODO: extend pagination if not done
+    url = "https://circleci.com/api/v1.1/project/gh/wandb/client?shallow=true&limit=100"
+    if completed:
+        url = url + "&filter=completed"
+    # print("SEND", url)
+    r = requests.get(url, auth=(api_token, ""))
+    assert r.status_code == 200, "Error making api request: {}".format(r)
+    lst = r.json()
+    cfirst = None
+    ret = []
+    done = False
+    for d in lst:
+        b = d.get("branch")
+        if b != bname:
+            continue
+        v = d.get("vcs_revision")
+        n = d.get("build_num")
+        j = d.get("workflows", {}).get("job_name")
+        w = d.get("workflows", {}).get("workflow_id")
+        # print("DDD", d)
+        cfirst = cfirst or v
+        if cfirst != v:
+            done = True
+            break
+        ret.append((v, n, j, w))
+    if not done:
+        return
+    return ret
+
+
+def grab(vhash, bnum):
+    # curl -H "Circle-Token: $CIRCLECI_TOKEN" https://circleci.com/api/v1.1/project/github/wandb/client/61238/artifacts
+    # curl -L  -o out.dat -H "Circle-Token: $CIRCLECI_TOKEN" https://61238-86031674-gh.circle-artifacts.com/0/cover-results/.coverage
+    cachedir = ".circle_cache"
+    cfbase = "cover-{}-{}.xml".format(vhash, bnum)
+    cfname = os.path.join(cachedir, cfbase)
+    if not os.path.exists(cachedir):
+        os.mkdir(cachedir)
+    if os.path.exists(cfname):
+        return
+    url = "https://circleci.com/api/v1.1/project/github/wandb/client/{}/artifacts".format(bnum)
+    r = requests.get(url, auth=(api_token, ""))
+    assert r.status_code == 200, "Error making api request: {}".format(r)
+    lst = r.json()
+    if not lst:
+        return
+    for item in lst:
+        p = item.get("path")
+        u = item.get("url")
+        # print("got", p)
+        if p != "cover-results/coverage.xml":
+            continue
+        # print("GRAB", p, u)
+        # TODO: use tempfile
+        s, o = subprocess.getstatusoutput('curl -L  -o out.dat -H "Circle-Token: {}" "{}"'.format(api_token, u))
+        assert s == 0
+        os.rename("out.dat", cfname)
+
+
+def status():
+    # TODO: check for current git hash only
+    got = get_ci_builds(bname=branch, completed=False)
+    if not got:
+        print("ERROR: couldnt find job, maybe we should poll?")
+        sys.exit(1)
+    work_ids = [workid for _, _, _, workid in got]
+    poll(workflow_ids=[work_ids[0]])
+
+
+def download():
+    got = get_ci_builds()
+    assert got
+    for v, n, _, _ in got:
+        grab(v, n)
+
+
 def main():
     if args.wait_workflow:
         poll(args.wait_workflow)
         return
 
-    for i in range(args.loop or 1):
-        if args.loop:
-            print("Loop: {} of {}".format(i + 1, args.loop))
-        req()
+    if args.trigger:
+        for i in range(args.loop or 1):
+            if args.loop:
+                print("Loop: {} of {}".format(i + 1, args.loop))
+            req()
+
+    if args.status:
+        # find my workflow report status, wait on it (if specified)
+        status()
+
+    if args.download:
+        download()
 
 
 if __name__ == "__main__":
