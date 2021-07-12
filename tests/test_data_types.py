ffea944d6cb3cfdc940608ced0e42b46c21b5ca5
Tim Sweeney <tim.s.sweeney@gmail.com>
2021-07-11 22:03:02 -0700

    test fix on windows?

--- tests/test_data_types.py
+++ tests/test_data_types.py
@@ -943,15 +943,7 @@ def test_table_logging(mocked_run, live_mock_server, test_settings, api):
     assert True
 
 
-def test_table_logging(mocked_run, live_mock_server, test_settings, api):
-    live_mock_server.set_ctx({"max_cli_version": None})
-    run = wandb.init(settings=test_settings)
-    t = wandb.Table(columns=["a"], data=[[wandb.Image(np.ones(shape=(32, 32)))]],)
-    run.log({"logged_table": t})
-    run.log({"logged_table": t})
-    run.finish()
-    assert True
-
+def test_reference_table_logging(mocked_run, live_mock_server, test_settings, api):
     live_mock_server.set_ctx({"max_cli_version": "0.10.33"})
     run = wandb.init(settings=test_settings)
     t = wandb.Table(columns=["a"], data=[[wandb.Image(np.ones(shape=(32, 32)))]],)
@@ -969,7 +961,7 @@ def test_table_logging(mocked_run, live_mock_server, test_settings, api):
     assert True
 
 
-def test_table_reference_artifact(mocked_run, live_mock_server, test_settings, api):
+def test_reference_table_artifacts(mocked_run, live_mock_server, test_settings, api):
     live_mock_server.set_ctx({"max_cli_version": "0.10.34"})
     run = wandb.init(settings=test_settings)
     t = wandb.Table(columns=["a"], data=[[wandb.Image(np.ones(shape=(32, 32)))]],)
