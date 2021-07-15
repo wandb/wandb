b0c042f7dc8a13f85e3489fe6b3b914589c144e5
Boris Dayma <boris.dayma@gmail.com>
2021-07-15 15:38:52 -0500

    fix(watch): disable logging graph by default

--- wandb/sdk_py27/wandb_run.py
+++ wandb/sdk_py27/wandb_run.py
@@ -2075,7 +2075,7 @@ class Run(object):
         return m
 
     # TODO(jhr): annotate this
-    def watch(self, models, criterion=None, log="gradients", log_freq=100, idx=None, log_graph=True):  # type: ignore
+    def watch(self, models, criterion=None, log="gradients", log_freq=100, idx=None, log_graph=False):  # type: ignore
         wandb.watch(models, criterion, log, log_freq, idx, log_graph)
 
     # TODO(jhr): annotate this
