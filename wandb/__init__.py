9f7d54b91b46655b7c190d43e33b42acf6dc9d9f
Jeff Raubitschek <jeff@wandb.com>
2021-07-15 11:38:32 -0700

    Enable wandb.require() (#2392)

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -52,8 +52,7 @@ finish = wandb_sdk.finish
 join = finish
 login = wandb_sdk.login
 helper = wandb_sdk.helper
-# TODO(require): Remove underscore when we are ready to ship
-_require = wandb_sdk.require
+require = wandb_sdk.require
 Artifact = wandb_sdk.Artifact
 AlertLevel = wandb_sdk.AlertLevel
 Settings = wandb_sdk.Settings
