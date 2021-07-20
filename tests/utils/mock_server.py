9bf21b548f66fe7248455d4ced1611801dd17fb5
KyleGoyette <kdgoyette@gmail.com>
2021-07-20 13:05:49 -0700

    manually pip install chardet (#2412)
    * manually pip install chardet
    
    * update
    
    * pylaunch -> launch
    
    * update to pylaunch
    
    * format, ignore

--- tests/utils/mock_server.py
+++ tests/utils/mock_server.py
@@ -117,11 +117,7 @@ def run(ctx):
         "events": ['{"cpu": 10}', '{"cpu": 20}', '{"cpu": 30}'],
         "files": {
             # Special weights url by default, if requesting upload we set the name
-            "edges": [
-                {
-                    "node": fileNode,
-                }
-            ]
+            "edges": [{"node": fileNode,}]
         },
         "sampledHistory": [[{"loss": 0, "acc": 100}, {"loss": 1, "acc": 0}]],
         "shouldStop": False,
@@ -176,9 +172,7 @@ def artifact(
                 "alias": "v%i" % ctx["page_count"],
             }
         ],
-        "artifactSequence": {
-            "name": collection_name,
-        },
+        "artifactSequence": {"name": collection_name,},
         "currentManifest": {
             "file": {
                 "directUrl": request_url_root
@@ -535,24 +529,14 @@ def create_app(user_ctx=None):
             )
         if "mutation CreateAgent(" in body["query"]:
             return json.dumps(
-                {
-                    "data": {
-                        "createAgent": {
-                            "agent": {
-                                "id": "mock-server-agent-93xy",
-                            }
-                        }
-                    }
-                }
+                {"data": {"createAgent": {"agent": {"id": "mock-server-agent-93xy",}}}}
             )
         if "mutation Heartbeat(" in body["query"]:
             return json.dumps(
                 {
                     "data": {
                         "agentHeartbeat": {
-                            "agent": {
-                                "id": "mock-server-agent-93xy",
-                            },
+                            "agent": {"id": "mock-server-agent-93xy",},
                             "commands": json.dumps(
                                 [
                                     {
@@ -658,13 +642,7 @@ def create_app(user_ctx=None):
                 },
             }
             ctx["manifests_created"].append(manifest)
-            return {
-                "data": {
-                    "createArtifactManifest": {
-                        "artifactManifest": manifest,
-                    }
-                }
-            }
+            return {"data": {"createArtifactManifest": {"artifactManifest": manifest,}}}
         if "mutation UpdateArtifactManifest(" in body["query"]:
             manifest = {
                 "id": 1,
@@ -681,13 +659,7 @@ def create_app(user_ctx=None):
                     "uploadHeaders": "",
                 },
             }
-            return {
-                "data": {
-                    "updateArtifactManifest": {
-                        "artifactManifest": manifest,
-                    }
-                }
-            }
+            return {"data": {"updateArtifactManifest": {"artifactManifest": manifest,}}}
         if "mutation CreateArtifactFiles" in body["query"]:
             return {
                 "data": {
