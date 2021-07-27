92d8af8cb245bf323c998db63daf70c061864e68
Kevin Shen <kshen3778@gmail.com>
2021-07-27 10:36:19 -0400

    extra setup

--- wandb/integration/prodigy/prodigy.py
+++ wandb/integration/prodigy/prodigy.py
@@ -1,10 +1,9 @@
 import spacy
+import pandas as pd
 import wandb
 from wandb import util
 from wandb.plots.utils import (
     test_missing,
-    test_types,
-    encode_labels,
     deprecation_notice,
 )
 
@@ -13,5 +12,38 @@ try:
 except:
     print("Warning: `prodigy` is required but not installed.")
 
+
+def _named_entity(docs):
+    deprecation_notice()
+
+    spacy = util.get_module(
+        "spacy",
+        required="part_of_speech requires the spacy library, install with `pip install spacy`",
+    )
+    en_core_web_md = util.get_module(
+        "en_core_web_md",
+        required="part_of_speech requires the en_core_web_md library, install with `python -m spacy download en_core_web_md`",
+    )
+
+    if test_missing(docs=docs):
+        wandb.termlog("Visualizing named entity recognition.")
+        html = spacy.displacy.render(
+            docs, style="ent", page=True, minify=True, jupyter=False
+        )
+        wandb_html = wandb.Html(html)
+        return wandb_html
+
 def upload_dataset(dataset):
-    pass
+    # Check if wandb.init has been called
+    if wandb.run is None:
+        raise ValueError("You must call wandb.init() before upload_dataset()")
+
+    # Retrieve prodigy dataset
+    database = connect()
+    data = database.get_dataset(dataset)
+
+    table_df = pd.DataFrame(data)
+    columns = list(table_df.columns)
+    main_table = wandb.Table(dataframe=table_df)
+    wandb.log({"dataset": main_table})
+
