1418edea111e365972a3e735c0e63b5f532e7511
Jeff Raubitschek <jeff@wandb.com>
2021-07-26 23:58:37 -0700

    updatee

--- tests/conftest.py
+++ tests/conftest.py
@@ -816,9 +816,7 @@ def publish_util(
 
 
 @pytest.fixture
-def tbwatcher_util(
-    mocked_run, mock_server, internal_hm, backend_interface, parse_ctx
-):
+def tbwatcher_util(mocked_run, mock_server, internal_hm, backend_interface, parse_ctx):
     def fn(write_function, logdir="./", save=True, root_dir="./"):
 
         with backend_interface() as interface:
