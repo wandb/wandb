5aa82688976c5e54a1f7f4a7ddefcb8fba5c8be6
Adrian Swanberg <aswanberg@wandb.com>
2021-07-27 15:43:17 -0700

    [WB-6175] Stop logging TransientError to sentry. (#2436)

---
+++ tests/test_file_upload.py
@@ -0,0 +1,33 @@
+"""
+file upload tests.
+"""
+
+from __future__ import print_function
+
+import json
+import os
+import pytest
+
+
+def test_file_upload_good(mocked_run, publish_util, mock_server):
+    def begin_fn():
+        with open(os.path.join(mocked_run.dir, "test.txt"), "w") as f:
+            f.write("TEST TEST")
+
+    files = [dict(files_dict=dict(files=[("test.txt", "now")]))]
+    ctx_util = publish_util(files=files, begin_cb=begin_fn)
+    assert "test.txt" in ctx_util.file_names
+
+
+def test_file_upload_inject(mocked_run, publish_util, mock_server, inject_requests):
+    def begin_fn():
+        with open(os.path.join(mocked_run.dir, "test.txt"), "w") as f:
+            f.write("TEST TEST")
+
+    query_str = "file=test.txt&run={}".format(mocked_run.id)
+    match = inject_requests.Match(path_suffix="/storage", query_str=query_str, count=2)
+    inject_requests.add(match=match, http_status=500)
+
+    files = [dict(files_dict=dict(files=[("test.txt", "now")]))]
+    ctx_util = publish_util(files=files, begin_cb=begin_fn)
+    assert "test.txt" in ctx_util.file_names
