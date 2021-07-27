503368f042745f4499fa1459403a101b26e9f529
Katia <katia@wandb.com>
2021-07-27 16:21:33 -0700

    fix test

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -1,44 +1,61 @@
-# import unittest
-
-
-# from mock import *
-# from datetime import datetime, timedelta
-# import time  # so we can override time.time
-
-# mock_time = Mock()
-# mock_time.return_value = time.mktime(datetime(2011, 6, 21).timetuple())
-
-
-# class TestCrawlerChecksDates(unittest.TestCase):
-#
-#     def test_mock_datetime_now(self):
-#         self.assertEqual(datetime(2011, 6, 21), datetime.now())
-
+import random
 import time
-from unittest.mock import patch
+from wandb.proto import wandb_internal_pb2 as pb
 
-# @patch("time.sleep", return_value=None)
+
 def test_runtime(
-    mocker,
+    internal_hm,
     mocked_run,
     mock_server,
-    _internal_sender,
-    _start_backend,
-    _stop_backend,
+    backend_interface,
     parse_ctx,
 ):
-    mocker.patch("time.sleep")
-    # interface = internal_sender
-    # with mock_time as tm:
-    threads = _start_backend()
-    # interface.publish_run(mocked_run)
-    time.sleep(10)
-    # interface.publish_pause()
-    # time.sleep(10)
-    # interface.publish_resume()
-    # time.sleep(10)
-    _stop_backend(threads=threads)
+    with backend_interface() as interface:
+
+        proto_run = pb.RunRecord()
+        mocked_run._make_proto_run(proto_run)
+
+        run_start = pb.RunStartRequest()
+        run_start.run.CopyFrom(proto_run)
+
+        record = interface._make_request(run_start=run_start)
+        internal_hm.handle_request_run_start(record)
+
+        # time.sleep(x)
+        # interface.publish_pause()
+        # time.sleep(x)
+        # interface.publish_resume()
+        # time.sleep(x)
+        # interface.publish_resume()
+        # time.sleep(x)
+        total_time = fuzzy_pause_resume(interface, 3)
 
     ctx_util = parse_ctx(mock_server.ctx)
-    print(ctx_util.config["_wandb"])
-    assert ctx_util.config["_wandb"]["rt"] >= 30
+    print(ctx_util.config_wandb["rt"])
+    assert ctx_util.config_wandb["rt"] >= total_time
+
+
+def fuzzy_pause_resume(interface, N):
+
+    current_state = random.choice(["IP", "IR"])
+    clock = [random.randint(0, 1) for _ in range(N)]
+    requests = random.choices(["P", "R"], k=N)
+    counter = 0
+    for c, req in zip(clock, requests):
+        if current_state in ["IR", "IP", "RP", "RR"]:
+            if c:
+                time.sleep(3)
+                counter += 3
+        elif current_state in ["PP", "PR"]:
+            if c:
+                time.sleep(3)
+        print(current_state, c)
+        publish = interface.publish_pause if req == "P" else interface.publish_resume
+        publish()
+        current_state = current_state[1] + req
+
+    print(counter)
+    return counter
+
+
+# fuzzy_pause_resume(4)
