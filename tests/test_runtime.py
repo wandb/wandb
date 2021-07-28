7dfafe11d6c3e86f97c6bde12a6ad1d97c20aa38
Katia <katia@wandb.com>
2021-07-27 17:17:00 -0700

    Cover all interesting cases

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -1,4 +1,3 @@
-import random
 import time
 from wandb.proto import wandb_internal_pb2 as pb
 
@@ -21,38 +20,119 @@ def test_runtime(
         record = interface._make_request(run_start=run_start)
         internal_hm.handle_request_run_start(record)
 
-        # time.sleep(x)
-        # interface.publish_pause()
-        # time.sleep(x)
-        # interface.publish_resume()
-        # time.sleep(x)
-        # interface.publish_resume()
-        # time.sleep(x)
-        total_time = fuzzy_pause_resume(interface, 3)
+        time.sleep(3)
 
     ctx_util = parse_ctx(mock_server.ctx)
-    print(ctx_util.config_wandb["rt"])
-    assert ctx_util.config_wandb["rt"] >= total_time
+    assert ctx_util.config_wandb["rt"] >= 3
 
 
-def fuzzy_pause_resume(interface, N):
+def test_runtime_pause_resume(
+    internal_hm,
+    mocked_run,
+    mock_server,
+    backend_interface,
+    parse_ctx,
+):
+    with backend_interface() as interface:
 
-    current_state = random.choice(["IP", "IR"])
-    clock = [random.randint(0, 1) for _ in range(N)]
-    requests = random.choices(["P", "R"], k=N)
-    counter = 0
-    for c, req in zip(clock, requests):
-        if current_state in ["IR", "IP", "RP", "RR"]:
-            if c:
-                time.sleep(3)
-                counter += 3
-        elif current_state in ["PP", "PR"]:
-            if c:
-                time.sleep(3)
-        print(current_state, c)
-        publish = interface.publish_pause if req == "P" else interface.publish_resume
-        publish()
-        current_state = current_state[1] + req
+        proto_run = pb.RunRecord()
+        mocked_run._make_proto_run(proto_run)
 
-    print(counter)
-    return counter
+        run_start = pb.RunStartRequest()
+        run_start.run.CopyFrom(proto_run)
+
+        record = interface._make_request(run_start=run_start)
+        internal_hm.handle_request_run_start(record)
+
+        time.sleep(3)
+        interface.publish_pause()
+        time.sleep(3)
+        interface.publish_resume()
+        time.sleep(3)
+
+    ctx_util = parse_ctx(mock_server.ctx)
+    assert 9 >= ctx_util.config_wandb["rt"] >= 6
+
+
+def test_runtime_pause_pause(
+    internal_hm,
+    mocked_run,
+    mock_server,
+    backend_interface,
+    parse_ctx,
+):
+    with backend_interface() as interface:
+
+        proto_run = pb.RunRecord()
+        mocked_run._make_proto_run(proto_run)
+
+        run_start = pb.RunStartRequest()
+        run_start.run.CopyFrom(proto_run)
+
+        record = interface._make_request(run_start=run_start)
+        internal_hm.handle_request_run_start(record)
+
+        time.sleep(3)
+        interface.publish_pause()
+        time.sleep(3)
+        interface.publish_pause()
+        time.sleep(3)
+
+    ctx_util = parse_ctx(mock_server.ctx)
+    assert 9 >= ctx_util.config_wandb["rt"] >= 3
+
+
+def test_runtime_resume_resume(
+    internal_hm,
+    mocked_run,
+    mock_server,
+    backend_interface,
+    parse_ctx,
+):
+    with backend_interface() as interface:
+
+        proto_run = pb.RunRecord()
+        mocked_run._make_proto_run(proto_run)
+
+        run_start = pb.RunStartRequest()
+        run_start.run.CopyFrom(proto_run)
+
+        record = interface._make_request(run_start=run_start)
+        internal_hm.handle_request_run_start(record)
+
+        time.sleep(3)
+        interface.publish_resume()
+        time.sleep(3)
+        interface.publish_resume()
+        time.sleep(3)
+
+    ctx_util = parse_ctx(mock_server.ctx)
+    assert ctx_util.config_wandb["rt"] >= 9
+
+
+def test_runtime_resume_pause(
+    internal_hm,
+    mocked_run,
+    mock_server,
+    backend_interface,
+    parse_ctx,
+):
+    with backend_interface() as interface:
+
+        proto_run = pb.RunRecord()
+        mocked_run._make_proto_run(proto_run)
+
+        run_start = pb.RunStartRequest()
+        run_start.run.CopyFrom(proto_run)
+
+        record = interface._make_request(run_start=run_start)
+        internal_hm.handle_request_run_start(record)
+
+        time.sleep(3)
+        interface.publish_resume()
+        time.sleep(3)
+        interface.publish_pause()
+        time.sleep(3)
+
+    ctx_util = parse_ctx(mock_server.ctx)
+    assert 9 >= ctx_util.config_wandb["rt"] >= 6
