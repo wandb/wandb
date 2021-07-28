db2febbce765eade1932fde4e57ffb4dd28e0bb1
Morgan McGuire <morganmcguire@Morgans-MacBook-Pro.local>
2021-07-28 12:58:40 +0100

    fixes black linting errors

--- functional_tests/fastai-integration.py
+++ functional_tests/fastai-integration.py
@@ -11,18 +11,20 @@ assert:
   - :wandb:runs[0][exitcode]: 0
 """
 
-from fastai.vision.all import *
 from fastai.callback.wandb import *
+from fastai.vision.all import *
 import wandb
 
-wandb.init(project='integrations_testing')
+wandb.init(project="integrations_testing")
 path = untar_data(URLs.MNIST_TINY)
-mnist = DataBlock(blocks = (ImageBlock(cls=PILImageBW),CategoryBlock),
-                  get_items = get_image_files,
-                  splitter = GrandparentSplitter(),
-                  get_y = parent_label)
+mnist = DataBlock(
+    blocks=(ImageBlock(cls=PILImageBW), CategoryBlock),
+    get_items=get_image_files,
+    splitter=GrandparentSplitter(),
+    get_y=parent_label,
+)
 
-dls = mnist.dataloaders(path/"train", bs=32)
+dls = mnist.dataloaders(path / "train", bs=32)
 learn = cnn_learner(dls, resnet18, metrics=error_rate)
 learn.fit(2, 1e-2, cbs=WandbCallback(log_preds=False, log_model=False))
 wandb.finish()
