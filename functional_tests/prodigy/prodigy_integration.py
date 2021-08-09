a10ffeb66bdd168520a0fbeaa5da063e23c050a6
Kevin Shen <kshen3778@gmail.com>
2021-08-09 10:27:03 -0400

    organized prodigy functional test into folder

--- functional_tests/prodigy/prodigy_integration.py
+++ functional_tests/prodigy/prodigy_integration.py
@@ -3,17 +3,17 @@
 
 ---
 id: 0.0.1
-prep:
-  commands:
-    - prodigy_download_datasets.py
 assert:
   - :wandb:runs_len: 1
   - :wandb:runs[0][summary][ner_dataset][nrows]: 4
   - :wandb:runs[0][exitcode]: 0
 """
 
+from io import BytesIO
 import sys
 from unittest.mock import Mock
+from urllib.request import urlopen
+from zipfile import ZipFile
 
 from prodigy_connect import Connect
 from wandb.integration.prodigy import upload_dataset
@@ -24,3 +24,10 @@ sys.modules['prodigy.components.db'] = Connect
 
 # download my files
 # run tests
+
+url = "INSERT DATASET URL"
+extract_dir = "prodigy_sample_datasets"
+
+http_response = urlopen(url)
+zipfile = ZipFile(BytesIO(http_response.read()))
+zipfile.extractall(path=extract_dir)
