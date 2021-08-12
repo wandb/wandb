75d2f88c220ce75df7d338e37edc294e30d224b3
Kyle Goyette <kdgoyette@gmail.com>
2021-08-12 07:49:25 -0700

    fix

--- tests/test_file_upload.py
+++ tests/test_file_upload.py
@@ -14,7 +14,7 @@ def test_file_upload_good(mocked_run, publish_util, mock_server):
         with open(os.path.join(mocked_run.dir, "test.txt"), "w") as f:
             f.write("TEST TEST")
 
-    files = [dict(files_dict=dict(files=[("test.txt", "now")]))]
+    files = [dict(files_dict=dict(files=[("", "test.txt", False, "now")]))]
     ctx_util = publish_util(files=files, begin_cb=begin_fn)
     assert "test.txt" in ctx_util.file_names
 
@@ -28,6 +28,6 @@ def test_file_upload_inject(mocked_run, publish_util, mock_server, inject_reques
     match = inject_requests.Match(path_suffix="/storage", query_str=query_str, count=2)
     inject_requests.add(match=match, http_status=500)
 
-    files = [dict(files_dict=dict(files=[("test.txt", "now")]))]
+    files = [dict(files_dict=dict(files=[("", "test.txt", False, "now")]))]
     ctx_util = publish_util(files=files, begin_cb=begin_fn)
     assert "test.txt" in ctx_util.file_names
