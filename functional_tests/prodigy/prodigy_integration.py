5e8a5902075c9ffbd6e171e5d1974026c39be22a
Kevin Shen <kshen3778@gmail.com>
2021-08-13 14:36:40 -0400

    added pillow and scipy to requirements

--- functional_tests/prodigy/prodigy_integration.py
+++ functional_tests/prodigy/prodigy_integration.py
@@ -15,6 +15,8 @@ depend:
     - wandb
     - spacy
     - https://github.com/explosion/spacy-models/releases/download/en_core_web_md-3.0.0/en_core_web_md-3.0.0.tar.gz#egg=en_core_web_md
+    - Pillow
+    - scipy
   files:
     - file: prodigy_test_resources.zip
       source: https://raw.githubusercontent.com/wandb/wandb-testing/master/test_data/prodigy/prodigy_test_resources.zip
