dd1ad9abc3714425fcb9beac0ed0ecbb8e9255b0
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-08-17 00:14:47 +0530

    update

--- tests/test_stdin_timeout.py
+++ tests/test_stdin_timeout.py
@@ -1,29 +1,10 @@
 from wandb.sdk.lib.stdin_timeout import stdin_timeout
 from wandb.errors import InputTimeoutError
-from wandb.util import prompt_choices, _prompt_choice_with_timeout
-import io
-import pytest
 
 
-@pytest.fixture
-def stdin_tests(monkeypatch):
-    monkeypatch.setattr("sys.stdin", io.StringIO("1"))
-    choice = stdin_timeout("", None, "")
-    assert choice == "1"
-
-    choices = ["1", "2"]
-    choice = prompt_choices(choices)
-    assert choice == "1"
-
-    choice = _prompt_choice_with_timeout()
-    assert choice == 0
-
-
-""""
-@pytest.fixture
-def input_timeout_test(pytestconfig):
-    capmanager = pytestconfig.pluginmanager.getplugin('capturemanager')
-    capmanager.suspend_global_capture(_in=True)
+def test_timeout(pytestconfig):
+    capmanager = pytestconfig.pluginmanager.getplugin("capturemanager")
+    capmanager.suspend_global_capture(in_=True)
 
     timeout_log = "input timeout!"
     try:
@@ -31,4 +12,3 @@ def input_timeout_test(pytestconfig):
     except InputTimeoutError as e:
         assert e.message == timeout_log
     capmanager.resume_global_capture()
-"""
