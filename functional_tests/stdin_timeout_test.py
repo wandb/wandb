844e058f7d7a54c4ce0f90668604443ae84b4044
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-08-19 13:24:38 +0530

    update test

--- functional_tests/stdin_timeout_test.py
+++ functional_tests/stdin_timeout_test.py
@@ -8,10 +8,13 @@ plugin:
 """
 from wandb.errors import InputTimeoutError
 from wandb.sdk.lib.stdin_timeout import stdin_timeout
-
+from wandb.util import prompt_choices
 
 timeout_log = "input timeout!"
 try:
     stdin_timeout("waiting for input", 1, timeout_log)
 except InputTimeoutError as e:
     assert str(e) == timeout_log
+
+c = prompt_choices(["1"], input_timeout=1)
+assert c == 0
