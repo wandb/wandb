7cb0ebfdb0557829769a66f4fea2fcf0048b8b7a
Kyle Goyette <kdgoyette@gmail.com>
2021-08-20 11:07:12 -0400

    format

--- wandb/apis/public.py
+++ wandb/apis/public.py
@@ -2765,10 +2765,7 @@ class Artifact(artifacts.Artifact):
         artifact = artifacts.get_artifacts_cache().get_artifact(artifact_id)
         if artifact is not None:
             return artifact
-        response = client.execute(
-            Artifact.QUERY,
-            variable_values={"id": artifact_id},
-        )
+        response = client.execute(Artifact.QUERY, variable_values={"id": artifact_id},)
 
         name = None
         if response.get("artifact") is not None:
@@ -2984,10 +2981,7 @@ class Artifact(artifacts.Artifact):
         """
         )
         self.client.execute(
-            mutation,
-            variable_values={
-                "id": self.id,
-            },
+            mutation, variable_values={"id": self.id,},
         )
         return True
 
@@ -3243,10 +3237,7 @@ class Artifact(artifacts.Artifact):
                 "description": self.description,
                 "metadata": util.json_dumps_safer(self.metadata),
                 "aliases": [
-                    {
-                        "artifactCollectionName": self._sequence_name,
-                        "alias": alias,
-                    }
+                    {"artifactCollectionName": self._sequence_name, "alias": alias,}
                     for alias in self._aliases
                 ],
             },
@@ -3415,10 +3406,7 @@ class Artifact(artifacts.Artifact):
             }
         """
         )
-        response = self.client.execute(
-            query,
-            variable_values={"id": self.id},
-        )
+        response = self.client.execute(query, variable_values={"id": self.id},)
         # yes, "name" is actually id
         runs = [
             Run(
@@ -3456,10 +3444,7 @@ class Artifact(artifacts.Artifact):
             }
         """
         )
-        response = self.client.execute(
-            query,
-            variable_values={"id": self.id},
-        )
+        response = self.client.execute(query, variable_values={"id": self.id},)
         run_obj = response.get("artifact", {}).get("createdBy", {})
         if run_obj is not None:
             return Run(
