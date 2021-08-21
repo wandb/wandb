6c7e5df410a74ba9b1866617a6f65ba28a42be59
Katia <katia@wandb.com>
2021-08-21 10:42:51 -0700

    change warn msg

--- tests/wandb_run_test.py
+++ tests/wandb_run_test.py
@@ -201,7 +201,7 @@ def test_local_warning(
     run.finish()
     captured = capsys.readouterr().err
 
-    msg = "WARNING Upgrade to W&B Local"
+    msg = "version of W&B Local to get the latest features"
 
     if empty_query:
         assert msg in captured
