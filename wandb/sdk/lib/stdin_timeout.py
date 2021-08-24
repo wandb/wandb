2f8c7e7931ba71bc7e692d7ea47db9d20b964f3f
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2021-08-24 17:23:06 +0000

    update

--- wandb/sdk/lib/stdin_timeout.py
+++ wandb/sdk/lib/stdin_timeout.py
@@ -1,3 +1,4 @@
+# --disable-error-code attr-defined
 import sys
 from typing import Optional
 
@@ -14,41 +15,40 @@ LF = "\n"
 CRLF = CR + LF
 
 
-def echo(prompt: str):
+def echo(prompt: str) -> None:
     sys.stdout.write(prompt)
     sys.stdout.flush()
 
 
 def posix_stdin_timeout(
-    prompt: Optional[str] = "",
-    timeout: Optional[float] = DEFAULT_TIMEOUT,
-    timeout_log: Optional[str] = "",
-):
+    prompt: str = "", timeout: Optional[float] = DEFAULT_TIMEOUT, timeout_log: str = "",
+) -> str:
     echo(prompt)
     sel = selectors.DefaultSelector()
     sel.register(sys.stdin, selectors.EVENT_READ)
     keys_and_events = sel.select(timeout)
     if keys_and_events:
         key, _ = keys_and_events[0]
-        return key.fileobj.readline().rstrip(LF)
+        return key.fileobj.readline().rstrip(LF)  # type: ignore[union-attr]
     else:
         echo(LF)
         termios.tcflush(sys.stdin, termios.TCIFLUSH)
-        raise InputTimeoutError(timeout_log)
+        raise InputTimeoutError(timeout_log)  # type: ignore[no-untyped-call]
 
 
 def windows_stdin_timeout(
-    prompt: Optional[str] = "",
-    timeout: Optional[float] = DEFAULT_TIMEOUT,
-    timeout_log: Optional[str] = "",
-):
+    prompt: str = "", timeout: Optional[float] = DEFAULT_TIMEOUT, timeout_log: str = "",
+) -> str:
     echo(prompt)
+    if not timeout:
+        return input()
+
     begin = time.monotonic()
     end = begin + timeout
     line = ""
     while time.monotonic() < end:
-        if msvcrt.kbhit():
-            c = msvcrt.getwche()
+        if msvcrt.kbhit():  # type: ignore[attr-defined]
+            c = msvcrt.getwche()  # type: ignore[attr-defined]
             if c in (CR, LF):
                 echo(CRLF)
                 return line
@@ -59,17 +59,16 @@ def windows_stdin_timeout(
                 line += c
         time.sleep(INTERVAL)
     echo(CRLF)
-    raise InputTimeoutError(timeout_log)
+    raise InputTimeoutError(timeout_log)  # type: ignore[no-untyped-call]
 
 
 try:
     import msvcrt
+    import time
+
+    stdin_timeout = windows_stdin_timeout
 except ImportError:
     import selectors
     import termios
 
     stdin_timeout = posix_stdin_timeout
-else:
-    import time
-
-    stdin_timeout = windows_stdin_timeout
