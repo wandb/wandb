b5663a3b354874267a16867b480a6c59bdd9815e
Kyle Goyette <kdgoyette@gmail.com>
2021-08-26 23:17:19 -0400

    initial cli logger fix

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -11,6 +11,7 @@ import os
 import shutil
 import subprocess
 import sys
+import tempfile as nativetempfile
 import textwrap
 import time
 import traceback
@@ -46,11 +47,16 @@ import yaml
 #
 # _wandb_dir = wandb_dir(env.get_dir())
 # wandb.wandb_sdk.lib.filesystem._safe_makedirs(_wandb_dir)
-# logging.basicConfig(
-#     filename=os.path.join(_wandb_dir, "debug-cli.log"),
-#     level=logging.DEBUG,
-# )
-# logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# Send cli logs to wandb/debug-cli.log by default and fallback to a temp dir.
+_wandb_dir = util.wandb_dir(env.get_dir())
+if not os.path.exists(_wandb_dir):
+    _wandb_dir = nativetempfile.gettempdir()
+
+logging.basicConfig(
+    filename=os.path.join(_wandb_dir, "debug-cli.log"), level=logging.DEBUG,
+)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
 logger = logging.getLogger("wandb")
 
 CONTEXT = dict(default_map={})
@@ -87,6 +93,11 @@ def display_error(func):
             exc_type, exc_value, exc_traceback = sys.exc_info()
             lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
             logger.error("".join(lines))
+            wandb.termerror(
+                "Find detailed error logs at: {}".format(
+                    os.path.join(_wandb_dir, "debug-cli.log")
+                )
+            )
             click_exc = ClickWandbException(e)
             click_exc.orig_type = exc_type
             six.reraise(ClickWandbException, click_exc, sys.exc_info()[2])
