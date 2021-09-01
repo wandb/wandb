6137abbcecbd7c005064b86a7c4dbb92fc74323d
Kyle Goyette <kdgoyette@gmail.com>
2021-09-01 08:03:26 -0700

    remove unused keys

--- tests/launch/test_launch.py
+++ tests/launch/test_launch.py
@@ -55,7 +55,13 @@ def mock_load_backend():
 
 
 def check_project_spec(
-    project_spec, api, uri, project=None, entity=None, config=None, parameters=None,
+    project_spec,
+    api,
+    uri,
+    project=None,
+    entity=None,
+    config=None,
+    parameters=None,
 ):
     assert project_spec.uri == uri
     expected_project = project or uri.split("/")[4]
@@ -129,7 +135,10 @@ def test_launch_add_base(live_mock_server):
     reason="wandb launch is not available for python versions <3.5",
 )
 def test_launch_specified_project(
-    live_mock_server, test_settings, mocked_fetchable_git_repo, mock_load_backend,
+    live_mock_server,
+    test_settings,
+    mocked_fetchable_git_repo,
+    mock_load_backend,
 ):
     api = wandb.sdk.internal.internal_api.Api(
         default_settings=test_settings, load_settings=False
@@ -245,8 +254,6 @@ def test_run_in_launch_context_with_artifacts(runner, live_mock_server, test_set
             "slot": "dataset",
             "requestName": "blah",
             "id": "QXJ0aWZhY3Q6NTI1MDk4",
-            "version": "v0",
-            "name": "mnist:v0",
             "_type": "artifactVersion",
             "_version": "v0",
         }
