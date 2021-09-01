82353d8834fa55c37d17ae17ed6da3c8aa189262
Danny Goldstein <dgold@berkeley.edu>
2021-09-01 15:05:28 -0700

    another attempt

--- wandb/sdk/internal/sender.py
+++ wandb/sdk/internal/sender.py
@@ -643,8 +643,7 @@ class SendManager(object):
             # the handler not to actually perform server updates for this uuid
             # because the user process will send a summary update when we resume
 
-            if self._run is not None:
-                resp.run_result.run.CopyFrom(self._run)
+            resp.run_result.run.CopyFrom(self._run)  # type ignore
             self._result_q.put(resp)
 
         # Only spin up our threads on the first run message
