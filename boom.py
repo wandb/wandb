41c678b96dcaec7a8c7a35b9a9386b580515d6d1
Vish Rajiv <vrajiv@vishwaeshs-mbp.lan>
2021-09-08 18:46:30 -0700

    test commit

---
+++ boom.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn
+import torch.optim
+import torch.profiler
+import torch.utils.data
+import torchvision.datasets
+import torchvision.models
+import torchvision.transforms as T
+
+import wandb
+
+wandb.init()
+
+transform = T.Compose(
+    [T.Resize(224),
+     T.ToTensor(),
+     T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
+
+device = torch.device('cpu')
+
+model = torchvision.models.resnet18(pretrained=True).to(device)
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+model.train()
+
+def train(data):
+    inputs, labels = data[0].to(device=device), data[1].to(device=device)
+    outputs = model(inputs)
+    loss = criterion(outputs, labels)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+
+with torch.profiler.profile(
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
+        on_trace_ready=wandb.profiler.trace_handler('/Users/vrajiv/test_profiler/boom/dog'),
+        record_shapes=True,
+        with_stack=True
+) as prof:
+    for step, batch_data in enumerate(train_loader):
+        if step >= (1 + 1 + 3) * 2:
+            break
+        train(batch_data)
+        prof.step()
