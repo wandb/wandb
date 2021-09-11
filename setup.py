7c4bc884865d7a90cdd51f0899cf97a839206812
Danny Goldstein <dgold@berkeley.edu>
2021-09-11 00:00:04 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -13,17 +13,7 @@ with open("package_readme.md") as readme_file:
 with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
 
-try:
-    with open("wandb/sweeps_engine/requirements.txt") as sweeps_requirements_file:
-        sweeps_requirements = sweeps_requirements_file.read().splitlines()
-except FileNotFoundError:
-    warnings.warn(
-        "Sweeps module is not present, unable to determine "
-        "extra requirements for sweeps. Will not be able to build "
-        "sweeps extras locally. Run `make submodule-update` to get module."
-    )
-    sweeps_requirements = []
-
+sweeps_requirements = ["sweeps>=0.0.1"]
 test_requirements = ["mock>=2.0.0", "tox-pyenv>=1.0.3"]
 
 gcp_requirements = ["google-cloud-storage"]
@@ -52,7 +42,7 @@ setup(
     url="https://github.com/wandb/client",
     packages=["wandb"],
     package_dir={"wandb": "wandb"},
-    package_data={"wandb": ["py.typed", "sweeps_engine/requirements.txt"]},
+    package_data={"wandb": ["py.typed"]},
     entry_points={
         "console_scripts": [
             "wandb=wandb.cli.cli:cli",
