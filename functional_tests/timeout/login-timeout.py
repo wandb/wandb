34381026ed7500214c9e9c239beaeee1014b2978
Jeff Raubitschek <jeff@wandb.com>
2021-09-13 11:37:08 -0700

    Add yea test

---
+++ functional_tests/timeout/login-timeout.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""Test stdin timeout
+---
+id: 0.timeout.1
+plugin:
+  - wandb
+"""
+
+import time
+
+import wandb
+
+
+timeout = 4
+slop = 0.50
+tm_start = time.time()
+wandb.login(timeout=timeout, relogin=True)
+tm_elapsed = time.time() - tm_start
+print(f"time elapsed: {tm_elapsed}")
+assert tm_elapsed > timeout * (1 - slop) and tm_elapsed < timeout * (1 + slop)
