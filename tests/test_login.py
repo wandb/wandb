e4732a92d8dd28469e3a0205ec3bfebd5d590165
Jeff Raubitschek <jeff@wandb.com>
2021-09-14 16:03:13 -0700

    [WB-6565] Fix relogin to only relogin once (#2668)

--- tests/test_login.py
+++ tests/test_login.py
@@ -22,7 +22,7 @@ def test_login_timeout(mock_tty):
     start_time = time.time()
     ret = wandb.login(timeout=4)
     elapsed = time.time() - start_time
-    assert 2 < elapsed < 6
+    assert 2 < elapsed < 15
     assert ret is False
     assert wandb.api.api_key is None
     assert wandb.setup().settings.mode == "disabled"
@@ -36,7 +36,7 @@ def test_login_timeout_choose(mock_tty):
     start_time = time.time()
     ret = wandb.login(timeout=8)
     elapsed = time.time() - start_time
-    assert elapsed < 6
+    assert elapsed < 15
     assert ret is False
     assert wandb.api.api_key is None
     assert wandb.setup().settings.mode == "offline"
@@ -48,7 +48,7 @@ def test_login_timeout_env_blank(mock_tty, reset_login_timeout):
     start_time = time.time()
     ret = wandb.login()
     elapsed = time.time() - start_time
-    assert elapsed < 6
+    assert elapsed < 15
     assert ret is False
     assert wandb.api.api_key is None
     assert wandb.setup().settings.mode == "disabled"
@@ -60,3 +60,10 @@ def test_login_timeout_env_invalid(mock_tty, reset_login_timeout):
 
     with pytest.raises(UsageError):
         wandb.login()
+
+
+def test_relogin_timeout(test_settings, dummy_api_key):
+    logged_in = wandb.login(relogin=True, key=dummy_api_key)
+    assert logged_in is True
+    logged_in = wandb.login()
+    assert logged_in is True
