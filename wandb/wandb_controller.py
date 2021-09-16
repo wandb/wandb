85da871eb37e3cea78e6bc4e7aba6d698a1979fc
Danny Goldstein <dgold@berkeley.edu>
2021-09-15 22:33:06 -0700

    remove --upgrade

--- wandb/wandb_controller.py
+++ wandb/wandb_controller.py
@@ -75,7 +75,7 @@ import yaml
 sweeps = get_module(
     "wandb.sweeps",
     required="wandb[sweeps] is required to use the local controller. "
-    "Please run `pip install --upgrade wandb[sweeps]`.",
+    "Please run `pip install wandb[sweeps]`.",
 )
 
 
