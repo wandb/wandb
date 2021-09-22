92268438161220a917418f69e7758cf3551ca358
Chris Van Pelt <vanpelt@gmail.com>
2021-09-22 10:10:43 -0700

    Fix formatting

--- wandb/sdk/verify/verify.py
+++ wandb/sdk/verify/verify.py
@@ -69,10 +69,8 @@ def check_logged_in(api: Api, host: str) -> bool:
     login_doc_url = "https://docs.wandb.ai/ref/login"
     fail_string = None
     if api.api_key is None:
-        fail_string = (
-            "Not logged in. Please log in using wandb login. See the docs: {}".format(
-                click.style(login_doc_url, underline=True, fg="blue")
-            )
+        fail_string = "Not logged in. Please log in using wandb login. See the docs: {}".format(
+            click.style(login_doc_url, underline=True, fg="blue")
         )
     # check that api key is correct
     # TODO: Better check for api key is correct
@@ -283,8 +281,7 @@ def log_use_download_artifact(
             return False, None, failed_test_strings
 
     with wandb.init(
-        project=PROJECT_NAME,
-        config={"test": "artifact use"},
+        project=PROJECT_NAME, config={"test": "artifact use"},
     ) as use_art_run:
         try:
             used_art = use_art_run.use_artifact("{}:{}".format(name, alias))
