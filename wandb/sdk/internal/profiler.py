b2d878512b7bc2bdb3ed0f84571d4e005e59905f
Vish Rajiv <vrajiv@vishwaeshs-mbp.lan>
2021-09-24 11:24:32 -0700

    warning if torch version less than 1.9

--- wandb/sdk/internal/profiler.py
+++ wandb/sdk/internal/profiler.py
@@ -6,11 +6,15 @@ import os
 import wandb
 from wandb.errors import UsageError
 
+PYTORCH_MODULE = "torch"
 PYTORCH_PROFILER_MODULE = "torch.profiler"
 
 
 def trace():
+    torch = wandb.util.get_module(PYTORCH_MODULE, required=True)
     torch_profiler = wandb.util.get_module(PYTORCH_PROFILER_MODULE, required=True)
+    version = tuple(map(lambda x: int(x), torch.__version__.split(".")))
+
     try:
         logdir = os.path.join(wandb.run.dir, "pytorch_traces")
         os.mkdir(logdir)
@@ -19,6 +23,10 @@ def trace():
             "Please call wandb.init() before wandb.profiler.trace()"
         ) from None
 
-    return torch_profiler.tensorboard_trace_handler(
-        logdir, worker_name=None, use_gzip=False
-    )
+    if version < (1, 9, 0):
+        wandb.termwarn(
+            f"It is recommended that torch version be at least 1.9 \
+            in order to use the PyTorch Profiler API.\nVersion of torch currently installed: {torch.__version__}"
+        )
+
+    return torch_profiler.tensorboard_trace_handler(logdir)
