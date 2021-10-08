19a29e74ec07a3f8de5038bd1bf32da4087aef55
Danny Goldstein <danny@goldstein.cloud>
2021-10-07 20:44:22 -0700

    [WB-6880] Acknowledge leased run queue items during upsert run (#2374)

--- tests/utils/mock_server.py
+++ tests/utils/mock_server.py
@@ -70,6 +70,7 @@ def default_ctx():
         "run_ids": [],
         "file_names": [],
         "emulate_artifacts": None,
+        "item_acked": False,
         "run_state": "running",
         "run_queue_item_check_count": 0,
         "return_jupyter_in_run_info": False,
@@ -583,7 +584,12 @@ def create_app(user_ctx=None):
                     {
                         "data": {
                             "QueryType": {"fields": [{"name": "serverInfo"},]},
-                            "ServerInfoType": {"fields": [{"name": "cliVersionInfo"},]},
+                            "ServerInfoType": {
+                                "fields": [
+                                    {"name": "cliVersionInfo"},
+                                    {"name": "exposesExplicitRunQueueAckPath"},
+                                ]
+                            },
                         }
                     }
                 )
@@ -596,6 +602,7 @@ def create_app(user_ctx=None):
                             "fields": [
                                 {"name": "cliVersionInfo"},
                                 {"name": "latestLocalVersionInfo"},
+                                {"name": "exposesExplicitRunQueueAckPath"},
                             ]
                         },
                     }
@@ -675,6 +682,7 @@ def create_app(user_ctx=None):
                                         "type": "run",
                                         "run_id": "mocker-sweep-run-x9",
                                         "args": {"learning_rate": {"value": 0.99124}},
+                                        "runqueue_item_id": "1jfskn2z",
                                     }
                                 ]
                             ),
@@ -745,6 +753,8 @@ def create_app(user_ctx=None):
                 response["data"]["upsertBucket"]["bucket"][
                     "sweepName"
                 ] = "test-sweep-id"
+            if body["variables"].get("runQueueItemId") == "1jfskn2z":
+                ctx["item_acked"] = True
             return json.dumps(response)
         if "mutation DeleteRun(" in body["query"]:
             return json.dumps({"data": {}})
@@ -1783,6 +1793,10 @@ class ParseCTX(object):
         for k, v in items.items():
             d[k] = getattr(self, v)
         return d
+
+    @property
+    def item_acked(self):
+        return self._ctx.get("item_acked")
 
 
 if __name__ == "__main__":
