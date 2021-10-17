12d9b047875c185d151bb978f7d6c6bbef46205a
Jeff Raubitschek <jeff@wandb.com>
2021-10-17 15:41:43 -0700

    Implement artifact polling interface

--- functional_tests/mp/14-artifact-log.py
+++ functional_tests/mp/14-artifact-log.py
@@ -12,8 +12,13 @@ def main():
     with open("my-dataset.txt", "w") as fp:
         fp.write("this-is-data")
     artifact = wandb.Artifact('my-dataset', type='dataset')
+    table = wandb.Table(columns=["a", "b", "c"], data=[[1, 2, 3]])
+    artifact.add(table, "my_table")
     artifact.add_file('my-dataset.txt')
-    run.log_artifact(artifact)
+    art = run.log_artifact(artifact)
+    art.wait()
+    get_table = art.get("my_table")
+    print("TABLE", get_table)
     run.finish()
 
 
