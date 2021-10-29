2ce6dcbabaab2946beffcade00ce3373beb31695
Danny Goldstein <dgold@berkeley.edu>
2021-10-28 19:17:42 -0700

    rename tests

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -1155,7 +1155,7 @@ class Run(object):
             [our guides to logging](https://docs.wandb.com/guides/track/log).
 
             Basic usage
-            <!--yeadoc-test:log-and-init-basic-->
+            <!--yeadoc-test:init-and-log-basic-->
             ```python
             import wandb
             wandb.init()
@@ -1163,7 +1163,7 @@ class Run(object):
             ```
 
             Incremental logging
-            <!--yeadoc-test:log-and-init-incremental-->
+            <!--yeadoc-test:init-and-log-incremental-->
             ```python
             import wandb
             wandb.init()
