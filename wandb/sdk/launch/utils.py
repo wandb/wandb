2264a0d57101e61211485eb53d509c3168cf8bcd
Kyle Goyette <kdgoyette@gmail.com>
2021-11-01 14:47:57 -0700

    handle IP addresses

--- wandb/sdk/launch/utils.py
+++ wandb/sdk/launch/utils.py
@@ -15,7 +15,9 @@ from wandb.errors import CommError, ExecutionError, LaunchError
 
 # TODO: this should be restricted to just Git repos and not S3 and stuff like that
 _GIT_URI_REGEX = re.compile(r"^[^/|^~|^\.].*(git|bitbucket)")
-_WANDB_URI_REGEX = re.compile(r"^https://(api.)?wandb")
+_VALID_IP_REGEX = r"^https?://[0-9]+(?:\.[0-9]+){3}(:[0-9]+)?"
+_VALID_WANDB_REGEX = r"^https?://(api.)?wandb"
+_WANDB_URI_REGEX = re.compile(r"|".join([_VALID_WANDB_REGEX, _VALID_IP_REGEX]))
 _WANDB_QA_URI_REGEX = re.compile(
     r"^https?://ap\w.qa.wandb"
 )  # for testing, not sure if we wanna keep this
