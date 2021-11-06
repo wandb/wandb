9bf80f6f6e55976effd11c6a8c493be03acb9c57
Danny Goldstein <dgold@berkeley.edu>
2021-11-06 16:24:20 -0700

    improve coverage of internal api with more unit tests

--- tests/test_internal_api.py
+++ tests/test_internal_api.py
@@ -1,6 +1,10 @@
 import pytest
+import queue
 import wandb
 from wandb.apis import internal
+from wandb.sdk.interface.interface_queue import InterfaceQueue
+from wandb.sdk.internal.sender import SendManager
+from wandb.proto import wandb_internal_pb2 as pb
 
 
 def test_agent_heartbeat_with_no_agent_id_fails(test_settings):
@@ -30,3 +34,24 @@ def test_resume_from_checkpoint(live_mock_server, test_settings):
     finished, progress = a.check_task_progress(taskid)
     assert finished
     assert progress == 100
+
+
+def test_maybe_setup_resume_checkpoint(live_mock_server, test_settings):
+    a = internal.Api()
+    run = wandb.init()
+    checkpoint_name = "test-checkpoint"
+    result = a.log_checkpoint(run.entity, run.project_name(), run.id, checkpoint_name)
+    assert result["name"] == checkpoint_name
+
+    live_mock_server.set_ctx({"resume": True})
+    proto = pb.RunRecord()
+    run._make_proto_run(proto)
+    proto.checkpoint = checkpoint_name
+
+    sm = SendManager(test_settings, queue.Queue(), queue.Queue(), InterfaceQueue())
+    sm._maybe_setup_resume(proto)
+
+    assert sm._resume_state.history == 15
+    assert sm._resume_state.events == 0
+    assert sm._resume_state.output == 14
+    assert sm._resume_state.resumed
