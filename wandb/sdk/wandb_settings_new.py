ac30d14c77688c18e6e630278a35ecac40431a66
Dmitry Duev <dmitry.duev@gmail.com>
2021-12-07 14:07:18 -0800

    save current thoughts

--- wandb/sdk/wandb_settings_new.py
+++ wandb/sdk/wandb_settings_new.py
@@ -8,8 +8,6 @@ from typing import (
     Union,
 )
 
-import attr
-
 
 def _build_inverse_map(prefix: str, d: Dict[str, Optional[str]]) -> Dict[str, str]:
     return {v or prefix + k.upper(): k for k, v in d.items()}
@@ -34,7 +32,7 @@ class Source(enum.IntEnum):
 
 
 class Property:
-    def __init__(
+    def __init__(  # pylint: disable=unused-argument
         self,
         name: str,
         value: Optional[Any] = None,
@@ -43,6 +41,7 @@ class Property:
         is_policy: bool = False,
         frozen: bool = False,
         source: int = Source.BASE,
+        **kwargs,
     ):
         self.name = name
         self._preprocessor = preprocessor
@@ -51,9 +50,15 @@ class Property:
         self._source = source
 
         # preprocess and validate value
-        self.__dict__["value"] = self._validate(self._preprocess(value))
+        # self.__dict__["value"] = self._validate(self._preprocess(value))
+        # object.__setattr__(self, "_value", self._validate(self._preprocess(value)))
+        self._value = self._validate(self._preprocess(value))
 
         self.__frozen = frozen
+
+    @property
+    def value(self):
+        return self._value
 
     def _preprocess(self, value):
         if self._preprocessor is not None:
@@ -71,9 +76,9 @@ class Property:
         return value
 
     def update(
-            self,
-            value: Optional[Any] = None,
-            source: Optional[int] = Source.OVERRIDE,
+        self,
+        value: Any,
+        source: Optional[int] = Source.OVERRIDE,
     ):
         if self.__frozen:
             raise TypeError("Property object is frozen")
@@ -82,48 +87,90 @@ class Property:
         #   - update value if source is lower than or equal to current source and property is policy
         #   - update value if source is higher than or equal to current source and property is not policy
         if (
-                (source == Source.OVERRIDE)
-                or (self._is_policy and self._source != Source.OVERRIDE and source <= self._source)
-                or (not self._is_policy and self._source != Source.OVERRIDE and source >= self._source)
+            (source == Source.OVERRIDE)
+            or (self._is_policy and self._source != Source.OVERRIDE and source <= self._source)
+            or (not self._is_policy and self._source != Source.OVERRIDE and source >= self._source)
         ):
-            self.__dict__["value"] = self._validate(self._preprocess(value))
+            # self.__dict__["_value"] = self._validate(self._preprocess(value))
+            self._value = self._validate(self._preprocess(value))
             self._source = source
 
     def __setattr__(self, key, value):
+        if "_Property__frozen" in self.__dict__ and self.__frozen:
+            raise TypeError(f"Property object {self.name} is frozen")
         if key == "value":
             raise AttributeError("Use update() to update property value")
         self.__dict__[key] = value
 
     def __repr__(self):
-        # return f"<Property {self.name}: value={self.value} source={self._source}>"
-        # return f"<Property {self.name}: value={self.value}>"
+        # return f"<Property {self.name}: value={self._value} source={self._source}>"
+        # return f"<Property {self.name}: value={self._value}>"
         # return self.__dict__.__repr__()
-        return f"{self.__dict__['value']}"
+        return f"{self._value}"
 
 
-@attr.s
 class Settings:
     """
     Settings for the wandb client.
     """
 
-    # base_url: str = attr.ib(
-    #     default="https://api.wandb.ai",
-    #     validator=[attr.validators.instance_of(str)],
-    #     metadata={
-    #         "source": Source.BASE,
-    #         "help": "The base url for the wandb api."
-    #     },
-    # )
-    # The base url for the wandb api.  fixme: add help string to Property instead?
-    base_url = attr.ib(Property(name="base_url", value="https://api.wandb.ai", is_policy=True))
-    meaning_of_life = attr.ib(Property(name="meaning_of_life", value="42", preprocessor=[lambda x: int(x)]))
+    def __init__(self):
+        settings = {
+            "base_url": {
+                "value": "https://api.wandb.ai",
+                "preprocessor": lambda x: str(x),
+                "validator": lambda x: isinstance(x, str),
+                "is_policy": True,
+                "help": "The base url for the wandb api.",
+            },
+            "meaning_of_life": {
+                "value": "42",
+                "preprocessor": lambda x: int(x),
+            }
+        }
+        for key, specs in settings.items():
+            object.__setattr__(
+                self,
+                key,
+                Property(name=key, **specs, source=Source.SETTINGS),
+            )
+
+    def __repr__(self):
+        # return f"<Settings {[{a: p.value} if isinstance(p, Property) else {a: p} for a, p in self.__dict__.items()]}>"
+        return f"<Settings {[{a: p} for a, p in self.__dict__.items()]}>"
 
     def __getattr__(self, item):
         return self.__dict__[item].value
 
     def __setattr__(self, key, value):
-        raise TypeError("Use update() to update property values")
+        raise TypeError("Use update() to update attribute values")
 
-    def update(self):
-        pass
+    # def _path_convert(self, *path: Any) -> Optional[str]:
+    #     """convert slashes, expand ~ and other macros."""
+    #
+    #     format_dict: Dict[str, Union[str, int]] = dict()
+    #     if self._start_time and self._start_datetime:
+    #         format_dict["timespec"] = datetime.strftime(
+    #             self._start_datetime, "%Y%m%d_%H%M%S"
+    #         )
+    #     if self.run_id:
+    #         format_dict["run_id"] = self.run_id
+    #     format_dict["run_mode"] = "offline-run" if self._offline else "run"
+    #     format_dict["proc"] = os.getpid()
+    #     # TODO(cling): hack to make sure we read from local settings
+    #     #              this is wrong if the run_dir changes later
+    #     format_dict["wandb_dir"] = self.wandb_dir or "wandb"
+    #
+    #     path_items: List[str] = []
+    #     for p in path:
+    #         part = self._path_convert_part(p, format_dict)
+    #         if part is None:
+    #             return None
+    #         path_items += part
+    #     converted_path = os.path.join(*path_items)
+    #     converted_path = os.path.expanduser(converted_path)
+    #     return converted_path
+
+    def update(self, settings: Dict[str, Any], source: int = Source.OVERRIDE):
+        for key, value in settings.items():
+            self.__dict__[key].update(value, source)
