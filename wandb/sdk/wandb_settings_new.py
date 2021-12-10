465babae9dc65c92fc0320e5f78bedebd779d318
Dmitry Duev <dmitry.duev@gmail.com>
2021-12-09 23:48:00 -0800

    save progress

--- wandb/sdk/wandb_settings_new.py
+++ wandb/sdk/wandb_settings_new.py
@@ -5,6 +5,7 @@ import multiprocessing
 import os
 import re
 import tempfile
+import time
 from typing import (
     Any,
     Callable,
@@ -86,13 +87,24 @@ class SettingsConsole(enum.Enum):
 
 
 class Property:
+    """
+    A class to represent attributes (individual settings) of the Settings object.
+
+        - Encapsulates the logic of how to preprocess and validate values of settings
+          throughout the lifetime of a class instance.
+        - Allows for runtime modification of settings with hooks, e.g. in the case when
+          a setting depends on another setting.
+        - The update() method is used to update the value of a setting.
+        - The `is_policy` attribute determines the source priority when updating the property value.
+          E.g. if `is_policy` is True, the smallest `Source` value takes precedence.
+    """
     def __init__(  # pylint: disable=unused-argument
         self,
         name: str,
         value: Optional[Any] = None,
         preprocessor: Union[Callable, Sequence[Callable], None] = None,
         validator: Union[Callable, Sequence[Callable], None] = None,
-        # runtime converter (hook?): properties can be e.g. tied to other properties
+        # runtime converter (hook): properties can be e.g. tied to other properties
         hook: Union[Callable, Sequence[Callable], None] = None,
         is_policy: bool = False,
         frozen: bool = False,
@@ -114,7 +126,8 @@ class Property:
         self.__frozen = frozen
 
     @property
-    def value(self):
+    def value(self) -> Any:
+        """Apply the runtime modifier(s) (if any) and return the value."""
         _value = self._value
         if _value is not None and self._hook is not None:
             _hook = [self._hook] if callable(self._hook) else self._hook
@@ -122,14 +135,14 @@ class Property:
                 _value = h(_value)
         return _value
 
-    def _preprocess(self, value):
+    def _preprocess(self, value: Any) -> Any:
         if value is not None and self._preprocessor is not None:
             _preprocessor = [self._preprocessor] if callable(self._preprocessor) else self._preprocessor
             for p in _preprocessor:
                 value = p(value)
         return value
 
-    def _validate(self, value):
+    def _validate(self, value: Any) -> Any:
         if value is not None and self._validator is not None:
             _validator = [self._validator] if callable(self._validator) else self._validator
             for v in _validator:
@@ -142,6 +155,7 @@ class Property:
         value: Any,
         source: Optional[int] = Source.OVERRIDE,
     ):
+        """Update the value of the property."""
         if self.__frozen:
             raise TypeError("Property object is frozen")
         if TYPE_CHECKING:
@@ -249,6 +263,14 @@ class Settings:
             elif re.match(r".*wandb\.ai[^\.]*$", value) and "http://" in value:
                 raise UsageError("http is not secure, please use https://api.wandb.ai")
         return True
+
+    # other helper methods
+    def _start_run(self) -> None:
+        time_stamp = time.time()
+        datetime_now: datetime = datetime.fromtimestamp(time_stamp)
+        time_now: float = time_stamp
+        object.__setattr__(self, "_Settings_start_datetime", datetime_now)
+        object.__setattr__(self, "_Settings_start_time", time_now)
 
     def __init__(
         self,
@@ -736,7 +758,17 @@ class Settings:
                 Property(name=key, **specs, source=Source.SETTINGS),
             )
 
-        # freeze settings
+        # setup private attributes
+        object.__setattr__(self, "_Settings_start_datetime", None)
+        object.__setattr__(self, "_Settings_start_time", None)
+        # class_defaults = self._get_class_defaults()
+        # self._apply_defaults(class_defaults)
+        # self._apply_defaults(defaults)
+        # self._update(kwargs, _source=self.Source.SETTINGS)
+        # if os.environ.get(wandb.env.DIR) is None:
+        #     self.root_dir = os.path.abspath(os.getcwd())
+
+        # freeze settings to prevent accidental changes
         # self.freeze()
 
     def __repr__(self):
@@ -745,6 +777,7 @@ class Settings:
 
     # access methods
     def __getattribute__(self, name: str):
+        """Expose attribute.value if attribute is a Property."""
         item = object.__getattribute__(self, name)
         if isinstance(item, Property):
             return item.value
@@ -754,11 +787,15 @@ class Settings:
         raise TypeError("Please use update() to update attribute values")
 
     def update(self, settings: Dict[str, Any], source: int = Source.OVERRIDE):
+        """Update individual settings using the Property.update() method."""
         if "_Settings__frozen" in self.__dict__ and self.__frozen:
             raise TypeError(f"Settings object is frozen")
         if TYPE_CHECKING:
             _source = cast(Optional[int], source)
         for key, value in settings.items():
+            # only allow updating known Properties
+            if key not in self.__dict__ or not isinstance(self.__dict__[key], Property):
+                raise KeyError(f"Unknown setting: {key}")
             self.__dict__[key].update(value, source)
 
     def freeze(self):
@@ -768,12 +805,19 @@ class Settings:
         object.__setattr__(self, "_Settings__frozen", False)
 
     def make_static(self):
+        """Generate a static, serializable version of the settings."""
         return {k: v.value for k, v in self.__dict__.items() if isinstance(v, Property)}
 
-    # properties
+    # computed properties
     @property
     def wandb_dir(self) -> str:
         return get_wandb_dir(self.root_dir or "")
+
+    @property
+    def _offline(self) -> bool:
+        if self.disabled or (self.mode in ("dryrun", "offline")):
+            return True
+        return False
 
     @property
     def is_local(self) -> bool:
