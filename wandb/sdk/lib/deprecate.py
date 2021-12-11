a4effa0460ca1befe0bf225558f766d39d03df54
Dmitry Duev <dmitry.duev@gmail.com>
2021-12-10 20:45:54 -0800

    move deprecate() to wandb.sdk.lib.deprecate

--- wandb/sdk/lib/deprecate.py
+++ wandb/sdk/lib/deprecate.py
@@ -1,12 +1,19 @@
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 import wandb
+from wandb.proto.wandb_telemetry_pb2 import Deprecated as TelemetryDeprecated
+# from wandb.proto.wandb_telemetry_pb2 import TelemetryRecord
+
+
+# avoid cycle, use string type reference
+if TYPE_CHECKING:
+    from .. import wandb_run
 
 
 def deprecate(
     field_name: str,
     warning_message: str,
-    run: Optional[wandb.sdk.wandb_run.Run] = None,
+    run: Optional["wandb_run.Run"] = None,
 ):
     """
     Warn the user that a feature has been deprecated
@@ -16,9 +23,9 @@ def deprecate(
         field_name: The name of the feature that has been deprecated.
                     Defined in wandb/proto/wandb_telemetry.proto::Deprecated
         warning_message: The message to display to the user.
-        run: The run, to whose telemetry the event will be added.
+        run: The run to whose telemetry the event will be added.
     """
-    wandb_run = wandb.run if run is None else run
-    with wandb.wandb_lib.telemetry.context(run=wandb_run) as tel:
+    _run = run or wandb.run
+    with wandb.wandb_lib.telemetry.context(run=_run) as tel:
         setattr(tel.deprecated, field_name, True)
     wandb.termwarn(warning_message, repeat=False)
