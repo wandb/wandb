cfa0744edc71cc5eb21e788de48d0cf6aecd1044
Jeff Raubitschek <jeff@wandb.com>
2021-12-15 14:37:57 -0800

    Revert "[WB-6880] Acknowledge leased run queue items during upsert run (#2804)"
    This reverts commit ba0d86cae34c8201c0e73b448963c90ab0881e48.

--- tests/utils/mock_server.py
+++ tests/utils/mock_server.py
@@ -70,7 +70,6 @@ def default_ctx():
         "run_ids": [],
         "file_names": [],
         "emulate_artifacts": None,
-        "item_acked": False,
         "run_state": "running",
         "run_queue_item_check_count": 0,
         "return_jupyter_in_run_info": False,
@@ -718,7 +717,6 @@ def create_app(user_ctx=None):
                                             "args": {
                                                 "a": {"value": ctx["n_sweep_runs"]}
                                             },
-                                            "runqueue_item_id": f"1jfskn2z",
                                         }
                                     ]
                                 )
@@ -817,8 +815,6 @@ def create_app(user_ctx=None):
                 response["data"]["upsertBucket"]["bucket"][
                     "sweepName"
                 ] = "test-sweep-id"
-            if body["variables"].get("runQueueItemId") == "1jfskn2z":
-                ctx["item_acked"] = True
             return json.dumps(response)
         if "mutation DeleteRun(" in body["query"]:
             return json.dumps({"data": {}})
@@ -2003,10 +1999,6 @@ class ParseCTX(object):
         for k, v in items.items():
             d[k] = getattr(self, v)
         return d
-
-    @property
-    def item_acked(self):
-        return self._ctx.get("item_acked")
 
 
 if __name__ == "__main__":
