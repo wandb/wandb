76a3b7633fae40234267040e8e63e781b3a88b91
stephchen <stephchen@wandb.com>
2021-12-15 21:47:27 -0500

    async support + messaging

--- wandb/sdk/launch/runner/gcp_vertex.py
+++ wandb/sdk/launch/runner/gcp_vertex.py
@@ -4,6 +4,7 @@ import subprocess
 from typing import Any, Dict, List, Optional
 import datetime
 import yaml
+import time
 
 from six.moves import shlex_quote
 
@@ -15,7 +16,6 @@ from wandb.apis.internal import Api
 from google.cloud import aiplatform
 
 from .._project_spec import (
-    DEFAULT_LAUNCH_METADATA_PATH,
     get_entry_point_command,
     LaunchProject,
 )
@@ -34,24 +34,52 @@ from ..utils import (
     PROJECT_SYNCHRONOUS,
 )
 
+GCP_CONSOLE_URI = "https://console.cloud.google.com"
 
 class VertexSubmittedRun(AbstractRun):
-    def __init__(self, model) -> None:
-        self._model = model
+    def __init__(self, job) -> None:
+        self._job = job
 
     @property
     def id(self) -> int:
-        pass
+        return self._job.name   # numeric ID of the custom training job
+
+    @property
+    def name(self) -> str:
+        return self._job.display_name
+
+    @property
+    def gcp_region(self) -> str:
+        return self._job.location
+
+    @property
+    def gcp_project(self) -> str:
+        return self._job.project
+
+    def get_page_link(self) -> str:
+        return "{console_uri}/vertex-ai/locations/{region}/training/{job_id}?project={project}".format(
+            console_uri=GCP_CONSOLE_URI,
+            region=self.gcp_region,
+            job_id=self.id,
+            project=self.gcp_project,
+        )
 
     def wait(self) -> bool:
-        pass
+        self._job.wait()
+        return self.get_status() == Status("finished")
 
     def get_status(self) -> Status:
-        # todo: only sync for now so always finished
-        return Status("finished")
+        job_state = str(self._job.state)     # extract from type PipelineState
+        if job_state == 'PipelineState.PIPELINE_STATE_SUCCEEDED':
+            return Status("finished")
+        if job_state == 'PipelineState.PIPELINE_STATE_FAILED':
+            return Status("failed")
+        if job_state == 'PipelineState.PIPELINE_STATE_RUNNING':
+            return Status("running")
+        return Status("unknown")
 
     def cancel(self) -> None:
-        pass
+        self._job.cancel()
 
 
 class VertexRunner(AbstractRunner):
@@ -87,6 +115,8 @@ class VertexRunner(AbstractRunner):
             "gcp_docker_host"
         ) or "{region}-docker.pkg.dev".format(region=gcp_region)
         gcp_machine_type = resource_args.get("gcp_machine_type") or "n1-standard-4"
+        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        gcp_training_job_name = resource_args.get("gcp_job_name") or "{project}_{time}".format(project=launch_project.target_project, time=timestamp)
 
         aiplatform.init(
             project=gcp_project, location=gcp_region, staging_bucket=gcp_staging_bucket
@@ -95,7 +125,7 @@ class VertexRunner(AbstractRunner):
         validate_docker_installation()
         synchronous: bool = self.backend_config[
             PROJECT_SYNCHRONOUS
-        ]  # todo: support this
+        ]
         docker_args: Dict[str, Any] = self.backend_config[PROJECT_DOCKER_ARGS]
         if docker_args:
             wandb.termwarn(
@@ -161,20 +191,33 @@ class VertexRunner(AbstractRunner):
                 )
                 return None
 
-        TIMESTAMP = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         job = aiplatform.CustomContainerTrainingJob(
-            display_name="test_job" + TIMESTAMP, container_uri=image,
+            display_name=gcp_training_job_name, container_uri=image,
         )
+        submitted_run = VertexSubmittedRun(job)
 
         # todo: support gcp dataset?
 
-        model = job.run(
-            machine_type=gcp_machine_type, accelerator_count=0, replica_count=1,
-        )
+        wandb.termlog("Running training job {name} on {compute}.".format(name=gcp_training_job_name, compute=gcp_machine_type))
 
-        # need to figure out what a managed model is
-        # todo: this runs sync only
-        return VertexSubmittedRun(model)
+        # when sync is True, vertex blocks the main thread on job completion. when False, vertex returns a Future
+        # on this thread but continues to block the process on another thread. always set sync=False so we can get
+        # the job info (dependent on job._gca_resource)
+        model = job.run(
+            machine_type=gcp_machine_type, accelerator_count=0, replica_count=1, sync=False   # @@@ accelerator
+        )
+        while job._gca_resource is None:
+            # give time for the gcp job object to be created, this should only loop a couple times max
+            time.sleep(1)
+
+        wandb.termlog("View your job status and logs at {url}.".format(url=submitted_run.get_page_link()))
+
+        # hacky: if user doesn't want blocking behavior, kill both main thread and the background thread. job continues
+        # to run remotely. this obviously doesn't work if we need to do some sort of postprocessing after this run fn
+        if not synchronous:
+            os._exit(0)
+
+        return submitted_run
 
 
 def run_shell(args):
