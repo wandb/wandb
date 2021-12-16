3195720f943b87caa347d98866153a2e997e97c1
Jeff Raubitschek <jeff@wandb.com>
2021-12-16 14:33:41 -0800

    Fix upload_file exception handler

--- wandb/sdk/internal/internal_api.py
+++ wandb/sdk/internal/internal_api.py
@@ -1573,7 +1573,8 @@ class Api(object):
         except requests.exceptions.RequestException as e:
             logger.error("upload_file exception {}: {}".format(url, e))
             logger.error("upload_file request headers: {}".format(e.request.headers))
-            logger.error("upload_file response body: {}".format(e.response.content))
+            response_content = e.response.content if e.response is not None else ""
+            logger.error("upload_file response body: {}".format(response_content))
             status_code = e.response.status_code if e.response != None else 0
             # We need to rewind the file for the next retry (the file passed in is seeked to 0)
             progress.rewind()
