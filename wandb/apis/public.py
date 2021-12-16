6ceabd33b85dbc9dccbf985b2728376f52738ba3
Vishwaesh Rajiv <vrajiv@vishwaeshs-mbp.lan>
2021-12-16 13:27:41 -0800

    artifact not in cache

--- wandb/apis/public.py
+++ wandb/apis/public.py
@@ -3396,6 +3396,7 @@ class Artifact(artifacts.Artifact):
         artifact = artifacts.get_artifacts_cache().get_artifact(artifact_id)
         if artifact is not None:
             return artifact
+        logger.info("DEBUG - ARTIFACT IS NOT IN CACHE")
         print("DEBUG - ARTIFACT IS NOT IN CACHE")
         response = client.execute(Artifact.QUERY, variable_values={"id": artifact_id},)
 
