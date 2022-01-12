e1d0e5e5649d8c5916cdad55db80f00e8ea8ac78
John Qian <johnlongqian@gmail.com>
2022-01-12 15:49:08 -0600

    last fix

---
+++ test_results.py
@@ -0,0 +1,28 @@
+
+
+
+
+
+RUN_ID=3oelfhnb
+
+
+INFO: Timed out waiting for server to start... http://localhost:62000 1642011483.4515772
+INFO: Timed out waiting for server to start... http://localhost:62000 1642011484.455464
+INFO: Mock server listing on 62000 see /Users/johnqian/go/src/github.com/wandb/client/.yea_cache/standalone-live_mock_server-1.log
+INFO: DEPEND= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/code-save/code-save-disabled.yea
+INFO: RUN= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/code-save/code-save-disabled.yea
+INFO: RUNNING= ['coverage', 'run', '--rcfile', '/Users/johnqian/go/src/github.com/wandb/client/.yea_cache/yea-covrc-28532-0.code-save.1-disabled.conf', './code-save.py']
+INFO: exit= 0
+INFO: DEPEND= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/code-save/code-save-enabled.yea
+INFO: RUN= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/code-save/code-save-enabled.yea
+INFO: RUNNING= ['coverage', 'run', '--rcfile', '/Users/johnqian/go/src/github.com/wandb/client/.yea_cache/yea-covrc-28532-0.code-save.1-enabled.conf', './code-save.py']
+INFO: exit= 0
+INFO: DEPEND= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/code-save/log-code/log-code.py
+INFO: RUN= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/code-save/log-code/log-code.py
+INFO: RUNNING= ['coverage', 'run', '--rcfile', '/Users/johnqian/go/src/github.com/wandb/client/.yea_cache/yea-covrc-28532-0.code-save.3-log_code.conf', './log-code.py']
+INFO: exit= 0
+WARNING: index 0 not found in :wandb:runs [0,output,stdout,0]
+invalid :op:contains {} contains source-3oelfhnb
+INFO: DEPEND= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/core/01-without-finish.py
+INFO: RUN= /Users/johnqian/go/src/github.com/wandb/client/functional_tests/core/01-without-finish.py
+INFO: RUNNING= ['coverage', 'run', '--rcfile', '/Users/johnqian/go/src/github.com/wandb/client/.yea_cache/yea-covrc-28532-0.core.01-without-finish.conf', './01-without-finish.py']
