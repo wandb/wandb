fa4a90fe36daa94e32fe1137f6fdfc7da85ab145
davebulaval <david.beauchemin.5@ulaval.ca>
2022-01-13 15:54:58 -0500

    added function to unwrap nested dict config such as DictConfig use by hydra-core

--- wandb/sdk/wandb_helper.py
+++ wandb/sdk/wandb_helper.py
@@ -1,6 +1,7 @@
 #
 import inspect
 import types
+from typing import Dict, Mapping, Sequence, Union
 
 import six
 from wandb.errors import UsageError
@@ -33,8 +34,8 @@ def _to_dict(params):
     if meta:
         is_tf_flags_module = (
             isinstance(params, types.ModuleType)
-            and meta.__name__ == "tensorflow.python.platform.flags"  # noqa: W503
-        )
+            and meta.__name__ == "tensorflow.python.platform.flags"
+        )  # noqa: W503
         if is_tf_flags_module or meta.__name__ == "absl.flags":
             params = params.FLAGS
             meta = inspect.getmodule(params)
@@ -49,6 +50,9 @@ def _to_dict(params):
         params = vars(params)["__flags"]
     elif not hasattr(params, "__dict__"):
         raise TypeError("config must be a dict or have a __dict__ attribute.")
+    elif isinstance(params, Mapping) or isinstance(params, Sequence):
+        # Cases where params is a dict of dict, a dict of config, a sequence or other similar mapping type.
+        params = _parse_nested_config(params)
     else:
         # params is a Namespace object (argparse)
         # or something else
@@ -56,3 +60,47 @@ def _to_dict(params):
 
     # assume argparse Namespace
     return params
+
+
+def _parse_nested_config(config_params: Union[Mapping, Sequence]):
+    """
+    Args:
+        config_params (Union[Mapping, Sequence]):
+            The config parameters of the training to log, such as number of epoch, loss function, optimizer etc.
+    """
+    un_nested_params = {}
+    if isinstance(config_params, Mapping):
+        for param_name, element in config_params.items():
+            un_nested_params.update(_unwrap_nested_config(param_name, element))
+    else:  # equivalent to "if isinstance(config_params, Sequence):"
+        for idx, element in enumerate(config_params):
+            un_nested_params.update(_unwrap_nested_config(str(idx), element))
+    return un_nested_params
+
+
+def _unwrap_nested_config(
+    parent_name: str, element: Union[int, float, str, Mapping, Sequence]
+) -> Dict:
+    """
+    Function to unwrap nested config such as {"nested": {"a_nested_dict": 1.0}}.
+    """
+    if isinstance(element, Mapping):
+        # Case where the value is another dict (a nested dict)
+        unwrapped_nested_params = {}
+        for key, value in element.items():
+            # We recursively open the element (Dict format type)
+            unwrapped_nested_params.update(
+                _unwrap_nested_config(f"{parent_name}.{key}", value)
+            )
+        return unwrapped_nested_params
+    elif isinstance(element, Sequence) and not isinstance(element, str):
+        # Case where the value is a list
+        # Since str are sequence we negate it to be logged in the next else
+        unwrapped_nested_params = {}
+        for idx, value in enumerate(element):
+            unwrapped_nested_params.update(
+                _unwrap_nested_config(f"{parent_name}.{idx}", value)
+            )
+        return unwrapped_nested_params
+    else:
+        return {parent_name: element}
