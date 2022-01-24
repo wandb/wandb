28cecd38aa56b3077bf694bee6b6099c55a0f876
Dmitry Duev <dmitry.duev@gmail.com>
2022-01-24 10:21:30 -0800

    fix linting

--- wandb/sdk/wandb_settings.py
+++ wandb/sdk/wandb_settings.py
@@ -45,8 +45,8 @@ elif sys.version_info >= (3, 7):
     from typing_extensions import get_args, get_origin, get_type_hints
 else:
 
-    def get_args(obj: Any) -> Optional[Tuple[type]]:
-        return obj.__args__ if hasattr(obj, "__args__") else tuple()
+    def get_args(obj: Any) -> Optional[Any]:
+        return obj.__args__ if hasattr(obj, "__args__") else None
 
     def get_origin(obj: Any) -> Optional[Any]:
         return obj.__origin__ if hasattr(obj, "__origin__") else None
