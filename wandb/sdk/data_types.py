f4a137ad022fd9c04434da9c430ac4a5782070df
Dmitry Duev <dmitry.duev@gmail.com>
2022-01-26 11:13:12 -0800

    fix docstring formatting

--- wandb/sdk/data_types.py
+++ wandb/sdk/data_types.py
@@ -860,7 +860,8 @@ class Molecule(BatchableMedia):
     Arguments:
         data_or_path: (string, io)
             Molecule can be initialized from a file name or an io object.
-        caption: (string) caption associated with the molecule for display
+        caption: (string)
+            Caption associated with the molecule for display.
     """
 
     SUPPORTED_TYPES = {
@@ -942,11 +943,11 @@ class Molecule(BatchableMedia):
             data_or_path: (string, rdkit.Chem.rdchem.Mol)
                 Molecule can be initialized from a file name or an rdkit.Chem.rdchem.Mol object.
             caption: (string)
-                Caption associated with the molecule for display
-            convert_to_3d_and_optimize: bool
+                Caption associated with the molecule for display.
+            convert_to_3d_and_optimize: (bool)
                 Convert to rdkit.Chem.rdchem.Mol with 3D coordinates.
                 This is an expensive operation that may take a long time for complicated molecules.
-            mmff_optimize_molecule_max_iterations: int
+            mmff_optimize_molecule_max_iterations: (int)
                 Number of iterations to use in rdkit.Chem.AllChem.MMFFOptimizeMolecule
         """
         rdkit_chem = util.get_module(
