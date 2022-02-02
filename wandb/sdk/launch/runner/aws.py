3725f4f2f7dd2a79d62bc8756e34f1f9b1712ec2
Kyle Goyette <kdgoyette@gmail.com>
2022-02-02 00:57:51 -0800

    wip

--- wandb/sdk/launch/runner/aws.py
+++ wandb/sdk/launch/runner/aws.py
@@ -85,22 +85,32 @@ class AWSSagemakerRunner(AbstractRunner):
     def run(self, launch_project: LaunchProject) -> Optional[AbstractRun]:
         _logger.info("using AWSSagemakerRunner")
         boto3 = get_module("boto3", "AWSSagemakerRunner requires boto3 to be installed")
+
         validate_docker_installation()
         if launch_project.resource_args.get("ecr_repo_name") is None:
             raise LaunchError(
-                "AWS Sagemaker jobs require an ecr repository name, set this using `--resource_args ecr_repo_name=<ecr_repo_name>`"
+                "AWS Sagemaker jobs require an ecr repository name, set this using `resource_args ecr_repo_name=<ecr_repo_name>`"
             )
-        if get_role_arn(launch_project.resource_args) is None:
+        if (
+            launch_project.resource_args.get("OutputDataConfig") is None
+            and launch_project.resource_args.get("output_data_config") is None
+        ):
             raise LaunchError(
-                "AWS sagemaker jobs, set this using `resource_args RoleArn=<role_arn>` or `resource_args role_arn=<role_arn>`"
-            )
-        if launch_project.resource_args.get("OutputDataConfig") is None:
-            raise LaunchError(
-                "AWS sagemaker jobs, set this using `resource_args OutputDataConfig=<output_data_config>`"
+                "AWS sagemaker jobs require an output config, set this using `resource_args OutputDataConfig=<output_data_config>`"
             )
         region = get_region(launch_project)
 
         access_key, secret_key = get_aws_credentials()
+
+        client = boto3.client(
+            "sts", aws_access_key_id=access_key, aws_secret_access_key=secret_key
+        )
+        account_id = client.get_caller_identity()["Account"]
+
+        if get_role_arn(launch_project.resource_args, account_id) is None:
+            raise LaunchError(
+                "AWS sagemaker jobs require a Sagemaker role, set this using `resource_args RoleArn=<role_arn>` or `resource_args role_arn=<role_arn>`"
+            )
 
         # if the user provided the image they want to use, use that, but warn it won't have swappable artifacts
         if (
@@ -158,10 +168,12 @@ class AWSSagemakerRunner(AbstractRunner):
             if not docker_image_exists(launch_project.base_image):
                 if generate_docker_base_image(launch_project, entry_cmd) is None:
                     raise LaunchError("Unable to build base image")
+                copy_code = False
             else:
                 wandb.termlog(
                     "Using existing base image: {}".format(launch_project.base_image)
                 )
+                copy_code = False
 
         command_args = []
 
@@ -224,6 +236,7 @@ class AWSSagemakerRunner(AbstractRunner):
                 )
                 return None
         _logger.info("Connecting to sagemaker client")
+
         sagemaker_client = boto3.client(
             "sagemaker",
             region_name=region,
@@ -236,7 +249,7 @@ class AWSSagemakerRunner(AbstractRunner):
             )
         )
 
-        sagemaker_args = build_sagemaker_args(launch_project, aws_tag)
+        sagemaker_args = build_sagemaker_args(launch_project, aws_tag, account_id)
         _logger.info(f"Launching sagemaker job with args: {sagemaker_args}")
         run = launch_sagemaker_job(launch_project, sagemaker_args, sagemaker_client)
         if self.backend_config[PROJECT_SYNCHRONOUS]:
@@ -290,7 +303,9 @@ def merge_aws_tag_with_algorithm_specification(
 
 
 def build_sagemaker_args(
-    launch_project: LaunchProject, aws_tag: Optional[str] = None
+    launch_project: LaunchProject,
+    aws_tag: Optional[str] = None,
+    account_id: Optional[str] = None,
 ) -> Dict[str, Any]:
     sagemaker_args = {}
     resource_args = launch_project.resource_args
@@ -311,10 +326,12 @@ def build_sagemaker_args(
     sagemaker_args["StoppingCondition"] = resource_args.get("StoppingCondition") or {
         "MaxRuntimeInSeconds": resource_args.get("MaxRuntimeInSeconds") or 3600
     }
-    output_data_config = resource_args.get("OutputDataConfig")
+    output_data_config = resource_args.get("OutputDataConfig") or resource_args.get(
+        "output_data_config"
+    )
 
     sagemaker_args["OutputDataConfig"] = output_data_config
-    sagemaker_args["RoleArn"] = get_role_arn(resource_args)
+    sagemaker_args["RoleArn"] = get_role_arn(resource_args, account_id)
     return sagemaker_args
 
 
@@ -376,6 +393,10 @@ def get_aws_credentials() -> Tuple[str, str]:
     return access_key, secret_key
 
 
-def get_role_arn(resource_args: Dict[str, Any]) -> Optional[str]:
+def get_role_arn(resource_args: Dict[str, Any], account_id: str) -> Optional[str]:
     role_arn = resource_args.get("RoleArn") or resource_args.get("role_arn")
-    return role_arn
+    if role_arn is None:
+        return None
+    if role_arn.startswith("arn:aws:iam::"):
+        return role_arn
+    return f"arn:aws:iam::{account_id}:role/{role_arn}"
