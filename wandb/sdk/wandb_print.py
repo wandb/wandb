214e43cfbfb131607ec2bca546a6a26b3fef57f0
Katia <katia@wandb.com>
2022-02-01 16:38:22 -0800

    use rich for format

--- wandb/sdk/wandb_print.py
+++ wandb/sdk/wandb_print.py
@@ -1,4 +1,3 @@
-from math import inf
 from .lib import ipython, proto_util, sparkline
 
 import click
@@ -7,10 +6,9 @@ import json
 import logging
 import numbers
 import os
-import platform
 import sys
 import time
-from typing import Any, Dict, Optional, Sequence, Union
+from typing import Any, Dict, Optional, List, Sequence, Union
 import wandb
 from wandb.proto.wandb_internal_pb2 import (
     CheckVersionResponse,
@@ -21,8 +19,11 @@ from wandb.proto.wandb_internal_pb2 import (
 
 logger = logging.getLogger("wandb")
 
-
-RUN_NAME_COLOR = "#cdcd00"
+from rich.console import Console
+from rich.table import Table
+from rich import box
+from rich.live import Live
+from rich._spinners import SPINNERS
 
 
 class Printer:
@@ -32,13 +33,113 @@ class Printer:
         self._info = []
         self._warnings = []
         self._errors = []
+        self._console = Console()
+
+    def _grid_html(self, rows: List[List[str]] = [], title: str = "") -> str:
+        def _row_html(row):
+            return "".join([f"<td>{cell}</td>" for cell in row])
+
+        grid = "".join([f"<tr>{_row_html(row)}</tr>" for row in rows])
+        grid = f"<h3>{title}</h3><br/>{grid}"
+
+        return f'<table class="wandb">{grid}</table>\n'
+
+    def _table_html(self, columns: List[str] = []) -> str:
+        # TODO change between table and grid
+        col_format = '<div class="wandb-col">{}</div>\n'
+        row_format = '<div class="wandb-row">{}</div>\n'
+        table = ipython.TABLE_STYLES
+        table += row_format.format(
+            "".join([col_format.format(column) for column in columns])
+        )
+        return table
+
+    def _grid_term(self, rows: List[List[str]], title: str) -> str:
+        grid = Table.grid()
+        if title:
+            grid.add_row("[bold blue]wandb[/]: ", f"[bold]{title}[/]")
+        for row in rows:
+            grid.add_row("[bold blue]wandb[/]: ", *row)
+        return grid
+
+    def _table_term(self, columns: List[str], rows: List[List[str]]) -> str:
+
+        table = Table(show_header=True, box=box.ROUNDED)
+        for column in columns:
+            table.add_column(column)
+        for row in rows:
+            table.add_row(*row)
+        return table
+
+    def bold(self, text: str) -> str:
+        if self._html:
+            return f'<span style="font-weight:bold">{text}</span>'
+        return f"[b]{text}[/]"
+
+    def files(self, text: str) -> str:
+        if self._html:
+            return f'<span style="color:magenta">{text}</span>'
+        return f"[bold magenta]{text}[/]"
+
+    def code(self, text: str) -> str:
+        if self._html:
+            return f"<code>{text}<code>"
+        # return f"[italic]`{text}`[/]"
+        return f"`{text}`"
+
+    def failure(self, text: str) -> str:
+        if self._html:
+            return f'<span style="color:red">{text}</span>'
+        return f"[red]{text}[/]"
+
+    def grid(self, rows: List[List[str]] = [], title: Optional[str] = None) -> str:
+        if self._html:
+            return self._grid_html(rows, title)
+        return self._grid_term(rows, title)
+
+    def link(self, link: str, text: Optional[str] = None) -> str:
+        if self._html:
+            return f'<a href="{link}" target="_blank">{text or link}</a>'
+        return f"[link={link}]{link}[/link]"
+
+    def name(self, text: str) -> str:
+        NAME_COLOR = "#cdcd00"
+        if self._html:
+            return f'<span style="color:{NAME_COLOR}">{text}</span>'
+        return f"[{NAME_COLOR}]{text}[/]"
+
+    def success(self, text: str) -> str:
+        if self._html:
+            return f'<span style="color:green">{text}</span>'
+        return f"[green]{text}[/]"
+
+    def table(self, columns: List[str] = [], rows: List[List[str]] = []) -> str:
+        if self._html:
+            return self._table_html(columns)
+        return self._table_term(columns, rows)
+
+    def _display_html(self, text: List[str]) -> None:
+        for string in text:
+            ipython.display_html(string)
+
+    def _display_term(self, text: List[str]) -> None:
+        for string in text:
+            if isinstance(string, Table):
+                self._console.print(string)
+            else:
+                self._console.print(
+                    "\n".join(
+                        [f"[bold blue]wandb[/]: {info}" for info in string.split("\n")]
+                    )
+                )
 
     def _display(self) -> None:
+
         if self._info:
             if self._html:
-                ipython.display_html("<br/>\n".join(self._info))
+                self._display_html(self._info)
             else:
-                wandb.termlog("\n".join(self._info))
+                self._display_term(self._info)
             self._info = []
 
         if self._warnings:
@@ -53,22 +154,30 @@ class Printer:
 class ProgressPrinter:
     def __init__(self, html: Optional[bool] = None) -> None:
         self._jupyter_progress = ipython.jupyter_progress_bar() if html else None
-        self._term_progress = itertools.cycle(["-", "\\", "|", "/"])
+        self._spinner = itertools.cycle(SPINNERS["dots9"]["frames"])
         self._html = html
+
+        self._term_progress = None
 
     def update(self, line, percent_done=None) -> None:
         if self._html:
             if self._jupyter_progress:
                 self._jupyter_progress.update(percent_done, line)
         else:
-            wandb.termlog(f"{next(self._term_progress)} {line}", newline=False)
+            line = f"[bold blue]wandb[/bold blue]: {next(self._spinner)} {line}"
+            if self._term_progress is None:
+                self._term_progress = Live(line)
+                self._term_progress.start()
+            else:
+                self._term_progress.update(line)
 
     def close(self) -> None:
         if self._html:
             if self._jupyter_progress:
                 self._jupyter_progress.close()
         else:
-            wandb.termlog(" " * 79)
+            self._term_progress.update("[bold blue]wandb[/bold blue]:")
+            self._term_progress.stop()
 
 
 class PrinterManager:
@@ -192,15 +301,10 @@ class PrinterManager:
         if quiet or not self._settings["_offline"]:
             return
 
-        self._info.append("You can sync this run to the cloud by running:")
-        if self._html:
-            self._printer._info.append(
-                f'<code> wandb sync {self._settings["sync_dir"]}<code>'
-            )
-        else:
-            self._printer._info.append(
-                click.style(f'wandb sync {self._settings["sync_dir"]}', fg="yellow")
-            )
+        self._printer._info.append("You can sync this run to the cloud by running:")
+        self._printer._info.append(
+            self._printer.code(f'wandb sync {self._settings["sync_dir"]}')
+        )
 
     def _append_sync_dir_info(self):
 
@@ -208,13 +312,13 @@ class PrinterManager:
             return
 
         sync_dir = self._settings["sync_dir"]
-        format_str = "Run data is saved locally in {}"
-        if self._html:
-            self._printer._info.append(format_str.format(f"<code>{sync_dir}</code>"))
-        else:
-            self._printer._info.append(format_str.format(sync_dir))
-            if not self._settings["_offline"]:
-                self._printer._info.append("Run `wandb offline` to turn off syncing.")
+        self._printer._info.append(
+            f"Run data is saved locally in {self._printer.files(sync_dir)}"
+        )
+        if not self._html and not self._settings["_offline"]:
+            self._printer._info.append(
+                f'Run {self._printer.code("wandb offline")} to turn off syncing.'
+            )
 
     def _append_file_sync_info(self) -> None:
 
@@ -235,14 +339,9 @@ class PrinterManager:
             return
 
         run_name = self._run_obj.display_name
-
-        if self._html:
-            run_name = f'<strong style="color:{RUN_NAME_COLOR}">{run_name}</strong>'
-            run_url = f'<a href="{run_url}" target="_blank">{run_url}</a>'
-        else:
-            run_name = click.style(run_name, fg="yellow")
-            run_url = click.style(run_url, fg="blue")
-        self._printer._info.append(f"Synced {run_name}: {run_url}")
+        self._printer._info.append(
+            f"Synced {self._printer.name(run_name)}: {self._printer.link(run_url, run_name)}"
+        )
 
     def _append_run_info(self, project_url, run_url, sweep_url,) -> None:
 
@@ -255,46 +354,36 @@ class PrinterManager:
         if self._html:
             if not wandb.jupyter.maybe_display():
 
-                run_line = f'<strong><a href="{run_url}" target="_blank">{run_name}</a></strong>'
+                run_line = f"<span>{self._printer.link(run_url, run_name)}</span>"
                 project_line, sweep_line = "", ""
 
                 if not wandb.jupyter.quiet():  # TODO: make settings the source of truth
 
-                    doc_html = (
-                        f'<a href="https://wandb.me/run" target="_blank">docs</a>'
-                    )
-                    project_html = (
-                        f'<a href="{project_url}" target="_blank">Weights & Biases</a>'
-                    )
+                    doc_html = self._printer.link("https://wandb.me/run", "docs")
+                    project_html = self._printer.link(project_url, "Weights & Biases")
                     project_line = f"to {project_html} ({doc_html})"
 
                     if sweep_url:
-                        sweep_line = f'Sweep page: <a href="{sweep_url}" target="_blank">{sweep_url}</a><br/>\n'
+                        sweep_line = f"Sweep page: {self._printer.link(sweep_url)}\n"
 
                 self._printer._info.append(
-                    f"{run_state_str} {run_line} {project_line}<br/>\n{sweep_line}"
+                    f"{run_state_str} {run_line} {project_line}\n{sweep_line}"
                 )
 
         else:
-            emojis = dict(star="", broom="", rocket="")
-            if platform.system() != "Windows" and wandb.util.is_unicode_safe(
-                sys.stdout
-            ):
-                emojis = dict(star="⭐️", broom="🧹", rocket="🚀")
-
             self._printer._info.append(
-                f'{run_state_str} {click.style(run_name, fg="yellow")}'
+                f"{run_state_str} {self._printer.name(run_name)}"
             )
             if not self._settings["_quiet"]:
                 self._printer._info.append(
-                    f'{emojis["star"]} View project at {click.style(project_url, underline=True, fg="blue")}'
+                    f":star: View project at {self._printer.link(project_url)}"
                 )
                 if sweep_url:
                     self._printer._info.append(
-                        f'{emojis["broom"]} View sweep at {click.style(sweep_url, underline=True, fg="blue")}'
+                        f":broom: View sweep at {self._printer.link(sweep_url)}"
                     )
             self._printer._info.append(
-                f'{emojis["rocket"]} View run at {click.style(run_url, underline=True, fg="blue")}'
+                f":rocket: View run at {self._printer.link(run_url)}"
             )
 
             api = wandb.apis.internal.Api()
@@ -309,24 +398,17 @@ class PrinterManager:
 
         info = ["Waiting for W&B process to finish..."]
         if not exit_code:
-            status = "(success)."
-            if self._html:
-                status = f'<strong style="color:green">{status}</strong>'
-            info.append(status)
+            info.append(self._printer.success("(success)"))
         else:
-            status = f"(failed {exit_code})."
-            if self._html:
-                status = f'<strong style="color:red">{status}</strong>'
-            info.append(status)
+            info.append(self._printer.failure(f"(failed {exit_code})"))
             if not self._settings["_offline"]:
                 info.append("Press ctrl-c to abort syncing.")
 
-        sep = "<br/>" if not quiet and self._html else ""
-        self._printer._info.append(f'{sep}{" ".join(info)}')
+        self._printer._info.append(f'{" ".join(info)}')
         self._printer._display()
 
     def _pusher_print_status(
-        self, progress: FilePusherStats, done: Optional[bool] = False,
+        self, progress: FilePusherStats, done: Optional[bool] = False
     ) -> None:
 
         if self._settings["_offline"]:
@@ -342,6 +424,7 @@ class PrinterManager:
         )
 
         self._printer._progress.update(line, percent_done)
+
         if done:
             self._printer._progress.close()
 
@@ -357,6 +440,7 @@ class PrinterManager:
             self._printer._display()
 
     def _wait_for_finish(self, interface) -> PollExitResponse:
+
         while True:
             if interface:
                 poll_exit_resp = interface.communicate_poll_exit()
@@ -371,7 +455,7 @@ class PrinterManager:
                     return poll_exit_resp
             time.sleep(0.1)
 
-    def _render_history_info(self, as_html: bool = False) -> Optional[str]:
+    def _render_history_info(self) -> Optional[str]:
         if not self._sampled_history:
             return
 
@@ -380,7 +464,7 @@ class PrinterManager:
             return
 
         logger.info("rendering history")
-        max_len, history_rows = 0, []
+        history_rows = []
         for key, values in sorted(self._sampled_history.items()):
             if key.startswith("_"):
                 continue
@@ -390,84 +474,45 @@ class PrinterManager:
             ):
                 continue
             history_rows.append((key, sparkline.sparkify(downsampled_values)))
-            max_len = max(max_len, len(key))
         if not history_rows:
             return
-        if as_html:
-            history_title = "<h3>Run history:</h3>"
-            history_table = "".join(
-                ["<tr><td>{}</td><td>{}</td></tr>".format(*row) for row in history_rows]
-            )
-            history_table = f'<table class="wandb">{history_table}</table>'
-            return f"{history_title}<br/>{history_table}<br/>"
-        else:
-            history_title = "Run history:"
-            history_table = "\n".join(
-                [("  {:>%s} {}" % max_len).format(*row) for row in history_rows]
-            )
-            history_table = f"{history_table.rstrip()}\n"
-            return f"{history_title}\n{history_table}\n"
+        return self._printer.grid(history_rows, "Run History")
 
-    def _render_summary_info(self, as_html: bool = False) -> Optional[str]:
+    def _render_summary_info(self) -> Optional[str]:
         if not self._final_summary:
             return
 
         logger.info("rendering summary")
-        max_len, summary_rows = 0, []
+        summary_rows = []
         for key, value in sorted(self._final_summary.items()):
             # arrays etc. might be too large. for now we just don't print them
             if key.startswith("_"):
                 continue
             if isinstance(value, str):
-                value = value[:20] + "..." * (len(value) >= 20)
-                summary_rows.append((key, value))
+                summary_rows.append((key, value[:20] + "..." * (len(value) >= 20)))
             elif isinstance(value, numbers.Number):
-                summary_rows.append((key, round(value, 5)))
+                summary_rows.append((key, str(round(value, 5))))
             else:
                 continue
-            max_len = max(max_len, len(key))
         if not summary_rows:
             return
-        if as_html:
-            summary_table = "".join(
-                ["<tr><td>{}</td><td>{}</td></tr>".format(*row) for row in summary_rows]
-            )
-            summary_table = f'<table class="wandb">{summary_table}</table>\n'
-            return f"<h3>Run summary:</h3><br/>{summary_table}"
-        else:
-            summary_table = "\n".join(
-                [("  {:>%s} {}" % max_len).format(*row) for row in summary_rows]
-            )
-            return f"Run summary:\n{summary_table}\n"
+        return self._printer.grid(summary_rows, "Run Summary")
 
     def _append_history_summary_info(self, quiet) -> str:
 
         if quiet:
             return
 
-        as_html = self._html
+        history = self._render_history_info()
+        summary = self._render_summary_info()
 
-        history = self._render_history_info(as_html)
-        summary = self._render_summary_info(as_html)
-
-        if not history and not summary:
-            return
-
-        if as_html:
-            col_format = '<div class="wandb-col">{}</div>\n'
-            row_format = '<div class="wandb-row">{}</div>\n'
-            table_style = ipython.TABLE_STYLES
+        if not self._html:
+            if history:
+                self._printer._info.extend([history, "\n"])
+            if summary:
+                self._printer._info.extend([summary, "\n"])
         else:
-            col_format = row_format = "{}"
-            table_style = ""
-
-        row = ""
-        if history:
-            row += col_format.format(history)
-        if summary:
-            row += col_format.format(summary)
-
-        self._printer._info.append(table_style + row_format.format(row))
+            self._printer._info.append(self._printer.table([history, summary]))
 
     def _append_local_warning(self) -> None:
         if not self._poll_exit_response or not self._poll_exit_response.local_info:
@@ -478,7 +523,7 @@ class PrinterManager:
             latest_version, out_of_date = local_info.version, local_info.out_of_date
             if out_of_date:
                 self._printer._warnings.append(
-                    f"Upgrade to the {latest_version} version of W&B Local to get the latest features. Learn more: http://wandb.me/local-upgrade"
+                    f"Upgrade to the {latest_version} version of W&B Local to get the latest features. Learn more: {self._printer.link('http://wandb.me/local-upgrade')}"
                 )
 
     def _append_reporter_info(self, quiet) -> None:
@@ -510,9 +555,4 @@ class PrinterManager:
 
         log_dir = os.path.dirname(log_dir.replace(os.getcwd(), "."))
 
-        if self._html:
-            log_dir = f"<code>{log_dir}</code><br/>\n"
-        else:
-            log_dir = f"{log_dir}\n"
-
-        self._printer._info.append(f"Find logs at: {log_dir}")
+        self._printer._info.append(f"Find logs at: {self._printer.files(log_dir)}")
