9a5a7e715c93921a482165190cf36d2dbed13bab
Dmitry Duev <dmitry.duev@gmail.com>
2022-02-16 12:59:29 -0800

    comb test

--- functional_tests/catboost/test_catboost.py
+++ functional_tests/catboost/test_catboost.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 """Test CatBoost integration."""
 
-import catboost
-from catboost import datasets
+from catboost import CatBoostClassifier, datasets, Pool
 import wandb
 from wandb.catboost import log_summary, WandbCallback
 
 train_df, _ = datasets.msrank_10k()
 X, Y = train_df[train_df.columns[1:]], train_df[train_df.columns[0]]
-pool = catboost.Pool(
+pool = Pool(
     data=X[:1000],
     label=Y[:1000],
-    feature_names=list(X.columns)
+    feature_names=list(X.columns),
 )
 
-cls = catboost.CatBoostClassifier(depth=2, random_seed=0, iterations=10, verbose=False)
+classifier = CatBoostClassifier(depth=2, random_seed=0, iterations=10, verbose=False)
 
-wandb.init(project='catboost-test')
-cls.fit(pool, callbacks=[WandbCallback()])
-log_summary(cls, save_model_checkpoint=True)
+wandb.init(project="catboost-test")
+classifier.fit(pool, callbacks=[WandbCallback()])
+log_summary(classifier, save_model_checkpoint=True)
