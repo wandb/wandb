adf2451f74ee3dc018918563c7a9a14738fa9c9c
Katia <katia@wandb.com>
2022-02-17 13:36:49 -0800

    format

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -467,8 +467,7 @@ class Run:
 
     def _telemetry_imports(self, imp: telemetry.TelemetryImports) -> None:
         telem_map = dict(
-            pytorch_ignite="ignite",
-            transformers_huggingface="transformers",
+            pytorch_ignite="ignite", transformers_huggingface="transformers",
         )
 
         # calculate mod_map, a mapping from module_name to telem_name
@@ -1733,8 +1732,7 @@ class Run:
                 )
                 logger.info(f"got exit ret: {self._poll_exit_response}")
                 self._footer_file_pusher_status_info(
-                    self._poll_exit_response,
-                    printer=self._printer,
+                    self._poll_exit_response, printer=self._printer,
                 )
             time.sleep(0.1)
 
@@ -1763,15 +1761,10 @@ class Run:
         )
 
     def _save_job_spec(self) -> None:
-        envdict = dict(
-            python="python3.6",
-            requirements=[],
-        )
+        envdict = dict(python="python3.6", requirements=[],)
         varsdict = {"WANDB_DISABLE_CODE": "True"}
         source = dict(
-            git="git@github.com:wandb/examples.git",
-            branch="master",
-            commit="bbd8d23",
+            git="git@github.com:wandb/examples.git", branch="master", commit="bbd8d23",
         )
         execdict = dict(
             program="train.py",
@@ -1780,13 +1773,8 @@ class Run:
             args=[],
         )
         configdict = (dict(self._config),)
-        artifactsdict = dict(
-            dataset="v1",
-        )
-        inputdict = dict(
-            config=configdict,
-            artifacts=artifactsdict,
-        )
+        artifactsdict = dict(dataset="v1",)
+        inputdict = dict(config=configdict, artifacts=artifactsdict,)
         job_spec = {
             "kind": "WandbJob",
             "version": "v0",
@@ -1923,8 +1911,8 @@ class Run:
                 f"Could not find {artifact_name} in launch artifact mapping. Searching for unique artifacts with sequence name: {artifact_name}"
             )
             sequence_name = artifact_name.split(":")[0].split("/")[-1]
-            unique_artifact_replacement_info = (
-                self._unique_launch_artifact_sequence_names.get(sequence_name)
+            unique_artifact_replacement_info = self._unique_launch_artifact_sequence_names.get(
+                sequence_name
             )
             if unique_artifact_replacement_info is not None:
                 new_name = unique_artifact_replacement_info.get("name")
@@ -2000,8 +1988,7 @@ class Run:
                 )
             artifact._use_as = use_as or artifact_or_name
             api.use_artifact(
-                artifact.id,
-                use_as=use_as or artifact_or_name,
+                artifact.id, use_as=use_as or artifact_or_name,
             )
             return artifact
         else:
@@ -2397,9 +2384,7 @@ class Run:
 
     @staticmethod
     def _header_wandb_version_info(
-        *,
-        settings: "Settings",
-        printer: Union["PrinterTerm", "PrinterJupyter"],
+        *, settings: "Settings", printer: Union["PrinterTerm", "PrinterJupyter"],
     ) -> None:
         if settings.quiet or settings.silent:
             return
@@ -2409,9 +2394,7 @@ class Run:
 
     @staticmethod
     def _header_sync_info(
-        *,
-        settings: "Settings",
-        printer: Union["PrinterTerm", "PrinterJupyter"],
+        *, settings: "Settings", printer: Union["PrinterTerm", "PrinterJupyter"],
     ) -> None:
 
         # printer = printer or get_printer(settings._jupyter)
@@ -2432,9 +2415,7 @@ class Run:
 
     @staticmethod
     def _header_run_info(
-        *,
-        settings: "Settings",
-        printer: Union["PrinterTerm", "PrinterJupyter"],
+        *, settings: "Settings", printer: Union["PrinterTerm", "PrinterJupyter"],
     ) -> None:
 
         if settings._offline or settings.silent:
@@ -2737,9 +2718,7 @@ class Run:
         if log_dir:
             # printer = printer or get_printer(settings._jupyter)
             log_dir = os.path.dirname(log_dir.replace(os.getcwd(), "."))
-            printer.display(
-                f"Find logs at: {printer.files(log_dir)}",
-            )
+            printer.display(f"Find logs at: {printer.files(log_dir)}",)
 
     @staticmethod
     def _footer_history_summary_info(
@@ -2777,10 +2756,7 @@ class Run:
                 if sparkline:
                     history_rows.append([key, sparkline])
             if history_rows:
-                history_grid = printer.grid(
-                    history_rows,
-                    "Run history:",
-                )
+                history_grid = printer.grid(history_rows, "Run history:",)
                 panel.append(history_grid)
 
         # Render summary if available
@@ -2805,10 +2781,7 @@ class Run:
                     continue
 
             if summary_rows:
-                summary_grid = printer.grid(
-                    summary_rows,
-                    "Run summary:",
-                )
+                summary_grid = printer.grid(summary_rows, "Run summary:",)
                 panel.append(summary_grid)
 
         if panel:
