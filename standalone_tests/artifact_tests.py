db33e142c68fa80affd306bf5a0589fd71137d55
Katia <katia@wandb.com>
2022-02-18 14:21:43 -0800

    revert artifact file

--- standalone_tests/artifact_tests.py
+++ standalone_tests/artifact_tests.py
@@ -4,7 +4,6 @@ import time
 import shutil
 import os
 
-
 def teardown():
     wandb.finish()
     if os.path.isdir("wandb"):
@@ -12,18 +11,15 @@ def teardown():
     if os.path.isdir("artifacts"):
         shutil.rmtree("artifacts")
 
-
 def _run_eq(run_a, run_b):
     return (
-        run_a.id == run_b.id
-        and run_a.entity == run_b.entity
-        and run_a.project == run_b.project
+        run_a.id == run_b.id and
+        run_a.entity == run_b.entity and
+        run_a.project == run_b.project
     )
-
 
 def _runs_eq(runs_a, runs_b):
     return all([_run_eq(run_a, run_b) for run_a, run_b in zip(runs_a, runs_b)])
-
 
 def test_artifact_run_lookup_apis():
     artifact_1_name = "a1-{}".format(str(time.time()))
@@ -31,17 +27,17 @@ def test_artifact_run_lookup_apis():
 
     # Initial setup
     run_1 = wandb.init()
-    artifact = wandb.Artifact(artifact_1_name, "test_type")
+    artifact = wandb.Artifact(artifact_1_name, "test_type");
     artifact.add(wandb.Image(np.random.randint(0, 255, (10, 10))), "image")
     run_1.log_artifact(artifact)
-    artifact = wandb.Artifact(artifact_2_name, "test_type")
+    artifact = wandb.Artifact(artifact_2_name, "test_type");
     artifact.add(wandb.Image(np.random.randint(0, 255, (10, 10))), "image")
     run_1.log_artifact(artifact)
     run_1.finish()
 
     # Create a second version for a1
     run_2 = wandb.init()
-    artifact = wandb.Artifact(artifact_1_name, "test_type")
+    artifact = wandb.Artifact(artifact_1_name, "test_type");
     artifact.add(wandb.Image(np.random.randint(0, 255, (10, 10))), "image")
     run_2.log_artifact(artifact)
     run_2.finish()
@@ -64,7 +60,6 @@ def test_artifact_run_lookup_apis():
     assert _runs_eq(a2.used_by(), [run_3, run_4])
     run_4.finish()
 
-
 def test_artifact_creation_with_diff_type():
     artifact_name = "a1-{}".format(str(time.time()))
 
@@ -79,7 +74,7 @@ def test_artifact_creation_with_diff_type():
         artifact = wandb.Artifact(artifact_name, "artifact_type_1")
         artifact.add(wandb.Image(np.random.randint(0, 255, (10, 10))), "image")
         run.log_artifact(artifact)
-
+    
     # invalid
     with wandb.init() as run:
         artifact = wandb.Artifact(artifact_name, "artifact_type_2")
@@ -89,12 +84,9 @@ def test_artifact_creation_with_diff_type():
             run.log_artifact(artifact)
         except ValueError as err:
             did_err = True
-            assert (
-                str(err)
-                == "Expected artifact type artifact_type_1, got artifact_type_2"
-            )
+            assert str(err) == "Expected artifact type artifact_type_1, got artifact_type_2"
         assert did_err
-
+        
     with wandb.init() as run:
         artifact = run.use_artifact(artifact_name + ":latest")
         # should work
@@ -103,7 +95,6 @@ def test_artifact_creation_with_diff_type():
         # should not work
         image_2 = artifact.get("image_2")
         assert image_2 is None
-
 
 if __name__ == "__main__":
     try:
