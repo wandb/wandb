cc86b2249decbf4a76fd4f8d50bd3cd0a3889207
morg <morganmcg1@users.noreply.github.com>
2022-02-21 23:02:41 +0000

    added more libs in batch4

--- functional_tests/imports/04-batch4.py
+++ functional_tests/imports/04-batch4.py
@@ -17,48 +17,46 @@ assert:
   - :wandb:runs[0][summary]:
       loss: 1
   - :wandb:runs[0][exitcode]: 0
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 28  # pycaret
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 10  # ignite
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 30  # pytorchvideo
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 9  # pytorch_lightning
   - :op:contains:
     - :wandb:runs[0][telemetry][1]  # imports init
-    - 31  # ray
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 5  # sklearn
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 32  # simpletransformers
-  # - :op:contains:
-  #   - :wandb:runs[0][telemetry][1]  # imports init
-  #   - 33  # skorch
+    - 28  # pycaret
   - :op:contains:
     - :wandb:runs[0][telemetry][1]  # imports init
-    - 34  # spacy
+    - 10  # ignite
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 29  # pytorchvideo
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 9  # pytorch_lightning
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 30  # ray
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 5  # sklearn
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 31  # simpletransformers
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 32  # skorch
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 33  # spacy
 """
 
 # # import paddle
 # # import parlai
 # # import prophet
-# import pycaret  # noqa: F401
-
-# import ignite
-# import pytorchvideo  # noqa: F401
-# import pytorch_lightning  # noqa: F401
+import pycaret  # noqa: F401
+import ignite
+import pytorchvideo  # noqa: F401
+import pytorch_lightning  # noqa: F401
 import ray  # noqa: F401
-
-# import sklearn  # noqa: F401
-# import simpletransformers  # noqa: F401
-# import skorch  # noqa: F401
+import sklearn  # noqa: F401
+import simpletransformers  # noqa: F401
+import skorch  # noqa: F401
 import spacy  # noqa: F401
 import wandb
 
