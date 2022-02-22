7445379d3615828a8c4709f7c341ff330139a28f
Andrew Truong <itsandrewtruong@gmail.com>
2022-02-22 13:57:57 -0500

    Expose SageMakerImporter

--- wandb/importers/__init__.py
+++ wandb/importers/__init__.py
@@ -1,4 +1,5 @@
 from .base import AbstractRun, Importer
 from .wandb import WandbImporter
+from .sagemaker import SageMakerImporter
 
-__all__ = [AbstractRun, Importer, WandbImporter]
+__all__ = [AbstractRun, Importer, WandbImporter, SageMakerImporter]
