f43fc35f42be7fb2e6d5c0c280c14a4a121fc8d1
Jeff Raubitschek <jeff@wandb.com>
2022-02-25 18:12:25 -0800

    add tensorboard test

---
+++ functional_tests/keras/test_keras_tensorboard.py
@@ -0,0 +1,34 @@
+import numpy as np
+import tensorflow as tf
+import wandb
+from wandb.keras import WandbCallback
+from tensorflow.keras.callbacks import TensorBoard
+
+wandb.tensorboard.patch(root_logdir="logs")
+run = wandb.init(project="keras")
+
+x = np.random.randint(255, size=(100, 28, 28, 1))
+y = np.random.randint(10, size=(100,))
+
+dataset = (x, y)
+
+
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(3, 3, activation="relu", input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(10, activation="softmax"))
+model.compile(
+    loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["accuracy"]
+)
+
+model.fit(
+    x,
+    y,
+    epochs=2,
+    validation_data=(x, y),
+    callbacks=[WandbCallback(input_type="image"),
+        TensorBoard(log_dir="logs")
+        ],
+)
+
+run.finish()
