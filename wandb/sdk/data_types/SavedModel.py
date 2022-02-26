8202c19c58f3e083d72816834cda4ab04c17ecee
Tim <tim.s.sweeney@gmail.com>
2022-02-26 14:06:30 -0800

    re-wrote to use adapter design

--- wandb/sdk/data_types/SavedModel.py
+++ wandb/sdk/data_types/SavedModel.py
@@ -1,7 +1,6 @@
 import os
 from typing import Any, ClassVar, Dict, List, Optional, Type, TYPE_CHECKING, Union
 
-import six
 from wandb import util
 
 from . import _dtypes
@@ -21,117 +20,116 @@ if TYPE_CHECKING:  # pragma: no cover
     ModelPathType = Union[ModelFilePathType, ModelDirPathType]
     ModelType = Union[ModelPathType, ModelObjType]
 
-    RegisteredSerializerMapType = Dict[str, "_IModelSerializer"]
+    RegisteredAdaptersMapType = Dict[str, Type["_IModelAdapter"]]
 
 
-class _SerializerRegistry(object):
-    _registered_serializers: ClassVar[Optional[RegisteredSerializerMapType]] = None
+class _ModelAdapterRegistry(object):
+    _registered_adapters: ClassVar[Optional[RegisteredAdaptersMapType]] = None
 
     @staticmethod
-    def register_serializer(serializer: "_IModelSerializer") -> None:
-        serializers = _SerializerRegistry.registered_serializers()
-        if serializer.serializer_id in serializers:
+    def register_adapter(adapter: Type["_IModelAdapter"]) -> None:
+        adapters = _ModelAdapterRegistry.registered_adapters()
+        if adapter.adapter_id in adapters:
             raise ValueError(
-                "Cannot add serializer with id {}, already exists".format(
-                    serializer.serializer_id
+                "Cannot add adapter with id {}, already exists".format(
+                    adapter.adapter_id
                 )
             )
-        serializers[serializer.serializer_id] = serializer
+        adapters[adapter.adapter_id] = adapter
 
     @staticmethod
-    def registered_serializers() -> RegisteredSerializerMapType:
-        if _SerializerRegistry._registered_serializers is None:
-            _SerializerRegistry._registered_serializers = {}
-        return _SerializerRegistry._registered_serializers
+    def registered_adapters() -> RegisteredAdaptersMapType:
+        if _ModelAdapterRegistry._registered_adapters is None:
+            _ModelAdapterRegistry._registered_adapters = {}
+        return _ModelAdapterRegistry._registered_adapters
 
     @staticmethod
-    def load_serializer(serializer_id: str) -> "_IModelSerializer":
-        selected_serializer = _SerializerRegistry.registered_serializers()[
-            serializer_id
+    def load_adapter(adapter_id: str) -> Type["_IModelAdapter"]:
+        selected_adapter = _ModelAdapterRegistry.registered_adapters()[
+            adapter_id
         ]
-        if selected_serializer is None:
-            raise ValueError(f"Serializer {serializer_id} not registered")
-        return selected_serializer
+        if selected_adapter is None:
+            raise ValueError(f"adapter {adapter_id} not registered")
+        return selected_adapter
 
     @staticmethod
     def handles_model_or_path(
-        serializer: "_IModelSerializer", model_or_path: ModelType,
+        adapter: Type["_IModelAdapter"], model_or_path: ModelType,
     ) -> bool:
         possible = False
         if _is_path(model_or_path):
-            possible = serializer.can_load_path(model_or_path)
+            possible = adapter.can_load_path(model_or_path)
         else:
-            possible = serializer.can_save_model(model_or_path)
+            possible = adapter.can_adapt_model(model_or_path)
         return possible
 
     @staticmethod
-    def find_suitable_serializer(
+    def find_suitable_adapter(
         model_or_path: ModelType,
-    ) -> Optional["_IModelSerializer"]:
-        serializers = _SerializerRegistry.registered_serializers()
-        for key in serializers:
-            serializer = serializers[key]
-            if _SerializerRegistry.handles_model_or_path(serializer, model_or_path):
-                return serializer
+    ) -> Optional[Type["_IModelAdapter"]]:
+        adapters = _ModelAdapterRegistry.registered_adapters()
+        for key in adapters:
+            adapter = adapters[key]
+            if _ModelAdapterRegistry.handles_model_or_path(adapter, model_or_path):
+                return adapter
         return None
 
 
 class SavedModel(Media):
     _log_type: ClassVar[str] = "saved-model"
 
-    _serializer: "_IModelSerializer"
-    _model_spec: "_ModelSpec"
-    _model_obj: Optional[ModelObjType]
+    _adapter_cls: Type["_IModelAdapter"]
+    _adapter: Optional["_IModelAdapter"]
     _path: Optional[str]
 
     def __init__(
         self,
         model_or_path: ModelType,
-        serializer_id: Optional[str] = None,
-        _model_spec: Optional["_ModelSpec"] = None,
+        adapter_id: Optional[str] = None,
     ) -> None:
         super(SavedModel, self).__init__()
-        if serializer_id is None:
-            selected_serializer = _SerializerRegistry.find_suitable_serializer(
+        if adapter_id is None:
+            selected_adapter_cls = _ModelAdapterRegistry.find_suitable_adapter(
                 model_or_path
             )
-            if selected_serializer is None:
-                raise ValueError("No suitable serializer found for model")
+            if selected_adapter_cls is None:
+                raise ValueError("No suitable adapter found for model")
         else:
-            selected_serializer = _SerializerRegistry.load_serializer(serializer_id)
-            if not _SerializerRegistry.handles_model_or_path(
-                selected_serializer, model_or_path
+            selected_adapter_cls = _ModelAdapterRegistry.load_adapter(adapter_id)
+            if not _ModelAdapterRegistry.handles_model_or_path(
+                selected_adapter_cls, model_or_path
             ):
                 raise ValueError(
-                    f"Serializer {selected_serializer} cannot load or save selected model"
+                    f"adapter {selected_adapter_cls} cannot load or save selected model"
                 )
 
-        self._serializer = selected_serializer
+        self._adapter_cls = selected_adapter_cls
         if _is_path(model_or_path):
             # TODO: make media support a directory path
             self._set_file(model_or_path)
         else:
+            # We immediately write the file(s) in case the user modifies the model
+            # after creating the SavedModel (ie. continues training)
+            self._adapter = selected_adapter_cls(model_or_path)
             tmp_path = os.path.join(MEDIA_TMP.name, str(util.generate_id()))
-            selected_serializer.save_model(model_or_path, tmp_path)
-            self._model_obj = model_or_path
+            self.adapter.save_model(tmp_path)
             # TODO: make media support a directory path (this is going to be non-trivial)
             self._set_file(tmp_path, is_tmp=True)
 
-        if _model_spec is None:
-            self._model_spec = self._serializer.get_model_specs(self.raw_model)
+    @property
+    def adapter(self) -> "_IModelAdapter":
+        if self._adapter is None:
+            self._adapter = self._adapter_cls.init_from_path(self._path)
+        return self._adapter
 
     @property
-    def raw_model(self) -> ModelObjType:
-        if self._model_obj is None:
-            if self._path is None:
-                raise ValueError("No model to load")
-            self._model_obj = self._serializer.load_path(self._path)
-        return self._model_obj
+    def raw(self) -> ModelObjType:
+        return self.adapter.raw()
 
     def to_json(self, run_or_artifact: Union["LocalRun", "LocalArtifact"]) -> dict:
         json_obj = super(SavedModel, self).to_json(run_or_artifact)
-        json_obj["serializer_id"] = self._serializer.serializer_id
-        json_obj["model_spec"] = self._model_spec.to_json()
+        json_obj["adapter_id"] = self.adapter.adapter_id
+        json_obj["model_spec"] = self.adapter.model_spec.to_json()
         return json_obj
 
     @classmethod
@@ -141,13 +139,13 @@ class SavedModel(Media):
         # TODO: make download support directories
         return cls(
             source_artifact.get_path(json_obj["path"]).download(),
-            json_obj["serializer_id"],
-            json_obj["model_spec"],
+            json_obj["adapter_id"],
+            # purposely ignoring the model spec - it can be re-calculated from the model
         )
 
 
 def _is_path(model_or_path: ModelType) -> bool:
-    return isinstance(model_or_path, six.string_types) and os.path.exists(model_or_path)
+    return isinstance(model_or_path, str) and os.path.exists(model_or_path)
 
 
 class _ModelSpec(object):
@@ -169,32 +167,39 @@ class _ModelSpec(object):
             "_outputs": self._outputs,
         }
 
+from typing import TypeVar, Generic
 
-class _IModelSerializer(object):
-    serializer_id: str
+ModelObjectType = TypeVar('ModelObjectType')
+class _IModelAdapter(Generic[ModelObjectType]):
+    adapter_id: str
+    _internal_model: ModelObjType
+
+    def __init__(self, model: ModelObjectType) -> None:
+        self._internal_model = model
+
+    @classmethod
+    def init_from_path(cls: Type["_IModelAdapter"], dir_or_file_path: ModelPathType) -> "_IModelAdapter":
+        raise NotImplementedError()
 
     @staticmethod
     def can_load_path(dir_or_file_path: ModelPathType) -> bool:
         raise NotImplementedError()
 
     @staticmethod
-    def load_path(dir_or_file_path: ModelPathType) -> ModelObjType:
+    def can_adapt_model(obj: Any) -> bool:
         raise NotImplementedError()
 
-    @staticmethod
-    def can_save_model(obj: ModelObjType) -> bool:
+    def save_model(self, dir_path: ModelDirPathType) -> None:
         raise NotImplementedError()
 
-    @staticmethod
-    def save_model(obj: ModelObjType, dir_path: ModelDirPathType) -> None:
+    def model_spec(self) -> _ModelSpec:
         raise NotImplementedError()
 
-    @staticmethod
-    def get_model_specs(obj: ModelObjType) -> _ModelSpec:
-        raise NotImplementedError()
+    def raw(self) -> ModelObjectType:
+        return self._internal_model
 
 
-# TODO: Implement the basic serializers
+# TODO: Implement the basic adapters
 
 
 # Note: Thinking about how the type system works now, I think
@@ -205,15 +210,15 @@ class _SavedModelType(_dtypes.Type):
     name = "saved-model"
     types = [SavedModel]
 
-    def __init__(self, serializer_id: str, model_spec: Dict[str, Any]) -> None:
-        self.params.update({"serializer_id": serializer_id, "model_spec": model_spec})
+    def __init__(self, adapter_id: str, model_spec: Dict[str, Any]) -> None:
+        self.params.update({"adapter_id": adapter_id, "model_spec": model_spec})
 
     @classmethod
     def from_obj(cls, py_obj: Optional[Any] = None) -> "_SavedModelType":
         if not isinstance(py_obj, SavedModel):
             raise TypeError("py_obj must be a SavedModel")
         else:
-            return cls(py_obj._serializer.serializer_id, py_obj._model_spec.to_json())
+            return cls(py_obj._adapter.adapter_id, py_obj._model_spec.to_json())
 
 
 _dtypes.TypeRegistry.add(_SavedModelType)
