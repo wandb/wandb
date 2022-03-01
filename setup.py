0b1245de1b9567574cfe60d14efa8a4e47be63ce
Jeff Raubitschek <jeff@jhr-mbp.lan>
2022-02-28 21:53:42 -0800

    Bump version: 0.12.11.dev1 → 0.12.11

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ launch_requirements = [
 
 setup(
     name="wandb",
-    version="0.12.11.dev1",
+    version="0.12.11",
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     long_description_content_type="text/markdown",
