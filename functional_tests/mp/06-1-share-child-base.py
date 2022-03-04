6bbf660b5cb5d7e0f1926323b88aa66da534f01f
Katia <katia@wandb.com>
2022-03-04 14:38:41 -0800

    improve share child testing

---
+++ functional_tests/mp/06-1-share-child-base.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""Test parent and child processes sharing a run.
+Compare to a run in a single process, base usage of `run.log`"""
+import multiprocessing as mp
+
+import wandb
+import yea
+
+
+def process_parent():
+    run = wandb.init()
+    assert run == wandb.run
+
+    run.config.c1 = 11
+    run.log({"s1": 11})
+    return run
+
+
+def process_child(run):
+    run.config.c2 = 22
+    run.log({"s1": 21})
+
+
+def refernce_run():
+    run = process_parent()
+    process_child(run)
+    run.finish()
+
+
+def share_run():
+    run = process_parent()
+    p = mp.Process(target=process_child, kwargs=dict(run=run))
+    p.start()
+    p.join()
+    run.finish()
+
+
+def main():
+    wandb.require("service")
+
+    refernce_run()
+    share_run()
+
+
+if __name__ == "__main__":
+    yea.setup()  # Use ":yea:start_method:" to set mp.set_start_method()
+    main()
---
+++ functional_tests/mp/06-2-share-child-gt-step.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""Test parent and child processes sharing a run. Compare to a run in a single process.
+example usage of `run.log` with user provide step greater than the internal step"""
+
+
+from contextlib import redirect_stderr
+import io
+import multiprocessing as mp
+
+import wandb
+import yea
+
+
+def process_child(run):
+    f = io.StringIO()
+    run.config.c2 = 22
+    with redirect_stderr(f):
+        run.log({"s1": 210}, step=12, commit=True)
+        assert (
+            "Note that setting step in multiprocessing can result in data loss. Please log your step values as a metric such as 'global_step'"
+            in f.getvalue()
+        )
+    print(f.getvalue())
+
+
+def process_parent(run):
+    assert run == wandb.run
+    run.config.c1 = 11
+    run.log({"s1": 11})
+
+
+def share_run():
+    with wandb.init() as run:
+        process_parent(run)
+        # Start a new run in parallel in a child process
+        p = mp.Process(target=process_child, kwargs=dict(run=run))
+        p.start()
+        p.join()
+
+
+def refernce_run():
+    with wandb.init() as run:
+        process_parent(run)
+        process_child(run=run)
+
+
+def main():
+    wandb.require("service")
+
+    refernce_run()
+
+    share_run()
+
+
+if __name__ == "__main__":
+    yea.setup()  # Use ":yea:start_method:" to set mp.set_start_method()
+    main()
