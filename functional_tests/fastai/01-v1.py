ce7e28f7615bb0f9e9f2214754984bc426bbd302
Dmitry Duev <dmitry.duev@gmail.com>
2022-03-07 00:32:25 -0800

    fastai v1 test

--- functional_tests/fastai/01-v1.py
+++ functional_tests/fastai/01-v1.py
@@ -3,11 +3,9 @@ import wandb
 from wandb.fastai import WandbCallback
 
 
-path = untar_data(URLs.MNIST_SAMPLE)  # noqa: F405
-data = ImageDataBunch.from_folder(path)  # noqa: F405
-model = simple_cnn((3, 16, 16, 2))  # noqa: F405
-
 wandb.init()
 
-learn = cnn_learner(data, model, callback_fns=WandbCallback)  # noqa: F405
-learn.fit(2)
+path = untar_data(URLs.MNIST_SAMPLE)  # noqa: F405
+data = ImageDataBunch.from_folder(path)  # noqa: F405
+learn = cnn_learner(data, models.resnet18, metrics=accuracy, callback_fns=WandbCallback)  # noqa: F405
+learn.fit(1)
