76e713cf9b1a9d60525f41716e1238789bd0f593
Kyle Goyette <kdgoyette@gmail.com>
2022-03-10 11:48:51 -0800

    clean up run

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -452,25 +452,8 @@ class Run:
                     wandb.termwarn("Malformed WANDB_CONFIG, using original config")
             if os.environ.get("WANDB_ARTIFACTS") is not None:
                 try:
-                    artifacts = ast.literal_eval(os.environ.get("WANDB_ARTIFACTS"))
-                    for key, item in artifacts.items():
-                        self._launch_artifact_mapping[key] = item
-                        artifact_sequence_tuple_or_slot = key.split(":")
-
-                        if len(artifact_sequence_tuple_or_slot) == 2:
-                            sequence_name = artifact_sequence_tuple_or_slot[0].split("/")[
-                                -1
-                            ]
-                            if self._unique_launch_artifact_sequence_names.get(
-                                sequence_name
-                            ):
-                                self._unique_launch_artifact_sequence_names.pop(
-                                    sequence_name
-                                )
-                            else:
-                                self._unique_launch_artifact_sequence_names[
-                                    sequence_name
-                                ] = item
+                    artifacts: Dict[str, Any] = ast.literal_eval(os.environ.get("WANDB_ARTIFACTS"))
+                    self._initialize_launch_artifact_maps(artifacts)
                 except Exception:
                     pass
 
@@ -483,26 +466,8 @@ class Run:
             with open(self._settings.launch_config_path) as fp:
                 launch_config = json.loads(fp.read())
             if launch_config.get("overrides", {}).get("artifacts") is not None:
-                for key, item in (
-                    launch_config.get("overrides").get("artifacts").items()
-                ):
-                    self._launch_artifact_mapping[key] = item
-                    artifact_sequence_tuple_or_slot = key.split(":")
-
-                    if len(artifact_sequence_tuple_or_slot) == 2:
-                        sequence_name = artifact_sequence_tuple_or_slot[0].split("/")[
-                            -1
-                        ]
-                        if self._unique_launch_artifact_sequence_names.get(
-                            sequence_name
-                        ):
-                            self._unique_launch_artifact_sequence_names.pop(
-                                sequence_name
-                            )
-                        else:
-                            self._unique_launch_artifact_sequence_names[
-                                sequence_name
-                            ] = item
+                artifacts = launch_config.get("overrides").get("artifacts")
+                self._initialize_launch_artifact_maps(artifacts)
 
             launch_run_config = launch_config.get("overrides", {}).get("run_config")
             if launch_run_config:
@@ -532,6 +497,26 @@ class Run:
 
     def _set_iface_port(self, iface_port: int) -> None:
         self._iface_port = iface_port
+
+    def _initialize_launch_artifact_maps(self, artifacts: Dict[str, Any]) -> None:
+        for key, item in artifacts.items():
+            self._launch_artifact_mapping[key] = item
+            artifact_sequence_tuple_or_slot = key.split(":")
+
+            if len(artifact_sequence_tuple_or_slot) == 2:
+                sequence_name = artifact_sequence_tuple_or_slot[0].split("/")[
+                    -1
+                ]
+                if self._unique_launch_artifact_sequence_names.get(
+                    sequence_name
+                ):
+                    self._unique_launch_artifact_sequence_names.pop(
+                        sequence_name
+                    )
+                else:
+                    self._unique_launch_artifact_sequence_names[
+                        sequence_name
+                    ] = item
 
     def _telemetry_callback(self, telem_obj: telemetry.TelemetryRecord) -> None:
         self._telemetry_obj.MergeFrom(telem_obj)
