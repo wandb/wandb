c9a3d2344b07ab3d244827d01a047e08a74e574d
Dmitry Duev <dmitry.duev@gmail.com>
2022-03-11 13:08:37 -0800

    fix linting

--- wandb/integration/tensorboard/__init__.py
+++ wandb/integration/tensorboard/__init__.py
@@ -2,7 +2,7 @@
 wandb integration tensorboard module.
 """
 
-from .log import _log, log, reset_state, tf_summary_to_dict
+from .log import _log, log, reset_state, tf_summary_to_dict  # noqa: F401
 from .monkeypatch import patch, unpatch
 
 __all__ = [
