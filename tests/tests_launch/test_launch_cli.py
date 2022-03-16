82541aefaa54290f1e0ed918e880747a55b67a2a
Kyle Goyette <kdgoyette@gmail.com>
2022-03-16 15:46:48 -0700

    format

--- tests/tests_launch/test_launch_cli.py
+++ tests/tests_launch/test_launch_cli.py
@@ -91,12 +91,7 @@ def test_agent_failed_default_create(runner, test_settings, live_mock_server):
         live_mock_server.set_ctx({"successfully_create_default_queue": False})
         live_mock_server.set_ctx({"run_queues_return_default": False})
         result = runner.invoke(
-            cli.launch_agent,
-            [
-                "test_project",
-                "--entity",
-                "mock_server_entity",
-            ],
+            cli.launch_agent, ["test_project", "--entity", "mock_server_entity",],
         )
         assert result.exit_code != 0
 
@@ -105,12 +100,7 @@ def test_agent_update_failed(runner, test_settings, live_mock_server):
     live_mock_server.set_ctx({"launch_agent_update_fail": True})
     with runner.isolated_filesystem():
         result = runner.invoke(
-            cli.launch_agent,
-            [
-                "test_project",
-                "--entity",
-                "mock_server_entity",
-            ],
+            cli.launch_agent, ["test_project", "--entity", "mock_server_entity",],
         )
 
         assert "Failed to update agent status" in result.output
@@ -128,12 +118,7 @@ def test_agent_stop_polling(runner, live_mock_server, monkeypatch):
     live_mock_server.set_ctx({"stop_launch_agent": True})
     with runner.isolated_filesystem():
         result = runner.invoke(
-            cli.launch_agent,
-            [
-                "test_project",
-                "--entity",
-                "mock_server_entity",
-            ],
+            cli.launch_agent, ["test_project", "--entity", "mock_server_entity",],
         )
 
     assert "Shutting down, active jobs" in result.output
@@ -155,8 +140,7 @@ def test_launch_cli_with_config_file_and_params(
     with runner.isolated_filesystem():
         with open("config.json", "w") as fp:
             json.dump(
-                config,
-                fp,
+                config, fp,
             )
 
         result = runner.invoke(
@@ -200,15 +184,11 @@ def test_launch_cli_with_config_and_params(
 
 
 def test_launch_no_docker_exec(
-    runner,
-    monkeypatch,
-    mocked_fetchable_git_repo,
-    test_settings,
+    runner, monkeypatch, mocked_fetchable_git_repo, test_settings,
 ):
     monkeypatch.setattr(wandb.sdk.launch.docker, "find_executable", lambda name: False)
     result = runner.invoke(
-        cli.launch,
-        ["https://wandb.ai/mock_server_entity/test_project/runs/1"],
+        cli.launch, ["https://wandb.ai/mock_server_entity/test_project/runs/1"],
     )
     assert result.exit_code == 1
     assert "Could not find Docker executable" in str(result.exception)
@@ -219,11 +199,7 @@ def test_launch_github_url(runner, mocked_fetchable_git_repo, live_mock_server):
     with runner.isolated_filesystem():
         result = runner.invoke(
             cli.launch,
-            [
-                "https://github.com/test/repo.git",
-                "--entry-point",
-                "train.py",
-            ],
+            ["https://github.com/test/repo.git", "--entry-point", "train.py",],
         )
     assert result.exit_code == 0
     assert "Launching run in docker with command: docker run" in result.output
@@ -237,10 +213,7 @@ def test_launch_local_dir(runner):
             f.write('print("ok")\n')
         with open("repo/requirements.txt", "w+") as f:
             f.write("wandb\n")
-        result = runner.invoke(
-            cli.launch,
-            ["repo"],
-        )
+        result = runner.invoke(cli.launch, ["repo"],)
 
     assert result.exit_code == 0
     assert "Launching run in docker with command: docker run" in result.output
@@ -297,24 +270,15 @@ def test_launch_cuda_flag(runner, live_mock_server, mocked_fetchable_git_repo):
         "train.py",
     ]
     with runner.isolated_filesystem():
-        result = runner.invoke(
-            cli.launch,
-            args + ["--cuda"],
-        )
+        result = runner.invoke(cli.launch, args + ["--cuda"],)
     assert result.exit_code == 0
 
     with runner.isolated_filesystem():
-        result = runner.invoke(
-            cli.launch,
-            args + ["--cuda", "False"],
-        )
+        result = runner.invoke(cli.launch, args + ["--cuda", "False"],)
     assert result.exit_code == 0
 
     with runner.isolated_filesystem():
-        result = runner.invoke(
-            cli.launch,
-            args + ["--cuda", "asdf"],
-        )
+        result = runner.invoke(cli.launch, args + ["--cuda", "asdf"],)
     assert result.exit_code != 0
     assert "Invalid value for --cuda:" in result.output
 
