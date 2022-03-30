39131737280c5cbbadc2d68e3a1cacf5e7b9175e
Katia <katia@wandb.com>
2022-03-30 13:17:28 -0700

    add test

--- functional_tests/mp/21-1-joblib-parallel.py
+++ functional_tests/mp/21-1-joblib-parallel.py
@@ -1,17 +1,23 @@
+# based on issue https://wandb.atlassian.net/browse/CLI-548
+
 from joblib import Parallel, delayed
-from math import sqrtx
+from math import sqrt
 import wandb
 
+
 def f(x):
-    with wandb.init(project="symppl", reinit=True) as run:
-        for i in range(10):
-            #Log metrics with wandb
-            run.log({"Loss": i})
+    with wandb.init() as run:
+        run.config.x = x
+        for i in range(3):
+            # Log metrics with wandb
+            run.log({"i": i * x})
     return sqrt(x)
 
+
 def main():
-    res = Parallel(n_jobs=2)(delayed(f)(i**2) for i in range(4))
+    res = Parallel(n_jobs=2)(delayed(f)(i ** 2) for i in range(4))
     print(res)
+
 
 if __name__ == "__main__":
     wandb.require("service")
