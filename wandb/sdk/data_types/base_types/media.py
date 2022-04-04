6eff0505904111297915cb5569acb89f51e6a56f
Tim <tim.s.sweeney@gmail.com>
2022-04-04 12:01:22 -0700

    removed incorrect artifact target

--- wandb/sdk/data_types/base_types/media.py
+++ wandb/sdk/data_types/base_types/media.py
@@ -240,8 +240,6 @@ class Media(WBValue):
                             self._path, name=name, is_tmp=self._is_tmp
                         )
                         name = entry.path
-                        if self._artifact_target is None:
-                            self._set_artifact_target(artifact, name)
 
                 json_obj["path"] = name
                 json_obj["sha256"] = self._sha256
