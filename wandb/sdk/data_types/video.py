936612258fd7f4331dd5262ddf3b7c0d29fa05c2
Tim <tim.s.sweeney@gmail.com>
2022-04-04 15:21:04 -0700

    clean 3

--- wandb/sdk/data_types/video.py
+++ wandb/sdk/data_types/video.py
@@ -27,10 +27,7 @@ if TYPE_CHECKING:  # pragma: no cover
 # See https://github.com/Zulko/moviepy/blob/7e3e8bb1b739eb6d1c0784b0cb2594b587b93b39/moviepy/video/io/gif_writers.py#L428
 #
 # Except, we close the writer!
-def write_gif_with_image_io(
-    clip: any, filename: str, fps=None
-):
-    """Writes the gif with the Python library ImageIO (calls FreeImage)."""
+def write_gif_with_image_io(clip: any, filename: str, fps=None):
     imageio = util.get_module(
         "imageio",
         required='wandb.Video requires imageio when passing raw data. Install with "pip install imageio"',
