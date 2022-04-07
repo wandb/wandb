a2824a24287d77689facc04660454b71bf66604c
Dmitry Duev <dmitry.duev@gmail.com>
2022-04-06 18:46:20 -0700

    make test more deterministic

--- functional_tests/login/03-login-cloud-local-key.py
+++ functional_tests/login/03-login-cloud-local-key.py
@@ -9,18 +9,13 @@ import wandb
 import wandb.errors
 
 
-# function to generate a random alphanumeric key of 40 chars
-def random_key():
-    return "".join(random.choice(string.ascii_letters + string.digits) for _ in range(40))
-
-
 if __name__ == "__main__":
     # api_key starts with "local", but base_url points to cloud
     with pytest.raises(wandb.errors.UsageError) as e:
-        wandb.login(key=f"local-{random_key()}")
+        wandb.login(key="local-87eLxjoRhY6u2ofg63NAJo7rVYHZo4NGACOvpSsF}")
         assert (
             "Attempting to use a local API key to connect to https://api.wandb.ai" in str(e.value)
         )
 
     # check that this logic does not apply if base_url is not cloud
-    assert wandb.login(key=f"local-{random_key()}", host="https://api.wandb.test")
+    assert wandb.login(key=f"local-87eLxjoRhY6u2ofg63NAJo7rVYHZo4NGACOvpSsF", host="https://api.wandb.test")
