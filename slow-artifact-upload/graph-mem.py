19c79b581103c2620e106d07e4d1ce68b0bf3157
Spencer Pearson <spencer.pearson@wandb.com>
2022-04-14 10:00:06 -0700

    graph-mem.py: plot step alongside memory usage

--- slow-artifact-upload/graph-mem.py
+++ slow-artifact-upload/graph-mem.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt, json
 def main(path):
     header, *js = [json.loads(l) for l in open(path)]
     plt.plot(*zip(*((j["elapsed"], j["mem_used"] / 1e9) for j in js if j.get('mem_used') is not None)), label="GB used")
+    avg_memgb_per_step = (
+        max(j['mem_used'] for j in js if j.get('mem_used') is not None)
+        / max(j['cur_step'] for j in js if j.get('cur_step') is not None)
+    ) / 1e9
+    plt.plot(*zip(*((j["elapsed"], j["cur_step"]*avg_memgb_per_step) for j in js if j.get('cur_step') is not None)), label=f"step (scaled to {avg_memgb_per_step*1e6:.01f}kB/step)")
     plt.legend()
     plt.xlabel("time elapsed")
     plt.ylabel("GB")
