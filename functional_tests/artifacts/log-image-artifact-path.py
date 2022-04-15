5cda92fbb10ce6be36a6700fb7d8f0ea30b8da58
Tim <tim.s.sweeney@gmail.com>
2022-04-15 15:25:22 -0700

    yeah test

---
+++ functional_tests/artifacts/log-image-artifact-path.py
@@ -0,0 +1,24 @@
+import wandb
+import numpy as np
+
+
+def make_image():
+    return wandb.Image(np.random.randint(255, size=(32,32)))
+
+
+def main():
+    # Base Case
+    with wandb.init() as run:
+        run.log({"image": make_image()})
+
+    # With Logged Target
+    with wandb.init() as run:
+        art = wandb.Artifact("examples", "images")
+        image = make_image()
+        art.add(image, "image")
+        run.log_artifact(art)
+        run.log({"image": image})
+
+
+if __name__ == "__main__":
+    main()
