6df00981d49995c59774e71a47da53a0d6aefde2
Hu Po <hugo.ponte@wandb.com>
2022-04-18 16:04:19 -0500

    consolidate is_dict function for readability

--- wandb/sdk/wandb_helper.py
+++ wandb/sdk/wandb_helper.py
@@ -80,7 +80,7 @@ def _unflatten_dict(d: Dict, delimiter: str) -> None:
 
 
 def parse_config(
-    params: Union[Dict, str],
+    params: Union[Dict, types.ModuleType, str],
     exclude: List[str] = None,
     include: List[str] = None,
 ) -> Dict:
@@ -89,46 +89,40 @@ def parse_config(
         raise UsageError("Expected at most only one of exclude or include")
     if isinstance(params, str):
         params = config_util.dict_from_config_file(params, must_exist=True)
-    params = _to_dict(params)
+
+    if not isinstance(params, dict):
+        # Handle some cases where params is not a dictionary
+        # by trying to convert it into a dictionary
+        meta = inspect.getmodule(params)
+        if meta:
+            is_tf_flags_module = (
+                isinstance(params, types.ModuleType)
+                and meta.__name__ == "tensorflow.python.platform.flags"  # noqa: W503
+            )
+            if is_tf_flags_module or meta.__name__ == "absl.flags":
+                params = params.FLAGS
+                meta = inspect.getmodule(params)
+
+        # newer tensorflow flags (post 1.4) uses absl.flags
+        if meta and meta.__name__ == "absl.flags._flagvalues":
+            params = {name: params[name].value for name in dir(params)}
+        elif "__flags" in vars(params):
+            # for older tensorflow flags (pre 1.4)
+            if not "__parsed" not in vars(params):
+                params._parse_flags()
+            params = vars(params)["__flags"]
+        elif not hasattr(params, "__dict__"):
+            raise TypeError("config must be a dict or have a __dict__ attribute.")
+        else:
+            # params is a Namespace object (argparse)
+            # or something else
+            params = vars(params)
+
+    # Filter items based on exclude/include
     if include:
         params = {key: value for key, value in params.items() if key in include}
     if exclude:
         params = {key: value for key, value in params.items() if key not in exclude}
     # Un-nest any nested dicts in the params
     params = unnest_config(params)
-    return params
-
-
-def _to_dict(params):
-    if isinstance(params, dict):
-        return params
-
-    # Handle some cases where params is not a dictionary
-    # by trying to convert it into a dictionary
-    meta = inspect.getmodule(params)
-    if meta:
-        is_tf_flags_module = (
-            isinstance(params, types.ModuleType)
-            and meta.__name__ == "tensorflow.python.platform.flags"  # noqa: W503
-        )
-        if is_tf_flags_module or meta.__name__ == "absl.flags":
-            params = params.FLAGS
-            meta = inspect.getmodule(params)
-
-    # newer tensorflow flags (post 1.4) uses absl.flags
-    if meta and meta.__name__ == "absl.flags._flagvalues":
-        params = {name: params[name].value for name in dir(params)}
-    elif "__flags" in vars(params):
-        # for older tensorflow flags (pre 1.4)
-        if not "__parsed" not in vars(params):
-            params._parse_flags()
-        params = vars(params)["__flags"]
-    elif not hasattr(params, "__dict__"):
-        raise TypeError("config must be a dict or have a __dict__ attribute.")
-    else:
-        # params is a Namespace object (argparse)
-        # or something else
-        params = vars(params)
-
-    # assume argparse Namespace
     return params
