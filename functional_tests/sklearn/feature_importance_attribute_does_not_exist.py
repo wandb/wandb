ad243c921ca15f5a7517b5c7b909a72a390d3b8f
Katia <katia@wandb.com>
2022-04-21 19:34:16 -0700

    new line

--- functional_tests/sklearn/feature_importance_attribute_does_not_exist.py
+++ functional_tests/sklearn/feature_importance_attribute_does_not_exist.py
@@ -21,6 +21,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import wandb
+
 wandb.init("my-scikit-integration")
 
 wbcd = wisconsin_breast_cancer_data = datasets.load_breast_cancer()
