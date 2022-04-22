3e4055438f917c5b6d5ad07bd7017a708f6e75da
Katia <katia@wandb.com>
2022-04-22 01:10:18 -0700

    more double finish

--- tests/test_save_policies.py
+++ tests/test_save_policies.py
@@ -2,7 +2,6 @@ import os
 import pytest
 import time
 
-import wandb
 from wandb.filesync.dir_watcher import PolicyLive
 
 
@@ -11,7 +10,6 @@ def mocked_live_policy(monkeypatch, wandb_init_run):
     fpath = os.path.join(wandb_init_run.dir, "test_file")
     with open(fpath, "w") as fp:
         fp.write("")
-        fp.close()
 
     livePolicy = PolicyLive(fpath, "saved_file", None, None)
 
@@ -31,7 +29,7 @@ def mocked_live_policy(monkeypatch, wandb_init_run):
     yield livePolicy
 
 
-def test_policy_on_modified(monkeypatch, wandb_init_run, mocked_live_policy):
+def test_policy_on_modified(monkeypatch, mocked_live_policy):
     # policy does not save empty files
     mocked_live_policy.on_modified()
     curr_time = time.time()
@@ -44,11 +42,9 @@ def test_policy_on_modified(monkeypatch, wandb_init_run, mocked_live_policy):
     time.sleep(1.1)
     with open(mocked_live_policy.file_path, "w") as fp:
         fp.write("a" * 1000)
-        fp.close()
     mocked_live_policy.on_modified()
     # policy saves a file if enough time has passed
     assert mocked_live_policy._last_uploaded_time > curr_time
-    wandb.finish()
 
 
 def test_policy_on_modified_rate_limited(mocked_live_policy):
@@ -56,31 +52,25 @@ def test_policy_on_modified_rate_limited(mocked_live_policy):
     first_upload_time = mocked_live_policy._last_uploaded_time
     with open(mocked_live_policy.file_path, "w") as fp:
         fp.write("a" * 1000)
-        fp.close()
     # policy does not save a file if not enough time has passed
     mocked_live_policy.on_modified()
     assert mocked_live_policy._last_uploaded_time == first_upload_time
     assert mocked_live_policy._last_uploaded_size == 0
-    wandb.finish()
 
 
 def test_policy_on_modified_size_rate_limited(mocked_live_policy):
     with open(mocked_live_policy.file_path, "w") as fp:
         fp.write("a" * 10)
-        fp.close()
     mocked_live_policy.save_file()
     first_upload_time = mocked_live_policy._last_uploaded_time
     time.sleep(1.1)
     with open(mocked_live_policy.file_path, "w") as fp:
         fp.write("a" * 11)
-        fp.close()
     # policy does not save a file if file change size not large enough
     mocked_live_policy.on_modified()
     assert mocked_live_policy._last_uploaded_time == first_upload_time
     assert mocked_live_policy._last_uploaded_size == 10
-    wandb.finish()
 
 
 def test_live_policy_policy(mocked_live_policy):
     assert mocked_live_policy.policy == "live"
-    wandb.finish()
