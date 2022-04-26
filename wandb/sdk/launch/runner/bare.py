1a76f8abae614b0f1b2c3cd112e7a645b17b5684
Hu Po <hugo.ponte@wandb.com>
2022-04-26 09:06:12 -0500

    bare runner based on local runner

---
+++ wandb/sdk/launch/runner/bare.py
@@ -0,0 +1,63 @@
+import logging
+import shlex
+from typing import Any, List, Optional
+
+import wandb
+
+from .abstract import AbstractRun, AbstractRunner
+from .local import _run_entry_point
+from .._project_spec import get_entry_point_command, LaunchProject
+from ..builder.build import get_env_vars_dict
+from ..utils import (
+    PROJECT_SYNCHRONOUS,
+    sanitize_wandb_api_key,
+)
+
+
+_logger = logging.getLogger(__name__)
+
+
+class BareRunner(AbstractRunner):
+    """Runner class, uses a project to create a LocallySubmittedRun.
+    
+    BareRunner is very similar to a LocalRunner, except it does not
+    run the command inside a docker container. Instead, it runs the
+    command specified directly on the BARE machine.
+    
+    """
+
+    def run(
+        self,
+        launch_project: LaunchProject,
+        *args, **kwargs,
+    ) -> Optional[AbstractRun]:
+        if args is not None:
+            _logger.warning(f"BareRunner.run received unused args {args}")
+        if kwargs is not None:
+            _logger.warning(f"BareRunner.run received unused kwargs {kwargs}")
+
+        synchronous: bool = self.backend_config[PROJECT_SYNCHRONOUS]
+        entry_point = launch_project.get_single_entry_point()
+        
+        cmd: List[Any] = []
+
+        env_vars = get_env_vars_dict(launch_project, self._api)
+        for env_key, env_value in env_vars.items():
+            cmd += ["-e", f"{shlex.quote(env_key)}={shlex.quote(env_value)}"]
+
+        if not self.ack_run_queue_item(launch_project):
+            return None
+
+        entry_cmd = get_entry_point_command(entry_point, launch_project.override_args)
+        cmd += [entry_cmd]
+
+        command_str = " ".join(cmd).strip()
+        wandb.termlog(
+            "Launching run in docker with command: {}".format(
+                sanitize_wandb_api_key(command_str)
+            )
+        )
+        run = _run_entry_point(command_str, launch_project.project_dir)
+        if synchronous:
+            run.wait()
+        return run
