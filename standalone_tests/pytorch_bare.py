c3752f203ca77d28652b8852481414e8da8ab52f
dmitryduev <dima@wandb.com>
2022-05-03 00:45:11 -0700

    e2e w gpu

--- standalone_tests/pytorch_bare.py
+++ standalone_tests/pytorch_bare.py
@@ -1,3 +1,10 @@
 import torch
 
 assert torch.cuda.is_available(), "CUDA not available!"
+
+print("CUDA available:", torch.cuda.is_available())
+print("CUDA device count:", torch.cuda.device_count())
+print("CUDA current device:", torch.cuda.current_device())
+print("CUDA device name:", torch.cuda.get_device_name(0))
+print("CUDA device properties:", torch.cuda.get_device_properties(0))
+print("CUDA device memory:", torch.cuda.get_device_capability(0))
