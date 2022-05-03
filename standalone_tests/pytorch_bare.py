c4ab60dd80fa6a222400a708c5b5b652a9ea71c3
dmitryduev <dima@wandb.com>
2022-05-03 00:46:36 -0700

    e2e w gpu

--- standalone_tests/pytorch_bare.py
+++ standalone_tests/pytorch_bare.py
@@ -8,3 +8,5 @@ print("CUDA current device:", torch.cuda.current_device())
 print("CUDA device name:", torch.cuda.get_device_name(0))
 print("CUDA device properties:", torch.cuda.get_device_properties(0))
 print("CUDA device memory:", torch.cuda.get_device_capability(0))
+
+print("exit_code: 0")
