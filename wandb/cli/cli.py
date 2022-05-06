73c2ffc4888e20478243bb576412a8e0cbdfc7a6
Katia <katia@wandb.com>
2022-05-05 17:15:58 -0700

    revert change

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -1423,8 +1423,13 @@ def local(ctx, port, env, daemon, upgrade, edge):
     api = InternalApi()
     if not find_executable("docker"):
         raise ClickException("Docker not installed, install it from https://docker.com")
-    if upgrade:
-        subprocess.call(["docker", "pull", "wandb/local"])
+    if wandb.docker.image_id("wandb/local") != wandb.docker.image_id_from_registry("wandb/local"):
+        if upgrade:
+            subprocess.call(["docker", "pull", "wandb/local"])
+        else:
+            wandb.termlog(
+                "A new version of W&B local is available, upgrade by calling `wandb local --upgrade`"
+            )
     running = subprocess.check_output(
         ["docker", "ps", "--filter", "name=wandb-local", "--format", "{{.ID}}"]
     )
