697e0127c25d94efa1667291f4d0943f220eb0d2
Hugo <8493427+hu-po@users.noreply.github.com>
2022-05-12 10:16:09 -0500

    [WB-9277] BareRunner based on LocalRunner (#3577)
    * bare runner based on local runner
    
    * validate local python dependencies
    
    * run name from uri
    
    * pair programming with kyle
    
    * mock fixtures, requirements diff, testing
    
    * bugfix
    
    * launch utils tests
    
    * quick bugfix
    
    * clean up fixtures
    
    * more progress
    
    * Fix -e error
    
    * format
    
    * mypy is shit
    
    * more mypy
    
    * more mypy
    
    * more code cov
    
    * mypy
    
    * PR comments
    
    * raise Launch error on empty uri
    
    * dependcies isolated
    
    * env vars no longer assumes local default
    
    * mypy, flake8, pyupgrade, black

--- wandb/sdk/launch/runner/loader.py
+++ wandb/sdk/launch/runner/loader.py
@@ -1,37 +1,51 @@
 import logging
-from typing import Any, Dict, Type
+from typing import Any, Dict, List
 
 from wandb.apis.internal import Api
 from wandb.errors import LaunchError
 
 from .abstract import AbstractRunner
-from .aws import AWSSagemakerRunner
-from .gcp_vertex import VertexRunner
-from .kubernetes import KubernetesRunner
-from .local import LocalRunner
 
 
 __logger__ = logging.getLogger(__name__)
 
 
 # Statically register backend defined in wandb
-WANDB_RUNNERS: Dict[str, Type["AbstractRunner"]] = {
-    "local": LocalRunner,
-    "gcp-vertex": VertexRunner,
-    "sagemaker": AWSSagemakerRunner,
-    "kubernetes": KubernetesRunner,
-}
+WANDB_RUNNERS: List[str] = [
+    "local",
+    "bare",
+    "gcp-vertex",
+    "sagemaker",
+    "kubernetes",
+]
 
 
 def load_backend(
     backend_name: str, api: Api, backend_config: Dict[str, Any]
 ) -> AbstractRunner:
     # Static backends
-    if backend_name in WANDB_RUNNERS:
-        return WANDB_RUNNERS[backend_name](api, backend_config)
+    if backend_name == "local":
+        from .local import LocalRunner
 
+        return LocalRunner(api, backend_config)
+    elif backend_name == "bare":
+        from .bare import BareRunner
+
+        return BareRunner(api, backend_config)
+    elif backend_name == "gcp-vertex":
+        from .gcp_vertex import VertexRunner
+
+        return VertexRunner(api, backend_config)
+    elif backend_name == "sagemaker":
+        from .aws import AWSSagemakerRunner
+
+        return AWSSagemakerRunner(api, backend_config)
+    elif backend_name == "kubernetes":
+        from .kubernetes import KubernetesRunner
+
+        return KubernetesRunner(api, backend_config)
     raise LaunchError(
         "Resource name not among available resources. Available resources: {} ".format(
-            ",".join(list(WANDB_RUNNERS.keys()))
+            ",".join(WANDB_RUNNERS)
         )
     )
