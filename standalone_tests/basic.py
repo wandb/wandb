a33e620be07f0d386aee37a711e0b661f4de3650
dmitryduev <dima@wandb.com>
2022-05-13 16:40:03 -0700

    setup WANDB_PROJECT with tox; smarter cluster termination

--- standalone_tests/basic.py
+++ standalone_tests/basic.py
@@ -1,22 +1,8 @@
-import datetime
-import os
-
 import wandb
 
 
 def main():
-    project_base = os.environ.get(
-        "PROJECT_BASE",
-        "nightly"
-    )
-    time_stamp = os.environ.get(
-        "TEST_RUN_TIME_STAMP",
-        datetime.datetime.now().strftime("%Y%m%d"),
-    )
-    run = wandb.init(
-        project=f"{project_base}-{time_stamp}",
-        name=__file__,
-    )
+    run = wandb.init(name=__file__)
     run.log({"boom": 1})
     run.finish()
 
