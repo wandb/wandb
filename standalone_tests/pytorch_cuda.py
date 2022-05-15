5712dc85c1aa03e3bf41d558073e5ff2ca5dc069
dmitryduev <dima@wandb.com>
2022-05-15 15:31:03 -0700

    debug pytorch/gpu

--- standalone_tests/pytorch_cuda.py
+++ standalone_tests/pytorch_cuda.py
@@ -2,10 +2,9 @@ import numpy as np
 import torch
 import wandb
 
-assert torch.cuda.is_available(), "CUDA not available!"
-
 
 run = wandb.init(name=__file__)
+run.log({"cuda_available": torch.cuda.is_available()})
 x = np.random.random((32, 100)).astype("f")
 t_cpu = torch.Tensor(x)
 t_gpu = t_cpu.cuda()
