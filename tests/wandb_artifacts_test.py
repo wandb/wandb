8b9d628bf516587c8f5216f052c8fb6377a9c834
Danny Goldstein <dgold@berkeley.edu>
2022-05-24 20:18:10 -0700

    fix tests

--- tests/wandb_artifacts_test.py
+++ tests/wandb_artifacts_test.py
@@ -667,7 +667,7 @@ def test_artifact_table_deserialize_timestamp_column():
         assert [row[timestamp_idx] for row in table.data] == [
             datetime.fromtimestamp(row[timestamp_idx] / 1000.0, tz=timezone.utc)
             if row[timestamp_idx] is not None
-            else row
+            else None
             for row in art["data"]
         ]
 
