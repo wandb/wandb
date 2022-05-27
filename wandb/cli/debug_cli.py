c9e0a29a8b7bf521602b4ce60ba13812da06f5ee
Jeff Raubitschek <jeff@wandb.com>
2022-05-27 10:28:55 -0700

    update

--- wandb/cli/debug_cli.py
+++ wandb/cli/debug_cli.py
@@ -22,23 +22,25 @@ def list_services():
     return ret
 
 
-def list_runs(service=None):
-
+def get_manager(service=None):
     if not service:
         services = list_services()
         if not services:
             # no service, no runs
-            return []
+            return None
         # assume just one, handle later
         assert len(services) == 1
         service = services[0]
-
     os.environ["WANDB_SERVICE"] = service
     settings = dict()
     manager = wandb_manager._Manager(settings)
-
     manager._inform_connect()
+    return manager
 
+def list_runs(service=None):
+    manager = get_manager()
+    if not manager:
+        return []
     got = manager._inform_list()
     return got
 
@@ -51,22 +53,48 @@ def service():
 
 @click.command()
 def run():
-    print("run")
+    pass
 
 
 @click.command()
-def junk():
-    print("junk")
+@click.pass_context
+def run_subcommand(ctx):
+    # print("junk", vars(ctx))
+    run_id = ctx.info_name
+    manager = get_manager()
+    if not manager:
+        return
+
+    svc = manager._get_service()
+    assert svc
+    svc_iface = svc.service_interface
+
+    svc_transport = svc_iface.get_transport()
+
+    assert svc_transport == "tcp"
+    from wandb.sdk.interface.interface_sock import InterfaceSock
+
+    # svc_iface_sock = cast("ServiceSockInterface", svc_iface)
+    svc_iface_sock = svc_iface
+    sock_client = svc_iface_sock._get_sock_client()
+    sock_interface = InterfaceSock(sock_client)
+    sock_interface._stream_id = run_id
+    sock_interface.publish_debug("junk")
+    data = sock_interface.communicate_debug_poll("data")
+    data_list = data.traceback.split("\n")
+    for l in data_list:
+        print(l)
+
+
 
 class CliRuns(click.MultiCommand):
 
     def list_commands(self, ctx):
         runs = list_runs()
-        # return ["r1", "r2", "r3"]
         return runs
 
     def get_command(self, ctx, name):
-        return junk
+        return run_subcommand
 
 
 def install_subcommands(base):
