eb57b66b73ec2f8f5b7a031695ab654374939c08
paperspace <itsandrewtruong@gmail.com>
2022-06-05 14:49:58 +0000

    mypy fixes

--- wandb/sdk/wandb_require_helpers.py
+++ wandb/sdk/wandb_require_helpers.py
@@ -1,33 +1,36 @@
 from functools import wraps
 import os
+from typing import Any, Callable, TypeVar, cast
+
+FuncT = TypeVar("FuncT", bound=Callable[..., Any])
 
 requirement_env_var_mapping = {"report-editing:v0": "WANDB_REQUIRE_REPORT_EDITING_V0"}
 
 
-def requires(requirement):
+def requires(requirement: str) -> FuncT:
     """
     The decorator for gating features.
     """
     env_var = requirement_env_var_mapping[requirement]
 
-    def deco(func):
+    def deco(func: FuncT) -> FuncT:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             if not os.getenv(env_var):
                 raise Exception(
                     f'You need to enable this feature with `wandb.require("{requirement}")`'
                 )
             return func(*args, **kwargs)
 
-        return wrapper
+        return cast(FuncT, wrapper)
 
-    return deco
+    return cast(FuncT, deco)
 
 
 class RequiresMixin:
-    requirement = None
+    requirement = ""
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
         env_var = requirement_env_var_mapping[self.requirement]
         if not os.getenv(env_var):
             raise Exception(
