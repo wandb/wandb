9a266d877513f5add612e1a9c90083795bd6a1bb
Hu Po <hugo.ponte@wandb.com>
2022-06-14 15:42:30 -0500

    full code cov modafuka

--- tests/test_internal_api.py
+++ tests/test_internal_api.py
@@ -9,9 +9,15 @@ def test_agent_heartbeat_with_no_agent_id_fails(test_settings):
         a.agent_heartbeat(None, {}, {})
 
 
-def test_run_state(test_settings):
+def test_get_run_state_invalid_kwargs():
     _api = internal.Api()
     with pytest.raises(CommError):
         _api.get_run_state("test_entity", None, "test_run")
     with pytest.raises(CommError):
         _api.get_run_state("test_entity", "test_project", None)
+
+
+def test_get_run_state(test_settings):
+    _api = internal.Api()
+    res = _api.get_run_state("test", "test", "test")
+    assert res == "running", "Test run must have state running"
