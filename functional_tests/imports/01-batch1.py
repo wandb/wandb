a961cdc9859da3a47c2d06dd4819ba499cd08648
dmitryduev <dima@wandb.com>
2022-06-16 20:23:56 -0700

    comment out allennlp

--- functional_tests/imports/01-batch1.py
+++ functional_tests/imports/01-batch1.py
@@ -17,9 +17,9 @@ assert:
   - :wandb:runs[0][summary]:
       loss: 1
   - :wandb:runs[0][exitcode]: 0
-  - :op:contains:
-    - :wandb:runs[0][telemetry][1]  # imports init
-    - 14  # allennlp
+  #- :op:contains:
+  #  - :wandb:runs[0][telemetry][1]  # imports init
+  #  - 14  # allennlp
   - :op:contains:
     - :wandb:runs[0][telemetry][1]  # imports init
     - 15  # autogluon
@@ -44,16 +44,19 @@ assert:
 """
 
 
-import allennlp  # noqa: F401
+# import allennlp  # noqa: F401
 import autogluon  # noqa: F401
 import autokeras  # noqa: F401
+
 # import avalanche
 import catalyst  # noqa: F401
 import catboost  # noqa: F401
+
 # import dalle_pytorch
 import datasets  # noqa: F401
 import deepchem  # noqa: F401
 import deepctr  # noqa: F401
+
 # import deeppavlov
 # import detectron
 import wandb
