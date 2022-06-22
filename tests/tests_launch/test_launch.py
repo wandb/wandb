58eae33f8739c43c9d163c35b799ee88689ef5fa
Kyle Goyette <kdgoyette@gmail.com>
2022-06-22 15:53:41 -0700

    wip

--- tests/tests_launch/test_launch.py
+++ tests/tests_launch/test_launch.py
@@ -9,6 +9,7 @@ import yaml
 import pytest
 import wandb
 from wandb.apis import PublicApi
+from wandb.apis.public import Run
 from wandb.errors import CommError, LaunchError
 
 from wandb.sdk.launch.agent.agent import LaunchAgent
@@ -352,11 +353,8 @@ def test_launch_add_base_queued_run(live_mock_server):
     assert queued_run.project == "tests"
 
     live_mock_server.set_ctx({"run_queue_item_return_type": "claimed"})
-    queued_run.wait_until_finished()
-    assert queued_run.run is not None
-    assert queued_run.entity == queued_run._run.entity
-    queued_run.id = "new-id"
-    assert queued_run._run._attrs["name"] == "new-id"
+    run = queued_run.wait_until_finished()
+    assert isinstance(run, Run)
 
 
 @pytest.mark.skipif(
