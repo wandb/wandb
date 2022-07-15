b186729eff6e7fb6eced1f59f87547663136ceee
Katia <katia@wandb.com>
2022-07-15 13:23:46 -0700

    bumpversion 0.13.0rc5.dev2 -> 0.13.0rc5

--- wandb/__init__.py
+++ wandb/__init__.py
@@ -11,7 +11,7 @@ For scripts and interactive notebooks, see https://github.com/wandb/examples.
 
 For reference documentation, see https://docs.wandb.com/ref/python.
 """
-__version__ = "0.13.0rc5.dev2"
+__version__ = "0.13.0rc5"
 
 # Used with pypi checks and other messages related to pip
 _wandb_module = "wandb"
