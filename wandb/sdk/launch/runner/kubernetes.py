d3b01d57a935d2e808b8162edb38719c4fa4363f
Kyle Goyette <kdgoyette@gmail.com>
2022-07-18 12:19:45 -0700

    agent controls namespace

--- wandb/sdk/launch/runner/kubernetes.py
+++ wandb/sdk/launch/runner/kubernetes.py
@@ -233,6 +233,14 @@ class KubernetesRunner(AbstractRunner):
         )
         return pod_names
 
+    def get_namespace(
+        self, resource_args: Dict[str, Any], default_namespace: str
+    ) -> str:
+        runner_config_namespace = self.backend_config.get("runner", {}).get("namespace")
+        if runner_config_namespace is not None:
+            return runner_config_namespace
+        return resource_args.get("namespace", default_namespace)
+
     def run(
         self,
         launch_project: LaunchProject,
@@ -272,13 +280,10 @@ class KubernetesRunner(AbstractRunner):
         # begin pulling resource arg overrides. all of these are optional
 
         # allow top-level namespace override, otherwise take namespace specified at the job level, or default in current context
-        default = (
+        default_namespace = (
             context["context"].get("namespace", "default") if context else "default"
         )
-        namespace = resource_args.get(
-            "namespace",
-            job_metadata.get("namespace", default),
-        )
+        namespace = self.get_namespace(resource_args, default_namespace)
 
         # name precedence: resource args override > name in spec file > generated name
         job_metadata["name"] = resource_args.get("job_name", job_metadata.get("name"))
