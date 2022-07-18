e508340a21f3aa96f1809e3eb9efd708382e2825
Jeff Raubitschek <jeff@wandb.com>
2022-07-18 11:47:25 -0700

    fix runtime from offline mode

--- wandb/sdk/internal/handler.py
+++ wandb/sdk/internal/handler.py
@@ -827,9 +827,7 @@ class HandleManager:
         # in that case set it to the first tfevent timestamp
         if self._run_start_time == 0:
             self._run_start_time = history_dict["_timestamp"]
-        history_dict["_runtime"] = int(
-            history_dict["_timestamp"] - self._run_start_time
-        )
+        history_dict["_runtime"] = history_dict["_timestamp"] - self._run_start_time
         item = history.item.add()
         item.key = "_runtime"
         item.value_json = json.dumps(history_dict[item.key])
