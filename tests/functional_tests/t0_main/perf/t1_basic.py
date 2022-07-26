2dccf9580b50a403fce317e93c0c5a5b1267b1cd
Jeff Raubitschek <jeff@wandb.com>
2022-07-26 06:05:06 -0700

    Add basic profile test

---
+++ tests/functional_tests/t0_main/perf/t1_basic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+import time
+
+
+def main():
+    import wandb
+
+    run = wandb.init()
+    wandb.log(dict(this=2))
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    import yea
+    yea.setup()
+
+    main()
