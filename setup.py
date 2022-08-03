1a02f64805c1e37c39902c27e6a77b5476a57401
Katia <katia@wandb.com>
2022-08-02 19:50:09 -0700

    Bump version: 0.13.1.dev1 → 0.13.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ models_requirements = [
 
 setup(
     name="wandb",
-    version="0.13.1.dev1",
+    version="0.13.0",
     description="A CLI and library for interacting with the Weights and Biases API.",
     long_description=readme,
     long_description_content_type="text/markdown",
