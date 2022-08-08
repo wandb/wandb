3cf2d9cbd18de131a3460a9402af835a687f3e70
gtarpenning <griffin.tarpenning@wandb.com>
2022-08-08 11:37:26 -0700

    Added error message in utils on the ENTITY path, which is left unset if the user is 'logged in' but with a bad api-key

--- wandb/sdk/launch/utils.py
+++ wandb/sdk/launch/utils.py
@@ -96,6 +96,13 @@ def set_project_entity_defaults(
         if launch_config:
             config_entity = launch_config.get("entity")
         entity = config_entity or api.default_entity
+
+    if entity is None:
+        raise LaunchError(
+            "Unable to resolve entity. Has the api-key been set properly? "
+            "Use `wandb login --relogin` to force login"
+        )
+
     prefix = ""
     if platform.system() != "Windows" and sys.stdout.encoding == "UTF-8":
         prefix = "🚀 "
