39f9c723393a0bd0df4156715fb681b69dad7b99
Hu Po <hugo.ponte@wandb.com>
2022-08-09 11:25:37 -0500

    tests

--- tests/unit_tests/tests_launch/test_launch_cli.py
+++ tests/unit_tests/tests_launch/test_launch_cli.py
@@ -177,47 +177,56 @@ def test_agent_stop_polling(runner, live_mock_server, monkeypatch):
 
 
 def test_launch_sweep_scheduler(runner, test_settings, live_mock_server):
-    # Create a test sweep
-    sweep_config = {
-        "name": "My Sweep",
-        "method": "grid",
-        "parameters": {"parameter1": {"values": [1, 2, 3]}},
-    }
-    sweep_id = wandb.sweep(sweep_config)
-    assert sweep_id == "test"
-    # Create the default queue
-    result = runner.invoke(
-        cli.launch,
-        [
-            "https://wandb.ai/mock_server_entity/test_project/runs/run",
-            "--project",
-            "test_project",
-            "--entity",
-            "mock_server_entity",
-            "--queue",
-            "default",
-        ],
-    )
-    assert result.exit_code == 0
-    ctx = live_mock_server.get_ctx()
-    assert len(ctx["run_queues"]["1"]) == 1
-    # Run the launch sweep scheduler CLI command
-    result = runner.invoke(
-        cli.scheduler,
-        [
-            "--project",
-            "test_project",
-            "--entity",
-            "mock_server_entity",
-            "--queue",
-            "default",
-            # TODO(hupo): No mock job artifacts for now
-            # "--job",
-            # "mock_job_artifact",
-            sweep_id,
-        ],
-    )
-    assert result.exit_code == 0
+    with runner.isolated_filesystem():
+        with open("mock_launch_config.json", "w") as f:
+            json.dump(
+                {
+                    "queue": "default",
+                    "resource": "local-process",
+                    "job": "mock-launch-job",
+                    "scheduler": {
+                        "resource": "local-process",
+                    },
+                }
+            )
+        # Create a test sweep
+        sweep_config = {
+            "name": "My Sweep",
+            "method": "grid",
+            "parameters": {"parameter1": {"values": [1, 2, 3]}},
+        }
+        sweep_id = wandb.sweep(sweep_config)
+        assert sweep_id == "test"
+        # Create the default queue
+        result = runner.invoke(
+            cli.launch,
+            [
+                "https://wandb.ai/mock_server_entity/test_project/runs/run",
+                "--project",
+                "test_project",
+                "--entity",
+                "mock_server_entity",
+                "--queue",
+                "default",
+            ],
+        )
+        assert result.exit_code == 0
+        ctx = live_mock_server.get_ctx()
+        assert len(ctx["run_queues"]["1"]) == 1
+        # Run the launch sweep scheduler CLI command
+        result = runner.invoke(
+            cli.scheduler,
+            [
+                "--project",
+                "test_project",
+                "--entity",
+                "mock_server_entity",
+                "--launch_config",
+                "mock_launch_config.json",
+                sweep_id,
+            ],
+        )
+        assert result.exit_code == 0
 
 
 # this test includes building a docker container which can take some time.
