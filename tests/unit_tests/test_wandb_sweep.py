357f78324c02cf8703e3799130b91f1e12fc5142
Hu Po <hugo.ponte@wandb.com>
2022-08-11 10:28:36 -0500

    boilerplate

--- tests/unit_tests/test_wandb_sweep.py
+++ tests/unit_tests/test_wandb_sweep.py
@@ -1,34 +1,72 @@
-"""Sweep tests"""
+"""Sweep tests."""
+from typing import Any, Dict
 
 import pytest
 import wandb
 
+# Sweep configs used for testing
+SWEEP_CONFIG_GRID: Dict[str, Any] = {
+    "name": "mock-sweep-grid",
+    "method": "grid",
+    "parameters": {"param1": {"values": [1, 2, 3]}},
+}
+SWEEP_CONFIG_GRID_NESTED: Dict[str, Any] = {
+    "name": "mock-sweep-grid-nested",
+    "method": "grid",
+    "parameters": {"parameter1": {"values": [1, 2, 3]}},
+}
+SWEEP_CONFIG_GRID_HYPERBAND: Dict[str, Any] = {
+    "name": "mock-sweep-grid-hyperband",
+    "method": "grid",
+    "early_terminate": {
+        "type": "hyperband",
+        "max_iter": 27,
+        "s": 2,
+        "eta": 3,
+    },
+    "metric": {"name": "metric1", "goal": "maximize"},
+    "parameters": {"param1": {"values": [1, 2, 3]}},
+}
+SWEEP_CONFIG_BAYES: Dict[str, Any] = {
+    "name": "mock-sweep-bayes",
+    "method": "bayes",
+    "metric": {"name": "metric1", "goal": "maximize"},
+    "parameters": {"param1": {"values": [1, 2, 3]}},
+}
+SWEEP_CONFIG_BAYES_NESTED: Dict[str, Any] = {
+    "name": "mock-sweep-bayes",
+    "method": "bayes",
+}
+SWEEP_CONFIG_RANDOM: Dict[str, Any] = {
+    "name": "mock-sweep-random",
+    "method": "random",
+}
 
-def test_create_sweep(user, relay_server):
+# Convenience lists of sweep configs
+_LIST_SWEEP_CONFIGS = [
+    SWEEP_CONFIG_GRID,
+    SWEEP_CONFIG_GRID_NESTED,
+    SWEEP_CONFIG_GRID_HYPERBAND,
+    SWEEP_CONFIG_BAYES,
+    SWEEP_CONFIG_BAYES_NESTED,
+    SWEEP_CONFIG_RANDOM,
+]
+
+
+@pytest.mark.parametrize("sweep_config", _LIST_SWEEP_CONFIGS)
+def test_sweep_create(relay_server, sweep_config):
     with relay_server() as relay:
-        sweep_config = {
-            "name": "My Sweep",
-            "method": "grid",
-            "parameters": {"parameter1": {"values": [1, 2, 3]}},
-        }
         sweep_id = wandb.sweep(sweep_config)
-
     assert sweep_id in relay.context.entries
 
 
-def test_sweep_entity_project_callable(user, relay_server):
-    sweep_config = {
-        "name": "My Sweep",
-        "method": "grid",
-        "parameters": {"parameter1": {"values": [1, 2, 3]}},
-    }
-
+@pytest.mark.parametrize("sweep_config", _LIST_SWEEP_CONFIGS)
+def test_sweep_entity_project_callable(user, relay_server, sweep_config):
     def sweep_callable():
         return sweep_config
 
     with relay_server() as relay:
         sweep_id = wandb.sweep(sweep_callable, project="test", entity=user)
-
     sweep_response = relay.context.entries[sweep_id]
     assert sweep_response["project"]["entity"]["name"] == user
     assert sweep_response["project"]["name"] == "test"
