e8879391b6a4ea0e12207a2180e5a43c6693b333
Jeff Raubitschek <jeff@wandb.com>
2022-08-12 00:29:27 -0700

    clean up mailbox, still need lock

--- wandb/sdk/lib/mailbox.py
+++ wandb/sdk/lib/mailbox.py
@@ -2,6 +2,8 @@
 mailbox.
 """
 
+import secrets
+import string
 import threading
 import time
 from typing import Callable, Dict, Optional
@@ -9,12 +11,21 @@ from typing import Callable, Dict, Optional
 from wandb.proto import wandb_internal_pb2 as pb
 
 
+def _generate_address(length: int = 12) -> str:
+    address = "".join(
+        secrets.choice(string.ascii_lowercase + string.digits) for i in range(length)
+    )
+    return address
+
+
 class MailboxSlot:
+    _mailbox: "Mailbox"
     _result: Optional[pb.Result]
     _event: threading.Event
     _address: str
 
-    def __init__(self, address: str) -> None:
+    def __init__(self, mailbox: "Mailbox", address: str) -> None:
+        self._mailbox = mailbox
         self._address = address
         self._result = None
         self._event = threading.Event()
@@ -26,6 +37,7 @@ class MailboxSlot:
         start_time = time.time()
         while True:
             if self._event.wait(timeout=1):
+                self.release()
                 return self._result
             if timeout is not None:
                 now = time.time()
@@ -33,7 +45,11 @@ class MailboxSlot:
                     break
             if on_progress:
                 on_progress()
+        self.release()
         return None
+
+    def release(self) -> None:
+        self._mailbox.release_slot(self._address)
 
 
 class Mailbox:
@@ -44,24 +60,17 @@ class Mailbox:
 
     def deliver(self, result: pb.Result) -> None:
         mailbox = result.control.mailbox_slot
-        self._slots[mailbox]._result = result
-        self._slots[mailbox]._event.set()
+        slot = self._slots.get(mailbox)
+        if not slot:
+            return
+        slot._result = result
+        slot._event.set()
 
     def allocate_slot(self) -> MailboxSlot:
-        address = "junk"
-        slot = MailboxSlot(address=address)
+        address = _generate_address()
+        slot = MailboxSlot(mailbox=self, address=address)
         self._slots[address] = slot
         return slot
 
-    def release_box(self, address: str) -> None:
-        pass
-
-    def wait_box(
-        self, address: str, timeout: Optional[float] = None
-    ) -> Optional[pb.Result]:
-        box = self._slots.get(address)
-        assert box
-        found = box._event.wait(timeout=timeout)
-        if found:
-            return box._result
-        return None
+    def release_slot(self, address: str) -> None:
+        self._slots.pop(address, None)
