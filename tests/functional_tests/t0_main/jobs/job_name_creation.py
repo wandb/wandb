2e4f60467f9aaf0c05f71ffdd929c79629ad1a2a
Kyle Goyette <kdgoyette@gmail.com>
2022-08-16 13:57:12 -0700

    fix test

--- tests/functional_tests/t0_main/jobs/job_name_creation.py
+++ tests/functional_tests/t0_main/jobs/job_name_creation.py
@@ -1,11 +1,12 @@
 import wandb
 
 settings = wandb.Settings()
-settings.update(
-    {"disable_git": True, "enable_job_creation": True}, job_name="test-job-name"
-)
+settings.update({"disable_git": True, "enable_job_creation": True})
 run = wandb.init(
-    project="test-job", config={"foo": "bar", "lr": 0.1, "epochs": 5}, settings=settings
+    project="test-job",
+    config={"foo": "bar", "lr": 0.1, "epochs": 5},
+    settings=settings,
+    job_name="test-job-name",
 )
 for i in range(1, run.config["epochs"]):
     wandb.log({"loss": i})
