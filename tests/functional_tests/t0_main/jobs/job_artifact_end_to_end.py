245327d28329803d5ec4d8c22992e849cb6ab02b
Kyle Goyette <kdgoyette@gmail.com>
2022-08-18 12:57:33 -0700

    bring in end t end tests

---
+++ tests/functional_tests/t0_main/jobs/job_artifact_end_to_end.py
@@ -0,0 +1,58 @@
+import os
+import platform
+import subprocess
+
+import pytest
+import wandb
+from wandb.apis.internal import InternalApi
+from wandb.sdk.data_types._dtypes import TypeRegistry
+from wandb.sdk.launch._project_spec import LaunchProject
+from wandb.util import to_forward_slash_path
+
+cmd = ["python", "job_artifact_creation.py"]
+
+subprocess.check_call(cmd)
+
+api = wandb.Api()
+job = api.job("job-source-test-job_artifact_creation.py:v0")
+assert job._job_artifact is not None
+assert job.name == "job-source-test-job_artifact_creation.py:v0"
+
+assert job._input_types == TypeRegistry.type_of({"foo": "bar", "lr": 0.1, "epochs": 5})
+assert job._source_info["source_type"] == "artifact"
+# manually insert defaults since mock server doesn't support metadata
+job._job_artifact.metadata["config_defaults"] = {"epochs": 5, "lr": 0.1, "foo": "bar"}
+with pytest.raises(TypeError):
+    job.call(config={"batch_size": 64})
+
+
+internal_api = InternalApi()
+kwargs = {
+    "uri": None,
+    "job": "job-source-test-job_artifact_creation.py:v0",
+    "api": internal_api,
+    "launch_spec": {},
+    "target_entity": api.default_entity,
+    "target_project": "test-job",
+    "name": None,
+    "docker_config": {},
+    "git_info": {},
+    "overrides": {},
+    "resource": "local",
+    "resource_args": {},
+    "cuda": False,
+}
+lp = LaunchProject(**kwargs)
+
+job.configure_launch_project(lp)
+if platform.system() == "Windows":
+    assert to_forward_slash_path(
+        lp.get_single_entry_point().compute_command({})[1]
+    ) == ("job_artifact_creation.py")
+
+else:
+    assert lp.get_single_entry_point().compute_command({}) == [
+        "python",
+        "job_artifact_creation.py",
+    ]
+assert "requirements.frozen.txt" in os.listdir(lp.project_dir)
---
+++ tests/functional_tests/t0_main/jobs/job_container_end_to_end.py
@@ -0,0 +1,47 @@
+import subprocess
+
+import pytest
+import wandb
+from wandb.apis.internal import InternalApi
+from wandb.sdk.data_types._dtypes import TypeRegistry
+from wandb.sdk.launch._project_spec import LaunchProject
+
+
+cmd = ["python", "job_container_creation.py"]
+
+subprocess.check_call(cmd)
+
+api = wandb.Api()
+job = api.job("job-my-test-containerdummy:v0")
+
+assert job._job_artifact is not None
+assert job.name == "job-my-test-containerdummy:v0"
+assert job._source_info["source_type"] == "image"
+assert job._input_types == TypeRegistry.type_of({"foo": "bar", "lr": 0.1, "epochs": 5})
+
+# manually insert defaults since mock server doesn't support metadata
+job._job_artifact.metadata["config_defaults"] = {"epochs": 5, "lr": 0.1, "foo": "bar"}
+with pytest.raises(TypeError):
+    job.call(config={"batch_size": 64})
+
+internal_api = InternalApi()
+kwargs = {
+    "uri": None,
+    "job": "job-my-test-containerdummy:v0",
+    "api": internal_api,
+    "launch_spec": {},
+    "target_entity": api.default_entity,
+    "target_project": "test-job",
+    "name": None,
+    "docker_config": {},
+    "git_info": {},
+    "overrides": {},
+    "resource": "local",
+    "resource_args": {},
+    "cuda": False,
+}
+lp = LaunchProject(**kwargs)
+
+job.configure_launch_project(lp)
+
+assert lp.docker_image == "my-test-container:dummy"
