dcfa9e387858707f96c7c2c18b735d36461709fd
Dmitry Duev <dima@wandb.com>
2022-08-23 07:34:53 -0700

    a hack to make t3_sentry_userproc.py less flaky

--- tests/functional_tests/t0_main/debug/t3_sentry_userproc.py
+++ tests/functional_tests/t0_main/debug/t3_sentry_userproc.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
+import sys
+import time
 from unittest import mock
 
 import wandb
 
-with mock.patch(
-    "wandb.sdk.wandb_init._WandbInit.init", mock.Mock(side_effect=Exception("injected"))
-):
-    wandb.sdk.wandb_init._WandbInit.init.sentry_repr = None
-    run = wandb.init()
+try:
+    with mock.patch(
+        "wandb.sdk.wandb_init._WandbInit.init",
+        mock.Mock(side_effect=Exception("injected")),
+    ):
+        wandb.sdk.wandb_init._WandbInit.init.sentry_repr = None
+        run = wandb.init()
+except Exception as e:
+    # todo: this is a hack to reduce flake
+    #  (sometimes, it takes time for the mock server to pick up the sentry event)
+    time.sleep(5)
+    sys.exit(4294967295)
