077a7e520e51e68646c820ae01e8d45cb3b0a75e
Dmitry Duev <dima@wandb.com>
2022-08-23 11:45:39 -0700

    try other magic

--- tests/functional_tests/t0_main/debug/t3_sentry_userproc.py
+++ tests/functional_tests/t0_main/debug/t3_sentry_userproc.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 from unittest import mock
 
-import wandb
-import wandb.util
-
 
 def sentry_exc(exc, delay):  # type: ignore
-    return wandb.util.sentry_exc(exc, delay=0.5)
+    import wandb.util
+
+    return wandb.util.sentry_exc(exc, delay=2)
 
 
 with mock.patch(
     "wandb.sdk.wandb_init._WandbInit.init",
     mock.Mock(side_effect=Exception("injected")),
 ), mock.patch("wandb.util.sentry_exc", sentry_exc):
+    import wandb
+
     wandb.sdk.wandb_init._WandbInit.init.sentry_repr = None
     print(wandb.util.sentry_client)
     print(wandb.util.sentry_hub)
