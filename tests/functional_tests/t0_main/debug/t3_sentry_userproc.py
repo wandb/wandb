56cf43b97016f291b2266ff31c9c56553abad9ed
Dmitry Duev <dima@wandb.com>
2022-08-25 11:21:11 -0700

    debug magic

--- tests/functional_tests/t0_main/debug/t3_sentry_userproc.py
+++ tests/functional_tests/t0_main/debug/t3_sentry_userproc.py
@@ -8,10 +8,21 @@ def sentry_exc(exc, delay):  # type: ignore
     return wandb.util.sentry_exc(exc, delay=5)
 
 
+def _send_request(self, body, headers, endpoint_type="store", envelope=None):  # type: ignore
+    # unzip bytes and decode to string
+    import gzip
+
+    data = gzip.decompress(body).decode("utf-8")
+    print(data)
+
+
 with mock.patch(
     "wandb.sdk.wandb_init._WandbInit.init",
     mock.Mock(side_effect=Exception("injected")),
-), mock.patch("wandb.util.sentry_exc", sentry_exc):
+), mock.patch("wandb.util.sentry_exc", sentry_exc,), mock.patch(
+    "sentry_sdk.transport.HttpTransport._send_request",
+    _send_request,
+):
     import wandb
 
     wandb.sdk.wandb_init._WandbInit.init.sentry_repr = None
