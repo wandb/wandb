43274860b504fcdfffb4d9f702f19538a1dee7f4
gtarpenning <griffin.tarpenning@wandb.com>
2022-08-26 16:48:58 -0700

    might this appease our circle overlords?

--- tests/unit_tests/test_launch_add.py
+++ tests/unit_tests/test_launch_add.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pytest
 import wandb
 from wandb.cli import cli
@@ -18,7 +19,7 @@ def launch_queue(api=None):
 
 
 @pytest.mark.timeout(300)  # builds a container
-def test_launch_build_push_job(relay_server, runner, user, monkeypatch):
+def test_launch_build_push_job(relay_server, runner, user):
     # create a project
     proj = "test_project_917"
     os.environ["WANDB_PROJECT"] = proj
@@ -39,6 +40,48 @@ def test_launch_build_push_job(relay_server, runner, user, monkeypatch):
     with relay_server() as relay:
         result = runner.invoke(cli.launch, args)
         print(relay.context.raw_data)
+
+    run.finish()  # weird file sync error if run ends too early
+
+    assert result.exit_code == 0
+    assert "'uri': None" in str(result.output)
+    assert "'job': 'oops'" not in str(result.output)
+
+
+def test_launch_build_with_config(relay_server, runner, user):
+    # create a project
+    proj = "test_project_919"
+    os.environ["WANDB_PROJECT"] = proj
+    run = wandb.init(project=proj)
+    # create a queue in the project
+    api = InternalApi()
+    queue = "queue-23"
+
+    api.create_run_queue(entity=user, project=proj, queue_name=queue, access="PROJECT")
+
+    config = {
+        "cuda": False,
+        "overrides": {"args": ["--epochs", "5"]},
+    }
+
+    args = [
+        "https://github.com/gtarpenning/wandb-launch-test",
+        f"--project={proj}",
+        f"--entity={user}",
+        "--job=oops",
+        f"--queue={queue}",
+        "--build",
+        f"--config={json.dumps(config)}",
+    ]
+    with relay_server() as relay:
+        result = runner.invoke(cli.launch, args)
+        print(relay.context.raw_data)
+
+        runQueueItem = api.pop_from_run_queue(
+            queue_name=queue, entity=user, project=proj
+        )
+
+        assert f"'entity': '{user}'" in str(runQueueItem)
 
     run.finish()  # weird file sync error if run ends too early
 
