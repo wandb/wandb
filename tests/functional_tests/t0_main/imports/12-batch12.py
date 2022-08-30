9dc3fce2215c1ad84f98cece6ff070153871926a
Manan Goel <manangoel1999@gmail.com>
2022-08-30 11:04:55 +0530

    Added telemetry for keras-cv

---
+++ tests/functional_tests/t0_main/imports/12-batch12.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+"""Test a batch of import telemetry
+
+---
+id: 0.imports.12-batch12
+tag:
+  shard: imports12
+plugin:
+  - wandb
+depend:
+  pip_install_timeout: 1500  # 25m
+  requirements:
+    - "-r 12-batch12-requirements.txt"
+assert:
+  - :wandb:runs_len: 1
+  - :wandb:runs[0][config]: {}
+  - :wandb:runs[0][summary]:
+      loss: 1
+  - :wandb:runs[0][exitcode]: 0
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 81  # keras_cv
+"""
+
+import keras_cv  # noqa: F401
+import wandb
+
+run = wandb.init()
+wandb.log(dict(loss=1))
+run.finish()
