4bfa2cf141100e4e314d566c280997b529b899ca
Ishita Bindlish <ishita.bindlish@wandb.com>
2022-08-30 12:38:08 -0700

    Added check to log artifact before link

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -2459,6 +2459,8 @@ class Run:
             aliases = []
 
         if self._backend and self._backend.interface:
+            if isinstance(artifact, wandb.Artifact):
+                artifact = self.log_artifact(artifact)
             if not self._settings._offline:
                 self._backend.interface.publish_link_artifact(
                     self,
