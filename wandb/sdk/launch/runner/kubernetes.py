76aa2f47c2871cfb77493628852abc3c18f9b3e8
Kyle Goyette <kdgoyette@gmail.com>
2022-09-01 16:51:14 -0700

    fix log message

--- wandb/sdk/launch/runner/kubernetes.py
+++ wandb/sdk/launch/runner/kubernetes.py
@@ -229,7 +229,7 @@ class KubernetesRunner(AbstractRunner):
 
         pod_names = [pi.metadata.name for pi in pods.items]
         wandb.termlog(
-            f"{LOG_PREFIX}Job {job_name} created on pod(s) {', '.join(pod_names)}. See logs with e.g. `kubectl logs {pod_names[0]}`."
+            f"{LOG_PREFIX}Job {job_name} created on pod(s) {', '.join(pod_names)}. See logs with e.g. `kubectl logs {pod_names[0]} -n {namespace}`."
         )
         return pod_names
 
