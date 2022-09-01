8e6ad15bc8635844b9b78c048840a8183fd90361
ayulockin <mein2work@gmail.com>
2022-09-01 09:19:54 +0000

    ironing out edge cases

--- wandb/integration/keras/callbacks/model_checkpoint.py
+++ wandb/integration/keras/callbacks/model_checkpoint.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import wandb
+import string
 from typing import Union, Optional, Dict, List
 
 from tensorflow.keras import callbacks
@@ -26,11 +27,11 @@ class WandbModelCheckpoint(callbacks.ModelCheckpoint):
     https://docs.wandb.ai/guides/artifacts
 
     This callback provides the following features:
-    - Save the model that has achieved "best performance" based on "monitor".
-    - Save the model at the end of every epoch regardless of the performance.
-    - Save the model at the end of epoch or after a fixed number of training batches.
-    - Save only model weights, or save the whole model.
-    - Save the model either in SavedModel format or in `.h5` format.
+        - Save the model that has achieved "best performance" based on "monitor".
+        - Save the model at the end of every epoch regardless of the performance.
+        - Save the model at the end of epoch or after a fixed number of training batches.
+        - Save only model weights, or save the whole model.
+        - Save the model either in SavedModel format or in `.h5` format.
 
     Arguments:
         filepath (str): string or `PathLike`, path to save the model file.
@@ -87,25 +88,31 @@ class WandbModelCheckpoint(callbacks.ModelCheckpoint):
 
         self.save_weights_only = save_weights_only
 
+        # User friendly warning when trying to save the best model.
+        if self.save_best_only:
+            self._check_filepath()
+
     def on_train_batch_end(self, batch: int, logs: Dict[str, float] = None):
-        super().on_train_batch_end(batch, logs)
-        # Check if the checkpoint was saved at this batch.
         if self._should_save_on_batch(batch):
+            # Save the model
+            self._save_model(epoch=self._current_epoch, batch=batch, logs=logs)
             # Get filepath where the model checkpoint is saved.
             filepath = self._get_file_path(
                 epoch=self._current_epoch, batch=batch, logs=logs
             )
             # Log the model as artifact
+            aliases = ["latest", f"epoch_{self._current_epoch}_batch_{batch}"]
             self._log_ckpt_as_artifact(filepath, aliases=aliases)
 
     def on_epoch_end(self, epoch, logs: Dict[str, float] = None):
         super().on_epoch_end(epoch, logs)
-        # Get filepath where the model checkpoint is saved.
-        filepath = self._get_file_path(epoch=epoch, batch=None, logs=logs)
-        print(filepath)
-        # Log the model as artifact
-        aliases = ["latest", f"epoch_{epoch}"]
-        self._log_ckpt_as_artifact(filepath, aliases=aliases)
+        # Check if model checkpoint is created at the end of epoch.
+        if self.save_freq == 'epoch':
+            # Get filepath where the model checkpoint is saved.
+            filepath = self._get_file_path(epoch=epoch, batch=None, logs=logs)
+            # Log the model as artifact
+            aliases = ["latest", f"epoch_{epoch}"]
+            self._log_ckpt_as_artifact(filepath, aliases=aliases)
 
     def _log_ckpt_as_artifact(self, filepath: str, aliases: List[str] = []):
         """Log model checkpoint as  W&B Artifact."""
@@ -121,15 +128,27 @@ class WandbModelCheckpoint(callbacks.ModelCheckpoint):
                 for file in glob.glob(f"{filepath}.data-*"):
                     model_artifact.add_file(file)
             elif filepath.endswith(".h5"):
-                # Model saved in .h5 format thus we get files.
+                # Model saved in .h5 format thus we get one file.
                 model_artifact.add_file(filepath)
             else:
                 # Model saved in the SavedModel format thus we have dir.
                 model_artifact.add_dir(filepath)
             wandb.log_artifact(model_artifact, aliases=aliases)
         except ValueError as e:
-            # This error occurs in the following scenarios:
-            # 1. `save_best_only=True` and the model checkpoint is not saved for
-            #   that epoch. Since TF/Keras is giving friendly warning, we can avoid
-            #   clutering the stdout.
+            # This error occurs when `save_best_only=True` and the model
+            # checkpoint is not saved for that epoch/batch. Since TF/Keras
+            # is giving friendly log, we can avoid clutering the stdout.
             pass
+
+    def _check_filepath(self):
+        placeholders = []
+        for tup in string.Formatter().parse(self.filepath):
+            if tup[1] is not None:
+                placeholders.append(tup[1])
+        if len(placeholders) == 0:
+            wandb.termwarn(
+                "When using `save_best_only` ensure that the `filepath` "
+                "contains placeholders such as `{epoch:02d}`,`{batch:02d}` "
+                "or `{mape:.2f}`. This ensures correct interpretation of the "
+                "logged artifacts in this mode."
+            )
