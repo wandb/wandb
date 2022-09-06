16d05c327c726b6595f5fa5136717364f7f79f0b
KyleGoyette <kdgoyette@gmail.com>
2022-09-06 10:16:13 -0700

    Update wandb/sdk/launch/agent/agent.py
    Co-authored-by: Dmitry Duev <dmitryduev@users.noreply.github.com>

--- wandb/sdk/launch/agent/agent.py
+++ wandb/sdk/launch/agent/agent.py
@@ -167,7 +167,7 @@ class LaunchAgent:
         _logger.info("Fetching resource...")
         resource = launch_spec.get("resource") or "local-container"
         backend_config: Dict[str, Any] = {
-            PROJECT_DOCKER_ARGS: launch_spec.get("docker", {}).get("args", {}),
+            PROJECT_DOCKER_ARGS: (launch_spec.get("docker", {}) or {}).get("args", {}) or {},
             PROJECT_SYNCHRONOUS: False,  # agent always runs async
         }
 
