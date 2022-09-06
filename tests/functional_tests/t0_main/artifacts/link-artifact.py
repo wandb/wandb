a2991f0ced715724a1ccacc333a1906b77963b5c
Ishita Bindlish <ishita.bindlish@wandb.com>
2022-09-06 08:45:48 -0700

    updated link-artifact test

--- tests/functional_tests/t0_main/artifacts/link-artifact.py
+++ tests/functional_tests/t0_main/artifacts/link-artifact.py
@@ -1,3 +1,7 @@
+"""
+Test for run.link_artifact()
+Allows user to link an artifact to a portfolio, without logging it first
+"""
 import tempfile
 
 import wandb
@@ -13,7 +17,10 @@ def main():
 
             local_path = f"{tmpdir}/boom.txt"
             artifact.add_file(local_path, "test-name")
-            artifact = run.log_artifact(artifact)
+            """
+            log_artifact() is now handled by link_artifact()
+            """
+            # artifact = run.log_artifact(artifact)
             run.link_artifact(artifact, "entity/project/test_portfolio")
 
 
