a85d0a69abd780116c3489e760246e470a968bc1
Spencer Pearson <spencer.pearson@wandb.com>
2022-09-21 23:26:28 -0700

    lint

--- tests/unit_tests/test_artifacts.py
+++ tests/unit_tests/test_artifacts.py
@@ -1,17 +1,23 @@
-import pytest
-
+from typing import TYPE_CHECKING
 import wandb
 
-from .conftest import InjectedGraphQLRequestCreator
+if TYPE_CHECKING:
+    from .conftest import InjectedGraphQLRequestCreator
+
 
 def test_commit_retries_on_500(
     relay_server,
     wandb_init,
-    inject_graphql_response: InjectedGraphQLRequestCreator,
+    inject_graphql_response: "InjectedGraphQLRequestCreator",
 ):
-    art = wandb.Artifact('test_TODO_1525', 'dataset')
+    art = wandb.Artifact("test", "dataset")
 
-    with relay_server(inject=[inject_graphql_response(operation_name="CommitArtifact", status=500, counter=1)]) as relay:
+    injected_resp = inject_graphql_response(
+        operation_name="CommitArtifact",
+        status=500,
+        counter=1,
+    )
+    with relay_server(inject=[injected_resp]):
         run = wandb_init()
         run.log_artifact(art).wait()
         run.finish()
