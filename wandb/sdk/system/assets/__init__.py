5e562d61a7f362d1236003d1b31b8872420e250c
Dmitry Duev <dima@wandb.com>
2022-09-29 15:23:50 -0700

    add IPU

--- wandb/sdk/system/assets/__init__.py
+++ wandb/sdk/system/assets/__init__.py
@@ -2,10 +2,11 @@ __all__ = ["asset_registry"]
 
 from .asset_registry import asset_registry
 
-# from .cpu import CPU
-# from .disk import Disk
-# from .gpu import GPU
-# from .gpu_apple import GPUApple
-# from .memory import Memory
-# from .network import Network
+from .cpu import CPU
+from .disk import Disk
+from .gpu import GPU
+from .gpu_apple import GPUApple
+from .ipu import IPU
+from .memory import Memory
+from .network import Network
 from .tpu import TPU
