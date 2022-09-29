4db11ebbec64a1c0a9f0c3ad0091bbd9354dd4d1
Dmitry Duev <dima@wandb.com>
2022-09-29 13:31:03 -0700

    comment everything but gpu

--- wandb/sdk/system/assets/__init__.py
+++ wandb/sdk/system/assets/__init__.py
@@ -1,10 +1,12 @@
 __all__ = ["asset_registry"]
 
 from .asset_registry import asset_registry
-from .cpu import CPU
-from .disk import Disk
+
+# from .cpu import CPU
+# from .disk import Disk
 from .gpu import GPU
-from .gpu_apple import GPUApple
-from .memory import Memory
-from .network import Network
-from .tpu import TPU
+
+# from .gpu_apple import GPUApple
+# from .memory import Memory
+# from .network import Network
+# from .tpu import TPU
