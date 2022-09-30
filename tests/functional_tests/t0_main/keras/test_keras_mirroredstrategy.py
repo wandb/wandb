84230a8e5a593b1db513ff3f72fb0a51cce240d8
Katia <katia@wandb.com>
2022-09-29 17:55:50 -0700

    add both cases

--- tests/functional_tests/t0_main/keras/test_keras_mirroredstrategy.py
+++ tests/functional_tests/t0_main/keras/test_keras_mirroredstrategy.py
@@ -3,32 +3,38 @@ import tensorflow as tf
 import wandb
 from wandb.keras import WandbCallback
 
-tf.keras.utils.set_random_seed(1234)
 
-x = np.random.randint(255, size=(100, 28, 28, 1))
-y = np.random.randint(10, size=(100,))
-dataset = (x, y)
+def main():
+    tf.keras.utils.set_random_seed(1234)
 
-run = wandb.init(project="keras")
+    def get_model():
+        inputs = tf.keras.layers.Input(shape=(28, 28, 1))
+        x = tf.keras.layers.Conv2D(3, 3, activation="relu")(inputs)
+        x = tf.keras.layers.Flatten()(x)
+        outputs = tf.keras.layers.Dense(10, activation="softmax")(x)
+
+        model = tf.keras.models.Model(inputs, outputs)
+        model.compile(
+            optimizer="sgd",
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"],
+        )
+
+        return model
+
+    strategy = tf.distribute.MirroredStrategy()
+
+    with strategy.scope():
+        model = get_model()
+
+    x = np.random.randint(255, size=(100, 28, 28, 1))
+    y = np.random.randint(10, size=(100,))
+
+    _ = wandb.init(project="keras")
+
+    _ = model.fit(x, y, epochs=2, callbacks=WandbCallback())
+    _ = model.fit(x, y, epochs=2, callbacks=WandbCallback(compute_flops=True))
 
 
-def get_model():
-    inputs = tf.keras.layers.Input(shape=(28, 28, 1))
-    x = tf.keras.layers.Conv2D(3, 3, activation="relu")(inputs)
-    x = tf.keras.layers.Flatten()(x)
-    outputs = tf.keras.layers.Dense(10, activation="softmax")(x)
-
-    model = tf.keras.models.Model(inputs, outputs)
-    model.compile(
-        optimizer="sgd", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
-    )
-
-    return model
-
-
-strategy = tf.distribute.MirroredStrategy()
-
-with strategy.scope():
-    model = get_model()
-
-_ = model.fit(x, y, epochs=2, callbacks=WandbCallback())
+if __name__ == "__main__":
+    main()
