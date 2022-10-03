df760571484375ff1f5e8f3b46927029c7d80c8b
Dmitry Duev <dima@wandb.com>
2022-10-03 11:56:05 -0700

    fix merge conflict

--- wandb/proto/_wandb_base_pb2.py
+++
@@ -1,8 +0,0 @@
-import google.protobuf
-
-protobuf_version = google.protobuf.__version__[0]
-
-if protobuf_version == "3":
-    from wandb.proto.v3.wandb_base_pb2 import *
-elif protobuf_version == "4":
-    from wandb.proto.v4.wandb_base_pb2 import *
