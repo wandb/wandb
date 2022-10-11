03d4acac759d89e11c88f097df12921d989c927c
Katia <katia@wandb.com>
2022-10-11 12:32:39 -0700

    partial fix for ipu and tpu tests

--- tests/unit_tests/test_ipu.py
+++ tests/unit_tests/test_ipu.py
@@ -1,6 +1,8 @@
+from unittest import mock
+
 import wandb
-from wandb.sdk.internal.ipu import IPUProfiler
-from wandb.sdk.internal.stats import SystemStats
+from wandb.sdk.system.assets.ipu import IPUStats, IPU
+# from wandb.sdk.system.system_monitor import SystemMonitor
 
 CURRENT_PID = 123
 OTHER_PID = 456
@@ -52,7 +54,7 @@ class MockGcIpuInfo:
 
 def test_profiler():
     gc_ipu_info = MockGcIpuInfo()
-    ipu_profiler = IPUProfiler(pid=CURRENT_PID, gc_ipu_info=gc_ipu_info)
+    ipu_profiler = IPUStats(pid=CURRENT_PID, gc_ipu_info=gc_ipu_info)
 
     metrics = {
         "ipu.1.user process id": CURRENT_PID,
@@ -65,7 +67,8 @@ def test_profiler():
         "ipu.1.ipu utilisation (%)": 21.25,
         "ipu.1.ipu utilisation (session) (%)": 15.65,
     }
-    assert ipu_profiler.get_metrics() == metrics
+    ipu_profiler.sample()
+    assert ipu_profiler.samples[0] == metrics
 
     changed_metrics = {
         "ipu.1.average board temp (C)": 30,
@@ -75,7 +78,8 @@ def test_profiler():
         "ipu.1.ipu utilisation (%)": 21.25,
         "ipu.1.ipu utilisation (session) (%)": 15.65,
     }
-    assert ipu_profiler.get_metrics() == changed_metrics
+    ipu_profiler.sample()
+    assert ipu_profiler.samples[1] == changed_metrics
 
 
 class MockIPUProfiler:
@@ -87,9 +91,10 @@ class MockIPUProfiler:
 
 
 def test_ipu_system_stats(monkeypatch, mocked_interface, test_settings):
-    monkeypatch.setattr(wandb.sdk.internal.stats.ipu, "is_ipu_available", lambda: True)
-    monkeypatch.setattr(wandb.sdk.internal.stats.ipu, "IPUProfiler", MockIPUProfiler)
-    stats = SystemStats(settings=test_settings(), interface=mocked_interface)
+    monkeypatch.setattr(wandb.sdk.system.assets.ipu.IPU, "is_available", lambda: True)
+    monkeypatch.setattr(wandb.sdk.system.assets.ipu, "IPUStats", MockIPUProfiler)
+    # monkeypatch.setattr(wandb.sdk.system.assets.ipu, "IPUStats", mock.MagicMock())
+    stats = IPU(settings=test_settings(), interface=mocked_interface)
     expected_stats = {"ipu.0.metric": 10}
     actual_stats = {
         key: expected_stats[key]
