e1e13d1b8740ac3a73b0462f23a8d8ec043a7976
gtarpenning <griffin.tarpenning@wandb.com>
2022-10-18 11:08:05 -0700

    non deterministic create artifact error

--- tests/unit_tests/tests_launch/test_launch_cli.py
+++ tests/unit_tests/tests_launch/test_launch_cli.py
@@ -1,5 +1,4 @@
 import json
-import os
 
 import pytest
 import wandb
@@ -11,23 +10,32 @@ from wandb.sdk.launch.runner.local_container import LocalContainerRunner
 @pytest.mark.parametrize(
     "args,override_config",
     [
-        (["--build", "--queue"], {"registry": {"repository": "testing123"}}),
+        (["--build", "--queue"], {"registry": {"url": "testing123"}}),
         (
             ["--queue", "--build", "--repository", "testing-override"],
-            {"registry": {"url": "testing123"}},
+            {
+                "registry": {"url": "testing123"},
+                "docker": {"args": ["--container_arg", "9-rams"]},
+            },
         ),
-        (["--build", "--queue"], {"docker": {"args": ["--container_arg", "9-rams"]}}),
     ],
     ids=[
         "queue default build",
-        "repository override",
-        "build with docker args",
+        "repository and docker args override",
     ],
 )
 def test_launch_build_succeeds(
-    relay_server, user, monkeypatch, runner, args, override_config
+    relay_server,
+    user,
+    monkeypatch,
+    runner,
+    args,
+    override_config,
+    test_settings,
+    wandb_init,
 ):
-    proj = "test"
+    proj = "testing123"
+    settings = test_settings({"project": proj})
     image_name = "fake-image123"
     base_args = [
         "https://github.com/wandb/examples.git",
@@ -48,9 +56,6 @@ def test_launch_build_succeeds(
     if "--repository" in args:
         true_repository = args[args.index("--repository") + 1]
 
-    def patched_validate_docker_installation():
-        return None
-
     def patched_build_image_with_builder(
         builder,
         launch_project,
@@ -67,7 +72,7 @@ def test_launch_build_succeeds(
     monkeypatch.setattr(
         wandb.sdk.launch.builder.build,
         "validate_docker_installation",
-        lambda: patched_validate_docker_installation(),
+        lambda: None,
     )
 
     monkeypatch.setattr(
@@ -82,25 +87,22 @@ def test_launch_build_succeeds(
         lambda *args, **kwargs: patched_build_image_with_builder(*args, **kwargs),
     )
 
-    os.environ["WANDB_PROJECT"] = proj  # required for artifact query
-    run = wandb.init(project=proj, entity=user)  # create project
-
     with runner.isolated_filesystem(), relay_server():
+        run = wandb_init(settings=settings)
         result = runner.invoke(cli.launch, base_args + args)
+        run.finish()
 
         assert result.exit_code == 0
         assert "Launching run in docker with command" not in result.output
         assert "Added run to queue" in result.output
         assert f"'job': '{user}/{proj}/job-{image_name}:v0'" in result.output
 
-    run.finish()
-
 
 @pytest.mark.timeout(100)
 @pytest.mark.parametrize(
     "args",
-    [(["--build"]), (["--build=builder"]), (["--build", "--queue=not-a-queue"])],
-    ids=["no queue flag", "builder argument", "queue does not exist"],
+    [(["--build"]), (["--build=builder"])],
+    ids=["no queue flag", "builder argument"],
 )
 def test_launch_build_fails(
     relay_server,
@@ -108,8 +110,11 @@ def test_launch_build_fails(
     monkeypatch,
     runner,
     args,
+    test_settings,
+    wandb_init,
 ):
-    proj = "test"
+    proj = "testing123"
+    settings = test_settings({"project": proj})
     base_args = [
         "https://github.com/wandb/examples.git",
         "--entity",
@@ -120,16 +125,13 @@ def test_launch_build_fails(
         "python ./examples/scikit/scikit-classification/train.py",
     ]
 
-    def patched_validate_docker_installation():
-        return None
-
     def patched_build_image_with_builder(*_):
         return "fakeImage123"
 
     monkeypatch.setattr(
         wandb.sdk.launch.builder.build,
         "validate_docker_installation",
-        lambda: patched_validate_docker_installation(),
+        lambda: None,
     )
 
     monkeypatch.setattr(
@@ -144,29 +146,32 @@ def test_launch_build_fails(
         lambda *args, **kwargs: patched_build_image_with_builder(*args, **kwargs),
     )
 
-    os.environ["WANDB_PROJECT"] = proj  # required for artifact query
-    run = wandb.init(project=proj)  # create project
-
     with runner.isolated_filesystem(), relay_server():
+        run = wandb_init(settings=settings)
         result = runner.invoke(cli.launch, base_args + args)
+
+        run.finish()
 
         if args == ["--build"]:
             assert result.exit_code == 1
             assert "Build flag requires a queue to be set" in result.output
         elif args == ["--build", "--queue=not-a-queue"]:
             assert result.exit_code == 1
+            assert "Unable to push to run queue not-a-queue." in result.output
             assert "Error adding run to queue" in result.output
         elif args == ["--build=builder"]:
             assert result.exit_code == 2
-            assert "Option '--build' does not take a value" in result.output
-    run.finish()
+            assert (
+                "Option '--build' does not take a value" in result.output
+                or "Error: --build option does not take a value" in result.output
+            )
 
 
 @pytest.mark.timeout(300)
 @pytest.mark.parametrize(
     "args",
-    [(["--repository=test_repo", "--resource=local"]), (["--respository="])],
-    ids=["set repository", "set repository empty"],
+    [(["--repository=test_repo", "--resource=local"])],
+    ids=["set repository"],
 )
 def test_launch_repository_arg(
     relay_server,
@@ -174,8 +179,10 @@ def test_launch_repository_arg(
     monkeypatch,
     runner,
     args,
+    test_settings,
+    wandb_init,
 ):
-    proj = "test"
+    proj = "testing123"
     base_args = [
         "https://github.com/wandb/examples.git",
         "--entity",
@@ -187,7 +194,7 @@ def test_launch_repository_arg(
     ]
 
     def patched_run(_, launch_project, builder, registry_config):
-        assert registry_config.get("url") == "test_repo"
+        assert registry_config.get("url") == "test_repo" or "--repository=" in args
 
         return "run"
 
@@ -206,10 +213,15 @@ def test_launch_repository_arg(
         lambda: patched_validate_docker_installation(),
     )
 
+    settings = test_settings({"project": proj})
+
     with runner.isolated_filesystem(), relay_server():
+        run = wandb_init(settings=settings)
         result = runner.invoke(cli.launch, base_args + args)
 
         if "--respository=" in args:  # incorrect param
             assert result.exit_code == 2
         else:
             assert result.exit_code == 0
+
+        run.finish()
