19fada9c863570402a37047c79d94c1630436714
Dmitry Duev <dima@wandb.com>
2022-10-26 16:03:01 -0700

    dope

--- wandb/sdk/internal/system/assets/gpu.py
+++ wandb/sdk/internal/system/assets/gpu.py
@@ -363,6 +363,7 @@ class GPUEnergyKiloWattHours:
                 t = [self.t_start[i]] + t
                 p = [self.p_start[i]] + p
             aggregate = trapezoidal(p, t)
+            print(t, p, aggregate)
             stats[self.name.format(i)] = aggregate / 3600000  # Watt-seconds to kWh
 
             handle = pynvml.nvmlDeviceGetHandleByIndex(i)  # type: ignore
