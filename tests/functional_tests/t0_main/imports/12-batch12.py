c1d63fc1c68382b696e9476e63c27fb45285a17d
Manan Goel <manangoel1999@gmail.com>
2022-10-26 15:32:33 +0530

    Added telemetry for huggingface diffusers and evaluate

--- tests/functional_tests/t0_main/imports/12-batch12.py
+++ tests/functional_tests/t0_main/imports/12-batch12.py
@@ -25,10 +25,18 @@ assert:
   - :op:contains:
     - :wandb:runs[0][telemetry][1]  # imports init
     - 82  # mmengine
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 83  # diffusers
+  - :op:contains:
+    - :wandb:runs[0][telemetry][1]  # imports init
+    - 84  # evaluate
 """
 
 import keras_cv  # noqa: F401
 import mmengine  # noqa: F401
+import diffusers  # noqa: F401
+import evaluate  # noqa: F401
 import wandb
 
 run = wandb.init()
