d0b62c3277dc4b9cfc01353eec1e6092aaf00557
Andrew Truong <itsandrewtruong@gmail.com>
2022-11-03 14:28:08 -0400

    add friendly init message

--- wandb/apis/reports/__init__.py
+++ wandb/apis/reports/__init__.py
@@ -3,3 +3,18 @@
 from .blocks import *
 from .panels import *
 from .reports import LineKey, PCColumn, Report, Runset
+from ... import termlog
+from inspect import cleandoc
+
+termlog(
+    cleandoc(
+        """
+        Thanks for trying out the Report API!
+          ∟ see panels:          \033[92m`wr.panels.<tab>`
+          ∟ see blocks:          \033[92m`wr.blocks.<tab>`
+          ∟ see everything else: \033[92m`wr.<tab>`
+        
+        If you have issues, please make a ticket on JIRA.
+        """
+    )
+)
