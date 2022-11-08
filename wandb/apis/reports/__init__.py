bbfff6305a95484409d9ba01d15209d61fe92c27
Andrew Truong <itsandrewtruong@gmail.com>
2022-11-08 12:22:55 -0500

    mention templates in init

--- wandb/apis/reports/__init__.py
+++ wandb/apis/reports/__init__.py
@@ -15,10 +15,11 @@ termlog(
         For a tutorial, check out https://colab.research.google.com/drive/1jm3GgVSZLwb9S4BG1091Vy-LIDGuqw8i
         
         Try out tab completion to see what's available.
-          ∟ everything:  `wr.<tab>`
-              ∟ panels:  `wr.panels.<tab>`
-              ∟ blocks:  `wr.blocks.<tab>`
-              ∟ helpers: `wr.helpers.<tab>`
+          ∟ everything:    `wr.<tab>`
+              ∟ panels:    `wr.panels.<tab>`
+              ∟ blocks:    `wr.blocks.<tab>`
+              ∟ helpers:   `wr.helpers.<tab>`
+              ∟ templates: `wr.templates.<tab>`
         
         If you have issues, please make a ticket on JIRA.
         """
