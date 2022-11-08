3710515d1a0be69a59f1c675f626de45bbcfa176
Dmitry Duev <dima@wandb.com>
2022-11-08 14:00:07 -0800

    replace borky yea test with a (borky) circle job

--- tests/functional_tests/t0_main/pex/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import wandb
-
-
-if __name__ == "__main__":
-    run = wandb.init(project="pex")
-    run.log({"test": 1})
-    run.finish()
--- tests/functional_tests/t0_main/pex/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="test",
-    version="1.0",
-    scripts=["main.py"],
-)
