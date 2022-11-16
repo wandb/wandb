11e19249429aa94dac9d52b708bfe13f18caecdd
Andrew Truong <itsandrewtruong@gmail.com>
2022-11-15 23:07:30 -0500

    Cleanup mappings

--- wandb/apis/public.py
+++ wandb/apis/public.py
@@ -3281,30 +3281,50 @@ class PanelMetricsHelper:
 
     # ScatterPlot and ParallelCoords have weird conventions
     def special_front_to_back(self, name):
-        if name is not None:
-            name, *rest = name.split(".")
-            rest = "." + ".".join(rest) if rest else ""
-            # special case for config
-            if name.startswith("c::"):
-                name = name[3:]
-                return f"config:{name}.value{rest}"
-
-            # special case for summary
-            if name.startswith("s::"):
-                name = name[3:]
-                return "summary:" + self.FRONTEND_NAME_MAPPING[name]
-
         if name is None:
             return name
-        elif name in self.RUN_MAPPING:
+
+        name, *rest = name.split(".")
+        rest = "." + ".".join(rest) if rest else ""
+
+        # special case for config
+        if name.startswith("c::"):
+            name = name[3:]
+            return f"config:{name}.value{rest}"
+
+        # special case for summary
+        if name.startswith("s::"):
+            name = name[3:] + rest
+            return f"summary:{name}"
+
+        name = name + rest
+        if name in self.RUN_MAPPING:
             return "run:" + self.RUN_MAPPING[name]
-        elif name in self.FRONTEND_NAME_MAPPING:
+        if name in self.FRONTEND_NAME_MAPPING:
             return "summary:" + self.FRONTEND_NAME_MAPPING[name]
-        elif name == "Index":
+        if name == "Index":
             return name
         return "summary:" + name
 
     def special_back_to_front(self, name):
+        if name is not None:
+            kind, rest = name.split(":", 1)
+
+            if kind == "config":
+                pieces = rest.split(".")
+                if len(pieces) <= 1:
+                    raise ValueError(f"Invalid name: {name}")
+                elif len(pieces) == 2:
+                    name = pieces[0]
+                elif len(pieces) >= 3:
+                    name = pieces[:1] + pieces[2:]
+                    name = ".".join(name)
+                return f"c::{name}"
+
+            elif kind == "summary":
+                name = rest
+                return f"s::{name}"
+
         if name is None:
             return name
         elif "summary:" in name:
