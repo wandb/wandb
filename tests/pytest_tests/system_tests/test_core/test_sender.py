7ddb1dc77326f351de18fc3881a7a26fc013fc10
Hugh Wimberly <hugh.wimberly@gmail.com>
2022-12-07 15:11:40 -0800

    Mark test_save_now_existing_file as flaky

--- tests/pytest_tests/system_tests/test_core/test_sender.py
+++ tests/pytest_tests/system_tests/test_core/test_sender.py
@@ -176,6 +176,7 @@ def test_save_now_write_after_policy(relay_server, user, mock_run, backend_inter
     assert uploaded_files.count("test.txt") == 1
 
 
+@pytest.mark.flaky
 def test_save_now_existing_file(relay_server, user, mock_run, backend_interface):
     run = mock_run(use_magic_mock=True)
     with relay_server() as relay, backend_interface(run) as interface:
