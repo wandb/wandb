1ea80638b9e24fbb4ea14cc0f6554c2fe42b7f19
Jeff Raubitschek <jeff@wandb.com>
2022-12-16 19:28:37 -0800

    update conftest

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -48,6 +48,7 @@ from wandb.sdk.internal import context
 from wandb.sdk.internal.handler import HandleManager
 from wandb.sdk.internal.sender import SendManager
 from wandb.sdk.internal.settings_static import SettingsStatic
+from wandb.sdk.internal.writer import WriteManager
 from wandb.sdk.lib.git import GitRepo
 from wandb.sdk.lib.mailbox import Mailbox
 
@@ -515,7 +516,6 @@ def internal_hm(
     runner,
     internal_record_q,
     internal_result_q,
-    internal_sender_q,
     internal_writer_q,
     _internal_sender,
     stopped_event,
@@ -528,9 +528,34 @@ def internal_hm(
                 record_q=internal_record_q,
                 result_q=internal_result_q,
                 stopped=stopped_event,
-                sender_q=internal_sender_q,
                 writer_q=internal_writer_q,
                 interface=_internal_sender,
+                context_keeper=_internal_context_keeper,
+            )
+            return hm
+
+    yield helper
+
+
+@pytest.fixture()
+def internal_wm(
+    runner,
+    internal_writer_q,
+    internal_result_q,
+    internal_sender_q,
+    stopped_event,
+    _internal_context_keeper,
+):
+    def helper(settings):
+        with runner.isolated_filesystem():
+            wandb_file = settings.sync_file
+            run_dir = Path(wandb_file).parent
+            os.makedirs(run_dir)
+            hm = WriteManager(
+                settings=SettingsStatic(settings.make_static()),
+                record_q=internal_record_q,
+                result_q=internal_result_q,
+                sender_q=internal_sender_q,
                 context_keeper=_internal_context_keeper,
             )
             return hm
@@ -580,6 +605,35 @@ def start_send_thread(
 
 
 @pytest.fixture()
+def start_write_thread(
+    internal_writer_q, internal_get_record, stopped_event, internal_process
+):
+    def start_send(write_manager):
+        def target():
+            try:
+                while True:
+                    payload = internal_get_record(
+                        input_q=internal_writer_q, timeout=0.1
+                    )
+                    if payload:
+                        write_manager.write(payload)
+                    elif stopped_event.is_set():
+                        break
+            except Exception:
+                stopped_event.set()
+                internal_process._alive = False
+
+        t = threading.Thread(target=target)
+        t.name = "testing-writer"
+        t.daemon = True
+        t.start()
+        return t
+
+    yield start_send
+    stopped_event.set()
+
+
+@pytest.fixture()
 def start_handle_thread(internal_record_q, internal_get_record, stopped_event):
     def start_handle(handle_manager):
         def target():
@@ -603,15 +657,19 @@ def start_handle_thread(internal_record_q, internal_get_record, stopped_event):
 @pytest.fixture()
 def _start_backend(
     internal_hm,
+    internal_wm,
     internal_sm,
     _internal_sender,
     start_handle_thread,
+    start_write_thread,
     start_send_thread,
 ):
     def start_backend_func(run=None, initial_run=True, initial_start=True):
         ihm = internal_hm(run.settings)
+        iwm = internal_wm(run.settings)
         ism = internal_sm(run.settings)
         ht = start_handle_thread(ihm)
+        wt = start_write_thread(iwm)
         st = start_send_thread(ism)
         if initial_run:
             _run = _internal_sender.communicate_run(run)
