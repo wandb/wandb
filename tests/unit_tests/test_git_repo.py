a24377d1ee51ff49943e6b2f9e0c573e3119ddfb
Kyle Goyette <kdgoyette@gmail.com>
2022-12-20 13:52:11 -0800

    add job_builder test

--- tests/unit_tests/test_git_repo.py
+++ tests/unit_tests/test_git_repo.py
@@ -15,24 +15,6 @@ import wandb
 GitRepo = wandb.wandb_lib.git.GitRepo
 
 
-@pytest.fixture
-def git_repo_fn() -> Callable:
-    def git_repo_fn_helper(
-        path: str = ".",
-        remote_name: str = "origin",
-        remote_url: Optional[str] = "https://foo:bar@github.com/FooTest/Foo.git",
-        commit_msg: Optional[str] = None,
-    ):
-        with git.Repo.init(path) as repo:
-            if remote_url is not None:
-                repo.create_remote(remote_name, remote_url)
-            if commit_msg is not None:
-                repo.index.commit(commit_msg)
-            return GitRepo(lazy=False)
-
-    yield git_repo_fn_helper
-
-
 class TestGitRepo:
     def test_last_commit(self, git_repo_fn):
         git_repo = git_repo_fn(commit_msg="My commit message")
