bf73b8b9f46033ec167fafe3c0148d47345afaa1
Hugh Wimberly <hugh.wimberly@gmail.com>
2022-12-21 11:38:51 -0800

    Remove unused variable i

--- wandb/sdk/lib/runid.py
+++ wandb/sdk/lib/runid.py
@@ -11,4 +11,4 @@ def generate_id(length: int = 8) -> str:
     # There are ~2.8T base-36 8-digit strings. If we generate 210k ids,
     # we'll have a ~1% chance of collision.
     alphabet = string.ascii_letters + string.digits
-    return "".join(secrets.choice(alphabet) for i in range(length))
+    return "".join(secrets.choice(alphabet) for _ in range(length))
