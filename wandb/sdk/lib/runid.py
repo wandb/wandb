524dd9deb6a232b5a46751b845fc3c0adda4b1a4
Hugh Wimberly <hugh.wimberly@gmail.com>
2022-12-21 11:45:47 -0800

    Use lowercase alphanumeric instead of mixed case

--- wandb/sdk/lib/runid.py
+++ wandb/sdk/lib/runid.py
@@ -10,5 +10,5 @@ def generate_id(length: int = 8) -> str:
     """Generate a random base-36 string of `length` digits."""
     # There are ~2.8T base-36 8-digit strings. If we generate 210k ids,
     # we'll have a ~1% chance of collision.
-    alphabet = string.ascii_letters + string.digits
+    alphabet = string.ascii_lowercase + string.digits
     return "".join(secrets.choice(alphabet) for _ in range(length))
