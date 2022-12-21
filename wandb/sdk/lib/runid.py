fdcc6244d1e875beee7cd98cd4d5ed18e798ea9b
Hugh Wimberly <hugh.wimberly@gmail.com>
2022-12-21 11:36:48 -0800

    Remove dependency on shortuuid, replace with builtin secrets

--- wandb/sdk/lib/runid.py
+++ wandb/sdk/lib/runid.py
@@ -2,12 +2,13 @@
 runid util.
 """
 
-import shortuuid
+import secrets
+import string
 
 
 def generate_id(length: int = 8) -> str:
     """Generate a random base-36 string of `length` digits."""
     # There are ~2.8T base-36 8-digit strings. If we generate 210k ids,
     # we'll have a ~1% chance of collision.
-    run_gen = shortuuid.ShortUUID(alphabet="0123456789abcdefghijklmnopqrstuvwxyz")
-    return run_gen.random(length)
+    alphabet = string.ascii_letters + string.digits
+    return "".join(secrets.choice(alphabet) for i in range(length))
