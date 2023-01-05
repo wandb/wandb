0c4193f7599820135e733c7ce3e0b039833e4d45
Jeff Raubitschek <jeff@wandb.com>
2023-01-05 06:30:36 -0800

    update test_fsm

--- tests/unit_tests/test_fsm.py
+++ tests/unit_tests/test_fsm.py
@@ -1,21 +1,44 @@
+from unittest.mock import patch
+
 from wandb.sdk.lib.fsm import Fsm, FsmEntry
 
 
+class TrackCalls:
+    def __init__(self, calls):
+        self._calls = calls
+
+
+class A(TrackCalls):
+    def __init__(self, calls):
+        super().__init__(calls)
+
+    def on_state(self, inputs) -> None:
+        self._calls.append("A:on_state")
+
+    def to_b(self, inputs) -> bool:
+        self._calls.append("to_b")
+        return True
+
+
+class B(TrackCalls):
+    def __init__(self, calls):
+        super().__init__(calls)
+
+    def on_state(self, inputs) -> None:
+        self._calls.append("B:on_state")
+
+    def to_a(self, inputs) -> bool:
+        self._calls.append("to_a")
+        return True
+
+
 def test_normal():
-    class A:
-        def on_state(self, inputs) -> None:
-            pass
-
-    class B:
-        def on_state(self, inputs) -> None:
-            pass
-
-    def to_b(inputs) -> bool:
-        return True
-
-    def to_a(inputs) -> bool:
-        return True
-
-    f = Fsm(states=[A(), B()], table={A: [FsmEntry(to_b, B)], B: [FsmEntry(to_a, A)]})
+    calls = []
+    sa = A(calls)
+    sb = B(calls)
+    f = Fsm(
+        states=(sa, sb), table={A: [FsmEntry(sa.to_b, B)], B: [FsmEntry(sb.to_a, A)]}
+    )
 
     f.input({"input1": 1, "input2": 2})
+    assert calls == ["to_b", "B:on_state"]
