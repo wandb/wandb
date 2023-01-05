cf00a32bcd1cbc88f02601daf465a23d51e59936
ayulockin <mein2work@gmail.com>
2023-01-05 14:35:50 +0000

    update docstring

--- wandb/integration/keras/callbacks/model_checkpoint.py
+++ wandb/integration/keras/callbacks/model_checkpoint.py
@@ -46,13 +46,21 @@ class WandbModelCheckpoint(callbacks.ModelCheckpoint):
         - Save the model either in SavedModel format or in `.h5` format.
 
     Arguments:
-        filepath (Union[str, os.PathLike]): path to save the model file.
-        monitor (str): The metric name to monitor.
+        filepath (Union[str, os.PathLike]): path to save the model file. `filepath`
+            can contain named formatting options, which will be filled by the value
+            of `epoch` and keys in `logs` (passed in `on_epoch_end`). For example:
+            if `filepath` is `model-{epoch:02d}-{val_loss:.2f}`, then the
+            model checkpoints will be saved with the epoch number and the
+            validation loss in the filename.
+        monitor (str): The metric name to monitor. Default to "val_loss".
         verbose (int): Verbosity mode, 0 or 1. Mode 0 is silent, and mode 1
             displays messages when the callback takes an action.
         save_best_only (bool): if `save_best_only=True`, it only saves when the model
             is considered the "best" and the latest best model according to the
-            quantity monitored will not be overwritten.
+            quantity monitored will not be overwritten. If `filepath` doesn't contain
+            formatting options like `{epoch}` then `filepath` will be overwritten by
+            each new better model locally. The model logged as artifact will still be
+            associated with the correct `monitor`. This will be reflected in the UI.
         save_weights_only (bool): if True, then only the model's weights will be saved.
         mode (Mode): one of {'auto', 'min', 'max'}. For `val_acc`, this should be `max`,
             for `val_loss` this should be `min`, etc.
