5b67671f9c2bb5a69bafc3fcdb9f4f460ac34e34
Dmitry Duev <dmitryduev@users.noreply.github.com>
2023-01-06 18:08:10 -0800

    feat(integrations): System Metrics for AWS Trainium (#4671)
    feat(integrations): System Metrics for AWS Trainium

--- wandb/sdk/internal/settings_static.py
+++ wandb/sdk/internal/settings_static.py
@@ -19,6 +19,7 @@ class SettingsStatic:
     _stats_sample_rate_seconds: float
     _stats_samples_to_average: int
     _stats_join_assets: bool
+    _stats_neuron_monitor_config_path: Optional[str]
     files_dir: str
     program_relpath: Optional[str]
     log_internal: str
