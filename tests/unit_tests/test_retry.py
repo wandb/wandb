e853837ecaffa88c4e7c46d882b2fc6acf47f87b
Spencer Pearson <spencer.pearson@wandb.com>
2023-01-06 17:42:12 -0800

    pull Backoff-related tests out of Async block, since they're not necessarily async-related

--- tests/unit_tests/test_retry.py
+++ tests/unit_tests/test_retry.py
@@ -149,139 +149,142 @@ class MyError(Exception):
 SECOND = datetime.timedelta(seconds=1)
 
 
-class TestAsync:
-    class TestFilteredBackoff:
-        def test_reraises_exc_failing_predicate(self):
-            wrapped = mock.Mock(spec=retry.Backoff)
-            filtered = retry.FilteredBackoff(
-                filter=lambda e: False,
-                wrapped=wrapped,
-            )
+class TestFilteredBackoff:
+    def test_reraises_exc_failing_predicate(self):
+        wrapped = mock.Mock(spec=retry.Backoff)
+        filtered = retry.FilteredBackoff(
+            filter=lambda e: False,
+            wrapped=wrapped,
+        )
 
-            with pytest.raises(MyError):
-                filtered.next_sleep_or_reraise(MyError("don't retry me"))
+        with pytest.raises(MyError):
+            filtered.next_sleep_or_reraise(MyError("don't retry me"))
 
-            wrapped.next_sleep_or_reraise.assert_not_called()
+        wrapped.next_sleep_or_reraise.assert_not_called()
 
-        def test_delegates_exc_passing_predicate(self):
-            retriable_exc = MyError("retry me")
-            wrapped = mock.Mock(
-                spec=retry.Backoff,
-                next_sleep_or_reraise=mock.Mock(return_value=123 * SECOND),
-            )
-            filtered = retry.FilteredBackoff(
-                filter=lambda e: e == retriable_exc,
-                wrapped=wrapped,
-            )
+    def test_delegates_exc_passing_predicate(self):
+        retriable_exc = MyError("retry me")
+        wrapped = mock.Mock(
+            spec=retry.Backoff,
+            next_sleep_or_reraise=mock.Mock(return_value=123 * SECOND),
+        )
+        filtered = retry.FilteredBackoff(
+            filter=lambda e: e == retriable_exc,
+            wrapped=wrapped,
+        )
 
-            assert filtered.next_sleep_or_reraise(retriable_exc) == 123 * SECOND
-            wrapped.next_sleep_or_reraise.assert_called_once_with(retriable_exc)
+        assert filtered.next_sleep_or_reraise(retriable_exc) == 123 * SECOND
+        wrapped.next_sleep_or_reraise.assert_called_once_with(retriable_exc)
 
-    class TestRetryLoopLoggingBackoff:
-        def test_respects_max_retries(self, mock_time: MockTime):
-            events = []
-            backoff = retry.RetryLoopLoggingBackoff(
-                wrapped=mock.MagicMock(spec=retry.Backoff),
-                on_loop_start=lambda e: events.append(("start", e)),
-                on_loop_end=lambda dt: events.append(("end", dt)),
-            )
 
-            excs = [MyError(str(i)) for i in range(3)]
+class TestRetryLoopLoggingBackoff:
+    def test_respects_max_retries(self, mock_time: MockTime):
+        events = []
+        backoff = retry.RetryLoopLoggingBackoff(
+            wrapped=mock.MagicMock(spec=retry.Backoff),
+            on_loop_start=lambda e: events.append(("start", e)),
+            on_loop_end=lambda dt: events.append(("end", dt)),
+        )
 
-            with backoff:
-                backoff.next_sleep_or_reraise(excs[0])
-                mock_time.sleep(1.0)
-                backoff.next_sleep_or_reraise(excs[1])
-                mock_time.sleep(1.0)
-                backoff.next_sleep_or_reraise(excs[2])
-                mock_time.sleep(1.0)
+        excs = [MyError(str(i)) for i in range(3)]
 
-            assert events == [("start", excs[0]), ("end", 3 * SECOND)]
+        with backoff:
+            backoff.next_sleep_or_reraise(excs[0])
+            mock_time.sleep(1.0)
+            backoff.next_sleep_or_reraise(excs[1])
+            mock_time.sleep(1.0)
+            backoff.next_sleep_or_reraise(excs[2])
+            mock_time.sleep(1.0)
+            assert events == [("start", excs[0])]
 
-    class TestExponentialBackoff:
-        def test_respects_max_retries(self):
-            backoff = retry.ExponentialBackoff(
-                initial_sleep=SECOND, max_sleep=SECOND, max_retries=3
-            )
-            for _ in range(3):
-                backoff.next_sleep_or_reraise(MyError())
-            with pytest.raises(MyError):
-                backoff.next_sleep_or_reraise(MyError())
+        assert events == [("start", excs[0]), ("end", 3 * SECOND)]
 
-        def test_respects_timeout(self, mock_time: MockTime):
-            t0 = mock_time.now()
-            dt = 300 * SECOND
-            backoff = retry.ExponentialBackoff(
-                initial_sleep=SECOND, max_sleep=10 * dt, timeout_at=t0 + dt
-            )
-            with pytest.raises(MyError):
-                for _ in range(9999):
-                    mock_time.sleep(
-                        backoff.next_sleep_or_reraise(MyError()).total_seconds()
-                    )
 
-            assert t0 + dt <= mock_time.now() <= t0 + 2 * dt
+class TestExponentialBackoff:
+    def test_respects_max_retries(self):
+        backoff = retry.ExponentialBackoff(
+            initial_sleep=SECOND, max_sleep=SECOND, max_retries=3
+        )
+        for _ in range(3):
+            backoff.next_sleep_or_reraise(MyError())
+        with pytest.raises(MyError):
+            backoff.next_sleep_or_reraise(MyError())
 
-    class TestRetryAsync:
-        def test_follows_backoff_schedule(self, mock_time: MockTime):
-            fn = mock.Mock(side_effect=MyError("oh no"))
-            with pytest.raises(MyError):
-                asyncio.run(
-                    retry.retry_async(
-                        mock.MagicMock(
-                            spec=retry.Backoff,
-                            next_sleep_or_reraise=mock.Mock(
-                                side_effect=[
-                                    1 * SECOND,
-                                    2 * SECOND,
-                                    MyError(),
-                                ]
-                            ),
-                        ),
-                        fn,
-                        "pos1",
-                        "pos2",
-                        kw1="kw1",
-                        kw2="kw2",
-                    )
+    def test_respects_timeout(self, mock_time: MockTime):
+        t0 = mock_time.now()
+        dt = 300 * SECOND
+        backoff = retry.ExponentialBackoff(
+            initial_sleep=SECOND, max_sleep=10 * dt, timeout_at=t0 + dt
+        )
+        with pytest.raises(MyError):
+            for _ in range(9999):
+                mock_time.sleep(
+                    backoff.next_sleep_or_reraise(MyError()).total_seconds()
                 )
 
-            mock_time.sleep_async.assert_has_calls(
-                [
-                    mock.call(1.0),
-                    mock.call(2.0),
-                ]
+        assert t0 + dt <= mock_time.now() <= t0 + 2 * dt
+
+
+class TestRetryAsync:
+    def test_follows_backoff_schedule(self, mock_time: MockTime):
+        fn = mock.Mock(side_effect=MyError("oh no"))
+        with pytest.raises(MyError):
+            asyncio.run(
+                retry.retry_async(
+                    mock.MagicMock(
+                        spec=retry.Backoff,
+                        next_sleep_or_reraise=mock.Mock(
+                            side_effect=[
+                                1 * SECOND,
+                                2 * SECOND,
+                                MyError(),
+                            ]
+                        ),
+                    ),
+                    fn,
+                    "pos1",
+                    "pos2",
+                    kw1="kw1",
+                    kw2="kw2",
+                )
             )
 
-            fn.assert_has_calls(
-                [
-                    mock.call("pos1", "pos2", kw1="kw1", kw2="kw2"),
-                    mock.call("pos1", "pos2", kw1="kw1", kw2="kw2"),
-                    mock.call("pos1", "pos2", kw1="kw1", kw2="kw2"),
-                ]
-            )
+        mock_time.sleep_async.assert_has_calls(
+            [
+                mock.call(1.0),
+                mock.call(2.0),
+            ]
+        )
 
-        def test_uses_backoff_context_manager(self, mock_time: MockTime):
-            backoff = mock.MagicMock(
-                spec=retry.Backoff,
-                next_sleep_or_reraise=mock.Mock(return_value=1 * SECOND),
-            )
+        fn.assert_has_calls(
+            [
+                mock.call("pos1", "pos2", kw1="kw1", kw2="kw2"),
+                mock.call("pos1", "pos2", kw1="kw1", kw2="kw2"),
+                mock.call("pos1", "pos2", kw1="kw1", kw2="kw2"),
+            ]
+        )
 
-            async def _fn():
-                assert backoff.__enter__.call_count == 1
-                assert backoff.__exit__.call_count == 0
+    def test_uses_backoff_context_manager(self, mock_time: MockTime):
+        backoff = mock.MagicMock(
+            spec=retry.Backoff,
+            next_sleep_or_reraise=mock.Mock(return_value=1 * SECOND),
+        )
 
-            fn = mock.AsyncMock(
-                wraps=_fn,
-                side_effect=[
-                    Exception("transient error"),
-                    Exception("transient error"),
-                    mock.DEFAULT,
-                ],
-            )
-
-            asyncio.run(retry.retry_async(backoff, fn))
-
-            assert fn.call_count == 3
+        async def _fn():
             assert backoff.__enter__.call_count == 1
-            assert backoff.__exit__.call_count == 1
+            assert backoff.__exit__.call_count == 0
+
+        fn = mock.AsyncMock(
+            wraps=_fn,
+            side_effect=[
+                Exception("transient error"),
+                Exception("transient error"),
+                mock.DEFAULT,
+            ],
+        )
+
+        asyncio.run(retry.retry_async(backoff, fn))
+
+        assert fn.call_count == 3
+        assert backoff.__enter__.call_count == 1
+        assert backoff.__exit__.call_count == 1
