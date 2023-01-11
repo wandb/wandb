aebe7110a16360ab66ef84e2d3c4c921a912a967
Katia <katia@wandb.com>
2023-01-11 11:38:15 -0800

    fix up

--- tests/unit_tests_old/tests_s_nb/test_notebooks.py
+++ tests/unit_tests_old/tests_s_nb/test_notebooks.py
@@ -181,10 +181,10 @@ def test_mocked_notebook_html_default(live_mock_server, test_settings, mocked_ip
     displayed_html = [args[0].strip() for args, _ in mocked_ipython.html.call_args_list]
     for i, html in enumerate(displayed_html):
         print(f"[{i}]: {html}")
-    assert len(displayed_html) == 7
+    assert len(displayed_html) == 9
     assert "lovely-dawn-32" in displayed_html[2]
-    assert "(success)" in displayed_html[3]
-    assert "Run history:" in displayed_html[4]
+    assert "(success)" in displayed_html[5]
+    assert "Run history:" in displayed_html[6]
 
 
 def test_mocked_notebook_html_quiet(live_mock_server, test_settings, mocked_ipython):
@@ -194,10 +194,10 @@ def test_mocked_notebook_html_quiet(live_mock_server, test_settings, mocked_ipyt
     displayed_html = [args[0].strip() for args, _ in mocked_ipython.html.call_args_list]
     for i, html in enumerate(displayed_html):
         print(f"[{i}]: {html}")
-    assert len(displayed_html) == 5
+    assert len(displayed_html) == 7
     assert "lovely-dawn-32" in displayed_html[2]
-    assert "(success)" in displayed_html[3]
-    assert "Run history:" not in displayed_html[4]
+    assert "(success)" in displayed_html[5]
+    assert "Run history:" not in displayed_html[6]
 
 
 def test_mocked_notebook_run_display(live_mock_server, test_settings, mocked_ipython):
@@ -207,8 +207,8 @@ def test_mocked_notebook_run_display(live_mock_server, test_settings, mocked_ipy
     displayed_html = [args[0].strip() for args, _ in mocked_ipython.html.call_args_list]
     for i, html in enumerate(displayed_html):
         print(f"[{i}]: {html}")
-    assert len(displayed_html) == 7
-    assert "<iframe" in displayed_html[3]
+    assert len(displayed_html) == 9
+    assert "<iframe" in displayed_html[5]
 
 
 def test_mocked_notebook_magic(live_mock_server, test_settings, mocked_ipython):
@@ -228,8 +228,8 @@ def test_mocked_notebook_magic(live_mock_server, test_settings, mocked_ipython):
     for i, html in enumerate(displayed_html):
         print(f"[{i}]: {html}")
     assert wandb.jupyter.__IFrame is None
-    # if versions are different this will fail (make sure you are up to date with master)
-    assert len(displayed_html) == 7
+    # if versions are different this will fail (make sure you are up-to-date with master)
+    assert len(displayed_html) == 9
     assert "<iframe" in displayed_html[2]
     magic.wandb("test/test/runs/test")
     displayed_html = [args[0].strip() for args, _ in mocked_ipython.html.call_args_list]
