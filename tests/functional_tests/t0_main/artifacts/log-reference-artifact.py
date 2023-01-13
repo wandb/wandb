78b4b1fb876b5fc1f0d2ffcb7057996bff272566
Tim Sweeney <tim@wandb.com>
2023-01-13 14:35:14 -0800

    maybe fix

--- tests/functional_tests/t0_main/artifacts/log-reference-artifact.py
+++ tests/functional_tests/t0_main/artifacts/log-reference-artifact.py
@@ -5,19 +5,20 @@ import wandb
 
 def main():
     with tempfile.TemporaryDirectory() as tmpdir:
-        local_path = tmpdir + "/hello.txt"
+        file_name = "hello.txt"
+        local_path = tmpdir + "/" + file_name
         with wandb.init() as run:
             artifact = wandb.Artifact("test-artifact", "test-type")
             with open(local_path, "w") as f:
                 f.write("hello world")
 
-            artifact.add_file(local_path)
+            artifact.add_file(local_path, file_name)
             run.log_artifact(artifact)
 
         with wandb.init() as run:
             artifact = wandb.Artifact("test-artifact-2", "test-type")
             art1 = run.use_artifact("test-artifact:latest")
-            local_entry = art1.get_path(local_path)
+            local_entry = art1.get_path(file_name)
             local_ref_path = local_entry.ref_url()
             artifact.add_reference(local_ref_path)
             run.log_artifact(artifact)
@@ -25,7 +26,7 @@ def main():
         with wandb.init() as run:
             artifact = wandb.Artifact("test-artifact-3", "test-type")
             art1 = run.use_artifact("test-artifact:latest")
-            local_entry = art1.get_path(local_path)
+            local_entry = art1.get_path(file_name)
             local_ref_path = local_entry.ref_url()
             # Blow away cache
             # This ensures our references work without having pre-loaded cache
