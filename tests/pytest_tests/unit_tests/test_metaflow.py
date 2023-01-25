2d785713bf448868b90e2e409ac645889cadd276
Jeff Raubitschek <jeff@wandb.com>
2023-01-25 12:40:14 -0800

    update

--- tests/pytest_tests/unit_tests/test_metaflow.py
+++ tests/pytest_tests/unit_tests/test_metaflow.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 if platform.system() == "Windows":
-    pytest.skip("metaflow does not support native Windows", allow_method_level=True)
+    pytest.skip("metaflow does not support native Windows", allow_module_level=True)
 
 import pandas as pd
 from metaflow import FlowSpec, step  # noqa: I100, I202
