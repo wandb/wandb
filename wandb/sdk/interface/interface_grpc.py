e6983bd2900c4b1a1427a10e9d63331eb27e4351
Jeff Raubitschek <jeff@wandb.com>
2023-01-25 06:32:41 -0800

    update

--- wandb/sdk/interface/interface_grpc.py
+++ wandb/sdk/interface/interface_grpc.py
@@ -351,6 +351,15 @@ class InterfaceGrpc(InterfaceBase):
         handle = self._deliver(result)
         return handle
 
+    def _deliver_check_version(self, check_version: pb.CheckVersionRequest) -> MailboxHandle:
+        assert self._stub
+        self._assign(check_version)
+        check_version_response = self._stub.CheckVersion(check_version)
+        response = pb.Response(check_version_response=check_version_response)
+        result = pb.Result(response=response)
+        handle = self._deliver(result)
+        return handle
+
     def _deliver_attach(self, attach: pb.AttachRequest) -> MailboxHandle:
         assert self._stub
         self._assign(attach)
