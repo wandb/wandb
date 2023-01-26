2cccef6ffd078d23dc6268d71bf33083f6a63fc8
Dmitry Duev <dima@wandb.com>
2023-01-25 23:30:56 -0800

    add ci jobs

--- tests/standalone_tests/executor_tests/executor-gunicorn.sh
+++ tests/standalone_tests/executor_tests/executor-gunicorn.sh
@@ -4,16 +4,16 @@ set -e
 script_dir=$(dirname "$(realpath "$0")")
 pushd $script_dir
 
-pex -r pex_requirements.txt -o pex_script.pex
-./pex_script.pex -- ./flask_app.py --port=8000 &
-flask_pid=$!
+#uwsgi --master --socket 0.0.0.0:6000 --protocol=http -w flask_app:app --processes 2&
+gunicorn -w 2 -b 0.0.0.0:6000 flask_app:app &
 
-sleep 10
+# Wait for uwsgi to start
+sleep 2
 
 # Loop to make 3 curl requests
 for i in {1..3}
 do
-    status_code=$(curl -s -w "%{http_code}" -o /dev/null http://localhost:8000/wandb)
+    status_code=$(curl -s -w "%{http_code}" -o /dev/null http://localhost:6000/wandb)
     if [ $status_code -eq 200 ]; then
         echo "Curl request $i succeeded"
     else
@@ -22,6 +22,7 @@ do
     fi
 done
 
-# Kill the process
-kill -9 $flask_pid
+# Kill the uwsgi process
+pkill -9 -f uwsgi
+
 popd
