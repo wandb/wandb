43ebd1884fe0f87e95e922ca8d9028af8ac18b00
Ben Sherman <bcanfieldsherman@gmail.com>
2023-01-31 16:59:38 -0800

    removed bad import structure added get_module for optionals

--- wandb/sdk/launch/environment/__init__.py
+++
@@ -1,5 +0,0 @@
-from .abstract import AbstractEnvironment
-from .aws_environment import AwsConfig, AwsEnvironment
-from .util import EnvironmentError
-
-__all__ = ["AbstractEnvironment", "AwsEnvironment", "AwsConfig", "EnvironmentError"]
