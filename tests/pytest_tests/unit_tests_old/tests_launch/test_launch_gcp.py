99a23ebae426bb37e95d029e7828b5e5a9cad463
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-02 12:24:14 -0800

    fixed isort version

--- tests/pytest_tests/unit_tests_old/tests_launch/test_launch_gcp.py
+++ tests/pytest_tests/unit_tests_old/tests_launch/test_launch_gcp.py
@@ -1,10 +1,9 @@
 import subprocess
 
 import pytest
-from google.cloud import aiplatform
-
 import wandb
 import wandb.sdk.launch.launch as launch
+from google.cloud import aiplatform
 from wandb.errors import LaunchError
 from wandb.sdk.launch.runner.gcp_vertex import (
     get_gcp_config,
