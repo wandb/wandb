3de045e18dbd5f468a066a2903357eee88ebe8e5
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-05 10:28:47 -0800

    added artifact registry to requirements
    added tests for gcp artifact registry
    
    fixed docker image name regex

--- tests/pytest_tests/unit_tests/test_launch/test_registry/test_gcp_artifact_registry.py
+++ tests/pytest_tests/unit_tests/test_launch/test_registry/test_gcp_artifact_registry.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 
 import pytest
+
 from wandb.sdk.launch.registry.google_artifact_registry import GoogleArtifactRegistry
 from wandb.sdk.launch.utils import LaunchError
 
