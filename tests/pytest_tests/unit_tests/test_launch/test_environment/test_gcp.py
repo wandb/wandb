ade85eef45b0fbb8d9b6975dd5e3ccadd7d13222
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-06 10:07:41 -0800

    added file upload + test to gcp

--- tests/pytest_tests/unit_tests/test_launch/test_environment/test_gcp.py
+++ tests/pytest_tests/unit_tests/test_launch/test_environment/test_gcp.py
@@ -52,3 +52,31 @@ def test_environment_verify_invalid_creds(mocker):
     credentials.refresh = MagicMock(side_effect=RefreshError("error"))
     with pytest.raises(LaunchError):
         GcpEnvironment("region")
+
+
+def test_upload_file(mocker):
+    credentials = MagicMock()
+    credentials.valid = True
+    mocker.patch(
+        "wandb.sdk.launch.environment.gcp_environment.google.auth.default",
+        return_value=(credentials, "project"),
+    )
+    mock_storage_client = MagicMock()
+    mocker.patch(
+        "wandb.sdk.launch.environment.gcp_environment.google.cloud.storage.Client",
+        mock_storage_client,
+    )
+    mock_bucket = MagicMock()
+    mock_storage_client.return_value.bucket.return_value = mock_bucket
+    mock_blob = MagicMock()
+    mock_bucket.blob.return_value = mock_blob
+    environment = GcpEnvironment("region", verify=False)
+    mocker.patch(
+        "wandb.sdk.launch.environment.gcp_environment.os.path.isfile",
+        return_value=True,
+    )
+    environment.upload_file("source", "gs://bucket/key")
+    mock_storage_client.assert_called_once_with(credentials=credentials)
+    mock_storage_client.return_value.bucket.assert_called_once_with("bucket")
+    mock_bucket.blob.assert_called_once_with("key")
+    mock_blob.upload_from_filename.assert_called_once_with("source")
