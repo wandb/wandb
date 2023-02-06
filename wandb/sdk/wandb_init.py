48139102511845ea08c6190675569d12c14a2e47
Katia <katia@wandb.com>
2023-02-06 13:30:09 -0800

    fix sb test + clean up

--- wandb/sdk/wandb_init.py
+++ wandb/sdk/wandb_init.py
@@ -51,7 +51,6 @@ if TYPE_CHECKING:
 
 logger: Optional[logging.Logger] = None  # logger configured during wandb.init()
 
-#testing!!
 
 def _set_logger(log_object: logging.Logger) -> None:
     """Configure module logger."""
