e060deefeee0d722f7558a0000eaa0cc6c80936a
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-06 16:00:48 -0800

    add abstract method decorator to abstract methods

--- wandb/sdk/launch/registry/abstract.py
+++ wandb/sdk/launch/registry/abstract.py
@@ -1,9 +1,11 @@
-from abc import ABC
+"""Abstract base class for registries."""
+from abc import ABC, abstractmethod
 
 
 class AbstractRegistry(ABC):
     """Abstract base class for registries."""
 
+    @abstractmethod
     def verify(self) -> None:
         """Verify that the registry is configured correctly."""
         raise NotImplementedError
