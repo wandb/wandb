745500b942ce78b99ae276719c871b8565576195
Kyle Goyette <kdgoyette@gmail.com>
2023-02-08 09:35:19 -0800

    fix last test

--- tests/pytest_tests/system_tests/tests_launch/test_launch_local_container.py
+++ tests/pytest_tests/system_tests/tests_launch/test_launch_local_container.py
@@ -1,4 +1,4 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 from wandb.apis.internal import Api
@@ -7,7 +7,26 @@ from wandb.sdk.launch.builder.loader import load_builder
 from wandb.sdk.launch.runner.loader import load_backend
 
 
-def test_local_container_entrypoint(relay_server, mock_run_local_container):
+def test_local_container_entrypoint(relay_server, monkeypatch):
+    def mock_run_entrypoint(*args, **kwargs):
+        # return first arg, which is command
+        print("ARGS: ", args)
+        return args[0]
+
+    monkeypatch.setattr(
+        "wandb.sdk.launch.runner.local_container._run_entry_point",
+        mock_run_entrypoint,
+    )
+
+    monkeypatch.setattr(
+        "wandb.sdk.launch.runner.local_container.pull_docker_image",
+        lambda x: None,
+    )
+    monkeypatch.setattr(
+        "wandb.sdk.launch.builder.docker.DockerBuilder.build_image",
+        lambda *args, **kwargs: "testimage",
+    )
+
     with relay_server():
         api = Api()
         runner = load_backend(
@@ -17,42 +36,36 @@ def test_local_container_entrypoint(relay_server, mock_run_local_container):
         )
         entity_name = "test_entity"
         project_name = "test_project"
-        entrypoint = ["python", "test.py"]
+        entry_command = ["python", "test.py"]
 
         # test with user provided image
         project = MagicMock()
+        entrypoint = EntryPoint("blah", entry_command)
         project.resource_args = {"local_container": {}}
         project.target_entity = entity_name
         project.target_project = project_name
+        project.name = None
+        project.run_id = "asdasd"
         project.override_config = {}
-        project.override_entrypoint = EntryPoint("blah", entrypoint)
+        project.override_entrypoint = entrypoint
+        project.get_single_entry_point.return_value = entrypoint
         project.override_args = {"a1": 20, "a2": 10}
         project.docker_image = "testimage"
+        project.image_name = "testimage"
         project.job = "testjob"
-        string_args = compute_command_args(project.override_args)
-        builder = load_builder({"type": "noop"})
+        string_args = " ".join(compute_command_args(project.override_args))
+        builder = load_builder({"type": "docker"})
 
         command = runner.run(
             launch_project=project, builder=builder, registry_config={}
         )
-        assert f"--entrypoint {entrypoint.join(' ')}" in command
+        assert f"--entrypoint {' '.join(entry_command)}" in command
         assert f"{project.docker_image} {string_args}" in command
-        # test with our image
+
+        # test with no user provided image
         project.docker_image = None
+        project.image_name = None
         command = runner.run(
             launch_project=project, builder=builder, registry_config={}
         )
-        assert "--entrypoint" not in command
-        assert f"WANDB_ARGS={string_args}" in command
-
-
-@pytest.fixture
-def mock_run_local_container(mocker):
-    def mock_run_entrypoint(*args, **kwargs):
-        # return first arg, which is command
-        return args[0]
-
-    mocker.patch(
-        "wandb.sdk.launch.runner.local_container._run_entry_point",
-        mock_run_entrypoint,
-    )
+        assert f"WANDB_ARGS='{string_args}'" in command
