6e599209324ae00067961a98a03b771605945c17
soumik12345 <19soumik.rakshit96@gmail.com>
2023-02-09 08:11:47 +0000

    fix: imports

--- wandb/integration/keras/callbacks/__init__.py
+++ wandb/integration/keras/callbacks/__init__.py
@@ -2,4 +2,4 @@ __all__ = ("WandbMetricsLogger", "WandbModelCheckpoint", "WandbEvalCallback")
 
 from .metrics_logger import WandbMetricsLogger
 from .model_checkpoint import WandbModelCheckpoint
-from .tables_builder import WandbEvalCallback
+from .eval_callback import WandbEvalCallback
