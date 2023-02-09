5d666b3f22cf13cdcd89fc596cfcf1f699d480ca
Katia <katia@wandb.com>
2023-02-09 00:49:55 -0800

    remove require-service

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -643,7 +643,7 @@ class Run:
         self._attach_pid = os.getpid()
 
         # for now, use runid as attach id, this could/should be versioned in the future
-        if self._settings._require_service:
+        if not self._settings._disable_service:
             self._attach_id = self._settings.run_id
 
     def _set_iface_pid(self, iface_pid: int) -> None:
@@ -777,7 +777,7 @@ class Run:
     def __getstate__(self) -> Any:
         """Custom pickler."""
         # We only pickle in service mode
-        if not self._settings or not self._settings._require_service:
+        if not self._settings or self._settings._disable_service:
             return
 
         _attach_id = self._attach_id
@@ -1962,7 +1962,7 @@ class Run:
             console = self._settings._console
         # only use raw for service to minimize potential changes
         if console == SettingsConsole.WRAP:
-            if self._settings._require_service:
+            if not self._settings._disable_service:
                 console = SettingsConsole.WRAP_RAW
             else:
                 console = SettingsConsole.WRAP_EMU
--- wandb/sdk/wandb_settings.py
+++ wandb/sdk/wandb_settings.py
@@ -369,9 +369,9 @@ class Settings:
     _console: SettingsConsole
     _cuda: str
     _disable_meta: bool
+    _disable_service: bool
     _disable_stats: bool
     _disable_viewer: bool  # Prevent early viewer query
-    _disable_service: bool
     _except_exit: bool
     _executable: str
     _flow_control_custom: bool
@@ -393,7 +393,6 @@ class Settings:
     _os: str
     _platform: str
     _python: str
-    _require_service: str
     _runqueue_item_id: str
     _save_requirements: bool
     _service_transport: str
@@ -506,9 +505,9 @@ class Settings:
         """
         return dict(
             _disable_meta={"preprocessor": _str_as_bool},
+            _disable_service={"preprocessor": _str_as_bool},
             _disable_stats={"preprocessor": _str_as_bool},
             _disable_viewer={"preprocessor": _str_as_bool},
-            _disable_service={"preprocessor": _str_as_bool},
             _network_buffer={"preprocessor": int},
             _colab={
                 "hook": lambda _: "google.colab" in sys.modules,
@@ -980,7 +979,7 @@ class Settings:
             if (
                 self._jupyter
                 or (self.start_method == "thread")
-                or self._require_service
+                or not self._disable_service
                 or self._windows
             ):
                 console = "wrap"
@@ -1361,8 +1360,7 @@ class Settings:
         env_prefix: str = "WANDB_"
         special_env_var_names = {
             "WANDB_TRACELOG": "_tracelog",
-            "WANDB_REQUIRE_SERVICE": "_require_service",
-            "WANDB_DISABE_SERVICE": "_disable_service",
+            "WANDB_DISABLE_SERVICE": "_disable_service",
             "WANDB_SERVICE_TRANSPORT": "_service_transport",
             "WANDB_DIR": "root_dir",
             "WANDB_NAME": "run_name",
