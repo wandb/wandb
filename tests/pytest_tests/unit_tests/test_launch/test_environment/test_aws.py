d93d7eb6f8695710ebe0eec3ce0185045436ce1f
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-10 10:34:20 -0800

    fixed AwsEnvironment.from_default test

--- tests/pytest_tests/unit_tests/test_launch/test_environment/test_aws.py
+++ tests/pytest_tests/unit_tests/test_launch/test_environment/test_aws.py
@@ -31,7 +31,7 @@ def test_from_default(mocker) -> None:
     mocker.patch(
         "wandb.sdk.launch.environment.aws_environment.AwsEnvironment", MagicMock()
     )
-    default_environment = AwsEnvironment.from_default(region="use-west-2", verify=False)
+    default_environment = AwsEnvironment.from_default(region="us-west-2", verify=False)
     assert default_environment._region == "us-west-2"
     assert default_environment._access_key == "access_key"
     assert default_environment._secret_key == "secret_key"
