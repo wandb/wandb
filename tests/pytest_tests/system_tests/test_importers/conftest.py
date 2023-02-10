ef0af40c54d85b1ac6be9f94f3050e80366614c9
Andrew Truong <itsandrewtruong@gmail.com>
2023-02-10 17:13:15 -0500

    fix fixture

--- tests/pytest_tests/system_tests/test_importers/conftest.py
+++ tests/pytest_tests/system_tests/test_importers/conftest.py
@@ -200,4 +200,4 @@ def mlflow_server():
 def prelogged_mlflow_server(mlflow_server):
     log_to_mlflow(mlflow_server)
     yield mlflow_server, EXPERIMENTS, RUNS_PER_EXPERIMENT, STEPS
-    yield mlflow_server, EXPERIMENTS, RUNS_PER_EXPERIMENT, STEPS
+    
