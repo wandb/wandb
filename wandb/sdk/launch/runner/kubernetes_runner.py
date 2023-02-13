3309e6daa5ab1c00527918bfafa3386c93998db9
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-13 12:43:04 -0800

    fixed bug that causes image pull secret to never be created

--- wandb/sdk/launch/runner/kubernetes_runner.py
+++ wandb/sdk/launch/runner/kubernetes_runner.py
@@ -416,8 +416,7 @@ def maybe_create_imagepull_secret(
     creds_info = {
         "auths": {
             registry.uri: {
-                "username": uname,
-                "password": token,
+                "auth": base64.b64encode(f"{uname}:{token}".encode()).decode(),
                 # need an email but the use is deprecated
                 "email": "deprecated@wandblaunch.com",
             }
@@ -433,6 +432,6 @@ def maybe_create_imagepull_secret(
         type="kubernetes.io/dockerconfigjson",
     )
     try:
-        core_api.create_namespaced_secret(namespace, secret)
+        return core_api.create_namespaced_secret(namespace, secret)
     except Exception as e:
         raise LaunchError(f"Exception when creating Kubernetes secret: {str(e)}\n")
