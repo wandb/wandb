c3f254b2c4e64c336dc8a4aa8f9af6950755f3fd
Bharat Ramanathan <ramanathan.parameshwaran@gohuddl.com>
2023-02-16 19:47:13 +0530

    feat(refactor): add top level import for ultralytics integration

---
+++ wandb/yolov8/__init__.py
@@ -0,0 +1,10 @@
+"""
+Compatibility yolov8 module
+
+In the future use e.g.:
+    from wandb.integration.yolov8 import WandbLogger
+"""
+
+from wandb.integration.yolov8 import WandbLogger
+
+__all__ = ("WandbLogger",)
