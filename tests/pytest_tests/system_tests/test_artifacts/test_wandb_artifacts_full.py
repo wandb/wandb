ab65f2baffcfd4092a6bc039a6436af40d826afb
Katia <katia@wandb.com>
2023-02-17 12:15:59 -0800

    add none back

--- tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts_full.py
+++ tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts_full.py
@@ -274,7 +274,7 @@ def test_artifact_wait_failure(wandb_init, timeout):
 # @pytest.mark.skip(
 #     reason="often makes tests time out on CI (despite only taking 3x10 seconds locally)"
 # )
-@pytest.mark.parametrize("async_upload_concurrency_limit", [1, 10])
+@pytest.mark.parametrize("async_upload_concurrency_limit", [None, 1, 10])
 def test_artifact_upload_succeeds_with_async(
     wandb_init: Callable[..., Run],
     async_upload_concurrency_limit: Optional[int],
