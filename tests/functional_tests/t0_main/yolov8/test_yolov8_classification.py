20716fa070dc189f0f97590a60a7e4bcbe9b1275
Bharat Ramanathan <ramanathan.parameshwaran@gohuddl.com>
2023-02-22 16:28:29 +0530

    chore(integrations): format with black and isort

--- tests/functional_tests/t0_main/yolov8/test_yolov8_classification.py
+++ tests/functional_tests/t0_main/yolov8/test_yolov8_classification.py
@@ -1,10 +1,15 @@
-from wandb.yolov8 import add_callbacks
 from ultralytics.yolo.engine.model import YOLO
+from wandb.yolov8 import add_callbacks
+
 
 def main():
     model = YOLO("yolov8n-cls.pt")
     model = add_callbacks(model)
-    model.train(data="mnist160", epochs=2, imgsz=32,)
+    model.train(
+        data="mnist160",
+        epochs=2,
+        imgsz=32,
+    )
 
 
 if __name__ == "__main__":
--- tests/functional_tests/t0_main/yolov8/test_yolov8_segmentation.py
+++ tests/functional_tests/t0_main/yolov8/test_yolov8_segmentation.py
@@ -1,10 +1,15 @@
-from wandb.yolov8 import add_callbacks
 from ultralytics.yolo.engine.model import YOLO
+from wandb.yolov8 import add_callbacks
+
 
 def main():
     model = YOLO("yolov8n-seg.pt")
     model = add_callbacks(model)
-    model.train(data="coco128-seg.yaml", epochs=2, imgsz=160,)
+    model.train(
+        data="coco128-seg.yaml",
+        epochs=2,
+        imgsz=160,
+    )
 
 
 if __name__ == "__main__":
