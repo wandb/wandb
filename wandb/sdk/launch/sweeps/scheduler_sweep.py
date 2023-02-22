3db9354498dc47c588861e01e1d094ee3fe2e980
gtarpenning <griffin.tarpenning@wandb.com>
2023-02-22 10:21:11 -0800

    refactor

--- wandb/sdk/launch/sweeps/scheduler_sweep.py
+++ wandb/sdk/launch/sweeps/scheduler_sweep.py
@@ -62,8 +62,9 @@ class SweepScheduler(Scheduler):
         if not self.is_alive():
             return False
         elif self.state == SchedulerState.FLUSH_RUNS:
-            # already hit run_cap, just no-op
+            # already hit run_cap, just noop
             return False
+
         # AgentHeartbeat wants a Dict of runs which are running or queued
         _run_states: Dict[str, bool] = {}
         for run_id, run in self._yield_runs():
@@ -81,41 +82,44 @@ class SweepScheduler(Scheduler):
         logger.debug(
             f"{LOG_PREFIX}AgentHeartbeat received {len(commands)} commands: \n{pprint.pformat(commands)}\n"
         )
-        if commands:
-            for command in commands:
-                # The command "type" can be one of "run", "resume", "stop", "exit"
-                _type = command.get("type", None)
-                if _type in ["exit", "stop"]:
-                    if command.get("run_cap") is not None:
-                        # Sweep hit run_cap, go into flushing state
-                        wandb.termlog(f"{LOG_PREFIX}Sweep hit run_cap, pausing.")
-                        self.state = SchedulerState.FLUSH_RUNS
-                    else:
-                        # Tell (virtual) agent to stop running
-                        self.state = SchedulerState.STOPPED
-                    return False
-                elif _type in ["run", "resume"]:
-                    _run_id = command.get("run_id", None)
-                    if _run_id is None:
-                        self.state = SchedulerState.FAILED
-                        raise SchedulerError(
-                            f"AgentHeartbeat command {command} missing run_id"
-                        )
-                    if _run_id in self._runs:
-                        wandb.termlog(f"{LOG_PREFIX}Skipping duplicate run {_run_id}")
-                    else:
-                        run = SweepRun(
-                            id=_run_id,
-                            args=command.get("args", {}),
-                            logs=command.get("logs", []),
-                            program=command.get("program", None),
-                            worker_id=worker_id,
-                        )
-                        self._runs[run.id] = run
-                        self._heartbeat_queue.put(run)
-                else:
+        if len(commands) == 0:
+            return False
+
+        for command in commands:
+            # The command "type" can be one of "run", "resume", "stop", "exit"
+            _type = command.get("type")
+            if _type in ["exit", "stop"]:
+                # If Sweep hit run_cap, go into flushing state
+                if command.get("run_cap"):
+                    wandb.termlog(
+                        f"{LOG_PREFIX}Sweep hit run_cap: {command.get('run_cap')}"
+                    )
+                    self.state = SchedulerState.FLUSH_RUNS
+                else:  # Else Tell (virtual) agent to stop running
+                    self.state = SchedulerState.STOPPED
+                return False
+            elif _type in ["run", "resume"]:
+                _run_id = command.get("run_id")
+                if not _run_id:
                     self.state = SchedulerState.FAILED
-                    raise SchedulerError(f"AgentHeartbeat unknown command type {_type}")
+                    raise SchedulerError(
+                        f"AgentHeartbeat command {command} missing run_id"
+                    )
+                if _run_id in self._runs:
+                    wandb.termlog(f"{LOG_PREFIX}Skipping duplicate run {_run_id}")
+                else:
+                    run = SweepRun(
+                        id=_run_id,
+                        args=command.get("args", {}),
+                        logs=command.get("logs", []),
+                        program=command.get("program", None),
+                        worker_id=worker_id,
+                    )
+                    self._runs[run.id] = run
+                    self._heartbeat_queue.put(run)
+            else:
+                self.state = SchedulerState.FAILED
+                raise SchedulerError(f"AgentHeartbeat unknown command type {_type}")
         return True
 
     def _run(self) -> None:
