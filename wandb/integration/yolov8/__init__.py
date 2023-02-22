c20444400da86c2b4b7a18a12b7b06e4d352b5f4
Bharat Ramanathan <ramanathan.parameshwaran@gohuddl.com>
2023-02-22 16:42:50 +0530

    fix(integrations): remove wandb logger from module imports

--- wandb/integration/yolov8/__init__.py
+++ wandb/integration/yolov8/__init__.py
@@ -2,6 +2,6 @@
 Tools for integrating `wandb` with [`ultralytics YOLOv8`](https://docs.ultralytics.com/),
 a computer vision framework for training and deploying YOLOv8 models.
 """
-from .yolov8 import WandbLogger, add_callbacks
+from .yolov8 import add_callbacks
 
-__all__ = ("WandbLogger", "add_callbacks")
+__all__ = ("add_callbacks",)
--- wandb/yolov8/__init__.py
+++ wandb/yolov8/__init__.py
@@ -5,6 +5,6 @@ In the future use e.g.:
     from wandb.integration.yolov8 import WandbLogger
 """
 
-from wandb.integration.yolov8 import WandbLogger, add_callbacks
+from wandb.integration.yolov8 import add_callbacks
 
-__all__ = ("WandbLogger", "add_callbacks")
+__all__ = ("add_callbacks",)
