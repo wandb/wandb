f853eb9585a1cc6ed868a30e3758fd0c9c67f50d
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-23 13:26:13 -0800

    Update wandb/sdk/launch/launch_add.py
    Co-authored-by: Griffin Tarpenning <griffin.tarpenning@wandb.com>

--- wandb/sdk/launch/launch_add.py
+++ wandb/sdk/launch/launch_add.py
@@ -174,8 +174,7 @@ def _launch_add(
             launch_spec["job"] = None
 
         launch_project = create_project_from_spec(launch_spec, api)
-        config = config or {}
-        docker_image_uri = build_image_from_project(launch_project, api, config)
+        docker_image_uri = build_image_from_project(launch_project, api, config or {})
         run = wandb.run or wandb.init(
             project=launch_spec["project"],
             entity=launch_spec["entity"],
