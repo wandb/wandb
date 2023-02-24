07688884aa834cf2d1e538817c9e90f11b55e4cc
Ben Sherman <bcanfieldsherman@gmail.com>
2023-02-24 11:48:20 -0800

    adding get_username_password as abstractmethod

--- wandb/sdk/launch/registry/abstract.py
+++ wandb/sdk/launch/registry/abstract.py
@@ -1,5 +1,6 @@
 """Abstract base class for registries."""
 from abc import ABC, abstractmethod
+from typing import Tuple
 
 
 class AbstractRegistry(ABC):
@@ -8,4 +9,13 @@ class AbstractRegistry(ABC):
     @abstractmethod
     def verify(self) -> None:
         """Verify that the registry is configured correctly."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_username_password(self) -> Tuple[str, str]:
+        """Get the username and password for the registry.
+
+        Returns:
+            (str, str): The username and password.
+        """
         raise NotImplementedError
