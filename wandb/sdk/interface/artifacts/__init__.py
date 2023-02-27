d5c369b4df2222f24010a4188c82ff620b78fef4
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-02-27 15:52:48 -0800

    Pull ArtifactsCache out of artifacts.py

--- wandb/sdk/interface/artifacts/__init__.py
+++ wandb/sdk/interface/artifacts/__init__.py
@@ -1,14 +1,20 @@
-from .artifacts import (
+from wandb.sdk.interface.artifacts.artifacts import (
     Artifact,
     ArtifactManifest,
     ArtifactManifestEntry,
     ArtifactNotLoggedError,
-    ArtifactsCache,
-    get_artifacts_cache,
     get_new_staging_file,
     get_staging_dir,
 )
-from .artifacts_storage import StorageHandler, StorageLayout, StoragePolicy
+from wandb.sdk.interface.artifacts.artifacts_cache import (
+    ArtifactsCache,
+    get_artifacts_cache,
+)
+from wandb.sdk.interface.artifacts.artifacts_storage import (
+    StorageHandler,
+    StorageLayout,
+    StoragePolicy,
+)
 
 __all__ = [
     "Artifact",
