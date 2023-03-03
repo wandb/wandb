8ba6dc3b6f957bade6ce29d2279051c5626e3963
Tim Sweeney <tim@wandb.com>
2023-03-02 16:54:31 -0800

    one more

--- tests/pytest_tests/unit_tests/test_dtypes.py
+++ tests/pytest_tests/unit_tests/test_dtypes.py
@@ -136,10 +136,10 @@ def test_object_type():
 
 
 def test_list_type():
-    assert ListType(int).assign([]) == ListType(int)
+    assert ListType(int).assign([]) == ListType(int, 0)
     assert ListType(int, 0).assign([]) == ListType(int, 0)
     assert ListType(int, 3).assign([]) == ListType(int)
-    assert ListType(int).assign([1, 2, 3]) == ListType(int)
+    assert ListType(int).assign([1, 2, 3]) == ListType(int, 3)
     assert ListType(int, 3).assign([1, 2, 3]) == ListType(int, 3)
     assert ListType(int, 5).assign([1, 2, 3]) == ListType(int)
     assert ListType(int).assign([1, "a", 3]) == InvalidType()
