1f9d87e4afc461ff00293571465cac0864f03263
Kyle Goyette <kdgoyette@gmail.com>
2023-03-08 17:03:01 -0800

    remove gcr stufff

--- wandb/sdk/launch/registry/google_artifact_registry.py
+++ wandb/sdk/launch/registry/google_artifact_registry.py
@@ -182,19 +182,22 @@ class GoogleArtifactRegistry(AbstractRegistry):
         _logger.info(
             f"Checking if image {image_uri} exists. In Google Artifact Registry {self.uri}."
         )
-        repo_uri, _ = image_uri.split(":")
-        if repo_uri != self.get_repo_uri():
-            raise LaunchError(
-                f"The image {image_uri} does not belong to the Google Artifact "
-                f"Repository {self.get_repo_uri()}."
-            )
-        credentials = self.environment.get_credentials()
-        request = google.cloud.artifactregistry.GetTagRequest(parent=image_uri)
-        client = google.cloud.artifactregistry.ArtifactRegistryClient(
-            credentials=credentials
-        )
-        try:
-            client.get_tag(request=request)
-            return True
-        except google.api_core.exceptions.NotFound:
-            return False
+
+        return False
+        # TODO: Test GCP Artifact Registry image exists to get working
+        # repo_uri, _ = image_uri.split(":")
+        # if repo_uri != self.get_repo_uri():
+        #     raise LaunchError(
+        #         f"The image {image_uri} does not belong to the Google Artifact "
+        #         f"Repository {self.get_repo_uri()}."
+        #     )
+        # credentials = self.environment.get_credentials()
+        # request = google.cloud.artifactregistry.GetTagRequest(parent=image_uri)
+        # client = google.cloud.artifactregistry.ArtifactRegistryClient(
+        #     credentials=credentials
+        # )
+        # try:
+        #     client.get_tag(request=request)
+        #     return True
+        # except google.api_core.exceptions.NotFound:
+        #     return False
