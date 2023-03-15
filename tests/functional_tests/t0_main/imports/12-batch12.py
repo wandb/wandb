eb1fecc4016d20305e06a573e4e3edb9913bac8b
ayulockin <mein2work@gmail.com>
2023-03-15 18:32:23 +0000

    code-check

--- tests/functional_tests/t0_main/imports/12-batch12.py
+++ tests/functional_tests/t0_main/imports/12-batch12.py
@@ -36,11 +36,11 @@ assert:
     - 85 # trlx
 """
 
+import diffusers  # noqa: F401
 import keras_cv  # noqa: F401
 import mmengine  # noqa: F401
-import diffusers # noqa: F401
-import trl # noqa: F401
-import trlx # noqa: F401
+import trl  # noqa: F401
+import trlx  # noqa: F401
 import wandb
 
 run = wandb.init()
