3fbf9707786295c2a6e6f4f9804c6786e8c1ab4c
Kyle Goyette <kyle@wandb.com>
2023-04-07 11:48:03 -0400

    wip

--- tests/pytest_tests/system_tests/tests_launch/test_launch_kubernetes.py
+++ tests/pytest_tests/system_tests/tests_launch/test_launch_kubernetes.py
@@ -100,7 +100,7 @@ def test_kubernetes_run_with_annotations(relay_server, monkeypatch, assets_path)
         project.target_entity = entity_name
         project.target_project = project_name
         project.override_config = {}
-        project.override_args = {"a": 2}
+        project.override_args = ["-a", "2"]
         project.job = "testjob"
         run = runner.run(launch_project=project, builder=builder)
     assert run.name == expected_run_name
