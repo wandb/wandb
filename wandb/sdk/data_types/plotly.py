7e55a31249c8480b95fe92f10c0ea17f16074453
Jeff Raubitschek <jeff@wandb.com>
2023-04-07 12:42:45 -0700

    revert(sdk): accidental checkin of two fixes without a PR (#5305)
    * Revert "safer access to settings attributes in sentry"
    
    This reverts commit b440c0be9d750445949939145bcc66b7b41e4ec9.
    
    * Revert "weird mypy demands"
    
    This reverts commit 12e2905c311203dba7bd2669443e5a91f7cfdbf8.

--- wandb/sdk/data_types/plotly.py
+++ wandb/sdk/data_types/plotly.py
@@ -12,7 +12,7 @@ from .image import Image
 
 if TYPE_CHECKING:  # pragma: no cover
     import matplotlib  # type: ignore
-    import pandas as pd
+    import pandas as pd  # type: ignore
     import plotly  # type: ignore
 
     from ..wandb_artifacts import Artifact as LocalArtifact
