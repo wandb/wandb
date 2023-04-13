10173775bbea31855feca532bc0614b24a99a654
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-04-12 22:11:29 -0700

    Respond to reviewer comments

--- wandb/sdk/lib/filesystem.py
+++ wandb/sdk/lib/filesystem.py
@@ -136,8 +136,13 @@ def safe_open(
     """Open a file, ensuring any changes only apply atomically after close.
 
     This context manager ensures that even unsuccessful writes will not leave a "dirty"
-    file or overwrite good data, and that all temp data is cleaned up. Otherwise, the
-    semantics and behavior should be identical to the built-in open() function.
+    file or overwrite good data, and that all temp data is cleaned up.
+
+    The semantics and behavior are intended to be nearly identical to the built-in
+    open() function. Differences:
+        - It creates any parent directories that don't exist, rather than raising.
+        - In 'x' mode, it checks at the beginning AND end of the write and fails if the
+            file exists either time.
     """
     path = Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
@@ -145,15 +150,12 @@ def safe_open(
     if "x" in mode and path.exists():
         raise FileExistsError(f"{path!s} already exists")
 
-    if "r" in mode:
-        if not path.exists():
-            raise FileNotFoundError(f"{path!s} does not exist")
-        if "+" not in mode:
-            # This is read-only, so we can just open the original file.
-            # TODO (hugh): create a reflink and read from that.
-            with path.open(mode, *args, **kwargs) as f:
-                yield f
-            return
+    if "r" in mode and "+" not in mode:
+        # This is read-only, so we can just open the original file.
+        # TODO (hugh): create a reflink and read from that.
+        with path.open(mode, *args, **kwargs) as f:
+            yield f
+        return
 
     with tempfile.TemporaryDirectory(dir=path.parent) as tmp_dir:
         tmp_path = Path(tmp_dir) / path.name
