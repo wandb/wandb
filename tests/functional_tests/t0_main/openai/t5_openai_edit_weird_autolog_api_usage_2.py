079236ac51c464ecde6e3dd5589e88ec3af54767
Dmitry Duev <dima@wandb.com>
2023-04-19 14:53:25 -0700

    add another test

--- tests/functional_tests/t0_main/openai/t5_openai_edit_weird_autolog_api_usage_2.py
+++ tests/functional_tests/t0_main/openai/t5_openai_edit_weird_autolog_api_usage_2.py
@@ -4,22 +4,23 @@ from wandb.integration.openai import autolog
 
 
 def main():
-    # emulate the situation where there is no run created by the user
+    # emulate the situation where there is a run already created by the user
+    run = wandb.init(project="skunkworks")  # noqa: F841
 
     assert autolog._run is None
     assert autolog._AutologOpenAI__run_created_by_autolog is False
 
-    autolog.enable(init={"project": "skunkworks"})
+    autolog.enable(init={"project": "marmotworks"})
 
     assert autolog._run is not None
-    assert autolog._AutologOpenAI__run_created_by_autolog
+    assert autolog._AutologOpenAI__run_created_by_autolog is False
     assert wandb.run.settings.project == "skunkworks"
 
     autolog.enable(init={"project": "groundsquirrelworks"})
 
     assert autolog._run is not None
-    assert autolog._AutologOpenAI__run_created_by_autolog
-    assert wandb.run.settings.project == "groundsquirrelworks"
+    assert autolog._AutologOpenAI__run_created_by_autolog is False
+    assert wandb.run.settings.project == "skunkworks"
 
     request_kwargs = dict(
         model="text-davinci-edit-001",
@@ -32,7 +33,7 @@ def main():
     autolog.disable()
     assert autolog._run is None
     assert autolog._AutologOpenAI__run_created_by_autolog is False
-    assert wandb.run is None
+    assert wandb.run is not None
 
 
 if __name__ == "__main__":
