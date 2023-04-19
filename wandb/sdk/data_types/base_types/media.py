53f64477f14afce71f6ff2712e7d72c8fcef10a0
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-04-19 15:58:46 -0700

    Use util.FilePathStr consistently

--- wandb/sdk/data_types/base_types/media.py
+++ wandb/sdk/data_types/base_types/media.py
@@ -9,7 +9,6 @@ import wandb
 from wandb import util
 from wandb._globals import _datatypes_callback
 from wandb.sdk.lib import filesystem
-from wandb.util import FilePathStr
 
 from .wb_value import WBValue
 
@@ -206,7 +205,7 @@ class Media(WBValue):
                 assert isinstance(self._path, str)
                 assert isinstance(self._sha256, str)
                 artifact = run  # Checks if the concrete image has already been added to this artifact
-                name = artifact.get_added_local_path_name(FilePathStr(self._path))
+                name = artifact.get_added_local_path_name(util.FilePathStr(self._path))
                 if name is None:
                     if self._is_tmp:
                         name = os.path.join(
