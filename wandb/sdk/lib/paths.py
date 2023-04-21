0b41441bdc670897ad5b42d003425541b9003c9c
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-04-21 15:28:07 -0700

    Don't use to_posixpath yet

--- wandb/sdk/lib/paths.py
+++ wandb/sdk/lib/paths.py
@@ -1,4 +1,3 @@
-from pathlib import Path, PurePosixPath
 from typing import TYPE_CHECKING, Union
 
 if TYPE_CHECKING:
@@ -8,11 +7,3 @@ if TYPE_CHECKING:
 # modeled after the hint defined in the Python standard library's `typeshed`:
 # https://github.com/python/typeshed/blob/0b1cd5989669544866213807afa833a88f649ee7/stdlib/_typeshed/__init__.pyi#L56-L65
 StrPath = Union[str, "os.PathLike[str]"]
-
-
-def to_posixpath(path: StrPath) -> PurePosixPath:
-    path = Path(path)
-    anchor = path.anchor
-    if anchor:
-        return PurePosixPath("/") / path.relative_to(anchor).as_posix()
-    return PurePosixPath(path.as_posix())
