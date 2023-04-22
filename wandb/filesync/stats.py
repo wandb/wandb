22b9957071a61f7e703f3cf345245a6f3e260f66
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-04-22 06:24:21 -0700

    Move filesync.stats to LogicalPath as well

--- wandb/filesync/stats.py
+++ wandb/filesync/stats.py
@@ -2,6 +2,7 @@ import threading
 from typing import MutableMapping, NamedTuple
 
 import wandb
+from wandb.sdk.lib.paths import LogicalPath, StrPath
 
 
 class FileStats(NamedTuple):
@@ -27,12 +28,13 @@ class FileCountsByCategory(NamedTuple):
 
 class Stats:
     def __init__(self) -> None:
-        self._stats: MutableMapping[str, "FileStats"] = {}
+        self._stats: MutableMapping[LogicalPath, "FileStats"] = {}
         self._lock = threading.Lock()
 
     def init_file(
-        self, save_name: str, size: int, is_artifact_file: bool = False
+        self, save_name: StrPath, size: int, is_artifact_file: bool = False
     ) -> None:
+        save_name = LogicalPath(save_name)
         with self._lock:
             self._stats[save_name] = FileStats(
                 deduped=False,
@@ -42,7 +44,8 @@ class Stats:
                 artifact_file=is_artifact_file,
             )
 
-    def set_file_deduped(self, save_name: str) -> None:
+    def set_file_deduped(self, save_name: StrPath) -> None:
+        save_name = LogicalPath(save_name)
         with self._lock:
             orig = self._stats[save_name]
             self._stats[save_name] = orig._replace(
@@ -50,13 +53,15 @@ class Stats:
                 uploaded=orig.total,
             )
 
-    def update_uploaded_file(self, save_name: str, total_uploaded: int) -> None:
+    def update_uploaded_file(self, save_name: StrPath, total_uploaded: int) -> None:
+        save_name = LogicalPath(save_name)
         with self._lock:
             self._stats[save_name] = self._stats[save_name]._replace(
                 uploaded=total_uploaded,
             )
 
-    def update_failed_file(self, save_name: str) -> None:
+    def update_failed_file(self, save_name: StrPath) -> None:
+        save_name = LogicalPath(save_name)
         with self._lock:
             self._stats[save_name] = self._stats[save_name]._replace(
                 uploaded=0,
