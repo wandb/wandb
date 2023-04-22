6915e6919f6237798345f7277b1f82f5e611c25a
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-04-22 11:06:17 -0700

    Add a new LogicalPath class along with extensive tests

---
+++ tests/pytest_tests/unit_tests/test_lib/test_paths.py
@@ -0,0 +1,146 @@
+import os
+import platform
+from pathlib import PurePath, PurePosixPath, PureWindowsPath
+
+import pytest
+from hypothesis import example, given, settings
+from hypothesis.strategies import binary, one_of, sampled_from, text
+from hypothesis_fspaths import fspaths
+from wandb.sdk.lib.paths import (
+    PROHIBITED_CHARS,
+    RESERVED_NAMES,
+    LogicalPath,
+    sanitize_path,
+)
+from wandb.util import to_forward_slash_path
+
+
+@pytest.mark.parametrize(
+    "target,posix,windows,_bytes",
+    [
+        ("foo/bar.txt", "foo/bar.txt", "foo\\bar.txt", b"foo/bar.txt"),
+        ("et/tu.txt", "et/tu.txt", "et/tu.txt", b"et\\tu.txt"),
+        ("/ab/ra.txt", "/ab/ra.txt", "\\ab\\ra.txt", b"/ab/ra.txt"),
+        ("C:/a/b.txt", "C:/a/b.txt", "C:\\a\\b.txt", b"C:/a/b.txt"),
+    ],
+)
+def test_path_groups(target, posix, windows, _bytes):
+    assert target == LogicalPath(target)
+    assert target == LogicalPath(PurePosixPath(posix))
+    assert target == LogicalPath(PureWindowsPath(windows))
+    assert target == LogicalPath(_bytes) or platform.system() != "Windows"
+
+
+@given(fspaths())
+@example(b"")
+@example(".")
+@example("\\")
+@example(PureWindowsPath("C:/foo/bar.txt"))
+@example(PurePosixPath("x\\a/b.bin"))
+def test_path_conversion(path):
+    logical_path = LogicalPath(path)
+    assert isinstance(logical_path, str)
+    if platform.system() == "Windows":
+        assert "\\" not in logical_path
+
+    # For compatibility, enforce output identical to `to_forward_slash_path`.
+    if isinstance(path, str):
+        assert logical_path == to_forward_slash_path(path)
+
+
+@given(fspaths())
+def test_logical_path_is_idempotent(path):
+    norm_path = LogicalPath(path)
+    logical_path = LogicalPath(norm_path)
+    assert logical_path == LogicalPath(logical_path)
+    assert logical_path == LogicalPath(to_forward_slash_path(logical_path))
+    assert logical_path == to_forward_slash_path(LogicalPath(logical_path))
+
+
+@given(fspaths())
+def test_logical_path_round_trip(path):
+    logical_path = LogicalPath(path)
+    posix_path = logical_path.to_path()
+    assert isinstance(posix_path, PurePosixPath)
+    assert posix_path == LogicalPath(posix_path).to_path()
+    assert str(posix_path) == LogicalPath(posix_path) or not path
+
+
+@given(fspaths(), fspaths())
+def test_logical_path_acts_like_posix_path(path1, path2):
+    path1 = LogicalPath(path1)
+    assert path1.is_absolute() == PurePosixPath(path1).is_absolute()
+    assert path1.parts == PurePosixPath(path1).parts
+    assert not path1.is_reserved()
+    if path1.is_absolute():
+        assert path1.root == "/"
+        assert path1.as_uri() == PurePosixPath(path1).as_uri()
+        assert not path1.relative_to(path1.root).is_absolute()
+    else:
+        assert path1.anchor == ""
+
+    assert path1.joinpath("bar").parent == path1
+
+    path2 = LogicalPath(path2)
+    assert path1 / path2 == path1.joinpath(path2)
+    assert path1 / PurePosixPath("/foo") == LogicalPath("/foo")
+
+
+@settings(max_examples=500)
+@given(one_of(fspaths(), text(), binary()))
+def test_sanitize_path_on_arbitrary_input(path):
+    try:
+        check_sanitized(sanitize_path(path))
+    except UnicodeDecodeError:
+        # Fuzz testing leads to some invalid UTF-8 sequences we don't try to handle.
+        pass
+
+
+@given(fspaths(), sampled_from(RESERVED_NAMES))
+def test_sanitize_path_on_paths_with_reserved_parts(path, name):
+    parts = list(PurePath(path_as_str(path)).parts)
+    for i in range(len(parts)):
+        cut = parts[:i] + [name] + parts[i:]
+        check_sanitized(sanitize_path(PurePath(*cut)))
+
+
+dangerous_chars = list(PROHIBITED_CHARS + "/\\ .")
+
+
+@given(fspaths(), text(dangerous_chars))
+def test_sanitize_path_on_paths_with_dangerous_chars(path, snippet):
+    path = path_as_str(path)
+    bookended = snippet + path + snippet
+    check_sanitized(sanitize_path(bookended))
+    if len(bookended) > 1:
+        m = len(bookended) // 2
+        inserted = bookended[:m] + snippet + bookended[m:]
+        check_sanitized(sanitize_path(inserted))
+
+
+def check_sanitized(path):
+    assert isinstance(path, PurePosixPath)
+    assert not path.is_absolute()
+
+    str_path = str(path)
+    assert "\\" not in str_path
+    assert all(c.isprintable() for c in str_path)
+    assert all(c not in r'<>:"\|?*' for c in str_path)
+    assert not str_path.endswith(" ")
+    assert not str_path.endswith(".") or str_path == "."
+
+    assert all(part not in RESERVED_NAMES for part in path.parts)
+    assert path.stem not in RESERVED_NAMES
+
+
+def path_as_str(path):
+    if isinstance(path, (str, PurePath)):
+        return str(path)
+    if hasattr(path, "__fspath__"):
+        path = path.__fspath__()
+    if isinstance(path, bytes):
+        try:
+            return os.fsdecode(path)
+        except UnicodeDecodeError:
+            return repr(path)
+    return str(path)
--- wandb/sdk/lib/paths.py
+++ wandb/sdk/lib/paths.py
@@ -1,7 +1,8 @@
-from typing import TYPE_CHECKING, NewType, Union
-
-if TYPE_CHECKING:
-    import os
+import os
+import platform
+from functools import wraps
+from pathlib import PurePath, PurePosixPath
+from typing import Any, NewType, Union
 
 # Path _inputs_ should generally accept any kind of path. This is named the same and
 # modeled after the hint defined in the Python standard library's `typeshed`:
@@ -19,3 +20,132 @@ FilePathStr = NewType("FilePathStr", str)
 
 # A URI. Likewise, it should be a urllib.parse.ParseResult, but it's too late now.
 URIStr = NewType("URIStr", str)
+
+
+class LogicalPath(str):
+    """A string that represents a path relative to an artifact or run.
+
+    The format of the string is always as a POSIX path, e.g. "foo/bar.txt".
+
+    A neat trick is that you can use this class as if it were a PurePosixPath. E.g.:
+    ```
+    >>> path = LogicalPath("foo/bar.txt")
+    >>> path.parts
+    ('foo', 'bar.txt')
+    >>> path.parent / "baz.txt"
+    'foo/baz.txt'
+    >>> type(path.relative_to("foo"))
+    LogicalPath
+    ```
+    """
+
+    # It should probably always be a relative path, but that would be a behavior change.
+    #
+    # These strings used to be the output of `to_forward_slash_path`, which only works
+    # with strings and whose behavior is pretty simple:
+    # ```
+    # if platform.system() == "Windows":
+    #     path = path.replace("\\", "/")
+    # ```
+    #
+    # This results in some weird things, such as backslashes being allowed from
+    # non-Windows platforms (which would probably break if such an artifact was used
+    # from Windows) and anchors or absolute paths being allowed. E.g., the Windows path
+    # "C:\foo\bar.txt" becomes "C:/foo/bar.txt", which then would mount as
+    # "./artifacts/artifact_name:v0/C:/foo/bar.txt" on MacOS and as
+    # "./artifacts/artifact_name-v0/C-/foo/bar.txt" on Windows.
+    #
+    # This implementation preserves behavior for strings but attempts to sanitize other
+    # formerly unsupported inputs more aggressively. It uses the `.as_posix()` form of
+    # pathlib objects rather than the `str()` form to reduce how often identical inputs
+    # will result in different outputs on different platforms; however, it doesn't alter
+    # absolute paths or check for prohibited characters etc.
+
+    def __new__(cls, path: StrPath) -> "LogicalPath":
+        if hasattr(path, "as_posix"):
+            return super().__new__(cls, path.as_posix())
+        if hasattr(path, "__fspath__"):
+            path = path.__fspath__()  # Can be str or bytes.
+        if isinstance(path, bytes):
+            path = os.fsdecode(path)
+        path = str(path)
+        if platform.system() == "Windows":
+            path = path.replace("\\", "/")
+        return super().__new__(cls, path)
+
+    def to_path(self) -> PurePosixPath:
+        """Convert this path to a PurePosixPath."""
+        return PurePosixPath(self)
+
+    def __getattr__(self, attr: str) -> Any:
+        """Act like a subclass of PurePosixPath for all methods not defined on str."""
+        result = getattr(self.to_path(), attr)
+        if isinstance(result, PurePosixPath):
+            return LogicalPath(result)
+        if callable(result):
+
+            @wraps(result)
+            def wrapper(*args: Any, **kwargs: Any) -> Any:
+                inner_result = result(*args, **kwargs)
+                if isinstance(inner_result, PurePosixPath):
+                    return LogicalPath(inner_result)
+                return inner_result
+
+            return wrapper
+        return result
+
+    def __truediv__(self, other: StrPath) -> "LogicalPath":
+        """Act like a PurePosixPath for the / operator, but return a LogicalPath."""
+        return LogicalPath(self.to_path() / other)
+
+
+PROHIBITED_CHARS = r'<>:"|?*'
+RESERVED_NAMES = (
+    ["CON", "PRN", "AUX", "NUL"]
+    + ["COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9"]
+    + ["LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"]
+)
+
+
+def sanitize_path(path: StrPath) -> PurePosixPath:
+    """Convert a path to a Path that can be used as a relative path on any platform.
+
+    This destructively modifies the path to be POSIX-style, removing anchors, converting
+    "legal" backslashes to forward slashes, and removing or replacing all characters
+    that might be allowed locally but are not allowed universally.
+    """
+    # Convert any non-pathlib object to a native PurePath.
+    if not isinstance(path, PurePath) and hasattr(path, "__fspath__"):
+        path = path.__fspath__()
+    if isinstance(path, bytes):
+        path = os.fsdecode(path)
+    if isinstance(path, str):
+        path = PurePath(path)
+
+    # If absolute, make relative to the root/drive. Either way, convert to a posix str.
+    path_str = path.relative_to(path.anchor).as_posix()
+
+    # Remove unprintable characters.
+    path_str = "".join(c for c in path_str if c.isprintable())
+
+    # Replace all backslashes with forward slashes.
+    path = PurePosixPath(path_str.replace("\\", "/"))
+
+    # We do this again because the previous steps may have introduced a new root.
+    path_str = path.relative_to(path.anchor).as_posix()
+
+    # Replace characters not allowed in Windows filenames.
+    path_str = "".join(c if c not in PROHIBITED_CHARS else "_" for c in path_str)
+
+    # Strip trailing dots and spaces (another Windows requirement).
+    # Also normalize by eliminating trailing slashes.
+    path_str = path_str.rstrip(" ./")
+
+    # Alter any path parts that are reserved on Windows.
+    parts = list(PurePosixPath(path_str).parts)
+    parts = [part if part not in RESERVED_NAMES else f"_{part}" for part in parts]
+    posix_path = PurePosixPath(*parts)
+    if posix_path.name.split(".")[0] in RESERVED_NAMES:
+        posix_path = posix_path.with_name(f"_{posix_path.name}")
+
+    return posix_path
