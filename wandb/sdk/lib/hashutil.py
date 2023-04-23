37a10149e99825a6bb061f70f85aa49e71b7351a
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-04-23 10:20:06 -0700

    Add V1 of new digest hierarchy

--- wandb/sdk/lib/hashutil.py
+++ wandb/sdk/lib/hashutil.py
@@ -1,13 +1,49 @@
 import base64
+import binascii
 import hashlib
 import sys
-from typing import NewType, Union
+from typing import TYPE_CHECKING, Union
 
-from wandb.sdk.lib.paths import StrPath
+if TYPE_CHECKING:
+    import os
 
-ETag = NewType("ETag", str)
-HexMD5 = NewType("HexMD5", str)
-B64MD5 = NewType("B64MD5", str)
+
+StrPath = Union[str, "os.PathLike[str]"]
+
+
+class Digest(str):
+    def __new__(cls, digest: str) -> "Digest":
+        if isinstance(digest, "Digest"):
+            return cls.from_bytes(bytes(digest))
+        if isinstance(digest, bytes):
+            return cls.from_bytes(digest)
+        return super().__new__(cls, digest)
+
+    def __init__(self) -> None:
+        """Ensure the input is a valid digest."""
+        raise NotImplementedError
+
+    def __bytes__(self) -> bytes:
+        raise NotImplementedError
+
+    def from_bytes(self, bytes_: bytes) -> "Digest":
+        raise NotImplementedError
+
+
+class MD5Digest(Digest):
+    """Abstract class with methods common to MD5 digests."""
+
+    @classmethod
+    def hash_bytes(cls, bytes_: bytes) -> "MD5Digest":
+        return cls.from_bytes(_md5(bytes_).digest())
+
+    @classmethod
+    def hash_string(cls, string: str) -> "MD5Digest":
+        return cls.hash_bytes(string.encode("utf-8"))
+
+    @classmethod
+    def hash_files(cls, *paths: StrPath) -> "MD5Digest":
+        return cls.from_bytes(_md5_file_hasher(*paths).digest())
 
 
 def _md5(data: bytes = b"") -> "hashlib._Hash":
@@ -18,33 +54,6 @@ def _md5(data: bytes = b"") -> "hashlib._Hash":
         return hashlib.md5(data)
 
 
-def md5_string(string: str) -> B64MD5:
-    return _b64_from_hasher(_md5(string.encode("utf-8")))
-
-
-def _b64_from_hasher(hasher: "hashlib._Hash") -> B64MD5:
-    return B64MD5(base64.b64encode(hasher.digest()).decode("ascii"))
-
-
-def b64_to_hex_id(string: B64MD5) -> HexMD5:
-    return HexMD5(base64.standard_b64decode(string).hex())
-
-
-def hex_to_b64_id(encoded_string: Union[str, bytes]) -> B64MD5:
-    if isinstance(encoded_string, bytes):
-        encoded_string = encoded_string.decode("utf-8")
-    as_str = bytes.fromhex(encoded_string)
-    return B64MD5(base64.standard_b64encode(as_str).decode("utf-8"))
-
-
-def md5_file_b64(*paths: StrPath) -> B64MD5:
-    return _b64_from_hasher(_md5_file_hasher(*paths))
-
-
-def md5_file_hex(*paths: StrPath) -> HexMD5:
-    return HexMD5(_md5_file_hasher(*paths).hexdigest())
-
-
 def _md5_file_hasher(*paths: StrPath) -> "hashlib._Hash":
     md5_hash = _md5()
     for path in sorted(str(p) for p in paths):
@@ -52,3 +61,86 @@ def _md5_file_hasher(*paths: StrPath) -> "hashlib._Hash":
             for chunk in iter(lambda: f.read(64 * 1024), b""):
                 md5_hash.update(chunk)
     return md5_hash
+
+
+class B64MD5(MD5Digest):
+    """Base64 encoded MD5 digest."""
+
+    def __init__(self) -> None:
+        try:
+            assert len(bytes(self)) == 16
+        except (AssertionError, ValueError, binascii.Error) as e:
+            raise ValueError(f"Invalid base-64 encoded MD5 digest: {self!r}") from e
+
+    def __bytes__(self) -> bytes:
+        return base64.standard_b64decode(self)
+
+    def from_bytes(self, bytes_: bytes) -> Digest:
+        return B64MD5(base64.standard_b64encode(bytes_).decode("ascii"))
+
+
+class HexMD5(MD5Digest):
+    """Hex encoded MD5 digest."""
+
+    def __init__(self) -> None:
+        try:
+            assert len(bytes(self)) == 16
+        except (AssertionError, ValueError) as e:
+            raise ValueError(f"Invalid hex encoded MD5 digest: {self!r}") from e
+
+    def __bytes__(self) -> bytes:
+        return bytes.fromhex(self)
+
+    def from_bytes(self, bytes_: bytes) -> Digest:
+        return HexMD5(bytes_.hex())
+
+
+class ETag(Digest):
+    """Entity Tag for an object in remote storage.
+
+    ETags are often but not always MD5 digests. Sometimes they are hex encoded,
+    sometimes base64 encoded; in general they are simply opaque values that change when
+    an object's contents change, so we can't validate them in any way.
+    """
+
+    def __init__(self) -> None:
+        # A base-64 encoded CRC32 is 6 characters long and a hex encoded FNV-1a can be
+        # up to 256 characters; lengths outside this range are unreasonable and probably
+        # a programming error (e.g. using the contents of the file instead of the ETag).
+        if len(self) < 6 or len(bytes(self)) > 256:
+            raise ValueError(f"Etags must be between 16 and 64 bytes: {self!r}")
+
+    def __bytes__(self) -> bytes:
+        """Convert the ETag to the bytes it represents.
+
+        In theory we can't differentiate between hex and base64 encodings of arbitrary
+        length, but the chance of ambiguity in practice is less than 1 in 16 million
+        even before counting length considerations. We don't attempt to decode other
+        encodings since we don't know what they might be.
+        """
+        # Try to hex decode it first, since that's basically guaranteed to fail if it
+        # isn't specifically a hex encoding.
+        # Assumption: no sane hex encoding uses mixed case, so we can discard those.
+        skip_hex = self.lower() != self and self.upper() != self
+        try:
+            if not skip_hex:
+                return bytes.fromhex(self)
+        except ValueError:
+            pass
+        # Otherwise, try base64 decoding it.
+        try:
+            return base64.standard_b64decode(self)
+        except (ValueError, binascii.Error):
+            pass
+        # Try an alternate base64 encoding.
+        try:
+            return base64.urlsafe_b64decode(self)
+        except (ValueError, binascii.Error):
+            raise ValueError(f"Unable to decode ETag: {self!r}")
+
+
+class RefDigest(Digest):
+    """Reference "digests" are URIs we use when we can't get the actual digest."""
+
+    def __init__(self) -> None:
+        pass
