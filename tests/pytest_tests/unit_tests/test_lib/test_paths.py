e8bb72ad746ac49c4226b89a4c0eedddd54e7684
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-05-04 17:41:47 -0700

    Use regexes and an exhaustive testing strategy

--- tests/pytest_tests/unit_tests/test_lib/test_paths.py
+++ tests/pytest_tests/unit_tests/test_lib/test_paths.py
@@ -1,4 +1,6 @@
+import itertools
 import platform
+import re
 from pathlib import PurePosixPath, PureWindowsPath
 
 import pytest
@@ -24,6 +26,54 @@ def test_path_groups(target, posix, windows, _bytes):
     assert target == LogicalPath(_bytes)
 
 
+def forward_slash_path_conversion(path):
+    # For compatibility, we want to enforce output identical to `to_forward_slash_path`.
+    # However, there are many paths that os.open etc. will coerce to the same path, and
+    # we'd like to keep only one canonical representation (otherwise we can break things
+    # by having artifact paths that have distinct keys but identical file paths).
+    #
+    # For completeness:
+    # 1. The empty path '.' or '' is always represented as '.'.
+    # 2. A directory is always represented as `.../d`, never as `.../d/` or `.../d/.`.
+    # 3. Current directories are folded: `./a/././b` is `a/b`.
+    # 4. Empty directories are folded: `a///b` is `a/b`.
+    #    4a. Anchors are preserved: `///a` is `/a`, `//a` is `//a`. ("//" is a separate
+    #        anchor from "/", but on posix systems "///" is considered "/").
+    #
+    # NOTE: there are still paths that collide! In particular, it's not possible in the
+    # presence of symlinks to determine whether `a/../b` and `b` are the same path
+    # without checking a specific filesystem, so those are still stored as separate
+    # paths, but will almost certainly break something if they're used.
+    #
+    # There are also deeply broken paths (mostly due to cross platform issues) that we
+    # aren't attempting to fix or work around and will just break in unpleasant ways.
+    canonical = to_forward_slash_path(path)
+
+    if re.match(r"//([^/]|$)", canonical):
+        anchor, body = "//", canonical[2:]  # The anchor is "//".
+    elif re.match(r"/+", canonical):
+        anchor, body = "/", canonical.lstrip("/")  # The anchor is "/".
+    else:
+        anchor, body = "", canonical  # There is no anchor.
+
+    while re.match(r"\./+", body):
+        body = re.sub(r"^\./+", "", body)  # Remove leading "./".
+    while re.findall(r"/+\./+", body):
+        body = re.sub(r"/+\./+", "/", body)  # Replace "/./" with "/".
+    while re.findall(r"/+\.?$", body):
+        body = re.sub(r"/+\.?$", "", body)  # Remove trailing "/." or "/".
+    while re.findall(r"//+", body):
+        body = re.sub(r"//+", "/", body)  # Replace "//" with "/".
+
+    if anchor and body == ".":
+        body = ""
+    canonical = anchor + body
+    if not canonical:
+        canonical = "."
+
+    return canonical
+
+
 @given(fspaths())
 @example(b"")
 @example(".")
@@ -32,28 +82,24 @@ def test_path_groups(target, posix, windows, _bytes):
 @example("/")
 @example(PureWindowsPath("C:/foo/bar.txt"))
 @example(PurePosixPath("x\\a/b.bin"))
-@pytest.mark.skip(reason="TODO: Hypothesis keeps finding corner cases.")
 def test_path_conversion(path):
     logical_path = LogicalPath(path)
     assert isinstance(logical_path, str)
     if platform.system() == "Windows":
         assert "\\" not in logical_path
 
-    # For compatibility, enforce output identical to `to_forward_slash_path`.
-    # Three exceptions, none should affect the interpretation of the path:
-    # 1. We send both `'.'` and `''` to `'.'`
-    # 2. We strip trailing slashes
-    # 3. We collapse multiple slashes (e.g. `'a//b'` -> `'a/b'`)
-    if path and isinstance(path, str):
-        canonical = to_forward_slash_path(path)
-        while "//" in canonical:
-            canonical = canonical.replace("//", "/")
-        # We strip trailing slashes, but obviously not the root path.
-        if canonical.startswith("/") and canonical.rstrip("/") == "":
-            canonical = "/"
-        else:
-            canonical = canonical.rstrip("/")
-        assert logical_path == canonical
+    if isinstance(path, str):
+        assert logical_path == forward_slash_path_conversion(path)
+
+
+def test_path_conversion_pathological():
+    path_chars = "./\\x"  # One normal character and three that cause problems.
+
+    # Exercise every path up to length 6 (all 5204 of them) consisting of path_chars.
+    for i in range(1, 5):
+        for seq in itertools.product(path_chars, repeat=i):
+            path = "".join(seq)
+            assert str(LogicalPath(path)) == forward_slash_path_conversion(path)
 
 
 @given(fspaths())
