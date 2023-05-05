2f3ba59ff8dfc9bbc5edc6b63222bbe8bd52fe16
Dmitry Duev <dima@wandb.com>
2023-05-05 13:42:56 -0700

    move tests around

--- tests/functional_tests/t0_main/llm/t1_llm_cohere_generate.py
+++ tests/functional_tests/t0_main/llm/t1_llm_cohere_generate.py
@@ -1,10 +1,7 @@
 import cohere
-import openai
 from wandb.integration.cohere import autolog as cohere_autolog
-from wandb.integration.openai import autolog as openai_autolog
 
 cohere_autolog()
-openai_autolog()
 
 
 def main():
@@ -20,19 +17,6 @@ def main():
     )
 
     print(prediction[0].text)
-
-    request_kwargs = dict(
-        engine="text-davinci-003",
-        prompt=prediction[0].text,
-        max_tokens=25,
-        temperature=0.1,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-
-    prediction = openai.Completion.create(**request_kwargs)
-    print(prediction.choices[0].text)
 
 
 if __name__ == "__main__":
