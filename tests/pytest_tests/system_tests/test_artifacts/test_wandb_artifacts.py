7fca2eceb4ce46e6b4d383b4b653d13a408998c4
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-05-06 10:10:46 -0700

    Fix expected error message

--- tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts.py
+++ tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts.py
@@ -251,7 +251,7 @@ def test_unsized_manifest_entry():
         wandb_artifacts.ArtifactManifestEntry(
             path="foo", digest="123", local_path="some/file.txt"
         )
-    assert "size required" in str(e.value)
+    assert "No such file" in str(e.value)
 
 
 def test_add_one_file():
