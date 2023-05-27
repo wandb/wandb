03751d233f8b6d471a0f960f97f1a72fae7a0079
Ben Sherman <bcanfieldsherman@gmail.com>
2023-05-26 17:40:29 -0700

    more redundancy collapses

--- wandb/sdk/launch/runner/kubernetes_runner.py
+++ wandb/sdk/launch/runner/kubernetes_runner.py
@@ -460,7 +460,6 @@ class KubernetesRunner(AbstractRunner):
                 raise LaunchError(
                     "Invalid specification of multiple containers. See https://docs.wandb.ai/guides/launch for guidance on submitting jobs."
                 )
-            launch_project.fill_macros(launch_project.docker_image)
             # dont specify run id if user provided image, could have multiple runs
             containers[0]["image"] = launch_project.docker_image
             # TODO: handle secret pulling image from registry
@@ -472,7 +471,6 @@ class KubernetesRunner(AbstractRunner):
             assert entry_point is not None
             assert builder is not None
             image_uri = builder.build_image(launch_project, entry_point)
-            launch_project.fill_macros(image_uri)
             # in the non instance case we need to make an imagePullSecret
             # so the new job can pull the image
             if not builder.registry:
@@ -489,6 +487,7 @@ class KubernetesRunner(AbstractRunner):
 
             containers[0]["image"] = image_uri
 
+        launch_project.fill_macros(launch_project.docker_image)
         inject_entrypoint_and_args(
             containers,
             entry_point,
--- wandb/sdk/launch/runner/local_container.py
+++ wandb/sdk/launch/runner/local_container.py
@@ -111,7 +111,6 @@ class LocalContainerRunner(AbstractRunner):
         if launch_project.docker_image:
             # user has provided their own docker image
             image_uri = launch_project.image_name
-            launch_project.fill_macros(image_uri)
             pull_docker_image(image_uri)
             entry_cmd = []
             if entry_point is not None:
@@ -133,7 +132,7 @@ class LocalContainerRunner(AbstractRunner):
                 launch_project,
                 entry_point,
             )
-            launch_project.fill_macros(image_uri)
+
             _logger.info(f"Docker image built with uri {image_uri}")
             # entry_cmd and additional_args are empty here because
             # if launch built the container they've been accounted
@@ -145,7 +144,7 @@ class LocalContainerRunner(AbstractRunner):
                     docker_args=docker_args,
                 )
             ).strip()
-
+        launch_project.fill_macros(image_uri)
         sanitized_cmd_str = sanitize_wandb_api_key(command_str)
         _msg = f"{LOG_PREFIX}Launching run in docker with command: {sanitized_cmd_str}"
         wandb.termlog(_msg)
