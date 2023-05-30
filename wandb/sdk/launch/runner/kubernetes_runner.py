e1c0de8e3b14d1c85114e022bdb06051bf7ee0f2
Kyle Goyette <kyle@wandb.com>
2023-05-30 14:34:04 -0700

    fix level

--- wandb/sdk/launch/runner/kubernetes_runner.py
+++ wandb/sdk/launch/runner/kubernetes_runner.py
@@ -392,7 +392,7 @@ class KubernetesRunner(AbstractRunner):
         )
         return (  # type: ignore[no-any-return]
             self.backend_config.get("runner", {}).get("namespace")
-            or resource_args.get("namespace")
+            or resource_args.get("metadata", {}).get("namespace")
             or default_namespace
         )
 
