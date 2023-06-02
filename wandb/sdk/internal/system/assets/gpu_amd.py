3c10466a916baac055d491ad45c4000ae10e66ef
Dmitry Duev <dima@wandb.com>
2023-06-02 00:25:13 -0700

    go paranoid

--- wandb/sdk/internal/system/assets/gpu_amd.py
+++ wandb/sdk/internal/system/assets/gpu_amd.py
@@ -65,27 +65,32 @@ class GPUAMDStats:
         """Parse stats from rocm-smi output."""
         parsed_stats: _Stats = {}
 
-        gpu = stats.get("GPU use (%)")
-        memory_allocated = stats.get("GPU memory use (%)")
-        temp = stats.get("Temperature (Sensor memory) (C)")
-        power_watts = stats.get("Average Graphics Package Power (W)")
-        average_graphics_package_power = stats.get("Average Graphics Package Power (W)")
-        max_graphics_package_power = stats.get("Max Graphics Package Power (W)")
-
-        if gpu:
-            parsed_stats["gpu"] = float(gpu)
-        if memory_allocated:
-            parsed_stats["memoryAllocated"] = float(memory_allocated)
-        if temp:
-            parsed_stats["temp"] = float(temp)
-        if power_watts:
-            parsed_stats["powerWatts"] = float(power_watts)
-        if average_graphics_package_power and max_graphics_package_power:
+        try:
+            parsed_stats["gpu"] = float(stats.get("GPU use (%)"))
+        except (TypeError, ValueError):
+            logger.warning("Could not parse GPU usage as float")
+        try:
+            parsed_stats["memoryAllocated"] = float(stats.get("GPU memory use (%)"))
+        except (TypeError, ValueError):
+            logger.warning("Could not parse GPU memory allocation as float")
+        try:
+            parsed_stats["temp"] = float(stats.get("Temperature (Sensor memory) (C)"))
+        except (TypeError, ValueError):
+            logger.warning("Could not parse GPU temperature as float")
+        try:
+            parsed_stats["powerWatts"] = float(
+                stats.get("Average Graphics Package Power (W)")
+            )
+        except (TypeError, ValueError):
+            logger.warning("Could not parse GPU power as float")
+        try:
             parsed_stats["powerPercent"] = (
-                float(average_graphics_package_power)
-                / float(max_graphics_package_power)
+                float(stats.get("Average Graphics Package Power (W)"))
+                / float(stats.get("Max Graphics Package Power (W)"))
                 * 100
             )
+        except (TypeError, ValueError):
+            logger.warning("Could not parse GPU average/max power as float")
 
         return parsed_stats
 
