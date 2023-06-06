29dad363dc27cdd25896a9d69078b3466b63775f
Dmitry Duev <dima@wandb.com>
2023-06-06 10:21:41 -0700

    chore(sdk): try to fix logical path/tb watcher bloopers on s3

--- wandb/sdk/internal/tb_watcher.py
+++ wandb/sdk/internal/tb_watcher.py
@@ -14,6 +14,7 @@ import wandb
 from wandb import util
 from wandb.sdk.interface.interface import GlobStr
 from wandb.sdk.lib import filesystem
+from wandb.sdk.lib.paths import LogicalPath
 from wandb.viz import CustomChart
 
 from . import run as internal_run
@@ -155,6 +156,8 @@ class TBWatcher:
     def add(self, logdir: str, save: bool, root_dir: str) -> None:
         # logdir = LogicalPath(logdir)
         # root_dir = LogicalPath(root_dir)
+        print(logdir, util.to_forward_slash_path(logdir), LogicalPath(root_dir))
+        print(root_dir, util.to_forward_slash_path(root_dir), LogicalPath(root_dir))
         if logdir in self._logdirs:
             return
         namespace = self._calculate_namespace(logdir, root_dir)
