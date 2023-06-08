1728dda569303d9c54c03a16ecb137b4cbb75a53
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2023-06-08 14:30:10 -0700

    docs(sdk): Fixed code snippet in docstring

--- wandb/sdk/artifacts/artifact.py
+++ wandb/sdk/artifacts/artifact.py
@@ -1457,7 +1457,7 @@ class Artifact:
             # Run using the artifact
             with wandb.init() as r:
                 artifact = r.use_artifact('my_dataset:latest')
-                table = r.get('my_table')
+                table = artifact.get('my_table')
             ```
         """
         if self._state == ArtifactState.PENDING:
