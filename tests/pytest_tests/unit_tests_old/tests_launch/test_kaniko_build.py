d10aec9e4e69394a7006ddf297d5b80b8902ea60
Ben Sherman <bcanfieldsherman@gmail.com>
2023-06-14 12:45:34 -0700

    fixing stinking test

--- tests/pytest_tests/unit_tests_old/tests_launch/test_kaniko_build.py
+++ tests/pytest_tests/unit_tests_old/tests_launch/test_kaniko_build.py
@@ -184,7 +184,6 @@ def test_create_kaniko_job_instance(mock_kubernetes_client, runner):
             MagicMock(),
             registry,
             build_context_store="s3://test-bucket/test-prefix",
-            image="gcr.io/kaniko-project/executor:v1.8.0",
         )
         job_name = "test_job_name"
         repo_url = "12345678.dkr.ecr.us-east-1.amazonaws.com/test-repo"
@@ -199,7 +198,7 @@ def test_create_kaniko_job_instance(mock_kubernetes_client, runner):
         assert job["metadata"]["labels"] == {"wandb": "launch"}
         assert (
             job["spec"]["template"]["spec"]["containers"][0]["image"]
-            == "gcr.io/kaniko-project/executor:v1.8.0"
+            == "gcr.io/kaniko-project/executor:v1.11.0"
         )
         assert job["spec"]["template"]["spec"]["containers"][0]["args"] == [
             f"--context={context_path}",
