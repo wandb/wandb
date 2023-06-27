1f5f65af2e090816e07ebdc4ec3c9791b41e8990
soumik12345 <19soumik.rakshit96@gmail.com>
2023-06-27 21:32:14 +0000

    add: prediction logic in plot_bboxes

--- wandb/integration/yolov8/bbox_utils.py
+++ wandb/integration/yolov8/bbox_utils.py
@@ -75,7 +75,7 @@ def create_prediction_metadata_map(model_predictions):
     """Create metadata map for model predictions by groupings them based on image ID."""
     pred_metadata_map = {}
     for prediction in model_predictions:
-        pred_metadata_map.setdefault(prediction['image_id'], [])
-        pred_metadata_map[prediction['image_id']].append(prediction)
+        pred_metadata_map.setdefault(prediction["image_id"], [])
+        pred_metadata_map[prediction["image_id"]].append(prediction)
 
     return pred_metadata_map
--- wandb/integration/yolov8/yolov8.py
+++ wandb/integration/yolov8/yolov8.py
@@ -1,16 +1,20 @@
+from typing import Union
+
 import wandb
 
 from ultralytics.yolo.v8.detect.train import DetectionTrainer
-from ultralytics.yolo.v8.detect.val import DetectionValidator
+from ultralytics.yolo.v8.detect.predict import DetectionPredictor
 
 from .bbox_utils import get_ground_truth_annotations, create_prediction_metadata_map
 
 
-def plot_bboxes(trainer: DetectionTrainer):
+def plot_bboxes(trainer: Union[DetectionTrainer, DetectionPredictor]):
     if isinstance(trainer, DetectionTrainer):
         dataloader = trainer.validator.dataloader
         class_label_map = trainer.validator.names
-        predictions_metadata_map = create_prediction_metadata_map(trainer.validator.jdict)
+        predictions_metadata_map = create_prediction_metadata_map(
+            trainer.validator.jdict
+        )
         class_label_dict = {
             idx: class_label_map[idx] for idx in range(len(class_label_map))
         }
@@ -37,3 +41,39 @@ def plot_bboxes(trainer: DetectionTrainer):
             if batch_idx + 1 == 1:
                 break
         wandb.log({"ground_truth": images}, commit=False)
+
+    elif isinstance(trainer, DetectionPredictor):
+        predictor = trainer
+        results = predictor.results
+        table = wandb.Table(columns=["Image", "Num-Objects", "Mean-Confidence"])
+        for idx, result in enumerate(results):
+            boxes = result.boxes.xywh.to("cpu").long().numpy()
+            classes = result.boxes.cls.to("cpu").long().numpy()
+            confidence = result.boxes.conf.to("cpu").numpy()
+            class_id_to_label = {int(k): str(v) for k, v in result.names.items()}
+            box_data, total_confidence = [], 0.0
+            for idx in range(len(boxes)):
+                box_data.append(
+                    {
+                        "position": {
+                            "middle": [int(boxes[idx][0]), int(boxes[idx][1])],
+                            "width": int(boxes[idx][2]),
+                            "height": int(boxes[idx][3]),
+                        },
+                        "domain": "pixel",
+                        "class_id": int(classes[idx]),
+                        "box_caption": class_id_to_label[int(classes[idx])],
+                        "scores": {"confidence": float(confidence[idx])},
+                    }
+                )
+                total_confidence += float(confidence[idx])
+            boxes = {
+                "predictions": {
+                    "box_data": box_data,
+                    "class_labels": class_id_to_label,
+                },
+            }
+            image = wandb.Image(result.orig_img[:, :, ::-1], boxes=boxes)
+            table.add_data(image, len(box_data), total_confidence / len(box_data))
+
+        wandb.log({"Object-Detection-Table": table})
