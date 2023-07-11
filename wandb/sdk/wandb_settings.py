bb1cbbe835ebb4f44f9eb6d14faa56f4c3d3b8ff
Ben Sherman <bcanfieldsherman@gmail.com>
2023-07-11 14:51:01 -0700

    changing back to WANDB_JOB_NAME

--- wandb/sdk/wandb_settings.py
+++ wandb/sdk/wandb_settings.py
@@ -1545,7 +1545,6 @@ class Settings(SettingsData):
             "WANDB_NOTES": "run_notes",
             "WANDB_TAGS": "run_tags",
             "WANDB_JOB_TYPE": "run_job_type",
-            "WANDB_LAUNCH_JOB_NAME": "job_name",
         }
         env = dict()
         for setting, value in environ.items():
