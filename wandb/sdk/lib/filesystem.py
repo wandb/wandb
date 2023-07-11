5ad91f3a24768043373eb62d962d293698f5098e
Jaron Maene <jaron.maene@kuleuven.be>
2023-07-11 10:17:58 +0200

    bug(mkdir_exists_ok): sweep crashes when folder is symlink

--- wandb/sdk/lib/filesystem.py
+++ wandb/sdk/lib/filesystem.py
@@ -17,19 +17,20 @@ logger = logging.getLogger(__name__)
 WRITE_PERMISSIONS = stat.S_IWUSR | stat.S_IWGRP | stat.S_IWOTH | stat.S_IWRITE
 
 
-def mkdir_exists_ok(dir_name: StrPath) -> None:
+def mkdir_exists_ok(dir_name: str) -> None:
     """Create `dir_name` and any parent directories if they don't exist.
 
     Raises:
         FileExistsError: if `dir_name` exists and is not a directory.
         PermissionError: if `dir_name` is not writable.
     """
-    try:
-        os.makedirs(dir_name, exist_ok=True)
-    except FileExistsError as e:
-        raise FileExistsError(f"{dir_name!s} exists and is not a directory") from e
-    except PermissionError as e:
-        raise PermissionError(f"{dir_name!s} is not writable") from e
+    if not os.path.islink(dir_name):
+        try:
+            os.makedirs(dir_name, exist_ok=True)
+        except FileExistsError as e:
+            raise FileExistsError(f"{dir_name!s} exists and is not a directory") from e
+        except PermissionError as e:
+            raise PermissionError(f"{dir_name!s} is not writable") from e
 
 
 class WriteSerializingFile:
