e271bfd32e761c20e364ad41fcadfd20253ba3d2
Jeff Raubitschek <jeff@wandb.com>
2023-07-12 09:57:28 -0700

    chore(nexus): github precommit should ship graphql checks for now (#51)

--- nexus/scripts/generate-graphql.sh
+++ nexus/scripts/generate-graphql.sh
@@ -7,7 +7,7 @@ BASE=$(dirname $(dirname $(readlink -f $0)))
 cd $BASE/api/graphql
 
 if [ ! -f "schemas/schema-latest.graphql" ]; then
-    echo "INFO: Not generating graphql as there is no schema-latest."
-    exit 0
+    echo "ERROR: Not generating graphql as there is no schema-latest."
+    exit 1
 fi
 go run github.com/Khan/genqlient genqlient.yaml
