283879db7dc8cde8fd97ac7986027e0042aca909
Jeff Raubitschek <jeff@wandb.com>
2023-07-13 12:25:37 -0700

    chore(nexus): clean up dev environment scripts (#55)

--- nexus/scripts/run-nexus.sh
+++ nexus/scripts/run-nexus.sh
@@ -3,7 +3,14 @@
 # Script used to start nexus from a go development directory.
 #
 # Usage in wandb:
-#   _WANDB_NEXUS_PATH=${NEXUS_DIR}/scripts/run-nexus.sh ./require-nexus.py
+#   1) manually setup the path
+#     _WANDB_NEXUS_PATH=${NEXUS_DIR}/scripts/run-nexus.sh python require-nexus.py
+#   2) set the environment variable in your shell
+#      source ${NEXUS_DIR}/scripts/setup-nexus-path.sh
+#      python ./require-nexus.py
+#   3) setup nexus path for a single command
+#      ${NEXUS_DIR}/scripts/setup-nexus-path.sh python require-nexus.py
+#
 
 set -e
 BASE=$(dirname $(dirname $(readlink -f $0)))
