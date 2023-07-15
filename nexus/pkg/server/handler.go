ca36887ccb6ca64ccd114f99fe954c61c2372819
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2023-07-14 18:33:26 -0700

    feat(nexus): add support for partial history (#58)

--- nexus/pkg/server/handler.go
+++ nexus/pkg/server/handler.go
@@ -34,9 +34,6 @@ type Handler struct {
 	// logger is the logger for the handler
 	logger *observability.NexusLogger
 
-	// currentStep is the current step
-	currentStep int64
-
 	// startTime is the start time
 	startTime float64
 
@@ -45,6 +42,10 @@ type Handler struct {
 
 	// summary is the summary
 	summary map[string]string
+
+	// historyRecord is the history record used to track
+	// current active history record for the run
+	historyRecord *service.HistoryRecord
 }
 
 // NewHandler creates a new handler
@@ -99,7 +100,6 @@ func (h *Handler) handleRecord(msg *service.Record) {
 	case *service.Record_Header:
 		// TODO: handle this
 	case *service.Record_History:
-		// TODO: handle this
 	case *service.Record_LinkArtifact:
 		// TODO: handle this
 	case *service.Record_Metric:
@@ -254,45 +254,128 @@ func (h *Handler) handleGetSummary(_ *service.Record, _ *service.GetSummaryReque
 func (h *Handler) handleDefer(rec *service.Record) {
 	req := rec.GetRequest().GetDefer()
 	switch req.State {
+	case service.DeferRequest_BEGIN:
+	case service.DeferRequest_FLUSH_STATS:
+	case service.DeferRequest_FLUSH_PARTIAL_HISTORY:
+		h.flushHistory(h.historyRecord)
+	case service.DeferRequest_FLUSH_TB:
+	case service.DeferRequest_FLUSH_SUM:
+	case service.DeferRequest_FLUSH_DEBOUNCER:
+	case service.DeferRequest_FLUSH_OUTPUT:
+	case service.DeferRequest_FLUSH_DIR:
+	case service.DeferRequest_FLUSH_FP:
+	case service.DeferRequest_JOIN_FP:
+	case service.DeferRequest_FLUSH_FS:
+	case service.DeferRequest_FLUSH_FINAL:
 	case service.DeferRequest_END:
-		h.sendRecord(rec)
 	default:
-		h.sendRecord(rec)
+		err := fmt.Errorf("handleDefer: unknown defer state %v", req.State)
+		h.logger.CaptureError("unknown defer state", err)
 	}
+	h.sendRecord(rec)
+}
+
+func (h *Handler) flushHistory(history *service.HistoryRecord) {
+
+	if history == nil || history.Item == nil {
+		return
+	}
+	// walk through items looking for _timestamp
+	// TODO: add a timestamp field to the history record
+	items := history.GetItem()
+	var runTime float64 = 0
+	for _, item := range items {
+		if item.Key == "_timestamp" {
+			val, err := strconv.ParseFloat(item.ValueJson, 64)
+			if err != nil {
+				h.logger.CaptureError("error parsing timestamp", err)
+			} else {
+				runTime = val - h.startTime
+			}
+		}
+	}
+
+	history.Item = append(history.Item,
+		&service.HistoryItem{Key: "_runtime", ValueJson: fmt.Sprintf("%f", runTime)},
+		&service.HistoryItem{Key: "_step", ValueJson: fmt.Sprintf("%d", history.GetStep().GetNum())},
+	)
+
+	rec := &service.Record{
+		RecordType: &service.Record_History{History: history},
+	}
+	h.updateSummary(history)
+	h.sendRecord(rec)
 }
 
 func (h *Handler) handlePartialHistory(_ *service.Record, req *service.PartialHistoryRequest) {
-	items := req.Item
 
-	stepNum := h.currentStep
-	h.currentStep += 1
-	s := service.HistoryStep{Num: stepNum}
-
-	var runTime float64 = 0
-	// walk through items looking for _timestamp
-	for i := 0; i < len(items); i++ {
-		if items[i].Key == "_timestamp" {
-			val, err := strconv.ParseFloat(items[i].ValueJson, 64)
-			if err != nil {
-				h.logger.CaptureError("error parsing timestamp", err)
-			}
-			runTime = val - h.startTime
+	// This is the first partial history record we receive
+	// for this step, so we need to initialize the history record
+	// and step. If the user provided a step in the request,
+	// use that, otherwise use 0.
+	if h.historyRecord == nil {
+		h.historyRecord = &service.HistoryRecord{}
+		if req.Step != nil {
+			h.historyRecord.Step = req.Step
+		} else {
+			h.historyRecord.Step = &service.HistoryStep{Num: 0}
 		}
 	}
-	items = append(items,
-		&service.HistoryItem{Key: "_runtime", ValueJson: fmt.Sprintf("%f", runTime)},
-		&service.HistoryItem{Key: "_step", ValueJson: fmt.Sprintf("%d", stepNum)},
-	)
 
-	record := service.HistoryRecord{Step: &s, Item: items}
-	r := service.Record{
-		RecordType: &service.Record_History{History: &record},
+	// The HistoryRecord struct is responsible for tracking data related to
+	//	a single step in the history. Users can send multiple partial history
+	//	records for a single step. Each partial history record contains a
+	//	step number, a flush flag, and a list of history items.
+	//
+	// The step number indicates the step number for the history record. The
+	// flush flag determines whether the history record should be flushed
+	// after processing the request. The history items are appended to the
+	// existing history record.
+	//
+	// The following logic is used to process the request:
+	//
+	// -  If the request includes a step number and the step number is greater
+	//		than the current step number, the current history record is flushed
+	//		and a new history record is created.
+	// - If the step number in the request is less than the current step number,
+	//		we ignore the request and log a warning.
+	// 		NOTE: the server requires the steps of the history records
+	// 		to be monotonically increasing.
+	// -  If the step number in the request matches the current step number, the
+	//		history items are appended to the current history record.
+	//
+	// - If the request has a flush flag, another flush might occur after for the
+	// current history record after processing the request.
+	//
+	// - If the request doesn't have a step, and doesn't have a flush flag, this is
+	//	equivalent to step being equal to the current step number and a flush flag
+	//	being set to true.
+	if req.Step != nil {
+		if req.Step.Num > h.historyRecord.Step.Num {
+			h.flushHistory(h.historyRecord)
+			h.historyRecord = &service.HistoryRecord{
+				Step: req.Step,
+			}
+		} else if req.Step.Num < h.historyRecord.Step.Num {
+			h.logger.CaptureWarn("received history record for a step that has already been received",
+				"received", req.Step, "current", h.historyRecord.Step)
+			return
+		}
 	}
-	h.updateSummary(&record)
 
-	// TODO: remove this once we have handleHistory
-	h.sendRecord(&r)
+	// Append the history items from the request to the current history record.
+	h.historyRecord.Item = append(h.historyRecord.Item, req.Item...)
 
+	// Flush the history record and start to collect a new one with
+	// the next step number.
+	if (req.Step == nil && req.Action == nil) || req.Action.Flush {
+		h.flushHistory(h.historyRecord)
+		h.historyRecord = &service.HistoryRecord{
+			Step: &service.HistoryStep{
+				Num: h.historyRecord.Step.Num + 1,
+			},
+		}
+	}
 }
 
 func (h *Handler) updateSummary(msg *service.HistoryRecord) {
