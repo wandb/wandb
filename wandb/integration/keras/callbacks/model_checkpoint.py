9a028a0db738758a52a69745ec30f5f4320f36f2
ayulockin <mein2work@gmail.com>
2023-07-18 11:41:26 +0000

    fix checkpointing callback

--- wandb/integration/keras/callbacks/model_checkpoint.py
+++ wandb/integration/keras/callbacks/model_checkpoint.py
@@ -188,10 +188,13 @@ class WandbModelCheckpoint(callbacks.ModelCheckpoint):
     def is_old_tf_keras_version(self) -> Optional[bool]:
         if self._is_old_tf_keras_version is None:
             from pkg_resources import parse_version
-
-            if parse_version(tf.keras.__version__) < parse_version("2.6.0"):
-                self._is_old_tf_keras_version = True
-            else:
+            
+            try:
+                if parse_version(tf.keras.__version__) < parse_version("2.6.0"):
+                    self._is_old_tf_keras_version = True
+                else:
+                    self._is_old_tf_keras_version = False
+            except AttributeError:
                 self._is_old_tf_keras_version = False
 
         return self._is_old_tf_keras_version
