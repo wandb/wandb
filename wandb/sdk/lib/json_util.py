0c093f97684234df554cbaa5feead4226d55225e
Dmitry Duev <dima@wandb.com>
2023-07-18 12:39:20 -0700

    add todo and guardrail

--- wandb/sdk/lib/json_util.py
+++ wandb/sdk/lib/json_util.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 from typing import Any, Union
 
 logger = logging.getLogger(__name__)
@@ -8,58 +9,72 @@ logger = logging.getLogger(__name__)
 try:
     import orjson  # type: ignore[import]
 
-    def dumps(obj: Any, **kwargs: Any) -> str:
-        """Wrapper for <json|orjson>.dumps."""
-        cls = kwargs.pop("cls", None)
-        try:
-            _kwargs = kwargs.copy()
-            if cls:
-                _kwargs["default"] = cls.default
-            encoded = orjson.dumps(
-                obj, option=orjson.OPT_NON_STR_KEYS, **_kwargs
-            ).decode()
-        except Exception as e:
-            logger.exception(f"Error using orjson.dumps: {e}")
-            if cls:
-                kwargs["cls"] = cls
-            encoded = json.dumps(obj, **kwargs)
+    # todo: orjson complies with the json standard and does not support
+    #  NaN, Infinity, and -Infinity. Should be fixed in the future.
 
-        return encoded  # type: ignore[no-any-return]
+    # additional safeguard for now
+    if os.environ.get("_WANDB_ORJSON"):
 
-    def dump(obj: Any, fp: Any, **kwargs: Any) -> None:
-        """Wrapper for <json|orjson>.dump."""
-        cls = kwargs.pop("cls", None)
-        try:
-            _kwargs = kwargs.copy()
-            if cls:
-                _kwargs["default"] = cls.default
-            encoded = orjson.dumps(obj, option=orjson.OPT_NON_STR_KEYS, **_kwargs)
-            fp.write(encoded)
-        except Exception as e:
-            logger.exception(f"Error using orjson.dump: {e}")
-            if cls:
-                kwargs["cls"] = cls
-            json.dump(obj, fp, **kwargs)
+        def dumps(obj: Any, **kwargs: Any) -> str:
+            """Wrapper for <json|orjson>.dumps."""
+            cls = kwargs.pop("cls", None)
+            try:
+                _kwargs = kwargs.copy()
+                if cls:
+                    _kwargs["default"] = cls.default
+                encoded = orjson.dumps(
+                    obj, option=orjson.OPT_NON_STR_KEYS, **_kwargs
+                ).decode()
+            except Exception as e:
+                logger.exception(f"Error using orjson.dumps: {e}")
+                if cls:
+                    kwargs["cls"] = cls
+                encoded = json.dumps(obj, **kwargs)
 
-    def loads(obj: Union[str, bytes]) -> Any:
-        """Wrapper for orjson.loads."""
-        try:
-            decoded = orjson.loads(obj)
-        except Exception as e:
-            logger.exception(f"Error using orjson.loads: {e}")
-            decoded = json.loads(obj)
+            return encoded  # type: ignore[no-any-return]
 
-        return decoded
+        def dump(obj: Any, fp: Any, **kwargs: Any) -> None:
+            """Wrapper for <json|orjson>.dump."""
+            cls = kwargs.pop("cls", None)
+            try:
+                _kwargs = kwargs.copy()
+                if cls:
+                    _kwargs["default"] = cls.default
+                encoded = orjson.dumps(obj, option=orjson.OPT_NON_STR_KEYS, **_kwargs)
+                fp.write(encoded)
+            except Exception as e:
+                logger.exception(f"Error using orjson.dump: {e}")
+                if cls:
+                    kwargs["cls"] = cls
+                json.dump(obj, fp, **kwargs)
 
-    def load(fp: Any) -> Any:
-        """Wrapper for orjson.load."""
-        try:
-            decoded = orjson.loads(fp.read())
-        except Exception as e:
-            logger.exception(f"Error using orjson.load: {e}")
-            decoded = json.load(fp)
+        def loads(obj: Union[str, bytes]) -> Any:
+            """Wrapper for orjson.loads."""
+            try:
+                decoded = orjson.loads(obj)
+            except Exception as e:
+                logger.exception(f"Error using orjson.loads: {e}")
+                decoded = json.loads(obj)
 
-        return decoded
+            return decoded
+
+        def load(fp: Any) -> Any:
+            """Wrapper for orjson.load."""
+            try:
+                decoded = orjson.loads(fp.read())
+            except Exception as e:
+                logger.exception(f"Error using orjson.load: {e}")
+                decoded = json.load(fp)
+
+            return decoded
+
+    else:
+        from json import (  # type: ignore[assignment] # noqa: F401
+            dump,
+            dumps,
+            load,
+            loads,
+        )
 
 except ImportError:
     from json import dump, dumps, load, loads  # type: ignore[assignment] # noqa: F401
