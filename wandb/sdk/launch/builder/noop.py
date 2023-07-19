f8f7583bf26e40e3330329b01e63aca1c3caacf6
Tim Hays <tim.hays@wandb.com>
2023-07-19 16:16:58 -0700

    More specific

--- wandb/sdk/launch/builder/noop.py
+++ wandb/sdk/launch/builder/noop.py
@@ -52,5 +52,6 @@ class NoOpBuilder(AbstractBuilder):
         """
         raise LaunchError(
             "Attempted build with noop builder. Specify a builder in your launch config at ~/.config/wandb/launch-config.yaml.\n"
-            "Note: Jobs sourced from Docker images do not require a builder. See https://docs.wandb.ai/guides/launch/create-job."
+            "Note: Jobs sourced from git repos and code artifacts require a builder, while jobs sourced from Docker images do not.\n"
+            "See https://docs.wandb.ai/guides/launch/create-job."
         )
