e55e521920b938e60ae11a7424d83388da9b07ad
Andrew Truong <itsandrewtruong@gmail.com>
2023-07-19 15:48:56 -0400

    test

--- wandb/apis/importers/base.py
+++ wandb/apis/importers/base.py
@@ -6,16 +6,18 @@ from dataclasses import dataclass
 from typing import Any, Dict, Iterable, List, Optional, Tuple
 from unittest.mock import patch
 
+from google.protobuf.json_format import ParseDict
 from tqdm import tqdm
 
 import wandb
 from wandb.proto import wandb_internal_pb2 as pb
+from wandb.proto import wandb_settings_pb2
 from wandb.proto import wandb_telemetry_pb2 as telem_pb
 from wandb.sdk.interface.interface import file_policy_to_enum
 from wandb.sdk.interface.interface_queue import InterfaceQueue
 from wandb.sdk.internal import context
 from wandb.sdk.internal.sender import SendManager
-from wandb.sdk.internal.settings_static import SettingsDict
+from wandb.sdk.internal.settings_static import SettingsStatic
 from wandb.util import cast_dictlike_to_dict, coalesce
 
 if sys.version_info >= (3, 8):
@@ -305,7 +307,7 @@ class RecordMaker:
 def send_run_with_send_manager(
     run: ImporterRun,
     overrides: Optional[Dict[str, Any]] = None,
-    settings_override: Optional[SettingsDict] = None,
+    settings_override: Optional[SettingsStatic] = None,
 ) -> None:
     # does this need to be here for pmap?
     if overrides:
@@ -343,10 +345,14 @@ def send_run_with_send_manager(
         "disable_job_creation": False,
         "_async_upload_concurrency_limit": None,
     }
-    settings = {**default_settings, **_settings_override}
+    combined_settings = {**default_settings, **_settings_override}
+    settings_message = wandb_settings_pb2.Settings()
+    ParseDict(combined_settings, settings_message)
 
-    record_q = queue.Queue()
-    result_q = queue.Queue()
+    settings = SettingsStatic(settings_message)
+
+    record_q: queue.Queue = queue.Queue()
+    result_q: queue.Queue = queue.Queue()
     interface = InterfaceQueue(record_q=record_q)
     context_keeper = context.ContextKeeper()
 
