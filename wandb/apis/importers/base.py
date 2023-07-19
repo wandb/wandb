2455f8835fddf5959d4d7eec795f1cc5b25d7349
Andrew Truong <itsandrewtruong@gmail.com>
2023-07-19 15:49:16 -0400

    test

--- wandb/apis/importers/base.py
+++ wandb/apis/importers/base.py
@@ -6,10 +6,12 @@ from dataclasses import dataclass
 from typing import Any, Dict, Iterable, List, Optional, Tuple
 from unittest.mock import patch
 
+from google.protobuf.json_format import ParseDict
 from tqdm import tqdm
 
 import wandb
 from wandb.proto import wandb_internal_pb2 as pb
+from wandb.proto import wandb_settings_pb2
 from wandb.proto import wandb_telemetry_pb2 as telem_pb
 from wandb.sdk.interface.interface import file_policy_to_enum
 from wandb.sdk.interface.interface_queue import InterfaceQueue
@@ -17,8 +19,6 @@ from wandb.sdk.internal import context
 from wandb.sdk.internal.sender import SendManager
 from wandb.sdk.internal.settings_static import SettingsStatic
 from wandb.util import cast_dictlike_to_dict, coalesce
-from wandb.proto import wandb_settings_pb2
-from google.protobuf.json_format import ParseDict
 
 if sys.version_info >= (3, 8):
     from typing import Protocol
