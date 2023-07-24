61ff621e60a39cc634d8d37cd5f290e029c14648
Andrew Truong <itsandrewtruong@gmail.com>
2023-07-24 01:23:43 -0400

    test

--- wandb/apis/importers/__init__.py
+++ wandb/apis/importers/__init__.py
@@ -1,6 +1,7 @@
 from wandb.util import get_module
 
-from .wandb import WandbParquetImporter, WandbParquetRun  # noqa: F401
+from .base import ImportReportConfig, ImportRunConfig
+from .wandb import WandbImporter, WandbParquetImporter, WandbParquetRun, WandbRun
 
 if get_module("mlflow"):
     from .mlflow import MlflowImporter, MlflowRun  # noqa: F401
