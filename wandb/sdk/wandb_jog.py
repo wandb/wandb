1b09ecb6f44bb830cb25e130e44519697edaf586
Dmitry Duev <dima@wandb.com>
2023-07-25 18:15:43 -0700

    save progress

--- wandb/sdk/wandb_jog.py
+++ wandb/sdk/wandb_jog.py
@@ -1,6 +1,86 @@
+import os
+from copy import copy
+from typing import TYPE_CHECKING
+
+from ..proto.wandb_internal_pb2 import (
+    ConfigRecord,
+    Record,
+    Request,
+    RunRecord,
+    RunStartRequest,
+)
+from .interface.interface_sock import InterfaceSock
+from .lib import filesystem, runid
+from .lib.mailbox import Mailbox
+from .lib.sock_client import SockClient
+
+if TYPE_CHECKING:
+    import wandb_settings
+
+
 class Jog:
-    def __init__(self, stream_name: str):
-        self.stream_name = stream_name
+    settings: "wandb_settings.Settings"
+    interface: InterfaceSock
+
+    def setup_jog_dirs(self) -> None:
+        settings = self.settings
+        filesystem.mkdir_exists_ok(os.path.dirname(settings.log_user))
+        filesystem.mkdir_exists_ok(os.path.dirname(settings.log_internal))
+        filesystem.mkdir_exists_ok(os.path.dirname(settings.sync_file))
+        filesystem.mkdir_exists_ok(settings.files_dir)
+        filesystem.mkdir_exists_ok(settings._tmp_code_dir)
+
+    def __init__(
+        self,
+        settings: "wandb_settings.Settings",
+        socket_client: SockClient,
+    ) -> None:
+        self.settings = copy(settings)
+        self.interface = InterfaceSock(socket_client, Mailbox())  # noqa
+
+        jog_id = runid.generate_id()
+        self.settings.update(run_id=jog_id)
+        self.settings._set_run_start_time()
+
+        self.interface._stream_id = jog_id
+
+        self.setup_jog_dirs()
+
+    def start(self):
+        jog_id = self.settings.run_id
+        config_pb = ConfigRecord()
+        update = config_pb.update.add()
+        update.key = "_wandb"
+        update.value_json = '{"cli_version": "1.0.0"}'
+
+        jog_pb = RunRecord(
+            run_id=jog_id,
+            config=config_pb,
+        )
+        record = Record(run=jog_pb)
+
+        # print("jog_pb", jog_pb)
+        handle = self.interface._deliver_record(record)
+        result = handle.wait(timeout=self.settings.init_timeout)
+        print("result", result)
+
+        run_start_settings = {
+            "entity": result.run_result.run.entity,
+            "project": result.run_result.run.project,
+            "display_name": result.run_result.run.display_name,
+        }
+        self.settings._apply_run_start(run_start_settings)
+
+        print(self.settings)
+
+        run_start_request = RunStartRequest(run=result.run_result.run)
+        request = Request(run_start=run_start_request)
+        record = Record(request=request)
+        # All requests do not get persisted
+        record.control.local = True
+        handle = self.interface._deliver_record(record)
+        result = handle.wait(timeout=self.settings.init_timeout)
+        print("result", result)
 
     def log(self, data):
         pass
--- wandb/sdk/wandb_nexus.py
+++ wandb/sdk/wandb_nexus.py
@@ -1,13 +1,11 @@
 import os
-from copy import copy
 from typing import TYPE_CHECKING, Dict
 
-from ..proto.wandb_internal_pb2 import RunRecord
 from . import wandb_manager, wandb_settings
-from .lib import filesystem, runid
+from .wandb_jog import Jog
 
 if TYPE_CHECKING:
-    from interface.interface_sock import InterfaceSock
+    from .service.service_sock import ServiceSockInterface
 
 
 def init_settings():
@@ -27,43 +25,30 @@ class Nexus:
     address: str
     settings: "wandb_settings.Settings"
     manager: "wandb_manager._Manager"
-    interface: "InterfaceSock"
-    jog_settings: Dict[str, "wandb_settings.Settings"]
+    command_interface: "ServiceSockInterface"
+    jogs: Dict[str, "Jog"]
 
     def __init__(self):
-        self.jog_settings = {}
+        self.jogs = {}
 
     def up(self):
         self.settings = init_settings()
         self.manager = wandb_manager._Manager(settings=self.settings)
         # print("manager", self.manager.__dict__)
 
-        self.interface = self.manager._service._service_interface
-        # print("interface", self.interface.__dict__)
-
-    @staticmethod
-    def setup_jog_dirs(settings: "wandb_settings.Settings"):
-        filesystem.mkdir_exists_ok(os.path.dirname(settings.log_user))
-        filesystem.mkdir_exists_ok(os.path.dirname(settings.log_internal))
-        filesystem.mkdir_exists_ok(os.path.dirname(settings.sync_file))
-        filesystem.mkdir_exists_ok(settings.files_dir)
-        filesystem.mkdir_exists_ok(settings._tmp_code_dir)
+        self.command_interface = self.manager._service._service_interface
+        # print("command_interface", self.command_interface.__dict__)
 
     def init(self):
         # return Jog("sprint")
-        jog_id = runid.generate_id()
-        jog_settings = copy(self.settings)
-        jog_settings.update(run_id=jog_id)
-        jog_settings._set_run_start_time()
+        jog = Jog(self.settings, self.command_interface._sock_client)
 
-        self.setup_jog_dirs(jog_settings)
+        jog_id = jog.settings.run_id
+        # send command to nexus to init stream for our jog
+        self.manager._inform_init(settings=jog.settings, run_id=jog_id)
+        self.jogs[jog_id] = jog
 
-        self.manager._inform_init(settings=jog_settings, run_id=jog_id)
-        self.jog_settings[jog_id] = jog_settings
-
-        jog_pb = RunRecord(run_id=jog_id)
-
-        print("jog_pb", jog_pb)
+        jog.start()
 
     def down(self):
         pass
