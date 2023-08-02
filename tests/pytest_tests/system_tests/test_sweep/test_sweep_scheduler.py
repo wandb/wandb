d535df78718209bf52412ce34ae713937713296c
gtarpenning <griffin.tarpenning@wandb.com>
2023-08-02 16:46:00 -0700

    fix test

--- tests/pytest_tests/system_tests/test_sweep/test_sweep_scheduler.py
+++ tests/pytest_tests/system_tests/test_sweep/test_sweep_scheduler.py
@@ -452,7 +452,7 @@ def test_sweep_scheduler_base_add_to_launch_queue(user, sweep_config, monkeypatc
     assert _scheduler.is_alive is False
     assert len(_scheduler._runs) == 1
     assert isinstance(_scheduler._runs["foo_run"].queued_run, public.QueuedRun)
-    assert not _scheduler._runs["foo_run"].state.is_alive
+    _scheduler._runs["foo_run"].queued_run.state = RunState.FINISHED
     assert _scheduler._runs["foo_run"].queued_run.args()[-2] == _project
 
     sweep_id2 = wandb.sweep(sweep_config, entity=user, project=_project)
