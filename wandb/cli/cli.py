d302c842d4d9346bf3c94b618af2b5a5d0873e59
Kyle Goyette <kyle@wandb.com>
2023-08-02 21:44:40 -0700

    wip

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -1536,7 +1536,7 @@ def job() -> None:
     pass
 
 
-@job.command("list")
+@job.command(help="List jobs in a project")
 @click.option(
     "--project",
     "-p",
@@ -1578,7 +1578,7 @@ def _list(project, entity):
         wandb.termlog(f"{name} -- versions ({len(aliases)}): {aliases_str}")
 
 
-@job.command()
+@job.command(help="Describe a job")
 @click.argument("job")
 def describe(job):
     public_api = PublicApi()
