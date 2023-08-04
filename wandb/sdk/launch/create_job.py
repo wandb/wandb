a9cf58571cca944df88d8c8d284c8c48e7a1da0c
Tim Hays <tim.hays@wandb.com>
2023-08-04 14:21:31 -0700

    Fix error message

--- wandb/sdk/launch/create_job.py
+++ wandb/sdk/launch/create_job.py
@@ -346,8 +346,8 @@ def _create_repo_metadata(
     if not os.path.exists(os.path.join(req_dir, "requirements.txt")):
         wandb.termerror(
             "Could not find requirements.txt file in git repo at "
-            f"{os.path.join(local_dir, 'requirements.txt')} "
-            "or parent directories"
+            f"{os.path.join(os.path.dirname(path), 'requirements.txt')} "
+            "or parent directories."
         )
         return None
 
