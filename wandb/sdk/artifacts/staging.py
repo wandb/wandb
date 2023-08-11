7763e6a46ee3acfcd63cf63a50ac97043bc3ee53
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-08-11 12:29:29 -0700

    Add docstring to staging.py

--- wandb/sdk/artifacts/staging.py
+++ wandb/sdk/artifacts/staging.py
@@ -1,3 +1,10 @@
+"""Manages artifact file staging.
+
+Artifact files are copied to the staging area as soon as they are added to an artifact
+in order to avoid file changes corrupting the artifact. Once the upload is complete, the
+file should be moved to the artifact cache.
+"""
+
 import os
 
 from wandb import env
