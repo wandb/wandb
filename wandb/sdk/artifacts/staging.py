71395426dd800906a2c7d92136e8d2d066141472
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-08-11 08:32:59 -0700

    Move staging logic to separate file

---
+++ wandb/sdk/artifacts/staging.py
@@ -0,0 +1,18 @@
+import os
+
+from wandb import env
+from wandb.sdk.lib.filesystem import mkdir_exists_ok
+from wandb.sdk.lib.paths import FilePathStr
+
+
+def get_staging_dir() -> FilePathStr:
+    path = os.path.join(env.get_data_dir(), "artifacts", "staging")
+    try:
+        mkdir_exists_ok(path)
+    except OSError as e:
+        raise PermissionError(
+            f"Unable to write staging files to {path}. To fix this problem, please set "
+            f"{env.DATA_DIR} to a directory where you have the necessary write access."
+        ) from e
+
+    return FilePathStr(os.path.abspath(os.path.expanduser(path)))
