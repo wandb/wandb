ca73071f7c191d20aa6e3bcab35b3b0f3c32d3c2
Jeff Raubitschek <jeff@wandb.com>
2023-08-13 03:34:29 -0700

    update proto server grpc

--- wandb/proto/wandb_server_pb2_grpc.py
+++ wandb/proto/wandb_server_pb2_grpc.py
@@ -1,8 +1,20 @@
+import os
+import sys
 import google.protobuf
 
 protobuf_version = google.protobuf.__version__[0]
+
+# Add protodir into path to resolve relative imports
+restore_path = None
+proto_dir = os.path.abspath(os.path.dirname(__file__))
+if proto_dir not in sys.path:
+    restore_path = [directory for directory in sys.path]
+    sys.path.insert(1, proto_dir)
 
 if protobuf_version == "3":
     from wandb.proto.v3.wandb_server_pb2_grpc import *
 elif protobuf_version == "4":
     from wandb.proto.v4.wandb_server_pb2_grpc import *
+
+if restore_path is not None:
+    sys.path = restore_path
