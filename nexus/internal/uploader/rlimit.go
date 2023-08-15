b6106cb26dff6d235a88f48014d5b99eea54d2e8
Dmitry Duev <dmitryduev@users.noreply.github.com>
2023-08-15 13:06:14 -0700

    feat(nexus): improve file uploads (#6052)

---
+++ nexus/internal/uploader/rlimit.go
@@ -0,0 +1,23 @@
+//go:build !windows
+
+package uploader
+
+import (
+	"fmt"
+
+	"golang.org/x/sys/unix"
+)
+
+func getRlimit(defaultValue int32) int32 {
+	if defaultValue > 0 {
+		return defaultValue
+	}
+	var rlim unix.Rlimit
+	err := unix.Getrlimit(unix.RLIMIT_NOFILE, &rlim)
+	fmt.Println("rlim", rlim)
+	if err != nil {
+		return defaultConcurrencyLimit
+	}
+	// todo: verify this is correct on macos
+	return int32(rlim.Cur)
+}
