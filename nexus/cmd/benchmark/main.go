9eda8f09eed65cf2fe6f8c7fbe8b1759b9f6609d
Jeff Raubitschek <jeff@wandb.com>
2023-08-17 11:50:52 -0700

    update

--- nexus/cmd/benchmark/main.go
+++ nexus/cmd/benchmark/main.go
@@ -12,6 +12,7 @@ func main() {
 	host := flag.String("host", "localhost", "host to connect to")
 	port := flag.Int("port", 0, "port to connect to")
 	numHistory := flag.Int("numHistory", 1000, "number of history records to log")
+	numHistoryElements := flag.Int("numHistoryElements", 5, "number of elements in a history record")
 	teardown := flag.Bool("close", false, "flag to close the server")
 	offline := flag.Bool("offline", false, "use offline mode")
 	flag.Parse()
@@ -35,9 +36,11 @@ func main() {
 		panic(err)
 	}
 
-	data := map[string]float64{
-		"loss": float64(100),
+	data := map[string]float64{}
+	for i := 0; i < *numHistoryElements; i++ {
+		data[fmt.Sprintf("loss_%d", i)] = float64(100+i)
 	}
+
 	for i := 0; i < *numHistory; i++ {
 		run.Log(data)
 	}
