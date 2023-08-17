4e7398cc9b4f0196bde4e2d9f9c70d73619bb94b
Nick Penaranda <nick.penaranda@wandb.com>
2023-08-17 18:00:28 -0400

    fix test

--- tests/pytest_tests/unit_tests/test_lib/test_filenames.py
+++ tests/pytest_tests/unit_tests/test_lib/test_filenames.py
@@ -1,15 +1,17 @@
+from pathlib import Path
+
 import pytest
 from wandb.sdk.lib.filenames import exclude_wandb_fn, filtered_dir
 
 
-def test_filtered_dir_one_parameter(tmp_path):
-    with open(tmp_path + "/foo.txt", "w") as f:
+def test_filtered_dir_one_parameter(tmp_path: Path):
+    with open(tmp_path / "foo.txt", "w") as f:
         f.write("test")
     filtered_dir(tmp_path, lambda path: True, lambda path: False)
 
 
-def test_filtered_dir_two_parameters(tmp_path):
-    with open(tmp_path + "/foo.txt", "w") as f:
+def test_filtered_dir_two_parameters(tmp_path: Path):
+    with open(tmp_path / "foo.txt", "w") as f:
         f.write("test")
     filtered_dir(tmp_path, lambda path, root: True, lambda path, root: False)
 
