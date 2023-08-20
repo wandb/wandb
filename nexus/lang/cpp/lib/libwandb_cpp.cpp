deebd1662e358f9e5345696c72c05a4cdd3ee39f
Jeff Raubitschek <jeff@wandb.com>
2023-08-20 16:38:36 -0700

    clang-format

--- nexus/lang/cpp/lib/libwandb_cpp.cpp
+++ nexus/lang/cpp/lib/libwandb_cpp.cpp
@@ -1,139 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <libwandb_cpp.h>
 #include <libwandb_core.h>
+#include <libwandb_cpp.h>
 
 namespace wandb {
 
 // Setup global session
 Session *Session::defaultSession_ = nullptr;
 
-Settings::Settings()
-{
+Settings::Settings() {}
+
+Settings::Settings(std::unordered_map<std::string, std::string>) {}
+
+Settings::Settings(settings::Options options) {}
+
+Session::Session(Settings *settings) {
+  // set static so future calls to functions initRun() have a default session
+  defaultSession_ = this;
 }
 
-Settings::Settings(std::unordered_map<std::string, std::string>)
-{
+Run::Run() { this->_num = 0; }
+
+void Run::logPartialCommit() { wandbcoreLogCommit(this->_num); }
+
+void Run::log(std::vector<const char *> &keys, std::vector<double> &values,
+              bool commit) {
+  wandbcoreLogDoubles(this->_num, commit, keys.size(), &keys[0], &values[0]);
 }
 
-Settings::Settings(settings::Options options) {
+void Run::log(std::vector<const char *> &keys, std::vector<int> &values,
+              bool commit) {
+  wandbcoreLogInts(this->_num, commit, keys.size(), &keys[0], &values[0]);
 }
 
-Session::Session(Settings *settings)
-{
-    // set static so future calls to functions initRun() have a default session
-    defaultSession_ = this;
-}
+void Run::log(std::unordered_map<std::string, Value> &myMap) {
+  std::vector<const char *> keyDoubles;
+  std::vector<const char *> keyInts;
+  std::vector<double> valDoubles;
+  std::vector<int> valInts;
 
-Run::Run()
-{
-    this->_num = 0;
-}
-
-void Run::logPartialCommit()
-{
-    wandbcoreLogCommit(this->_num);
-}
-
-void Run::log(std::vector<const char *>& keys, std::vector<double>& values, bool commit)
-{
-    wandbcoreLogDoubles(this->_num, commit, keys.size(), &keys[0], &values[0]);
-}
-
-void Run::log(std::vector<const char *>& keys, std::vector<int>& values, bool commit)
-{
-    wandbcoreLogInts(this->_num, commit, keys.size(), &keys[0], &values[0]);
-}
-
-void Run::log(std::unordered_map<std::string, Value>& myMap)
-{
-    std::vector<const char *> keyDoubles;
-    std::vector<const char *> keyInts;
-    std::vector<double> valDoubles;
-    std::vector<int> valInts;
-
-    for ( const auto &[key, val] : myMap ) {
-        if (std::holds_alternative<int>(val)) {
-            keyInts.push_back(key.c_str());
-            valInts.push_back(std::get<int>(val));
-	} else if (std::holds_alternative<double>(val)) {
-            keyDoubles.push_back(key.c_str());
-            valDoubles.push_back(std::get<double>(val));
-        }
+  for (const auto &[key, val] : myMap) {
+    if (std::holds_alternative<int>(val)) {
+      keyInts.push_back(key.c_str());
+      valInts.push_back(std::get<int>(val));
+    } else if (std::holds_alternative<double>(val)) {
+      keyDoubles.push_back(key.c_str());
+      valDoubles.push_back(std::get<double>(val));
     }
-    if (keyDoubles.size()) {
-        this->log(keyDoubles, valDoubles, false);
-    }
-    if (keyInts.size()) {
-        this->log(keyInts, valInts, false);
-    }
-    this->logPartialCommit();
+  }
+  if (keyDoubles.size()) {
+    this->log(keyDoubles, valDoubles, false);
+  }
+  if (keyInts.size()) {
+    this->log(keyInts, valInts, false);
+  }
+  this->logPartialCommit();
 }
 
-void Run::finish()
-{
-    wandbcoreFinish(this->_num);
-}
+void Run::finish() { wandbcoreFinish(this->_num); }
 
-void _session_teardown() {
-    wandbcoreTeardown();
-}
+void _session_teardown() { wandbcoreTeardown(); }
 
 void _session_setup() {
-    wandbcoreSetup();
-    atexit(_session_teardown);
+  wandbcoreSetup();
+  atexit(_session_teardown);
 }
 
 Session *Session::GetInstance() {
-    if (defaultSession_ == nullptr) {
-	    new Session(NULL);
-    }
-    return defaultSession_;
+  if (defaultSession_ == nullptr) {
+    new Session(NULL);
+  }
+  return defaultSession_;
 }
 
 Run Session::_initRun(Settings *settings) {
-    _session_setup();
-    int n = wandbcoreInit();
-    auto r = Run();
-    r._num = n;
-    return r;
+  _session_setup();
+  int n = wandbcoreInit();
+  auto r = Run();
+  r._num = n;
+  return r;
 }
 
-Run Session::initRun() {
-    return _initRun(NULL);
-}
+Run Session::initRun() { return _initRun(NULL); }
 
-Run Session::initRun(Settings settings) {
-    return _initRun(&settings);
-}
+Run Session::initRun(Settings settings) { return _initRun(&settings); }
 
 Run _initRun(Settings *settings = NULL) {
-    auto s = Session::GetInstance();
-    if (settings != nullptr) {
-    	return s->initRun(*settings);
-    }
-    return s->initRun();
+  auto s = Session::GetInstance();
+  if (settings != nullptr) {
+    return s->initRun(*settings);
+  }
+  return s->initRun();
 }
 
-Run initRun(Settings settings) {
-    return _initRun(&settings);
-}
+Run initRun(Settings settings) { return _initRun(&settings); }
 
-Run initRun() {
-    return _initRun(NULL);
-}
+Run initRun() { return _initRun(NULL); }
 
 Run initRun(std::initializer_list<run::InitRunOption> options) {
-    return _initRun(NULL);
+  return _initRun(NULL);
 }
 
 namespace run {
-InitRunOption::InitRunOption() {
-}
+InitRunOption::InitRunOption() {}
 
-WithSettings::WithSettings(Settings s) {
-}
-}
+WithSettings::WithSettings(Settings s) {}
+} // namespace run
 
-}
+} // namespace wandb
--- nexus/lang/cpp/lib/libwandb_cpp.h
+++ nexus/lang/cpp/lib/libwandb_cpp.h
@@ -1,29 +1,26 @@
 #include <string>
+#include <unordered_map>
 #include <variant>
 #include <vector>
-#include <unordered_map>
 
 namespace wandb {
 
-typedef std::variant<
-  int,
-  float,
-  double
-  > Value;
+typedef std::variant<int, float, double> Value;
 
 typedef std::unordered_map<std::string, wandb::Value> History;
 
 namespace settings {
 typedef struct settings_options_s {
-    bool offline;
-    std::string apiKey;
-}Options;
-}
+  bool offline;
+  std::string apiKey;
+} Options;
+} // namespace settings
 
 class Settings {
 private:
   bool offline_;
   std::string apiKey;
+
 public:
   Settings();
   Settings(std::unordered_map<std::string, std::string>);
@@ -32,15 +29,19 @@ public:
 
 class Run {
 private:
-  void log(std::vector<const char *>&, std::vector<double>&, bool commit = false);
-  void log(std::vector<const char *>&, std::vector<int>&, bool commit = false);
+  void log(std::vector<const char *> &, std::vector<double> &,
+           bool commit = false);
+  void log(std::vector<const char *> &, std::vector<int> &,
+           bool commit = false);
   void logPartialCommit();
+
 protected:
   int _num;
+
 public:
   Run();
   Run(Settings settings);
-  void log(std::unordered_map<std::string, Value>&);
+  void log(std::unordered_map<std::string, Value> &);
   // void log(std::vector<const char *>&, std::vector<Value>&);
   void finish();
   friend class Session;
@@ -51,6 +52,7 @@ private:
   Run _initRun(Settings *settings = NULL);
   static Session *defaultSession_;
   Session(Settings *settings = NULL);
+
 public:
   Session() : Session(NULL) {}
   Session(Settings settings) : Session(&settings) {}
@@ -69,9 +71,9 @@ class WithSettings : public InitRunOption {
 public:
   WithSettings(Settings s);
 };
-}
+} // namespace run
 
 // Run initRun(Settings settings);
 Run initRun(std::initializer_list<run::InitRunOption>);
 
-}
+} // namespace wandb
