55e37f5be6b1289101e018bf769446bc0f24e499
Jeff Raubitschek <jeff@wandb.com>
2023-08-20 16:41:19 -0700

    clang-format

--- nexus/lang/cpp/examples/train.cpp
+++ nexus/lang/cpp/examples/train.cpp
@@ -1,26 +1,24 @@
 #include <libwandb_cpp.h>
 
 int main() {
-    wandb::Settings settings(
-        (wandb::settings::Options){
-            .offline = false,
-        }
-    );
+  wandb::Settings settings((wandb::settings::Options){
+      .offline = false,
+  });
 
-    auto run = wandb::initRun({
-            wandb::run::WithSettings(settings),
-        });
+  auto run = wandb::initRun({
+      wandb::run::WithSettings(settings),
+  });
 
-    for (int i = 0; i < 5; i++) {
-        wandb::History data = {
-            {"val", 3.14 + i},
-            {"val2", 1.23 + i},
-            {"val23", int(1)},
-        };
-        run.log(data);
-    }
+  for (int i = 0; i < 5; i++) {
+    wandb::History data = {
+        {"val", 3.14 + i},
+        {"val2", 1.23 + i},
+        {"val23", int(1)},
+    };
+    run.log(data);
+  }
 
-    run.finish();
+  run.finish();
 
-    return 0;
+  return 0;
 }
