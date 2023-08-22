8524e7ccc79c778ed2f80067bc8400245062fc58
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-08-22 13:07:23 -0700

    Remove WandbStoragePolicy's dependence on individual storage handlers
    Load the MultiHandler and its dependent handlers on __init__ to decrease interdependence

--- wandb/sdk/artifacts/storage_policies/wandb_storage_policy.py
+++ wandb/sdk/artifacts/storage_policies/wandb_storage_policy.py
@@ -11,17 +11,7 @@ import urllib3
 from wandb.apis import InternalApi
 from wandb.errors.term import termwarn
 from wandb.sdk.artifacts.artifacts_cache import ArtifactsCache, get_artifacts_cache
-from wandb.sdk.artifacts.storage_handlers.azure_handler import AzureHandler
-from wandb.sdk.artifacts.storage_handlers.gcs_handler import GCSHandler
-from wandb.sdk.artifacts.storage_handlers.http_handler import HTTPHandler
-from wandb.sdk.artifacts.storage_handlers.local_file_handler import LocalFileHandler
-from wandb.sdk.artifacts.storage_handlers.multi_handler import MultiHandler
-from wandb.sdk.artifacts.storage_handlers.s3_handler import S3Handler
-from wandb.sdk.artifacts.storage_handlers.tracking_handler import TrackingHandler
-from wandb.sdk.artifacts.storage_handlers.wb_artifact_handler import WBArtifactHandler
-from wandb.sdk.artifacts.storage_handlers.wb_local_artifact_handler import (
-    WBLocalArtifactHandler,
-)
+from wandb.sdk.artifacts.storage_handlers import get_multi_handler
 from wandb.sdk.artifacts.storage_layout import StorageLayout
 from wandb.sdk.artifacts.storage_policy import StoragePolicy
 from wandb.sdk.internal.thread_local_settings import _thread_local_api_settings
@@ -76,29 +66,8 @@ class WandbStoragePolicy(StoragePolicy):
         self._session.mount("http://", adapter)
         self._session.mount("https://", adapter)
 
-        s3 = S3Handler()
-        gcs = GCSHandler()
-        azure = AzureHandler()
-        http = HTTPHandler(self._session)
-        https = HTTPHandler(self._session, scheme="https")
-        artifact = WBArtifactHandler()
-        local_artifact = WBLocalArtifactHandler()
-        file_handler = LocalFileHandler()
-
         self._api = api or InternalApi()
-        self._handler = MultiHandler(
-            handlers=[
-                s3,
-                gcs,
-                azure,
-                http,
-                https,
-                artifact,
-                local_artifact,
-                file_handler,
-            ],
-            default_handler=TrackingHandler(),
-        )
+        self._handler = get_multi_handler(self._session)
 
     def config(self) -> Dict:
         return self._config
