a99809e260162faa61da8f7e64a4739545ba7fd1
Hugh Wimberly <hugh.wimberly@gmail.com>
2023-08-22 10:23:09 -0700

    Add not equality tests as well

--- tests/pytest_tests/unit_tests/test_artifacts/test_artifact_manifest_entry.py
+++ tests/pytest_tests/unit_tests/test_artifacts/test_artifact_manifest_entry.py
@@ -4,7 +4,7 @@ from wandb.sdk.artifacts.artifact_manifest_entry import ArtifactManifestEntry
 def test_repr():
     entry = ArtifactManifestEntry(
         path="foo",
-        digest="bar",
+        digest="",
         ref="baz",
         birth_artifact_id="qux",
         size=123,
@@ -14,12 +14,15 @@ def test_repr():
     assert eval(repr(entry)) == entry
 
     blank_entry = ArtifactManifestEntry(
-        path="foo", digest="", ref="", birth_artifact_id="", size=0
+        path="foo", digest="bar", ref="", birth_artifact_id="", size=0
     )
     assert (
         repr(blank_entry) == "ArtifactManifestEntry"
-        "(path='foo', digest='', ref='', birth_artifact_id='', size=0)"
+        "(path='foo', digest='bar', ref='', birth_artifact_id='', size=0)"
     )
+    assert entry != blank_entry
+    assert entry != repr(entry)
 
     short_entry = ArtifactManifestEntry(path="foo", digest="bar")
     assert repr(short_entry) == "ArtifactManifestEntry(path='foo', digest='bar')"
+    assert entry != short_entry
