b1c89a5e74a4e3654d4da1c892cc84138ea14b53
Katia <katia@wandb.com>
2023-08-21 23:25:09 -0700

    remove duplication

--- nexus/internal/uploader/upload_manager.go
+++ nexus/internal/uploader/upload_manager.go
@@ -121,9 +121,6 @@ func (um *UploadManager) AddTask(task *UploadTask) {
 
 // Close closes the uploader
 func (um *UploadManager) Close() {
-	if um == nil {
-		return
-	}
 	um.logger.Debug("uploader: Close")
 	if um.inChan == nil {
 		return
