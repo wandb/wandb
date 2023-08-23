827c2079c7eb90b0344e4e5fca6108f845d4e280
Griffin Tarpenning <griffin.tarpenning@wandb.com>
2023-08-23 10:57:36 -0700

    fix(launch): manually created image jobs can rerun correctly (#6148)

--- wandb/sdk/interface/interface.py
+++ wandb/sdk/interface/interface.py
@@ -466,7 +466,7 @@ class InterfaceBase:
             source.git.entrypoint.extend(metadata.get("entrypoint", []))
             source.git.notebook = metadata.get("notebook", False)
         elif source_type == "image":
-            source.image.image = metadata.get("image", "")
+            source.image.image = metadata.get("docker", "")
         else:
             raise ValueError("Invalid source type")
 
