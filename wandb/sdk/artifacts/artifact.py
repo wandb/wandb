863684695d4736aa924f13d6ddd93e487e9c6030
Hugh Wimberly <hugh.wimberly@wandb.com>
2023-08-23 21:06:01 -0700

    chore(artifacts): consolidate LinkArtifact calls  (#6138)

--- wandb/sdk/artifacts/artifact.py
+++ wandb/sdk/artifacts/artifact.py
@@ -1987,6 +1987,7 @@ class Artifact:
             },
         )
 
+    @normalize_exceptions
     def link(self, target_path: str, aliases: Optional[List[str]] = None) -> None:
         """Link this artifact to a portfolio (a promoted collection of artifacts).
 
@@ -1999,66 +2000,21 @@ class Artifact:
         Raises:
             ArtifactNotLoggedError: if the artifact has not been logged
         """
-        self._ensure_logged("link")
         if not self._ttl_is_inherited:
             termwarn(
                 "Artifact TTL will be removed for source artifacts that are linked to portfolios."
             )
-        self._link(target_path, aliases)
 
-    @normalize_exceptions
-    def _link(self, target_path: str, aliases: Optional[List[str]] = None) -> None:
-        if ":" in target_path:
-            raise ValueError(
-                f"target_path {target_path} cannot contain `:` because it is not an "
-                f"alias."
-            )
-
-        portfolio, project, entity = util._parse_entity_project_item(target_path)
-        aliases = util._resolve_aliases(aliases)
-
-        run_entity = wandb.run.entity if wandb.run else None
-        run_project = wandb.run.project if wandb.run else None
-        entity = entity or run_entity or self.entity
-        project = project or run_project or self.project
-
-        mutation = gql(
-            """
-            mutation LinkArtifact(
-                $artifactID: ID!,
-                $artifactPortfolioName: String!,
-                $entityName: String!,
-                $projectName: String!,
-                $aliases: [ArtifactAliasInput!]
-            ) {
-                linkArtifact(
-                    input: {
-                        artifactID: $artifactID,
-                        artifactPortfolioName: $artifactPortfolioName,
-                        entityName: $entityName,
-                        projectName: $projectName,
-                        aliases: $aliases
-                    }
-                ) {
-                    versionIndex
-                }
-            }
-            """
-        )
-        assert self._client is not None
-        self._client.execute(
-            mutation,
-            variable_values={
-                "artifactID": self.id,
-                "artifactPortfolioName": portfolio,
-                "entityName": entity,
-                "projectName": project,
-                "aliases": [
-                    {"alias": alias, "artifactCollectionName": portfolio}
-                    for alias in aliases
-                ],
-            },
-        )
+        if wandb.run is None:
+            with wandb.init(
+                entity=self._source_entity,
+                project=self._source_project,
+                job_type="auto",
+                settings=wandb.Settings(silent="true"),
+            ) as run:
+                run.link_artifact(self, target_path, aliases)
+        else:
+            wandb.run.link_artifact(self, target_path, aliases)
 
     def used_by(self) -> List[Run]:
         """Get a list of the runs that have used this artifact.
