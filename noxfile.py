ad2c9e1554e25e8ad488e43b119d976ca7e969ff
Dmitry Duev <dima@wandb.com>
2023-08-24 17:23:33 -0700

    add docstrings to noxfile

--- noxfile.py
+++ noxfile.py
@@ -7,6 +7,7 @@ NEXUS_VERSION = "0.0.1a3"
 
 @nox.session(python=False, name="build-nexus")
 def build_nexus(session):
+    """Builds the nexus binary for the current platform."""
     system = platform.system().lower()
     arch = "amd64" if platform.machine() == "x86_64" else platform.machine()
     session.run(
@@ -23,11 +24,13 @@ def build_nexus(session):
 
 @nox.session(python=False, name="build-nexus-all")
 def build_nexus_all(session):
+    """Builds the nexus binary for all platforms."""
     session.run("python", "-m", "build", "-w", "-n", "./nexus", external=True)
 
 
 @nox.session(python=False, name="install-nexus")
 def install_nexus(session):
+    """Installs the nexus wheel into the current environment."""
     session.run(
         "pip",
         "install",
