48338a4150f5bc8f00ee47c1810c04eb7d123dd3
Dmitry Duev <dima@wandb.com>
2023-08-29 12:39:40 -0700

    docs(nexus): add docs on running nexus-related system tests locally

--- noxfile.py
+++ noxfile.py
@@ -38,3 +38,40 @@ def install_nexus(session):
         f"./nexus/dist/wandb_core-{NEXUS_VERSION}-py3-none-any.whl",
         external=True,
     )
+
+
+@nox.session(python=False, name="list-failing-tests-nexus")
+def list_failing_tests_nexus(session):
+    """Lists the nexus failing tests grouped by feature."""
+    import pandas as pd
+    import pytest
+
+    class MyPlugin:
+        def __init__(self):
+            self.collected = []
+            self.features = []
+
+        def pytest_collection_modifyitems(self, items):
+            for item in items:
+                marks = item.own_markers
+                for mark in marks:
+                    if mark.name == "nexus_failure":
+                        self.collected.append(item.nodeid)
+                        self.features.append(
+                            {
+                                "name": item.nodeid,
+                                "feature": mark.kwargs.get("feature", "unspecified"),
+                            }
+                        )
+
+        def pytest_collection_finish(self):
+            print("\n\nFailing tests grouped by feature:")
+            df = pd.DataFrame(self.features)
+            for feature, group in df.groupby("feature"):
+                print(f"\n{feature}:")
+                for name in group["name"]:
+                    print(f"  {name}")
+
+    my_plugin = MyPlugin()
+    pytest.main(
+        ["-m", "nexus_failure", "tests/pytest_tests/system_tests/test_core", '--collect-only'], plugins=[my_plugin])
