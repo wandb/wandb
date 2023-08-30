79b3bbdd3ff371d860dae7a1a1dddbfd328d1bf8
Ishita Bindlish <ishita.bindlish@wandb.com>
2023-08-30 11:50:04 -0700

    fix link artifact unit test

--- nexus/pkg/server/sender_test.go
+++ nexus/pkg/server/sender_test.go
@@ -77,12 +77,21 @@ func TestSendRun(t *testing.T) {
 }
 
 func TestSendLinkArtifact(t *testing.T) {
+	// Verify that arguments are properly passed through to graphql
 	to := nexustest.MakeTestObject(t)
 	defer to.TeardownTest()
 
 	sender := makeSender(to.MockClient, make(chan *service.Result, 1))
 
-	run := &service.Record{
+	respEncode := &graphql.Response{
+		Data: &gql.LinkArtifactResponse{
+			LinkArtifact: &gql.LinkArtifactLinkArtifactLinkArtifactPayload{
+				VersionIndex: nexustest.IntPtr(0),
+			},
+		}}
+
+	// 1. When both clientId and serverId are sent, serverId is used
+	linkArtifact := &service.Record{
 		RecordType: &service.Record_LinkArtifact{
 			LinkArtifact: &service.LinkArtifactRecord{
 				ClientId:         "clientId",
@@ -96,12 +105,6 @@ func TestSendLinkArtifact(t *testing.T) {
 		},
 	}
 
-	respEncode := &graphql.Response{
-		Data: &gql.LinkArtifactResponse{
-			LinkArtifact: &gql.LinkArtifactLinkArtifactLinkArtifactPayload{
-				VersionIndex: nexustest.IntPtr(0),
-			},
-		}}
 	to.MockClient.EXPECT().MakeRequest(
 		gomock.Any(), // context.Context
 		gomock.Any(), // *graphql.Request
@@ -109,10 +112,80 @@ func TestSendLinkArtifact(t *testing.T) {
 	).Return(nil).Do(nexustest.InjectResponse(
 		respEncode,
 		func(vars nexustest.RequestVars) {
-			assert.Equal(t, "portfolioProject", vars["portfolio_project"]) // Currently vars["<anything>"] returns nil
+			assert.Equal(t, "portfolioProject", vars["projectName"])
+			assert.Equal(t, "portfolioEntity", vars["entityName"])
+			assert.Equal(t, "portfolioName", vars["artifactPortfolioName"])
+			assert.Nil(t, vars["clientId"])
+			assert.Equal(t, "serverId", vars["artifactId"])
 		},
 	))
 
-	sender.sendRecord(run)
+	sender.sendRecord(linkArtifact)
+	<-sender.outChan
+
+	// 2. When only clientId is sent, clientId is used
+	linkArtifact = &service.Record{
+		RecordType: &service.Record_LinkArtifact{
+			LinkArtifact: &service.LinkArtifactRecord{
+				ClientId:         "clientId",
+				ServerId:         "",
+				PortfolioName:    "portfolioName",
+				PortfolioEntity:  "portfolioEntity",
+				PortfolioProject: "portfolioProject",
+			}},
+		Control: &service.Control{
+			MailboxSlot: "junk",
+		},
+	}
+
+	to.MockClient.EXPECT().MakeRequest(
+		gomock.Any(), // context.Context
+		gomock.Any(), // *graphql.Request
+		gomock.Any(), // *graphql.Response
+	).Return(nil).Do(nexustest.InjectResponse(
+		respEncode,
+		func(vars nexustest.RequestVars) {
+			assert.Equal(t, "portfolioProject", vars["projectName"])
+			assert.Equal(t, "portfolioEntity", vars["entityName"])
+			assert.Equal(t, "portfolioName", vars["artifactPortfolioName"])
+			assert.Equal(t, "clientId", vars["clientId"])
+			assert.Nil(t, vars["artifactId"])
+		},
+	))
+
+	sender.sendRecord(linkArtifact)
+	<-sender.outChan
+
+	// 2. When only serverId is sent, serverId is used
+	linkArtifact = &service.Record{
+		RecordType: &service.Record_LinkArtifact{
+			LinkArtifact: &service.LinkArtifactRecord{
+				ClientId:         "",
+				ServerId:         "serverId",
+				PortfolioName:    "portfolioName",
+				PortfolioEntity:  "portfolioEntity",
+				PortfolioProject: "portfolioProject",
+			}},
+		Control: &service.Control{
+			MailboxSlot: "junk",
+		},
+	}
+
+	to.MockClient.EXPECT().MakeRequest(
+		gomock.Any(), // context.Context
+		gomock.Any(), // *graphql.Request
+		gomock.Any(), // *graphql.Response
+	).Return(nil).Do(nexustest.InjectResponse(
+		respEncode,
+		func(vars nexustest.RequestVars) {
+			assert.Equal(t, "portfolioProject", vars["projectName"])
+			assert.Equal(t, "portfolioEntity", vars["entityName"])
+			assert.Equal(t, "portfolioName", vars["artifactPortfolioName"])
+			assert.Nil(t, vars["clientId"])
+			assert.Equal(t, "serverId", vars["artifactId"])
+		},
+	))
+
+	sender.sendRecord(linkArtifact)
 	<-sender.outChan
 }
