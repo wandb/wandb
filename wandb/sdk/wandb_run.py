9d4cb4c9ecf2b48a92250c30142e4f4865366d98
Ishita Bindlish <ishita.bindlish@wandb.com>
2023-09-05 13:25:11 -0700

    move model apis to run

--- wandb/sdk/wandb_run.py
+++ wandb/sdk/wandb_run.py
@@ -3034,6 +3034,102 @@ class Run:
         artifact.finalize()
         return artifact, _resolve_aliases(aliases)
 
+    def log_model(
+        self,
+        local_path: StrPath,
+        model_name: Optional[str] = None,
+        aliases: Optional[List[str]] = None,
+    ) -> None:
+        """Declare a model artifact as an output of a run.
+
+        Arguments:
+            local_path: (str) A path to the contents of this model,
+                can be in the following forms:
+                    - `/local/directory`
+                    - `/local/directory/file.txt`
+                    - `s3://bucket/path`
+            model_name: (str, optional) An artifact name. May be prefixed with entity/project.
+                Valid names can be in the following forms:
+                    - name:version
+                    - name:alias
+                    - digest.
+                This will default to the basename of the path prepended with the current
+                run id  if not specified.
+            aliases: (list, optional) Aliases to apply to this artifact,
+                    defaults to `["latest"]`
+
+        Returns:
+            None
+        """
+        self.log_artifact(
+            artifact_or_path=local_path, name=model_name, type="model", aliases=aliases
+        )
+        return None
+
+    def use_model(self, model_name: str) -> StrPath:
+        """Download a logged model artifact.
+
+        Arguments:
+            model_name: (str) A model artifact name.
+                May be prefixed with entity/project/. Valid names
+                can be in the following forms:
+                    - name:version
+                    - name:alias
+                    - digest.
+        """
+
+        artifact = self.use_artifact(artifact_or_name=model_name)
+        # TODO: Assert type == model?
+        local_path = artifact.download()
+        return local_path
+
+    def link_model(
+        self,
+        local_path: StrPath,
+        linked_model_name: str,
+        model_name: Optional[str] = None,
+        aliases: Optional[List[str]] = None,
+    ) -> None:
+        """Link a model version to a model portfolio (a promoted collection of model artifacts).
+
+        The linked model will be visible in the UI for the specified portfolio.
+
+        Arguments:
+            local_path: (str) A path to the contents of this model,
+                can be in the following forms:
+                    - `/local/directory`
+                    - `/local/directory/file.txt`
+                    - `s3://bucket/path`
+            linked_model_name: (str) - the name of the portfolio that the model is to be linked to. The entity will be derived from the run
+            model_name: (str) - the name of the model artifact that files in local_path will be logged to.
+            aliases: (List[str], optional) - alias(es) that will only be applied on this linked artifact
+                inside the portfolio.
+                The alias "latest" will always be applied to the latest version of an artifact that is linked.
+
+        Returns:
+            None
+        """
+        temp_run = False
+        if wandb.run is None:
+            run = wandb.init(settings=wandb.Settings(silent="true"))
+            temp_run = True
+        else:
+            run = wandb.run
+
+        name_parts = linked_model_name.split("/")
+        assert len(name_parts) == 1
+        project = "model-registry"
+        target_path = run.entity + "/" + project + "/" + linked_model_name
+
+        artifact = run.log_artifact(
+            artifact_or_path=local_path, name=model_name, type="model"
+        )
+        run.link_artifact(artifact=artifact, target_path=target_path, aliases=aliases)
+
+        if temp_run:
+            run.finish()
+        return None
+
     @_run_decorator._noop_on_finish()
     @_run_decorator._attach
     def alert(
