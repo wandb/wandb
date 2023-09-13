dab3f4b122ed533f471acbf3895b2f07411a7900
Dmitry Duev <dima@wandb.com>
2023-09-12 17:43:07 -0700

    use go in gh action

---
+++ nexus/scripts/build/install_go.py
@@ -0,0 +1,13 @@
+import subprocess
+
+VERSION = "1.21.1"
+
+
+def go_get_go():
+    subprocess.check_call(["wget", f"https://golang.org/dl/go{VERSION}.linux-amd64.tar.gz"])
+    subprocess.check_call(["tar", "-C", "/usr/local", "-xzf", f"go{VERSION}.linux-amd64.tar.gz"])
+    subprocess.check_call(["rm", f"go{VERSION}.linux-amd64.tar.gz"])
+
+
+if __name__ == "__main__":
+    go_get_go()
