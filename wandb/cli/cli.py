a95a1c001c4cfcf7a1b7b84f37bd628cb99d8c13
Kyle Goyette <kyle@wandb.com>
2023-09-13 12:50:25 -0700

    wip

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -1342,14 +1342,14 @@ def launch(
                 entity=entity,
                 docker_image=docker_image,
                 name=name,
+                entry_point=entry_point,
+                version=git_version,
                 resource=resource,
                 resource_args=resource_args,
                 launch_config=config,
                 synchronous=(not run_async),
                 run_id=run_id,
                 repository=repository,
-                entry_point=entry_point,
-                git_version=git_version,
             )
             if run.get_status().state in ["failed", "stopped", "preempted"]:
                 wandb.termerror("Launched run exited with non-zero status")
