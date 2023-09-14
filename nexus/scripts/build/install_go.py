94a73e7d86bfb6b45958bbc889cc34663e6cada7
Dmitry Duev <dima@wandb.com>
2023-09-14 11:19:11 -0700

    update go get go

--- nexus/scripts/build/install_go.py
+++ nexus/scripts/build/install_go.py
@@ -7,16 +7,17 @@ VERSION = "1.21.1"
 def go_get_go():
     system = platform.system().lower()
     machine = platform.machine().lower().replace("x86_64", "amd64")
-    extension = "tar.gz" if system != "windows" else "zip"
+    extension = "tar.gz" if system != "windows" else "msi"
+    out_path = "/usr/local" if system == "Linux" else "/tmp/go"
 
     file_name = f"go{VERSION}.{system}-{machine}.{extension}"
 
     subprocess.check_call(["wget", f"https://golang.org/dl/{file_name}"])
 
     if system != "windows":
-        subprocess.check_call(["tar", "-C", "/usr/local", "-xzf", file_name])
+        subprocess.check_call(["tar", "-C", out_path, "-xzf", file_name])
     else:
-        subprocess.check_call(["unzip", file_name, "-d", "/usr/local"])
+        subprocess.check_call(["msiexec", "/i", file_name, "/quiet"])
 
 
 if __name__ == "__main__":
