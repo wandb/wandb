02d92235a13f30b199e8471af2d6950f93f6e301
Dmitry Duev <dima@wandb.com>
2023-09-15 01:12:27 -0700

    gnu gold linker, is that you bro?

--- nexus/setup.py
+++ nexus/setup.py
@@ -64,7 +64,9 @@ class NexusBase:
             .strip()
         )
 
-        ldflags = f"-s -w -X main.commit={commit}"
+        ldflags = f'-s -w -X main.commit={commit}'
+        if f"{goos}-{goarch}" in PLATFORMS_TO_BUILD_WITH_CGO and goos == "linux":
+            ldflags += ' -extldflags "-fuse-ld=gold -Wl,--weak-unresolved-symbols"'
         cmd = (
             "go",
             "build",
