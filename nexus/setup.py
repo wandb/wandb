6dc2d056b55dcc8fb5d65cdd47ab249e43fa3132
Dmitry Duev <dima@wandb.com>
2023-09-15 17:32:33 -0700

    build(nexus): fix reading readme on windows

--- nexus/setup.py
+++ nexus/setup.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
         name="wandb-core" if not _is_wandb_core_alpha else "wandb-core-alpha",
         version=NEXUS_VERSION,
         description="W&B Core Library",
-        long_description=open("README.md").read(),
+        long_description=open("README.md", encoding="utf-8").read(),
         long_description_content_type="text/markdown",
         packages=[PACKAGE],
         zip_safe=False,
