2cdef14520d1ef6584cea70dfc3a99ef2025bfb8
Ishita Bindlish <ishita.bindlish@wandb.com>
2023-09-22 13:20:02 -0700

    add dir test to improve test coverage for s3 handler

--- tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts.py
+++ tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts.py
@@ -544,6 +544,21 @@ def test_add_s3_reference_object():
     }
 
 
+def test_add_s3_reference_object_dir():
+    artifact = wandb.Artifact(type="dataset", name="my-arty")
+    mock_boto(artifact, content_type="application/x-directory; charset=UTF-8")
+    artifact.add_reference("s3://my-bucket/my_dir")
+
+    assert artifact.digest == "17955d00a20e1074c3bc96c74b724bfe"
+    manifest = artifact.manifest.to_manifest_json()
+    assert manifest["contents"]["my_object.pb"] == {
+        "digest": "1234567890abcde",
+        "ref": "s3://my-bucket/my_dir",
+        "extra": {"etag": "1234567890abcde", "versionID": "1"},
+        "size": 10,
+    }
+
+
 def test_add_s3_reference_object_no_version():
     artifact = wandb.Artifact(type="dataset", name="my-arty")
     mock_boto(artifact, version_id=None)
