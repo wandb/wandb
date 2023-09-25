4ae0044f212519055b2ad7035b6b79006b362f66
Jeff Raubitschek <jeff@wandb.com>
2023-09-25 11:18:54 -0700

    add session login stubs

---
+++ nexus/lang/cpp/examples/train_session.cpp
@@ -0,0 +1,31 @@
+#include <libwandb_cpp.h>
+
+int main() {
+  auto wb = new wandb::Session();
+
+  wb->loginSession({
+          wandb::session::WithHostname("host"),
+          wandb::session::WithAPIKey("myapikey"),
+          });
+  wandb::Config config = {
+      {"param1", 4},
+      {"param2", 4.2},
+      {"param3", "smiles"},
+  };
+  auto run = wb->initRun({
+          wandb::run::WithConfig(config),
+          });
+
+  for (int i = 0; i < 5; i++) {
+    wandb::History history = {
+        {"val", 3.14 + i},
+        {"val2", 1.23 + i},
+        {"val23", 1},
+        {"cat", "dog"},
+    };
+    run.log(history);
+  }
+
+  run.finish();
+  return 0;
+}
