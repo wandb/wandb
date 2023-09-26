d9829427f09b1a8cc8e4f4b164738869dd6f735b
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2023-09-26 16:51:38 -0700

    feat(nexus): implement config debouncing mechanism (#6331)
    Co-authored-by: Dmitry Duev <dima@wandb.com>
    Co-authored-by: Dmitry Duev <dmitryduev@users.noreply.github.com>

--- nexus/pkg/server/sender.go
+++ nexus/pkg/server/sender.go
@@ -9,6 +9,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/wandb/wandb/nexus/internal/debounce"
+
 	"github.com/wandb/wandb/nexus/internal/clients"
 	"github.com/wandb/wandb/nexus/internal/gql"
 	"github.com/wandb/wandb/nexus/internal/nexuslib"
@@ -27,7 +29,9 @@ import (
 const (
 	MetaFilename = "wandb-metadata.json"
 	// RFC3339Micro Modified from time.RFC3339Nano
-	RFC3339Micro = "2006-01-02T15:04:05.000000Z07:00"
+	RFC3339Micro             = "2006-01-02T15:04:05.000000Z07:00"
+	configDebouncerRateLimit = 1 / 30.0 // todo: audit rate limit
+	configDebouncerBurstSize = 1        // todo: audit burst size
 )
 
 // Sender is the sender for a stream it handles the incoming messages and sends to the server
@@ -66,6 +70,8 @@ type Sender struct {
 	telemetry *service.TelemetryRecord
 
 	ms *MetricSender
+
+	configDebouncer *debounce.Debouncer
 
 	// Keep track of summary which is being updated incrementally
 	summaryMap map[string]*service.SummaryItem
@@ -156,6 +162,12 @@ func NewSender(ctx context.Context, settings *service.Settings, logger *observab
 		)
 
 	}
+	sender.configDebouncer = debounce.NewDebouncer(
+		configDebouncerRateLimit,
+		configDebouncerBurstSize,
+		logger,
+	)
+
 	return sender
 }
 
@@ -163,6 +175,8 @@ func NewSender(ctx context.Context, settings *service.Settings, logger *observab
 func (s *Sender) do(inChan <-chan *service.Record) {
 	defer s.logger.Reraise()
 	s.logger.Info("sender: started", "stream_id", s.settings.RunId)
+
+	s.configDebouncer.Start(s.upsertConfig)
 
 	for record := range inChan {
 		s.sendRecord(record)
@@ -276,6 +290,7 @@ func (s *Sender) sendDefer(request *service.DeferRequest) {
 		request.State++
 		s.sendRequestDefer(request)
 	case service.DeferRequest_FLUSH_DEBOUNCER:
+		s.configDebouncer.Close()
 		request.State++
 		s.sendRequestDefer(request)
 	case service.DeferRequest_FLUSH_OUTPUT:
@@ -545,15 +560,9 @@ func (s *Sender) sendSummary(_ *service.Record, summary *service.SummaryRecord) 
 	s.fileStream.StreamRecord(record)
 }
 
-// sendConfig sends a config record to the server via an upsertBucket mutation
-// and updates the in memory config
-func (s *Sender) sendConfig(_ *service.Record, configRecord *service.ConfigRecord) {
+func (s *Sender) upsertConfig() {
 	if s.graphqlClient == nil {
 		return
-	}
-
-	if configRecord != nil {
-		s.updateConfig(configRecord)
 	}
 
 	config := s.serializeConfig()
@@ -584,6 +593,15 @@ func (s *Sender) sendConfig(_ *service.Record, configRecord *service.ConfigRecor
 	if err != nil {
 		s.logger.Error("sender: sendConfig:", "error", err)
 	}
+}
+
+// sendConfig sends a config record to the server via an upsertBucket mutation
+// and updates the in memory config
+func (s *Sender) sendConfig(_ *service.Record, configRecord *service.ConfigRecord) {
+	if configRecord != nil {
+		s.updateConfig(configRecord)
+	}
+	s.configDebouncer.Trigger()
 }
 
 // sendSystemMetrics sends a system metrics record via the file stream
