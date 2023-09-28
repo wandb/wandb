24c0dd138fca2d8382604cdd8dfcf318c3d15a70
Kyle Goyette <kyle@wandb.com>
2023-09-28 08:40:52 -0700

    wip

--- tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
+++ tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
@@ -1,7 +1,7 @@
 from unittest.mock import MagicMock
 
 import wandb
-from wandb.sdk.launch.agent.agent import JobAndRunStatusTracker
+from wandb.sdk.launch.agent.job_status_tracker import JobAndRunStatusTracker
 
 
 def test_check_stop_run_not_exist(wandb_init):
@@ -18,6 +18,7 @@ def test_check_stop_run_not_exist(wandb_init):
 
     res = job_tracker.check_wandb_run_stopped(api)
     assert not res
+    run.finish()
 
 
 def test_check_stop_run_exist_stopped(wandb_init):
@@ -38,3 +39,4 @@ def test_check_stop_run_exist_stopped(wandb_init):
 
     res = job_tracker.check_wandb_run_stopped(api)
     assert res
+    run.finish()
