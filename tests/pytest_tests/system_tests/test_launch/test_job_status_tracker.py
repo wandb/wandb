96f8325b1124c49eac1f2d3b62aca5320ee604cf
Kyle Goyette <kyle@wandb.com>
2023-09-28 10:03:46 -0700

    wip

--- tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
+++ tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
@@ -1,3 +1,5 @@
+import base64
+from unittest import mock
 from unittest.mock import MagicMock
 
 import wandb
@@ -21,22 +23,22 @@ def test_check_stop_run_not_exist(wandb_init):
     run.finish()
 
 
-def test_check_stop_run_exist_stopped(wandb_init):
+def test_check_stop_run_exist_stopped(user, wandb_init):
+    mock.patch("wandb.sdk.wandb_run.thread.interrupt_main", lambda x: None)
     job_tracker = JobAndRunStatusTracker(
         "run_queue_item_id", "test-queue", MagicMock(), MagicMock()
     )
-    run = wandb_init(id="testrun")
-    api = wandb.InternalApi(
-        default_settings={"project": run._project, "entity": run._entity}
-    )
+    run = wandb_init(id="testrun", entity=user)
+    print(run._entity)
+    api = wandb.InternalApi()
+    encoded_run_id = base64.standard_b64encode(
+        f"Run:v1:testrun:{run._project}:{run._entity}".encode()
+    ).decode("utf-8")
     mock_launch_project = MagicMock()
     mock_launch_project.target_entity = run._entity
     mock_launch_project.target_project = run._project
     mock_launch_project.run_id = run._run_id
     job_tracker.update_run_info(mock_launch_project)
-
-    assert api.stop_run(run._run_id)
-
-    res = job_tracker.check_wandb_run_stopped(api)
-    assert res
+    assert api.stop_run(encoded_run_id)
+    assert job_tracker.check_wandb_run_stopped(api)
     run.finish()
