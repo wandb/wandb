1ee11af90a3de43c9dea8915ae42866b16a24c3f
Kyle Goyette <kyle@wandb.com>
2023-09-28 08:00:13 -0700

    wip

--- tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
+++ tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
@@ -1,15 +1,10 @@
-import threading
 from unittest.mock import MagicMock
 
-import pytest
 import wandb
-from wandb.errors import CommError
-from wandb.sdk.launch.agent.agent import JobAndRunStatusTracker, LaunchAgent
-from wandb.sdk.launch.errors import LaunchDockerError, LaunchError
+from wandb.sdk.launch.agent.agent import JobAndRunStatusTracker
 
 
 def test_check_stop_run_not_exist(wandb_init):
-    """Should handle the raised CommError and return False when a run does not exist"""
     job_tracker = JobAndRunStatusTracker(
         "run_queue_item_id", "test-queue", MagicMock(), MagicMock()
     )
@@ -26,7 +21,6 @@ def test_check_stop_run_not_exist(wandb_init):
 
 
 def test_check_stop_run_exist_stopped(wandb_init):
-    """Should handle the raised CommError and return False when a run does not exist"""
     job_tracker = JobAndRunStatusTracker(
         "run_queue_item_id", "test-queue", MagicMock(), MagicMock()
     )
