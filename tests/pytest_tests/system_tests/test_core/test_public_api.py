a96e4d6c5d9b8a447861bcf423069844bfa8244c
Dmitry Duev <dima@wandb.com>
2023-10-04 12:38:31 -0700

    re-re-enable some of the newly-fixed artifact tests

--- tests/pytest_tests/system_tests/test_core/test_public_api.py
+++ tests/pytest_tests/system_tests/test_core/test_public_api.py
@@ -59,6 +59,7 @@ def test_run_from_tensorboard(runner, relay_server, user, api, copy_asset):
         assert len(uploaded_files) == 17
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_fetching_artifact_files(user, wandb_init):
     project = "test"
 
@@ -90,6 +91,7 @@ def test_fetching_artifact_files(user, wandb_init):
     assert open(file_path).read() == "testing"
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_save_aliases_after_logging_artifact(user, wandb_init):
     project = "test"
     run = wandb_init(entity=user, project=project)
@@ -111,6 +113,7 @@ def test_save_aliases_after_logging_artifact(user, wandb_init):
     assert "hello" in aliases
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_update_aliases_on_artifact(user, wandb_init):
     project = "test"
     run = wandb_init(entity=user, project=project)
@@ -150,6 +153,7 @@ def test_update_aliases_on_artifact(user, wandb_init):
     assert "sequence" not in aliases
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_artifact_version(wandb_init):
     def create_test_artifact(content: str):
         art = wandb.Artifact("test-artifact", "test-type")
@@ -181,6 +185,7 @@ def test_artifact_version(wandb_init):
     assert artifact.source_version == "v1"
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_delete_collection(wandb_init):
     with wandb_init(project="test") as run:
         art = wandb.Artifact("test-artifact", "test-type")
@@ -216,6 +221,7 @@ def test_delete_collection(wandb_init):
         )
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_log_with_wrong_type_entity_project(wandb_init, logged_artifact):
     # todo: logged_artifact does not work with nexus
     entity, project = logged_artifact.entity, logged_artifact.project
@@ -267,6 +273,7 @@ def test_run_queue(user):
         queue.delete()
 
 
+@pytest.mark.nexus_failure(feature="artifacts")
 def test_run_log_artifact(wandb_init):
     # Prepare data.
     with wandb_init() as run:
