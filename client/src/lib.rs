07e92248229a8b0916734ae3ee33b4ad88b72553
Dmitry Duev <dima@wandb.com>
2023-10-17 14:40:47 -0700

    add lib

---
+++ client/src/lib.rs
@@ -0,0 +1,22 @@
+use pyo3::prelude::*;
+
+pub mod connection;
+pub mod launcher;
+pub mod run;
+pub mod session;
+pub mod wandb_internal;
+
+// /// Communication layer between user code and nexus
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn wandb_lib(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(session::generate_run_id, m)?)?;
+    // m.add_class::<session::Lol>()?;
+    m.add_class::<session::Settings>()?;
+    m.add_class::<session::Session>()?;
+    // m.add_class::<session::Run>()?;
+    Ok(())
+}
