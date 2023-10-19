3e03a603f678384d565ea5245db26f6300dace35
Jeff Raubitschek <jeff@wandb.com>
2023-10-18 17:46:52 -0700

    add table test

--- nexus/internal/flowcontrol/flowcontrol_test.go
+++ nexus/internal/flowcontrol/flowcontrol_test.go
@@ -1,11 +1,15 @@
 package flowcontrol
 
 import (
+	"fmt"
+	"strings"
+	"strconv"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/wandb/wandb/nexus/internal/nexustest"
 	"github.com/wandb/wandb/nexus/pkg/service"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
@@ -30,27 +34,51 @@ func makeRecord(offset int64) *service.Record {
 }
 
 func TestFlowControl(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	m := NewMockTestFlowcontrol(ctrl)
-
-	settings := &service.Settings{
-		XNetworkBuffer: &wrapperspb.Int32Value{Value: 32},
+	cases := []struct{NetworkBuffer int32; Records []int64; Expect string}{
+		// no flowcontrol
+		{64, []int64{0, 16, 32}, "s0,s1,s2"},
+		// pause
+		{32, []int64{0, 16, 32}, "s0,s1,s2,p"},
+		// pause and no final send
+		{32, []int64{0, 16, 32, 40}, "s0,s1,s2,p"},
 	}
-	records := []*service.Record{
-		makeRecord(0),
-		makeRecord(10),
-		makeRecord(32),
-	}
-	gomock.InOrder(
-		m.EXPECT().SendRecord(nexustest.MatchRecord(records[0], nexustest.RecordCompare)),
-		m.EXPECT().SendRecord(nexustest.MatchRecord(records[1], nexustest.RecordCompare)),
-		m.EXPECT().SendRecord(nexustest.MatchRecord(records[2], nexustest.RecordCompare)),
-		m.EXPECT().SendPause(),
-	)
-	// m.EXPECT().RecoverRecords(gomock.Eq(99), gomock.Eq(33))
-	flowControl := NewFlowControl(settings, m.SendRecord, m.SendPause, m.RecoverRecords)
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%d+%+v", tc.NetworkBuffer, tc.Records), func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			m := NewMockTestFlowcontrol(ctrl)
 
-	for _, record := range records {
-		flowControl.Flow(record)
+			settings := &service.Settings{
+				XNetworkBuffer: &wrapperspb.Int32Value{Value: tc.NetworkBuffer},
+			}
+			records := []*service.Record{}
+			for _, recordOffset := range(tc.Records) {
+				records = append(records, makeRecord(recordOffset))
+			}
+
+			var orders []*gomock.Call
+			expectStrings := strings.Split(tc.Expect, ",")
+			for _, expect := range expectStrings {
+				var gocall *gomock.Call
+				switch expect[0] {
+				case 's':
+					num, err := strconv.Atoi(expect[1:])
+					assert.Equal(t, nil, err)
+					gocall = m.EXPECT().SendRecord(nexustest.MatchRecord(records[num], nexustest.RecordCompare))
+				case 'p':
+					gocall = m.EXPECT().SendPause()
+				case 'r':
+					gocall = m.EXPECT().RecoverRecords(gomock.Eq(99), gomock.Eq(33))
+				default:
+					panic("unhandled")
+				}
+				orders = append(orders, gocall)
+			}
+			gomock.InOrder(orders...)
+
+			flowControl := NewFlowControl(settings, m.SendRecord, m.SendPause, m.RecoverRecords)
+			for _, record := range records {
+				flowControl.Flow(record)
+			}
+		})
 	}
 }
