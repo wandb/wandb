52d758b1c9d0f2f159a5df6a9854c95c14586c3f
Dmitry Duev <dima@wandb.com>
2023-10-18 19:48:47 -0700

    move functions to printer

--- client/src/binder.rs
+++ client/src/binder.rs
@@ -1,128 +1,11 @@
 // use sentry;
 use std::collections::HashMap;
+use std::time::Duration;
 // use std::io;
 
-use colored::*;
-
 use std::thread;
-use std::time::Duration;
-use std::{cmp::min, fmt::Write};
-
-use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 
 use wandbinder::{printer, session};
-
-fn hyperlink(text: &str, url: &str) -> ColoredString {
-    format!("\x1B]8;;{}\x07{}\x1B]8;;\x07", url, text).white()
-}
-
-fn get_prefix() -> ColoredString {
-    // String::from("│ ").truecolor(250, 193, 60)
-    // String::from("▏").truecolor(250, 193, 60)
-    // String::from("▎").truecolor(250, 193, 60)
-    String::from("▍ ").truecolor(250, 193, 60)
-    // String::from("▌ ").truecolor(250, 193, 60)
-}
-
-fn get_checkmark() -> ColoredString {
-    String::from("✓").truecolor(122, 166, 56)
-}
-
-// fn get_crossmark() -> ColoredString {
-//     String::from("✗").truecolor(227, 50, 79)
-// }
-
-fn get_header() -> ColoredString {
-    String::from("wandb").white().bold()
-}
-
-fn print_header(name: &str, url: &str) {
-    let link = hyperlink(name, url);
-
-    let prefix = get_prefix();
-    let checkmark = get_checkmark();
-    let header = get_header();
-
-    println!("{}{}", prefix, header);
-
-    // spinner
-    let pb = ProgressBar::new_spinner();
-    pb.enable_steady_tick(Duration::from_millis(120));
-    pb.set_prefix(prefix.to_string());
-    pb.set_style(
-        ProgressStyle::with_template("{prefix}{spinner:.magenta} {msg}")
-            .unwrap()
-            // For more spinners check out the cli-spinners project:
-            // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
-            .tick_strings(&["⠋", "⠙", "⠚", "⠞", "⠖", "⠦", "⠴", "⠲", "⠳", "⠓"]),
-    );
-    pb.set_message("Creating run...");
-    thread::sleep(Duration::from_secs(3));
-
-    // pb.finish_with_message("Done");
-    pb.finish_and_clear();
-
-    println!("{}{} Run created - {}", prefix, checkmark, link);
-    // println!("{}{} {}", prefix, crossmark, dimmed);
-    // println!();
-}
-
-fn print_footer(name: &str, url: &str, run_dir: &str) {
-    let link = hyperlink(name, url);
-
-    let mut downloaded = 0;
-    let total_size = 23123123;
-
-    let prefix = get_prefix();
-    let checkmark = get_checkmark();
-    let header = get_header();
-
-    println!("{}{}", prefix, header);
-
-    // run stats
-    // fake loss that exponentially decreases
-    let loss = vec![0.1, 0.07, 0.05, 0.03, 0.01, 0.005, 0.001, 0.0005];
-    let sparkline = printer::generate_sparkline(loss);
-    let formatted_loss = format!("{}{:<20} {}", prefix, "Loss (0.0005)", sparkline);
-    println!("{}", formatted_loss);
-
-    // fake accuracy slowly increasing to 0.98
-    let accuracy = vec![0.5, 0.75, 0.52, 0.63, 0.43, 0.74, 0.85, 0.92, 0.95, 0.98];
-    let sparkline = printer::generate_sparkline(accuracy);
-    let formatted_accuracy = format!("{}{:<20} {}", prefix, "Accuracy (0.98314)", sparkline);
-    println!("{}", formatted_accuracy);
-
-    let local_dir = String::from(format!("Run dir - {}", run_dir))
-        .white()
-        .dimmed();
-    println!("{}{}", prefix, local_dir);
-
-    // sync progress bar
-    let pb = ProgressBar::new(total_size);
-    pb.set_prefix(prefix.to_string());
-    pb.set_style(
-        ProgressStyle::with_template(
-            "{prefix}Syncing run {wide_bar:.magenta/white.dim} {bytes}/{total_bytes} ({eta})",
-        )
-        .unwrap()
-        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
-            write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
-        })
-        // .progress_chars("⠿⠇"),
-        .progress_chars("⣿⡇"),
-    );
-
-    while downloaded < total_size {
-        let new = min(downloaded + 223211, total_size);
-        downloaded = new;
-        pb.set_position(new);
-        thread::sleep(Duration::from_millis(12));
-    }
-
-    pb.finish_and_clear();
-
-    println!("{}{} Run synced - {}", prefix, checkmark, link);
-}
 
 fn main() {
     // let _guard = sentry::init(
@@ -139,7 +22,7 @@ fn main() {
     let name = "glorious-capybara-23";
     let url = "https://wandb.ai/dimaduev/uncategorized/runs/KEHHBT";
 
-    print_header(name, url);
+    printer::print_header(name, url);
 
     let mut data: HashMap<String, f64> = HashMap::new();
     data.insert("loss".to_string(), 13.37);
@@ -150,5 +33,5 @@ fn main() {
 
     run.finish();
 
-    print_footer(name, url, "/Users/.wandb/run-20201231_123456");
+    printer::print_footer(name, url, "/Users/.wandb/run-20201231_123456");
 }
---
+++ client/src/printer.rs
@@ -0,0 +1,143 @@
+use std::thread;
+use std::time::Duration;
+use std::{cmp::min, fmt::Write};
+
+use colored::*;
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
+
+// const BARS: &'static str = "▁▂▃▄▅▆▇█";
+const BARS: &'static str = "▁▂▃▄▅▆▇";
+
+pub fn generate_sparkline(values: Vec<f64>) -> String {
+    let max_val = values.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+    let min_val = values.iter().cloned().fold(f64::INFINITY, f64::min);
+    let range = max_val - min_val;
+
+    let v = BARS.chars().collect::<Vec<char>>();
+
+    values
+        .iter()
+        .map(|&x| {
+            let normalized = if range == 0.0 {
+                0
+            } else {
+                ((x - min_val) / range * (v.len() - 1) as f64).round() as usize
+            };
+            v[normalized]
+        })
+        .collect()
+}
+
+fn hyperlink(text: &str, url: &str) -> ColoredString {
+    // format!("\x1B]8;;{}\x07{}\x1B]8;;\x07", url, text).white()
+    format!("\x1B]8;;{}\x07{}\x1B]8;;\x07", url, text).truecolor(250, 193, 60)
+    // format!("\x1B]8;;{}\x07{}\x1B]8;;\x07", url, text).truecolor(226, 129, 254)
+}
+
+fn get_prefix() -> ColoredString {
+    // String::from("│ ").truecolor(250, 193, 60)
+    // String::from("▏").truecolor(250, 193, 60)
+    // String::from("▎").truecolor(250, 193, 60)
+    // String::from("▌ ").truecolor(250, 193, 60)
+    String::from("▍ ").truecolor(250, 193, 60) // this one is the best
+}
+
+fn get_checkmark() -> ColoredString {
+    String::from("✓").truecolor(122, 166, 56)
+}
+
+// fn get_crossmark() -> ColoredString {
+//     String::from("✗").truecolor(227, 50, 79)
+// }
+
+fn get_header() -> ColoredString {
+    String::from("wandb").white().bold()
+}
+
+pub fn print_header(name: &str, url: &str) {
+    let link = hyperlink(name, url);
+
+    let prefix = get_prefix();
+    let checkmark = get_checkmark();
+    let header = get_header();
+
+    println!("{}{}", prefix, header);
+
+    // spinner
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(120));
+    pb.set_prefix(prefix.to_string());
+    pb.set_style(
+        ProgressStyle::with_template("{prefix}{spinner:.magenta} {msg}")
+            .unwrap()
+            // For more spinners check out the cli-spinners project:
+            // https://github.com/sindresorhus/cli-spinners/blob/master/spinners.json
+            .tick_strings(&["⠋", "⠙", "⠚", "⠞", "⠖", "⠦", "⠴", "⠲", "⠳", "⠓"]),
+    );
+    pb.set_message("Creating run...");
+    thread::sleep(Duration::from_secs(3));
+
+    // pb.finish_with_message("Done");
+    pb.finish_and_clear();
+
+    println!("{}{} Run created - {}", prefix, checkmark, link);
+    // println!("{}{} {}", prefix, crossmark, dimmed);
+    // println!();
+}
+
+pub fn print_footer(name: &str, url: &str, run_dir: &str) {
+    let link = hyperlink(name, url);
+
+    let mut downloaded = 0;
+    let total_size = 23123123;
+
+    let prefix = get_prefix();
+    let checkmark = get_checkmark();
+    let header = get_header();
+
+    println!("{}{}", prefix, header);
+
+    // run stats
+    // fake loss that exponentially decreases
+    let loss = vec![0.1, 0.07, 0.05, 0.03, 0.01, 0.005, 0.001, 0.0005];
+    let sparkline = generate_sparkline(loss);
+    let formatted_loss = format!("{}{:<20} {}", prefix, "Loss (0.0005)", sparkline);
+    println!("{}", formatted_loss);
+
+    // fake accuracy slowly increasing to 0.98
+    let accuracy = vec![0.5, 0.75, 0.52, 0.63, 0.43, 0.74, 0.85, 0.92, 0.95, 0.98];
+    let sparkline = generate_sparkline(accuracy);
+    let formatted_accuracy = format!("{}{:<20} {}", prefix, "Accuracy (0.98314)", sparkline);
+    println!("{}", formatted_accuracy);
+
+    // sync progress bar
+    let pb = ProgressBar::new(total_size);
+    pb.set_prefix(prefix.to_string());
+    pb.set_style(
+        ProgressStyle::with_template(
+            "{prefix}Syncing run {wide_bar:.magenta/white.dim} {bytes}/{total_bytes} ({eta})",
+        )
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
+            write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
+        })
+        // .progress_chars("⠿⠇"),
+        .progress_chars("⣿⡇"),
+    );
+
+    while downloaded < total_size {
+        let new = min(downloaded + 223211, total_size);
+        downloaded = new;
+        pb.set_position(new);
+        thread::sleep(Duration::from_millis(12));
+    }
+
+    pb.finish_and_clear();
+
+    println!("{}{} Run synced - {}", prefix, checkmark, link);
+
+    let local_dir = String::from(format!("Run dir - {}", run_dir))
+        .white()
+        .dimmed();
+    println!("{}{}", prefix, local_dir);
+}
