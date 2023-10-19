4c262f21b40cab0dfcef5e8e1cd78478ceec6a9b
Jeff Raubitschek <jeff@wandb.com>
2023-10-18 17:50:37 -0700

    clean

--- nexus/internal/flowcontrol/flowcontrol.go
+++ nexus/internal/flowcontrol/flowcontrol.go
@@ -1,28 +1,32 @@
 // Package flowcontrol
 //
 // States:
-//    FORWARDING
-//    PAUSING
+//
+//	FORWARDING
+//	PAUSING
 //
 // New messages:
-//    pb.SenderMarkRequest    writer -> sender (empty message)
-//    pb.StatusReportRequest  sender -> writer (reports current sender progress)
-//    pb.SenderReadRequest    writer -> sender (requests read of transaction log)
+//
+//	pb.SenderMarkRequest    writer -> sender (empty message)
+//	pb.StatusReportRequest  sender -> writer (reports current sender progress)
+//	pb.SenderReadRequest    writer -> sender (requests read of transaction log)
 //
 // Thresholds:
-//    Threshold_High_MaxOutstandingData      - When above this, stop sending requests to sender
-//    Threshold_Mid_StartSendingReadRequests - When below this, start sending read requests
-//    Threshold_Low_RestartSendingData       - When below this, start sending normal records
+//
+//	Threshold_High_MaxOutstandingData      - When above this, stop sending requests to sender
+//	Threshold_Mid_StartSendingReadRequests - When below this, start sending read requests
+//	Threshold_Low_RestartSendingData       - When below this, start sending normal records
 //
 // State machine:
-//    FORWARDING
-//      -> PAUSED if should_pause
-//         There is too much work outstanding to the sender thread, after the current request
-//         lets stop sending data.
-//    PAUSING
-//      -> FORWARDING if should_unpause
-//      -> PAUSING if should_recover
-//      -> PAUSING if should_quiesce
+//
+//	FORWARDING
+//	  -> PAUSED if should_pause
+//	     There is too much work outstanding to the sender thread, after the current request
+//	     lets stop sending data.
+//	PAUSING
+//	  -> FORWARDING if should_unpause
+//	  -> PAUSING if should_recover
+//	  -> PAUSING if should_quiesce
 package flowcontrol
 
 import (
