dc903b60d0936ee99e8571175b9834245a12d33a
luisbergua <luis.bergua@wandb.com>
2023-10-20 19:02:27 +0200

    fix(sdk) fix step logic when resuming runs with no metrics logged

--- wandb/sdk/internal/sender.py
+++ wandb/sdk/internal/sender.py
@@ -717,7 +717,7 @@ class SendManager:
 
         result.response.server_info_response.local_info.CopyFrom(self.get_local_info())
         for message in self._server_messages:
-            # guard against the case the message level returns malformed from server
+            # guard agains the case the message level returns malformed from server
             message_level = str(message.get("messageLevel"))
             message_level_sanitized = int(
                 printer.INFO if not message_level.isdigit() else message_level
@@ -821,7 +821,7 @@ class SendManager:
         # TODO: Do we need to restore config / summary?
         # System metrics runtime is usually greater than history
         self._resume_state.runtime = max(events_rt, history_rt)
-        self._resume_state.step = history.get("_step", -1) + 1 if history else 0
+        self._resume_state.step = history.get("_step", -1) + 1 if history and bool(set(summary.keys()) - {"_runtime", "_timestamp", "_step", "_wandb"}) else 0
         self._resume_state.history = resume_status["historyLineCount"]
         self._resume_state.events = resume_status["eventsLineCount"]
         self._resume_state.output = resume_status["logLineCount"]
