8f3b96703f84814c0b4ab97a13c42db75569cf96
Jeff Raubitschek <jeff@wandb.com>
2023-10-19 17:10:56 -0700

    add builder

---
+++ nexus/pkg/artifacts/builder.go
@@ -0,0 +1,58 @@
+package artifacts
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/wandb/wandb/nexus/pkg/service"
+	"github.com/wandb/wandb/nexus/pkg/utils"
+)
+
+type ArtifactBuilder struct {
+	artifactRecord *service.ArtifactRecord
+}
+
+func NewArtifactBuilder(artifactRecord *service.ArtifactRecord) *ArtifactBuilder {
+	return &ArtifactBuilder{
+		artifactRecord: artifactRecord,
+	}
+}
+
+func (b *ArtifactBuilder) AddData(name string, dataMap map[string]interface{}) error {
+	filename, digest, err := b.writeToFile(dataMap)
+	if err != nil {
+		return err
+	}
+	b.artifactRecord.Manifest.Contents = append(b.artifactRecord.Manifest.Contents,
+		&service.ArtifactManifestEntry{
+			Path: name,
+			Digest: digest,
+			LocalPath: filename,
+		})
+	return nil
+}
+
+func (b *ArtifactBuilder) GetArtifact() *service.ArtifactRecord {
+	return b.artifactRecord
+}
+
+func (b *ArtifactBuilder) writeToFile(dataMap map[string]interface{}) (filename string, digest string, rerr error) {
+	data, rerr := json.Marshal(dataMap)
+	if rerr != nil {
+		return
+	}
+
+	f, rerr := os.CreateTemp("", "tmpfile-")
+	if rerr != nil {
+		return
+	}
+	defer f.Close()
+	_, rerr = f.Write(data)
+	if rerr != nil {
+		return
+	}
+	filename = f.Name()
+
+	digest, rerr = utils.ComputeB64MD5(data)
+	return
+}
