7316f50b801008bef93329fbeb0e0203a0a40af1
Ishita Bindlish <ishita.bindlish@wandb.com>
2023-10-23 11:42:48 -0700

    lint

--- wandb/sdk/artifacts/artifacts_cache.py
+++ wandb/sdk/artifacts/artifacts_cache.py
@@ -5,6 +5,7 @@ import hashlib
 import os
 import shutil
 import subprocess
+import sys
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 from typing import IO, TYPE_CHECKING, ContextManager, Generator, Optional, Tuple
@@ -15,7 +16,6 @@ from wandb.errors import term
 from wandb.sdk.lib.filesystem import files_in
 from wandb.sdk.lib.hashutil import B64MD5, ETag, b64_to_hex_id
 from wandb.sdk.lib.paths import FilePathStr, StrPath, URIStr
-import sys
 
 if TYPE_CHECKING:
     import sys
