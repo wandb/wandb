ac2e5c6028ea6f30dd4014f1dd296c34c0c6cbd6
Ishita Bindlish <ishita.bindlish@wandb.com>
2023-10-27 16:45:27 -0700

    fix download root test

--- wandb/sdk/artifacts/artifact.py
+++ wandb/sdk/artifacts/artifact.py
@@ -1665,6 +1665,9 @@ class Artifact:
         allow_missing_references: bool = False,
     ) -> FilePathStr:
         self._ensure_logged("download")
+
+        root = root or self._default_root()
+        self._add_download_root(root)
         if wandb.run is None:
             with wandb.init(
                 entity=self._source_entity,
