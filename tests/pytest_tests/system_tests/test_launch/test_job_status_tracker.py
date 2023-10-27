bd00768681d18cb09ad24434f1cba02e3cd158d5
gtarpenning <griffin.tarpenning@wandb.com>
2023-10-27 13:09:01 -0400

    chore(launch): improve launch test flake

--- tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
+++ tests/pytest_tests/system_tests/test_launch/test_job_status_tracker.py
@@ -32,7 +32,6 @@ async def test_check_stop_run_exist_stopped(user, wandb_init):
         "run_queue_item_id", "test-queue", MagicMock(), MagicMock()
     )
     run = wandb_init(id="testrun", entity=user)
-    print(run._entity)
     api = wandb.InternalApi()
     encoded_run_id = base64.standard_b64encode(
         f"Run:v1:testrun:{run._project}:{run._entity}".encode()
@@ -41,6 +40,10 @@ async def test_check_stop_run_exist_stopped(user, wandb_init):
     mock_launch_project.target_entity = run._entity
     mock_launch_project.target_project = run._project
     mock_launch_project.run_id = run._run_id
+
+    api_run = api.run_config(project=run._project, entity=run._entity, run=run._run_id)
+    assert api_run
+
     job_tracker.update_run_info(mock_launch_project)
     assert api.stop_run(encoded_run_id)
     assert await job_tracker.check_wandb_run_stopped(api)
