dc7b0360842eaad870370f9bd4fe5ac526cb097f
luisbergua <113048227+luisbergua@users.noreply.github.com>
2023-10-30 19:10:11 +0100

    fix(sdk): fix step logic when resuming runs with no metrics logged (#6480)
    Co-authored-by: Katia Patkin <87335417+kptkin@users.noreply.github.com>

--- wandb/sdk/internal/sender.py
+++ wandb/sdk/internal/sender.py
@@ -821,8 +821,10 @@ class SendManager:
         # TODO: Do we need to restore config / summary?
         # System metrics runtime is usually greater than history
         self._resume_state.runtime = max(events_rt, history_rt)
-        self._resume_state.step = history.get("_step", -1) + 1 if history else 0
-        self._resume_state.history = resume_status["historyLineCount"]
+        last_step = history.get("_step", 0)
+        history_line_count = resume_status["historyLineCount"]
+        self._resume_state.step = last_step + 1 if history_line_count > 0 else last_step
+        self._resume_state.history = history_line_count
         self._resume_state.events = resume_status["eventsLineCount"]
         self._resume_state.output = resume_status["logLineCount"]
         self._resume_state.config = config
