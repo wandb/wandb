439203c0a9ef66c812bc7fa9d8b420d1a8409cd0
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-11-17 19:14:28 +0000

    add: docstring for DiffusersPipelineResolver

--- wandb/integration/diffusers/pipeline_resolver.py
+++ wandb/integration/diffusers/pipeline_resolver.py
@@ -11,6 +11,13 @@ from .resolvers import (
 
 
 class DiffusersPipelineResolver:
+    """Resolver for `DiffusionPipeline` request and responses from
+    [HuggingFace Diffusers](https://huggingface.co/docs/diffusers/index),
+    providing necessary data transformations, formatting, and logging.
+
+    This is based off `wandb.sdk.integration_utils.auto_logging.RequestResponseResolver`.
+    """
+
     def __init__(self) -> None:
         self.wandb_table = None
 
@@ -22,6 +29,18 @@ class DiffusersPipelineResolver:
         start_time: float,
         time_elapsed: float,
     ) -> Any:
+        """Main call method for the `DiffusersPipelineResolver` class.
+
+        Arguments:
+            args: (Sequence[Any]) List of arguments.
+            kwargs: (Dict[str, Any]) Dictionary of keyword arguments.
+            response: (wandb.sdk.integration_utils.auto_logging.Response) The response from
+                the request.
+            start_time: (float) Time when request started.
+            time_elapsed: (float) Time elapsed for the request.
+        Returns:
+            Packed data as a dictionary for logging to wandb, None if an exception occurred.
+        """
         pipeline_name = args[0].__class__.__name__
         resolver = None
         if pipeline_name in SUPPORTED_MULTIMODAL_PIPELINES:
