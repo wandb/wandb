09ed4b761a81cda7c68b32d3d624b0780bc7472d
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2023-11-28 04:25:10 +0000

    chore: make ci happy

--- wandb/integration/diffusers/resolvers/sdxl.py
+++ wandb/integration/diffusers/resolvers/sdxl.py
@@ -2,8 +2,8 @@ import logging
 from typing import Any, Dict, List, Sequence
 
 import wandb
-from wandb.util import get_module
 from wandb.sdk.integration_utils.auto_logging import Response
+from wandb.util import get_module
 
 from .utils import chunkify, get_updated_kwargs
 
