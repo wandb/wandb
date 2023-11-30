8b5bd0ecd0bfec2c7ad0d4cf3f6d1cf839f2a98e
ayulockin <mein2work@gmail.com>
2023-11-30 18:29:01 +0000

    fix version check

--- wandb/integration/openai/__init__.py
+++ wandb/integration/openai/__init__.py
@@ -10,7 +10,11 @@ from pkg_resources import parse_version
 from .openai import autolog
 
 if _openai:
-    if parse_version(_openai.__version__) > parse_version("0.28.1"):
+    if parse_version(_openai.__version__) > parse_version("1.0.1"):
         from .fine_tune import WandbLogger
 else:
-    wandb.termerror("`openai` is not installed. To fix, please `pip install openai`")
+    wandb.termerror(
+        "`openai` is not installed or your current `openai` version is {openai_version}. "
+        "This integration requires openai version 1.0.1 and above. "
+        "To fix, please `pip install openai`"
+    )
