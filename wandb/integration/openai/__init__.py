afffbe60f9867181b5be10048ca9952f6aed5db0
Ayush Thakur <mein2work@gmail.com>
2023-11-30 23:43:26 +0530

    feat(integrations): port OpenAI WandbLogger for openai-python v1.0 (#6498)
    Co-authored-by: Katia <katia@wandb.com>
    Co-authored-by: Morgan McGuire <morganmcg1@users.noreply.github.com>
    Co-authored-by: Katia Patkin <87335417+kptkin@users.noreply.github.com>

--- wandb/integration/openai/__init__.py
+++ wandb/integration/openai/__init__.py
@@ -1,3 +1,16 @@
-__all__ = ("autolog",)
+__all__ = ("autolog", "WandbLogger")
+
+import wandb
+from wandb import util
+
+_openai = util.get_module("openai")
+
+from pkg_resources import parse_version
 
 from .openai import autolog
+
+if _openai:
+    if parse_version(_openai.__version__) > parse_version("0.28.1"):
+        from .fine_tune import WandbLogger
+else:
+    wandb.termerror("`openai` is not installed. To fix, please `pip install openai`")
