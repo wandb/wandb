0e95ba53a3962be927152c33698636e20db73610
Dmitry Duev <dima@wandb.com>
2023-12-01 17:09:45 -0800

    fix old pythons

--- tests/pytest_tests/unit_tests/test_wandb_settings_old.py
+++ tests/pytest_tests/unit_tests/test_wandb_settings_old.py
@@ -12,11 +12,10 @@ def test__global_path_default_exists_and_is_writable():
 
 
 def test__global_path_default_does_not_exist_and_is_not_writable():
-    with (
-        mock.patch("os.makedirs") as mock_makedirs,
-        mock.patch("os.access", return_value=True),
-        mock.patch("tempfile.gettempdir", return_value="/tmp"),
-        mock.patch("getpass.getuser", return_value="testuser"),
+    with mock.patch("os.makedirs") as mock_makedirs, mock.patch(
+        "os.access", return_value=True
+    ), mock.patch("tempfile.gettempdir", return_value="/tmp"), mock.patch(
+        "getpass.getuser", return_value="testuser"
     ):
         mock_makedirs.side_effect = [OSError, True]
         assert Settings._global_path() == "/tmp/.config/wandb/settings"
