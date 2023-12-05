d9e7183ab27e121df23d612262a90a0afcec30b3
Jeff Raubitschek <jeff@wandb.com>
2023-12-05 15:25:32 -0800

    update from other branch

--- nexus/pkg/artifacts/builder.go
+++ nexus/pkg/artifacts/builder.go
@@ -1,25 +1,46 @@
 package artifacts
 
 import (
-	"encoding/json"
-	"os"
+	"crypto/md5"
+	"fmt"
+	"sort"
 
 	"github.com/wandb/wandb/nexus/pkg/service"
 	"github.com/wandb/wandb/nexus/pkg/utils"
+
+	"google.golang.org/protobuf/proto"
 )
 
 type ArtifactBuilder struct {
-	artifactRecord *service.ArtifactRecord
+	artifactRecord   *service.ArtifactRecord
+	isDigestUpToDate bool
 }
 
 func NewArtifactBuilder(artifactRecord *service.ArtifactRecord) *ArtifactBuilder {
-	return &ArtifactBuilder{
-		artifactRecord: artifactRecord,
+	artifactClone := proto.Clone(artifactRecord).(*service.ArtifactRecord)
+	builder := &ArtifactBuilder{
+		artifactRecord: artifactClone,
+	}
+	builder.initDefaultManifest()
+	return builder
+}
+
+func (b *ArtifactBuilder) initDefaultManifest() {
+	if b.artifactRecord.Manifest != nil {
+		return
+	}
+	b.artifactRecord.Manifest = &service.ArtifactManifest{
+		Version:       1,
+		StoragePolicy: "wandb-storage-policy-v1",
+		StoragePolicyConfig: []*service.StoragePolicyConfigItem{{
+			Key:       "storageLayout",
+			ValueJson: "\"V2\"",
+		}},
 	}
 }
 
 func (b *ArtifactBuilder) AddData(name string, dataMap map[string]interface{}) error {
-	filename, digest, err := b.writeToFile(dataMap)
+	filename, digest, err := utils.WriteJsonToFileWithDigest(dataMap)
 	if err != nil {
 		return err
 	}
@@ -29,41 +50,51 @@ func (b *ArtifactBuilder) AddData(name string, dataMap map[string]interface{}) e
 			Digest:    digest,
 			LocalPath: filename,
 		})
+	b.isDigestUpToDate = false
 	return nil
 }
 
-func (b *ArtifactBuilder) updateManifestDigest() error {
+func (b *ArtifactBuilder) updateManifestDigest() {
+	if b.isDigestUpToDate {
+		return
+	}
 	manifest, err := NewManifestFromProto(b.artifactRecord.Manifest)
 	if err != nil {
-		return err
+		panic("unable to create manifest (unexpected)")
 	}
-	manifestDigest := manifest.ComputeDigest()
+	manifestDigest := computeManifestDigest(&manifest)
 	b.artifactRecord.Digest = manifestDigest
-	return nil
+	b.isDigestUpToDate = true
 }
 
 func (b *ArtifactBuilder) GetArtifact() *service.ArtifactRecord {
-	_ = b.updateManifestDigest()
+	b.updateManifestDigest()
 	return b.artifactRecord
 }
 
-func (b *ArtifactBuilder) writeToFile(dataMap map[string]interface{}) (filename string, digest string, rerr error) {
-	data, rerr := json.Marshal(dataMap)
-	if rerr != nil {
-		return
+func computeManifestDigest(manifest *Manifest) string {
+	type hashedEntry struct {
+		name   string
+		digest string
 	}
 
-	f, rerr := os.CreateTemp("", "tmpfile-")
-	if rerr != nil {
-		return
+	var entries []hashedEntry
+	for name, entry := range manifest.Contents {
+		entries = append(entries, hashedEntry{
+			name:   name,
+			digest: entry.Digest,
+		})
 	}
-	defer f.Close()
-	_, rerr = f.Write(data)
-	if rerr != nil {
-		return
-	}
-	filename = f.Name()
 
-	digest, rerr = utils.ComputeB64MD5(data)
-	return
+	sort.Slice(entries, func(i, j int) bool {
+		return entries[i].name < entries[j].name
+	})
+
+	hasher := md5.New()
+	hasher.Write([]byte("wandb-artifact-manifest-v1\n"))
+	for _, entry := range entries {
+		hasher.Write([]byte(fmt.Sprintf("%s:%s\n", entry.name, entry.digest)))
+	}
+
+	return utils.EncodeBytesAsHex(hasher.Sum(nil))
 }
