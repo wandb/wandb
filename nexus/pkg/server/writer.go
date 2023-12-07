36da80d238a84ead9bf8d01c81531f0de11754d3
Katia <katia@wandb.com>
2023-12-07 13:18:56 -0800

    feat(nexus): refactor store logic and add store writer

--- nexus/pkg/server/writer.go
+++ nexus/pkg/server/writer.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"os"
 	"sync"
 
 	"github.com/wandb/wandb/nexus/pkg/observability"
@@ -58,7 +59,8 @@ func (w *Writer) do(inChan <-chan *service.Record) {
 	w.storeChan = make(chan *service.Record, BufferSize*8)
 
 	var err error
-	w.store, err = NewStore(w.ctx, w.settings.GetSyncFile().GetValue(), w.logger)
+	w.store = NewStore(w.ctx, w.settings.GetSyncFile().GetValue(), w.logger)
+	err = w.store.Open(os.O_WRONLY)
 	if err != nil {
 		w.logger.CaptureFatalAndPanic("writer: error creating store", err)
 	}
@@ -67,7 +69,7 @@ func (w *Writer) do(inChan <-chan *service.Record) {
 	w.wg.Add(1)
 	go func() {
 		for record := range w.storeChan {
-			if err = w.store.storeRecord(record); err != nil {
+			if err = w.store.Write(record); err != nil {
 				w.logger.Error("writer: error storing record", "error", err)
 			}
 		}
@@ -77,6 +79,14 @@ func (w *Writer) do(inChan <-chan *service.Record) {
 		}
 		w.wg.Done()
 	}()
+}
+
+// do is the main loop of the writer to process incoming messages
+func (w *Writer) do(inChan <-chan *service.Record) {
+	defer w.logger.Reraise()
+	w.logger.Info("writer: started", "stream_id", w.settings.RunId)
+
+	w.startStore()
 
 	for record := range inChan {
 		w.handleRecord(record)
@@ -88,7 +98,9 @@ func (w *Writer) do(inChan <-chan *service.Record) {
 // which includes the store
 func (w *Writer) Close() {
 	close(w.fwdChan)
-	close(w.storeChan)
+	if w.storeChan != nil {
+		close(w.storeChan)
+	}
 	w.logger.Info("writer: closed", "stream_id", w.settings.RunId)
 }
 
