ba514ed20b616f3e1490fd4075b0f5f32ce4b38f
Szymon Piechowicz <127154459+szymon-piechowicz-wandb@users.noreply.github.com>
2023-12-12 13:39:11 -0800

    fix(artifacts): only skip file download if digest matches (#6694)

--- core/pkg/utils/files.go
+++ core/pkg/utils/files.go
@@ -1,10 +1,22 @@
 package utils
 
 import (
+	"errors"
 	"os"
 
 	"github.com/segmentio/encoding/json"
 )
+
+func FileExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
 
 func WriteJsonToFileWithDigest(marshallable interface{}) (filename string, digest string, rerr error) {
 	data, rerr := json.Marshal(marshallable)
