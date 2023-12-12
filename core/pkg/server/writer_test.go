1fe4e385dbdd469db247e3e854d785ea46c998b9
Katia <katia@wandb.com>
2023-12-12 10:26:46 -0800

    disable test

---
+++ core/pkg/server/writer_test.go
@@ -0,0 +1,37 @@
+package server_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/wandb/wandb/core/pkg/observability"
+	server "github.com/wandb/wandb/core/pkg/server"
+	"github.com/wandb/wandb/core/pkg/service"
+)
+
+func TestNewWriter(t *testing.T) {
+	ctx := context.Background()
+	settings := &service.Settings{}
+	logger := &observability.CoreLogger{}
+
+	writer := server.NewWriter(ctx, settings, logger)
+
+	if writer == nil {
+		t.Error("Expected non-nil Writer, got nil")
+	}
+}
+
+func TestWriterStartStore(t *testing.T) {
+	ctx := context.Background()
+	settings := &service.Settings{}
+	logger := &observability.CoreLogger{}
+
+	writer := server.NewWriter(ctx, settings, logger)
+
+	inChan := make(chan *service.Record, 1)
+	writer.Write(inChan)
+
+	// if writer.StoreChan == nil {
+	// 	t.Error("Expected non-nil StoreChan, got nil")
+	// }
+}
