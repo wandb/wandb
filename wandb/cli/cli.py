277d7cd3dd4bcc36aee4718c744145e2fb0276d5
TimH98 <39655500+TimH98@users.noreply.github.com>
2023-12-13 15:08:05 -0700

    chore(launch): Better check for empty cli template vars (#6715)

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -1493,7 +1493,7 @@ def launch(
             config["overrides"] = {"dockerfile": dockerfile}
 
     template_variables = None
-    if cli_template_vars is not None:
+    if cli_template_vars:
         if queue is None:
             raise LaunchError("'--set-var' flag requires queue to be set")
         public_api = PublicApi()
