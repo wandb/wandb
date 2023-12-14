120ffd0603bfd5a58c469b82cf08fc9579ecb5e6
Kyle Goyette <kyle@wandb.com>
2023-12-13 23:19:56 -0800

    add test file

---
+++ core/pkg/launch/job_builder_test.go
@@ -0,0 +1,149 @@
+package launch
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/wandb/wandb/core/pkg/service"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+func writeRequirements(t *testing.T, fdir string) {
+	f, err := os.OpenFile(filepath.Join(fdir, REQUIREMENTS_FNAME), os.O_CREATE|os.O_WRONLY, 0777)
+	assert.Nil(t, err)
+	_, err = f.WriteString("wandb")
+	assert.Nil(t, err)
+	err = f.Sync()
+	assert.Nil(t, err)
+	f.Close()
+}
+
+func writeWandbMetadata(t *testing.T, fdir string, metadata map[string]interface{}) {
+	f, err := os.OpenFile(filepath.Join(fdir, WANDB_METADATA_FNAME), os.O_CREATE|os.O_WRONLY, 0777)
+	assert.Nil(t, err)
+	metaDataString, err := json.Marshal(metadata)
+	assert.Nil(t, err)
+	_, err = f.Write(metaDataString)
+	assert.Nil(t, err)
+	err = f.Sync()
+	assert.Nil(t, err)
+	f.Close()
+}
+
+func writeDiffFile(t *testing.T, fdir string) {
+	f, err := os.OpenFile(filepath.Join(fdir, DIFF_FNAME), os.O_CREATE|os.O_WRONLY, 0777)
+	assert.Nil(t, err)
+	_, err = f.WriteString("wandb")
+	assert.Nil(t, err)
+	err = f.Sync()
+	assert.Nil(t, err)
+	f.Close()
+}
+
+func TestJobBuilder(t *testing.T) {
+	toWrapperPb := func(val interface{}) *wrapperspb.StringValue {
+		return &wrapperspb.StringValue{
+			Value: fmt.Sprintf("%v", val),
+		}
+	}
+	t.Run("buildRepoSourcedJob", func(t *testing.T) {
+		metadata := map[string]interface{}{
+			"python": "3.11.2",
+			"git": map[string]interface{}{
+				"commit": "1234567890",
+				"remote": "example.com",
+			},
+			"codePath": "/path/to/train.py",
+		}
+
+		fdir := filepath.Join(os.TempDir(), "test")
+		err := os.MkdirAll(fdir, 0777)
+		assert.Nil(t, err)
+		writeRequirements(t, fdir)
+		writeDiffFile(t, fdir)
+		writeWandbMetadata(t, fdir, metadata)
+
+		defer os.RemoveAll(fdir)
+		settings := &service.Settings{
+			Project:  toWrapperPb("testProject"),
+			Entity:   toWrapperPb("testEntity"),
+			RunId:    toWrapperPb("testRunId"),
+			FilesDir: toWrapperPb(fdir),
+		}
+		jobBuilder := NewJobBuilder(settings)
+		artifact, err := jobBuilder.Build()
+		assert.Nil(t, err)
+		assert.Equal(t, "job-example.com__path_to_train.py", artifact.Name)
+		assert.Equal(t, "testProject", artifact.Project)
+		assert.Equal(t, "testEntity", artifact.Entity)
+		assert.Equal(t, "testRunId", artifact.RunId)
+		assert.Equal(t, 3, len(artifact.Manifest.Contents))
+		assert.Equal(t, "5c90116a39060cbe9ec20345f6a34a58", artifact.Digest)
+		for _, content := range artifact.Manifest.Contents {
+			if content.Path == "wandb-job.json" {
+				jobFile, err := os.Open(content.LocalPath)
+				assert.Nil(t, err)
+				defer jobFile.Close()
+				assert.Nil(t, err)
+				data := make(map[string]interface{})
+				err = json.NewDecoder(jobFile).Decode(&data)
+				assert.Nil(t, err)
+				assert.Equal(t, "3.11.2", data["runtime"])
+				assert.Equal(t, "1234567890", data["source"].(map[string]interface{})["git"].(map[string]interface{})["commit"])
+				assert.Equal(t, "example.com", data["source"].(map[string]interface{})["git"].(map[string]interface{})["remote"])
+				assert.Equal(t, []interface{}([]interface{}{"python3.11", "/path/to/train.py"}), data["source"].(map[string]interface{})["entrypoint"])
+			}
+		}
+	})
+
+	// t.Run("buildArtifactSourcedJob", func(t *testing.T) {
+	// 	metadata := map[string]interface{}{
+	// 		"python":   "3.11.2",
+	// 		"codePath": "/path/to/train.py",
+	// 	}
+
+	// 	fdir := filepath.Join(os.TempDir(), "test")
+	// 	err := os.MkdirAll(fdir, 0777)
+	// 	assert.Nil(t, err)
+	// 	writeRequirements(t, fdir)
+	// 	writeDiffFile(t, fdir)
+	// 	writeWandbMetadata(t, fdir, metadata)
+
+	// 	defer os.RemoveAll(fdir)
+	// 	settings := &service.Settings{
+	// 		Project:  toWrapperPb("testProject"),
+	// 		Entity:   toWrapperPb("testEntity"),
+	// 		RunId:    toWrapperPb("testRunId"),
+	// 		FilesDir: toWrapperPb(fdir),
+	// 	}
+	// 	jobBuilder := NewJobBuilder(settings)
+	// 	artifact, err := jobBuilder.Build()
+	// 	assert.Nil(t, err)
+	// 	assert.Equal(t, "job-example.com__path_to_train.py", artifact.Name)
+	// 	assert.Equal(t, "testProject", artifact.Project)
+	// 	assert.Equal(t, "testEntity", artifact.Entity)
+	// 	assert.Equal(t, "testRunId", artifact.RunId)
+	// 	assert.Equal(t, 3, len(artifact.Manifest.Contents))
+	// 	assert.Equal(t, "5c90116a39060cbe9ec20345f6a34a58", artifact.Digest)
+	// 	for _, content := range artifact.Manifest.Contents {
+	// 		if content.Path == "wandb-job.json" {
+	// 			jobFile, err := os.Open(content.LocalPath)
+	// 			assert.Nil(t, err)
+	// 			defer jobFile.Close()
+	// 			assert.Nil(t, err)
+	// 			data := make(map[string]interface{})
+	// 			err = json.NewDecoder(jobFile).Decode(&data)
+	// 			assert.Nil(t, err)
+	// 			assert.Equal(t, "3.11.2", data["runtime"])
+	// 			assert.Equal(t, "1234567890", data["source"].(map[string]interface{})["git"].(map[string]interface{})["commit"])
+	// 			assert.Equal(t, "example.com", data["source"].(map[string]interface{})["git"].(map[string]interface{})["remote"])
+	// 			assert.Equal(t, []interface{}([]interface{}{"python3.11", "/path/to/train.py"}), data["source"].(map[string]interface{})["entrypoint"])
+	// 		}
+	// 	}
+	// })
+
+}
