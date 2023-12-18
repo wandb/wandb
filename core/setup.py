4b1192e39851294d8ff2763ec6294b1e6f9fbb5e
Dmitry Duev <dmitryduev@users.noreply.github.com>
2023-12-18 13:09:12 -0800

    chore(core): release wandb-core==0.17.0b5 (#6736)

--- core/setup.py
+++ core/setup.py
@@ -14,7 +14,7 @@ from wheel.bdist_wheel import bdist_wheel, get_platform
 
 # wandb-core versioning
 # ---------------------
-CORE_VERSION = "0.17.0b4"
+CORE_VERSION = "0.17.0b5"
 
 
 PACKAGE: str = "wandb_core"
