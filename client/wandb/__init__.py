1ef35deb859d0c8aa95c75295a4c5ede86bd43ec
Dmitry Duev <dima@wandb.com>
2023-12-20 13:35:52 -0800

    lint lol

--- client/wandb/__init__.py
+++ client/wandb/__init__.py
@@ -3,8 +3,8 @@ import pathlib
 
 os.environ["_WANDB_CORE_PATH"] = str(pathlib.Path(__file__).parent.absolute())
 
-from .wandb import *
+from .wandb import *  # noqa: F403
 
-__doc__ = wandb.__doc__
-if hasattr(wandb, "__all__"):
-    __all__ = wandb.__all__
+__doc__ = wandb.__doc__  # noqa: F405
+if hasattr(wandb, "__all__"):  # noqa: F405
+    __all__ = wandb.__all__  # noqa: F405
