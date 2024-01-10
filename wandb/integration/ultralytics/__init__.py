740ad7981a05db43498189ff7fca51271fa1c137
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2024-01-10 18:56:46 +0530

    update: docstrings

--- wandb/integration/ultralytics/__init__.py
+++ wandb/integration/ultralytics/__init__.py
@@ -1,7 +1,6 @@
 """Tools for integrating with [`ultralytics`](https://docs.ultralytics.com/).
 
-Ultralytics is a computer vision framework for training and deploying YOLOv8
-models.
+Ultralytics is a computer vision framework for training and deploying YOLOv8 models.
 """
 
 from wandb.integration.ultralytics.callback import add_wandb_callback
