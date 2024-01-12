dc87d0dd1cc566530fd8673b9d02c1eac8da9628
soumik12345 <19soumik.rakshit96@gmail.com>
2024-01-12 13:16:18 +0000

    update: docstrings

--- wandb/integration/monai/__init__.py
+++ wandb/integration/monai/__init__.py
@@ -1,4 +1,8 @@
+"""Tools for integrating `wandb` with [`MonAI`](https://github.com/Project-MONAI/MONAI).
+
+MONAI is a PyTorch-based, open-source framework for deep learning in healthcare imaging, part of PyTorch Ecosystem.
+"""
+__all__ = ["WandbModelCheckpoint", "WandbStatsHandler"]
+
 from .model_checkpoint import WandbModelCheckpoint
 from .stats_handler import WandbStatsHandler
-
-__all__ = ["WandbModelCheckpoint", "WandbStatsHandler"]
