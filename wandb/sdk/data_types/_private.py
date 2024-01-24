bae9544cd40c3d9ac55d532e59d596c1debabcc3
Dmitry Duev <dima@wandb.com>
2024-01-23 19:31:39 -0800

    fix(sdk): clean up temp folders after thyself

--- wandb/sdk/data_types/_private.py
+++ wandb/sdk/data_types/_private.py
@@ -1,37 +1,12 @@
-import atexit
 import tempfile
-import threading
-from typing import Optional
-
-media_tmp_dir_lock = threading.Lock()
 
 # Staging directory, so we can encode raw data into files, then hash them before
 # we put them into the Run directory to be uploaded.
-MEDIA_TMP: Optional[tempfile.TemporaryDirectory] = None
+MEDIA_TMP = tempfile.TemporaryDirectory("wandb-media")
+print("Created new media tmpdir", MEDIA_TMP.name)
 
 
-def _get_media_tmp_dir() -> tempfile.TemporaryDirectory:
-    global MEDIA_TMP
-
-    if MEDIA_TMP:
-        print("Using existing media tmpdir", MEDIA_TMP.name)
-        return MEDIA_TMP
-
-    with media_tmp_dir_lock:
-        # check again in case another thread created it
-        if MEDIA_TMP:
-            print("Using existing media tmpdir", MEDIA_TMP.name)
-            return MEDIA_TMP
-
-        MEDIA_TMP = tempfile.TemporaryDirectory("wandb-media")
-        print("Created new media tmpdir", MEDIA_TMP.name)
-
-        # clear the tmpdir on exit
-        def cleanup() -> None:
-            if MEDIA_TMP:
-                print("Cleaning up media tmpdir", MEDIA_TMP.name)
-                MEDIA_TMP.cleanup()
-
-        atexit.register(cleanup)
-
-        return MEDIA_TMP
+# clear the tmpdir on exit
+def cleanup() -> None:
+    print("Cleaning up media tmpdir", MEDIA_TMP.name)
+    MEDIA_TMP.cleanup()
