45f9519c10baa1a5a8a361eefedcb52c27a42ec5
Dmitry Duev <dima@wandb.com>
2024-01-24 14:56:51 -0800

    fix(sdk): do not create temp folders on import

--- wandb/sdk/data_types/_private.py
+++ wandb/sdk/data_types/_private.py
@@ -1,9 +1,28 @@
 import atexit
 import tempfile
+import threading
+from typing import Optional
 
 # Staging directory, so we can encode raw data into files, then hash them before
 # we put them into the Run directory to be uploaded.
-MEDIA_TMP = tempfile.TemporaryDirectory("wandb-media")
+MEDIA_TMP: Optional[tempfile.TemporaryDirectory] = None
+media_tmp_lock = threading.Lock()
+
+
+def _get_media_tmp_dir() -> tempfile.TemporaryDirectory:
+    global MEDIA_TMP
+
+    if MEDIA_TMP:
+        return MEDIA_TMP
+
+    with media_tmp_lock:
+        # Check again in case another thread created it while we were waiting
+        if MEDIA_TMP:
+            return MEDIA_TMP
+
+        MEDIA_TMP = tempfile.TemporaryDirectory("wandb-media")
+        atexit.register(MEDIA_TMP.cleanup)  # Register the cleanup only once
+        return MEDIA_TMP
 
 
 def _cleanup_media_tmp_dir() -> None:
