99437be4603498f7c701b466c8254a51b3bb9e10
Andrew Truong <itsandrewtruong@gmail.com>
2024-02-01 14:33:32 -0500

    test

--- tests/pytest_tests/unit_tests/test_importer_utils.py
+++ tests/pytest_tests/unit_tests/test_importer_utils.py
@@ -1,57 +1,48 @@
 import sys
 
-import pytest
+if sys.version_info >= (3, 8):
+    from wandb.apis.importers.internals.util import for_each, parallelize
 
-pytest.mark.skipif(sys.version_info < (3, 8), reason="Requires py38+")
+    def test_parallelize():
+        def safe_func(x):
+            return x + 1
 
-from wandb.apis.importers.internals.util import for_each, parallelize
+        result = set(parallelize(safe_func, [1, 2, 3]))
+        expected = set([2, 3, 4])
 
+        assert result == expected
 
-def test_parallelize():
-    def safe_func(x):
-        return x + 1
+        def unsafe_func(x):
+            if x > 2:
+                raise Exception("test")
+            return x
 
-    result = set(parallelize(safe_func, [1, 2, 3]))
-    expected = set([2, 3, 4])
+        result = set(parallelize(unsafe_func, [1, 2, 3]))
+        expected = set([1, 2, None])
 
-    assert result == expected
+        assert result == expected
 
-    def unsafe_func(x):
-        if x > 2:
-            raise Exception("test")
-        return x
+    def test_for_each():
+        def safe_func(x):
+            return x + 1
 
-    result = set(parallelize(unsafe_func, [1, 2, 3]))
-    expected = set([1, 2, None])
+        result = set(for_each(safe_func, [1, 2, 3]))
+        expected = set([2, 3, 4])
+        assert result == expected
 
-    assert result == expected
+        result = set(for_each(safe_func, [1, 2, 3], parallel=True))
+        expected = set([2, 3, 4])
+        assert result == expected
 
+        def unsafe_func(x):
+            if x > 2:
+                raise Exception("test")
+            return x
 
-def test_for_each():
-    def safe_func(x):
-        return x + 1
+        result = set(for_each(unsafe_func, [1, 2, 3]))
+        expected = set([1, 2, None])
+        assert result == expected
 
-    result = set(for_each(safe_func, [1, 2, 3]))
-    expected = set([2, 3, 4])
-    assert result == expected
-
-    result = set(for_each(safe_func, [1, 2, 3], parallel=True))
-    expected = set([2, 3, 4])
-    assert result == expected
-
-    def unsafe_func(x):
-        if x > 2:
-            raise Exception("test")
-        return x
-
-    result = set(for_each(unsafe_func, [1, 2, 3]))
-    expected = set([1, 2, None])
-    assert result == expected
-
-    result = set(for_each(unsafe_func, [1, 2, 3], parallel=True))
-    expected = set([1, 2, None])
-    assert result == expected
-
-
-def test_nothing():
-    pass
+        result = set(for_each(unsafe_func, [1, 2, 3], parallel=True))
+        expected = set([1, 2, None])
+        assert result == expected
