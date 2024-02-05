f8625f976349cdc6e41c97cd93f000da88e78df5
Ben Sherman <ben@wandb.com>
2024-02-05 13:43:03 -0800

    fix(launch): remove deadsnakes from accelerator build step (#6933)

--- wandb/sdk/launch/builder/build.py
+++ wandb/sdk/launch/builder/build.py
@@ -178,11 +178,6 @@ FROM {accelerator_base_image} as base
 # make non-interactive so build doesn't block on questions
 ENV DEBIAN_FRONTEND=noninteractive
 
-# TODO: once NVIDIA their linux repository keys for all docker images
-RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/$(cat /etc/os-release | grep ^ID= |  cut -d "=" -f2 )$(cat /etc/os-release | grep ^VERSION_ID= |  cut -d "=" -f2 | sed -e 's/[\".]//g' )/$(uname -i)/3bf863cc.pub
-RUN apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/machine-learning/repos/$(cat /etc/os-release | grep ^ID= |  cut -d "=" -f2 )$(cat /etc/os-release | grep ^VERSION_ID= |  cut -d "=" -f2 | sed -e 's/[\".]//g' )/$(uname -i)/7fa2af80.pub
-RUN apt-get update -qq && apt-get install -y software-properties-common && add-apt-repository -y ppa:deadsnakes/ppa
-
 # install python
 RUN apt-get update -qq && apt-get install --no-install-recommends -y \
     {python_packages} \
