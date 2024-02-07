3ab2ba6f304daa7ecc221d12f97991db3e871f6c
biaslucas <lucas.ouyang@wandb.com>
2024-02-07 10:38:56 -0800

    Remove lock from artifact download logger

--- wandb/sdk/artifacts/artifact_download_logger.py
+++ wandb/sdk/artifacts/artifact_download_logger.py
@@ -1,10 +1,13 @@
 """Artifact download logger."""
-import multiprocessing.dummy
+import threading
 import time
 from typing import Callable
 
+
 from wandb.errors.term import termlog
 
+
+MAX_SECS_BETWEEN_DOWNLOAD = 3600 * 10
 
 class ArtifactDownloadLogger:
     def __init__(
@@ -20,19 +23,17 @@ class ArtifactDownloadLogger:
         self._n_files_downloaded = 0
         self._spinner_index = 0
         self._last_log_time = self._clock()
-        self._lock = multiprocessing.dummy.Lock()
+        self._last_download_time = self._clock()
 
-    def notify_downloaded(self) -> None:
-        with self._lock:
-            self._n_files_downloaded += 1
-            if self._n_files_downloaded == self._nfiles:
-                self._termlog(
-                    f"  {self._nfiles} of {self._nfiles} files downloaded.  ",
-                    # ^ trailing spaces to wipe out ellipsis from previous logs
-                    newline=True,
-                )
-                self._last_log_time = self._clock()
-            elif self._clock() - self._last_log_time > 0.1:
+        self._logging_thread = threading.Thread(target=self._logging_loop)
+        self._logging_thread.daemon = True
+        self._logging_thread.start()
+
+    def _logging_loop(self):
+        self._termlog(f"Kicking off new thread for logging management")
+        last_download_progress = self._n_files_downloaded
+        while self._n_files_downloaded < self._nfiles:
+            if self._clock() - self._last_log_time > 0.1:
                 self._spinner_index += 1
                 spinner = r"-\|/"[self._spinner_index % 4]
                 self._termlog(
@@ -40,3 +41,16 @@ class ArtifactDownloadLogger:
                     newline=False,
                 )
                 self._last_log_time = self._clock()
+
+            if last_download_progress != self._n_files_downloaded:
+                self._last_download_time = self._clock()
+                last_download_progress = self._n_files_downloaded
+            elif self._clock() - self._last_download_time > MAX_SECS_BETWEEN_DOWNLOAD:
+                self._termlog(
+                    f"  Download is taking too long, aborting!  ",
+                    newline=True,
+                )
+                break
+
+    def notify_downloaded(self) -> None:
+        self._n_files_downloaded += 1
