4c854b32474f8784235f8be9e1e190603602f11b
Timofey Peshin <timoffex@gmail.com>
2024-02-21 10:19:01 -0800

    Clean up Go style in config.go

--- core/pkg/server/config.go
+++ core/pkg/server/config.go
@@ -37,8 +37,8 @@ type RunConfig struct {
 type ConfigFormat int
 
 const (
-	FORMAT_YAML ConfigFormat = iota
-	FORMAT_JSON
+	FormatYaml ConfigFormat = iota
+	FormatJson
 )
 
 func NewRunConfig() *RunConfig {
@@ -75,7 +75,7 @@ func (runConfig *RunConfig) ApplyChangeRecord(
 		); err != nil {
 			onError(
 				fmt.Errorf(
-					"Failed to unmarshall JSON for config key %v: %w",
+					"config: failed to unmarshall JSON for config key %v: %w",
 					path,
 					err,
 				),
@@ -156,13 +156,13 @@ func (runConfig *RunConfig) Serialize(format ConfigFormat) ([]byte, error) {
 	}
 
 	switch format {
-	case FORMAT_YAML:
+	case FormatYaml:
 		return yaml.Marshal(valueConfig)
-	case FORMAT_JSON:
+	case FormatJson:
 		return json.Marshal(valueConfig)
 	}
 
-	return nil, fmt.Errorf("Unknown format: %v", format)
+	return nil, fmt.Errorf("config: unknown format: %v", format)
 }
 
 // Uses the given subtree for keys that aren't already set.
@@ -280,7 +280,7 @@ func getOrMakeSubtree(
 		subtree, ok := node.(RunConfigDict)
 		if !ok {
 			return nil, fmt.Errorf(
-				"Config value at path %v is type %T, not a map",
+				"config: value at path %v is type %T, not a map",
 				path,
 				node,
 			)
