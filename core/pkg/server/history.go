292b67ba28129ccef952c4785f2383598cafb561
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-02-21 07:02:58 -0800

    Update core/pkg/server/history.go

--- core/pkg/server/history.go
+++ core/pkg/server/history.go
@@ -363,7 +363,7 @@ func (h *Handler) handlePartialHistorySync(request *service.PartialHistoryReques
 	if h.activeHistory == nil {
 
 		h.activeHistory = NewActiveHistory(
-			// Although technically infra allows negative steps, in practice it is all set up to work with non-negative steps
+			// Although technically the backend allows negative steps, in practice it is all set up to work with non-negative steps
 			// so if we receive a negative step, it will be discarded.
 			WithStep(h.runRecord.GetStartingStep()),
 			WithFlush(
