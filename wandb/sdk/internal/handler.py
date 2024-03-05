ecb355e48763c7a17a3d6e9a0a9eccb3a10d90f6
Tim Sweeney <tim@wandb.com>
2024-03-05 08:58:49 -0800

    init

--- wandb/sdk/internal/handler.py
+++ wandb/sdk/internal/handler.py
@@ -371,31 +371,31 @@ class HandleManager:
                     updated = True
             return updated
         # If the dict is a media object, update the pointer to the latest alias
-        elif isinstance(v, dict) and handler_util.metric_is_wandb_dict(v):
-            if "_latest_artifact_path" in v and "artifact_path" in v:
-                # TODO: Make non-destructive?
-                v["artifact_path"] = v["_latest_artifact_path"]
+        # elif isinstance(v, dict) and handler_util.metric_is_wandb_dict(v):
+        #     if "_latest_artifact_path" in v and "artifact_path" in v:
+        #         # TODO: Make non-destructive?
+        #         v["artifact_path"] = v["_latest_artifact_path"]
         updated = self._update_summary_leaf(kl=kl, v=v, d=d)
         return updated
 
-    def _update_summary_media_objects(self, v: Dict[str, Any]) -> Dict[str, Any]:
-        # For now, non-recursive - just top level
-        for nk, nv in v.items():
-            if (
-                isinstance(nv, dict)
-                and handler_util.metric_is_wandb_dict(nv)
-                and "_latest_artifact_path" in nv
-                and "artifact_path" in nv
-            ):
-                # TODO: Make non-destructive?
-                nv["artifact_path"] = nv["_latest_artifact_path"]
-                v[nk] = nv
-        return v
+    # def _update_summary_media_objects(self, v: Dict[str, Any]) -> Dict[str, Any]:
+    #     # For now, non-recursive - just top level
+    #     for nk, nv in v.items():
+    #         if (
+    #             isinstance(nv, dict)
+    #             and handler_util.metric_is_wandb_dict(nv)
+    #             and "_latest_artifact_path" in nv
+    #             and "artifact_path" in nv
+    #         ):
+    #             # TODO: Make non-destructive?
+    #             nv["artifact_path"] = nv["_latest_artifact_path"]
+    #             v[nk] = nv
+    #     return v
 
     def _update_summary(self, history_dict: Dict[str, Any]) -> List[str]:
         # keep old behavior fast path if no define metrics have been used
         if not self._metric_defines:
-            history_dict = self._update_summary_media_objects(history_dict)
+            # history_dict = self._update_summary_media_objects(history_dict)
             self._consolidated_summary.update(history_dict)
             return list(history_dict.keys())
         updated_keys = []
