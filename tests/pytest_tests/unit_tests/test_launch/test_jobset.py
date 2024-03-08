aef8eb2f036b487d3cc7be09a040aa391f3e7f23
Nick Penaranda <nick.penaranda@wandb.com>
2024-03-08 18:59:06 -0500

    lint

--- tests/pytest_tests/unit_tests/test_launch/test_jobset.py
+++ tests/pytest_tests/unit_tests/test_launch/test_jobset.py
@@ -6,95 +6,99 @@ from wandb.sdk.launch.agent2.jobset import JobSet, JobSetSpec, create_jobset
 
 
 def test_create_jobset(mocker):
-  api = MagicMock()
+    api = MagicMock()
 
-  get_jobset_response = {
-    "metadata": {
-      "@id": "test-id",
-      "@name": "test-queue",
-      "@target_resource": "test-resource",
-    },
-    "jobs": []
-  }
+    get_jobset_response = {
+        "metadata": {
+            "@id": "test-id",
+            "@name": "test-queue",
+            "@target_resource": "test-resource",
+        },
+        "jobs": [],
+    }
 
-  api.get_jobset_by_spec = MagicMock(return_value=get_jobset_response)
-  spec = JobSetSpec("test-queue", "test-entity", None)
-  js = create_jobset(spec, api, "test-agent-id", MagicMock())
-  assert isinstance(js, JobSet)
+    api.get_jobset_by_spec = MagicMock(return_value=get_jobset_response)
+    spec = JobSetSpec("test-queue", "test-entity", None)
+    js = create_jobset(spec, api, "test-agent-id", MagicMock())
+    assert isinstance(js, JobSet)
+
 
 def test_jobset_init():
-  api = MagicMock()
-  get_jobset_response = {
-    "metadata": {
-      "@id": "test-id",
-      "@name": "test-queue",
-      "@target_resource": "test-resource",
-    },
-    "jobs": []
-  }
-  js = JobSet(api, get_jobset_response, "test-agent-id", MagicMock())
-  assert js.id == "test-id"
-  assert js.name == "test-queue"
-  assert js._metadata == get_jobset_response["metadata"]
+    api = MagicMock()
+    get_jobset_response = {
+        "metadata": {
+            "@id": "test-id",
+            "@name": "test-queue",
+            "@target_resource": "test-resource",
+        },
+        "jobs": [],
+    }
+    js = JobSet(api, get_jobset_response, "test-agent-id", MagicMock())
+    assert js.id == "test-id"
+    assert js.name == "test-queue"
+    assert js._metadata == get_jobset_response["metadata"]
+
 
 @pytest.mark.asyncio
 async def test_jobset_loop_start_stop(event_loop, mocker):
-  mocker.patch("asyncio.sleep", AsyncMock())
+    mocker.patch("asyncio.sleep", AsyncMock())
 
-  api = MagicMock()
-  get_jobset_response = {
-    "metadata": {
-      "@id": "test-id",
-      "@name": "test-queue",
-      "@target_resource": "test-resource",
-    },
-    "jobs": []
-  }
-  js = JobSet(api, get_jobset_response, "test-agent-id", MagicMock())
-  js.start_sync_loop(event_loop)
+    api = MagicMock()
+    get_jobset_response = {
+        "metadata": {
+            "@id": "test-id",
+            "@name": "test-queue",
+            "@target_resource": "test-resource",
+        },
+        "jobs": [],
+    }
+    js = JobSet(api, get_jobset_response, "test-agent-id", MagicMock())
+    js.start_sync_loop(event_loop)
 
-  with pytest.raises(RuntimeError):
-    await js.start_sync_loop(event_loop)
+    with pytest.raises(RuntimeError):
+        await js.start_sync_loop(event_loop)
 
-  js.stop_sync_loop()
+    js.stop_sync_loop()
 
-  await js.wait_for_done
+    await js.wait_for_done
 
 
 @pytest.mark.asyncio
 async def test_jobset_ops(event_loop, mocker):
-  mocker.patch("asyncio.sleep", AsyncMock())
+    mocker.patch("asyncio.sleep", AsyncMock())
 
-  api = MagicMock()
-  api.lease_jobset_item = AsyncMock(return_value=True)
-  api.ack_jobset_item = AsyncMock(return_value=True)
-  api.fail_run_queue_item = AsyncMock(return_value=True)
-  api.get_jobset_diff_by_id = AsyncMock(return_value={
-    "version": 1,
-    "complete": True,
-    "metadata": {},
-    "upsert_jobs": [],
-    "remove_jobs": []
-  })
+    api = MagicMock()
+    api.lease_jobset_item = AsyncMock(return_value=True)
+    api.ack_jobset_item = AsyncMock(return_value=True)
+    api.fail_run_queue_item = AsyncMock(return_value=True)
+    api.get_jobset_diff_by_id = AsyncMock(
+        return_value={
+            "version": 1,
+            "complete": True,
+            "metadata": {},
+            "upsert_jobs": [],
+            "remove_jobs": [],
+        }
+    )
 
-  get_jobset_response = {
-    "metadata": {
-      "@id": "test-id",
-      "@name": "test-queue",
-      "@target_resource": "test-resource",
-    },
-    "jobs": []
-  }
-  js = JobSet(api, get_jobset_response, "test-agent-id", MagicMock())
-  js.start_sync_loop(event_loop)
+    get_jobset_response = {
+        "metadata": {
+            "@id": "test-id",
+            "@name": "test-queue",
+            "@target_resource": "test-resource",
+        },
+        "jobs": [],
+    }
+    js = JobSet(api, get_jobset_response, "test-agent-id", MagicMock())
+    js.start_sync_loop(event_loop)
 
-  tasks = asyncio.gather(
-    js.lease_job("test-job-1"),
-    js.ack_job("test-job-1", "test-run-1"),
-    js.fail_job("test-job-1", "test message", "test stage"),
-  )
+    tasks = asyncio.gather(
+        js.lease_job("test-job-1"),
+        js.ack_job("test-job-1", "test-run-1"),
+        js.fail_job("test-job-1", "test message", "test stage"),
+    )
 
-  await tasks
+    await tasks
 
-  js.stop_sync_loop()
-  await js.wait_for_done
+    js.stop_sync_loop()
+    await js.wait_for_done
