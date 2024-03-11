2b543f5212f84c51ddd3c5f3945b701949c9cad3
Katia Patkin <katia@wandb.com>
2024-03-11 14:15:21 -0700

    simplify printer

--- core/pkg/observability/printer.go
+++ core/pkg/observability/printer.go
@@ -1,87 +1,28 @@
 package observability
 
 import (
-	"strings"
-	"sync/atomic"
+	"sync"
 )
 
-type Messages []string
-
-// ForwardingService is a service for printing messages to the console, it serves
-// as an aggregator for messages other services want to print. It doesn't have
-// direct access to the console, it passes the messages to the client that
-// have access to the console.
-type ForwardingService struct {
-	// requestChan is the channel for adding print requests (messages)
-	requestChan chan Messages
-
-	// pollChan is the channel for polling messages to be printed
-	pollChan chan chan Messages
-
-	// messages is the list of messages to be printed
-	messages Messages
-
-	// started is a flag to indicate if the service has been started
-	started *atomic.Bool
+type Printer struct {
+	messages []string
+	mutex    sync.Mutex
 }
 
-// NewForwardingService returns a new PrinterService
-func NewForwardingService() *ForwardingService {
-	return &ForwardingService{
-		requestChan: make(chan Messages, 100),
-		pollChan:    make(chan chan Messages),
-		started:     &atomic.Bool{},
-	}
+func NewPrinter() *Printer {
+	return &Printer{}
 }
 
-// Write writes a message to the printer service
-func (p *ForwardingService) Write(message []byte) (int, error) {
-	if p == nil {
-		return 0, nil
-	}
-	p.requestChan <- Messages{strings.TrimSuffix(string(message), "\n")}
-	return len(message), nil
+func (s *Printer) Write(message string) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	s.messages = append(s.messages, message)
 }
 
-// Poll polls the messages channel
-func (p *ForwardingService) Poll() Messages {
-	if p == nil {
-		return nil
-	}
-	poll := make(chan Messages)
-	p.pollChan <- poll
-	return <-poll
-}
-
-// Start starts the printer service
-func (p *ForwardingService) Start() {
-	if p == nil {
-		return
-	}
-
-	if p.started.Swap(true) {
-		return
-	}
-
-	go func() {
-		for {
-			select {
-			case msg := <-p.requestChan:
-				p.messages = append(p.messages, msg...)
-			case poll := <-p.pollChan:
-				poll <- p.messages
-				p.messages = Messages{}
-			}
-		}
-	}()
-}
-
-// Clear the messages and the go routine
-func (p *ForwardingService) Close() {
-	if p == nil {
-		return
-	}
-	close(p.requestChan)
-	close(p.pollChan)
-	p.messages = nil
+func (s *Printer) Read() []string {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	polledMessages := s.messages
+	s.messages = make([]string, 0)
+	return polledMessages
 }
