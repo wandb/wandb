4d5f1dd070fa9f3970300444b9fab2f6f9a86ed0
Ben Sherman <bcanfieldsherman@gmail.com>
2024-03-11 20:19:09 -0700

    some proto files needed to be regenerated

--- wandb/proto/v3/wandb_internal_pb2.pyi
+++ wandb/proto/v3/wandb_internal_pb2.pyi
@@ -468,12 +468,12 @@ class LaunchWandbConfigParametersRecord(google.protobuf.message.Message):
     """Specifies include and exclude paths for filtering job inputs.
 
     If this record is published to the core internal process then it will filter
-    the given paths into or out of the job inputs it builds. 
+    the given paths into or out of the job inputs it builds.
 
-    If include_paths is not empty, then endpoints of the config not prefixed by 
+    If include_paths is not empty, then endpoints of the config not prefixed by
     an include path will be ignored.
 
-    If exclude_paths is not empty, then endpoints of the config prefixed by an 
+    If exclude_paths is not empty, then endpoints of the config prefixed by an
     exclude path will ignored.
     """
 
