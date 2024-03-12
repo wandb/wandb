22a6277617bb115e0fc920b76fd38b7f06e3487d
Timofey Peshin <timoffex@gmail.com>
2024-03-11 22:24:38 -0700

    Enable tests

--- tests/pytest_tests/unit_tests/test_wandb_save.py
+++ tests/pytest_tests/unit_tests/test_wandb_save.py
@@ -1,79 +1,105 @@
-import os
-import tempfile
-
-import pytest
-from wandb.sdk.lib import filesystem
+import pathlib
 
 # ----------------------------------
 # wandb.save
 # ----------------------------------
 
 
-@pytest.mark.xfail(reason="This test is flaky")
-def test_save_policy_symlink(mock_run, parse_records, record_q):
-    run = mock_run()
+def test_save_relative_file(
+    monkeypatch,
+    tmp_path,
+    mock_run,
+    parse_records,
+    record_q,
+):
+    # Use a fake working directory for the test.
+    monkeypatch.chdir(tmp_path)
+    pathlib.Path("test.rad").touch()
 
-    with open("test.rad", "w") as f:
-        f.write("something")
-    run.save("test.rad")
-    assert os.path.exists(os.path.join(run.dir, "test.rad"))
+    run = mock_run()
+    run.save("test.rad", policy="now")
+
+    assert (pathlib.Path(run.dir) / "test.rad").exists()
     parsed = parse_records(record_q)
     file_record = parsed.files[0].files[0]
     assert file_record.path == "test.rad"
-    assert file_record.policy == 2
 
 
-@pytest.mark.xfail(reason="This test is flaky")
-def test_save_policy_glob_symlink(mock_run, parse_records, record_q, capsys):
+def test_save_relative_glob(
+    monkeypatch,
+    tmp_path,
+    mock_run,
+    parse_records,
+    record_q,
+    capsys,
+):
+    # Use a fake working directory for the test.
+    monkeypatch.chdir(tmp_path)
+    pathlib.Path("test.rad").touch()
+    pathlib.Path("foo.rad").touch()
+
     run = mock_run()
+    run.save("*.rad", policy="now")
 
-    with open("test.rad", "w") as f:
-        f.write("something")
-    with open("foo.rad", "w") as f:
-        f.write("something")
-    run.save("*.rad")
     _, err = capsys.readouterr()
     assert "Symlinked 2 files" in err
-    assert os.path.exists(os.path.join(run.dir, "test.rad"))
-    assert os.path.exists(os.path.join(run.dir, "foo.rad"))
-
-    # test_save_policy_glob_symlink
+    assert (pathlib.Path(run.dir) / "test.rad").exists()
+    assert (pathlib.Path(run.dir) / "foo.rad").exists()
     parsed = parse_records(record_q)
     file_record = parsed.files[0].files[0]
     assert file_record.path == "*.rad"
-    assert file_record.policy == 2
 
 
-@pytest.mark.xfail(reason="This test is flaky")
-def test_save_absolute_path(mock_run, parse_records, record_q, capsys):
+def test_save_absolute_glob(
+    tmp_path: pathlib.Path,
+    mock_run,
+    parse_records,
+    record_q,
+):
+    test_path = tmp_path / "subdir" / "test.txt"
+    test_path.parent.mkdir()
+    test_path.touch()
+
     run = mock_run()
-    root = tempfile.gettempdir()
-    test_path = os.path.join(root, "test.txt")
-    with open(test_path, "w") as f:
-        f.write("something")
+    run.save(str((tmp_path / "*" / "test.txt").absolute()), policy="now")
 
-    run.save(test_path)
-    _, err = capsys.readouterr()
-    assert "Saving files without folders" in err
-    assert os.path.exists(os.path.join(run.dir, "test.txt"))
     parsed = parse_records(record_q)
     file_record = parsed.files[0].files[0]
     assert file_record.path == "test.txt"
-    assert file_record.policy == 2
 
 
-@pytest.mark.xfail(reason="This test is flaky")
-def test_save_relative_path(mock_run, parse_records, record_q):
+def test_save_absolute_path(
+    tmp_path: pathlib.Path,
+    mock_run,
+    parse_records,
+    record_q,
+):
+    test_path = (tmp_path / "test.txt").absolute()
+    test_path.touch()
+
     run = mock_run()
-    root = tempfile.gettempdir()
-    test_path = os.path.join(root, "tmp", "test.txt")
-    print("DAMN", os.path.dirname(test_path))
-    filesystem.mkdir_exists_ok(os.path.dirname(test_path))
-    with open(test_path, "w") as f:
-        f.write("something")
-    run.save(test_path, base_path=root, policy="now")
-    assert os.path.exists(os.path.join(run.dir, test_path))
+    run.save(str(test_path), policy="now")
+
+    assert (pathlib.Path(run.dir) / "test.txt").exists()
     parsed = parse_records(record_q)
     file_record = parsed.files[0].files[0]
-    assert file_record.path == os.path.relpath(test_path, root)
-    assert file_record.policy == 0
+    assert file_record.path == "test.txt"
+
+
+def test_save_relative_path(
+    tmp_path: pathlib.Path,
+    mock_run,
+    parse_records,
+    record_q,
+):
+    test_path = tmp_path / "subdir" / "test.txt"
+    test_path.parent.mkdir(exist_ok=True)
+    test_path.touch()
+
+    run = mock_run()
+    run.save(str(test_path), base_path=str(tmp_path), policy="now")
+
+    assert (pathlib.Path(run.dir) / "subdir" / "test.txt").exists()
+    parsed = parse_records(record_q)
+    file_record = parsed.files[0].files[0]
+    assert file_record.path == "subdir/test.txt"
