ea9cb6262735d5c935ad5b523bf9c4add6191951
Ben Sherman <bcanfieldsherman@gmail.com>
2024-03-12 16:52:39 -0700

    docstrings

--- core/pkg/launch/wandb_config.go
+++ core/pkg/launch/wandb_config.go
@@ -5,6 +5,11 @@ import (
 	"github.com/wandb/wandb/core/pkg/service"
 )
 
+// Create a typed representation of the wandb config inputs.
+//
+// Loads config filters received by the internal process, uses them to filter
+// down a copy of the run config, and then produces a TypedRepresentation for
+// the filtered config.
 func (j *JobBuilder) getWandbConfigInputs() data_types.TypeRepresentation {
 	include, exclude := j.getWandbConfigFilters()
 	config := NewConfigFrom(j.runConfig.CloneTree())
@@ -32,6 +37,10 @@ func (j *JobBuilder) getWandbConfigFilters() ([]ConfigPath, []ConfigPath) {
 	return include, exclude
 }
 
+// Saves the received LaunchWandbConfigParametersRecords for later use.
+//
+// Also sets a flag to save the shape of the run config to the metadata rather
+// than `wandb-job.json`.
 func (j *JobBuilder) HandleLaunchWandbConfigParametersRecord(wandbConfigParameters *service.LaunchWandbConfigParametersRecord) {
 	j.saveShapeToMetadata = true
 	j.wandbConfigParameters = append(j.wandbConfigParameters, wandbConfigParameters)
