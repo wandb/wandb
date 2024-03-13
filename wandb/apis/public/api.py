22561e91317d4159811203497ec7078f23bab8e6
Tim Hays <tim.hays@wandb.com>
2024-03-13 15:41:55 -0700

    wip

--- wandb/apis/public/api.py
+++ wandb/apis/public/api.py
@@ -313,12 +313,12 @@ class Api:
             entity: (str) Optional name of the entity to create the queue. If None, will use the configured or default entity.
             prioritization_mode: (str) Optional version of prioritization to use. Either "V0" or None
             config: (dict) Optional default resource configuration to be used for the queue. Use handlebars (eg. "{{var}}") to specify template variables.
-            template_variables (dict): TIMH TEST A dictionary of template variable schemas to be used with the config. Expected format of:
+            template_variables (dict): TIMH TEST2 A dictionary of template variable schemas to be used with the config. Expected format of:
                 ```
                 {
                     "var-name": {
                         "schema": {
-                            "type": "<string | number | integer>",
+                            "type": "<string \\| number \\| integer>",
                             "default": <optional value>,
                             "minimum": <optional minimum>,
                             "maximum": <optional maximum>,
