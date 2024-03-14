2efcf618728955b7d5fbc2311142c68dcffce7e0
Ishita Bindlish <ishita.bindlish@wandb.com>
2024-03-14 18:31:46 -0500

    ruff

--- wandb/sdk/artifacts/artifact_saver.py
+++ wandb/sdk/artifacts/artifact_saver.py
@@ -10,7 +10,6 @@ import wandb
 import wandb.filesync.step_prepare
 from wandb import util
 from wandb.sdk.artifacts.artifact_manifest import ArtifactManifest
-from wandb.sdk.artifacts.staging import get_staging_dir
 from wandb.sdk.lib.hashutil import B64MD5, b64_to_hex_id, md5_file_b64
 from wandb.sdk.lib.paths import URIStr
 
