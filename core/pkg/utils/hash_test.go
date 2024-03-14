cd9eb44fefb806ffd42e95d02130b5feecb81eda
Hugh Wimberly <hugh.wimberly@wandb.com>
2024-03-14 07:48:23 -0700

    Add test for HexToB64

--- core/pkg/utils/hash_test.go
+++ core/pkg/utils/hash_test.go
@@ -10,3 +10,11 @@ func TestEncode(t *testing.T) {
 	encoded := EncodeBytesAsHex([]byte(`junk`))
 	assert.Equal(t, "6a756e6b", encoded)
 }
+
+func TestHexToB64(t *testing.T) {
+	data := "6a756e6b"
+	expected := "anVuaw=="
+	result, err := HexToB64(data)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, result)
+}
