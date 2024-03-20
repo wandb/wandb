250d9175eb98b162b647d563a838e47fae8290ed
Danny Goldstein <danny@wandb.com>
2024-03-20 10:43:15 -0700

    add some tests

---
+++ tests/pytest_tests/unit_tests/test_run_moment.py
@@ -0,0 +1,34 @@
+import pytest
+from wandb.sdk.lib.run_moment import RunMoment
+
+
+def test_run_moment_from_uri_valid():
+    uri = "ans3bsax?_step=123"
+    run_moment = RunMoment.from_uri(uri)
+    assert run_moment.run == "ans3bsax"
+    assert run_moment.metric == "_step"
+    assert run_moment.value == 123
+
+
+def test_run_moment_from_uri_invalid_format():
+    uri = "ans3bsax?metric=123"
+    with pytest.raises(ValueError):
+        RunMoment.from_uri(uri)
+
+
+def test_run_moment_from_uri_invalid_metric():
+    uri = "ans3bsax?_metric=123"
+    with pytest.raises(ValueError):
+        RunMoment.from_uri(uri)
+
+
+def test_run_moment_from_uri_invalid_value():
+    uri = "ans3bsax?_step=abc"
+    with pytest.raises(ValueError):
+        RunMoment.from_uri(uri)
+
+
+def test_run_moment_from_uri_invalid_path():
+    uri = "ans3bsax/metric?_step=123"
+    with pytest.raises(ValueError):
+        RunMoment.from_uri(uri)
