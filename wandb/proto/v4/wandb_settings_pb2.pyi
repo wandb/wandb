2e15b33a24b5d65c5b99a7b01c0bd2cf3b39dbf6
Timofey Peshin <timoffex@gmail.com>
2024-03-19 18:33:02 -0700

    chore(sdk): Document Settings._offline (#7184)

--- wandb/proto/v4/wandb_settings_pb2.pyi
+++ wandb/proto/v4/wandb_settings_pb2.pyi
@@ -131,11 +131,14 @@ class Settings(google.protobuf.message.Message):
     2. Run settings
 
     Some fields such as `run_id` only make sense at the run level.
+
+    Next ID: 164
     """
 
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
 
     API_KEY_FIELD_NUMBER: builtins.int
+    _OFFLINE_FIELD_NUMBER: builtins.int
     RUN_ID_FIELD_NUMBER: builtins.int
     RUN_URL_FIELD_NUMBER: builtins.int
     PROJECT_FIELD_NUMBER: builtins.int
@@ -172,7 +175,6 @@ class Settings(google.protobuf.message.Message):
     _NETWORK_BUFFER_FIELD_NUMBER: builtins.int
     _NOOP_FIELD_NUMBER: builtins.int
     _NOTEBOOK_FIELD_NUMBER: builtins.int
-    _OFFLINE_FIELD_NUMBER: builtins.int
     _SYNC_FIELD_NUMBER: builtins.int
     _OS_FIELD_NUMBER: builtins.int
     _PLATFORM_FIELD_NUMBER: builtins.int
@@ -305,6 +307,9 @@ class Settings(google.protobuf.message.Message):
         This can be empty if we're in offline mode.
         """
     @property
+    def _offline(self) -> google.protobuf.wrappers_pb2.BoolValue:
+        """Whether we are in offline mode."""
+    @property
     def run_id(self) -> google.protobuf.wrappers_pb2.StringValue:
         """The ID of the run."""
     @property
@@ -383,8 +388,6 @@ class Settings(google.protobuf.message.Message):
     def _noop(self) -> google.protobuf.wrappers_pb2.BoolValue: ...
     @property
     def _notebook(self) -> google.protobuf.wrappers_pb2.BoolValue: ...
-    @property
-    def _offline(self) -> google.protobuf.wrappers_pb2.BoolValue: ...
     @property
     def _sync(self) -> google.protobuf.wrappers_pb2.BoolValue: ...
     @property
@@ -639,6 +642,7 @@ class Settings(google.protobuf.message.Message):
         self,
         *,
         api_key: google.protobuf.wrappers_pb2.StringValue | None = ...,
+        _offline: google.protobuf.wrappers_pb2.BoolValue | None = ...,
         run_id: google.protobuf.wrappers_pb2.StringValue | None = ...,
         run_url: google.protobuf.wrappers_pb2.StringValue | None = ...,
         project: google.protobuf.wrappers_pb2.StringValue | None = ...,
@@ -675,7 +679,6 @@ class Settings(google.protobuf.message.Message):
         _network_buffer: google.protobuf.wrappers_pb2.Int32Value | None = ...,
         _noop: google.protobuf.wrappers_pb2.BoolValue | None = ...,
         _notebook: google.protobuf.wrappers_pb2.BoolValue | None = ...,
-        _offline: google.protobuf.wrappers_pb2.BoolValue | None = ...,
         _sync: google.protobuf.wrappers_pb2.BoolValue | None = ...,
         _os: google.protobuf.wrappers_pb2.StringValue | None = ...,
         _platform: google.protobuf.wrappers_pb2.StringValue | None = ...,
