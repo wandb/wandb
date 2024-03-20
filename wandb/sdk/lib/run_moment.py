5a70189fd8381d73f79dacbe75964e478c8ce40a
Danny Goldstein <danny@wandb.com>
2024-03-20 10:40:43 -0700

    implement moment parser

--- wandb/sdk/lib/run_moment.py
+++ wandb/sdk/lib/run_moment.py
@@ -1,12 +1,12 @@
 import typing
 import pydantic
 
+from urllib import parse
+
 
 class RunMoment(pydantic.BaseModel):
     """A moment in time in a run."""
 
-    entity: str  # run entity
-    project: str  # run project
     run: str  # run name
 
     # only step for now, in future this will be relaxed to be any metric
@@ -14,3 +14,37 @@ class RunMoment(pydantic.BaseModel):
 
     # currently, the _step value to fork from. in future, this will be optional
     value: typing.Union[int, float]
+
+    @classmethod
+    def from_uri(cls, uri: str) -> "RunMoment":
+        """Create a RunMoment from a URI."""
+
+        parsable = "runmoment://" + uri
+        parse_err_msg = lambda uri: (
+            f"Could not parse passed run moment string '{uri}', "
+            f"expected format '[<entity>/<project>/]<run>?<metric>=<value>'. "
+            f"Currently, only the metric '_step' is supported. "
+            f"Example: 'ans3bsax?_step=123'."
+        )
+
+        try:
+            parsed = parse.urlparse(parsable)
+        except ValueError as e:
+            raise ValueError(parse_err_msg(uri)) from e
+
+        # extract entity, project, run, metric, value from parsed
+        spl_path = parsed.path.split("/")
+        if len(spl_path) != 1:
+            raise ValueError(parse_err_msg(uri))
+        else:
+            run = spl_path[0]
+
+        query = parse.parse_qs(parsed.query)
+        if len(query) != 1:
+            raise ValueError(parse_err_msg(uri))
+        else:
+            metric = list(query.keys())[0]
+            if metric != "_step":
+                raise ValueError(parse_err_msg(uri))
+            value = query[metric][0]
+        return cls(run=run, metric=metric, value=value)
