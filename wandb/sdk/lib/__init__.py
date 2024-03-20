ee3effc194d70e0b9eeac2756d2b3e9217ce48d2
Danny Goldstein <danny@wandb.com>
2024-03-19 20:59:06 -0700

    add StartSpec

--- wandb/sdk/lib/__init__.py
+++ wandb/sdk/lib/__init__.py
@@ -1,8 +1,5 @@
 from . import lazyloader
 from .disabled import RunDisabled, SummaryDisabled
+from .start_spec import StartSpec
 
-__all__ = (
-    "lazyloader",
-    "RunDisabled",
-    "SummaryDisabled",
-)
+__all__ = ("lazyloader", "RunDisabled", "SummaryDisabled", "StartSpec")
