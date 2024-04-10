81d7bb1ae0dd327ad9128e7d20edca92765c06f5
Tim Hays <tim.hays@wandb.com>
2024-04-10 13:44:00 -0700

    Fix label_job, wip find_orphaned_jobs

--- wandb/sdk/launch/agent2/controllers/vertex.py
+++ wandb/sdk/launch/agent2/controllers/vertex.py
@@ -2,12 +2,23 @@ import asyncio
 import logging
 from typing import Any, Dict, List
 
+from wandb.sdk.lib.hashutil import b64_to_hex_id, md5_string
+
+if False:
+    from google.cloud import aiplatform  # type: ignore   # noqa: F401
+
+from wandb.sdk.launch.utils import event_loop_thread_exec
+from wandb.util import get_module
+
 from ..._project_spec import LaunchProject
 from ...queue_driver.standard_queue_driver import StandardQueueDriver
 from ..controller import LaunchControllerConfig, LegacyResources
 from ..jobset import JobSet
-from .base import WANDB_JOBSET_DISCOVERABILITY_LABEL, BaseManager
+from .base import BaseManager
 from .util import parse_max_concurrency
+
+# Required due to Vertex's limitations on label keys
+WANDB_VERTEX_JOBSET_DISCOVERABILITY_LABEL = "wandb-jobset"
 
 
 async def vertex_controller(
@@ -54,13 +65,29 @@ class VertexManager(BaseManager):
         # relationships of agents and jobs to queues in a cluster)
 
     async def find_orphaned_jobs(self) -> List[Dict[str, Any]]:
+        aiplatform = get_module(  # noqa: F811
+            "google.cloud.aiplatform",
+            "VertexRunner requires google.cloud.aiplatform to be installed",
+        )
+        jobset_label = self._construct_jobset_discoverability_label()
+        self.logger.debug(f"Jobset label: {jobset_label}")
+        list = event_loop_thread_exec(aiplatform.CustomJob.list)
+        jobs = await list(filter=f'labels.{WANDB_VERTEX_JOBSET_DISCOVERABILITY_LABEL}={jobset_label}')
+        self.logger.debug(f"Found orphaned jobs: {jobs}")
+
+        # TODO convert returned CustomJobs to dicts or Jobs
         raise NotImplementedError
 
-    async def label_job(self, project: LaunchProject) -> None:
-        vertex_block = await self._get_resource_block(project)
+    def _construct_jobset_discoverability_label(self) -> str:
+        return b64_to_hex_id(md5_string(f"{self.config['jobset_spec'].entity_name}/{self.config['jobset_spec'].name}"))
+
+    def label_job(self, project: LaunchProject) -> None:
+        vertex_block = self._get_resource_block(project)
         if vertex_block is None:
             return
-        jobset_label = await self._construct_jobset_discoverability_label()
-        labels = vertex_block.get("labels", {})
-        labels[WANDB_JOBSET_DISCOVERABILITY_LABEL] = jobset_label
-        vertex_block["labels"] = labels
+        jobset_label = self._construct_jobset_discoverability_label()
+        spec = vertex_block.get("spec", {})
+        labels = spec.get("labels", {})
+        labels[WANDB_VERTEX_JOBSET_DISCOVERABILITY_LABEL] = jobset_label
+        spec["labels"] = labels
+        vertex_block["spec"] = spec
