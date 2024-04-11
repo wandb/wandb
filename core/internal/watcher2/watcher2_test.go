87412b826ae918ef6173744e804ef41b79ee0dd6
Timofey Peshin <timoffex@gmail.com>
2024-04-11 12:02:54 -0700

    make test robust to mtime problems
    Created using spr 1.3.5

--- core/internal/watcher2/watcher2_test.go
+++ core/internal/watcher2/watcher2_test.go
@@ -1,19 +1,17 @@
 package watcher2_test
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"syscall"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/wandb/wandb/core/internal/watcher2"
 )
 
-func writeFile(t *testing.T, path string, content string) time.Time {
+func writeFileAndGetModTime(t *testing.T, path string, content string) time.Time {
 	require.NoError(t,
 		os.MkdirAll(
 			filepath.Dir(path),
@@ -27,6 +25,10 @@ func writeFile(t *testing.T, path string, content string) time.Time {
 	require.NoError(t, err)
 
 	return info.ModTime()
+}
+
+func writeFile(t *testing.T, path string, content string) {
+	_ = writeFileAndGetModTime(t, path, content)
 }
 
 func TestWatcher(t *testing.T) {
@@ -70,30 +72,37 @@ func TestWatcher(t *testing.T) {
 		waitWithDeadline(t, finished, "expected Finish() to complete")
 	}
 
-	// TODO: DO NOT MERGE!!! Just testing CI flakiness.
-	for i := range 10 {
-		t.Run(fmt.Sprintf("runs callback on file write (%d)", i), func(t *testing.T) {
-			t.Parallel()
+	t.Run("runs callback on file write", func(t *testing.T) {
+		t.Parallel()
 
-			onChangeChan := make(chan struct{})
-			file := filepath.Join(t.TempDir(), "file.txt")
-			t1 := writeFile(t, file, "")
+		onChangeChan := make(chan struct{})
+		file := filepath.Join(t.TempDir(), "file.txt")
+		t1 := writeFileAndGetModTime(t, file, "")
 
-			watcher := newTestWatcher()
-			defer finishWithDeadline(t, watcher)
-			require.NoError(t,
-				watcher.Watch(file, func() {
-					onChangeChan <- struct{}{}
-				}))
-			t2 := writeFile(t, file, "xyz")
+		watcher := newTestWatcher()
+		defer finishWithDeadline(t, watcher)
+		require.NoError(t,
+			watcher.Watch(file, func() {
+				onChangeChan <- struct{}{}
+			}))
+		time.Sleep(100 * time.Millisecond) // see below
+		t2 := writeFileAndGetModTime(t, file, "xyz")
 
-			// DO NOT MERGE: Testing mtime precision.
-			assert.NotEqual(t, t2, t1)
+		if t1 == t2 {
+			// We sleep before updating the file to try to increase the
+			// likelihood of the second write updating the file's ModTime.
+			//
+			// The ModTime (mtime) is sometimes not very precise, causing
+			// the file to look unchanged to the poll-based watcher that
+			// we use.
+			//
+			// Great blog post about it: https://apenwarr.ca/log/20181113
+			t.Skip("test ran too fast and mtime didn't change")
+		}
 
-			waitWithDeadline(t, onChangeChan,
-				"expected file callback to be called")
-		})
-	}
+		waitWithDeadline(t, onChangeChan,
+			"expected file callback to be called")
+	})
 
 	t.Run("fails if file does not exist", func(t *testing.T) {
 		t.Parallel()
