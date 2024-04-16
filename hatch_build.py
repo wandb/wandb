9d70a3840d1837745a022620ea8773a761c9fb04
Timofey Peshin <timoffex@gmail.com>
2024-04-16 10:16:40 -0700

    fix macOS platform tags, replace pytest-openfiles
    Created using spr 1.3.5

--- hatch_build.py
+++ hatch_build.py
@@ -3,7 +3,6 @@ import pathlib
 import platform
 import re
 import shutil
-import subprocess
 import sys
 import sysconfig
 from typing import Any, Dict, List
@@ -36,10 +35,35 @@ class CustomBuildHook(BuildHookInterface):
             artifacts.extend(self._build_apple_stats())
 
         if self._is_platform_wheel():
-            # Replace dots, spaces and dashes with underscores following
-            # https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/#platform-tag
-            platform_tag = re.sub("[-. ]", "_", sysconfig.get_platform())
-            build_data["tag"] = f"py3-none-{platform_tag}"
+            build_data["tag"] = f"py3-none-{self._get_platform_tag()}"
+
+    def _get_platform_tag(self) -> str:
+        """Returns the platform tag for the current platform."""
+        # Replace dots, spaces and dashes with underscores following
+        # https://packaging.python.org/en/latest/specifications/platform-compatibility-tags/#platform-tag
+        platform_tag = re.sub("[-. ]", "_", sysconfig.get_platform())
+
+        # On macOS versions >=11, pip expects the minor version to be 0:
+        #   https://github.com/pypa/packaging/issues/435
+        #
+        # You can see the list of tags that pip would support on your machine
+        # using `pip debug --verbose`. On my macOS, get_platform() returns
+        # 14.1, but `pip debug --verbose` reports only these py3 tags with 14:
+        #
+        # * py3-none-macosx_14_0_arm64
+        # * py3-none-macosx_14_0_universal2
+        #
+        # We do this remapping here because otherwise, it's possible for `pip wheel`
+        # to successfully produce a wheel that you then cannot `pip install` on the
+        # same machine.
+        macos_match = re.fullmatch(r"macosx_(\d+_\d+)_(\w+)", platform_tag)
+        if macos_match:
+            major, _ = macos_match.group(1).split("_")
+            if int(major) >= 11:
+                arch = macos_match.group(2)
+                platform_tag = f"macosx_{major}_0_{arch}"
+
+        return platform_tag
 
     def _must_build_universal(self) -> bool:
         """Returns whether we must build a universal wheel."""
