f8248cd848dbccde5f253a3840e44994e9241848
Jeff Raubitschek <jeff@wandb.com>
2024-04-17 12:25:44 -0700

    add new api

---
+++ core/lang/py/examples/simple-newapi.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import wandb
+
+"""
+Top level:
+new_api() -> API (? or sdk or library or core..or not)
+default_sdk
+default_session
+default_run
+(promote mothods from default_sdk, default_session - and maybe default_run to top level namespace?)
+
+API:
+  new_session -> Session
+
+Session:
+  login()
+  configure_auth()
+  new_run()
+  get_run()  # might have mutable and readonly versions of the run? readonly by default?
+  # alternate, prefix with object type? run_new, run_get... dont love
+  # ? are api runs just like runapi --> can we log to a run from the public api? why not?
+
+Run:
+  log()
+  history() -> how does this work for a run in progress
+
+
+"""
+
+api = wandb.new_api()
+session = wandb.new_session()
+run = session.get_run()
+for row in run.history():
+  pass
