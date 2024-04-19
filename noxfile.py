c3f734bd4dd564b32431f1776e94fb03792abcd4
Timofey Peshin <timoffex@gmail.com>
2024-04-19 11:50:40 -0700

    try forwarding PATH
    Created using spr 1.3.5

--- noxfile.py
+++ noxfile.py
@@ -61,6 +61,7 @@ def unit_tests(session: nox.Session, core: bool) -> None:
         "WANDB__NETWORK_BUFFER": "1000",
         "WANDB_ERROR_REPORTING": "false",
         "USERNAME": os.getenv("USERNAME"),
+        "PATH": os.getenv("PATH"),
     }
 
     # Print 20 slowest tests.
