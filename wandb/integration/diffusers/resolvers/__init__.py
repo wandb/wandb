455f8c8009f9b8cf8db389270f13fb73518ad7ac
soumik12345 <19soumik.rakshit96@gmail.com>
2024-04-22 12:25:09 +0000

    refactor: got rid of additional resolver class

--- wandb/integration/diffusers/resolvers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .multimodal import (
-    SUPPORTED_MULTIMODAL_PIPELINES,
-    DiffusersMultiModalPipelineResolver,
-)
-
-__all__ = [
-    "SUPPORTED_MULTIMODAL_PIPELINES",
-    "DiffusersMultiModalPipelineResolver",
-]
