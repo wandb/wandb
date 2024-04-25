9a60f71767319330e1790134d4ca72a744dd1e4a
Hugh Wimberly <hugh.wimberly@wandb.com>
2024-04-25 11:29:04 -0700

    chore(artifacts): deprecate Api.artifact_versions (#7475)

--- wandb/proto/v4/wandb_telemetry_pb2.pyi
+++ wandb/proto/v4/wandb_telemetry_pb2.pyi
@@ -782,6 +782,7 @@ class Deprecated(google.protobuf.message.Message):
     LANGCHAIN_TRACER_FIELD_NUMBER: builtins.int
     ARTIFACT__GET_PATH_FIELD_NUMBER: builtins.int
     ARTIFACTMANIFESTENTRY__NAME_FIELD_NUMBER: builtins.int
+    API__ARTIFACT_VERSIONS_FIELD_NUMBER: builtins.int
     keras_callback__data_type: builtins.bool
     """wandb.keras.WandbCallback(data_type=...) called"""
     run__mode: builtins.bool
@@ -806,6 +807,8 @@ class Deprecated(google.protobuf.message.Message):
     """wandb.sdk.artifacts.artifact.Artifact.get_path(...) called"""
     artifactmanifestentry__name: builtins.bool
     """wandb.sdk.artifacts.artifact_manifest_entry.ArtifactManifestEntry.name() called"""
+    api__artifact_versions: builtins.bool
+    """wandb.Api().artifact_versions(...) called"""
     def __init__(
         self,
         *,
@@ -821,8 +824,9 @@ class Deprecated(google.protobuf.message.Message):
         langchain_tracer: builtins.bool = ...,
         artifact__get_path: builtins.bool = ...,
         artifactmanifestentry__name: builtins.bool = ...,
+        api__artifact_versions: builtins.bool = ...,
     ) -> None: ...
-    def ClearField(self, field_name: typing_extensions.Literal["artifact__get_path", b"artifact__get_path", "artifactmanifestentry__name", b"artifactmanifestentry__name", "init__config_exclude_keys", b"init__config_exclude_keys", "init__config_include_keys", b"init__config_include_keys", "keras_callback__data_type", b"keras_callback__data_type", "keras_callback__save_model", b"keras_callback__save_model", "langchain_tracer", b"langchain_tracer", "plots", b"plots", "run__join", b"run__join", "run__log_sync", b"run__log_sync", "run__mode", b"run__mode", "run__save_no_args", b"run__save_no_args"]) -> None: ...
+    def ClearField(self, field_name: typing_extensions.Literal["api__artifact_versions", b"api__artifact_versions", "artifact__get_path", b"artifact__get_path", "artifactmanifestentry__name", b"artifactmanifestentry__name", "init__config_exclude_keys", b"init__config_exclude_keys", "init__config_include_keys", b"init__config_include_keys", "keras_callback__data_type", b"keras_callback__data_type", "keras_callback__save_model", b"keras_callback__save_model", "langchain_tracer", b"langchain_tracer", "plots", b"plots", "run__join", b"run__join", "run__log_sync", b"run__log_sync", "run__mode", b"run__mode", "run__save_no_args", b"run__save_no_args"]) -> None: ...
 
 global___Deprecated = Deprecated
 
