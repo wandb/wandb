c2c7d262dbf67d022a0c7ee2c7a80e03ae41b230
Jeff Raubitschek <jeff@wandb.com>
2024-05-01 14:56:47 -0700

    format

--- core/internal/processlib/processlib_linux.go
+++ core/internal/processlib/processlib_linux.go
@@ -14,7 +14,7 @@ func ShutdownOnParentDeath(parentPid int) {
 	_, _, errno := syscall.Syscall(uintptr(PRCTL_SYSCALL), uintptr(PR_SET_PDEATHSIG), uintptr(syscall.SIGKILL), 0)
 	if errno != 0 {
 		os.Exit(127 + int(errno))
-        }
+	}
 	// One last check... there is a possibility that the parent died right before the syscall was sent
 	if os.Getppid() != parentPid {
 		os.Exit(1)
