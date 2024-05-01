e7d75ce7f79263e1f5ce85237ffc687e8ee2658e
Jeff Raubitschek <jeff@wandb.com>
2024-05-01 14:56:33 -0700

    add processlib

---
+++ core/internal/processlib/processlib_linux.go
@@ -0,0 +1,22 @@
+package processlib
+
+import (
+	"os"
+	"syscall"
+)
+
+const (
+	PRCTL_SYSCALL    = 157
+	PR_SET_PDEATHSIG = 1
+)
+
+func ShutdownOnParentDeath(parentPid int) {
+	_, _, errno := syscall.Syscall(uintptr(PRCTL_SYSCALL), uintptr(PR_SET_PDEATHSIG), uintptr(syscall.SIGKILL), 0)
+	if errno != 0 {
+		os.Exit(127 + int(errno))
+        }
+	// One last check... there is a possibility that the parent died right before the syscall was sent
+	if os.Getppid() != parentPid {
+		os.Exit(1)
+	}
+}
