dc9c99c91b11014310f604bfde8b10b35c77d01f
Dmitry Duev <dima@wandb.com>
2024-05-01 14:55:44 -0700

    clean up

--- core/pkg/server/stream.go
+++ core/pkg/server/stream.go
@@ -302,6 +302,7 @@ func (s *Stream) HandleRecord(rec *service.Record) {
 	select {
 	case <-s.ctx.Done():
 		s.logger.Error("context done, not handling record", "record", rec)
+		return
 	default:
 		s.inChan <- rec
 	}
