fae52f884af031eca0af3150624e957f1713a036
Kyle Goyette <kyle@wandb.com>
2024-05-02 16:20:14 -0700

    ruff ordering

--- wandb/sdk/launch/agent2/controllers/__init__.py
+++ wandb/sdk/launch/agent2/controllers/__init__.py
@@ -1,8 +1,8 @@
 from .k8s import k8s_controller
 from .local_container import local_container_controller
 from .local_process import local_process_controller
-from .vertex import vertex_controller
 from .sagemaker import sagemaker_controller
+from .vertex import vertex_controller
 
 __all__ = [
     "local_process_controller",
