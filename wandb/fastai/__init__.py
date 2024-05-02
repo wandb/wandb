02da97133f381d795c39e9b02a53ae9510fc800e
Dmitry Duev <dima@wandb.com>
2024-05-02 16:37:48 -0700

    chore(sdk): clean up ununsed files

--- wandb/fastai/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Compatibility fastai module.
-
-In the future use:
-    from wandb.integration.fastai import WandbCallback
-"""
-
-from wandb.integration.fastai import WandbCallback
-
-__all__ = ["WandbCallback"]
