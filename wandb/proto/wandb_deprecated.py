651a19d597f1f359315120789a708459666d2f6d
Dmitry Duev <dima@wandb.com>
2024-05-02 17:08:46 -0700

    clean up

--- wandb/proto/wandb_deprecated.py
+++ wandb/proto/wandb_deprecated.py
@@ -15,6 +15,7 @@ DEPRECATED_FEATURES = Literal[
     "run__mode",
     "run__save_no_args",
     "run__join",
+    "plots",
     "run__log_sync",
     "init__config_include_keys",
     "init__config_exclude_keys",
@@ -31,6 +32,7 @@ class Deprecated:
     run__mode: DEPRECATED_FEATURES = "run__mode"
     run__save_no_args: DEPRECATED_FEATURES = "run__save_no_args"
     run__join: DEPRECATED_FEATURES = "run__join"
+    plots: DEPRECATED_FEATURES = "plots"
     run__log_sync: DEPRECATED_FEATURES = "run__log_sync"
     init__config_include_keys: DEPRECATED_FEATURES = "init__config_include_keys"
     init__config_exclude_keys: DEPRECATED_FEATURES = "init__config_exclude_keys"
