4c563ad4b255a079bf2be891c20d7d08c8e3ca09
Timofey Peshin <timoffex@gmail.com>
2024-05-05 05:45:51 -0400

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.5
    
    [skip ci]

--- core/pkg/filestream/update.go
+++ core/pkg/filestream/update.go
@@ -1,7 +1,25 @@
 package filestream
 
-// Update is unprocessed data that the filestream operates on.
+import (
+	"github.com/wandb/wandb/core/pkg/observability"
+	"github.com/wandb/wandb/core/pkg/service"
+)
+
+// Update is a modification to the filestream's next API request.
 type Update interface {
-	// Chunk processes the update and transmits zero or more chunks.
-	Chunk(*fileStream) error
+	// Apply processes data and modifies the next API request.
+	Apply(UpdateContext) error
+}
+
+type UpdateContext struct {
+	// ModifyRequest updates the next filestream API request.
+	//
+	// The state update runs in a separate goroutine.
+	ModifyRequest func(CollectorStateUpdate)
+
+	Settings *service.Settings
+	ClientID string
+
+	Logger  *observability.CoreLogger
+	Printer *observability.Printer
 }
