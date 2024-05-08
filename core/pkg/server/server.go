e16c76618f84db359d67a0f1f7aae6950c645f6a
Jeff Raubitschek <jeff@wandb.com>
2024-05-07 19:23:30 -0700

    update

--- core/pkg/server/server.go
+++ core/pkg/server/server.go
@@ -58,28 +58,6 @@ func NewServer(ctx context.Context, addr string, portFile string) (*Server, erro
 	return s, nil
 }
 
-func (s *Server) loopCheckIfParentGone(pid int) bool {
-	for {
-		select {
-		case <-s.pidwatchChan:
-			return false
-		case <-time.After(50 * time.Millisecond):
-		}
-		parentpid := os.Getppid()
-		if parentpid != pid {
-			return true
-		}
-	}
-}
-
-func (s *Server) WatchParentPid(pid int) {
-	shouldExit := s.loopCheckIfParentGone(pid)
-	if shouldExit {
-		os.Exit(2)
-	}
-	s.wg.Done()
-}
-
 func (s *Server) SetDefaultLoggerPath(path string) {
 	if path == "" {
 		return
