af0ca5a92e7cee8ecc27caf453f8457cf4a06cfd
Timofey Peshin <timoffex@gmail.com>
2024-05-08 17:54:58 +0100

    refactor(core): TransmitChunk no longer a CollectorStateUpdate
    Description
    ---
    Removes the `CollectorStateUpdate.Apply` implementation for `TransmitChunk` and moves that responsibility into each update. It's a little more boilerplate, but it improves cohesion by making each `Update`'s data flow explicit.
    
    Reviewers: kptkin
    
    Reviewed By: kptkin
    
    Pull Request: https://github.com/wandb/wandb/pull/7569

--- core/pkg/filestream/transmitchunk.go
+++ core/pkg/filestream/transmitchunk.go
@@ -1,9 +1,6 @@
 package filestream
 
 // TransmitChunk is data to be sent to the filestream endpoint.
-//
-// A TransmitChunk is also a CollectorStateUpdate via updating
-// the buffered data.
 type TransmitChunk struct {
 	HistoryLines    []string
 	EventsLines     []string
@@ -15,27 +12,6 @@ type TransmitChunk struct {
 
 	HasPreempting bool
 	Preempting    bool
-}
-
-func (c *TransmitChunk) Apply(state *CollectorState) {
-	state.Buffer.HistoryLines =
-		append(state.Buffer.HistoryLines, c.HistoryLines...)
-	state.Buffer.EventsLines =
-		append(state.Buffer.EventsLines, c.EventsLines...)
-	state.Buffer.ConsoleLogLines =
-		append(state.Buffer.ConsoleLogLines, c.ConsoleLogLines...)
-
-	if c.LatestSummary != "" {
-		state.Buffer.LatestSummary = c.LatestSummary
-	}
-
-	state.Buffer.UploadedFiles =
-		append(state.Buffer.UploadedFiles, c.UploadedFiles...)
-
-	if c.HasPreempting {
-		state.Buffer.HasPreempting = true
-		state.Buffer.Preempting = c.Preempting
-	}
 }
 
 // Write writes the buffered data to the filestream request.
