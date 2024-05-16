3e8d499103e9648c632d170d18f04b9e1bf5a34f
ArtsiomWB <artsiom.skarakhod@wandb.com>
2024-05-16 17:08:01 -0600

    ramit test

--- tests/pytest_tests/unit_tests/test_wandb_log.py
+++ tests/pytest_tests/unit_tests/test_wandb_log.py
@@ -46,3 +46,27 @@ def test_log_not_dict(mock_run):
     run = mock_run()
     with pytest.raises(ValueError):
         run.log(10)
+
+
+def test_wandb_nested_visualize(mock_run,parse_records,record_q,):
+    run = mock_run()
+    data = [
+            ("Dog", "Dog", 34),
+            ("Cat", "Cat", 29),
+            ("Dog", "Cat", 5),
+            ("Cat", "Dog", 3),
+            ("Bird", "Bird", 40),
+            ("Bird", "Cat", 2),
+        ]
+
+    logged_table = wandb.Table(columns=["Predicted", "Actual", "Count"], data=data)
+    # table_name = "test2"
+    print(run)
+    run.log({"ramit":logged_table})
+    # run.log({"test":{f"{table_name}": wandb.visualize("wandb/confusion_matrix/v1", logged_table)}})
+    # run.finish()
+    # parsed = parse_records(record_q)
+    # print(parsed)
+    assert run
+    # file_record = parsed.files[0].files[0]
+    # assert file_record.path == "test.rad"
