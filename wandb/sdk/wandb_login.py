b0c5f8463c2bb655dc334c9ec2c1567f295fdf47
Timofey Peshin <timoffex@gmail.com>
2024-05-17 11:26:25 +0100

    remove unused import
    Created using spr 1.3.5

--- wandb/sdk/wandb_login.py
+++ wandb/sdk/wandb_login.py
@@ -6,7 +6,7 @@ This authenticates your machine to log data to your account.
 import enum
 import os
 import sys
-from typing import Dict, Optional, Tuple
+from typing import Optional, Tuple
 
 if sys.version_info >= (3, 8):
     from typing import Literal
