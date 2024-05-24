2fa3962668b6b4edcc052d02b5732fad42e1b1c4
Jeff Raubitschek <jeff@wandb.com>
2024-05-24 09:16:44 -0700

    format

--- experimental/client-py/wandb/__init__.py
+++ experimental/client-py/wandb/__init__.py
@@ -13,6 +13,7 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Any, List, Optional
+
     from wandb import mailbox
 
 from wandb.proto import wandb_internal_pb2 as pb2
