c0d5c108139ddcb9bdaf06178096c0c2e1885840
Jeff Raubitschek <jeff@wandb.com>
2024-05-24 17:33:31 -0700

    move to hatch

--- experimental/client-go/bindings/py/lib/build_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-BASE=../../
-DEST=py/lib/
-cd $BASE/
-./scripts/base-build.sh
-mkdir -p $DEST/wandb/lib
-cp export/lib/libwandb_core.so $DEST/wandb/lib
--- experimental/client-go/bindings/py/lib/wandb/__init__.py
+++ experimental/client-go/bindings/py/lib/wandb/__init__.py
@@ -1,5 +1,7 @@
 """wandb library."""
 
+__version__ = "0.0.2.dev1"
+
 from wandb.proto import wandb_internal_pb2 as pb2
 
 
