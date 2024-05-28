effbac4d65a811bf86e5ac121ecc5dba9e7bac42
Danny Goldstein <danny@wandb.com>
2024-05-28 10:52:46 -0400

    fix lint

--- wandb/sdk/internal/internal_api.py
+++ wandb/sdk/internal/internal_api.py
@@ -2266,6 +2266,8 @@ class Api:
         """
 
         mutation = gql(query_string)
+        
+        kwargs = {}
         if num_retries is not None:
             kwargs["num_retries"] = num_retries
 
