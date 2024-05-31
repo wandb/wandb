68209e1eb9c5bd3cebced00ed2ec5917b84cdaa1
Dmitry Duev <dima@wandb.com>
2024-05-31 14:12:28 -0700

    add custom retry policy to file stream and add more logging

--- core/internal/filestream/retry_policy.go
+++ core/internal/filestream/retry_policy.go
@@ -13,22 +13,22 @@ func RetryPolicy(
 	resp *http.Response,
 	err error,
 ) (bool, error) {
-
 	// non-retryable status codes
-	statusCode := resp.StatusCode
-	switch {
-	case statusCode == http.StatusBadRequest: // don't retry on 400 bad request or 409 conflict
-		return false, err
-	case statusCode == http.StatusUnauthorized: // don't retry on 401 unauthorized
-		return false, err
-	case statusCode == http.StatusForbidden: // don't retry on 403 forbidden
-		return false, err
-	case statusCode == http.StatusNotFound: // don't retry on 404 not found
-		return false, err
-	case statusCode == http.StatusConflict: // don't retry on 409 conflict
-		return false, err
-	case statusCode == http.StatusGone: // don't retry on 410 Gone
-		return false, err
+	if resp != nil {
+		switch resp.StatusCode {
+		case http.StatusBadRequest: // don't retry on 400 bad request or 409 conflict
+			return false, err
+		case http.StatusUnauthorized: // don't retry on 401 unauthorized
+			return false, err
+		case http.StatusForbidden: // don't retry on 403 forbidden
+			return false, err
+		case http.StatusNotFound: // don't retry on 404 not found
+			return false, err
+		case http.StatusConflict: // don't retry on 409 conflict
+			return false, err
+		case http.StatusGone: // don't retry on 410 Gone
+			return false, err
+		}
 	}
 	// if err != nil, retryablehttp's base policy is to retry
 	// if the error is recoverable, meaning it's not:
@@ -36,12 +36,8 @@ func RetryPolicy(
 	//   - due to an invalid protocol scheme.
 	//   - due to an invalid header.
 	//   - due to TLS cert verification failure.
-	shoudRetry, err := retryablehttp.ErrorPropagatedRetryPolicy(ctx, resp, err)
 	// if resp != nil and err == nil, retryablehttp's base policy is to retry
 	// if the status code is 429, 5xx, or invalid. We want to be extra cautious
 	// and retry in other remaining cases as well.
-	if !shoudRetry && err == nil {
-		return true, nil
-	}
-	return shoudRetry, err
+	return retryablehttp.ErrorPropagatedRetryPolicy(ctx, resp, err)
 }
