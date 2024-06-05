526982c44e7d27ab1bd46482b25ff1c9d3a6927f
elaina <ehodgkin9418@gmail.com>
2024-06-05 16:55:44 -0700

    wip

--- tests/pytest_tests/unit_tests/test_lib/test_credentials.py
+++ tests/pytest_tests/unit_tests/test_lib/test_credentials.py
@@ -4,7 +4,6 @@ from pathlib import Path
 
 import pytest
 import responses
-
 from wandb import errors
 from wandb.sdk.lib.credentials import _expires_at_fmt, access_token
 
@@ -17,6 +16,7 @@ def write_credentials(data: dict, credentials_file: str):
 def write_token(token_file: str):
     with open(token_file, "w") as f:
         f.write("eykldfkma94wp4rm")
+
 
 def test_write_credentials(tmp_path: Path):
     base_url = "http://localhost"
@@ -132,7 +132,12 @@ def test_token_expired(tmp_path: Path):
     write_token(token_file)
 
     with responses.RequestsMock() as rsps:
-        rsps.add(responses.POST, base_url + "/oidc/token", json={'error': 'Token expired'}, status=401)
+        rsps.add(
+            responses.POST,
+            base_url + "/oidc/token",
+            json={"error": "Token expired"},
+            status=401,
+        )
 
         with pytest.raises(errors.AuthenticationError):
             access_token(base_url, token_file, credentials_file)
@@ -146,5 +151,3 @@ def test_token_file_not_found(tmp_path: Path):
     with responses.RequestsMock() as rsps:
         with pytest.raises(FileNotFoundError):
             access_token(base_url, token_file, credentials_file)
-
-
