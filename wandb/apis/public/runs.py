c5464389d6d429e7935111ef2cc8f8678d9e772d
Francesco Mamberti <francescomamberti@Francesco-Mamberti-NMWCQGGFDV.local>
2024-06-07 18:46:09 +0100

    fix:public-api: update _wandb only if exists without rawconfig loop

--- wandb/apis/public/runs.py
+++ wandb/apis/public/runs.py
@@ -562,9 +562,9 @@ class Run(Attrs):
     @property
     def json_config(self):
         config = {}
+        if "_wandb" in self.rawconfig:
+            config["_wandb"] = {"value": self.rawconfig['_wandb'], "desc": None}
         for k, v in self.config.items():
-            config[k] = {"value": v, "desc": None}
-        for k, v in self.rawconfig.items():
             config[k] = {"value": v, "desc": None}
         return json.dumps(config)
 
