fa51a0301bf3bead2b8de3dd0991fd2f334797c7
Kyle Goyette <kyle@wandb.com>
2024-06-18 16:41:21 -0700

    add test

--- tests/pytest_tests/system_tests/test_launch/test_launch_sweep.py
+++ tests/pytest_tests/system_tests/test_launch/test_launch_sweep.py
@@ -3,6 +3,7 @@ import json
 import pytest
 import wandb
 from wandb.apis.public import Api as PublicApi
+from wandb.cli import cli
 from wandb.sdk.launch.agent.agent import LaunchAgent
 from wandb.sdk.launch.sweeps.scheduler import Scheduler
 from wandb.sdk.launch.utils import LAUNCH_DEFAULT_PROJECT, construct_launch_spec
@@ -138,7 +139,16 @@ def test_launch_agent_scheduler(
     job_name = job_artifact.wait().name
 
     api = wandb.sdk.internal.internal_api.Api()
-    api.create_run_queue(entity=user, project=proj, queue_name=queue, access="USER")
+    res = api.create_default_resource_config(
+        user,
+        "local=container",
+        json.dumps({"e": "{{var}}"}),
+        {"var": {"schema": {"type": "string", "enum": ["1", "2"]}}},
+    )
+    id = res.get("defaultResourceConfigID")
+    api.create_run_queue(
+        entity=user, project=proj, queue_name=queue, access="USER", config_id=id
+    )
 
     sweep_config = {
         "job": job_name,
@@ -231,3 +241,70 @@ def test_launch_agent_scheduler(
     else:
         assert launch_agent._max_schedulers == int(max_schedulers)
     run.finish()
+
+
+def test_sweep_scheduler_job_with_queue(
+    runner, user, wandb_init, test_settings, mocker
+):
+    # cant download artifacts in tests, so patch this
+    mocker.patch("wandb.sdk.launch.sweeps.utils.check_job_exists", return_value=True)
+    queue = "queue"
+    settings = test_settings({"project": LAUNCH_DEFAULT_PROJECT})
+    run = wandb_init(settings=settings)
+
+    job_artifact = run._log_job_artifact_with_image("docker_image", args=[])
+    job_name = job_artifact.wait().name
+
+    api = wandb.sdk.internal.internal_api.Api()
+    res = api.create_default_resource_config(
+        user,
+        "local-container",
+        json.dumps({"resource_args": {"local-container": {"e": "{{var}}"}}}),
+        {"var": {"schema": {"type": "string", "enum": ["1", "2"]}}},
+    )
+    id = res.get("defaultResourceConfigID")
+    api.create_run_queue(
+        entity=user,
+        project=LAUNCH_DEFAULT_PROJECT,
+        queue_name=queue,
+        access="USER",
+        config_id=id,
+    )
+    cli._get_cling_api(reset=True)
+    with runner.isolated_filesystem():
+        with open("config.json", "w") as f:
+            json.dump(
+                {
+                    "queue": queue,
+                    "resource": "local-container",
+                    "job": f"{user}/{LAUNCH_DEFAULT_PROJECT}/{job_name}",
+                    "scheduler": {
+                        "job": f"{user}/{LAUNCH_DEFAULT_PROJECT}/{job_name}",
+                        "resource": "local-container",
+                        "template_variables": {"var": "1"},
+                    },
+                },
+                f,
+            )
+        sweep_config = {
+            "name": "My Sweep",
+            "method": "grid",
+            "parameters": {"parameter1": {"values": [1, 2, 3]}},
+        }
+        wandb.sweep(sweep_config)
+        res = runner.invoke(
+            cli.launch_sweep,
+            ["config.json", "--queue", queue],
+        )
+
+        rqi = api.pop_from_run_queue(
+            queue,
+            user,
+            LAUNCH_DEFAULT_PROJECT,
+        )
+        assert (
+            rqi.get("runSpec").get("resource_args").get("local-container").get("e")
+            == "1"
+        )
+        assert res.exit_code == 0
+        assert queue in res.output
