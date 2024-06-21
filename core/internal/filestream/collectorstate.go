54905a9fc4939c23de7836a55a789e3218d7ad0c
Timofey Peshin <timoffex@gmail.com>
2024-06-21 11:25:45 -0700

    feat(core): Lower rate-limit and better batching in filestream
    Description
    ---
    Send at most one filestream request per 15 seconds and accumulate data while waiting.
    
    I broke out `TransmitLoop` and `CollectLoop` out of `filestreamimpl.go` because testing it thoroughly was becoming a pain.
    
    Fixes WB-19169 (but see note below).
    
    This solves two problems:
    
    1. We're less likely to overload the backend with too many small requests
    2. On a slow network, we'll send fewer, larger requests
        * Before, we would have sent the same requests but slower
        * Before, filestream could easily clog up, causing the service to not handle any new messages until requests go through
    
    NOTE: There's no request size cap. In extreme cases, `filestream` may accumulate a request so large that it will never succeed. I plan to implement this later.
    
    Testing
    ---
    Manually run the following with debug logging:
    
    ```python
    import time
    from tqdm import trange
    import wandb
    
    with wandb.init() as run:
        for i in trange(60):
            run.log({"i": i})
            time.sleep(1)
    ```
    
    Then examine the posted requests:
    
    ```
    jq '
      select(.msg | contains("filestream: post request"))
        | [.time, (.request | fromjson)]
    ' wandb/latest-run/logs/debug-internal.log
    ```
    
    The requests are spaced 15 seconds apart except for the last one.
    
    Reviewers: kptkin
    
    Reviewed By: kptkin
    
    Pull Request: https://github.com/wandb/wandb/pull/7831

--- core/internal/filestream/collectorstate.go
+++ core/internal/filestream/collectorstate.go
@@ -1,6 +1,10 @@
 package filestream
 
-import "github.com/wandb/wandb/core/internal/sparselist"
+import (
+	"slices"
+
+	"github.com/wandb/wandb/core/internal/sparselist"
+)
 
 // CollectorState is the filestream's buffered data.
 type CollectorState struct {
@@ -58,10 +62,10 @@ type CollectorStateUpdate interface {
 	Apply(*CollectorState)
 }
 
-// MakeRequest moves buffered data into an API request and returns it.
+// PrepRequest prepares an API request from the collected data.
 //
-// Returns a boolean that's true if the request is non-empty.
-func (s *CollectorState) MakeRequest(isDone bool) (*FsTransmitData, bool) {
+// If the request is sent, `RequestSent` must be invoked.
+func (s *CollectorState) PrepRequest(isDone bool) *FsTransmitData {
 	files := make(map[string]FsTransmitFileData)
 	addLines := func(chunkType ChunkTypeEnum, lineNum int, lines []string) {
 		if len(lines) == 0 {
@@ -69,14 +73,11 @@ func (s *CollectorState) MakeRequest(isDone bool) (*FsTransmitData, bool) {
 		}
 		files[chunkFilename[chunkType]] = FsTransmitFileData{
 			Offset:  lineNum,
-			Content: lines,
+			Content: slices.Clone(lines),
 		}
 	}
 
 	addLines(HistoryChunk, s.HistoryLineNum, s.HistoryLines)
-	s.HistoryLineNum += len(s.HistoryLines)
-	s.HistoryLines = nil
-
 	addLines(EventsChunk, s.EventsLineNum, s.EventsLines)
 	s.EventsLineNum += len(s.EventsLines)
 	s.EventsLines = nil
@@ -88,10 +89,6 @@ func (s *CollectorState) MakeRequest(isDone bool) (*FsTransmitData, bool) {
 			Offset:  run.Start + s.ConsoleLogLineOffset,
 			Content: run.Items,
 		}
-
-		for i := run.Start; i < run.Start+len(run.Items); i++ {
-			s.ConsoleLogUpdates.Delete(i)
-		}
 	}
 
 	if s.LatestSummary != "" {
@@ -102,33 +99,49 @@ func (s *CollectorState) MakeRequest(isDone bool) (*FsTransmitData, bool) {
 		// to update the last line, since all other lines are ignored. This
 		// applies to resumed runs.
 		addLines(SummaryChunk, s.SummaryLineNum, []string{s.LatestSummary})
-		s.LatestSummary = ""
 	}
 
 	transmitData := FsTransmitData{}
-	hasData := false
 
 	if len(files) > 0 {
 		transmitData.Files = files
-		hasData = true
 	}
 
 	if len(s.UploadedFiles) > 0 {
-		transmitData.Uploaded = s.UploadedFiles
-		s.UploadedFiles = nil
-		hasData = true
+		transmitData.Uploaded = slices.Clone(s.UploadedFiles)
 	}
 
 	if s.HasPreempting {
 		transmitData.Preempting = s.Preempting
-		hasData = true
 	}
 
 	if isDone {
 		transmitData.Exitcode = s.ExitCode
 		transmitData.Complete = s.Complete
-		hasData = true
 	}
 
-	return &transmitData, hasData
+	return &transmitData
+}
+
+// RequestSent indicates that the result of PrepRequest was used.
+func (s *CollectorState) RequestSent() {
+	s.HistoryLineNum += len(s.HistoryLines)
+	s.HistoryLines = nil
+
+	s.EventsLineNum += len(s.EventsLines)
+	s.EventsLines = nil
+
+	// Drop uploaded lines.
+	if s.ConsoleLogUpdates.Len() > 0 {
+		run := s.ConsoleLogUpdates.ToRuns()[0]
+		for i := run.Start; i < run.Start+len(run.Items); i++ {
+			s.ConsoleLogUpdates.Delete(i)
+		}
+	}
+
+	s.LatestSummary = ""
+
+	s.UploadedFiles = nil
+
+	s.HasPreempting = false
 }
