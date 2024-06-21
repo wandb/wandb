adeca962c35fde5c29c73cfd87c7e5ef4f6c6007
Dmitry Duev <dima@wandb.com>
2024-06-21 15:05:32 -0700

    nits

--- core/internal/settings/settings.go
+++ core/internal/settings/settings.go
@@ -114,11 +114,12 @@ func (s *Settings) GetIgnoreGlobs() []string {
 	return s.Proto.IgnoreGlobs.GetValue()
 }
 
-// Custom proxy settings for the W&B backend.
+// Custom proxy for http requests to W&B.
 func (s *Settings) GetHTTPProxy() string {
 	return s.Proto.HttpProxy.GetValue()
 }
 
+// Custom proxy for https requests to W&B.
 func (s *Settings) GetHTTPSProxy() string {
 	return s.Proto.HttpsProxy.GetValue()
 }
