7869eb4c2a3977a687fabe9673fdfe4d81a902be
Katia Patkin <katia@wandb.com>
2024-07-11 18:36:20 -0700

    serialize write before send

--- core/pkg/server/writer.go
+++ core/pkg/server/writer.go
@@ -4,11 +4,9 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"sync"
 
 	"github.com/wandb/wandb/core/pkg/observability"
 	"github.com/wandb/wandb/core/pkg/service"
-	"google.golang.org/protobuf/proto"
 )
 
 type WriterOption func(*Writer)
@@ -48,24 +46,17 @@ type Writer struct {
 	// fwdChan is the channel for forwarding messages to the sender
 	fwdChan chan *service.Record
 
-	// storeChan is the channel for messages to be stored
-	storeChan chan *service.Record
-
 	// store is the store for the writer
 	store *Store
 
 	// recordNum is the running count of stored records
 	recordNum int64
-
-	// wg is the wait group for the writer
-	wg sync.WaitGroup
 }
 
 // NewWriter returns a new Writer
 func NewWriter(ctx context.Context, params WriterParams) *Writer {
 	w := &Writer{
 		ctx:      ctx,
-		wg:       sync.WaitGroup{},
 		logger:   params.Logger,
 		settings: params.Settings,
 		fwdChan:  params.FwdChan,
@@ -79,8 +70,6 @@ func (w *Writer) startStore() {
 		return
 	}
 
-	w.storeChan = make(chan *service.Record, BufferSize*8)
-
 	var err error
 	w.store = NewStore(w.ctx, w.settings.GetSyncFile().GetValue())
 	err = w.store.Open(os.O_WRONLY)
@@ -88,25 +77,6 @@ func (w *Writer) startStore() {
 		w.logger.CaptureFatalAndPanic(
 			fmt.Errorf("writer: startStore: error creating store: %v", err))
 	}
-
-	w.wg.Add(1)
-	go func() {
-		for record := range w.storeChan {
-			if err = w.store.Write(record); err != nil {
-				w.logger.CaptureError(
-					fmt.Errorf(
-						"writer: startStore: error storing record: %v",
-						err,
-					))
-			}
-		}
-
-		if err = w.store.Close(); err != nil {
-			w.logger.CaptureError(
-				fmt.Errorf("writer: startStore: error closing store: %v", err))
-		}
-		w.wg.Done()
-	}()
 }
 
 // Do is the main loop of the writer to process incoming messages
@@ -121,15 +91,15 @@ func (w *Writer) Do(inChan <-chan *service.Record) {
 		w.writeRecord(record)
 	}
 	w.Close()
-	w.wg.Wait()
 }
 
 // Close closes the writer and all its resources
 // which includes the store
 func (w *Writer) Close() {
 	close(w.fwdChan)
-	if w.storeChan != nil {
-		close(w.storeChan)
+	if err := w.store.Close(); err != nil {
+		w.logger.CaptureError(
+			fmt.Errorf("writer: startStore: error closing store: %v", err))
 	}
 	w.logger.Info("writer: Close: closed", "stream_id", w.settings.RunId)
 }
@@ -145,20 +115,27 @@ func (w *Writer) writeRecord(record *service.Record) {
 	case nil:
 		w.logger.Error("writer: writeRecord: nil record type")
 	default:
-		w.fwdRecord(record)
 		w.storeRecord(record)
+		w.fwdRecord(record)
 	}
 }
 
 // storeRecord stores the record in the append-only log
 func (w *Writer) storeRecord(record *service.Record) {
+	// local control messages are not stored in the transaction log
 	if record.GetControl().GetLocal() {
 		return
 	}
-	recordCopy := proto.Clone(record).(*service.Record)
+
 	w.recordNum += 1
-	recordCopy.Num = w.recordNum
-	w.storeChan <- recordCopy
+	record.Num = w.recordNum
+	if err := w.store.Write(record); err != nil {
+		w.logger.CaptureError(
+			fmt.Errorf(
+				"writer: startStore: error storing record: %v",
+				err,
+			))
+	}
 }
 
 func (w *Writer) fwdRecord(record *service.Record) {
