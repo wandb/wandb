ef0fa0bb1fc4e190573ad64648908e8ea6923cac
Tim Hays <tim.hays@wandb.com>
2024-07-17 16:42:00 -0500

    fix tests

--- tests/pytest_tests/unit_tests/test_launch/test_builder/test_context_manager.py
+++ tests/pytest_tests/unit_tests/test_launch/test_builder/test_context_manager.py
@@ -46,11 +46,11 @@ def test_create_build_context_wandb_dockerfile(mock_git_project):
     path = pathlib.Path(path)
     dockerfile = (path / "Dockerfile.wandb").read_text()
     assert "FROM python:3.8" in dockerfile
-    assert "pip install -r requirements.txt" in dockerfile
+    assert "uv pip install -r requirements.txt" in dockerfile
     assert (path / "src" / "entrypoint.py").exists()
     assert (path / "src" / "requirements.txt").exists()
     assert (
-        image_tag == "0c0f4554"
+        image_tag == "7fd24842"
     )  # This is the hash of the Dockerfile + image_source_string.
 
 
@@ -140,10 +140,10 @@ def test_create_build_context_buildx_enabled(mocker, mock_git_project):
     path = pathlib.Path(path)
     dockerfile = (path / "Dockerfile.wandb").read_text()
     assert "FROM python:3.8" in dockerfile
-    assert "pip install -r requirements.txt" in dockerfile
+    assert "uv pip install -r requirements.txt" in dockerfile
     assert "RUN WANDB_DISABLE_CACHE=true" not in dockerfile
     assert (path / "src" / "entrypoint.py").exists()
     assert (path / "src" / "requirements.txt").exists()
     assert (
-        image_tag == "a3a51084"
+        image_tag == "de70c825"
     )  # This is the hash of the Dockerfile + image_source_string.
