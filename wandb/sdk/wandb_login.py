58febd8485de119670921deb28061f71475ff1f8
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2024-07-22 15:53:51 -0700

    Removed trailing space

--- wandb/sdk/wandb_login.py
+++ wandb/sdk/wandb_login.py
@@ -63,7 +63,7 @@ def login(
             If set to "must", always log a user in anonymously. If set to
             "allow", only create an anonymous user if the user
             isn't already logged in. If set to "never", never log a 
-            user anonymously. Default set to "never". 
+            user anonymously. Default set to "never".
         relogin: (bool, optional) If true, will re-prompt for API key.
         host: (string, optional) The host to connect to.
         force: (bool, optional) If true, will force a relogin.
