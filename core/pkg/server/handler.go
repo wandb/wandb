c630986d351a3762d0a284cc137ae0ce111e844a
Katia Patkin <katia@wandb.com>
2024-07-24 08:23:50 -0700

    remove todo comment

--- core/pkg/server/handler.go
+++ core/pkg/server/handler.go
@@ -568,7 +568,6 @@ func (h *Handler) handleRequestRunStart(record *service.Record, request *service
 	var ok bool
 	run := request.Run
 
-	// TODO: verify the logic
 	// offsset by run.Runtime to account for potential run branching
 	startTime := run.StartTime.AsTime().Add(time.Duration(-run.Runtime) * time.Second)
 	// start the run timer
