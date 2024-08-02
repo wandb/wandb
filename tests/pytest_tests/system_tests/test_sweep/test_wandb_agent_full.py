3ba9c1e7b447a36eb35663245db0f7fccbcae5e4
Kyle Goyette <kyle@wandb.com>
2024-08-02 09:06:01 -0700

    test wandb agent

--- tests/pytest_tests/system_tests/test_sweep/test_wandb_agent_full.py
+++ tests/pytest_tests/system_tests/test_sweep/test_wandb_agent_full.py
@@ -4,6 +4,7 @@ import os
 import unittest.mock
 
 import wandb
+import wandb.apis
 from wandb.apis.public import Api
 
 
@@ -106,3 +107,62 @@ def test_agent_ignore_project_entity_run_id(wandb_init, user):
     assert sweep_projects[0] == "actual"
     assert sweep_entities[0] == user
     assert sweep_run_ids[0] != "also_ignored"
+
+
+def test_pyagent_ack_rqi(wandb_init, monkeypatch):
+    sweep_ids = []
+    sweep_configs = []
+    sweep_resumed = []
+
+    ack_rqi = unittest.mock.MagicMock()
+    monkeypatch.setattr(wandb.apis.internal.InternalApi, "ack_run_queue_item", ack_rqi)
+    sweep_config = {
+        "name": "My Sweep",
+        "method": "grid",
+        "parameters": {"a": {"values": [1, 2, 3]}},
+    }
+
+    def train():
+        run = wandb_init()
+        sweep_ids.append(run.sweep_id)
+        sweep_configs.append(dict(run.config))
+        sweep_resumed.append(run.resumed)
+        run.finish()
+
+    sweep_id = wandb.sweep(sweep_config)
+
+    wandb.agent(sweep_id, function=train, count=1)
+
+    assert len(sweep_ids) == len(sweep_configs) == 1
+    assert sweep_ids[0] == sweep_id
+    assert sweep_configs[0] == {"a": 1}
+    assert sweep_resumed[0] is False
+    assert ack_rqi.call_count == 1
+
+
+def test_wandb_agent_ack_rqi(monkeypatch, mocker):
+    sweep_ids = []
+    sweep_configs = []
+    sweep_resumed = []
+
+    ack_rqi = unittest.mock.MagicMock()
+    monkeypatch.setattr(wandb.apis.internal.InternalApi, "ack_run_queue_item", ack_rqi)
+    agent_process = unittest.mock.MagicMock()
+    monkeypatch.setattr("wandb.wandb_agent.AgentProcess", agent_process)
+    sweep_config = {
+        "name": "My Sweep",
+        "method": "grid",
+        "parameters": {"a": {"values": [1, 2, 3]}},
+        "program": "train.py",
+    }
+
+    sweep_id = wandb.sweep(sweep_config)
+
+    wandb.agent(sweep_id, count=1)
+
+    assert len(sweep_ids) == len(sweep_configs) == 1
+    assert sweep_ids[0] == sweep_id
+    assert sweep_configs[0] == {"a": 1}
+    assert sweep_resumed[0] is False
+    assert ack_rqi.call_count == 1
+    assert agent_process.call_count == 1
