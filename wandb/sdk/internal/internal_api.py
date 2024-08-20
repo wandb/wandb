2ff13dfd53c20f30c08738da67ee5b1631c18ea5
tonyyli-wandb <tony.li@wandb.com>
2024-08-20 14:18:42 -0500

    chore(sdk): cleanup, edits for consistency

--- wandb/sdk/internal/internal_api.py
+++ wandb/sdk/internal/internal_api.py
@@ -3699,7 +3699,7 @@ class Api:
         artifact_fields = self.server_artifact_introspection()
         if "ttlIsInherited" not in artifact_fields and ttl_duration_seconds:
             wandb.termwarn(
-                "Server not compatible with setting Artifact TTL, "
+                "Server not compatible with setting Artifact TTLs, "
                 "Please upgrade the server to use Artifact TTL."
             )
             # ttlDurationSeconds is only usable if ttlIsInherited is also present
