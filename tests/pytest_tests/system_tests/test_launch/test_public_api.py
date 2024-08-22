cacb675a6eead364f1d2a0a4deefe376abe8e62e
Dmitry Duev <dima@wandb.com>
2024-08-22 12:22:24 -0700

    chore(sdk): clean up ci

--- tests/pytest_tests/system_tests/test_launch/test_public_api.py
+++ tests/pytest_tests/system_tests/test_launch/test_public_api.py
@@ -1,12 +1,12 @@
 import wandb
 
 
-def test_from_path(mock_server, api):
+def test_from_path(api):
     sweep = api.from_path("test/test/sweeps/test")
     assert isinstance(sweep, wandb.apis.public.Sweep)
 
 
-def test_sweep(runner, mock_server, api):
+def test_sweep(api):
     sweep = api.sweep("test/test/test")
     assert sweep.entity == "test"
     assert sweep.best_run().name == "beast-bug-33"
