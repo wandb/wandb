2a8f26c16c4f84f419b5a85dd2a49ce9bda7fe47
tonyyli-wandb <tony.li@wandb.com>
2024-08-28 16:11:12 -0700

    chore(sdk): cleanup tests

--- tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts_api.py
+++ tests/pytest_tests/system_tests/test_artifacts/test_wandb_artifacts_api.py
@@ -4,6 +4,7 @@ import os
 import re
 
 import pytest
+
 import wandb
 from wandb import Api
 from wandb.errors import CommError
@@ -226,64 +227,6 @@ def test_save_invalid_tags_after_logging_artifact(
         assert sorted(final_tags) == sorted(set(orig_tags))
     else:
         assert not final_tags
-
-
-# @pytest.fixture
-# def mock_old_server_artifact_introspection(monkeypatch) -> None:
-#     # Partially mocks an older server that doesn't recognize
-#     # newer Artifact fields, e.g. `Artifact.tags` at least via introspection
-#     from wandb.sdk.internal import internal_api
-#
-#     current_fields = internal_api.Api().server_artifact_introspection()
-#     excluded_fields = {"tags", "ttlIsInherited", "ttlDurationSeconds"}
-#
-#     def fake_server_artifact_introspection(self) -> list[str]:
-#         return [name for name in current_fields if name not in excluded_fields]
-#
-#     monkeypatch.setattr(
-#         internal_api.Api,
-#         "server_artifact_introspection",
-#         fake_server_artifact_introspection,
-#     )
-#
-#     yield
-#
-#
-# def test_save_tags_without_server_support(
-#     tmp_path,
-#     user,
-#     wandb_init,
-#     api,
-#     mock_old_server_artifact_introspection,
-# ):
-#     project = "test"
-#     artifact_name = "test-artifact"
-#     artifact_type = "test-type"
-#     artifact_fullname = f"{user}/{project}/{artifact_name}:v0"
-#
-#     artifact_filepath = tmp_path / "boom.txt"
-#     artifact_filepath.write_text("testing")
-#
-#     # We've mocked an older server that doesn't recognize `Artifact.tags` on introspection
-#     tags_to_add = ["new-tag", "other tag"]
-#
-#     with wandb_init(entity=user, project=project) as run:
-#         artifact = wandb.Artifact(name=artifact_name, type=artifact_type)
-#         artifact.add_file(str(artifact_filepath), "test-name")
-#         run.log_artifact(artifact, tags=tags_to_add)
-#         artifact.wait()
-#
-#     fetched_artifact = api.artifact(name=artifact_fullname, type=artifact_type)
-#
-#     assert fetched_artifact.tags == []
-#
-#     # Try adding new tags
-#     fetched_artifact.tags = tags_to_add
-#     fetched_artifact.save()
-#
-#     # tags should remain unchanged, since server doesn't support tags
-#     final_tags = api.artifact(name=artifact_fullname, type=artifact_type).tags
-#     assert final_tags == []
 
 
 @pytest.mark.parametrize("invalid_tags", INVALID_TAG_LISTS)
