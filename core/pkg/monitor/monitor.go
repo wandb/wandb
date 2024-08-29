1d424b511f02e9fa224ecc4aac9c611813756c17
Dmitry Duev <dima@wandb.com>
2024-08-29 08:52:02 -0700

    comment

--- core/pkg/monitor/monitor.go
+++ core/pkg/monitor/monitor.go
@@ -224,8 +224,7 @@ func (sm *SystemMonitor) Start() {
 //
 // Pause and Resume are used in notebook environments to ensure that
 // metrics are only collected when a cell is running. We do it this way
-// as opposed to stopping and starting the monitor to prevent the overhead of
-// starting and stopping the monitor for each cell.
+// to prevent the overhead of starting and stopping the monitor for each cell.
 func (sm *SystemMonitor) Pause() {
 	if sm.state.CompareAndSwap(StateRunning, StatePaused) {
 		sm.logger.Info("Pausing system monitor")
