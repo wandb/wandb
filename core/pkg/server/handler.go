96a59e6144a762ccd105a49f1df79b69d46d6e5a
Timofey Peshin <timoffex@gmail.com>
2024-08-30 17:20:04 -0700

    use fwdRecord after handleMetric in partial history code
    Created using spr 1.3.5

--- core/pkg/server/handler.go
+++ core/pkg/server/handler.go
@@ -1101,9 +1101,11 @@ func (h *Handler) flushPartialHistory(useStep bool, nextStep int64) {
 	for _, newMetric := range newMetricDefs {
 		// We don't mark the record 'Local' because partial history updates
 		// are not already written to the transaction log.
-		h.handleMetric(&spb.Record{
+		rec := &spb.Record{
 			RecordType: &spb.Record_Metric{Metric: newMetric},
-		})
+		}
+		h.handleMetric(rec)
+		h.fwdRecord(rec)
 	}
 	h.metricHandler.InsertStepMetrics(h.partialHistory)
 
