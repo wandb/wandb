58fe91383a0bd69a52836134b1a1a0c58380cb4b
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-08-31 12:15:05 -0700

    test(sdk): port keras tests and fix keras callback (#8241)

---
+++ tests/system_tests/test_functional/keras/keras_deprecated.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+import wandb
+from wandb.integration.keras import WandbCallback
+
+
+def main():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(3, 3, activation="relu", input_shape=(28, 28, 1)))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(10, activation="softmax"))
+    model.compile(
+        loss="sparse_categorical_crossentropy", optimizer="sgd", metrics=["accuracy"]
+    )
+
+    with wandb.init(
+        project="keras",
+    ):
+        model.fit(
+            tf.ones((10, 28, 28, 1)),
+            tf.ones((10,)),
+            epochs=7,
+            validation_split=0.2,
+            callbacks=[
+                WandbCallback(
+                    save_graph=False,  # wandb implementation is broken
+                    save_model=False,  # wandb implementation is broken
+                )
+            ],
+        )
+
+
+if __name__ == "__main__":
+    main()
