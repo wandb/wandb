e152a97152828edd12b7397fd35c5241911ea393
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-09-04 14:36:47 -0700

    chore(sdk): move viz from top level (#8258)

--- tests/system_tests/test_core/test_wandb.py
+++ tests/system_tests/test_core/test_wandb.py
@@ -16,8 +16,8 @@ from unittest import mock
 import pytest
 import wandb
 from wandb.sdk.lib import filesystem
+from wandb.sdk.lib.viz import custom_chart
 from wandb.sdk.wandb_init import init as real_wandb_init
-from wandb.viz import custom_chart
 
 
 @pytest.fixture
