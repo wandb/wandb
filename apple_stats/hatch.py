cc621f3906d5136e47a81f53a0b94d1a2395c5ba
Dmitry Duev <dima@wandb.com>
2024-09-10 14:39:24 -0700

    fix: remove platform arch check in apple_stats/hatch.py

--- apple_stats/hatch.py
+++ apple_stats/hatch.py
@@ -20,9 +20,6 @@ def build_applestats(output_path: pathlib.PurePath) -> None:
         output_path: The path where to output the binary, relative to the
             workspace root.
     """
-    if platform.machine() != "arm64":
-        raise AppleStatsBuildError("This script only builds for arm64 architecture.")
-
     source_path = pathlib.Path("./apple_stats")
 
     cmd = [
