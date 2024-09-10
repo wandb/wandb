864b9593849498b49b69385ef3e7bf936c8d1cac
Dmitry Duev <dima@wandb.com>
2024-09-10 13:45:34 -0700

    chore: add GPUNvidia to noop_windows.go in system monitor

--- core/pkg/monitor/noop_linux.go
+++ core/pkg/monitor/noop_linux.go
@@ -4,7 +4,7 @@ package monitor
 
 import spb "github.com/wandb/wandb/core/pkg/service_go_proto"
 
-// GPUApple is a dummy implementation of the Asset interface for Apple GPU.
+// GPUApple is a dummy implementation of the Asset interface for Apple GPUs.
 type GPUApple struct {
 	name string
 }
