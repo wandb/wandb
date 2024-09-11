ca962e8c7b743bd0c44067e64ff1a299e2c557ae
elaina <ehodgkin9418@gmail.com>
2024-09-11 11:23:12 -0600

    wip

--- core/internal/api/credentials_test.go
+++ core/internal/api/credentials_test.go
@@ -1,16 +1,42 @@
 package api_test
 
-import "net/http"
+import (
+	"net/http"
+	"testing"
 
-type MockCredentialProvider struct {
-	CallCount int
-	ApplyFunc func(req *http.Request) error
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/wandb/wandb/core/internal/api"
+	wbsettings "github.com/wandb/wandb/core/internal/settings"
+	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
+	"google.golang.org/protobuf/types/known/wrapperspb"
+)
+
+func TestNewAPIKeyCredentialProvider(t *testing.T) {
+	settings := wbsettings.From(&spb.Settings{
+		ApiKey: &wrapperspb.StringValue{Value: "test-api-key"},
+	})
+	credentialProvider, err := api.NewCredentialProvider(settings)
+	require.NoError(t, err)
+
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	require.NoError(t, err)
+	err = credentialProvider.Apply(req)
+	require.NoError(t, err)
+
+	assert.Equal(t, "Basic YXBpOnRlc3QtYXBpLWtleQ==", req.Header.Get("Authorization"))
 }
 
-func (m *MockCredentialProvider) Apply(req *http.Request) error {
-	m.CallCount++
-	if m.ApplyFunc != nil {
-		return m.Apply(req)
-	}
-	return nil
+func TestNewAPIKeyCredentialProvider_NoAPIKey(t *testing.T) {
+	settings := wbsettings.From(&spb.Settings{})
+	_, err := api.NewCredentialProvider(settings)
+	assert.Error(t, err)
+}
+
+func TestNewIdentityTokenCredentialProvider(t *testing.T) {
+	settings := wbsettings.From(&spb.Settings{
+		IdentityTokenFile: &wrapperspb.StringValue{Value: "jwt.txt"},
+	})
+	_, err := api.NewCredentialProvider(settings)
+	assert.Error(t, err)
 }
