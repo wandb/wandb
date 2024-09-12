38e7b0e37cea8a063a4349dbfa6562dfe5554b01
elaina <ehodgkin9418@gmail.com>
2024-09-12 10:19:47 -0600

    wip

--- core/internal/api/credentials_test.go
+++ core/internal/api/credentials_test.go
@@ -1,8 +1,12 @@
 package api_test
 
 import (
+	"encoding/json"
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -33,10 +37,73 @@ func TestNewAPIKeyCredentialProvider_NoAPIKey(t *testing.T) {
 	assert.Error(t, err)
 }
 
-func TestNewAccessTokenCredentialProvider(t *testing.T) {
+func authServer() *httptest.Server {
+	server := httptest.NewServer(nil)
+	server.Config.Handler = http.HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			response := map[string]interface{}{
+				"access_token": "fake-token",
+				"expires_at":   time.Now().Add(time.Hour),
+			}
+
+			w.Header().Set("Content-Type", "application/json")
+
+			if err := json.NewEncoder(w).Encode(response); err != nil {
+				http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+				return
+			}
+		})
+	return server
+}
+
+func TestNewOAuth2CredentialProvider_CreatesNewToken(t *testing.T) {
+	// create identity token file
+	tokenFile, err := os.CreateTemp("", "jwt.txt")
+	require.NoError(t, err)
+	defer os.Remove(tokenFile.Name())
+
+	// write id token to file
+	_, err = tokenFile.Write([]byte("id-token"))
+	require.NoError(t, err)
+	require.NoError(t, tokenFile.Close())
+
+	// create credentials file
+	credsFile, err := os.CreateTemp("", "credentials.json")
+	require.NoError(t, err)
+	defer os.Remove(credsFile.Name())
+	//
+	//// write credentials json to file
+	_, err = credsFile.Write([]byte(`{"credentials": {}}`))
+	require.NoError(t, err)
+	require.NoError(t, credsFile.Close())
+
+	server := authServer()
+	defer server.Close()
+
 	settings := wbsettings.From(&spb.Settings{
-		IdentityTokenFile: &wrapperspb.StringValue{Value: "jwt.txt"},
+		BaseUrl:           &wrapperspb.StringValue{Value: server.URL},
+		IdentityTokenFile: &wrapperspb.StringValue{Value: tokenFile.Name()},
+		CredentialsFile:   &wrapperspb.StringValue{Value: credsFile.Name()},
 	})
-	_, err := api.NewCredentialProvider(settings)
-	assert.Error(t, err)
+	credentialProvider, err := api.NewCredentialProvider(settings)
+	require.NoError(t, err)
+
+	req, err := http.NewRequest("GET", "http://example.com", nil)
+	require.NoError(t, err)
+	err = credentialProvider.Apply(req)
+	require.NoError(t, err)
+
+	assert.Equal(t, "Bearer fake-token", req.Header.Get("Authorization"))
+}
+
+func TestNewOAuth2CredentialProvider_RefreshesToken(t *testing.T) {
+
+}
+
+func TestNewOAuth2CredentialProvider_CreatesCredentialsFile(t *testing.T) {
+
+}
+
+func TestNewOAuth2CredentialProvider_CreatesNewTokenForNewBaseURL(t *testing.T) {
+
 }
