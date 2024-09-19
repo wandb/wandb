868b33cb67c4479b6aadc0d543d4488fb69ba532
Abraham Leal <45460452+abraham-leal@users.noreply.github.com>
2024-09-18 19:08:43 -0500

    Add Andrew's old fixes to new client

--- wandb/apis/importers/wandb.py
+++ wandb/apis/importers/wandb.py
@@ -25,7 +25,7 @@ from wandb_workspaces.reports.v1 import Report
 import wandb
 from wandb.apis.public import ArtifactCollection, Run
 from wandb.apis.public.files import File
-from wandb.util import coalesce, remove_keys_with_none_values
+from wandb.util import coalesce, random_string, remove_keys_with_none_values
 
 from . import validation
 from .internals import internal
@@ -368,7 +368,9 @@ class WandbImporter:
         }
 
     def __repr__(self):
-        return f"<WandbImporter src={self.src_base_url}, dst={self.dst_base_url}>"  # pragma: no cover
+        return (
+            f"<WandbImporter src={self.src_base_url}, dst={self.dst_base_url}>"
+        )  # pragma: no cover
 
     def _import_run(
         self,
@@ -444,8 +446,8 @@ class WandbImporter:
         """
         entity = coalesce(namespace.entity, seq.entity)
         project = coalesce(namespace.project, seq.project)
-        art_type = f"{entity}/{project}/{seq.type_}"
-        art_name = seq.name
+        art_type = seq.type_
+        art_name = os.path.join(entity, project, seq.name)
 
         logger.info(
             f"Deleting collection {entity=}, {project=}, {art_type=}, {art_name=}"
@@ -1444,8 +1446,8 @@ class _DummyUser:
 class _DummyRun:
     entity: str = ""
     project: str = ""
-    run_id: str = RUN_DUMMY_PLACEHOLDER
-    id: str = RUN_DUMMY_PLACEHOLDER
+    run_id: str = RUN_DUMMY_PLACEHOLDER + random_string()
+    id: str = RUN_DUMMY_PLACEHOLDER + random_string()
     display_name: str = RUN_DUMMY_PLACEHOLDER
     notes: str = ""
     url: str = ""
@@ -1513,8 +1515,8 @@ def _get_run_or_dummy_from_art(art: Artifact, api=None):
     run = _DummyRun(
         entity=art.entity,
         project=art.project,
-        run_id=creator.get("name", RUN_DUMMY_PLACEHOLDER),
-        id=creator.get("name", RUN_DUMMY_PLACEHOLDER),
+        run_id=creator.get("name", RUN_DUMMY_PLACEHOLDER + random_string()),
+        id=creator.get("name", RUN_DUMMY_PLACEHOLDER + random_string()),
     )
     return run
 
