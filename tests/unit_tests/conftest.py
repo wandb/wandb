2b66b90cbac9ef69e596e161aaf25f7af0e0973a
tonyyli-wandb <tony.li@wandb.com>
2024-09-24 14:15:18 -0700

    test(artifacts): Increase default hypothesis deadline

--- tests/unit_tests/conftest.py
+++ tests/unit_tests/conftest.py
@@ -1,10 +1,15 @@
+from datetime import timedelta
 from queue import Queue
 from typing import Callable, Dict, Generator, List
 
 import pytest
 from hypothesis import settings
 
-settings.register_profile("ci", max_examples=10)
+settings.register_profile(
+    "ci",
+    max_examples=10,
+    deadline=timedelta(milliseconds=500),
+)
 settings.load_profile("ci")
 
 
