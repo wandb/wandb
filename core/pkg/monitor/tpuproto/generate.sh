e9318a7d0c4b1ea8e6cf2ef0c33a56fad0f3d0c5
Dmitry Duev <dima@wandb.com>
2024-09-27 13:46:34 -0700

    add proto files for grpc

--- core/pkg/monitor/tpuproto/generate.sh
+++ core/pkg/monitor/tpuproto/generate.sh
@@ -3,4 +3,6 @@ proto_path="${PWD%'core/pkg/monitor/tpuproto'}"
 protoc -I"$proto_path" \
     --go_out="$proto_path" \
     --go_opt=paths=source_relative \
+    --go-grpc_out="$proto_path" \
+    --go-grpc_opt=paths=source_relative \
     "$PWD/tpu_metric_service.proto"
