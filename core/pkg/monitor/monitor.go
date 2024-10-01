1fdc228e30936f79b0bc70345a177e25ca6c8ee0
Dmitry Duev <dima@wandb.com>
2024-10-01 00:51:53 -0700

    fix: tpu discovery

--- core/pkg/monitor/monitor.go
+++ core/pkg/monitor/monitor.go
@@ -141,6 +141,7 @@ func (sm *SystemMonitor) InitializeAssets(settings *spb.Settings) {
 		sm.assets = append(sm.assets, gpu)
 	}
 	if tpu := NewTPU(); tpu != nil {
+		fmt.Println("TPU", tpu)
 		sm.assets = append(sm.assets, tpu)
 	}
 	if slurm := NewSLURM(); slurm != nil {
