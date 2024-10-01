7b9c73a932947f4bbbc39cb1ba91d9b0715457ba
Estella Xin <estella.xin@wandb.com>
2024-10-01 17:46:08 -0400

    remove utils file

--- core/pkg/artifacts/utils.go
+++
@@ -1,9 +0,0 @@
-package artifacts
-
-import "strings"
-
-const RegistryProjectPrefix = "wandb-registry-"
-
-func IsArtifactRegistryProject(project string) bool {
-	return strings.HasPrefix(project, RegistryProjectPrefix)
-}
