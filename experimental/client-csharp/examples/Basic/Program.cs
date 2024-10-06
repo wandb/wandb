ff6271837f3f9b0c92d9718ff294a2f4a69d0605
Gerard Woods <gewoods@microsoft.com>
2024-10-06 12:38:18 -0700

    WandB Integration - Disposal Example

--- experimental/client-csharp/examples/Basic/Program.cs
+++ experimental/client-csharp/examples/Basic/Program.cs
@@ -11,12 +11,14 @@ class Program
 
     static async Task Main()
     {
+        string? apiKey = null;
+
         using (var session = new Session())
         {
             // Initialize a new run:
             var run1 = await session.Init(
                 settings: new Settings(
-                    // apiKey: "my-api",
+                    apiKey: apiKey,
                     // entity: "my-entity",
                     // displayName: "smart-capybara-42",
                     project: "csharp",
@@ -38,6 +40,7 @@ class Program
 
             // Finish the run without marking it as finished on the server:
             await run1.Finish(markFinished: false);
+            run1.Dispose();
 
             // Simulate waiting for the next batch of data:
             await Task.Delay(3000);
@@ -45,7 +48,7 @@ class Program
             // Resume run1:
             var run2 = await session.Init(
                 settings: new Settings(
-                    // apiKey: "my-api",
+                    apiKey: apiKey,
                     // entity: "my-entity",
                     project: "csharp",
                     resume: ResumeOption.Allow, // resume if exists, or create a new run
@@ -70,6 +73,7 @@ class Program
 
             // Finish the resumed run and mark it as finished on the server:
             await run2.Finish();
+            run2.Dispose();
         }
     }
 }
--- experimental/client-csharp/src/Wandb/Internal/TcpClient.cs
+++ experimental/client-csharp/src/Wandb/Internal/TcpClient.cs
@@ -253,7 +253,7 @@ namespace Wandb.Internal
         {
             _cancellationTokenSource.Cancel();
             _cancellationTokenSource.Dispose();
-            _receiveTask?.Wait(TimeSpan.FromSeconds(5));
+            _receiveTask?.Wait();
             _networkStream?.Close();
             _tcpClient.Close();
         }
