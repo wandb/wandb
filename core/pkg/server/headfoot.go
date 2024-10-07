7566f323dca163e853cb43dd399bc1fe747de04b
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-10-07 15:00:47 -0700

    chore: move client print function into the go client (#8565)

--- core/pkg/server/headfoot.go
+++ core/pkg/server/headfoot.go
@@ -1,3 +1,4 @@
+// This file should be depreacted and removed in the future, in favor of printing the footer in the client
 package server
 
 import (
@@ -25,38 +26,6 @@ const (
 
 func format(str string, color string) string {
 	return fmt.Sprintf("%v%v%v", color, str, resetFormat)
-}
-
-// This is used by the go wandb client to print the header and footer of the run
-func PrintHeadFoot(run *spb.RunRecord, settings *spb.Settings, footer bool) {
-	if run == nil {
-		return
-	}
-
-	appURL := strings.Replace(settings.GetBaseUrl().GetValue(), "//api.", "//", 1)
-	url := fmt.Sprintf("%v/%v/%v/runs/%v", appURL, run.Entity, run.Project, run.RunId)
-
-	fmt.Printf("%v: 🚀 View run %v at: %v\n",
-		format("wandb", colorBrightBlue),
-		format(run.DisplayName, colorYellow),
-		format(url, colorBlue),
-	)
-
-	if footer {
-		currentDir, err := os.Getwd()
-		if err != nil {
-			return
-		}
-		logDir := settings.GetLogDir().GetValue()
-		relLogDir, err := filepath.Rel(currentDir, logDir)
-		if err != nil {
-			return
-		}
-		fmt.Printf("%v: Find logs at: %v\n",
-			format("wandb", colorBrightBlue),
-			format(relLogDir, colorBrightMagenta),
-		)
-	}
 }
 
 func PrintFooterOnline(run *spb.RunRecord, settings *spb.Settings) {
