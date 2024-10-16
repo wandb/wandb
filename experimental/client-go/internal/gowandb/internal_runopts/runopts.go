c5273ae2503260f171131daf41ccb8a6a075f6f8
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-10-15 21:24:15 -0700

    chore: update go client dependencies (#8557)
    Co-authored-by: Dmitry Duev <dmitryduev@users.noreply.github.com>

--- experimental/client-go/internal/gowandb/internal_runopts/runopts.go
+++ experimental/client-go/internal/gowandb/internal_runopts/runopts.go
@@ -1,11 +1,11 @@
 package internal_runopts
 
 import (
-	"github.com/wandb/wandb/core/pkg/service"
+	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
 	"github.com/wandb/wandb/experimental/client-go/pkg/opts/runopts"
 )
 
-func WithTelemetry(telemetry *service.TelemetryRecord) runopts.RunOption {
+func WithTelemetry(telemetry *spb.TelemetryRecord) runopts.RunOption {
 	return func(p *runopts.RunParams) {
 		p.Telemetry = telemetry
 	}
