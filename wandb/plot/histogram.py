34a4e70c0cbfa4727e490ac55661908131b2aead
Katia Patkin <katia@wandb.com>
2024-10-18 18:01:24 -0700

    chore: update wandb.plot.histogram docs + typing

--- wandb/plot/histogram.py
+++ wandb/plot/histogram.py
@@ -1,39 +1,63 @@
-from typing import TYPE_CHECKING, Optional
+from __future__ import annotations
 
-from wandb.plot.viz import custom_chart
+from typing import TYPE_CHECKING
+
+from wandb.plot.viz import CustomChart
 
 if TYPE_CHECKING:
     import wandb
 
 
 def histogram(
-    table: "wandb.Table",
+    table: wandb.Table,
     value: str,
-    title: Optional[str] = None,
-    split_table: Optional[bool] = False,
+    title: str = "",
+    split_table: bool = False,
 ):
-    """Construct a histogram plot.
+    """Constructs a histogram chart from a W&B Table.
 
     Args:
-        table (wandb.Table): Table of data.
-        value (string): Name of column to use as data for bucketing.
-        title (string): Plot title.
-        split_table (bool): If True, adds "Custom Chart Tables/" to the key of the table so that it's logged in a different section.
+        table (wandb.Table): The W&B Table containing the data for the histogram.
+        value (str): The label for the bin axis (x-axis).
+        title (str): The title of the histogram plot.
+        split_table (bool): Whether to split the table into a different section
+            in the UI. Default is False.
 
     Returns:
-        A plot object, to be passed to wandb.log()
+        CustomChart: A custom chart object that can be logged to W&B. To log the
+            chart, pass it to `wandb.log()`.
 
     Example:
         ```
+        import math
+        import random
+        import wandb
+
+        # Generate random data
         data = [[i, random.random() + math.sin(i / 10)] for i in range(100)]
-        table = wandb.Table(data=data, columns=["step", "height"])
-        wandb.log({'histogram-plot1': wandb.plot.histogram(table, "height")})
+
+        # Create a W&B Table
+        table = wandb.Table(
+            data=data,
+            columns=["step", "height"],
+        )
+
+        # Create a histogram plot
+        histogram = wandb.plot.histogram(
+            table,
+            value="height",
+            title="My Histogram",
+        )
+
+        # Log the histogram plot to W&B
+        with wandb.init(...) as run:
+            run.log({'histogram-plot1': histogram})
         ```
     """
-    return custom_chart(
-        "wandb/histogram/v0",
-        table,
-        {"value": value},
-        {"title": title},
+    return CustomChart(
+        id="wandb/histogram/v0",
+        data=table,
+        fields={"value": value},
+        string_fields={"title": title},
         split_table=split_table,
     )
