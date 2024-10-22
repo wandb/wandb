18fd2b6a72a44f070b10dc75cb4f1568338d82e0
jacobromero <jacob.romero@wandb.com>
2024-10-22 09:33:07 -0700

    increase failure rate

--- tests/unit_tests/test_wandb_config.py
+++ tests/unit_tests/test_wandb_config.py
@@ -59,7 +59,7 @@ def test_locked_set_key(consolidated, config):
     import random
 
     random_number = random.randint(0, 100)
-    assert random_number < 70
+    assert random_number < 30
 
 
 def test_update(consolidated, config):
