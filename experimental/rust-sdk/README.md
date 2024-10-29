6ebda46caa6527bb3aedbe86f689cc6338bcc415
Katia Patkin <katia@wandb.com>
2024-10-29 14:08:51 -0700

    chore: create placeholder cargo crate

---
+++ experimental/rust-sdk/README.md
@@ -0,0 +1,1 @@
+Coming soon to crates.io
---
+++ experimental/rust-sdk/src/lib.rs
@@ -0,0 +1,12 @@
+/// Placeholder function
+pub fn placeholder() {
+    // Empty placeholder
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
