0e42ebc1e3a2f66c2748d195b225b353bbdaf391
Estella Xin <estella.xin@wandb.com>
2024-10-30 12:33:38 -0400

    feat(artifacts): display s3 path uri for artifact files (#8668)
    Co-authored-by: Tony Li <tony.li@wandb.com>

---
+++ tests/unit_tests/test_file/test_file_public_api.py
@@ -0,0 +1,65 @@
+from unittest import mock
+
+import wandb
+from pytest import fixture
+from wandb.apis.public.files import File
+
+
+@fixture
+def mock_client(mocker) -> mock.MagicMock:
+    return mocker.MagicMock()
+
+
+@fixture
+def termwarn_spy(mocker) -> mock.MagicMock:
+    return mocker.spy(wandb, "termwarn")
+
+
+def test_path_uri_s3_url(mock_client, termwarn_spy):
+    attrs = {
+        "directUrl": "https://my-bucket.s3.us-west-2.amazonaws.com/wandb-artifacts/my-artifact.txt"
+    }
+    file = File(mock_client, attrs)
+
+    assert file.path_uri == "s3://my-bucket/wandb-artifacts/my-artifact.txt"
+    termwarn_spy.assert_not_called()
+
+
+def test_path_uri_non_s3_url(mock_client, termwarn_spy):
+    attrs = {
+        "directUrl": "https://storage.googleapis.com/wandb-artifacts/my-artifact.txt"
+    }
+    file = File(mock_client, attrs)
+
+    assert file.path_uri == ""
+    termwarn_spy.assert_called_once_with(
+        "path_uri is only available for files stored in S3"
+    )
+
+
+def test_path_uri_invalid_url(mock_client, termwarn_spy):
+    attrs = {"directUrl": "not-a-valid-url"}
+    file = File(mock_client, attrs)
+
+    assert file.path_uri == ""
+    termwarn_spy.assert_called_once_with(
+        "path_uri is only available for files stored in S3"
+    )
+
+
+def test_path_uri_empty_url(mock_client, termwarn_spy):
+    attrs = {"directUrl": ""}
+    file = File(mock_client, attrs)
+
+    assert file.path_uri == ""
+    termwarn_spy.assert_called_once_with(
+        "path_uri is only available for files stored in S3"
+    )
+
+
+def test_path_uri_missing_direct_url(mock_client, termwarn_spy):
+    attrs = {}
+    file = File(mock_client, attrs)
+
+    assert file.path_uri == ""
+    termwarn_spy.assert_called_once_with("Unable to find direct_url of file")
