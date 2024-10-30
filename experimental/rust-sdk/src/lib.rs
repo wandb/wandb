6290bcac8d1788973af9a8b96c2551bdf7c7d91a
Katia Patkin <katia@wandb.com>
2024-10-29 19:09:49 -0700

    remove unused function

--- experimental/rust-sdk/src/lib.rs
+++ experimental/rust-sdk/src/lib.rs
@@ -1,5 +1,3 @@
-use std::sync::OnceLock;
-
 pub mod connection;
 pub mod launcher;
 pub mod printer;
@@ -7,19 +5,6 @@ pub mod run;
 pub mod session;
 pub mod settings;
 pub mod wandb_internal;
-
-/// Communication layer between user code and nexus
-
-pub fn get_core_version() -> &'static str {
-    static VERSION: OnceLock<String> = OnceLock::new();
-    VERSION.get_or_init(|| {
-        let version = env!("CARGO_PKG_VERSION");
-        version
-            .replace("-alpha.", "a")
-            .replace("-beta.", "b")
-            .replace("-rc.", "rc")
-    })
-}
 
 pub fn init(
     project: Option<String>,
