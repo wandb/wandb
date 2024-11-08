6f9d242e4d445cd668f6e3201f7457b395c39181
domphan-wandb <dominic.phan@wandb.com>
2024-11-08 13:27:42 -0800

    fix(sdk): ensure table artifact path keys are updated in offline mode (#8792)

--- wandb/sdk/wandb_settings.py
+++ wandb/sdk/wandb_settings.py
@@ -385,6 +385,7 @@ class SettingsData:
     _unsaved_keys: Sequence[str]
     _windows: bool
     _show_operation_stats: bool
+    allow_offline_artifacts: bool
     allow_val_change: bool
     anonymous: str
     api_key: str
@@ -776,6 +777,7 @@ class Settings(SettingsData):
                 "auto_hook": True,
             },
             _show_operation_stats={"preprocessor": _str_as_bool},
+            allow_offline_artifacts={"value": "True", "preprocessor": _str_as_bool},
             anonymous={"validator": self._validate_anonymous},
             api_key={"validator": self._validate_api_key},
             base_url={
