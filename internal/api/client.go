4918126b010711422b9c05237d87950510a9624c
Justin Brooks <justin@wandb.com>
2024-11-13 11:58:02 -0500

    remove generate

--- internal/api/client.go
+++ internal/api/client.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 )
 
-//go:generate oapi-codegen -config openapi.client.yaml https://raw.githubusercontent.com/ctrlplanedev/ctrlplane/refs/heads/main/openapi.v1.json
-
 func NewAPIKeyClientWithResponses(server string, apiKey string) (*ClientWithResponses, error) {
 	server = strings.TrimSuffix(server, "/")
 	server = strings.TrimSuffix(server, "/api")
