a26e3d95e44042abd772ae95324a3d7628cd16cc
Katia Patkin <katia@wandb.com>
2024-11-18 19:41:29 -0800

    chore: remove wandb_init fixutre from test_console

--- tests/system_tests/test_core/test_console.py
+++ tests/system_tests/test_core/test_console.py
@@ -5,7 +5,6 @@ from unittest import mock
 
 import numpy as np
 import pytest
-import tqdm
 import wandb
 import wandb.sdk.lib.redirect
 import wandb.util
@@ -19,43 +18,44 @@ if os.name != "nt":
 
 
 @pytest.mark.parametrize("console", console_modes)
-def test_run_with_console_redirect(wandb_init, capfd, console):
+def test_run_with_console_redirect(user, capfd, console):
+    tqdm = pytest.importorskip("tqdm")
+
     with capfd.disabled():
-        run = wandb_init(settings={"console": console})
+        with wandb.init(settings={"console": console}):
+            print(np.random.randint(64, size=(40, 40, 40, 40)))
 
-        print(np.random.randint(64, size=(40, 40, 40, 40)))
+            for _ in tqdm.tqdm(range(100)):
+                time.sleep(0.02)
 
-        for _ in tqdm.tqdm(range(100)):
-            time.sleep(0.02)
-
-        print("\n" * 1000)
-        print("---------------")
-        time.sleep(1)
-        run.finish()
+            print("\n" * 1000)
+            print("---------------")
+            time.sleep(1)
 
 
 @pytest.mark.parametrize("console", console_modes)
-def test_offline_compression(wandb_init, capfd, console):
+def test_offline_compression(user, capfd, console):
+    tqdm = pytest.importorskip("tqdm")
+
     # map to old style wrap implementation until test is refactored
     if console == "wrap":
         console = "wrap_emu"
     with capfd.disabled():
-        run = wandb_init(settings={"console": console, "mode": "offline"})
-        run_dir, run_id = run.dir, run.id
+        with wandb.init(settings={"console": console, "mode": "offline"}) as run:
+            run_dir, run_id = run.dir, run.id
 
-        for _ in tqdm.tqdm(range(100), ncols=139, ascii=" 123456789#"):
-            time.sleep(0.05)
+            for _ in tqdm.tqdm(range(100), ncols=139, ascii=" 123456789#"):
+                time.sleep(0.05)
 
-        print("\n" * 1000)
+            print("\n" * 1000)
 
-        print("QWERT")
-        print("YUIOP")
-        print("12345")
+            print("QWERT")
+            print("YUIOP")
+            print("12345")
 
-        print("\x1b[A\r\x1b[J\x1b[A\r\x1b[1J")
+            print("\x1b[A\r\x1b[J\x1b[A\r\x1b[1J")
 
-        time.sleep(2)
-        run.finish()
+            time.sleep(2)
 
         binary_log_file = (
             os.path.join(os.path.dirname(run_dir), "run-" + run_id) + ".wandb"
@@ -83,7 +83,7 @@ def test_offline_compression(wandb_init, capfd, console):
 @pytest.mark.parametrize("console", console_modes)
 @pytest.mark.parametrize("numpy", [True, False])
 @pytest.mark.timeout(300)
-def test_very_long_output(wandb_init, capfd, console, numpy):
+def test_very_long_output(user, capfd, console, numpy):
     # https://wandb.atlassian.net/browse/WB-5437
     with capfd.disabled():
         with mock.patch.object(
@@ -91,19 +91,18 @@ def test_very_long_output(wandb_init, capfd, console, numpy):
             "np",
             wandb.sdk.lib.redirect._Numpy() if not numpy else np,
         ):
-            run = wandb_init(
+            with wandb.init(
                 settings={
                     "console": console,
                     "mode": "offline",
                     "run_id": runid.generate_id(),
                 }
-            )
-            run_dir, run_id = run.dir, run.id
-            print("LOG" * 1000000)
-            print("\x1b[31m\x1b[40m\x1b[1mHello\x01\x1b[22m\x1b[39m" * 100)
-            print("===finish===")
-            time.sleep(5)
-            run.finish()
+            ) as run:
+                run_dir, run_id = run.dir, run.id
+                print("LOG" * 1000000)
+                print("\x1b[31m\x1b[40m\x1b[1mHello\x01\x1b[22m\x1b[39m" * 100)
+                print("===finish===")
+                time.sleep(5)
 
             binary_log_file = (
                 os.path.join(os.path.dirname(run_dir), "run-" + run_id) + ".wandb"
@@ -120,16 +119,16 @@ def test_very_long_output(wandb_init, capfd, console, numpy):
 
 
 @pytest.mark.parametrize("console", console_modes)
-def test_no_numpy(wandb_init, capfd, console):
+def test_no_numpy(user, capfd, console):
     with capfd.disabled():
         with mock.patch.object(
             wandb.wandb_sdk.lib.redirect,
             "np",
             wandb.sdk.lib.redirect._Numpy(),
         ):
-            run = wandb_init(settings={"console": console})
-            print("\x1b[31m\x1b[40m\x1b[1mHello\x01\x1b[22m\x1b[39m")
-            run.finish()
+            with wandb.init(settings={"console": console}) as run:
+                print("\x1b[31m\x1b[40m\x1b[1mHello\x01\x1b[22m\x1b[39m")
+
             binary_log_file = (
                 os.path.join(os.path.dirname(run.dir), "run-" + run.id) + ".wandb"
             )
@@ -141,14 +140,13 @@ def test_no_numpy(wandb_init, capfd, console):
 
 
 @pytest.mark.parametrize("console", console_modes)
-def test_memory_leak2(wandb_init, capfd, console):
+def test_memory_leak2(user, capfd, console):
     # map to old style wrap implementation until test is refactored
     if console == "wrap":
         console = "wrap_emu"
     with capfd.disabled():
-        run = wandb_init(settings={"console": console})
-        for _ in range(1000):
-            print("ABCDEFGH")
-        time.sleep(3)
-        assert len(run._out_redir._emulator.buffer) < 1000
-        run.finish()
+        with wandb.init(settings={"console": console}) as run:
+            for _ in range(1000):
+                print("ABCDEFGH")
+            time.sleep(3)
+            assert len(run._out_redir._emulator.buffer) < 1000
