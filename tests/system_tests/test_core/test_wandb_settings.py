833380768ab48327e8d6e47c7319472d28fd59b2
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-11-19 15:36:37 -0800

    chore: remove wandb_init fixture from test_wandb_settings (#8918)

--- tests/system_tests/test_core/test_wandb_settings.py
+++ tests/system_tests/test_core/test_wandb_settings.py
@@ -13,80 +13,73 @@ from wandb import env
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_sync_dir(wandb_init):
-    run = wandb_init(settings={"mode": "offline"})
-    print(run._settings.sync_dir)
-    assert run._settings.sync_dir == os.path.realpath(
-        os.path.join(".", "wandb", "latest-run")
-    )
-    run.finish()
+def test_sync_dir(user):
+    with wandb.init(settings={"mode": "offline"}) as run:
+        print(run._settings.sync_dir)
+        assert run._settings.sync_dir == os.path.realpath(
+            os.path.join(".", "wandb", "latest-run")
+        )
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_sync_file(wandb_init):
-    run = wandb_init(mode="offline")
-    assert run._settings.sync_file == os.path.realpath(
-        os.path.join(".", "wandb", "latest-run", f"run-{run.id}.wandb")
-    )
-    run.finish()
+def test_sync_file(user):
+    with wandb.init(mode="offline") as run:
+        assert run._settings.sync_file == os.path.realpath(
+            os.path.join(".", "wandb", "latest-run", f"run-{run.id}.wandb")
+        )
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_files_dir(wandb_init):
-    run = wandb_init(mode="offline")
-    assert run._settings.files_dir == os.path.realpath(
-        os.path.join(".", "wandb", "latest-run", "files")
-    )
-    run.finish()
+def test_files_dir(user):
+    with wandb.init(mode="offline") as run:
+        assert run._settings.files_dir == os.path.realpath(
+            os.path.join(".", "wandb", "latest-run", "files")
+        )
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_tmp_code_dir(wandb_init):
-    run = wandb_init(mode="offline")
-    assert run._settings._tmp_code_dir == os.path.realpath(
-        os.path.join(".", "wandb", "latest-run", "tmp", "code")
-    )
-    run.finish()
+def test_tmp_code_dir(user):
+    with wandb.init(mode="offline") as run:
+        assert run._settings._tmp_code_dir == os.path.realpath(
+            os.path.join(".", "wandb", "latest-run", "tmp", "code")
+        )
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_log_symlink_user(wandb_init):
-    run = wandb_init(settings=dict(mode="offline"))
-    assert os.path.realpath(run._settings.log_symlink_user) == os.path.abspath(
-        run._settings.log_user
-    )
-    run.finish()
+def test_log_symlink_user(user):
+    with wandb.init(settings=dict(mode="offline")) as run:
+        assert os.path.realpath(run._settings.log_symlink_user) == os.path.abspath(
+            run._settings.log_user
+        )
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_log_symlink_internal(wandb_init):
-    run = wandb_init(mode="offline")
-    assert os.path.realpath(run._settings.log_symlink_internal) == os.path.abspath(
-        run._settings.log_internal
-    )
-    run.finish()
+def test_log_symlink_internal(user):
+    with wandb.init(mode="offline") as run:
+        assert os.path.realpath(run._settings.log_symlink_internal) == os.path.abspath(
+            run._settings.log_internal
+        )
 
 
 @pytest.mark.skipif(
     platform.system() == "Windows", reason="backend crashes on Windows in CI"
 )
-def test_sync_symlink_latest(wandb_init):
-    run = wandb_init(mode="offline")
-    time_tag = run._settings._start_datetime
-    assert os.path.realpath(run._settings.sync_symlink_latest) == os.path.abspath(
-        os.path.join(".", "wandb", f"offline-run-{time_tag}-{run.id}")
-    )
-    run.finish()
+def test_sync_symlink_latest(user):
+    with wandb.init(mode="offline") as run:
+        time_tag = run._settings._start_datetime
+        assert os.path.realpath(run._settings.sync_symlink_latest) == os.path.abspath(
+            os.path.join(".", "wandb", f"offline-run-{time_tag}-{run.id}")
+        )
 
 
 def test_manual_git_run_metadata_from_settings(wandb_backend_spy):
