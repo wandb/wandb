193b95829a82325ef24827955ca64ae5ce909b91
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-11-27 16:31:33 -0800

    chore: rename client-go to go-sdk (#8961)

--- experimental/go-sdk/pkg/gowandb/run.go
+++ experimental/go-sdk/pkg/gowandb/run.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"sync"
 
+	"github.com/wandb/wandb/core/pkg/service"
 	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
 
 	"github.com/wandb/wandb/experimental/client-go/pkg/opts/runopts"
