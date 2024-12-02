16d06eb10c204df2508d0b869379169f464c422b
waiip <waiip@Man-Wai-Ip-M4GXRH949Y.local>
2024-12-02 12:54:17 -0800

    fixing spaces

--- experimental/perf/scripts/test_case_helper.py
+++ experimental/perf/scripts/test_case_helper.py
@@ -15,7 +15,7 @@ def bench_log(root_folder: str, loop_count: int, step: int):
     step = 2000
     sort_key = 1
 
-    for loop in range(1, loop_count+1):
+    for loop in range(1, loop_count + 1):
         run_experiment_helper(loop, step, mc, root_folder, sort_key)
         sort_key += 1
 
@@ -47,7 +47,9 @@ def bench_log_scale_metric(root_folder: str, list_of_metric_count: list[int]):
         sort_key += 1
 
 
-def run_experiment_helper(loop, step, mc, root_folder, sort_key, output_file="results.json"):
+def run_experiment_helper(
+    loop, step, mc, root_folder, sort_key, output_file="results.json"
+):
     """A helper to do the standard perf test setup.
 
     1) create a folder for this particular load test iteration
@@ -55,7 +57,9 @@ def run_experiment_helper(loop, step, mc, root_folder, sort_key, output_file="re
     3) run the actual load tests
     4) end the resource metrics and compute the summary stats
     """
-    log_folder = os.path.join(root_folder, f"loop{loop}_step{step}_metriccount{mc}_{sort_key}")
+    log_folder = os.path.join(
+        root_folder, f"loop{loop}_step{step}_metriccount{mc}_{sort_key}"
+    )
 
     os.makedirs(log_folder, exist_ok=True)
 
