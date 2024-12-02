54613afe9dd9650118a3cf6e436d6b7616823278
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-12-02 11:29:08 -0800

    chore: explicitly specify the name of the fields in the metadata info (#8985)

--- wandb/proto/v5/wandb_internal_pb2.pyi
+++ wandb/proto/v5/wandb_internal_pb2.pyi
@@ -4027,8 +4027,8 @@ class MetadataRequest(google.protobuf.message.Message):
 
     OS_FIELD_NUMBER: builtins.int
     PYTHON_FIELD_NUMBER: builtins.int
-    HEARTBEATAT_FIELD_NUMBER: builtins.int
-    STARTEDAT_FIELD_NUMBER: builtins.int
+    HEARTBEAT_AT_FIELD_NUMBER: builtins.int
+    STARTED_AT_FIELD_NUMBER: builtins.int
     DOCKER_FIELD_NUMBER: builtins.int
     CUDA_FIELD_NUMBER: builtins.int
     ARGS_FIELD_NUMBER: builtins.int
@@ -4077,9 +4077,9 @@ class MetadataRequest(google.protobuf.message.Message):
     gpu_count: builtins.int
     cuda_version: builtins.str
     @property
-    def heartbeatAt(self) -> google.protobuf.timestamp_pb2.Timestamp: ...
+    def heartbeat_at(self) -> google.protobuf.timestamp_pb2.Timestamp: ...
     @property
-    def startedAt(self) -> google.protobuf.timestamp_pb2.Timestamp: ...
+    def started_at(self) -> google.protobuf.timestamp_pb2.Timestamp: ...
     @property
     def args(self) -> google.protobuf.internal.containers.RepeatedScalarFieldContainer[builtins.str]: ...
     @property
@@ -4107,8 +4107,8 @@ class MetadataRequest(google.protobuf.message.Message):
         *,
         os: builtins.str = ...,
         python: builtins.str = ...,
-        heartbeatAt: google.protobuf.timestamp_pb2.Timestamp | None = ...,
-        startedAt: google.protobuf.timestamp_pb2.Timestamp | None = ...,
+        heartbeat_at: google.protobuf.timestamp_pb2.Timestamp | None = ...,
+        started_at: google.protobuf.timestamp_pb2.Timestamp | None = ...,
         docker: builtins.str = ...,
         cuda: builtins.str = ...,
         args: collections.abc.Iterable[builtins.str] | None = ...,
@@ -4138,8 +4138,8 @@ class MetadataRequest(google.protobuf.message.Message):
         trainium: global___TrainiumInfo | None = ...,
         tpu: global___TPUInfo | None = ...,
     ) -> None: ...
-    def HasField(self, field_name: typing.Literal["apple", b"apple", "cpu", b"cpu", "git", b"git", "heartbeatAt", b"heartbeatAt", "memory", b"memory", "startedAt", b"startedAt", "tpu", b"tpu", "trainium", b"trainium"]) -> builtins.bool: ...
-    def ClearField(self, field_name: typing.Literal["apple", b"apple", "args", b"args", "code_path", b"code_path", "code_path_local", b"code_path_local", "colab", b"colab", "cpu", b"cpu", "cpu_count", b"cpu_count", "cpu_count_logical", b"cpu_count_logical", "cuda", b"cuda", "cuda_version", b"cuda_version", "disk", b"disk", "docker", b"docker", "email", b"email", "executable", b"executable", "git", b"git", "gpu_amd", b"gpu_amd", "gpu_count", b"gpu_count", "gpu_nvidia", b"gpu_nvidia", "gpu_type", b"gpu_type", "heartbeatAt", b"heartbeatAt", "host", b"host", "memory", b"memory", "os", b"os", "program", b"program", "python", b"python", "root", b"root", "slurm", b"slurm", "startedAt", b"startedAt", "state", b"state", "tpu", b"tpu", "trainium", b"trainium", "username", b"username"]) -> None: ...
+    def HasField(self, field_name: typing.Literal["apple", b"apple", "cpu", b"cpu", "git", b"git", "heartbeat_at", b"heartbeat_at", "memory", b"memory", "started_at", b"started_at", "tpu", b"tpu", "trainium", b"trainium"]) -> builtins.bool: ...
+    def ClearField(self, field_name: typing.Literal["apple", b"apple", "args", b"args", "code_path", b"code_path", "code_path_local", b"code_path_local", "colab", b"colab", "cpu", b"cpu", "cpu_count", b"cpu_count", "cpu_count_logical", b"cpu_count_logical", "cuda", b"cuda", "cuda_version", b"cuda_version", "disk", b"disk", "docker", b"docker", "email", b"email", "executable", b"executable", "git", b"git", "gpu_amd", b"gpu_amd", "gpu_count", b"gpu_count", "gpu_nvidia", b"gpu_nvidia", "gpu_type", b"gpu_type", "heartbeat_at", b"heartbeat_at", "host", b"host", "memory", b"memory", "os", b"os", "program", b"program", "python", b"python", "root", b"root", "slurm", b"slurm", "started_at", b"started_at", "state", b"state", "tpu", b"tpu", "trainium", b"trainium", "username", b"username"]) -> None: ...
 
 global___MetadataRequest = MetadataRequest
 
