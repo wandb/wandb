f9a1a99fc017e2651a91068470c7c0fc4ea98a57
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2024-12-06 15:30:21 -0800

    object_3D.py

--- wandb/sdk/data_types/object_3d.py
+++ wandb/sdk/data_types/object_3d.py
@@ -191,9 +191,10 @@ class Object3D(BatchableMedia):
             Object3D can be initialized from a file or a numpy array.
 
             You can pass a path to a file or an io object and a file_type
-            which must be one of SUPPORTED_TYPES
+            which must be one of `SUPPORTED_TYPES`
 
     The shape of the numpy array must be one of either:
+
     ```
     [[x y z],       ...] nx3
     [[x y z c],     ...] nx4 where c is a category with supported range [1, 14]
@@ -358,6 +359,7 @@ class Object3D(BatchableMedia):
 
 
         The shape of the numpy array must be one of either:
+
         ```
         [[x y z],       ...]  # nx3.
         [[x y z c],     ...]  # nx4 where c is a category with supported range [1, 14].
