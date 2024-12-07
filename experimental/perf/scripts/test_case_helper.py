85c85549d3c0830cd3e98425cbfb2e251ed96641
wai-ip <man.waiip@wandb.com>
2024-12-06 17:01:21 -0800

    add multi wandbs perf test

--- experimental/perf/scripts/test_case_helper.py
+++ experimental/perf/scripts/test_case_helper.py
@@ -1,11 +1,16 @@
+import multiprocessing as mp
 from pathlib import Path
 
 from bench_run_log import run_experiment
 from process_sar_helper import process_sar_files
-from setup_helper import capture_sar_metrics
+from setup_helper import capture_sar_metrics, get_logger
+
+logger = get_logger(__name__)
 
 
-def bench_log(root_folder: str, loop_count: int, step: int = 2000) -> None:
+def bench_log(
+    root_folder: str, loop_count: int, step: int, num_of_parallel_runs: int
+) -> None:
     """Runs a set of load tests with the same parameter.
 
     The goal is to measure the performance of a baseline test with reliable
@@ -13,13 +18,16 @@ def bench_log(root_folder: str, loop_count: int, step: int = 2000) -> None:
 
     Args:
         root_folder (str): The root directory where results will be stored.
-        loop_count (int): Number of iterations to test
-        step (int, optional): Number of step in each iteration. Defaults to 2000.
+        loop_count (int): Number of iterations to test.
+        step (int): Number of step in each iteration.
+        num_of_parallel_runs (int): Number of parallel experiments to run.
     """
     mc = 100
 
     for sort_key, loop in enumerate(range(1, loop_count + 1), start=1):
-        run_experiment_helper(loop, step, mc, root_folder, sort_key)
+        run_parallel_experiments_helper(
+            loop, step, mc, root_folder, sort_key, num_of_parallel_runs
+        )
 
 
 def bench_log_scale_step(root_folder: str, list_of_steps: list[int]) -> None:
@@ -85,8 +93,62 @@ def run_experiment_helper(
 
     log_folder.mkdir(parents=True, exist_ok=True)
 
+    # This starts background sar process, doesn't block main thread
     capture_sar_metrics(log_folder)
 
     run_experiment(loop, step, mc, output_file=str(log_folder / output_file))
+
+    process_sar_files(log_folder)
+
+
+def run_parallel_experiments_helper(
+    loop: int,
+    step: int,
+    mc: int,
+    root_folder: str,
+    sort_key: int,
+    num_of_processes: int,
+) -> None:
+    """A helper to start multiple wandbs in parallel by calling the run_experiment().
+
+    Args:
+        loop (int): The current loop iteration number.
+        step (int): Number of steps within the loop.
+        mc (int): Number of metrics to log per step.
+        root_folder (str): The root directory where results will be stored.
+        sort_key (str): A key used for sorting the test data (for naming the folder).
+        num_of_processes (int): Number of parallel wandb runs to start.
+
+    Returns:
+        None: This function does not return any value. It performs file and metric operations.
+
+    """
+    log_folder = Path(root_folder) / f"loop{loop}_step{step}_metriccount{mc}_{sort_key}"
+
+    log_folder.mkdir(parents=True, exist_ok=True)
+
+    capture_sar_metrics(log_folder)
+
+    processes = []
+    print(f"num_of_processes: {num_of_processes}, type: {type(num_of_processes)}")
+    for iter in range(1, num_of_processes + 1):
+        p = mp.Process(
+            target=run_experiment,
+            kwargs=dict(
+                loop_count=loop,
+                step_count=step,
+                metric_count=mc,
+                output_file=str(log_folder / f"results.{iter}.json"),
+            ),
+        )
+        p.start()
+        logger.info(f"The {iter}-th process (pid: {p.pid}) has started.")
+        processes.append(p)
+
+    # now wait for all processes to finish and exit
+    for proc in processes:
+        proc.join()
+
+    logger.info("All experiements have finished.")
 
     process_sar_files(log_folder)
