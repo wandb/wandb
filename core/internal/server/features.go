44ef915add33ac7268a5082e8f2041182b9b52f3
jacobromero <jacob.romero@wandb.com>
2024-12-12 18:56:14 -0800

    add server features file for providing server capabilities and features

---
+++ core/internal/server/features.go
@@ -0,0 +1,89 @@
+package server
+
+import (
+	"context"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/wandb/wandb/core/internal/gql"
+	"github.com/wandb/wandb/core/internal/observability"
+	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
+)
+
+// ServerFeatures is responsible for providing the capabilities of a server
+// Features and capabilities are retrieved from the server via a GraphQL query
+type ServerFeatures struct {
+	Features map[string]ServerFeature
+}
+
+type ServerFeature struct {
+	DefaultValue string
+	Description  string
+	Enabled      bool
+	Name         string
+}
+
+func NewServerFeatures(
+	graphqlClient graphql.Client,
+	ctx context.Context,
+) *ServerFeatures {
+	if graphqlClient == nil {
+		return &ServerFeatures{
+			Features: make(map[string]ServerFeature),
+		}
+	}
+
+	features := map[string]ServerFeature{}
+
+	// Query the server for the features provided by the server
+	resp, err := gql.ServerFeaturesQuery(
+		ctx,
+		graphqlClient,
+		gql.RampIDTypeUsername,
+	)
+	if err == nil {
+		for _, f := range resp.ServerInfo.FeatureFlags {
+			features[f.RampKey] = ServerFeature{
+				Name:    f.RampKey,
+				Enabled: f.IsEnabled,
+			}
+		}
+	}
+
+	return &ServerFeatures{
+		Features: features,
+	}
+}
+
+func (sf *ServerFeatures) GetFeature(name string) *spb.ServerFeatureItem {
+	// Default value, if feature is not in map
+	serverFeature := &spb.ServerFeatureItem{
+		Name:    name,
+		Enabled: false,
+	}
+
+	feature, ok := sf.Features[name]
+	if ok {
+		serverFeature = &spb.ServerFeatureItem{
+			Name:    feature.Name,
+			Enabled: feature.Enabled,
+		}
+	}
+	return serverFeature
+}
+
+func (sf *ServerFeatures) Get(
+	featureNames []string,
+	logger *observability.CoreLogger,
+) *spb.ServerFeatureResponse {
+	// Get feature from map or default value
+	features := map[string]*spb.ServerFeatureItem{}
+	for _, name := range featureNames {
+		// Default value, if feature is not in map
+		serverFeature := sf.GetFeature(name)
+		features[name] = serverFeature
+	}
+
+	return &spb.ServerFeatureResponse{
+		Features: features,
+	}
+}
