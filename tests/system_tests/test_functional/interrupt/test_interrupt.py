24d35294b3b1a34fd40bcd566ff96b6d02192ef3
Timofey Peshin <timoffex@gmail.com>
2024-12-16 14:01:50 -0800

    add test
    Created using spr 1.3.5

---
+++ tests/system_tests/test_functional/interrupt/test_interrupt.py
@@ -0,0 +1,14 @@
+import os
+import pathlib
+import subprocess
+
+
+def test_run_stops_if_asked(wandb_backend_spy):
+    gql = wandb_backend_spy.gql
+    wandb_backend_spy.stub_gql(
+        gql.Matcher(operation="RunStoppedStatus"),
+        gql.Constant(content={"data": {"project": {"run": {"stopped": True}}}}),
+    )
+
+    script = pathlib.Path(__file__).parent / "pass_if_interrupted.py"
+    subprocess.check_call(["python", str(script)], env=os.environ)
