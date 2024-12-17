13c367de5ee283fa2af892a7ba8029e57c53bdb2
Timofey Peshin <timoffex@gmail.com>
2024-12-17 14:54:43 -0800

    try changing pyproject instead
    Created using spr 1.3.5

--- tests/system_tests/test_functional/conftest.py
+++ tests/system_tests/test_functional/conftest.py
@@ -1,8 +1,5 @@
-import os
 import pathlib
 import runpy
-import subprocess
-import sys
 from typing import Any
 from unittest import mock
 
@@ -11,44 +8,19 @@ import pytest
 
 @pytest.fixture
 def execute_script():
-    """Execute a script in the current interpreter using runpy.
+    # Define a helper function that will take in the script path and command-line arguments
+    def helper(train_script_path: pathlib.Path, *args: Any) -> None:
+        # sys_argv simulates the command-line arguments.
+        sys_argv = [str(train_script_path)] + list(args)
 
-    This fixture is a function that accepts the script's path and a list of
-    arguments to pass to it, as if invoking it on the command line. The script
-    runs with `__name__ == "__main__"`.
-    """
-
-    def helper(script_path: pathlib.Path, *args: Any) -> None:
-        sys_argv = [str(script_path)] + list(args)
-
+        # Patch sys.argv to replace the real command-line arguments with our simulated ones.
+        # This ensures that the target script sees the provided arguments when run.
         with mock.patch("sys.argv", sys_argv):
+            # Execute the target script at `train_script_path` in a similar way
+            # as if it was run directly from the command line.
             # runpy.run_path executes the script with the specified `run_name`.
-            # By using `run_name="__main__"`, we simulate the script being
-            # executed as if it were the main entry point, like running
-            # `python script.py`.
-            runpy.run_path(str(script_path), run_name="__main__")
-
-    return helper
-
-
-@pytest.fixture
-def check_call_script():
-    """Run a Python script in a new interpreter.
-
-    This is a replacement for `subprocess.check_call(["python", ...])`.
-    It calls the script and raises an error if its exit code is nonzero.
-
-    The PYTHONPATH is set to the current `sys.path`, so that the script imports
-    the same modules as the current interpreter.
-    """
-
-    def helper(script_path: pathlib.Path) -> None:
-        subprocess.check_call(
-            ["python", str(script_path)],
-            env={
-                **os.environ,
-                "PYTHONPATH": os.pathsep.join(sys.path),
-            },
-        )
+            # By using `run_name="__main__"`, we simulate the script being executed
+            # as if it were the main entry point, like running `python script.py`.
+            runpy.run_path(str(train_script_path), run_name="__main__")
 
     return helper
