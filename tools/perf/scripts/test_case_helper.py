33df5f3e81d4ae07781b3fd066149edcfc7fcece
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2024-12-18 20:22:43 -0800

    refactor: modify the perf code to be more encapsulated (#9100)
    Co-authored-by: wai-ip <man.waiip@wandb.com>

--- tools/perf/scripts/test_case_helper.py
+++ tools/perf/scripts/test_case_helper.py
@@ -1,31 +1,31 @@
-import multiprocessing as mp
 import time
 from pathlib import Path
+from typing import Literal
 
-import wandb
-from bench_run_log import run_experiment
-from process_sar_helper import process_sar_files
-from setup_helper import capture_sar_metrics, get_logger
+from .bench_run_log import run_parallel_experiment
+from .process_sar_helper import capture_sar_metrics, process_sar_files
+from .setup_helper import get_logger
 
 logger = get_logger(__name__)
 
 
 def run_perf_tests(
     loop_count: int,
-    step_count: list[int],
-    metric_count: list[int],
+    num_steps_options: list[int],
+    num_metrics_options: list[int],
     root_folder: str,
-    num_of_processes: int,
-    data_type: str = "scalar",
+    num_processes: int,
+    data_type: Literal["scalar", "audio"] = "scalar",
+    metric_key_size: int = 10,
 ) -> None:
     """A helper to start a series of tests.
 
     Args:
         loop_count (int): The # of iterations to test repeatedly.
-        step_count (list[int]): A list of step counts for each iteration.
-        metric_count (list[int]): A list of metric counts for each iteration.
+        num_steps_options: A list of number of steps to iterate over.
+        num_metrics_options: A list of number of metrics to iterate over.
         root_folder (str): The root directory where results will be stored.
-        num_of_processes (int): Number of parallel wandb runs to start.
+        num_processes (int): Number of parallel wandb runs to start.
         data_type (str): Wandb data type for the test payload
 
     Returns:
@@ -34,75 +34,38 @@ def run_perf_tests(
     """
     sort_key = 1
     for _ in range(loop_count):
-        for step in step_count:
-            for mc in metric_count:
+        for num_steps in num_steps_options:
+            for num_metrics in num_metrics_options:
                 logger.info(
                     "##############################################################"
                 )
                 logger.info(f"The {sort_key}-th run:")
-                logger.info(f"  # of steps in each run: {step_count}")
-                logger.info(f"  # of metrics in each step: {metric_count}")
-                logger.info(f"  # of wandb processes: {num_of_processes}")
-                logger.info(f"  data type of the payload: {data_type}")
-                run_parallel_experiments_helper(
-                    step, mc, root_folder, sort_key, num_of_processes, data_type
+                logger.info(f"\tnumber of steps in each run: {num_steps}")
+                logger.info(f"\tnumber of metrics in each step: {num_metrics}")
+                logger.info(f"\tnumber of wandb processes: {num_processes}")
+                logger.info(f"\tdata type of the payload: {data_type}")
+
+                log_folder = (
+                    Path(root_folder)
+                    / f"step{num_steps}_metric_count{num_metrics}_datatype{data_type}_{sort_key}"
                 )
+
+                log_folder.mkdir(parents=True, exist_ok=True)
+
+                capture_sar_metrics(log_folder)
+
+                run_parallel_experiment(
+                    num_processes=num_processes,
+                    num_steps=num_steps,
+                    num_metrics=num_metrics,
+                    metric_key_size=metric_key_size,
+                    data_type=data_type,
+                    log_folder=log_folder,
+                )
+
+                logger.info("All experiements have finished.")
+
+                process_sar_files(log_folder)
+
                 sort_key += 1
                 time.sleep(10)  # sleep some time between run to let it finish flushing
-
-
-def run_parallel_experiments_helper(
-    step: int,
-    mc: int,
-    root_folder: str,
-    sort_key: int,
-    num_of_processes: int,
-    data_type: str = "scalar",
-) -> None:
-    """A helper to start multiple wandbs in parallel by calling the run_experiment().
-
-    Args:
-        step (int): Number of steps within the loop.
-        mc (int): Number of metrics to log per step.
-        root_folder (str): The root directory where results will be stored.
-        sort_key (str): A key used for sorting the test data (for naming the folder).
-        num_of_processes (int): Number of parallel wandb runs to start.
-        data_type (str): Wandb data type for the test payload
-
-    Returns:
-        None: This function does not return any value. It performs file and metric operations.
-
-    """
-    log_folder = (
-        Path(root_folder) / f"step{step}_metriccount{mc}_datatype{data_type}_{sort_key}"
-    )
-
-    log_folder.mkdir(parents=True, exist_ok=True)
-
-    capture_sar_metrics(log_folder)
-
-    # To ensure only 1 wandb-core process is launched to reduce overhead
-    wandb.setup()
-
-    processes = []
-    for iter in range(1, num_of_processes + 1):
-        p = mp.Process(
-            target=run_experiment,
-            kwargs=dict(
-                step_count=step,
-                metric_count=mc,
-                output_file=str(log_folder / f"results.{iter}.json"),
-                data_type=data_type,
-            ),
-        )
-        p.start()
-        logger.info(f"The {iter}-th process (pid: {p.pid}) has started.")
-        processes.append(p)
-
-    # now wait for all processes to finish and exit
-    for proc in processes:
-        proc.join()
-
-    logger.info("All experiements have finished.")
-
-    process_sar_files(log_folder)
