56879be5e115c8e6e7b442ccbdaea12a47f0a910
Timofey Peshin <timoffex@gmail.com>
2024-12-24 13:11:39 -0800

    chore: no relay_server in test_{catboost,lightning,finetuning,launch_run} (#9148)

--- tests/system_tests/test_functional/catboost/test_catboost.py
+++ tests/system_tests/test_functional/catboost/test_catboost.py
@@ -4,27 +4,27 @@ import pytest
 
 
 @pytest.mark.wandb_core_only
-def test_regression(user, relay_server, execute_script):
-    with relay_server() as relay:
-        script_path = pathlib.Path(__file__).parent / "regression.py"
-        execute_script(script_path)
+def test_regression(wandb_backend_spy, execute_script):
+    script_path = pathlib.Path(__file__).parent / "regression.py"
+    execute_script(script_path)
 
-    run_ids = relay.context.get_run_ids()
-    assert len(run_ids) == 1
-    run_id = run_ids[0]
+    with wandb_backend_spy.freeze() as snapshot:
+        run_ids = snapshot.run_ids()
+        assert len(run_ids) == 1
+        run_id = run_ids.pop()
 
-    config = relay.context.get_run_config(run_id)
-    assert config["classes_count"]["value"] == 0
-    assert config["depth"]["value"] == 2
-    assert config["eval_metric"]["value"] == "MultiClass"
-    assert config["iterations"]["value"] == 10
+        config = snapshot.config(run_id=run_id)
+        assert config["classes_count"]["value"] == 0
+        assert config["depth"]["value"] == 2
+        assert config["eval_metric"]["value"] == "MultiClass"
+        assert config["iterations"]["value"] == 10
 
-    summary = relay.context.get_run_summary(run_id)
-    assert summary["iteration@metric-period-1"] == 10
-    assert summary["Feature Importance_table"]["_type"] == "table-file"
-    assert summary["learn-MultiClass"] > 0.0
-    assert summary["best_score"]["learn"]["MultiClass"] > 0.0
+        summary = snapshot.summary(run_id=run_id)
+        assert summary["iteration@metric-period-1"] == 10
+        assert summary["Feature Importance_table"]["_type"] == "table-file"
+        assert summary["learn-MultiClass"] > 0.0
+        assert summary["best_score"]["learn"]["MultiClass"] > 0.0
 
-    telemetry = relay.context.get_run_telemetry(run_id)
-    assert 27 in telemetry["3"]  # feature=catboost_wandb_callback
-    assert 28 in telemetry["3"]  # feature=catboost_log_summary
+        telemetry = snapshot.telemetry(run_id=run_id)
+        assert 27 in telemetry["3"]  # feature=catboost_wandb_callback
+        assert 28 in telemetry["3"]  # feature=catboost_log_summary
