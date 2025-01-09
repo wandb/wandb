ed551121bea9161f095e5f59a02637a473734589
Timofey Peshin <timoffex@gmail.com>
2025-01-09 11:13:11 -0800

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.5

--- wandb/__init__.pyi
+++ wandb/__init__.pyi
@@ -651,7 +651,9 @@ def log(
         run = wandb.init()
         examples = []
         for i in range(3):
-            pixels = np.random.randint(low=0, high=256, size=(100, 100, 3), dtype=np.uint8)
+            pixels = np.random.randint(
+                low=0, high=256, size=(100, 100, 3), dtype=np.uint8
+            )
             pil_image = PILImage.fromarray(pixels, mode="RGB")
             image = wandb.Image(pil_image, caption=f"random field {i}")
             examples.append(image)
@@ -666,7 +668,9 @@ def log(
 
         run = wandb.init()
         # axes are (time, channel, height, width)
-        frames = np.random.randint(low=0, high=256, size=(10, 3, 100, 100), dtype=np.uint8)
+        frames = np.random.randint(
+            low=0, high=256, size=(10, 3, 100, 100), dtype=np.uint8
+        )
         run.log({"video": wandb.Video(frames, fps=4)})
         ```
 
