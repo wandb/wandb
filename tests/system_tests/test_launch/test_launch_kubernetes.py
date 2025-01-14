3edefb198407269bdfb4c810450f4a14ddebd2e2
Kyle Goyette <kyle@wandb.com>
2025-01-13 20:09:33 -0800

    wip

--- tests/system_tests/test_launch/test_launch_kubernetes.py
+++ tests/system_tests/test_launch/test_launch_kubernetes.py
@@ -90,6 +90,7 @@ async def test_kubernetes_run_clean_generate_name(
     )
     assert job["metadata"]["generateName"] == expected_generate_name
 
+
 @pytest.mark.skip(reason="This test is flaky and should be fixed")
 @pytest.mark.asyncio
 async def test_kubernetes_run_with_annotations(
