1eb767873e079b636449680a9b9d9df73520c95b
Timofey Peshin <timoffex@gmail.com>
2025-01-14 16:14:57 -0800

    [𝘀𝗽𝗿] changes to main this commit is based on
    Created using spr 1.3.5
    
    [skip ci]

--- wandb/sdk/interface/interface.py
+++ wandb/sdk/interface/interface.py
@@ -10,7 +10,6 @@ InterfaceRelay: Responses are routed to a relay queue (not matching uuids)
 
 import gzip
 import logging
-import os
 import time
 from abc import abstractmethod
 from pathlib import Path
@@ -100,8 +99,6 @@ class InterfaceBase:
 
     def _hack_set_run(self, run: "Run") -> None:
         self._run = run
-        current_pid = os.getpid()
-        self._run._set_iface_pid(current_pid)
 
     def publish_header(self) -> None:
         header = pb.HeaderRecord()
@@ -688,12 +685,11 @@ class InterfaceBase:
 
     def publish_history(
         self,
+        run: "Run",
         data: dict,
         step: Optional[int] = None,
-        run: Optional["Run"] = None,
         publish_step: bool = True,
     ) -> None:
-        run = run or self._run
         data = history_dict_to_json(run, data, step=step)
         history = pb.HistoryRecord()
         if publish_step:
