8e2fe5842fff99415da5240eac72ceacbbf1078a
Dmitry Duev <dima@wandb.com>
2025-01-15 01:12:18 -0800

    fix: TPU issues on GKE

--- core/pkg/monitor/tpu.go
+++ core/pkg/monitor/tpu.go
@@ -62,7 +62,7 @@ type TPU struct {
 	client RuntimeMetricServiceClient
 
 	// TPU chip specifications.
-	chip *TPUChip
+	chip TPUChip
 
 	// Total number of TPU devices detected.
 	count int
@@ -73,7 +73,7 @@ func NewTPU() *TPU {
 	t := &TPU{name: "tpu"}
 
 	chip, count := getLocalTPUChips()
-	if chip == nil {
+	if count == 0 {
 		return nil
 	}
 	t.chip = chip
@@ -101,7 +101,7 @@ func (t *TPU) SetName(name string) {
 	t.name = name
 }
 
-func (t *TPU) SetChip(chip *TPUChip, count int) {
+func (t *TPU) SetChip(chip TPUChip, count int) {
 	t.chip = chip
 	t.count = count
 }
@@ -112,8 +112,7 @@ func (t *TPU) SetClient(client RuntimeMetricServiceClient) {
 
 // Sample returns TPU metrics such as memory usage in % and in bytes, and duty cycle.
 func (t *TPU) Sample() (*spb.StatsRecord, error) {
-	if t.client == nil || t.chip == nil {
-		fmt.Println("TPU client or chip is not available")
+	if t.client == nil {
 		return nil, nil
 	}
 
@@ -206,7 +205,7 @@ func (t *TPU) Sample() (*spb.StatsRecord, error) {
 }
 
 func (t *TPU) IsAvailable() bool {
-	return t.chip != nil
+	return t.count > 0
 }
 
 // Close closes the gRPC connection and releases resources.
@@ -220,14 +219,14 @@ func (t *TPU) Close() {
 
 // getLocalTPUChips scans the PCI devices to detect local TPU chips and
 // returns the most common chip type and the total count.
-func getLocalTPUChips() (*TPUChip, int) {
+func getLocalTPUChips() (TPUChip, int) {
 	devices, err := filepath.Glob("/sys/bus/pci/devices/*")
 	fmt.Println("devices", devices)
 	if err != nil {
-		return nil, 0
+		return TPUChip{}, 0
 	}
 
-	counter := make(map[*TPUChip]int)
+	counter := make(map[TPUChip]int)
 
 	for _, pciPath := range devices {
 		vendorPath := filepath.Join(pciPath, "vendor")
@@ -264,10 +263,10 @@ func getLocalTPUChips() (*TPUChip, int) {
 	fmt.Println("counter", counter)
 
 	if len(counter) == 0 {
-		return nil, 0
+		return TPUChip{}, 0
 	}
 
-	var mostCommonChip *TPUChip
+	var mostCommonChip TPUChip
 	var maxCount int
 	for chip, count := range counter {
 		if count > maxCount {
@@ -278,26 +277,26 @@ func getLocalTPUChips() (*TPUChip, int) {
 	return mostCommonChip, maxCount
 }
 
-func tpuChipFromPCIDeviceID(deviceID, subsystemID string) (*TPUChip, error) {
+func tpuChipFromPCIDeviceID(deviceID, subsystemID string) (TPUChip, error) {
 	switch deviceID {
 	case "0x0027":
 		switch subsystemID {
 		case "0x004e":
-			return &TPUChip{Name: "v2", HbmGiB: 8, DevicesPerChip: 2}, nil
+			return TPUChip{Name: "v2", HbmGiB: 8, DevicesPerChip: 2}, nil
 		case "0x004f":
-			return &TPUChip{Name: "v3", HbmGiB: 16, DevicesPerChip: 2}, nil
+			return TPUChip{Name: "v3", HbmGiB: 16, DevicesPerChip: 2}, nil
 		}
 	case "0x005e":
-		return &TPUChip{Name: "v4", HbmGiB: 32, DevicesPerChip: 1}, nil
+		return TPUChip{Name: "v4", HbmGiB: 32, DevicesPerChip: 1}, nil
 	case "0x0063":
-		return &TPUChip{Name: "v5e", HbmGiB: 16, DevicesPerChip: 1}, nil
+		return TPUChip{Name: "v5e", HbmGiB: 16, DevicesPerChip: 1}, nil
 	case "0x0062":
-		return &TPUChip{Name: "v5p", HbmGiB: 95, DevicesPerChip: 1}, nil
+		return TPUChip{Name: "v5p", HbmGiB: 95, DevicesPerChip: 1}, nil
 	case "0x006f":
-		return &TPUChip{Name: "v6e", HbmGiB: 32, DevicesPerChip: 1}, nil
+		return TPUChip{Name: "v6e", HbmGiB: 32, DevicesPerChip: 1}, nil
 	}
 
-	return nil, fmt.Errorf("unknown TPU chip")
+	return TPUChip{}, fmt.Errorf("unknown TPU chip")
 }
 
 // getMetrics retrieves metrics from the TPU runtime gRPC service for the given metric name.
@@ -315,7 +314,7 @@ func (t *TPU) getMetrics(metricName TPUMetricName) ([]*tpuproto.Metric, error) {
 
 // Probe returns the TPU metadata.
 func (t *TPU) Probe() *spb.MetadataRequest {
-	if t.chip == nil {
+	if t.count == 0 {
 		return nil
 	}
 
