2c7ce377c57b5cc11f879c4dd761f8618bb5b964
Estella Xin <estella.xin@wandb.com>
2025-02-19 14:16:21 -0500

    chore(artifacts): make name filter prepend wandb-registry- (#9468)

--- wandb/apis/public/registries.py
+++ wandb/apis/public/registries.py
@@ -1,7 +1,7 @@
 """Public API: registries."""
 
 import json
-from typing import TYPE_CHECKING, Any, Dict, Optional
+from typing import TYPE_CHECKING, Any, Dict, Mapping, Optional, Sequence
 
 if TYPE_CHECKING:
     from wandb_gql import Client
@@ -54,7 +54,7 @@ class Registries(Paginator):
     ):
         self.client = client
         self.organization = organization
-        self.filter = filter or {}
+        self.filter = _ensure_registry_prefix_on_names(filter or {})
         variables = {
             "organization": organization,
             "filters": json.dumps(self.filter),
@@ -494,3 +494,33 @@ class Versions(Paginator):
             ]["edges"]
         )
         return artifacts
+
+
+def _ensure_registry_prefix_on_names(query, in_name=False):
+    """Traverse the filter to prepend the `name` key value with the registry prefix unless the value is a regex.
+
+    - in_name: True if we are under a "name" key (or propagating from one).
+
+    EX: {"name": "model"} -> {"name": "wandb-registry-model"}
+    """
+    if isinstance((txt := query), str):
+        if in_name:
+            return txt if txt.startswith(REGISTRY_PREFIX) else f"{REGISTRY_PREFIX}{txt}"
+        return txt
+    if isinstance((dct := query), Mapping):
+        new_dict = {}
+        for key, obj in dct.items():
+            if key == "name":
+                new_dict[key] = _ensure_registry_prefix_on_names(obj, in_name=True)
+            elif key == "$regex":
+                # For regex operator, we skip transformation of its value.
+                new_dict[key] = obj
+            else:
+                # For any other key, propagate the in_name and skip_transform flags as-is.
+                new_dict[key] = _ensure_registry_prefix_on_names(obj, in_name=in_name)
+        return new_dict
+    if isinstance((objs := query), Sequence):
+        return list(
+            map(lambda x: _ensure_registry_prefix_on_names(x, in_name=in_name), objs)
+        )
+    return query
