1125313431055759fa8523255cde907da43c2e2d
Estella Xin <estella.xin@wandb.com>
2025-02-20 16:49:44 -0500

    ruff

--- wandb/apis/public/api.py
+++ wandb/apis/public/api.py
@@ -34,7 +34,6 @@ from wandb.apis.public.utils import (
     parse_org_from_registry_path,
 )
 from wandb.proto.wandb_internal_pb2 import ServerFeature
-
 from wandb.sdk.artifacts._validators import is_artifact_registry_project
 from wandb.sdk.internal.internal_api import Api as InternalApi
 from wandb.sdk.internal.thread_local_settings import _thread_local_api_settings
@@ -1511,7 +1510,6 @@ class Api:
         Returns:
             A registry iterator.
         """
-
         if not self._check_server_feature_with_fallback(
             ServerFeature.ARTIFACT_REGISTRY_SEARCH
         ):
