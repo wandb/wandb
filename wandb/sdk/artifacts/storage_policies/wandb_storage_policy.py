222749c71fe20505c8d2cd652826ab77cfc6042b
Ishita Bindlish <ishita.bindlish@wandb.com>
2025-03-06 23:53:44 -0800

    add file name to end of url

--- wandb/sdk/artifacts/storage_policies/wandb_storage_policy.py
+++ wandb/sdk/artifacts/storage_policies/wandb_storage_policy.py
@@ -209,16 +209,19 @@ class WandbStoragePolicy(StoragePolicy):
             if api._check_server_feature_with_fallback(
                 ServerFeature.ARTIFACT_COLLECTION_MEMBERSHIP_FILE_DOWNLOAD_HANDLER  # type: ignore
             ):
-                return "{}/artifactsV2/{}/{}/{}/{}".format(
+                return "{}/artifactsV2/{}/{}/{}/{}/{}/{}".format(
                     api.settings("base_url"),
                     storage_region,
-                    entity_name,
+                    quote(entity_name),
+                    quote(project_name),
+                    quote(artifact_name),
                     quote(
                         manifest_entry.birth_artifact_id
                         if manifest_entry.birth_artifact_id is not None
                         else ""
                     ),
                     md5_hex,
+                    manifest_entry.path.split("/")[-1],
                 )
             return "{}/artifactsV2/{}/{}/{}/{}".format(
                 api.settings("base_url"),
