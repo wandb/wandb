599d7950767d864820d616748a06c75eea540289
Dmitry Duev <dima@wandb.com>
2025-03-07 16:27:37 -0800

    add a test

--- core/internal/runconsolelogs/filestreamwriter.go
+++ core/internal/runconsolelogs/filestreamwriter.go
@@ -2,8 +2,6 @@ package runconsolelogs
 
 import (
 	"encoding/json"
-	"fmt"
-	"strings"
 
 	"github.com/wandb/wandb/core/internal/filestream"
 	"github.com/wandb/wandb/core/internal/sparselist"
@@ -15,61 +13,6 @@ type filestreamWriter struct {
 
 	// StructuredLogs indicates whether to send structured logs.
 	Structured bool
-}
-
-type logLine struct {
-	Level   string `json:"level,omitempty"`
-	TS      string `json:"ts"`
-	Label   string `json:"label,omitempty"`
-	Content string `json:"content"`
-}
-
-// Timestamps are generated in the Python ISO format (microseconds without Z)
-const rfc3339Micro = "2006-01-02T15:04:05.000000Z07:00"
-
-// MarshalJSON implements the json.Marshaler interface.
-func (r *RunLogsLine) MarshalJSON() ([]byte, error) {
-	// Format timestamp and trim the Z suffix as specified
-	timestamp := strings.TrimSuffix(r.Timestamp.UTC().Format(rfc3339Micro), "Z")
-
-	// Convert rune slice to string for content
-	content := string(r.Content)
-
-	// We only indicate the log level if it is an error
-	level := ""
-	if r.StreamPrefix != "" {
-		level = "error"
-	}
-
-	// Create the JSON structure with the required fields
-	jsonLine := logLine{
-		Level:   level,
-		TS:      timestamp,
-		Label:   r.StreamLabel,
-		Content: content,
-	}
-
-	// Marshal the custom struct to JSON
-	return json.Marshal(jsonLine)
-}
-
-func (line *RunLogsLine) legacyFormat() string {
-	timestamp := strings.TrimSuffix(line.Timestamp.UTC().Format(rfc3339Micro), "Z")
-	if line.StreamLabel != "" {
-		return fmt.Sprintf(
-			"%s%s [%s] %s",
-			line.StreamPrefix,
-			timestamp,
-			line.StreamLabel,
-			string(line.Content),
-		)
-	}
-	return fmt.Sprintf(
-		"%s%s %s",
-		line.StreamPrefix,
-		timestamp,
-		string(line.Content),
-	)
 }
 
 func (w *filestreamWriter) SendChanged(
@@ -84,7 +27,7 @@ func (w *filestreamWriter) SendChanged(
 			return string(jsonLine)
 		}
 
-		return line.legacyFormat()
+		return line.LegacyFormat()
 	})
 
 	w.FileStream.StreamUpdate(&filestream.LogsUpdate{
