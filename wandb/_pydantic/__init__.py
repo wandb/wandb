2dabeb6c3af555d21f96d2d82817b730072d60c7
tonyyli-wandb <tony.li@wandb.com>
2025-03-25 12:24:38 -0700

    refactor(sdk): cleanup pydantic codegen and define reusable pydantic helper modules

---
+++ wandb/_pydantic/__init__.py
@@ -0,0 +1,10 @@
+"""Internal utilities for working with pydantic."""
+
+from .base import Base, GQLBase, GQLId, Typename
+
+__all__ = [
+    "Base",
+    "GQLBase",
+    "Typename",
+    "GQLId",
+]
