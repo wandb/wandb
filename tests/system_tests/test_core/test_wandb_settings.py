6bb4939b07ae1fcb6671de446ddcbe6d74b3924c
Jacob Romero <jacob.romero@wandb.com>
2025-04-02 10:55:41 -0700

    fix wording, and test styling

--- tests/system_tests/test_core/test_wandb_settings.py
+++ tests/system_tests/test_core/test_wandb_settings.py
@@ -138,7 +138,6 @@ def test_git_root(runner, wandb_backend_spy):
 @pytest.mark.parametrize("clean_files", [True, False])
 def test_cleanup_files_during_run(user, clean_files):
     settings = wandb.Settings(clean_run_files=clean_files)
-
     image = np.zeros((2, 2, 3), dtype=np.uint8)
     wandb_image = wandb.Image(image)
 
@@ -147,12 +146,10 @@ def test_cleanup_files_during_run(user, clean_files):
 
     start_time = time.monotonic()
     timeout_sec = 0
-
     while time.monotonic() - start_time < timeout_sec:
         if os.path.exists(run.settings.files_dir) != clean_files:
             return True
         time.sleep(0.1)
-
     pytest.fail("Files directory was not deleted in expected time.")
 
 
