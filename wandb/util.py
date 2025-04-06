111f6fc358e6aecbbd72a64db3829d4a1daf3584
Travis Cline <travis.cline@gmail.com>
2025-04-06 16:00:30 -0700

    feat(sdk): add mlx array type handling

--- wandb/util.py
+++ wandb/util.py
@@ -402,6 +402,10 @@ def get_jax_tensor(obj: Any) -> Optional[Any]:
     return jax.device_get(obj)
 
 
+def is_mlx_array_typename(typename: str) -> bool:
+    return typename == 'mlx.core.array'
+
+
 def is_fastai_tensor_typename(typename: str) -> bool:
     return typename.startswith("fastai.") and ("Tensor" in typename)
 
@@ -612,6 +616,8 @@ def json_friendly(  # noqa: C901
         obj = tuple(obj)
     elif isinstance(obj, enum.Enum):
         obj = obj.name
+    elif is_mlx_array_typename(typename):
+        obj = obj.tolist()
     else:
         converted = False
     if getsizeof(obj) > VALUE_BYTES_LIMIT:
