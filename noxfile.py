721e660985c9d624725975efcbddee3cc4c98ce2
tonyyli-wandb <tony.li@wandb.com>
2025-04-25 17:21:42 -0700

    chore: try --dist=loadscope

--- noxfile.py
+++ noxfile.py
@@ -129,6 +129,7 @@ def run_pytest(
 
     # (pytest-xdist) Run tests in parallel.
     pytest_opts.append(f"-n={opts.get('n', 'auto')}")
+    pytest_opts.append(f"--dist={opts.get('dist', 'loadfile')}")
 
     # Limit the # of workers in CI. Due to heavy tensorflow and pytorch imports,
     # each worker uses up 700MB+ of memory, so with a large number of workers,
