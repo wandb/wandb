12f445cd778325597ae12d404883576657fe0f2b
Tim Hays <tim.hays@wandb.com>
2025-05-23 09:32:20 -0500

    skip launch tests if python < 3.9

--- tests/unit_tests/test_launch/conftest.py
+++ tests/unit_tests/test_launch/conftest.py
@@ -1,5 +1,12 @@
+import sys
+
 import pytest
 from wandb.apis.internal import InternalApi
+
+if sys.version_info < (3, 9):
+    pytest.skip(
+        "Launch is not supported on Python versions < 3.9", allow_module_level=True
+    )
 
 
 @pytest.fixture
