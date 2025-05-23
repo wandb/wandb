91d3b8ecfe19d3f009e9c17de8c59a198b216b1c
Tim Hays <tim.hays@wandb.com>
2025-05-23 11:48:31 -0500

    skip using circle config instead

--- tests/system_tests/test_launch/conftest.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-import pytest
-
-if sys.version_info < (3, 9):
-    pytest.skip(
-        "Launch is not supported on Python versions < 3.9", allow_module_level=True
-    )
--- tests/unit_tests/test_launch/conftest.py
+++ tests/unit_tests/test_launch/conftest.py
@@ -1,12 +1,5 @@
-import sys
-
 import pytest
 from wandb.apis.internal import InternalApi
-
-if sys.version_info < (3, 9):
-    pytest.skip(
-        "Launch is not supported on Python versions < 3.9", allow_module_level=True
-    )
 
 
 @pytest.fixture
