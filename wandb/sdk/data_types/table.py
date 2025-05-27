f62f58d5f4aba98891111317f9bbb08191ec615f
dom phan <dominic.phan@wandb.com>
2025-05-27 07:32:52 -0700

    chore: remove unused fns from merge

--- wandb/sdk/data_types/table.py
+++ wandb/sdk/data_types/table.py
@@ -190,30 +190,6 @@ class _ForeignIndexType(_dtypes.Type):
         return cls(table)
 
 
-def allow_relogging_after_mutation(method):
-    def wrapper(self, *args, **kwargs):
-        res = method(self, *args, **kwargs)
-
-        has_been_logged = self._run is not None or self._artifact_target is not None
-
-        if self.log_mode == "MUTABLE":
-            self._run = None
-            self._artifact_target = None
-        elif self.log_mode == "IMMUTABLE" and has_been_logged:
-            logging.warning(
-                "You are mutating a Table with log_mode='IMMUTABLE' that has been"
-                "logged already. Subsequent log() calls will have no effect."
-                "Set log_mode='MUTABLE' to enable re-logging after mutations"
-            )
-
-        return res
-
-    return wrapper
-
-
-supported_logging_modes = ["IMMUTABLE", "MUTABLE"]
-
-
 class Table(Media):
     """The Table class used to display and analyze tabular data.
 
