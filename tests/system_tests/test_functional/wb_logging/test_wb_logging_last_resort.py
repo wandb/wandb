c7875390b1f585c47a6fb0d97bdccff9321142c0
Dmitry Duev <dima@wandb.com>
2025-06-02 14:12:29 -0700

    chore: remove legacy wandb service

--- tests/system_tests/test_functional/wb_logging/test_wb_logging_last_resort.py
+++ tests/system_tests/test_functional/wb_logging/test_wb_logging_last_resort.py
@@ -1,10 +1,7 @@
 import pathlib
 import subprocess
 
-import pytest
 
-
-(reason="does not depend on service")
 def test_wb_logging_last_resort():
     script = pathlib.Path(__file__).parent / "last_resort.py"
 
