3c3603ae54aaa8e3103d53c3c9d56e49d7e9c98a
Timofey Peshin <timoffex@gmail.com>
2025-06-05 17:06:25 -0700

    core server cleanup

--- core/pkg/server/server.go
+++ core/pkg/server/server.go
@@ -20,18 +20,7 @@ const (
 	IntervalCheckParentPidMilliseconds = 100
 )
 
-type ServerParams struct {
-	ListenIPAddress     string
-	PortFilename        string
-	ParentPid           int
-	SentryClient        *sentry_ext.Client
-	Commit              string
-	LoggerPath          string
-	LogLevel            slog.Level
-	EnableDCGMProfiling bool
-}
-
-// Server is the core server
+// Server is the top-level object for the wandb-core process.
 type Server struct {
 	// serverLifetimeCtx is cancelled when the server should shut down.
 	serverLifetimeCtx context.Context
@@ -39,8 +28,11 @@ type Server struct {
 	// stopServer cancels serverLifetimeCtx.
 	stopServer context.CancelFunc
 
-	// listener is the underlying listener
-	listener net.Listener
+	// streamMux maps stream IDs to streams.
+	streamMux *stream.StreamMux
+
+	// gpuResourceManager manages costly resources for GPU system metrics.
+	gpuResourceManager *monitor.GPUResourceManager
 
 	// sentryClient is the client used to report errors to sentry.io
 	sentryClient *sentry_ext.Client
@@ -49,10 +41,12 @@ type Server struct {
 	// and for the serve goroutine to finish
 	wg sync.WaitGroup
 
-	// parentPid is the parent pid to watch and exit if it goes away
-	parentPid int
+	// parentPID is parent process's ID.
+	//
+	// The server exits if the parent process is gone.
+	parentPID int
 
-	// commit is the W&B Git commit hash
+	// commit is the W&B Git commit hash.
 	commit string
 
 	// loggerPath is the default logger path
@@ -60,70 +54,71 @@ type Server struct {
 
 	// logLevel is the log level
 	logLevel slog.Level
+}
 
-	// enableDCGMProfiling is true if DCGM profiling is enabled
-	enableDCGMProfiling bool
+type ServerParams struct {
+	ParentPid           int
+	SentryClient        *sentry_ext.Client
+	Commit              string
+	LoggerPath          string
+	LogLevel            slog.Level
+	EnableDCGMProfiling bool
 }
 
 // NewServer creates a new server
-func NewServer(params *ServerParams) (*Server, error) {
-	if params == nil {
-		return nil, errors.New("unconfigured params")
-	}
+func NewServer(params ServerParams) *Server {
 	serverLifetimeCtx, stopServer := context.WithCancel(context.Background())
 
-	listener, err := net.Listen("tcp", params.ListenIPAddress)
-	if err != nil {
-		stopServer()
-		return nil, err
+	return &Server{
+		serverLifetimeCtx:  serverLifetimeCtx,
+		stopServer:         stopServer,
+		streamMux:          stream.NewStreamMux(),
+		gpuResourceManager: monitor.NewGPUResourceManager(params.EnableDCGMProfiling),
+		sentryClient:       params.SentryClient,
+		wg:                 sync.WaitGroup{},
+		parentPID:          params.ParentPid,
+		commit:             params.Commit,
+		loggerPath:         params.LoggerPath,
+		logLevel:           params.LogLevel,
 	}
-
-	s := &Server{
-		serverLifetimeCtx:   serverLifetimeCtx,
-		stopServer:          stopServer,
-		listener:            listener,
-		wg:                  sync.WaitGroup{},
-		parentPid:           params.ParentPid,
-		sentryClient:        params.SentryClient,
-		commit:              params.Commit,
-		loggerPath:          params.LoggerPath,
-		logLevel:            params.LogLevel,
-		enableDCGMProfiling: params.EnableDCGMProfiling,
-	}
-
-	port := s.listener.Addr().(*net.TCPAddr).Port
-	if err := writePortFile(params.PortFilename, port); err != nil {
-		slog.Error("failed to write port file", "error", err)
-		return nil, err
-	}
-
-	return s, nil
 }
 
 // exitWhenParentIsGone exits the process if the parent process is killed.
 func (s *Server) exitWhenParentIsGone() {
-	slog.Info("Will exit if parent process dies.", "ppid", s.parentPid)
+	slog.Info("server: will exit if parent process dies", "ppid", s.parentPID)
 
-	for os.Getppid() == s.parentPid {
+	for os.Getppid() == s.parentPID {
 		time.Sleep(IntervalCheckParentPidMilliseconds * time.Millisecond)
 	}
 
-	slog.Info("Parent process exited, terminating service process.")
+	slog.Info("server: parent process exited, terminating service process")
 
 	// The user process has exited, so there's no need to sync
 	// uncommitted data, and we can quit immediately.
 	os.Exit(1)
 }
 
-func (s *Server) Serve() {
-	if s.parentPid != 0 {
+func (s *Server) Serve(portFile string) error {
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return fmt.Errorf("server: failed to listen on localhost: %v", err)
+	}
+
+	portInfo := PortInfo{
+		LocalhostPort: listener.Addr().(*net.TCPAddr).Port,
+	}
+	if err := portInfo.WriteToFile(portFile); err != nil {
+		return err
+	}
+
+	if s.parentPID != 0 {
 		go s.exitWhenParentIsGone()
 	}
 
 	s.wg.Add(1)
 	go func() {
 		defer s.wg.Done()
-		s.serve()
+		s.acceptConnections(listener)
 	}()
 
 	// Wait for the signal to shut down.
@@ -131,7 +126,7 @@ func (s *Server) Serve() {
 	slog.Info("server is shutting down")
 
 	// Stop accepting new connections.
-	if err := s.listener.Close(); err != nil {
+	if err := listener.Close(); err != nil {
 		slog.Error("failed to Close listener", "error", err)
 	}
 
@@ -139,79 +134,64 @@ func (s *Server) Serve() {
 	s.wg.Wait()
 
 	slog.Info("server is closed")
-}
-
-func (s *Server) serve() {
-	slog.Info("server is running", "addr", s.listener.Addr())
-
-	streamMux := stream.NewStreamMux()
-	gpuResourceManager := monitor.NewGPUResourceManager(s.enableDCGMProfiling)
-
-	// Run a separate goroutine to handle incoming connections
-	for {
-		conn, err := s.listener.Accept()
-		if err != nil {
-			select {
-			case <-s.serverLifetimeCtx.Done():
-				slog.Debug("server shutting down...")
-				return
-			default:
-				slog.Error("failed to accept conn.", "error", err)
-			}
-		} else {
-			s.wg.Add(1)
-			go func() {
-				NewConnection(
-					s.serverLifetimeCtx,
-					s.stopServer,
-					ConnectionParams{
-						Conn:               conn,
-						StreamMux:          streamMux,
-						GPUResourceManager: gpuResourceManager,
-						SentryClient:       s.sentryClient,
-						Commit:             s.commit,
-						LoggerPath:         s.loggerPath,
-						LogLevel:           s.logLevel,
-					},
-				).ManageConnectionData()
-
-				s.wg.Done()
-			}()
-		}
-	}
-}
-
-func writePortFile(portFile string, port int) error {
-	tempFile := fmt.Sprintf("%s.tmp", portFile)
-	f, err := os.Create(tempFile)
-	if err != nil {
-		err = fmt.Errorf("fail create temp file: %w", err)
-		return err
-	}
-
-	if _, err = fmt.Fprintf(f, "sock=%d\n", port); err != nil {
-		err = fmt.Errorf("fail write port: %w", err)
-		return err
-	}
-
-	if _, err = f.WriteString("EOF"); err != nil {
-		err = fmt.Errorf("fail write EOF: %w", err)
-		return err
-	}
-
-	if err = f.Sync(); err != nil {
-		err = fmt.Errorf("fail sync: %w", err)
-		return err
-	}
-
-	if err := f.Close(); err != nil {
-		err = fmt.Errorf("fail close: %w", err)
-		return err
-	}
-
-	if err = os.Rename(tempFile, portFile); err != nil {
-		err = fmt.Errorf("fail rename: %w", err)
-		return err
-	}
 	return nil
+}
+
+// acceptConnections accepts incoming connections on the listener.
+//
+// It blocks until the listener is closed or an error is encountered.
+func (s *Server) acceptConnections(listener net.Listener) {
+	slog.Info("server: accepting connections", "addr", listener.Addr())
+
+	for {
+		conn, err := listener.Accept()
+
+		if errors.Is(err, net.ErrClosed) {
+			slog.Info("server: listener closed", "addr", listener.Addr())
+			return
+		}
+
+		// We shut down on any error, even though some may be recoverable.
+		// From the accept(2) manual pages, these include:
+		//
+		//   - EMFILE, ENFILE (out of file descriptors)
+		//   - ENOBUFS, ENOMEM (out of memory)
+		//
+		// Maybe ECONNABORTED as well.
+		//
+		// Though in theory these are recoverable, we don't have a clean way
+		// to distinguish them from other errors in Go, and we want to avoid
+		// busy-looping in case Accept() immediately returns an error
+		// each time it is called.
+		if err != nil {
+			slog.Error("server: failed to accept connection", "error", err)
+			s.stopServer()
+			return
+		}
+
+		s.wg.Add(1)
+		go func() {
+			s.handleConnection(conn)
+			s.wg.Done()
+		}()
+	}
+}
+
+// handleConnection processes incoming data on a connection.
+//
+// This blocks until the connection closes.
+func (s *Server) handleConnection(conn net.Conn) {
+	NewConnection(
+		s.serverLifetimeCtx,
+		s.stopServer,
+		ConnectionParams{
+			Conn:               conn,
+			StreamMux:          s.streamMux,
+			GPUResourceManager: s.gpuResourceManager,
+			SentryClient:       s.sentryClient,
+			Commit:             s.commit,
+			LoggerPath:         s.loggerPath,
+			LogLevel:           s.logLevel,
+		},
+	).ManageConnectionData()
 }
