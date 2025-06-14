818eca48ea4588d6d08de203657e431098772f32
Timofey Peshin <timoffex@gmail.com>
2025-06-13 18:40:06 -0700

    tensorboard exit handling

---
+++ core/internal/tensorboard/tbwork.go
@@ -0,0 +1,55 @@
+package tensorboard
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/wandb/wandb/core/internal/observability"
+	"github.com/wandb/wandb/core/internal/runwork"
+	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
+)
+
+// TBWork makes the TensorBoard integration watch a TB logging directory.
+type TBWork struct {
+	runwork.NotASentinelMixin
+	runwork.AlwaysAcceptMixin
+	runwork.NoopProcessMixin
+
+	Record *spb.Record
+
+	Logger    *observability.CoreLogger
+	TBHandler *TBHandler
+}
+
+// Schedule implements Work.Schedule.
+//
+// TensorBoard is special: we handle its records at Schedule time rather than
+// at Accept (in the Handler) or Process (in the Sender). This is because of
+// its interaction with the Exit record, which calls TBHandler.Finish()
+// during Schedule.
+//
+// The right way to think about the TensorBoard integration is to pretend it
+// exists entirely in the client: the Schedule step can be viewed as something
+// that happens in the client itself.
+func (w *TBWork) Schedule(wg *sync.WaitGroup, process func()) {
+	err := w.TBHandler.Handle(w.Record.GetTbrecord())
+	if err != nil {
+		w.Logger.CaptureError(err)
+	}
+	process()
+}
+
+// Save implements Work.Save.
+func (w *TBWork) Save(write func(*spb.Record)) {
+	write(w.Record)
+}
+
+// DebugInfo implements Work.DebugInfo
+func (w *TBWork) DebugInfo() string {
+	return fmt.Sprintf(
+		"TBWork(log_dir=%q, root_dir=%q, save=%t)",
+		w.Record.GetTbrecord().GetLogDir(),
+		w.Record.GetTbrecord().GetRootDir(),
+		w.Record.GetTbrecord().GetSave(),
+	)
+}
