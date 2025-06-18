eeab9d41716816290105ae5136b236bb257ac803
Dmitry Duev <dima@wandb.com>
2025-06-18 13:08:40 -0700

    fix a unit test

--- tests/unit_tests/test_sender_config.py
+++ tests/unit_tests/test_sender_config.py
@@ -61,6 +61,7 @@ def test_to_backend_dict():
         framework="some-framework",
         start_time_millis=123454321,
         metric_pbdicts=[],
+        metadata_record=wandb_internal_pb2.MetadataRecord(),
     )
 
     assert backend_dict == {
