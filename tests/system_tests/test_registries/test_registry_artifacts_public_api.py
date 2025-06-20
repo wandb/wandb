4bd4c83867ed155a1ca652ef4cc4a0cbd9b32810
Ishita Bindlish <ishita.bindlish@wandb.com>
2025-06-20 14:20:42 -0700

    update comment

--- tests/system_tests/test_registries/test_registry_artifacts_public_api.py
+++ tests/system_tests/test_registries/test_registry_artifacts_public_api.py
@@ -71,6 +71,8 @@ def test_fetch_migrated_registry_artifact(
         "wandb.sdk.artifacts.artifact.Artifact._from_attrs",
     )
     mock_fetch_artifact_by_name = mocker.patch.object(api.client, "execute")
+    
+    # Mock the GQL response to return the version in the new org registry
     mock_fetch_artifact_by_name.return_value = {
         "project": {
             "artifact": {
@@ -93,6 +95,7 @@ def test_fetch_migrated_registry_artifact(
         }
     }
 
+    # Fetching an artifact from the legacy model registry
     api.artifact("test-team/model-registry/test-collection:v0")
     mock_fetch_artifact_by_name.assert_called_once()
     captured = capsys.readouterr()
