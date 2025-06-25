c636b0c7b3cd99e4644f413c62776b7f8a077c7c
Dmitry Duev <dima@wandb.com>
2025-06-25 12:52:09 -0700

    chore: capture CPU model info on Linux

--- core/pkg/monitor/system.go
+++ core/pkg/monitor/system.go
@@ -400,6 +400,11 @@ func (s *System) Probe() *spb.EnvironmentRecord {
 		info.CpuCountLogical = uint32(cpuCountLogical)
 	}
 
+	// cpuInfo, err := cpu.Info()
+	// if err != nil {
+	// 	fmt.Printf("%+v\n", cpuInfo)
+	// }
+
 	// Collect disk information
 	for _, diskPath := range s.diskPaths {
 		if usage, err := disk.Usage(diskPath); err == nil {
