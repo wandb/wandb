7a21977a43ee67366067690def1443b7f5021f18
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-07-01 09:31:24 -0700

    pre commit

--- wandb/apis/public/files.py
+++ wandb/apis/public/files.py
@@ -119,14 +119,14 @@ class Files(SizedPaginator["File"]):
 
     def __init__(self, client, run, names=None, per_page=50, upload=False):
         """An iterable collection of `File` objects for a specific run.
-        
+
         Args:
         client: The run object that contains the files
         run: The run object that contains the files
         names (list, optional): A list of file names to filter the files
         per_page (int, optional): The number of files to fetch per page
         upload (bool, optional): If `True`, fetch the upload URL for each file
-        
+
         """
         self.run = run
         variables = {
