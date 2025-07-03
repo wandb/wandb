f843c36b74c836d43fce6e5b274a48c3673f475d
Katia Patkin <katia@wandb.com>
2025-07-02 17:52:20 -0700

    chore: remove internal process check

--- wandb/sync/sync.py
+++ wandb/sync/sync.py
@@ -57,7 +57,6 @@ class SyncThread(threading.Thread):
     ):
         threading.Thread.__init__(self)
         # mark this process as internal
-        wandb._set_internal_process(disable=True)
         self._sync_list = sync_list
         self._project = project
         self._entity = entity
