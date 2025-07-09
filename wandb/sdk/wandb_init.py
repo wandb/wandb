b4e8c0c04a392688375ea1bd5fd41b2680715262
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-07-08 17:12:37 -0700

    return before raises

--- wandb/sdk/wandb_init.py
+++ wandb/sdk/wandb_init.py
@@ -1427,17 +1427,16 @@ def init(  # noqa: C901
         settings: Specifies a dictionary or `wandb.Settings` object with advanced
             settings for the run.
 
+    Returns:
+        A `Run` object.
+
     Raises:
         Error: If some unknown or internal error happened during the run
             initialization.
         AuthenticationError: If the user failed to provide valid credentials.
         CommError: If there was a problem communicating with the WandB server.
         UsageError: If the user provided invalid arguments.
-        KeyboardInterrupt: If user interrupts the run.
-
-    Returns:
-        A `Run` object.
-
+        KeyboardInterrupt: If user interrupts the run.        
 
     Examples:
     `wandb.init()` returns a `Run` object. Use the run object to log data,
