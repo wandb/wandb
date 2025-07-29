67130c09be2c9bc7a2097ad19205d7ed54eefeb1
Timofey Peshin <timoffex@gmail.com>
2025-07-29 12:53:26 -0700

    wire stream runwork and ops

--- core/internal/stream/wire_gen.go
+++ core/internal/stream/wire_gen.go
@@ -6,6 +6,13 @@
 
 package stream
 
+import (
+	"github.com/google/wire"
+	"github.com/wandb/wandb/core/internal/observability"
+	"github.com/wandb/wandb/core/internal/runwork"
+	"github.com/wandb/wandb/core/internal/wboperation"
+)
+
 // Injectors from streaminject.go:
 
 // InjectStream returns a new Stream.
@@ -15,6 +22,24 @@ func InjectStream(params StreamParams) *Stream {
 	client := params.Sentry
 	level := params.LogLevel
 	coreLogger := streamLogger(streamStreamLoggerFile, settings, client, level)
-	stream := NewStream(params, streamStreamLoggerFile, coreLogger)
+	wandbOperations := wboperation.NewOperations()
+	runWork := provideStreamRunWork(coreLogger)
+	stream := NewStream(params, streamStreamLoggerFile, coreLogger, wandbOperations, runWork)
 	return stream
+}
+
+// streaminject.go:
+
+var streamProviders = wire.NewSet(
+	NewStream, wire.FieldsOf(
+		new(StreamParams),
+		"Settings",
+		"Sentry",
+		"LogLevel",
+	), provideStreamRunWork,
+	streamLoggerProviders, wboperation.NewOperations,
+)
+
+func provideStreamRunWork(logger *observability.CoreLogger) runwork.RunWork {
+	return runwork.New(BufferSize, logger)
 }
