9b91079df557f45dcf09cdc780ce83b14fbbaf4f
Jacob Romero <jacob.romero@wandb.com>
2025-08-07 11:49:23 -0700

    walk and create orjson fragments for non serializable values

--- wandb/sdk/lib/json_util.py
+++ wandb/sdk/lib/json_util.py
@@ -6,6 +6,26 @@ from typing import Any, Union
 logger = logging.getLogger(__name__)
 
 
+def _create_orjson_fragments(obj: Any) -> Any:
+    """Create orjson fragments for non-serializable objects.
+
+    Orjson fragments are not serialized by orjson.
+    So this allows us to still support non-standard json types. (inf, -inf, nan)
+    """
+    if isinstance(obj, float) and (math.isnan(obj) or math.isinf(obj)):
+        return orjson.Fragment(json.dumps(obj))
+    return obj
+
+
+def _walk_and_create_fragments(obj: Any) -> Any:
+    if isinstance(obj, dict):
+        return {k: _walk_and_create_fragments(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [_walk_and_create_fragments(v) for v in obj]
+    else:
+        return _create_orjson_fragments(obj)
+
+
 try:
     import orjson  # type: ignore
 
@@ -17,18 +37,15 @@ try:
         """Wrapper for <json|orjson>.dumps."""
         cls = kwargs.pop("cls", None)
         try:
-            if isinstance(obj, float) and obj == float("inf"):
-                return "Infinity"
-            elif isinstance(obj, float) and obj == float("-inf"):
-                return "-Infinity"
-            elif isinstance(obj, float) and math.isnan(obj):
-                return "NaN"
+            obj = _walk_and_create_fragments(obj)
 
             _kwargs = kwargs.copy()
             if cls:
                 _kwargs["default"] = cls.default
             encoded = orjson.dumps(
-                obj, option=orjson.OPT_NON_STR_KEYS, **_kwargs
+                obj,
+                option=orjson.OPT_NON_STR_KEYS,
+                **_kwargs,
             ).decode()
         except Exception:
             logger.exception("Error using orjson.dumps")
