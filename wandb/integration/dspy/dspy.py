3186b5307df5d849248b7a9fd2c779b646d1fffe
Ayush Thakur <mein2work@gmail.com>
2025-08-11 19:42:06 +0530

    ruff

--- wandb/integration/dspy/dspy.py
+++ wandb/integration/dspy/dspy.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import logging
 from collections.abc import Mapping, Sequence
-from typing import Any, Optional
+from typing import Any
 
 import wandb
 import wandb.util
