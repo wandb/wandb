4064b0fe94a90e3bd4cd966a4d1edb5fae0ff8e7
Ayush Thakur <mein2work@gmail.com>
2025-08-12 16:05:32 +0530

    full test

--- wandb/integration/dspy/dspy.py
+++ wandb/integration/dspy/dspy.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 
 class WandbDSPyCallback(dspy.utils.BaseCallback):
-    def __init__(self, log_devset: bool = False) -> None:
+    def __init__(self) -> None:
         if wandb.run is None:
             raise wandb.Error(
                 "You must call `wandb.init()` before instantiating WandbDSPyCallback()."
@@ -32,7 +32,6 @@ class WandbDSPyCallback(dspy.utils.BaseCallback):
         # Record feature usage for internal telemetry (optional but recommended).
         # with wandb.wandb_lib.telemetry.context(run=wandb.run) as tel:
         #     tel.feature.dspy = True
-        self.log_devset: bool = log_devset
         self._did_log_config: bool = False
         self._temp_info_dict: dict[str, Any] = {}
         self._program_table: wandb.Table | None = None
@@ -122,12 +121,6 @@ class WandbDSPyCallback(dspy.utils.BaseCallback):
                 )
             finally:
                 self._did_log_config = True
-
-            # # Log devset as artifact if user requested it
-            # if self.log_devset:
-            #     devset_obj = inputs.get("devset", None)
-            #     if devset_obj:
-            #         print("devset_obj", devset_obj)
 
         # 2) Build/append program signature tables from the 'program' input
         program_obj = inputs.get("program", None)
