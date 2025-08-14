3d322935f756a394aa79f1f20d7b12f8b2e89c91
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2025-08-14 10:41:45 -0700

    removed extra indents

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -2786,26 +2786,26 @@ def enabled(service):
     Checks that the host is not `api.wandb.ai` (host check).
     
     Verifies if the user is logged in correctly using the provided API key (login check).
-    
+
     Checks that requests are made over HTTPS (secure requests).
-    
+
     Validates the CORS (Cross-Origin Resource Sharing) configuration of the
     object store (CORS configuration).
-    
+
     Logs metrics, saves, and downloads files to check if runs are correctly
     recorded and accessible (run check).
-    
+
     Saves and downloads artifacts to verify that the artifact storage and
     retrieval system is working as expected (artifact check).
-    
+
     Tests the GraphQL endpoint by uploading a file to ensure it can handle
     signed URL uploads (GraphQL PUT check).
-    
+
     Checks the ability to send large payloads through the proxy (large payload check).
-    
+
     Verifies that the installed version of the W&B package is up-to-date and
     compatible with the server (W&B version check).
-    
+
     Creates and executes a sweep to ensure that sweep functionality is
     working correctly (sweeps check).
 """,
