56ff9e7c60220b9448e348915815afe9a6ebb7e7
Timofey Peshin <timoffex@gmail.com>
2025-08-15 10:01:43 -0700

    asyncio mailbox

--- wandb/sdk/lib/service/service_client.py
+++ wandb/sdk/lib/service/service_client.py
@@ -11,6 +11,9 @@ from wandb.sdk.mailbox.mailbox_handle import MailboxHandle
 
 _logger = logging.getLogger(__name__)
 
+_HEADER_BYTE_INT_LEN = 5
+_HEADER_BYTE_INT_FMT = "<BI"
+
 
 class ServiceClient:
     """Implements socket communication with the internal service."""
@@ -25,7 +28,11 @@ class ServiceClient:
         self._reader = reader
         self._writer = writer
         self._mailbox = Mailbox(asyncer)
-        asyncer.run_soon(self._forward_responses, daemon=True)
+        asyncer.run_soon(
+            self._forward_responses,
+            daemon=True,
+            name="ServiceClient._forward_responses",
+        )
 
     def publish(self, request: spb.ServerRequest) -> None:
         """Send a request without waiting for a response."""
@@ -39,13 +46,17 @@ class ServiceClient:
 
         NOTE: This may mutate the request. The request should not be used
         after.
+
+        Raises:
+            MailboxClosedError: If used after the client is closed or has
+                stopped due to an error.
         """
         handle = self._mailbox.require_response(request)
         self._asyncer.run_soon(lambda: self._send_server_request(request))
         return handle
 
     async def _send_server_request(self, request: spb.ServerRequest) -> None:
-        header = struct.pack("<BI", ord("W"), request.ByteSize())
+        header = struct.pack(_HEADER_BYTE_INT_FMT, ord("W"), request.ByteSize())
         self._writer.write(header)
 
         data = request.SerializeToString()
@@ -77,14 +88,14 @@ class ServiceClient:
 
     async def _read_server_response(self) -> spb.ServerResponse | None:
         try:
-            header = await self._reader.readexactly(5)
+            header = await self._reader.readexactly(_HEADER_BYTE_INT_LEN)
         except asyncio.IncompleteReadError as e:
             if e.partial:
                 raise
             else:
                 return None
 
-        magic, length = struct.unpack("<BI", header)
+        magic, length = struct.unpack(_HEADER_BYTE_INT_FMT, header)
 
         if magic != ord("W"):
             raise ValueError(f"Bad header: {header.hex()}")
