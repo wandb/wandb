d345ae259620a74aa193e2aff952a2e7e2431ef2
tonyyli-wandb <tony.li@wandb.com>
2025-08-15 19:36:02 -0700

    chore(artifacts): make ArtifactManifestEntry a pydantic class

--- wandb/sdk/artifacts/artifact_manifests/artifact_manifest_v1.py
+++ wandb/sdk/artifacts/artifact_manifests/artifact_manifest_v1.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from operator import itemgetter
-from typing import Any, Mapping
+from typing import Mapping
 
 from wandb.sdk.artifacts.artifact_manifest import ArtifactManifest
 from wandb.sdk.artifacts.artifact_manifest_entry import ArtifactManifestEntry
@@ -30,19 +30,9 @@ class ArtifactManifestV1(ArtifactManifest):
         storage_policy_config = manifest_json.get("storagePolicyConfig", {})
         storage_policy_cls = StoragePolicy.lookup_by_name(storage_policy_name)
 
-        entries: Mapping[str, ArtifactManifestEntry]
         entries = {
-            name: ArtifactManifestEntry(
-                path=name,
-                digest=val["digest"],
-                birth_artifact_id=val.get("birthArtifactID"),
-                ref=val.get("ref"),
-                size=val.get("size"),
-                extra=val.get("extra"),
-                local_path=val.get("local_path"),
-                skip_cache=val.get("skip_cache"),
-            )
-            for name, val in manifest_json["contents"].items()
+            path: ArtifactManifestEntry(**{**val, "path": path})
+            for path, val in manifest_json["contents"].items()
         }
 
         return cls(
@@ -56,6 +46,11 @@ class ArtifactManifestV1(ArtifactManifest):
     ) -> None:
         super().__init__(storage_policy, entries=entries)
 
+    #: Inner ArtifactManifestEntry fields to keep when dumping the ArtifactManifest to JSON.
+    _JSONABLE_ENTRY_FIELDS: frozenset[str] = frozenset(
+        {"digest", "birth_artifact_id", "ref", "extra", "size"}
+    )
+
     def to_manifest_json(self) -> dict:
         """This is the JSON that's stored in wandb_manifest.json.
 
@@ -64,25 +59,18 @@ class ArtifactManifestV1(ArtifactManifest):
         system. We don't need to include the local paths in the artifact manifest
         contents.
         """
-        contents = {}
-        for name, entry in sorted(self.entries.items(), key=itemgetter(0)):
-            json_entry: dict[str, Any] = {
-                "digest": entry.digest,
-            }
-            if entry.birth_artifact_id:
-                json_entry["birthArtifactID"] = entry.birth_artifact_id
-            if entry.ref:
-                json_entry["ref"] = entry.ref
-            if entry.extra:
-                json_entry["extra"] = entry.extra
-            if entry.size is not None:
-                json_entry["size"] = entry.size
-            contents[name] = json_entry
         return {
-            "version": self.__class__.version(),
+            "version": self.version(),
             "storagePolicy": self.storage_policy.name(),
             "storagePolicyConfig": self.storage_policy.config() or {},
-            "contents": contents,
+            "contents": {
+                name: entry.model_dump(
+                    by_alias=True,
+                    include=set(self._JSONABLE_ENTRY_FIELDS),
+                    exclude_defaults=True,
+                )
+                for name, entry in sorted(self.entries.items(), key=itemgetter(0))
+            },
         }
 
     def digest(self) -> HexMD5:
