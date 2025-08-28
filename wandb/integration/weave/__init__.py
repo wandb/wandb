0df81c5be2b37c0d451a00d0698d8a653afe7740
andrewtruong <itsandrewtruong@gmail.com>
2025-08-28 12:27:44 -0400

    test

--- wandb/integration/weave/__init__.py
+++ wandb/integration/weave/__init__.py
@@ -1,11 +1,7 @@
 """Weave integration for W&B."""
 
-from wandb.integration.weave.weave import (
-    cleanup_weave_integration,
-    setup_weave_integration,
-)
-
 from .interface import RunPath, active_run_path
+from .weave import cleanup_weave_integration, setup_weave_integration
 
 __all__ = (
     "active_run_path",
