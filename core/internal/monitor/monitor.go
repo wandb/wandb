0aaead8f569814828db1554cb8c1ba3ad396b31e
Dmitry Duev <dima@wandb.com>
2025-09-16 17:22:55 -0700

    put isprimary check back as it is unrelated to this pr

--- core/internal/monitor/monitor.go
+++ core/internal/monitor/monitor.go
@@ -351,7 +351,7 @@ func (sm *SystemMonitor) Start(git *spb.GitRepoRecord) {
 //
 // This operation may take some time, so we perform it on a best-effort basis.
 func (sm *SystemMonitor) Probe() {
-	if !sm.settings.IsDisableMeta() && !sm.settings.IsDisableMachineInfo() {
+	if !sm.settings.IsDisableMeta() && !sm.settings.IsDisableMachineInfo() && sm.settings.IsPrimary() {
 		go func() {
 			sm.logger.Debug("monitor: collecting the environment and resource information")
 
