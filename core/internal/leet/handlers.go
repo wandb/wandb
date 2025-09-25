265bc2bce82d1ba155110298346defd3c9405427
Dmitry Duev <dima@wandb.com>
2025-09-24 18:25:47 -0700

    chore: add help page to leet

--- core/internal/leet/handlers.go
+++ core/internal/leet/handlers.go
@@ -7,6 +7,10 @@ import tea "github.com/charmbracelet/bubbletea"
 //gocyclo:ignore
 func (m *Model) handleKeyMsg(msg tea.KeyMsg) (*Model, tea.Cmd) {
 	switch msg.String() {
+	case "h", "?":
+		m.help.Toggle()
+		return m, nil
+
 	case "q", "ctrl+c":
 		return m, tea.Quit
 	}
