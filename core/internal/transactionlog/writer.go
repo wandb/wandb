0e08ea513de721d448e44a2d9c2cf9df7a47c367
Timofey Peshin <timoffex@gmail.com>
2025-09-26 16:51:36 -0700

    refactor: internal/transactionlog (#10508)
    Breaks `Store` into a `transactionlog.Reader` and `transactionlog.Writer` which were unnecessarily coupled considering a single `Store` cannot be used for both reading and writing.
    
    I introduced one small semantic difference: `transactionlog.OpenWriter`​ will return an error instead of truncating an existing .wandb file, which would never be done intentionally. This actually exposes a bug: if you run `wandb.init()`​ twice too quickly (or possibly simultaneously) and with the same ID (such as what happens with `resume=”auto”`​) your second instance of the run can overwrite the first because it’ll get the same run directory. This caused the test failure [here](https://app.circleci.com/pipelines/github/wandb/wandb/52190/workflows/6d6c095a-a219-4328-987f-5652ba58eb65/jobs/1473409/tests). Fixed by PR #10511 .
    
    In the write path, there are a few `err`​ returns that aren’t tested because I don’t know a way of triggering an error in file writing or proto marshaling. It is possible to update `Writer`​ to accept an `io.WriteCloser`​ and create a test `WriteCloser`​ that returns errors, but that felt like it wasn’t worth the lines of code.

---
+++ core/internal/transactionlog/writer.go
@@ -0,0 +1,103 @@
+package transactionlog
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/wandb/wandb/core/pkg/leveldb"
+	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
+	"google.golang.org/protobuf/proto"
+)
+
+// Writer creates a .wandb file.
+//
+// Not safe for use in multiple goroutines.
+type Writer struct {
+	writer *leveldb.Writer // nil when closed
+	file   *os.File
+}
+
+// OpenWriter opens a .wandb file for writing.
+//
+// The file must not already exist. It is created with permissions 0o666
+// (meaning read and write permissions for user, group and others).
+//
+// The file header is output immediately, so that OpenReader() can open it
+// before any records are written.
+//
+// The parent directory must exist.
+//
+// Wraps errors from the os.OpenFile() call so that they can be checked
+// with errors.Is().
+func OpenWriter(path string) (*Writer, error) {
+	// O_EXCL returns an error if the file already exists.
+	//
+	// Note that os.Create() silently truncates an existing file,
+	// which is very bad if it happens to be an actual transaction log.
+	// Could happen due to a race between two wandb scripts!
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0o666)
+	if err != nil {
+		return nil, fmt.Errorf("transactionlog: error creating file: %w", err)
+	}
+
+	writer := leveldb.NewWriterExt(f, leveldb.CRCAlgoIEEE, wandbStoreVersion)
+
+	// Flush immediately to write the file's header.
+	if err := writer.Flush(); err != nil {
+		_ = f.Close()
+		return nil, fmt.Errorf("transactionlog: error writing header: %v", err)
+	}
+
+	return &Writer{writer: writer, file: f}, nil
+}
+
+// Write writes the next record into the transaction log.
+func (w *Writer) Write(msg *spb.Record) error {
+	if w.writer == nil {
+		return errors.New("transactionlog: writer is closed")
+	}
+
+	msgBytes, err := proto.Marshal(msg)
+	if err != nil {
+		return fmt.Errorf("transactionlog: error marshaling: %v", err)
+	}
+
+	recordWriter, err := w.writer.Next()
+	if err != nil {
+		return fmt.Errorf("transactionlog: error starting next record: %v", err)
+	}
+
+	// NOTE: The io.Writer contract guarantees a non-nil error on a short write.
+	_, err = recordWriter.Write(msgBytes)
+	if err != nil {
+		return fmt.Errorf("transactionlog: error writing: %v", err)
+	}
+
+	return nil
+}
+
+// Close closes the file.
+//
+// The writer may not be used after.
+// An error could indicate that not all data was written.
+func (w *Writer) Close() error {
+	if w.writer == nil {
+		return errors.New("transactionlog: writer already closed")
+	}
+
+	var errs []error
+
+	if err := w.writer.Close(); err != nil {
+		errs = append(errs,
+			fmt.Errorf("transactionlog: error closing writer: %v", err))
+	}
+
+	if err := w.file.Close(); err != nil {
+		errs = append(errs,
+			fmt.Errorf("transactionlog: error closing file: %v", err))
+	}
+
+	w.writer = nil
+	return errors.Join(errs...)
+}
