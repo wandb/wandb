wandb/wandb

commit 3cb36c568b1c15c199f8e80d132cd49774969dbe

refactor: filestream request size calculation (#10470)
Cleans up the logic estimating whether the accumulated FileStream request is at maximum size.

`isAtMaxSize` didn't take the summary size into consideration before (`addStringsToRequest`​ is not used for the summary). The only difference this makes is that if the user does something like `run.summary["key"] = "huge value ..."` while turning off system metrics, not logging any data and not printing anything to the console, then we'll now make the request immediately rather than waiting to accumulate more data.

It was odd to use a stateful computation to compute this value.

core/internal/filestream/filestreamrequest.go
@@ -144,7 +144,6 @@ func NewRequestReader(
 	requestSizeLimitBytes int,
 ) (*FileStreamRequestReader, bool) {
 	requestSizeApprox := 0
-	isAtMaxSize := false
 
 	// Increases requestSizeApprox and returns the number of strings to
 	// send from the list.
@@ -152,21 +151,20 @@ func NewRequestReader(
 		for i, line := range data {
 			nextSize := requestSizeApprox + len(line)
 
-			if nextSize > requestSizeLimitBytes {
-				isAtMaxSize = true
+			switch {
+			case nextSize <= requestSizeLimitBytes:
+				requestSizeApprox = nextSize
 
-				// As a special case, if the first line is larger than the limit
-				// and we're not sending any other data, send the line and hope
-				// it goes through.
-				if requestSizeApprox == 0 && i == 0 {
-					requestSizeApprox = nextSize
-					return 1
-				} else {
-					return i
-				}
+			// As a special case, if the first line is larger than the limit
+			// and we're not sending any other data, send the line and hope
+			// it goes through.
+			case requestSizeApprox == 0:
+				requestSizeApprox = nextSize
+				return 1
+
+			default:
+				return i
 			}
-
-			requestSizeApprox = nextSize
 		}
 
 		return len(data)
@@ -210,6 +208,7 @@ func NewRequestReader(
 	// isAtMaxSize is different from isFullRequest: a request could be partial
 	// but still below the size limit if it contains nonconsecutive console
 	// lines.
+	isAtMaxSize := requestSizeApprox >= requestSizeLimitBytes
 	return reader, isAtMaxSize
 }
 

commit dd3f13c52abb56c077052f800728f566cee25bc0

fix: filestream request truncation detection (#10599)
Fixes the logic that calculates `isAtMaxSize`. I broke it in PR #10470, causing FileStream to fail to ignore rate limits once `x_file_stream_max_bytes` is reached (not a terrible bug).

> It was odd to use a stateful computation to compute this value.

Turns out it wasn't odd! I just confused myself by naming it `isAtMaxSize`​. I renamed it to `isTruncatedDueToSize`​ in this PR.

core/internal/filestream/filestreamrequest.go
@@ -144,25 +144,28 @@ func NewRequestReader(
 	requestSizeLimitBytes int,
 ) (*FileStreamRequestReader, bool) {
 	requestSizeApprox := 0
+	isTruncatedDueToSize := false
 
 	// Increases requestSizeApprox and returns the number of strings to
 	// send from the list.
-	addStringsToRequest := func(data []string) int {
+	addStringsToRequest := func(data ...string) int {
 		for i, line := range data {
 			nextSize := requestSizeApprox + len(line)
 
-			switch {
-			case nextSize <= requestSizeLimitBytes:
+			if nextSize <= requestSizeLimitBytes {
 				requestSizeApprox = nextSize
+				continue
+			}
+
+			isTruncatedDueToSize = true
 
 			// As a special case, if the first line is larger than the limit
 			// and we're not sending any other data, send the line and hope
 			// it goes through.
-			case requestSizeApprox == 0:
+			if requestSizeApprox == 0 {
 				requestSizeApprox = nextSize
 				return 1
-
-			default:
+			} else {
 				return i
 			}
 		}
@@ -170,16 +173,14 @@ func NewRequestReader(
 		return len(data)
 	}
 
-	// We always send the summary.
-	requestSizeApprox += len(request.LatestSummary)
-
-	historyLinesToSend := addStringsToRequest(request.HistoryLines)
-	eventsLinesToSend := addStringsToRequest(request.EventsLines)
+	addStringsToRequest(request.LatestSummary)
+	historyLinesToSend := addStringsToRequest(request.HistoryLines...)
+	eventsLinesToSend := addStringsToRequest(request.EventsLines...)
 
 	consoleLineRuns := request.ConsoleLines.ToRuns()
 	consoleLinesToSend := 0
 	if len(consoleLineRuns) > 0 {
-		consoleLinesToSend = addStringsToRequest(consoleLineRuns[0].Items)
+		consoleLinesToSend = addStringsToRequest(consoleLineRuns[0].Items...)
 	}
 
 	reader := &FileStreamRequestReader{
@@ -205,11 +206,9 @@ func NewRequestReader(
 		reader.isFullRequest = true
 	}
 
-	// isAtMaxSize is different from isFullRequest: a request could be partial
-	// but still below the size limit if it contains nonconsecutive console
-	// lines.
-	isAtMaxSize := requestSizeApprox >= requestSizeLimitBytes
-	return reader, isAtMaxSize
+	// isTruncatedDueToSize is different from isFullRequest: a request could be
+	// partial if it contains nonconsecutive console lines.
+	return reader, isTruncatedDueToSize
 }
 
 // FileStreamState is state necessary to turn a [FileStreamRequest]

core/internal/filestream/filestreamrequest_test.go
@@ -26,6 +26,31 @@ func TestSizeLimit_HugeLine_SentAlone(t *testing.T) {
 	assert.False(t, done)
 }
 
+func TestSizeLimit_AboveMaxSize(t *testing.T) {
+	reader, isTruncated := NewRequestReader(
+		&FileStreamRequest{HistoryLines: []string{"one", "two"}},
+		// "one" will fit, "two" will not, and the size will not be exactly 5
+		5,
+	)
+
+	json := reader.GetJSON(&FileStreamState{})
+
+	assert.True(t, isTruncated)
+	assert.Equal(t, []string{"one"}, json.Files[HistoryFileName].Content)
+}
+
+func TestSizeLimit_BelowMaxSize(t *testing.T) {
+	reader, isTruncated := NewRequestReader(
+		&FileStreamRequest{HistoryLines: []string{"one", "two"}},
+		10, // both lines will fit
+	)
+
+	json := reader.GetJSON(&FileStreamState{})
+
+	assert.False(t, isTruncated)
+	assert.Equal(t, []string{"one", "two"}, json.Files[HistoryFileName].Content)
+}
+
 func TestHistory_MergeAppends(t *testing.T) {
 	req1 := &FileStreamRequest{HistoryLines: []string{"original"}}
 	req2 := &FileStreamRequest{HistoryLines: []string{"new"}}