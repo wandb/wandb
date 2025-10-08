773273fe080cc923eaa5e815fcab722e688103db
Tony Li <tony.li@wandb.com>
2025-10-08 15:27:34 -0700

    refactor(artifacts): make ArtifactManifestEntry a pydantic class (#10352)

--- wandb/sdk/artifacts/artifact_manifests/artifact_manifest_v1.py
+++ wandb/sdk/artifacts/artifact_manifests/artifact_manifest_v1.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from operator import itemgetter
-from typing import Any, Mapping
+from typing import Mapping
 
 from wandb.sdk.artifacts.artifact_manifest import ArtifactManifest
 from wandb.sdk.artifacts.artifact_manifest_entry import ArtifactManifestEntry
@@ -30,19 +30,9 @@ class ArtifactManifestV1(ArtifactManifest):
         storage_policy_config = manifest_json.get("storagePolicyConfig", {})
         storage_policy_cls = StoragePolicy.lookup_by_name(storage_policy_name)
 
-        entries: Mapping[str, ArtifactManifestEntry]
         entries = {
-            name: ArtifactManifestEntry(
-                path=name,
-                digest=val["digest"],
-                birth_artifact_id=val.get("birthArtifactID"),
-                ref=val.get("ref"),
-                size=val.get("size"),
-                extra=val.get("extra"),
-                local_path=val.get("local_path"),
-                skip_cache=val.get("skip_cache"),
-            )
-            for name, val in manifest_json["contents"].items()
+            path: ArtifactManifestEntry(**{**val, "path": path})
+            for path, val in manifest_json["contents"].items()
         }
 
         return cls(
@@ -64,25 +54,15 @@ class ArtifactManifestV1(ArtifactManifest):
         system. We don't need to include the local paths in the artifact manifest
         contents.
         """
-        contents = {}
-        for name, entry in sorted(self.entries.items(), key=itemgetter(0)):
-            json_entry: dict[str, Any] = {
-                "digest": entry.digest,
-            }
-            if entry.birth_artifact_id:
-                json_entry["birthArtifactID"] = entry.birth_artifact_id
-            if entry.ref:
-                json_entry["ref"] = entry.ref
-            if entry.extra:
-                json_entry["extra"] = entry.extra
-            if entry.size is not None:
-                json_entry["size"] = entry.size
-            contents[name] = json_entry
+        omit_entry_fields = {"path", "local_path", "skip_cache"}
         return {
-            "version": self.__class__.version(),
+            "version": self.version(),
             "storagePolicy": self.storage_policy.name(),
             "storagePolicyConfig": self.storage_policy.config() or {},
-            "contents": contents,
+            "contents": {
+                path: entry.model_dump(exclude=omit_entry_fields, exclude_defaults=True)
+                for path, entry in self.entries.items()
+            },
         }
 
     def digest(self) -> HexMD5:
