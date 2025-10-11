e4dafe5b6ddcd8eae321f666a92e3b51f0e03cd0
tonyyli-wandb <tony.li@wandb.com>
2025-10-11 09:38:50 -0700

    clean up codegen

--- tools/graphql_codegen/plugin.py
+++ tools/graphql_codegen/plugin.py
@@ -8,10 +8,13 @@ For more info, see:
 from __future__ import annotations
 
 import ast
+import sys
 from collections import defaultdict, deque
 from contextlib import suppress
 from itertools import chain, groupby
+from operator import attrgetter
 from pathlib import Path
+from shutil import rmtree
 from typing import Any, ClassVar, Iterable, Iterator
 
 from ariadne_codegen import Plugin
@@ -82,29 +85,25 @@ class FixFragmentOrder(Plugin):
         class_defs = grouped_stmts.pop(ast.ClassDef)
 
         # Drop the `.model_rebuild()` statements (we'll regenerate them)
-        _ = grouped_stmts.pop(ast.Expr)
+        grouped_stmts.pop(ast.Expr)
 
         # Since we've popped all the expected statement groups, verify there's nothing left
         if grouped_stmts:
             raise ValueError(f"Unexpected statements in module: {list(grouped_stmts)}")
+
+        # Deterministically reorder the class definitions/model_rebuild() statements,
+        # ensuring parent classes are defined first
+        sorter = ClassDefSorter(class_defs)
+        class_defs = sorter.sort_class_defs(class_defs)
 
         # For safety, we're going to apply `.model_rebuild()` to all generated fragment types
         # This'll prevent errors that pop up in pydantic v1 like:
         #
         #   pydantic.errors.ConfigError: field "node" not yet prepared so type is still a
         #   ForwardRef, you might need to call FilesFragmentEdges.update_forward_refs().
-        model_rebuilds = [
-            make_model_rebuild(class_def.name) for class_def in class_defs
-        ]
+        model_rebuilds = [make_model_rebuild(cls_def.name) for cls_def in class_defs]
 
-        # Deterministically reorder the class definitions/model_rebuild() statements,
-        # ensuring parent classes are defined first
-        sorter = ClassDefSorter(class_defs)
-        module.body = [
-            *imports,
-            *sorter.sort_class_defs(class_defs),
-            *sorter.sort_model_rebuilds(model_rebuilds),
-        ]
+        module.body = [*imports, *class_defs, *model_rebuilds]
         return module
 
 
@@ -112,28 +111,21 @@ class ClassDefSorter:
     """A sorter for a collection of class definitions."""
 
     def __init__(self, class_defs: Iterable[ast.ClassDef]) -> None:
-        #: Used to topologically sort the class definitions (which may depend on each other)
-        self.toposorter = TopologicalSorter()
+        # Topologically sort the class definitions (which may depend on each other)
+        # Class definitions are pre-sorted so that the final order is deterministic.
+        class_dependencies = {  # Maps {class_name -> [base_class_names]}
+            cls_def.name: base_class_names(cls_def)
+            for cls_def in sorted(class_defs, key=attrgetter("name"))
+        }
 
-        # Pre-sort the class definitions to ensure deterministic final topological order
-        for class_def in sorted(class_defs, key=lambda cls: cls.name):
-            self.toposorter.add(class_def.name, *base_class_names(class_def))
-
-        #: The deterministic, topologically sorted order of class definitions
-        self.static_order: list[str] = list(self.toposorter.static_order())
+        # The deterministic, topologically sorted order of class names
+        self.names = list(TopologicalSorter(class_dependencies).static_order())
 
     def sort_class_defs(self, class_defs: Iterable[ast.ClassDef]) -> list[ast.ClassDef]:
         """Return the class definitions in topologically sorted order."""
         return sorted(
             class_defs,
-            key=lambda class_def: self.static_order.index(class_def.name),
-        )
-
-    def sort_model_rebuilds(self, model_rebuilds: Iterable[ast.Expr]) -> list[ast.Expr]:
-        """Return the model rebuild statements in topologically sorted order."""
-        return sorted(
-            model_rebuilds,
-            key=lambda expr: self.static_order.index(expr.value.func.value.id),
+            key=lambda class_def: self.names.index(class_def.name),
         )
 
 
@@ -149,25 +141,25 @@ class GraphQLCodegenPlugin(Plugin):
     schema: GraphQLSchema
     config_dict: dict[str, Any]
 
-    #: The directory where the generated modules will be added
     package_dir: Path
-    #: Generated classes that we don't need in the final code
+    """The directory where the generated modules will be added."""
+
     classes_to_drop: set[str]
-    #: Maps GraphQL interface type names to the concrete GraphQL object type names that implement them
+    """Generated classes that we don't need in the final code."""
+
     interface2typenames: dict[str, list[str]]
+    """Maps GraphQL interface type names to the concrete GraphQL object type names that implement them."""
 
     # From ariadne-codegen, we don't currently need the generated httpx client,
-    # exceptions, etc., so drop these generated modules in favor of
-    # the existing internal GQL client.
-    modules_to_drop: ClassVar[frozenset[str]] = frozenset(
-        {
-            "async_base_client",
-            "base_client",
-            "base_model",  # We'll swap in a module with our own custom base class
-            "client",
-            "exceptions",
-        }
-    )
+    # base model, exceptions, etc., so drop these generated modules in favor of
+    # the existing, internal GQL client.
+    modules_to_drop: ClassVar[set[str]] = {
+        "async_base_client",
+        "base_client",
+        "base_model",  # We'll swap in a module with our own custom base class
+        "client",
+        "exceptions",
+    }
 
     def __init__(self, schema: GraphQLSchema, config_dict: dict[str, Any]) -> None:
         super().__init__(schema, config_dict)
@@ -180,6 +172,9 @@ class GraphQLCodegenPlugin(Plugin):
 
         self.classes_to_drop = set()
         self.interface2typenames = {}
+
+        # Remove any previously-generated files
+        self._remove_target_package_dir()
 
         # HACK: Override the default python type that ariadne-codegen uses for GraphQL's `ID` type.
         # See: https://github.com/mirumee/ariadne-codegen/issues/316
@@ -194,6 +189,17 @@ class GraphQLCodegenPlugin(Plugin):
         # like `__type`, `__schema`, etc.  Inject them here so codegen can proceed.
         if query_type := self.schema.query_type:
             query_type.fields["__type"] = TypeMetaFieldDef
+
+    def _remove_target_package_dir(self) -> None:
+        """Remove the existing generated files in the target package directory, if any."""
+        # Only remove existing files if `shutil.rmtree` is safe to use on the current platform.
+        if not rmtree.avoids_symlink_attacks:
+            sys.stdout.write(f"Skipping removal of {self.package_dir!s}\n")
+            return
+
+        with suppress(FileNotFoundError):
+            sys.stdout.write(f"Removing existing files in: {self.package_dir!s}\n")
+            rmtree(self.package_dir)
 
     def generate_init_code(self, generated_code: str) -> str:
         # This should be the last hook in the codegen process, after all modules have been generated.
@@ -343,35 +349,25 @@ class GraphQLCodegenPlugin(Plugin):
 
     @staticmethod
     def _filter_init_imports(
-        stmts: Iterable[ast.stmt],
-        omit_modules: Iterable[str],
-        omit_names: Iterable[str],
+        stmts: Iterable[ast.stmt], omit_modules: set[str], omit_names: set[str]
     ) -> Iterator[ast.ImportFrom]:
         """Yield only import statements to keep from the given module statements."""
-        import_from_stmts = (
-            stmt
-            for stmt in stmts
-            # Ignore imports from modules that are being dropped
-            if is_import_from(stmt) and (stmt.module not in omit_modules)
-        )
-        excluded_names = set(omit_names)
-        for stmt in import_from_stmts:
-            # Keep only imported names that aren't being dropped
-            kept_names = sorted(set(imported_names(stmt)) - excluded_names)
-            yield make_import_from(stmt.module, kept_names, level=stmt.level)
+        for stmt in stmts:
+            # Keep only imports from modules that aren't being dropped
+            if is_import_from(imp := stmt) and (imp.module not in omit_modules):
+                # Keep only imported names that aren't being dropped
+                kept_names = sorted(set(imported_names(imp)) - omit_names)
+                yield make_import_from(imp.module, kept_names, level=imp.level)
 
 
 class PydanticModuleRewriter(ast.NodeTransformer):
     """Applies various modifications to a generated module with pydantic classes."""
 
-    def visit_Module(self, node: ast.Module) -> ast.Module:
+    def visit_Module(self, node: ast.Module) -> Any:
         # Prepend shared import statements to the module. Ruff will clean this up later.
         node.body = [
             make_import_from("__future__", "annotations"),
-            make_import_from(
-                "wandb._pydantic",
-                [GQL_INPUT, GQL_RESULT, TYPENAME_TYPE],
-            ),
+            make_import_from("wandb._pydantic", [GQL_INPUT, GQL_RESULT, TYPENAME_TYPE]),
             make_import_from("typing_extensions", TYPING_EXTENSIONS_TYPES),
             *node.body,
         ]
@@ -386,7 +382,7 @@ class PydanticModuleRewriter(ast.NodeTransformer):
         else:
             return node  # Otherwise, keep the import as-is
 
-    def visit_AnnAssign(self, node: ast.AnnAssign) -> ast.AnnAssign:
+    def visit_AnnAssign(self, node: ast.AnnAssign) -> Any:
         if node.target.id == "typename__":
             # e.g. BEFORE: `typename__: Literal["MyType"] = Field(alias="__typename")`
             # e.g. AFTER:  `typename__: Typename[Literal["MyType"]]`
@@ -429,12 +425,12 @@ class RedundantClassReplacer(ast.NodeTransformer):
     def __init__(self, replacement_names: dict[str, str]):
         self.replacement_names = replacement_names
 
-    def visit_ClassDef(self, node: ast.ClassDef) -> ast.ClassDef:
+    def visit_ClassDef(self, node: ast.ClassDef) -> Any:
         if node.name in self.replacement_names:
             return None
         return self.generic_visit(node)
 
-    def visit_Name(self, node: ast.Name) -> ast.Name:
+    def visit_Name(self, node: ast.Name) -> Any:
         # node.id may be the name of the hinted type, e.g. `MyType`
         # or an implicit forward ref, e.g. `"MyType"`, `'MyType'`
         unquoted_name = node.id.strip("'\"")
