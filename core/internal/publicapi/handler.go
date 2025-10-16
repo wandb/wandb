f16941daede55681388d8797e9a0f077ce2a677c
Jacob Romero <jacob.romero@wandb.com>
2025-10-16 13:33:50 -0700

    remove apiwork

--- core/internal/publicapi/handler.go
+++ core/internal/publicapi/handler.go
@@ -1,7 +1,6 @@
 package publicapi
 
 import (
-	"github.com/wandb/wandb/core/internal/publicapi/work"
 	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
 	"golang.org/x/sync/errgroup"
 )
@@ -15,7 +14,7 @@ func NewApiRequestHandler() *ApiRequestHandler {
 	errgroup.SetLimit(10)
 
 	return &ApiRequestHandler{
-		errgroup: errgroup,
+		errgroup:                  errgroup,
 	}
 }
 
@@ -25,26 +24,6 @@ func (p *ApiRequestHandler) HandleRequest(
 	respondFn func(response *spb.ServerResponse),
 ) {
 	p.errgroup.Go(func() error {
-		work := p.parseApiRequest(request)
-		if work == nil {
-			return nil
-		}
-
-		apiResponse := work.Process()
-		respondFn(&spb.ServerResponse{
-			RequestId: id,
-			ServerResponseType: &spb.ServerResponse_ApiResponse{
-				ApiResponse: apiResponse,
-			},
-		})
 		return nil
 	})
-}
-
-// parseApiRequest parses an API request and returns the appropriate work item,
-// in order to process the request.
-func (p *ApiRequestHandler) parseApiRequest(
-	request *spb.ApiRequest,
-) work.ApiWork {
-	return &work.NoopWork{}
 }
