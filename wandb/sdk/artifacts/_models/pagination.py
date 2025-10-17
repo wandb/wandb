742712de246faf496e3acfcddb623a79c0deca72
tonyyli-wandb <tony.li@wandb.com>
2025-10-17 13:27:54 -0700

    Revert "fix(artifacts): when fetching an artifact by membership, use aliases directly from membership data"
    This reverts commit 7eb017f8b3a1cd09fb9c61e85cf01682c0733a85.

--- wandb/sdk/artifacts/_models/pagination.py
+++ wandb/sdk/artifacts/_models/pagination.py
@@ -3,15 +3,22 @@
 from wandb._pydantic import Connection, ConnectionWithTotal
 
 from .._generated.fragments import (
+    ArtifactCollectionFragment,
     ArtifactFragment,
     ArtifactTypeFragment,
     FileFragment,
     FileWithUrlFragment,
+    RegistryCollectionFragment,
+    RegistryFragment,
 )
 
 ArtifactTypeConnection = Connection[ArtifactTypeFragment]
+ArtifactCollectionConnection = ConnectionWithTotal[ArtifactCollectionFragment]
 
 FileWithUrlConnection = Connection[FileWithUrlFragment]
 ArtifactFileConnection = Connection[FileFragment]
 
 RunArtifactConnection = ConnectionWithTotal[ArtifactFragment]
+
+RegistryConnection = Connection[RegistryFragment]
+RegistryCollectionConnection = ConnectionWithTotal[RegistryCollectionFragment]
