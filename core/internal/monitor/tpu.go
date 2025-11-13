8c6b7fc20e3305953be858cad2050c61c3dd55f7
Dmitry Duev <dmitryduev@users.noreply.github.com>
2025-11-12 16:40:51 -0800

    chore: log available TPU metric names (#10488)
    Co-authored-by: Katia Patkin <kptkin@users.noreply.github.com>

--- core/internal/monitor/tpu.go
+++ core/internal/monitor/tpu.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/wandb/wandb/core/internal/monitor/tpuproto"
+	"github.com/wandb/wandb/core/internal/observability"
 	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/local"
@@ -44,7 +45,16 @@ type TPUChip struct {
 }
 
 type RuntimeMetricServiceClient interface {
-	GetRuntimeMetric(ctx context.Context, in *tpuproto.MetricRequest, opts ...grpc.CallOption) (*tpuproto.MetricResponse, error)
+	GetRuntimeMetric(
+		ctx context.Context,
+		in *tpuproto.MetricRequest,
+		opts ...grpc.CallOption,
+	) (*tpuproto.MetricResponse, error)
+	ListSupportedMetrics(
+		ctx context.Context,
+		in *tpuproto.ListSupportedMetricsRequest,
+		opts ...grpc.CallOption,
+	) (*tpuproto.ListSupportedMetricsResponse, error)
 }
 
 // TPU represents a TPU resource with gRPC connection and client.
@@ -63,11 +73,14 @@ type TPU struct {
 
 	// Total number of TPU devices detected.
 	count int
+
+	// logger is the debug logger.
+	logger *observability.CoreLogger
 }
 
 // NewTPU creates a new TPU instance by detecting local TPU chips and initializing the gRPC connection.
-func NewTPU() *TPU {
-	t := &TPU{}
+func NewTPU(logger *observability.CoreLogger) *TPU {
+	t := &TPU{logger: logger}
 
 	chip, count := getLocalTPUChips()
 	if count == 0 {
@@ -286,9 +299,20 @@ func (t *TPU) getMetrics(metricName TPUMetricName) ([]*tpuproto.Metric, error) {
 }
 
 // Probe returns the TPU metadata.
-func (t *TPU) Probe(_ context.Context) *spb.EnvironmentRecord {
+func (t *TPU) Probe(ctx context.Context) *spb.EnvironmentRecord {
 	if t.count == 0 {
 		return nil
+	}
+
+	// Log available metric names.
+	req := &tpuproto.ListSupportedMetricsRequest{}
+	resp, err := t.client.ListSupportedMetrics(ctx, req)
+	if err == nil {
+		supportedMetrics := []string{}
+		for _, sm := range resp.SupportedMetric {
+			supportedMetrics = append(supportedMetrics, sm.MetricName)
+		}
+		t.logger.Debug("tpu: supported metrics", "metrics", supportedMetrics)
 	}
 
 	return &spb.EnvironmentRecord{
