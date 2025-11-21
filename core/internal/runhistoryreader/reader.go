43aa11e682eb287e084aa6de71deb393cdb679f7
Jacob Romero <jacob.romero@wandb.com>
2025-11-20 18:12:51 -0800

    feat: add get run history file urls from backend (#10568)
    Description
    -----------
    <!--
    Include reference to internal ticket "Fixes WB-NNNNN" and/or GitHub issue "Fixes #NNNN" (if applicable)
    -->
    
    What does the PR do? Include a concise description of the PR contents.
    
    This PR adds implementation for functions in `runhistoryreader.reader` to get signed urls from the backend allowing for downloading or querying files via http requests.
    
    <!--
    NEW: We're using a new changelog format that's more useful for users. Please
    see CHANGELOG.unreleased.md for details and update on relevant changes such as feature
    additions, bug fixes, or removals/deprecations.
    -->
    - [ ] I updated CHANGELOG.unreleased.md, or it's not applicable
    
    
    Testing
    -------
    How was this PR tested?
    
    <!--
    Ensure PR title compliance with the [conventional commits standards](https://github.com/wandb/wandb/blob/main/CONTRIBUTING.md#conventional-commits)
    -->

--- core/internal/runhistoryreader/reader.go
+++ core/internal/runhistoryreader/reader.go
@@ -1,14 +1,25 @@
 package runhistoryreader
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/Khan/genqlient/graphql"
+	"github.com/wandb/wandb/core/internal/gql"
+	"github.com/wandb/wandb/core/internal/runhistoryreader/parquet/iterator"
 )
 
 // HistoryReader downloads and reads an exisiting run's logged metrics.
 type HistoryReader struct {
-	entity  string
-	project string
-	runId   string
+	entity        string
+	graphqlClient graphql.Client
+	httpClient    *http.Client
+	project       string
+	runId         string
 }
 
 // New returns a new HistoryReader.
@@ -16,11 +27,15 @@ func New(
 	entity string,
 	project string,
 	runId string,
+	graphqlClient graphql.Client,
+	httpClient *http.Client,
 ) *HistoryReader {
 	return &HistoryReader{
-		entity:  entity,
-		project: project,
-		runId:   runId,
+		entity:        entity,
+		graphqlClient: graphqlClient,
+		httpClient:    httpClient,
+		project:       project,
+		runId:         runId,
 	}
 }
 
@@ -33,4 +48,60 @@ func (h *HistoryReader) GetHistorySteps(
 ) error {
 	// TODO: Implement
 	return fmt.Errorf("not implemented")
+}
+
+// getRunHistoryFileUrls gets URLs
+// that can be used to download a run's history files.
+//
+// The order of the URLs returned is not guaranteed
+// to be the same order as the order the run history partitions were created in.
+func (h *HistoryReader) getRunHistoryFileUrls() ([]string, error) {
+	response, err := gql.RunParquetHistory(
+		context.Background(),
+		h.graphqlClient,
+		h.entity,
+		h.project,
+		h.runId,
+		[]string{iterator.StepKey},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.GetProject() == nil || response.GetProject().GetRun() == nil {
+		return nil, fmt.Errorf("no parquet history found for run %s", h.runId)
+	}
+
+	signedUrls := response.GetProject().GetRun().GetParquetHistory().ParquetUrls
+	return signedUrls, nil
+}
+
+func (h *HistoryReader) downloadRunHistoryFile(
+	fileUrl string,
+	downloadDir string,
+	fileName string,
+) error {
+	err := os.MkdirAll(downloadDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(filepath.Join(downloadDir, fileName))
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	resp, err := h.httpClient.Get(fileUrl)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(file, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
