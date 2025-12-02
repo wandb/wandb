84f2121db41617df3bb82117159131e96b65caed
Dmitry Duev <dima@wandb.com>
2025-12-02 00:00:58 -0800

    undo arrow

--- core/vendor/github.com/apache/arrow-go/v18/parquet/pqarrow/encode_dict_compute.go
+++ core/vendor/github.com/apache/arrow-go/v18/parquet/pqarrow/encode_dict_compute.go
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.18
+//go:build go1.18 && !parquet_read_only
 
 package pqarrow
 
