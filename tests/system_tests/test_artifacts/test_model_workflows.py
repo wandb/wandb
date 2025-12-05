7b12277f64c85e4ff95ecf78cce78a60e63288c1
tonyyli-wandb <tony.li@wandb.com>
2025-12-05 15:21:39 -0800

    cleanup

--- tests/system_tests/test_artifacts/test_model_workflows.py
+++ tests/system_tests/test_artifacts/test_model_workflows.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-import pathlib
+from pathlib import Path
 
 import wandb
 from pytest import raises
@@ -21,10 +21,7 @@ def test_offline_link_artifact(user):
             run.link_artifact(FakeArtifact(), "entity/project/portfolio", "latest")
 
 
-def test_log_model(
-    tmp_path: pathlib.Path,
-    user,
-):
+def test_log_model(tmp_path: Path, user):
     with wandb.init() as run:
         local_path = tmp_path / "boom.txt"
         local_path.write_text("testing")
@@ -36,10 +33,7 @@ def test_log_model(
         assert file == f"{env.get_artifact_dir()}/test-model:v0/boom.txt"
 
 
-def test_use_model(
-    tmp_path: pathlib.Path,
-    user,
-):
+def test_use_model(tmp_path: Path, user):
     with wandb.init() as run:
         local_path = tmp_path / "boom.txt"
         local_path.write_text("testing")
@@ -51,10 +45,7 @@ def test_use_model(
         assert file == f"{env.get_artifact_dir()}/test-model:v0/boom.txt"
 
 
-def test_use_model_error_artifact_type(
-    user,
-    tmp_path: pathlib.Path,
-):
+def test_use_model_error_artifact_type(user, tmp_path: Path):
     with wandb.init() as run:
         local_path = tmp_path / "boom.txt"
         local_path.write_text("testing")
@@ -65,10 +56,7 @@ def test_use_model_error_artifact_type(
             _ = run.use_model("test-model:v0")
 
 
-def test_link_model(
-    user,
-    tmp_path: pathlib.Path,
-):
+def test_link_model(user, tmp_path: Path):
     with wandb.init() as run:
         local_path = tmp_path / "boom.txt"
         local_path.write_text("testing")
@@ -80,10 +68,7 @@ def test_link_model(
         assert file == f"{env.get_artifact_dir()}/test_model:v0/boom.txt"
 
 
-def test_link_model_error_artifact_type(
-    user,
-    tmp_path: pathlib.Path,
-):
+def test_link_model_error_artifact_type(user, tmp_path: Path):
     with wandb.init() as run:
         local_path = tmp_path / "boom.txt"
         local_path.write_text("testing")
@@ -96,10 +81,7 @@ def test_link_model_error_artifact_type(
             run.link_model(local_path, "test_portfolio", "test_model")
 
 
-def test_link_model_log_new_artifact(
-    user,
-    tmp_path: pathlib.Path,
-):
+def test_link_model_log_new_artifact(user, tmp_path: Path):
     with wandb.init() as run:
         local_path = tmp_path / "boom.txt"
         local_path.write_text("testing")
