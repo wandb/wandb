2389d178efd30a3b86318fefafd14b9df27f74f3
Timofey Peshin <timoffex@gmail.com>
2025-12-29 09:37:58 -0800

    run url printing

--- core/internal/runbranch/state_test.go
+++ core/internal/runbranch/state_test.go
@@ -98,3 +98,46 @@ func TestSetsSummary(t *testing.T) {
 		},
 		updatedProto.Summary)
 }
+
+func TestRunPath_URL(t *testing.T) {
+	t.Run("produces clean escaped URL", func(t *testing.T) {
+		path := runbranch.RunPath{
+			Entity:  "entity with space",
+			Project: "project?",
+			// Common special characters people include in run IDs.
+			// Just the forward slash should be URL-escaped.
+			RunID: "me@machine/x=1&y=+$2",
+		}
+
+		url, err := path.URL("https://my-web-ui///")
+
+		assert.NoError(t, err)
+		assert.Equal(t,
+			"https://my-web-ui/entity%20with%20space/project%3F/runs/me@machine%2Fx=1&y=+$2",
+			url)
+	})
+
+	t.Run("no entity", func(t *testing.T) {
+		path := runbranch.RunPath{Project: "project", RunID: "id"}
+
+		_, err := path.URL("https://wandb.ai")
+
+		assert.ErrorContains(t, err, "no entity")
+	})
+
+	t.Run("no project", func(t *testing.T) {
+		path := runbranch.RunPath{Entity: "entity", RunID: "id"}
+
+		_, err := path.URL("https://wandb.ai")
+
+		assert.ErrorContains(t, err, "no project")
+	})
+
+	t.Run("no ID", func(t *testing.T) {
+		path := runbranch.RunPath{Entity: "entity", Project: "project"}
+
+		_, err := path.URL("https://wandb.ai")
+
+		assert.ErrorContains(t, err, "no run ID")
+	})
+}
