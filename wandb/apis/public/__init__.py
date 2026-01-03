988d867b5b4acabac89583a1935de8e50c819f31
tonyyli-wandb <tony.li@wandb.com>
2026-01-03 14:27:23 -0800

    fix(sdk): validated types for Files GQL data

--- wandb/apis/public/__init__.py
+++ wandb/apis/public/__init__.py
@@ -52,7 +52,7 @@ from wandb.apis.public.artifacts import (
     RunArtifacts,
 )
 from wandb.apis.public.automations import Automations
-from wandb.apis.public.files import FILE_FRAGMENT, File, Files
+from wandb.apis.public.files import File, Files
 from wandb.apis.public.history import BetaHistoryScan, HistoryScan, SampledHistoryScan
 from wandb.apis.public.integrations import SlackIntegrations, WebhookIntegrations
 from wandb.apis.public.jobs import (
