292f430e5757968ca42cd685ef2b90e629adb3db
tonyyli-wandb <tony.li@wandb.com>
2026-01-03 22:41:32 -0800

    fix(sdk): validated types for Jobs GQL data

--- wandb/apis/_generated/operations.py
+++ wandb/apis/_generated/operations.py
@@ -10,10 +10,14 @@ __all__ = [
     "CREATE_USER_FROM_ADMIN_GQL",
     "DELETE_API_KEY_GQL",
     "DELETE_FILES_GQL",
+    "DELETE_FROM_RUN_QUEUE_GQL",
     "DELETE_INVITE_GQL",
     "DELETE_RUN_GQL",
+    "DELETE_RUN_QUEUE_GQL",
+    "FETCH_RUN_QUEUE_FROM_PROJECT_GQL",
     "GENERATE_API_KEY_GQL",
     "GET_DEFAULT_ENTITY_GQL",
+    "GET_DEFAULT_RESOURCE_CONFIG_GQL",
     "GET_LIGHT_RUNS_GQL",
     "GET_LIGHT_RUN_GQL",
     "GET_PROJECTS_GQL",
@@ -24,6 +28,10 @@ __all__ = [
     "GET_RUN_GQL",
     "GET_RUN_HISTORY_GQL",
     "GET_RUN_HISTORY_KEYS_GQL",
+    "GET_RUN_QUEUE_ITEMS_GQL",
+    "GET_RUN_QUEUE_ITEMS_LEGACY_GQL",
+    "GET_RUN_QUEUE_ITEM_GQL",
+    "GET_RUN_QUEUE_METADATA_GQL",
     "GET_RUN_SAMPLED_HISTORY_GQL",
     "GET_RUN_STATE_GQL",
     "GET_SWEEPS_GQL",
@@ -122,6 +130,111 @@ query ProbeInputFields($type: String!) {
   typeInfo: __type(name: $type) {
     inputFields {
       name
+    }
+  }
+}
+"""
+
+GET_RUN_QUEUE_ITEMS_LEGACY_GQL = """
+query GetRunQueueItemsLegacy($projectName: String!, $entityName: String!, $runQueue: String!) {
+  project(name: $projectName, entityName: $entityName) {
+    runQueue(name: $runQueue) {
+      runQueueItems {
+        edges {
+          node {
+            id
+            state
+            associatedRunId
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+GET_RUN_QUEUE_ITEM_GQL = """
+query GetRunQueueItem($projectName: String!, $entityName: String!, $runQueue: String!, $itemId: ID!) {
+  project(name: $projectName, entityName: $entityName) {
+    runQueue(name: $runQueue) {
+      runQueueItem(id: $itemId) {
+        id
+        state
+        associatedRunId
+      }
+    }
+  }
+}
+"""
+
+FETCH_RUN_QUEUE_FROM_PROJECT_GQL = """
+query FetchRunQueueFromProject($entityName: String!, $projectName: String!, $runQueueName: String!) {
+  project(name: $projectName, entityName: $entityName) {
+    runQueue(name: $runQueueName) {
+      id
+    }
+  }
+}
+"""
+
+DELETE_FROM_RUN_QUEUE_GQL = """
+mutation DeleteFromRunQueue($queueID: ID!, $runQueueItemId: ID!) {
+  deleteFromRunQueue(input: {queueID: $queueID, runQueueItemId: $runQueueItemId}) {
+    success
+    clientMutationId
+  }
+}
+"""
+
+DELETE_RUN_QUEUE_GQL = """
+mutation DeleteRunQueue($id: ID!) {
+  deleteRunQueues(input: {queueIDs: [$id]}) {
+    success
+    clientMutationId
+  }
+}
+"""
+
+GET_RUN_QUEUE_METADATA_GQL = """
+query GetRunQueueMetadata($projectName: String!, $entityName: String!, $runQueue: String!) {
+  project(name: $projectName, entityName: $entityName) {
+    runQueue(name: $runQueue) {
+      id
+      access
+      defaultResourceConfigID
+      prioritizationMode
+      externalLinks
+    }
+  }
+}
+"""
+
+GET_DEFAULT_RESOURCE_CONFIG_GQL = """
+query GetDefaultResourceConfig($entityName: String!, $id: ID!) {
+  entity(name: $entityName) {
+    defaultResourceConfig(id: $id) {
+      config
+      resource
+      templateVariables {
+        name
+        schema
+      }
+    }
+  }
+}
+"""
+
+GET_RUN_QUEUE_ITEMS_GQL = """
+query GetRunQueueItems($projectName: String!, $entityName: String!, $runQueue: String!) {
+  project(name: $projectName, entityName: $entityName) {
+    runQueue(name: $runQueue) {
+      runQueueItems(first: 100) {
+        edges {
+          node {
+            id
+          }
+        }
+      }
     }
   }
 }
@@ -255,13 +368,7 @@ query GetRuns($project: String!, $entity: String!, $cursor: String, $perPage: In
   }
 }
 
-fragment PageInfoFragment on PageInfo {
-  __typename
-  endCursor
-  hasNextPage
-}
-
-fragment RunFragment on Run {
+fragment LightRunFragment on Run {
   __typename
   id
   tags
@@ -283,30 +390,12 @@ fragment RunFragment on Run {
     name
     username
   }
-  config
-  systemMetrics
-  summaryMetrics
-  historyKeys
 }
-"""
 
-GET_LIGHT_RUNS_GQL = """
-query GetLightRuns($project: String!, $entity: String!, $cursor: String, $perPage: Int = 50, $order: String, $filters: JSONString) {
-  project(name: $project, entityName: $entity) {
-    internalId
-    readOnly
-    runs(filters: $filters, after: $cursor, first: $perPage, order: $order) {
-      totalCount
-      pageInfo {
-        ...PageInfoFragment
-      }
-      edges {
-        node {
-          ...LightRunFragment
-        }
-      }
-    }
-  }
+fragment PageInfoFragment on PageInfo {
+  __typename
+  endCursor
+  hasNextPage
 }
 
 fragment LightRunFragment on Run {
@@ -341,10 +430,11 @@ fragment PageInfoFragment on PageInfo {
 """
 
 GET_RUN_GQL = """
-query GetRun($name: String!, $project: String!, $entity: String!) {
+query GetRun($name: String!, $project: String!, $entity: String!, $lazy: Boolean!) {
   project(name: $project, entityName: $entity) {
     run(name: $name) {
-      ...RunFragment
+      ...RunFragment @skip(if: $lazy)
+      ...LightRunFragment @include(if: $lazy)
     }
   }
 }
