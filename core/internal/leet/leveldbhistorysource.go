61e6940cea44d68634e1f3ee8df6120b34b5231b
Jacob Romero <jacob.romero@wanbd.com>
2026-02-01 15:27:13 -0800

    update to use api.RetryableClient

--- core/internal/leet/leveldbhistorysource.go
+++ core/internal/leet/leveldbhistorysource.go
@@ -66,6 +66,7 @@ func InitializeLevelDBHistorySource(
 		return InitMsg{Source: source}
 	}
 }
+
 // Read implements HistorySource.Read.
 func (hs *LevelDBHistorySource) Read(
 	chunkSize int,
--- core/internal/leet/model.go
+++ core/internal/leet/model.go
@@ -146,7 +146,6 @@ func (m *Model) Init() tea.Cmd {
 	return tea.Batch(cmds...)
 }
 
-
 // Update handles incoming events and updates the model accordingly.
 //
 // Implements tea.Model.Update.
