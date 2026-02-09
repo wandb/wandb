64ab746eeb8de3186d11cd29a01cfafd6f939c04
Dmitry Duev <dmitryduev@users.noreply.github.com>
2026-02-09 14:51:23 -0800

    move cow to a separate pr

--- core/internal/leet/workspace.go
+++ core/internal/leet/workspace.go
@@ -464,7 +464,6 @@ func (w *Workspace) renderMetrics() string {
 		logoContent := lipgloss.JoinVertical(
 			lipgloss.Center,
 			artStyle.Render(wandbArt),
-			artStyle.Render(sphericalCowInAVacuum),
 			artStyle.Render(leetArt),
 		)
 
