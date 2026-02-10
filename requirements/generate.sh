fb5e9c7b629e987763868dc3d235abcccaf0ae72
Timofey Peshin <timoffex@gmail.com>
2026-02-09 16:57:31 -0800

    chore: remove python 3.8 requirements files (#11307)
    Added in PR #11298, but we dropped 3.8 while that was merging 😄

--- requirements/generate.sh
+++ requirements/generate.sh
@@ -11,23 +11,14 @@ function platform_python_name() {
     fi
 }
 
-# Extracts "major.minor" from a version specifier ($1).
-function major_minor() {
-    echo "$1" | cut -d. -f1,2
-}
-
 # Compiles requirements for a Python version ($1) and platform ($2).
 function compile() {
     uv pip compile \
         --python-version "$1" \
         --python-platform "$(platform_python_name $2)" \
         requirements/requirements_dev.txt \
-        -o "requirements/requirements_dev.$(major_minor $1).$2.txt"
+        -o "requirements/requirements_dev.$1.$2.txt"
 }
-
-# Patch versions needed for 3.8 due to what seems like a uv bug.
-compile 3.8.20 darwin
-compile 3.8.20 linux
 
 compile 3.9 darwin
 compile 3.9 linux
