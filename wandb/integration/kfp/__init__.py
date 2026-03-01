258a5b78f2e8d65327ab9764baf24ae5ef9ae2a3
Ayush Thakur <mein2work@gmail.com>
2026-03-01 19:31:22 +0530

    deprecate kfp v1 and add test for v2

--- wandb/integration/kfp/__init__.py
+++ wandb/integration/kfp/__init__.py
@@ -9,7 +9,19 @@ except Exception:
 if _KFP_V2:
     from .wandb_logging import wandb_log_v2 as wandb_log
 else:
+    from wandb.proto.wandb_telemetry_pb2 import Deprecated
+    from wandb.sdk.lib.deprecation import warn_and_record_deprecation
+
     from .wandb_logging import wandb_log
+
+    warn_and_record_deprecation(
+        feature=Deprecated(kfp_v1_wandb_log=True),
+        message=(
+            "KFP v1 (kfp<2.0.0) support for @wandb_log is deprecated "
+            "and will be removed in a future release. "
+            "Please upgrade to kfp>=2.0.0."
+        ),
+    )
 
 from .kfp_patch import patch_kfp, unpatch_kfp
 
