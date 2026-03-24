1bd52904f37f457d227d3c2e9357432797ebc2e9
Timofey Peshin <timoffex@gmail.com>
2026-03-23 17:35:31 -0700

    refactor: make ServerFeature 0 map to unspecified (#11551)
    The ServerFeature enum breaks many protobuf conventions, one of which is actually important: making 0 an "unspecified" value for forward compatibility.
    
    It seems the enum is used by-name in Python, and the integers are not meaningful other than in FeatureProvider, so it should be safe to change. Before this PR, an unknown enabled feature would make FeatureProvider think "LARGE_FILENAMES" is enabled.

--- gpu_stats/src/wandb_internal.rs
+++ gpu_stats/src/wandb_internal.rs
@@ -2886,8 +2886,9 @@ pub struct JobInputRequest {
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum ServerFeature {
+    Unspecified = 0,
     /// Indicates that the server supports recieving an array of filenames as metadata.
-    LargeFilenames = 0,
+    LargeFilenames = 17,
     /// Indicates that the server supports adding tags to artifacts.
     ArtifactTags = 1,
     /// Indicates that the server supports client IDs for artifact reference urls.
@@ -2930,6 +2931,7 @@ impl ServerFeature {
     /// (if the ProtoBuf definition does not change) and safe for programmatic use.
     pub fn as_str_name(&self) -> &'static str {
         match self {
+            Self::Unspecified => "SERVER_FEATURE_UNSPECIFIED",
             Self::LargeFilenames => "LARGE_FILENAMES",
             Self::ArtifactTags => "ARTIFACT_TAGS",
             Self::ClientIds => "CLIENT_IDS",
@@ -2966,6 +2968,7 @@ impl ServerFeature {
     /// Creates an enum from field names used in the ProtoBuf definition.
     pub fn from_str_name(value: &str) -> ::core::option::Option<Self> {
         match value {
+            "SERVER_FEATURE_UNSPECIFIED" => Some(Self::Unspecified),
             "LARGE_FILENAMES" => Some(Self::LargeFilenames),
             "ARTIFACT_TAGS" => Some(Self::ArtifactTags),
             "CLIENT_IDS" => Some(Self::ClientIds),
