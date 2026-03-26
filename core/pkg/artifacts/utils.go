334432a7d68431bd5eb8973e3568f60bad4a6c0c
Timofey Peshin <timoffex@gmail.com>
2026-03-26 14:51:25 -0700

    create artifact input introspection

--- core/pkg/artifacts/utils.go
+++ core/pkg/artifacts/utils.go
@@ -16,28 +16,6 @@ func IsArtifactRegistryProject(project string) bool {
 	return strings.HasPrefix(project, RegistryProjectPrefix)
 }
 
-// GetGraphQLInputFields returns the fields of a GraphQL input type given its name.
-func GetGraphQLInputFields(
-	ctx context.Context,
-	client graphql.Client,
-	typeName string,
-) ([]string, error) {
-	response, err := gql.InputFields(ctx, client, typeName)
-	if err != nil {
-		return nil, err
-	}
-	typeInfo := response.GetTypeInfo()
-	if typeInfo == nil {
-		return nil, fmt.Errorf("unable to verify allowed fields for %s", typeName)
-	}
-	fields := typeInfo.GetInputFields()
-	fieldNames := make([]string, len(fields))
-	for i, field := range fields {
-		fieldNames[i] = field.GetName()
-	}
-	return fieldNames, nil
-}
-
 // GetGraphQLFields returns the fields for a given a GraphQL object type name into a string array.
 func GetGraphQLFields(
 	ctx context.Context,
