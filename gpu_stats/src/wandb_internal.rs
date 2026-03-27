717e9417c3776c8a586cbfccc34ff431e0948c66
Ishita Bindlish <ibindlish@gmail.com>
2026-03-27 14:10:26 -0700

    chore(artifacts): use artifact id instead of birth artifact id for download url (#11407)

--- gpu_stats/src/wandb_internal.rs
+++ gpu_stats/src/wandb_internal.rs
@@ -2923,6 +2923,9 @@ pub enum ServerFeature {
     TotalCountInFileConnection = 15,
     /// Indicates that the server supports filtering artifact collections when querying on a project
     ArtifactCollectionsFilteringSorting = 16,
+    /// Indicates that the server supports both the artifact id and the birth artifact id in the artifact file download
+    /// url.
+    ArtifactV2DownloadHandlerSupportsArtifactId = 18,
 }
 impl ServerFeature {
     /// String value of the enum field names used in the ProtoBuf definition.
@@ -2963,6 +2966,9 @@ impl ServerFeature {
             Self::ArtifactCollectionsFilteringSorting => {
                 "ARTIFACT_COLLECTIONS_FILTERING_SORTING"
             }
+            Self::ArtifactV2DownloadHandlerSupportsArtifactId => {
+                "ARTIFACT_V2_DOWNLOAD_HANDLER_SUPPORTS_ARTIFACT_ID"
+            }
         }
     }
     /// Creates an enum from field names used in the ProtoBuf definition.
@@ -3001,6 +3007,9 @@ impl ServerFeature {
             "TOTAL_COUNT_IN_FILE_CONNECTION" => Some(Self::TotalCountInFileConnection),
             "ARTIFACT_COLLECTIONS_FILTERING_SORTING" => {
                 Some(Self::ArtifactCollectionsFilteringSorting)
+            }
+            "ARTIFACT_V2_DOWNLOAD_HANDLER_SUPPORTS_ARTIFACT_ID" => {
+                Some(Self::ArtifactV2DownloadHandlerSupportsArtifactId)
             }
             _ => None,
         }
