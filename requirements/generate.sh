c073bb7a3fc9510b9c3c4c28c38b771493795e16
Pinglei Guo <pinglei.guo@wandb.com>
2026-04-22 12:36:29 -0700

    chore: use x86_64-manylinux_2_28 in generate.sh

--- requirements/generate.sh
+++ requirements/generate.sh
@@ -35,12 +35,16 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-# Maps platform.system() output ($1) to a platform understood by uv.
-function platform_python_name() {
-    if [ "$1" = darwin ]; then
+# Maps a Python version ($1) and platform.system() output ($2) to a platform
+# understood by uv.
+function uv_python_platform() {
+    if [ "$2" = darwin ]; then
         echo macos
+    elif [ "$1" = 3.13 ] && [ "$2" = linux ]; then
+        # rdkit only publishes CPython 3.13 Linux wheels for manylinux_2_28.
+        echo x86_64-manylinux_2_28
     else
-        echo "$1"
+        echo "$2"
     fi
 }
 
@@ -48,7 +52,7 @@ function platform_python_name() {
 function compile() {
     uv pip compile "${PIP_COMPILE_ARGS[@]}" \
         --python-version "$1" \
-        --python-platform "$(platform_python_name $2)" \
+        --python-platform "$(uv_python_platform "$1" "$2")" \
         requirements/requirements_dev.txt \
         -o "requirements/requirements_dev.$1.$2.txt" \
         >/dev/null  # https://github.com/astral-sh/uv/issues/3701
