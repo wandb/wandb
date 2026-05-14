036eb6cfc3c3feeb90a53d5fd730d311c6eee0c5
Kaelan Mikowicz <kmikowicz@corewave.com>
2026-05-14 15:04:23 -0700

    fix(sweeps): agent heartbeat should allow in-progress runs to finish on 404

--- wandb/cli/cli.py
+++ wandb/cli/cli.py
@@ -2194,7 +2194,6 @@ def agent(ctx, project, entity, count, forward_signals, sweep_id):
         )
     # TODO: handle other errors with correct exit codes
     except SweepNotFoundError:
-        wandb.termerror("Sweep was deleted or agent was not found. Stopping agent.")
         sys.exit(1)
 
     # you can send local commands like so:
