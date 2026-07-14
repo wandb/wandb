cd9f57fb596a0ea5947631d26ce509ece67cadb8
Geoff Hardy <5132793+geoffhardy@users.noreply.github.com>
2026-07-13 18:37:05 -0700

    update changelog

--- xpu/src/wandb_internal.rs
+++ xpu/src/wandb_internal.rs
@@ -842,7 +842,7 @@ pub struct RunRecord {
     /// Information about the source if this is a fork or rewind of another run.
     #[prost(message, optional, tag = "23")]
     pub branch_point: ::core::option::Option<BranchPoint>,
-    /// The user-provided resume mode to apply when syncing an offline run.
+    /// The user-provided resume mode to apply when syncing.
     ///
     /// This is input intent, unlike `resumed`, which is a backend result.
     #[prost(string, tag = "24")]
