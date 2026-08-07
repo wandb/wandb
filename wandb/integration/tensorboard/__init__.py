da14b7ef04f25c1fc02ba41f6300fdb555e6e2b6
Timofey Peshin <timoffex@gmail.com>
2026-08-07 16:40:24 -0700

    wandb tensorboard log

--- wandb/integration/tensorboard/__init__.py
+++ wandb/integration/tensorboard/__init__.py
@@ -1,10 +1,9 @@
 """wandb integration tensorboard module."""
 
-from .log import _log, log, reset_state, tf_summary_to_dict
+from .log import _log, reset_state, tf_summary_to_dict
 from .monkeypatch import patch, unpatch
 
 __all__ = [
     "patch",
     "unpatch",
-    "log",
 ]
