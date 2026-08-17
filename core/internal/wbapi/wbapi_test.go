19310a90358a7e0d564063ec6d1b14b957d3b583
Dmitry Duev <dmitryduev@users.noreply.github.com>
2026-08-17 16:37:30 -0700

    rm useless test

--- core/internal/wbapi/wbapi_test.go
+++ core/internal/wbapi/wbapi_test.go
@@ -2,63 +2,12 @@ package wbapi
 
 import (
 	"context"
-	"encoding/json"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/Khan/genqlient/graphql"
-	"google.golang.org/protobuf/proto"
-
 	spb "github.com/wandb/wandb/core/pkg/service_go_proto"
 )
-
-// stubGQLClient returns a canned GraphQL result for any request.
-type stubGQLClient struct {
-	respJSON string
-}
-
-func (c stubGQLClient) MakeRequest(
-	_ context.Context,
-	_ *graphql.Request,
-	resp *graphql.Response,
-) error {
-	return json.Unmarshal([]byte(c.respJSON), resp.Data)
-}
-
-func TestHandleRequestRoutesReadRunConsoleLogs(t *testing.T) {
-	api := &WandbAPI{
-		semaphore: make(chan struct{}, 1),
-		runHandler: NewRunHandler(stubGQLClient{
-			respJSON: `{"project": {"run": {"logLineCount": 1, "logLines": {
-				"edges": [{"node": {"number": 0, "timestamp": "t", "level": "info",
-				"label": "", "line": "hello"}, "cursor": "c0"}]}}}}`,
-		}),
-	}
-
-	response := api.HandleRequest(
-		context.Background(),
-		"request-id",
-		&spb.ApiRequest{
-			Request: &spb.ApiRequest_ReadRunConsoleLogsRequest{
-				ReadRunConsoleLogsRequest: &spb.ReadRunConsoleLogsRequest{
-					Entity:  "e",
-					Project: "p",
-					RunId:   "r",
-					Last:    proto.Int32(1),
-				},
-			},
-		},
-	)
-
-	logs := response.GetReadRunConsoleLogsResponse()
-	if logs == nil {
-		t.Fatalf("expected ReadRunConsoleLogsResponse, got %v", response)
-	}
-	if len(logs.GetLines()) != 1 || logs.GetLines()[0].GetContent() != "hello" {
-		t.Fatalf("unexpected lines: %v", logs.GetLines())
-	}
-}
 
 func TestHandleRequestUnknownTypeIsError(t *testing.T) {
 	// A newer client attached to an older wandb-core sends request types
