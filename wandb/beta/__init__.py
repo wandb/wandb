d0afcf5ded4a6098709d4796b5560ea850fa0e89
Dmitry Duev <dmitryduev@users.noreply.github.com>
2026-09-09 12:25:39 -0700

    resolve conflict

---
+++ wandb/beta/__init__.py
@@ -0,0 +1,8 @@
+"""Experimental features.
+
+Anything in this package may change or be removed in any release of wandb.
+"""
+
+from wandb.beta.local_api import LocalApi, LocalRun
+
+__all__ = ["LocalApi", "LocalRun"]
